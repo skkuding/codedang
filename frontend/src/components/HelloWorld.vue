6e8bb14023c5aec335155f6beb9c61e569c976f0
Jaemin Choi <1dotolee@gmail.com>
2022-03-30 04:17:52 +0000

    style: format all with prettier

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -9,8 +9,10 @@ defineProps<{
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <a target="_blank" href="https://vitejs.dev/">Vite</a> +
-      <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
+      <a target="_blank" href="https://vitejs.dev/">Vite</a>
+      +
+      <a target="_blank" href="https://vuejs.org/">Vue 3</a>
+      .
     </h3>
   </div>
 </template>
