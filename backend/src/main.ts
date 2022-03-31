8420cc9a585dd068e386067f10afa56543dd85ee
Jaemin Choi <1dotolee@gmail.com>
2022-03-31 20:40:30 +0900

    style: use prettier (#25)
    * style: apply prettier
    
    * style: use prettier from workspace root
    
    * style: correct weird html and vue formatting
    
    HTML tag가 잘못된 모양으로 formatting 되는 issue를 바로잡는다.
    https://github.com/prettier/prettier/issues/6096
    
    * style: format all with prettier

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,8 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  const app = await NestFactory.create(AppModule)
+  await app.listen(5000)
 }
-bootstrap();
+bootstrap()
