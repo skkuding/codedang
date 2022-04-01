589e718b99b55a34a02b549e1d338fe30b47ce29
Jaemin Choi <1dotolee@gmail.com>
2022-04-01 11:10:24 +0000

    style: configure eslint for typescript
    typescript-eslint를 이용해 typescript linting을 세팅한다.
    https://typescript-eslint.io/docs/linting/

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  extends: ['plugin:prettier/recommended']
+  env: { node: true },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ]
 }
