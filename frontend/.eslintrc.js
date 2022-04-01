01bf717107ac06d557f33a9c675479cb037e9286
Jaemin Choi <1dotolee@gmail.com>
2022-04-01 11:38:01 +0000

    style: configure eslint for vue

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    'vue/setup-compiler-macros': true
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended']
+}
