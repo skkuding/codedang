216a2c5e071aa6f1dfb3bb6fd605ec16b4b77a58
Jaemin Choi <1dotolee@gmail.com>
2022-04-03 04:35:31 +0000

    feat(fe): use iconify
    다양한 icon system을 쉽게 적용하기 위해 Iconify를 세팅한다.
    더불어 Vite plugin인 unplugin-icons를 이용해 bundle 시 사용된 아이콘의 svg만 포함시킨다.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), icons({ autoInstall: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
