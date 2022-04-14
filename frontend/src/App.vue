f00814533b83e638d788476f2ceb6188b1074357
Jimin Ha <jimin9038@g.skku.edu>
2022-04-14 15:31:30 +0900

    move home components into home

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import HelloWorld from './home/components/HelloWorld.vue'
+import TheWelcome from './home/components/TheWelcome.vue'
 </script>
 
 <template>
