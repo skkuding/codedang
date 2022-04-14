e0fa04a1632d64155cf088f15006d0cf9675f443
Jimin Ha <jimin9038@g.skku.edu>
2022-04-14 17:33:48 +0900

    fix: logo.svg src path

--- frontend/src/home/pages/index.vue
+++ frontend/src/home/pages/index.vue
@@ -8,7 +8,7 @@ import TheWelcome from '../components/TheWelcome.vue'
     <img
       alt="Vue logo"
       class="logo"
-      src="./common/assets/logo.svg"
+      src="@/common/assets/logo.svg"
       width="125"
       height="125"
     />
