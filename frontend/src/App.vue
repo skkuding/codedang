d8fada6e685352a94348fa1ddc3dccf607f7def4
Jimin Ha <jimin9038@g.skku.edu>
2022-04-14 15:35:07 +0900

    move logo.svg to common/assets

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,7 +8,7 @@ import TheWelcome from './home/components/TheWelcome.vue'
     <img
       alt="Vue logo"
       class="logo"
-      src="./assets/logo.svg"
+      src="./common/assets/logo.svg"
       width="125"
       height="125"
     />
