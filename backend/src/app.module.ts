1c65bf8d60630502e16655d1a242a13468598888
Jaemin Choi <1dotolee@gmail.com>
2022-04-21 16:45:55 +0900

    feat(be): use prisma (#38)
    * chore: add prisma
    
    * chore: use prisma vscode extension
    
    * chore: add prisma client package
    
    * chore: setup db container for dev
    
    * chore: create initial migration
    
    * chore: use setup script on gitpod start
    
    * chore: add prisma service and module
    
    Prisma client를 module에서 직접 instantiate하는 대신,
    DI를 활용해 각 module에서 inject한다.
    
    * fix: prisma service should not be global
    
    * fix: prisma generators are not triggerred
    
    * fix: realign docker compose command option

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
   controllers: [AppController],
   providers: [AppService]
 })
---
+++ scripts/setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# set -x
+
+BASEDIR=$(dirname $(dirname $(realpath $0)))
+
+cd $BASEDIR/backend
+
+# Save database URL to dotenv file for Prisma
+echo "DATABASE_URL=\"postgresql://postgres:1234@localhost:5433/skkuding?schema=public\"" > .env
+echo "DATABASE_URL=\"postgresql://postgres:1234@localhost:5434/skkuding?schema=public\"" > .env.test.local
+
+docker-compose up -d dev-db
+
+pnpm install
+
+for i in {1..5}
+do
+  npx prisma migrate dev && break # break if migration succeed
+  echo -e '\n⚠️ Failed to migrate. Waiting for db to be ready...\n'
+  sleep 5
+done
