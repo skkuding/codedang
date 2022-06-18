0de9c0efd00931ea1ccabfe677ef7d33b652b8f5
Jaemin Choi <1dotolee@gmail.com>
2022-06-18 18:22:49 +0900

    feat(dev): configure devcontainer (#52)
    * feat: setup devcontainer
    
    * fix: specify remote non-root user of devcontainer
    
    Devcontainer의 Node.js image는 remote user가 vscode 대신 user로 정의되어있다.
    
    * chore: recommend vscode exts for devcontainer
    
    * chore: update vue language extension ids
    
    * feat: create db container with devcontainer
    
    * fix: config for pnpm v7
    
    * fix: enable corepack only if pnpm is unavailable
    
    * chore: lint
    
    * fix: use pnpm v7 on every job
    
    * fix: specify prisma client output as a workaround
    
    ref: https://github.com/prisma/prisma/issues/2584#issuecomment-646414549
    
    * fix: cannot import prisma client
    
    * fix: cannot import prisma client

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,21 +1,31 @@
 #!/usr/bin/env bash
 
-# set -x
+set -ex
 
 BASEDIR=$(dirname $(dirname $(realpath $0)))
 
-cd $BASEDIR/backend
+cd $BASEDIR
 
 # Save database URL to dotenv file for Prisma
-echo "DATABASE_URL=\"postgresql://postgres:1234@localhost:5433/skkuding?schema=public\"" > .env
-echo "DATABASE_URL=\"postgresql://postgres:1234@localhost:5434/skkuding?schema=public\"" > .env.test.local
+if [ -z $DEVCONTAINER ]
+then
+  echo "DATABASE_URL=\"postgresql://postgres:1234@localhost:5433/skkuding?schema=public\"" > backend/.env
+  echo "DATABASE_URL=\"postgresql://postgres:1234@localhost:5434/skkuding?schema=public\"" > backend/.env.test.local
+  docker-compose up -d dev-db
+else
+  echo "DATABASE_URL=\"postgresql://postgres:1234@dev-db:5432/skkuding?schema=public\"" > backend/.env
+  echo "DATABASE_URL=\"postgresql://postgres:1234@test-db:5432/skkuding?schema=public\"" > backend/.env.test.local
+fi
 
-docker-compose up -d dev-db
-
+# Install pnpm
+pnpm --version || sudo corepack enable
+corepack prepare pnpm@7.2.1 --activate
 pnpm install
 
+# Apply database migration
 for i in {1..5}
 do
+  cd $BASEDIR/backend
   npx prisma migrate dev && break # break if migration succeed
   echo -e '\n⚠️ Failed to migrate. Waiting for db to be ready...\n'
   sleep 5
