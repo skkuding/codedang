be6b76f11ddbe369fc6c9def888730a863f5ac20
coc0a25 <cranemont@gmail.com>
2022-06-19 12:30:38 +0000

    chore(be): udpate app.module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+
 import { PrismaModule } from './prisma/prisma.module'
+import { AuthModule } from './auth/auth.module'
+import { ContestModule } from './contest/contest.module'
+import { GroupModule } from './group/group.module'
+import { NoticeModule } from './notice/notice.module'
+import { ProblemModule } from './problem/problem.module'
+import { SubmissionModule } from './submission/submission.module'
+import { UserModule } from './user/user.module'
+import { WorkbookModule } from './workbook/workbook.module'
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
+    ContestModule,
+    GroupModule,
+    NoticeModule,
+    ProblemModule,
+    SubmissionModule,
+    UserModule,
+    WorkbookModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
