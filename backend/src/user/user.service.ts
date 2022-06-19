3e6f21f799fbe01b88719b3fa0031c3d0b2cff30
coc0a25 <cranemont@gmail.com>
2022-06-19 12:30:02 +0000

    chore(be): setup user module

---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+@Injectable()
+export class UserService {
+  constructor(private readonly prisma: PrismaService) {}
+}
