d0d6af9b4b5bdf8b54860f7e8557284eb2cfaab3
SOLBI1028 <hagkthfql@naver.com>
2022-06-28 11:05:20 +0000

    chore(fe): setup color palette
    컴포넌트 및  텍스트에 공통적으로 사용되는 색 세팅

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,25 @@ module.exports = {
       white: colors.white,
       black: colors.black,
       transparent: colors.transparent,
-      gray: colors.gray,
+      green: {
+        DEFAULT: '#8dc63f',
+        dark: '#2d4e00'
+      },
+      gray: {
+        light: '#f1f3f6',
+        DEFAULT: '#cdcdcd',
+        dark: '#7c7a7b'
+      },
+      blue: {
+        DEFUALT: '#2279fd',
+        dark: '#002d71'
+      },
+      red: '#ff6663',
+      text: {
+        title: '#7c7a7b',
+        subtitle: '#a13e51',
+        DEFAULT: '#212529'
+      },
       slate: {
         50: '#eceff0',
         100: '#dadfe2',
@@ -21,58 +39,6 @@ module.exports = {
         700: '#173747', // Basis (Problem.vue)
         800: '#122c39',
         900: '#0e212b'
-      },
-      zinc: colors.zinc,
-      neutral: colors.neutral,
-      stone: colors.stone,
-      amber: colors.amber,
-      yellow: colors.yellow,
-      green: colors.green,
-      emerald: colors.emerald,
-      teal: colors.teal,
-      cyan: colors.cyan,
-      blue: colors.blue,
-      indigo: colors.indigo,
-      violet: colors.violet,
-      purple: colors.purple,
-      funchsia: colors.fuchsia,
-      pink: colors.pink,
-      rose: colors.rose,
-      red: colors.red,
-      orange: {
-        100: '#ffecd9',
-        200: '#ffd9b3',
-        300: '#ffc58e',
-        400: '#ffb268',
-        500: '#ff9f42', // Basis
-        600: '#cc7f35',
-        700: '#995f28',
-        800: '#66401a',
-        900: '#33200d'
-      },
-      lime: {
-        50: '#f4f9ec',
-        100: '#e8f4d9',
-        200: '#d1e8b2',
-        300: '#bbdd8c',
-        400: '#a4d165',
-        500: '#8dc63f', // Basis
-        600: '#719e32',
-        700: '#557726',
-        800: '#384f19',
-        900: '#1c280d'
-      },
-      sky: {
-        50: '#ebf4fc',
-        100: '#d6e9fa',
-        200: '#add3f5',
-        300: '#85bdef',
-        400: '#5ca7ea',
-        500: '#3391e5', // Basis
-        600: '#2974b7',
-        700: '#1f5789',
-        800: '#143a5c',
-        900: '#0a1d2e'
       },
       level: {
         1: '#CC99C9',
