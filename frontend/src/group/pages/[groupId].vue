a8115f41043c1bda203574cf92139163434fb32a
SOLBI1028 <hagkthfql@naver.com>
2022-06-28 16:43:26 +0900

    chore(fe) : setup group module

---
+++ frontend/src/group/pages/[groupId].vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import MyComponent from '../components/MyComponent.vue'
+defineProps<{
+  groupId: number
+}>()
+</script>
+
+<template>
+  <div>This is group detail page : id = {{ groupId }}</div>
+  <MyComponent msg="group detail!" />
+</template>
+
+<script lang="ts">
+// setup()을 제외한 나머지 script code를 작성해주세요.
+export default {
+  name: 'GroupDetail',
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
---
+++ frontend/src/group/pages/index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import MyComponent from '../components/MyComponent.vue'
+</script>
+
+<template>
+  <div>This is contest list page</div>
+  <MyComponent msg="group list!" />
+</template>
+
+<script lang="ts">
+// setup()을 제외한 나머지 script code를 작성해주세요.
+export default {
+  name: 'GroupList',
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
