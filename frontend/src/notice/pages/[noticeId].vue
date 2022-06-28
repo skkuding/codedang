6ae96255f3fe6cebc0128099c6edf1be76c1fc32
SOLBI1028 <hagkthfql@naver.com>
2022-06-28 08:11:27 +0000

    chore(fe): setup notice module

---
+++ frontend/src/notice/pages/[noticeId].vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import MyComponent from '@/notice/components/MyComponent.vue'
+defineProps<{
+  noticeId: number
+}>()
+</script>
+
+<template>
+  <div>This is notice detail page : id = {{ noticeId }}</div>
+  <MyComponent msg="notice detail!" />
+</template>
+
+<script lang="ts">
+// setup()을 제외한 나머지 script code를 작성해주세요.
+export default {
+  name: 'NoticeDetail',
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
