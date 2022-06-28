45b904d1022c15cf98f100e61d62c72ea6e15502
SOLBI1028 <hagkthfql@naver.com>
2022-06-28 16:37:04 +0900

    chore(fe): setup contest module

---
+++ frontend/src/contest/pages/[contestId]/Problem.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import MyComponent from '@/contest/components/MyComponent.vue'
+defineProps<{
+  contestId: number
+}>()
+</script>
+
+<template>
+  <div>This is contest problem page : id = {{ contestId }}</div>
+  <MyComponent msg="contest problem!" />
+</template>
+
+<script lang="ts">
+// setup()을 제외한 나머지 script code를 작성해주세요.
+export default {
+  name: 'ContestProblem',
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
---
+++ frontend/src/contest/pages/[contestId]/Top.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import MyComponent from '@/contest/components/MyComponent.vue'
+defineProps<{
+  contestId: number
+}>()
+</script>
+
+<template>
+  <div>This is contest top page : id = {{ contestId }}</div>
+  <MyComponent msg="contest top page!" />
+</template>
+
+<script lang="ts">
+// setup()을 제외한 나머지 script code를 작성해주세요.
+export default {
+  name: 'ContestTop',
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
