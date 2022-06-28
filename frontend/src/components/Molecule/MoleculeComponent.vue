4461d7194b112c1d9026c04042f51b9c2a1ac508
SOLBI1028 <hagkthfql@naver.com>
2022-06-28 08:12:05 +0000

    chore(fe): setup component directory

---
+++ frontend/src/components/Molecule/MoleculeComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div>Molecule Component props : {{ msg }}</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'MoleculeComponent',
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+
+<!--
+<style scoped>
+style code를 작성해주세요.
+</style>
+-->
---
+++ frontend/src/components/Organism/OrganismComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div>Organism Component props : {{ msg }}</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'OrganismComponent',
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+
+<!--
+<style scoped>
+style code를 작성해주세요.
+</style>
+-->
