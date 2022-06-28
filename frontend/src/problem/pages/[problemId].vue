623e647a7db283091d42093a5edb9bb4ac7b7d96
SOLBI1028 <hagkthfql@naver.com>
2022-06-28 08:11:44 +0000

    chore(fe): setup problem module

---
+++ frontend/src/problem/pages/[problemId].vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import MyComponent from '@/problem/components/MyComponent.vue'
+defineProps<{
+  problemId: number
+}>()
+</script>
+
+<template>
+  <div>This is problem detail page : id = {{ problemId }}</div>
+  <MyComponent msg="problem detail!" />
+</template>
+
+<script lang="ts">
+// setup()을 제외한 나머지 script code를 작성해주세요.
+export default {
+  name: 'ProblemDetail',
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
