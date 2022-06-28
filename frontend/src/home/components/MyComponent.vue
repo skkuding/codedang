17f32cdb060055f623b40741c49c9aee5760426d
SOLBI1028 <hagkthfql@naver.com>
2022-06-28 08:11:04 +0000

    chore(fe): setup home module

---
+++ frontend/src/home/components/MyComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
+<template>
+  <div>My Component props : {{ msg }}</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'MyComponent',
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+<!--
+<style scoped>
+style code를 작성해주세요.
+</style>
+-->
--- frontend/src/home/pages/index.vue
+++ frontend/src/home/pages/index.vue
@@ -1,85 +1,12 @@
 <script setup lang="ts">
-import HelloWorld from '../components/HelloWorld.vue'
-import TheWelcome from '../components/TheWelcome.vue'
+import MyComponent from '@/home/components/MyComponent.vue'
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/common/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>This is Main page</div>
+  <MyComponent msg="Main!" />
 </template>
 
-<style>
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
-
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-a,
-.green {
-  text-decoration: none;
-  color: hsla(160, 100%, 37%, 1);
-  transition: 0.4s;
-}
-
-@media (hover: hover) {
-  a:hover {
-    background-color: hsla(160, 100%, 37%, 0.2);
-  }
-}
-
-@media (min-width: 1024px) {
-  body {
-    display: flex;
-    place-items: center;
-  }
-
-  #app {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    padding: 0 2rem;
-  }
-
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-}
-</style>
+<!--<style>
+style code를 작성해주세요.
+</style>-->
