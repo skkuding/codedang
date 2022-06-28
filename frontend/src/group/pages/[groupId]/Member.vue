89b95f464b4cb88abcf1f8ccd61b3418d7a4f49f
SOLBI1028 <hagkthfql@naver.com>
2022-06-28 08:12:15 +0000

    chore(fe): setup group module

---
+++ frontend/src/group/pages/[groupId]/Member.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import MyComponent from '@/group/components/MyComponent.vue'
+defineProps<{
+  groupId: number
+}>()
+</script>
+
+<template>
+  <div>This is group member page : id = {{ groupId }}</div>
+  <MyComponent msg="group member!" />
+</template>
+
+<script lang="ts">
+// setup()을 제외한 나머지 script code를 작성해주세요.
+export default {
+  name: 'GroupMember',
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {}
+}
+</script>
--- frontend/src/group/pages/[groupId]/Workbook.vue
+++ frontend/src/group/pages/[groupId]/Workbook.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import MyComponent from '../components/MyComponent.vue'
+import MyComponent from '@/group/components/MyComponent.vue'
 defineProps<{
   groupId: number
 }>()
 </script>
 
 <template>
-  <div>This is group detail page : id = {{ groupId }}</div>
-  <MyComponent msg="group detail!" />
+  <div>This is group workbook page : id = {{ groupId }}</div>
+  <MyComponent msg="group workbook page!" />
 </template>
 
 <script lang="ts">
 // setup()을 제외한 나머지 script code를 작성해주세요.
 export default {
-  name: 'GroupDetail',
+  name: 'GroupWorkbook',
   data() {
     return {}
   },
