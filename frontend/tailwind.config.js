5eddc3e91e6d214e400c90f50fb291642b759b0a
Jaemin Choi <1dotolee@gmail.com>
2022-06-29 14:26:16 +0000

    chore: apply typescript support to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
