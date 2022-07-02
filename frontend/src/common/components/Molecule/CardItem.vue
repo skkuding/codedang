c582bca1ccfc793da667ef68833362db20c4bb1a
goo314 <rn0731@g.skku.edu>
2022-07-02 08:13:17 +0000

    style(fe): add hover style in card-item
    카드 아이템에 마우스 커서를 올리면 포인터로 바뀌고 그림자가 생긴다.

--- frontend/src/common/components/Molecule/CardItem.vue
+++ frontend/src/common/components/Molecule/CardItem.vue
@@ -20,10 +20,13 @@ const borderColor: borderColorType = {
 </script>
 
 <template>
-  <div class="mx-40 flex border p-3" :class="borderColor[ccolor]">
+  <div
+    class="mx-40 flex cursor-pointer border p-3 hover:shadow"
+    :class="borderColor[ccolor]"
+  >
     <img :src="img" class="aspect-square w-16" />
     <div class="flex w-full pl-2">
-      <div class="relative w-full bg-white pl-2">
+      <div class="relative w-full pl-2">
         <div class="text-xl font-bold">{{ leftTop }}</div>
         <div class="text-gray-dark">
           {{ leftBottom }}
@@ -39,7 +42,7 @@ const borderColor: borderColorType = {
         >
           {{ rightBottomFull }}
         </div>
-        <!-- when screen size is 768px -->
+        <!-- when screen size is smaller than 768px -->
         <div
           class="text-green absolute bottom-0 right-0 block text-right md:hidden"
         >
