06bf7df01f3a12f51650e90c4c55a04d4b8a1867
goo314 <rn0731@g.skku.edu>
2022-07-02 11:27:39 +0000

    style(fe): change variable name
    props 이름을 알아보기 쉽게 변경했다
    몇몇 props를 전달하지 않아도 카드아이템을 보여준다

--- frontend/src/common/components/Molecule/CardItem.vue
+++ frontend/src/common/components/Molecule/CardItem.vue
@@ -1,52 +1,47 @@
 <script setup lang="ts">
 defineProps<{
-  ccolor: string
-  img: string
-  leftTop: string
-  rightTop: string
-  leftBottom: string
-  rightBottomFull: string
-  rightBottomShort: string
+  borderColor?: string
+  img?: string
+  title: string // left-top
+  description?: string // left-bottom
+  additionalText?: string // right-top
+  coloredText?: string // right-bottom
+  coloredTextShort?: string // right-bottom
 }>()
 
-interface borderColorType {
-  [key: string]: string
-}
-
-const borderColor: borderColorType = {
-  green: 'border-green',
-  gray: 'border-gray'
+function borderColorClass(borderColor?: string): string {
+  return borderColor === 'gray' ? 'border-gray' : 'border-green'
 }
 </script>
 
 <template>
   <div
     class="mx-40 flex cursor-pointer border p-3 hover:shadow"
-    :class="borderColor[ccolor]"
+    :class="borderColorClass(borderColor)"
   >
-    <img :src="img" class="aspect-square w-16" />
+    <img v-if="img" :src="img" class="aspect-square w-16" />
     <div class="flex w-full pl-2">
       <div class="relative w-full pl-2">
-        <div class="text-xl font-bold">{{ leftTop }}</div>
+        <div class="text-xl font-bold">{{ title }}</div>
         <div class="text-gray-dark">
-          {{ leftBottom }}
+          {{ description }}
         </div>
       </div>
       <div class="relative w-1/2">
         <div class="hidden text-right md:block">
-          {{ rightTop }}
+          {{ additionalText }}
         </div>
         <!-- when screen size is 1440px -->
         <div
           class="text-green absolute bottom-0 right-0 hidden text-right md:block"
         >
-          {{ rightBottomFull }}
+          {{ coloredText }}
         </div>
         <!-- when screen size is smaller than 768px -->
         <div
           class="text-green absolute bottom-0 right-0 block text-right md:hidden"
         >
-          {{ rightBottomShort }}
+          {{ coloredTextShort }}
         </div>
       </div>
     </div>
