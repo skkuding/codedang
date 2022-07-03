c72c0e1dc0069528bde26b85b3cf519ba4348ec9
HakSan <cranemont@gmail.com>
2022-07-03 07:28:45 +0000

    chore(be): implement jwt strategy base

---
+++ backend/src/auth/jwt.constants.ts
@@ -0,0 +1,25 @@
+// TODO: .env로 옮기기, env, production 환경 구분
+export const SECRET_KEY = 'dev'
+export const ACCESS_TOKEN_EXPIRATION_SEC = 600
+export const REFRESH_TOKEN_EXPIRATION_SEC = 20 //1209600 on production
+
+//TODO: 개발 환경에서는 httponly, secure 관련 해제, signed cookie 사용하기
+export const REFRESH_TOKEN_COOKIE_OPTIONS = {
+  maxAge: REFRESH_TOKEN_EXPIRATION_SEC * 1000,
+  // secure: true,
+  path: '/auth/reissue',
+  httpOnly: true
+}
+
+// TODO: Cache key 디렉토리 옮기기
+export const refreshTokenCacheKey = (userId) => `user:${userId}:refresh_token`
+
+export type JwtPayload = {
+  username: string
+  userId: number
+}
+
+export type JwtObject = JwtPayload & {
+  iat: number
+  exp: number
+}
---
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt } from 'passport-jwt'
+import { Strategy } from 'passport-jwt'
+import { AuthService } from '../auth.service'
+import { JwtObject, SECRET_KEY } from '../jwt.constants'
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: SECRET_KEY
+    })
+  }
+
+  // req.user object에 이게 들어감!
+  async validate(payload: JwtObject) {
+    // validation logic 추가하기. 지금은 그냥 바로 뱉어버림
+    // userId와 username으로 user가 존재하는지 확인, 있다면 role이 추가된 무언가를 return해서
+    // req.user로 사용할 수 있도록 함
+    return { userId: payload.userId, username: payload.username }
+  }
+}
