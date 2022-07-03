fce45c492727e2cf028211936912d934a782eb60
hbsongk <68498323+hbsongk@users.noreply.github.com>
2022-07-03 15:07:00 +0000

    feat(fe): add button component

---
+++ frontend/src/common/components/Atom/Button.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+const props = defineProps<{
+  text?: string
+  buttoncolor?: string
+  isrounded?: boolean
+}>()
+
+const borderRadius = computed(() => {
+  return props.isrounded ? ' rounded-full' : ' rounded'
+})
+
+const backgroundColor = computed(() => {
+  if (props.buttoncolor === 'blue') {
+    return 'bg-blue hover:bg-blue-dark text-white'
+  } else if (props.buttoncolor === 'gray') {
+    return 'bg-gray hover:bg-gray-dark text-white'
+  } else if (props.buttoncolor === 'white') {
+    return 'bg-white hover:bg-gray-light border-black border text-black'
+  } else {
+    return 'bg-green hover:bg-green-dark text-white'
+  }
+})
+</script>
+
+<template>
+  <div>
+    <button class="px-2.5 py-1" :class="backgroundColor + borderRadius">
+      <slot name="icon"></slot>
+      {{ text }}
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'ButtonAtom',
+  data() {
+    return {}
+  },
+  methods: {}
+}
+</script>
