224eb09b743faa97ecb3d4bb4ea77bc33be2975a
coc0a25 <cranemont@gmail.com>
2022-07-04 16:26:41 +0000

    fix(be): refresh token sent to all path

--- backend/src/auth/config/jwt.config.ts
+++ backend/src/auth/config/jwt.config.ts
@@ -1,12 +1,11 @@
-export const ACCESS_TOKEN_EXPIRATION_SEC = 600
-export const REFRESH_TOKEN_EXPIRATION_SEC = 86400
+export const ACCESS_TOKEN_EXPIRATION_SEC = 1800 // 30m
+export const REFRESH_TOKEN_EXPIRATION_SEC = 86400 // 24h
 
-//TODO: 개발 환경에서는 httponly, secure 관련 해제, signed cookie 사용하기
+//TODO: production 환경에서는 secure, signed cookie 사용하기
 export const REFRESH_TOKEN_COOKIE_OPTIONS = {
   maxAge: REFRESH_TOKEN_EXPIRATION_SEC * 1000,
-  // secure: true,
-  path: '/auth/reissue',
   httpOnly: true
+  // secure: true,
 }
 
 export const AUTH_TYPE = 'Bearer'
