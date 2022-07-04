d507730c0e968ae75054a33f81e2ab055f555a81
Jaemin Choi <1dotolee@gmail.com>
2022-07-04 08:23:05 +0000

    feat: use hitoire for component documentation
    Vue로 story 작성을 지원하는 Histoire로 component를 문서화한다.
    histoire.setup.ts를 이용해 Histoire에 필요한 css를 import한다.

---
+++ frontend/histoire.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'histoire'
+
+export default defineConfig({
+  setupFile: '/src/histoire.setup.ts',
+  theme: {
+    title: 'SKKUding Histoire'
+  }
+})
---
+++ frontend/src/histoire.setup.ts
@@ -0,0 +1,5 @@
+import './common/styles/style.css'
+import './histoire.css'
+
+/* trigger script */
+console.log('Histoire theme loaded.')
