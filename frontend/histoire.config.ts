64f49d196691e46d364605409480025658ee5293
Jaemin Choi <1dotolee@gmail.com>
2022-07-04 13:31:54 +0000

    chore: use port 3030 for histoire
    vite의 기본 port인 3000은 frontend용 서버와 port가 겹치므로 3030을 대신 사용한다.
    GitPod을 위한 hmr url 설정을 추가한다.

--- frontend/histoire.config.ts
+++ frontend/histoire.config.ts
@@ -4,5 +4,21 @@ export default defineConfig({
   setupFile: '/src/histoire.setup.ts',
   theme: {
     title: 'SKKUding Histoire'
+  },
+  vite: {
+    server: {
+      // configure vite for HMR with Gitpod
+      hmr: process.env.GITPOD_HOST
+        ? {
+            // removes the protocol and replaces it with the port we're connecting to
+            host: process.env.GITPOD_WORKSPACE_URL?.replace(
+              'https://',
+              '3030-'
+            ),
+            protocol: 'wss',
+            clientPort: 443
+          }
+        : true
+    }
   }
 })
