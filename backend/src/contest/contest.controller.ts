49e8178323284fe7c524d66e799e121403fb2342
nimod7890 <nimod1234@g.skku.edu>
2022-07-06 12:39:59 +0000

    feat(be): implement contest get api sets

--- backend/src/contest/contest.controller.ts
+++ backend/src/contest/contest.controller.ts
@@ -1,7 +1,54 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get, Param, ParseIntPipe, Req } from '@nestjs/common'
+import { Contest } from '@prisma/client'
 import { ContestService } from './contest.service'
 
 @Controller()
 export class ContestController {
   constructor(private readonly contestService: ContestService) {}
+
+  /* group admin page */
+  @Get('admin')
+  async getAllAdminContest(@Req() req) {
+    const userId = req.body.user.id
+    return await this.contestService.getAllAdminContest(userId)
+  }
+
+  @Get('admin/ongoing')
+  async getAdminOngoing(@Req() req) {
+    const userId = req.body.user.id
+    return await this.contestService.getAdminOngoing(userId)
+  }
+
+  /* User Page */
+  /* contest list page */
+  @Get('ongoing')
+  async getOngoing(): Promise<Partial<Contest>[]> {
+    return await this.contestService.getOngoing()
+  }
+
+  @Get('upcoming')
+  async getUpcoming(): Promise<Partial<Contest>[]> {
+    return await this.contestService.getUpcoming()
+  }
+
+  @Get('finished')
+  async getFinished(): Promise<Partial<Contest>[]> {
+    return await this.contestService.getFinished()
+  }
+
+  @Get('group/:id')
+  async findByGroupId(@Req() req, @Param('id', ParseIntPipe) groupId: number) {
+    const userId = req.body.user.id
+    return await this.contestService.findByGroupId(userId, groupId)
+  }
+
+  /* contest detail page */
+  @Get(':id')
+  async findByContestId(
+    @Req() req,
+    @Param('id', ParseIntPipe) contestId: number
+  ): Promise<Partial<Contest>> {
+    const userId = req.body.user.id
+    return await this.contestService.findByContestId(userId, contestId)
+  }
 }
--- backend/src/contest/contest.service.ts
+++ backend/src/contest/contest.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common'
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common'
 import { Contest } from '@prisma/client'
 import {
   EntityNotExistException,
@@ -7,6 +11,21 @@ import {
 import { PrismaService } from 'src/prisma/prisma.service'
 import { CreateContestDto } from './dto/create-contest.dto'
 import { UpdateContestDto } from './dto/update-contest.dto'
+
+const contestListselectOption = {
+  id: true,
+  title: true,
+  description: true,
+  start_time: true,
+  end_time: true,
+  visible: true,
+  group_id: true,
+  type: true
+}
+const userContestListPageOption = {
+  where: { visible: true },
+  select: contestListselectOption
+}
 
 @Injectable()
 export class ContestService {
@@ -91,5 +110,103 @@ export class ContestService {
         id: contestId
       }
     })
+  }
+
+  filterOngoing(allContest) {
+    const now = new Date()
+    const ongoingContest = allContest.filter(
+      (contest) => contest.start_time <= now && contest.end_time > now
+    )
+    return ongoingContest
+  }
+
+  /* group admin page */
+  async getAllAdminContest(user_id: number) {
+    return await this.prisma.userGroup.findFirst({
+      where: { user_id, is_group_manager: true },
+      select: { group: { include: { Contest: true } } }
+    })
+  }
+
+  async getAdminOngoing(user_id: number) {
+    const allContest = await this.getAllAdminContest(user_id)
+    return this.filterOngoing(allContest)
+  }
+
+  /* User Page */
+
+  /* contest list page */
+  async getOngoing(): Promise<Partial<Contest>[]> {
+    const allContest = await this.prisma.contest.findMany(
+      userContestListPageOption
+    )
+    return this.filterOngoing(allContest)
+  }
+
+  async getUpcoming(): Promise<Partial<Contest>[]> {
+    const allContest = await this.prisma.contest.findMany(
+      userContestListPageOption
+    )
+    const returnContest = allContest.filter(
+      (contest) => contest.start_time > new Date()
+    )
+    return returnContest
+  }
+
+  async getFinished(): Promise<Partial<Contest>[]> {
+    const allContest = await this.prisma.contest.findMany(
+      userContestListPageOption
+    )
+    const returnContest = allContest.filter(
+      (contest) => contest.end_time <= new Date()
+    )
+    return returnContest
+  }
+
+  async findByGroupId(user_id: number, group_id: number) {
+    const group = await this.prisma.group.findUnique({
+      where: { id: group_id }
+    })
+    if (!group) {
+      throw new NotFoundException()
+    }
+    return await this.prisma.userGroup.findFirst({
+      where: { user_id, group_id, is_registered: true },
+      select: {
+        group: {
+          select: {
+            Contest: {
+              where: { group_id, visible: true },
+              select: contestListselectOption
+            }
+          }
+        }
+      }
+    })
+  }
+
+  /* contest detail page */
+  async findByContestId(
+    user_id: number,
+    contest_id: number
+  ): Promise<Partial<Contest>> {
+    const contest = await this.prisma.contest.findUnique({
+      where: { id: contest_id },
+      select: contestListselectOption
+    })
+    if (!contest) {
+      throw new NotFoundException()
+    }
+    const isUserInGroup = await this.prisma.userGroup.findFirst({
+      where: { user_id, group_id: contest.group_id },
+      select: { is_group_manager: true }
+    })
+    if (
+      (!isUserInGroup && contest.end_time > new Date()) ||
+      (contest.visible == false && isUserInGroup.is_group_manager == false)
+    ) {
+      throw new BadRequestException()
+    }
+    return contest
   }
 }
