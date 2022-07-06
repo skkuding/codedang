e2ebfdc349757d7ad166fc00f942c61db2e50373
SOLBI1028 <hagkthfql@naver.com>
2022-07-06 04:58:24 +0000

    chore(fe): add copy email feature

--- frontend/src/common/components/Organism/Footer.vue
+++ frontend/src/common/components/Organism/Footer.vue
@@ -4,21 +4,32 @@ import CarbonEmail from '~icons/carbon/email'
 import SimpleIconsKakaotalk from '~icons/simple-icons/kakaotalk'
 import PhLinkThin from '~icons/ph/link-thin'
 import { ref } from 'vue'
-const email = ref('example@skku.edu')
-function copy() {
-  console.log('Email is copied')
+import { copyText } from 'vue3-clipboard'
+
+const email = 'skkuding@gmail.com'
+
+const copyEmail = () => {
+  copyText('email', undefined, (error, event) => {
+    if (error) {
+      alert('Can not copy')
+      console.log(error)
+    } else {
+      alert('Copied')
+      console.log(event)
+    }
+  })
 }
 </script>
 
 <template>
   <footer
-    class="footer bg-gray-light fixed inset-x-0 bottom-0 flex flex-col justify-center align-middle"
+    class="bg-gray-light fixed inset-x-0 bottom-0 flex flex-col justify-center align-middle"
   >
-    <div class="footer-info flex flex-row justify-center">
+    <div class="flex flex-row justify-center">
       <a href="https://github.com/skkuding/skku-coding-platform" class="m-1">
         <CarbonLogoGithub />
       </a>
-      <a v-clipboard:copy="email" class="m-1" @click="copy">
+      <a @click="copyEmail" class="m-1">
         <CarbonEmail />
       </a>
       <a href="http://pf.kakao.com/_UKraK/chat" class="m-1">
@@ -26,11 +37,8 @@ function copy() {
       </a>
       <a href="https://npc.skku.edu" class="m-1"><PhLinkThin /></a>
     </div>
-    <div class="footer-bottom m-1 flex flex-row justify-center">
-      <span>© 2011-2021 SKKU NPC Club</span>
+    <div class="m-1 flex flex-row justify-center">
+      <span>© 2011-2022 SKKUDING</span>
     </div>
   </footer>
 </template>
-<!-- <style scoped>
-
-</style> -->
