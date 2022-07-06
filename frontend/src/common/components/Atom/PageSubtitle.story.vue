61452d999a538782b2841fb313ef729f30b83959
Jaemin Choi <1dotolee@gmail.com>
2022-07-06 14:02:13 +0900

    feat(fe): use histoire for component documentation (#104)
    * feat: use hitoire for component documentation
    
    Vue로 story 작성을 지원하는 Histoire로 component를 문서화한다.
    histoire.setup.ts를 이용해 Histoire에 필요한 css를 import한다.
    
    * chore: use port 3030 for histoire
    
    vite의 기본 port인 3000은 frontend용 서버와 port가 겹치므로 3030을 대신 사용한다.
    GitPod을 위한 hmr url 설정을 추가한다.
    
    * feat: create stories for existing components
    
    * chore: remove example components

---
+++ frontend/src/common/components/Atom/PageSubtitle.story.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import PageSubtitle from './PageSubtitle.vue'
+</script>
+
+<template>
+  <Story>
+    <PageSubtitle text="Lorem Ipsum is simply dummy text" />
+  </Story>
+</template>
