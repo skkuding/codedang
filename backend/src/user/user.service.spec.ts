76241da8e7747df24841daf8e942911881a4f86c
realsehwan@gmail.com <realsehwan@gmail.com>
2022-07-08 09:27:27 +0000

    chore(be): add providers in .spec file
    종속성 해결을 위해 user.service에 주입된 provider를 추가합니다.
    다만, mock 객체 및 test suites는 아직 구현되지 않았습니다.

--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,16 +1,28 @@
+import { CACHE_MANAGER, NotFoundException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
+import { Cache } from 'cache-manager'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { UserService } from './user.service'
 
 describe('UserService', () => {
   let service: UserService
+  let prisma: PrismaService
+  let cache: Cache
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService, { provide: PrismaService, useValue: {} }]
+      providers: [
+        UserService,
+        { provide: PrismaService, useValue: {} },
+        { provide: CACHE_MANAGER, useValue: {} },
+        { provide: ConfigService, useValue: {} }
+      ]
     }).compile()
 
     service = module.get<UserService>(UserService)
+    prisma = module.get<PrismaService>(PrismaService)
+    cache = module.get(CACHE_MANAGER)
   })
 
   it('should be defined', () => {
