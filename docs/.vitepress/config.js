b7e5454b88092c7602bafc1cf82526f22e49b5e7
SOLBI1028 <hagkthfql@naver.com>
2022-07-10 18:32:39 +0900

    style: fix code style

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -16,36 +16,42 @@ export default {
       {
         text: 'Introduction',
         items: [
-            { text: 'What is Coding Platform?', link: '/introduction/coding-platform'},
-            { text: 'Contributing Guide', link: '/introduction/contributing-guide'},
-            { text: 'Getting Started', link: '/introduction/getting-started'},
+          {
+            text: 'What is Coding Platform?',
+            link: '/introduction/coding-platform'
+          },
+          {
+            text: 'Contributing Guide',
+            link: '/introduction/contributing-guide'
+          },
+          { text: 'Getting Started', link: '/introduction/getting-started' }
         ]
       },
       {
         text: '[FE] Atom',
         items: [
-          { text: 'Button', link:'/frontend/Atom/button'},
+          { text: 'Button', link: '/frontend/Atom/button' }
           // please add Atom items
         ]
       },
       {
         text: '[FE] Molecule',
         items: [
-          { text: 'Progress Card', link:'/frontend/Molecule/card-progress'},
+          { text: 'Progress Card', link: '/frontend/Molecule/card-progress' }
           // please add Molecule items
         ]
       },
       {
         text: '[FE] Organism',
         items: [
-          { text: 'Header', link:'/frontend/Organism/header'},
+          { text: 'Header', link: '/frontend/Organism/header' }
           // please add Organism items
         ]
       },
       {
         text: '[BE] API',
         items: [
-          { text: 'Contest', link:'/backend/API/contest'},
+          { text: 'Contest', link: '/backend/API/contest' }
           // please add API items
         ]
       }
