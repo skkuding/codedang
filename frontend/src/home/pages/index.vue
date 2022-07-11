59adb5c008704095ee818677b857ba4c210e0769
hbsongk <68498323+hbsongk@users.noreply.github.com>
2022-07-11 06:35:03 +0000

    chore(fe) : resolve conflict

--- frontend/src/home/pages/index.vue
+++ frontend/src/home/pages/index.vue
@@ -1,8 +1,31 @@
 <script setup lang="ts">
 import MyComponent from '../components/MyComponent.vue'
+import Button from '@/common/components/Atom/Button.vue'
+import SearchBar from '@/common/components/Atom/SearchBar.vue'
+import Search from '~icons/carbon/search'
+
+function log(data: any) {
+  console.log(data)
+}
 </script>
 
 <template>
   <div>This is Main page</div>
   <MyComponent msg="Main!" />
+  <Button color="gray-dark" rounded>
+    <Search class="inline" />
+    Button
+  </Button>
+  <Button color="white">
+    Yo
+    <Search />
+  </Button>
+  <SearchBar
+    content="keywords"
+    @search="
+      (data) => {
+        log(data)
+      }
+    "
+  ></SearchBar>
 </template>
