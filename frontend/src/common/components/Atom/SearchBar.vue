166224462c93a4899f656a3fe4a0cd7bd9046892
hbsongk <68498323+hbsongk@users.noreply.github.com>
2022-07-11 07:20:28 +0000

    feat(fe): add searchbar

---
+++ frontend/src/common/components/Atom/SearchBar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Button from './Button.vue'
+import Search from '~icons/carbon/search'
+import { ref } from 'vue'
+
+defineProps<{
+  content?: string
+}>()
+
+defineEmits(['search'])
+const data = ref('')
+</script>
+
+<template>
+  <div class="flex w-fit">
+    <input
+      v-model="data"
+      class="border-gray-dark rounded-l-lg border-2 px-2 focus:outline-0"
+      type="text"
+      :placeholder="content"
+      @keyup.enter="$emit('search', data)"
+    />
+    <Button
+      color="gray-dark"
+      class="border-gray-dark !rounded-l-none rounded-r-lg border-2 border-l-0"
+      @click="$emit('search', data)"
+    >
+      <Search />
+    </Button>
+  </div>
+</template>
