9476d575a89064e6be84f3272e90b76b506c5f74
goo314 <rn0731@g.skku.edu>
2022-07-12 12:48:24 +0000

    chore(fe): enable event for changing page

--- frontend/src/common/components/Organism/PaginationTable.vue
+++ frontend/src/common/components/Organism/PaginationTable.vue
@@ -10,9 +10,9 @@ defineProps<{
   items: {
     [key: string]: any
   }[]
-  perPage?: number
+  // perPage?: number
   // currentPage?: number
-  text?: string // show text if there's no data in item
+  text?: string // show if there's no data in item
   noHeader?: boolean
   noSearchBar?: boolean
   noPagination?: boolean
@@ -34,49 +34,53 @@ const capitalize = (key: string) => {
 </script>
 
 <template>
-  <div class="flex justify-end">
-    <SearchBar
-      v-if="!noSearchBar"
-      placeholder="keywords"
-      class="mb-5"
-    ></SearchBar>
-  </div>
-  <table class="mb-5 w-full table-auto">
-    <thead v-if="!noHeader">
-      <tr class="text-text-title border-gray border-b-2 bg-[#F9F9F9]">
-        <th
-          v-for="(field, index) in fields"
-          :key="index"
-          class="p-2.5 pl-4 text-left"
-        >
-          <span v-if="Object.keys(field).includes('label')">
-            {{ field.label }}
-          </span>
-          <span v-else>{{ capitalize(field.key) }}</span>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-if="items.length === 0">
-        <td class="p-2.5 pl-4">{{ text }}</td>
-      </tr>
-      <template v-for="(row, index) in items" v-else :key="index">
-        <!-- <tr
+  <div>
+    <div class="flex justify-end">
+      <SearchBar
+        v-if="!noSearchBar"
+        placeholder="keywords"
+        class="mb-5"
+      ></SearchBar>
+    </div>
+    <table class="mb-5 w-full table-auto">
+      <thead v-if="!noHeader">
+        <tr class="text-text-title border-gray border-b-2 bg-[#F9F9F9]">
+          <th
+            v-for="(field, index) in fields"
+            :key="index"
+            class="p-2.5 pl-4 text-left"
+          >
+            <span v-if="Object.keys(field).includes('label')">
+              {{ field.label }}
+            </span>
+            <span v-else>{{ capitalize(field.key) }}</span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-if="items.length === 0">
+          <td class="p-2.5 pl-4">{{ text }}</td>
+        </tr>
+        <template v-for="(row, index) in items" v-else :key="index">
+          <!-- <tr
           v-if="index >= startIndex && index <= endIndex"
           :key="index"
           class="hover:bg-gray-light border-gray cursor-pointer border-b"
           @click="clickEvent(row)"
         > -->
-        <tr
-          class="hover:bg-gray-light border-gray cursor-pointer border-b"
-          @click="$emit('row-clicked', row)"
-        >
-          <td v-for="(field, idx) in fields" :key="idx" class="p-2.5 pl-4">
-            <slot :name="field.key" :row="row" :index="index"></slot>
-          </td>
-        </tr>
-      </template>
-    </tbody>
-  </table>
-  <Pagination v-if="!noPagination" :number-of-pages="5" />
+          <tr
+            class="hover:bg-gray-light border-gray cursor-pointer border-y"
+            @click="$emit('row-clicked', row)"
+          >
+            <td v-for="(field, idx) in fields" :key="idx" class="p-2.5 pl-4">
+              <slot :name="field.key" :row="row" :index="index"></slot>
+            </td>
+          </tr>
+        </template>
+      </tbody>
+    </table>
+    <div class="flex justify-end">
+      <Pagination v-if="!noPagination" v-bind="$attrs" :number-of-pages="5" />
+    </div>
+  </div>
 </template>
