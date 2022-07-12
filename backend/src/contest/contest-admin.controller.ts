28fb13f36f9774cbd570f515629aeda7fa14221a
nimod7890 <nimod1234@g.skku.edu>
2022-07-12 14:38:32 +0000

    feat(be): add user guard

--- backend/src/contest/contest-admin.controller.ts
+++ backend/src/contest/contest-admin.controller.ts
@@ -1,6 +1,14 @@
-import { Controller, Get, Param, ParseIntPipe, Req, UseGuards } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Req,
+  UseGuards
+} from '@nestjs/common'
 import { Contest } from '@prisma/client'
 import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard'
+import { AuthenticatedRequest } from 'src/auth/interface/authenticated-request.interface'
 import { ContestService } from './contest.service'
 
 @Controller('contest/admin')
@@ -9,24 +17,24 @@ export class ContestAdminController {
 
   /* group admin page */
   @Get()
+  @UseGuards(JwtAuthGuard)
   async getAdminContests(@Req() req) {
     const userId = req.body.user.id
     return await this.contestService.getAdminContests(userId)
   }
 
   @Get('ongoing')
-  async getAdminOngoingContests(@Req() req) {
-    const userId = req.body.user.id
-    return await this.contestService.getAdminOngoingContests(userId)
+  @UseGuards(JwtAuthGuard)
+  async getAdminOngoingContests(@Req() req: AuthenticatedRequest) {
+    return await this.contestService.getAdminOngoingContests(req.user.id)
   }
 
   @Get(':id')
   @UseGuards(JwtAuthGuard)
   async getAdminContestById(
-    @Req() req,
+    @Req() req: AuthenticatedRequest,
     @Param('id', ParseIntPipe) contestId: number
   ): Promise<Partial<Contest>> {
-    const userId = req.body.user.id
-    return await this.contestService.getAdminContestById(userId, contestId)
+    return await this.contestService.getAdminContestById(req.user.id, contestId)
   }
 }
