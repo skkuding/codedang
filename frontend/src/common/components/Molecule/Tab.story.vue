72a3e45ecc522b5ad4a0bf6c41c5e3f53f497176
SOLBI1028 <hagkthfql@naver.com>
2022-07-14 05:50:19 +0000

    feat: create story for tab

---
+++ frontend/src/common/components/Molecule/Tab.story.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import Tab from './Tab.vue'
+
+const items = ['notice', 'contest', 'workbook', 'member']
+</script>
+
+<template>
+  <Story>
+    <Tab :items="items">
+      <template #notice>This is Notice Page</template>
+      <template #contest>This is Contest Page</template>
+      <template #workbook>This is Workbook Page</template>
+      <template #member>This is Member Page</template>
+    </Tab>
+    <h2 class="mt-8 mb-2 text-lg">* custom example</h2>
+    <Tab :items="items" color="#FF1993">
+      <template #notice>This is Notice Page</template>
+      <template #contest>This is Contest Page</template>
+      <template #workbook>This is Workbook Page</template>
+      <template #member>This is Member Page</template>
+    </Tab>
+  </Story>
+</template>
