5f0c06b04ab46747e79683bf305029d04b996058
kee711 <kee711@naver.com>
2022-07-14 04:13:31 +0000

    feat(fe): add badge component

---
+++ frontend/src/common/components/Molecule/Badge.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+defineProps<{
+  endtime: string
+  ended: boolean
+}>()
+</script>
+
+<template>
+  <div
+    class="border-gray bg-gray-light relative box-border flex h-14 w-40 rounded-md border p-1"
+  >
+    <div
+      v-if="ended"
+      class="bg-green margin top-0 bottom-0 my-auto ml-3 h-6 w-6 rounded-full"
+    ></div>
+    <div
+      v-else
+      class="bg-red margin top-0 bottom-0 my-auto ml-3 h-6 w-6 rounded-full"
+    ></div>
+    <div
+      v-if="ended"
+      class="text-gray-dark top-0 bottom-0 my-auto ml-2 text-[22px] font-bold"
+    >
+      {{ endtime }}
+    </div>
+    <div
+      v-else
+      class="text-gray-dark top-0 bottom-0 my-auto ml-5 text-[22px] font-bold"
+    >
+      Ended
+    </div>
+  </div>
+</template>
