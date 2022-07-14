27a54369c62f09a9b68c094e527eea91f6551fc7
kee711 <kee711@naver.com>
2022-07-14 16:33:35 +0000

    feat(fe): create story for Badge

---
+++ frontend/src/common/components/Molecule/Badge.story.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Badge from '@/common/components/Molecule/Badge.vue'
+</script>
+
+<template>
+  <Story>
+    <Badge endtime="12:30:50" :ended="true" />
+    <h2 class="mt-8 mb-2 text-lg">Another Example</h2>
+    <Badge endtime="14:00:00" :ended="false" />
+  </Story>
+</template>
