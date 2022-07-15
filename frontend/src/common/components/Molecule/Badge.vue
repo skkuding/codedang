efe0c6e1b3bae4edb43a6e1cbaff392261fdd95a
Jaemin Choi <1dotolee@gmail.com>
2022-07-15 13:51:57 +0000

    fix: create explicit color mapper
    TailwindCSS에서 template string으로 생성된 class를 인식하지 못하기 때문에
    사용될 class가 미리 정의된 mapper를 사용한다.

--- frontend/src/common/components/Molecule/Badge.vue
+++ frontend/src/common/components/Molecule/Badge.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
 import { computed } from 'vue'
+
 const props = defineProps<{
   color: string
   text: string
 }>()
 
-const setColor = computed(() => `bg-${props.color}`)
+const colorMapper: Record<string, string> = {
+  red: 'bg-red',
+  green: 'bg-green',
+  blue: 'bg-blue'
+}
+
+const highlightColor = computed(() => {
+  return colorMapper[props.color] || 'bg-gray-dark'
+})
+
 const textSize = computed(() => [
   props.text.length > 8 ? 'text-[18px]' : 'text-[22px]',
   props.text.length < 6 ? 'ml-6' : 'ml-2'
@@ -17,7 +27,7 @@ const textSize = computed(() => [
     class="border-gray bg-gray-light relative box-border flex h-14 w-40 rounded-md border p-1"
   >
     <div
-      :class="setColor"
+      :class="highlightColor"
       class="margin top-0 bottom-0 my-auto ml-3 h-6 w-6 rounded-full"
     ></div>
     <div
