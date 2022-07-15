8c6d66a9e3913896f6b33a7b1226c3c54f2ae684
goo314 <rn0731@g.skku.edu>
2022-07-15 16:03:55 +0000

    feat(fe): create story for search-bar and pagination-table

---
+++ frontend/src/common/components/Organism/PaginationTable.story.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import PaginationTable from './PaginationTable.vue'
+</script>
+
+<template>
+  <Story>
+    <PaginationTable
+      :fields="[{ key: 'title' }, { key: 'date' }]"
+      :items="[
+        { title: 'Lorem Ipsum is simply dummy text', date: '2022-01-01' }
+      ]"
+      :per-page="4"
+      placeholder="keywords"
+      text="No data"
+      :number-of-pages="5"
+    >
+      <template #title="data">
+        {{ data.row.title }}
+      </template>
+      <template #date="data">
+        {{ data.row.date }}
+      </template>
+    </PaginationTable>
+  </Story>
+</template>
