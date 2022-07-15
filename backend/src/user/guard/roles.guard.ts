254cc61b30ab16def63f3591911868f2e07a73d2
HakSan <cranemont@gmail.com>
2022-07-15 16:58:59 +0000

    feat(be): implement role-based guard

---
+++ backend/src/user/guard/roles.guard.ts
@@ -0,0 +1,52 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { Role } from '@prisma/client'
+import { IS_PUBLIC_KEY } from 'src/common/decorator/public.decorator'
+import { ROLES_KEY } from 'src/common/decorator/roles.decorator'
+import { UserService } from 'src/user/user.service'
+import { AuthenticatedRequest } from '../../auth/interface/authenticated-request.interface'
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  rolesHierarchy = {}
+
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly userService: UserService
+  ) {
+    Object.keys(Role).forEach((key, index) => {
+      this.rolesHierarchy[key] = index
+    })
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ])
+    const role = this.reflector.getAllAndOverride<Role>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass()
+    ])
+
+    if (isPublic || !role) {
+      return true
+    }
+
+    const request: AuthenticatedRequest = context.switchToHttp().getRequest()
+    const userRole = await this.userService.getUserRole(request.user.id)
+    if (!userRole) {
+      throw new UnauthorizedException()
+    }
+
+    if (this.rolesHierarchy[userRole.role] >= this.rolesHierarchy[role]) {
+      return true
+    }
+    return false
+  }
+}
