817317ea6d5377055684f871196b31192279f710
djc06048 <gsafe1213@gmail.com>
2022-07-18 12:45:06 +0000

    fix(fe): add card story and change to slot

---
+++ frontend/src/home/components/Card.story.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Card from './Card.vue'
+import GridiconsNoticeOutline from '~icons/gridicons/notice-outline'
+</script>
+
+<template>
+  <Story>
+    <Card
+      :noticelist="[
+        { title: 'SKKUDING', date: '2020-03-03' },
+        { title: 'SKKUDING', date: '2020-03-03' }
+      ]"
+    >
+      <GridiconsNoticeOutline class="text-text-title text-xl" />
+      <span class="ml-2">Notice</span>
+    </Card>
+  </Story>
+</template>
---
+++ frontend/src/home/components/Card.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import MaterialSymbolsMenu from '~icons/material-symbols/menu'
+import MaterialSymbolsArrowForwardIosRounded from '~icons/material-symbols/arrow-forward-ios-rounded'
+interface data {
+  title: string
+  date: string
+}
+
+defineProps<{
+  noticelist: data[]
+}>()
+</script>
+
+<template>
+  <div class="text-gray mb-3 max-w-[610px] rounded-lg p-5 shadow-xl">
+    <div class="relative w-full">
+      <div class="flex flex-row align-middle">
+        <table class="mb-3 w-full">
+          <tr>
+            <th class="mb-3 flex flex-row align-middle">
+              <slot></slot>
+              <div class="absolute right-0">
+                <a href="#"><MaterialSymbolsMenu /></a>
+              </div>
+            </th>
+          </tr>
+          <ul>
+            <tr
+              v-for="item in noticelist"
+              :key="item.title"
+              class="mt-2 flex flex-row align-middle"
+            >
+              <td>
+                <a href="#"><MaterialSymbolsArrowForwardIosRounded /></a>
+              </td>
+              <td class="w-2/3">
+                <a href="#">{{ item.title }}</a>
+              </td>
+              <td class="w-1/3 text-right">
+                <a href="#">{{ item.date }}</a>
+              </td>
+            </tr>
+          </ul>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
