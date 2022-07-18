3a594fdb7bea7d6cc1874ad79d35c739c9d78223
hbsongk <68498323+hbsongk@users.noreply.github.com>
2022-07-18 12:41:20 +0000

    chore(fe): renew carousel

--- frontend/src/home/components/Carousel.vue
+++ frontend/src/home/components/Carousel.vue
@@ -1,90 +1,26 @@
 <script setup lang="ts">
-import { onMounted, onBeforeUnmount } from 'vue'
-import CarouselItem from './CarouselItem.vue'
-import CarouselIndicators from './CarouselIndicators.vue'
-
+import Button from '@/common/components/Atom/Button.vue'
+import { ref } from 'vue'
 const props = defineProps<{
-  slides: Array<any>
+  slides: string[]
 }>()
 
-let currentSlide = 0
-let slideInterval = 0
-let direction = 'right'
-
-// 현재 슬라이드 지정
-const setCurrentSlide = (index: number) => {
-  currentSlide = index
-}
-
-const prev = (step = -1) => {
-  const index = currentSlide > 0 ? currentSlide + step : props.slides.length - 1
-  setCurrentSlide(index)
-  direction = 'left'
-  startSlideTimer()
-}
-
-// 우측으로 가게해줌
-// 끝이아니고 더 남았으면 우측 끝이면 0
-const _next = (step = 1) => {
-  const index = currentSlide < props.slides.length - 1 ? currentSlide + step : 0
-  setCurrentSlide(index)
-  direction = 'right'
-}
-
-const next = (step = 1) => {
-  _next(step)
-  startSlideTimer()
-}
-
-const startSlideTimer = () => {
-  stopSlideTimer()
-  slideInterval = setInterval(() => {
-    _next()
-  }, 3000)
-}
-
-const stopSlideTimer = () => {
-  clearInterval(slideInterval)
-}
-
-// 눌렀을때 해당 index로 가게 해줌.
-
+let currentSlide = ref(0)
 const switchSlide = (index: number) => {
-  const step = index - currentSlide
-  if (step > 0) {
-    next(step)
-  } else {
-    prev(step)
-  }
+  currentSlide.value = index
 }
-
-onMounted(() => {
-  startSlideTimer()
-})
-
-onBeforeUnmount(() => {
-  stopSlideTimer()
-})
 </script>
 
 <template>
-  <div class="flex justify-center">
-    <div class="w-225 h-100 relative overflow-hidden">
-      <carousel-indicators
-        :total="props.slides.length"
-        :current-index="currentSlide"
-        @switch="switchSlide($event)"
-      ></carousel-indicators>
-      <carousel-item
-        v-for="(slide, index) in slides"
-        :key="`item-${index}`"
-        :slide="slide"
-        :current-slide="currentSlide"
-        :index="index"
-        :direction="direction"
-        @mouseenter="stopSlideTimer"
-        @mouseout="startSlideTimer"
-      ></carousel-item>
+  <div class="relative">
+    <img :src="slides[currentSlide]" class="absolute z-0" />
+    <div class="z-1 absolute top-1/2 left-1/2">
+      <Button
+        v-for="(i, index) in slides.length"
+        :key="index"
+        class="aspect-square !rounded-full"
+        @click="switchSlide(index)"
+      ></Button>
     </div>
   </div>
 </template>
