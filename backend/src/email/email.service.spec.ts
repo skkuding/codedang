5e733878216d7007c684c41404fc9ae9e8b74224
realsehwan@gmail.com <realsehwan@gmail.com>
2022-07-20 07:14:49 +0000

    test: add test cases of emailService and userService
    emailService의 테스트케이스는 다음과 같습니다.
    1. email이 잘 보내졌다면 sendMail을 호출했을 것이다
    2. email이 잘 보내지지 않았다면 에러를 발생시켰을 것이다
    
    userService의 테스트케이스는 다음과 같습니다.
    1. getTokenFromCache의 반환값이 기댓값과 동일한가
    2. createTokenInCache가 호출될 때 cache의 set함수에 인자가 잘 전달되었는가
    3. deleteTokenFromCache가 호출될 때 cache의 del함수에 인자가 잘 전달되었는가
    4. 등록된 user 이메일이 없을 때 에러를 발생시키는가
    5. 등록된 user 이메일이 있다면, sendEmailResetLink에 인자가 잘 전달되었고 토큰이 생성되었는가
    6. 사용자에게 받은 토큰이 유효할 때 return값이 기댓값과 일치하는가
    7. 사용자에게 받은 토큰이 cache에 저장된 값과 다를 때 에러를 발생시키는가
    8. 사용자에게 받은 토큰이 cache에 없을 때 에러를 발생시키는가
    9. updatePassword가 호출되면 deleteTokenFromCache가 호출되는가
    10. updatePassword가 호출되면 updatePasswordInPrima가 호출되는가

--- backend/src/email/email.service.spec.ts
+++ backend/src/email/email.service.spec.ts
@@ -1,18 +1,69 @@
+import { MailerService } from '@nestjs-modules/mailer'
 import { Test, TestingModule } from '@nestjs/testing'
+import { EmailTransmissionFailedException } from 'src/common/exception/business.exception'
 import { EmailService } from './email.service'
 
 describe('EmailService', () => {
   let service: EmailService
 
+  let recipient: string
+  let expectedEmailInfo: {
+    accepted: string[]
+    rejected: string[]
+    envelopeTime: number
+    messageTime: number
+    messageSize: number
+    response: string
+    envelope: { from: string; to: string[] }
+    messageId: string
+  }
+
+  const MailerMock = {
+    sendMail: jest.fn()
+  }
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EmailService]
+      providers: [
+        EmailService,
+        { provide: MailerService, useValue: MailerMock }
+      ]
     }).compile()
 
     service = module.get<EmailService>(EmailService)
+
+    recipient = 'abc@abc.com'
+
+    expectedEmailInfo = {
+      accepted: [recipient],
+      rejected: [],
+      envelopeTime: 715,
+      messageTime: 590,
+      messageSize: 473,
+      response:
+        '250 2.0.0 OK  1658043820 q93-56600b001f061359022sm8791552pjk.54 - gsmtp',
+      envelope: { from: 'sender@gmail.com', to: [recipient] },
+      messageId: '6bd7-3d62-c84b-61865f06534b@gmail.com'
+    }
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  it('Email transmission success', async () => {
+    MailerMock.sendMail.mockReturnValueOnce(expectedEmailInfo)
+    await service.sendPasswordResetLink(recipient, 123, 'thisistoken')
+
+    expect(MailerMock.sendMail.mock.calls.length).toBe(1)
+  })
+
+  it('Email transmission failure', async () => {
+    expectedEmailInfo['accepted'] = []
+    MailerMock.sendMail.mockReturnValueOnce(Promise.resolve(expectedEmailInfo))
+
+    await expect(async () => {
+      await service.sendPasswordResetLink(recipient, 123, 'thisistoken')
+    }).rejects.toThrowError(EmailTransmissionFailedException)
   })
 })
--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,31 +1,213 @@
-import { CACHE_MANAGER, NotFoundException } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
+import { MailerService } from '@nestjs-modules/mailer'
+import { CACHE_MANAGER } from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
+import { User } from '@prisma/client'
 import { Cache } from 'cache-manager'
+import { pwResetTokenCacheKey } from 'src/common/cache/keys'
+import {
+  InvalidTokenException,
+  InvalidUserException
+} from 'src/common/exception/business.exception'
+import { EmailService } from 'src/email/email.service'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { UserService } from './user.service'
 
 describe('UserService', () => {
-  let service: UserService
-  let prisma: PrismaService
+  let userService: UserService
   let cache: Cache
+  let emailService: EmailService
+  let mailer: MailerService
+
+  const USER_ID = 1
+  const EMAIL_ADDRESS = 'email@email.com'
+  const PW_RESET_TOKEN_KEY = pwResetTokenCacheKey(USER_ID)
+  const PW_RESET_TOKEN = 'thisIsPwResetToken'
+  const TIME_TO_LIVE = 300
+  const NEW_PASSWORD = 'thisISNewPassword'
+
+  const mailerMock = {
+    sendMail: jest.fn()
+  }
+  const cacheMock = {
+    get: jest.fn().mockReturnValueOnce(PW_RESET_TOKEN),
+    set: jest.fn(),
+    del: jest.fn()
+  }
+  const user: User = {
+    id: USER_ID,
+    username: 'user',
+    password: 'password',
+    role: 'User',
+    email: EMAIL_ADDRESS,
+    has_email_authenticated: false,
+    last_login: undefined,
+    create_time: undefined,
+    update_time: undefined
+  }
+  const expectedEmailInfo = {
+    accepted: [EMAIL_ADDRESS],
+    rejected: [],
+    envelopeTime: 715,
+    messageTime: 590,
+    messageSize: 473,
+    response:
+      '250 2.0.0 OK  1658043820 q93-56600b001f061359022sm8791552pjk.54 - gsmtp',
+    envelope: { from: 'sender_email', to: [EMAIL_ADDRESS] },
+    messageId: '6bd7-3d62-c84b-61865f06534b@gmail.com'
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserService,
+        EmailService,
         { provide: PrismaService, useValue: {} },
-        { provide: CACHE_MANAGER, useValue: {} },
-        { provide: ConfigService, useValue: {} }
+        { provide: CACHE_MANAGER, useValue: cacheMock },
+        { provide: MailerService, useValue: mailerMock }
       ]
     }).compile()
 
-    service = module.get<UserService>(UserService)
-    prisma = module.get<PrismaService>(PrismaService)
+    userService = module.get<UserService>(UserService)
+    emailService = module.get<EmailService>(EmailService)
+    mailer = module.get<MailerService>(MailerService)
     cache = module.get(CACHE_MANAGER)
   })
 
   it('should be defined', () => {
-    expect(service).toBeDefined()
+    expect(userService).toBeDefined()
+  })
+
+  it('get token', async () => {
+    const ret = await userService.getTokenFromCache(PW_RESET_TOKEN_KEY)
+
+    expect(ret).toEqual(PW_RESET_TOKEN)
+  })
+
+  it('set token', async () => {
+    const spy = jest.spyOn(cache, 'set')
+
+    await userService.createTokenInCache(
+      PW_RESET_TOKEN_KEY,
+      PW_RESET_TOKEN,
+      TIME_TO_LIVE
+    )
+
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy.mock.calls[0][0]).toBe(PW_RESET_TOKEN_KEY)
+    expect(spy.mock.calls[0][1]).toBe(PW_RESET_TOKEN)
+    expect(spy.mock.calls[0][2]).toEqual({ ttl: TIME_TO_LIVE })
+  })
+
+  it('delete token', async () => {
+    const spy = jest.spyOn(cache, 'del')
+
+    await userService.deleteTokenFromCache(PW_RESET_TOKEN_KEY)
+
+    expect(spy).toHaveBeenCalledTimes(1)
+    expect(spy.mock.calls[0][0]).toBe(PW_RESET_TOKEN_KEY)
+  })
+
+  describe('create a password reset token and send it to given email', () => {
+    let getUserCredentialByEmailSpy
+    let sendMailSpy
+    let sendPasswordResetLinkSpy
+
+    beforeEach(() => {
+      getUserCredentialByEmailSpy = jest
+        .spyOn(userService, 'getUserCredentialByEmail')
+        .mockResolvedValue(user)
+
+      sendMailSpy = jest
+        .spyOn(mailer, 'sendMail')
+        .mockResolvedValue(expectedEmailInfo)
+
+      sendPasswordResetLinkSpy = jest
+        .spyOn(emailService, 'sendPasswordResetLink')
+        .mockResolvedValueOnce(expectedEmailInfo)
+    })
+
+    it('check if user email exist (Non-Exist)', async () => {
+      getUserCredentialByEmailSpy = jest
+        .spyOn(userService, 'getUserCredentialByEmail')
+        .mockReturnValueOnce(Promise.resolve(null))
+      expect(async () => {
+        await userService.createTokenAndSendEmail({ email: EMAIL_ADDRESS })
+      }).rejects.toThrowError(InvalidUserException)
+    })
+
+    it('if user email exist, create token and send email', async () => {
+      const tokenGeneratorSpy = jest.spyOn(
+        userService,
+        'createBase64UrlEncodedTokenRandomly'
+      )
+      await userService.createTokenAndSendEmail({ email: EMAIL_ADDRESS })
+      expect(sendPasswordResetLinkSpy).toBeCalledTimes(1)
+      expect(sendPasswordResetLinkSpy.mock.calls[0][0]).toBe(EMAIL_ADDRESS)
+      expect(sendPasswordResetLinkSpy.mock.calls[0][1]).toBe(USER_ID)
+      expect(tokenGeneratorSpy).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('update user password if given token is valid', () => {
+    beforeEach(() => {
+      userService.updateUserPasswordInPrisma = jest.fn()
+      userService.getTokenFromCache = jest
+        .fn()
+        .mockReturnValueOnce(Promise.resolve(PW_RESET_TOKEN))
+    })
+
+    it('token is valid', async () => {
+      const returnMessage = await userService.updatePassword(
+        1,
+        PW_RESET_TOKEN,
+        {
+          newPassword: NEW_PASSWORD
+        }
+      )
+      expect(returnMessage).toEqual('Password Reset successfully')
+    })
+
+    it('token is invalid', async () => {
+      userService.getTokenFromCache = jest
+        .fn()
+        .mockReturnValueOnce('reset_token_invalid')
+        .mockReturnValueOnce(null)
+
+      expect(async () => {
+        await userService.updatePassword(USER_ID, PW_RESET_TOKEN, {
+          newPassword: NEW_PASSWORD
+        })
+      }).rejects.toThrowError(InvalidTokenException)
+
+      expect(async () => {
+        await userService.updatePassword(USER_ID, PW_RESET_TOKEN, {
+          newPassword: NEW_PASSWORD
+        })
+      }).rejects.toThrowError(InvalidTokenException)
+    })
+
+    it('check if deleteToken function is called', async () => {
+      const deleteTokenSpy = jest.spyOn(userService, 'deleteTokenFromCache')
+      await userService.updatePassword(USER_ID, PW_RESET_TOKEN, {
+        newPassword: NEW_PASSWORD
+      })
+      expect(deleteTokenSpy).toHaveBeenCalledTimes(1)
+      expect(deleteTokenSpy.mock.calls[0][0]).toBe(
+        pwResetTokenCacheKey(USER_ID)
+      )
+    })
+
+    it('check if updatePasswordInPrisma function is called', async () => {
+      const updatePasswordInPrismaSpy = jest.spyOn(
+        userService,
+        'updateUserPasswordInPrisma'
+      )
+      await userService.updatePassword(USER_ID, PW_RESET_TOKEN, {
+        newPassword: NEW_PASSWORD
+      })
+      expect(updatePasswordInPrismaSpy).toHaveBeenCalledTimes(1)
+      expect(updatePasswordInPrismaSpy.mock.calls[0][0]).toBe(USER_ID)
+      expect(updatePasswordInPrismaSpy.mock.calls[0][1]).toBe(NEW_PASSWORD)
+    })
   })
 })
