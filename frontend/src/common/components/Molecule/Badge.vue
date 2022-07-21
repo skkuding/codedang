ae7261cb136078716a5a606c4d71b4e6dd122807
Jaemin Choi <1dotolee@gmail.com>
2022-07-21 00:16:22 +0000

    style: shorten code

--- frontend/src/common/components/Molecule/Badge.vue
+++ frontend/src/common/components/Molecule/Badge.vue
@@ -1,31 +1,21 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-
-type Color = 'red' | 'green' | 'blue'
-
-const props = defineProps<{
-  color: Color
+defineProps<{
+  color: 'red' | 'green' | 'blue'
 }>()
 
-const colorMapper: Record<string, string> = {
+const colorMapper = {
   red: 'bg-red',
   green: 'bg-green',
   blue: 'bg-blue'
 }
-
-const highlightColor = computed(() => {
-  return colorMapper[props.color] || 'bg-gray-dark'
-})
 </script>
 
 <template>
   <div
-    class="border-gray bg-gray-light relative box-border flex h-8 w-fit rounded-md border px-2"
+    class="border-gray bg-gray-light flex h-8 w-fit items-center rounded-md border px-2"
   >
-    <div class="my-auto w-6">
-      <div :class="highlightColor" class="m-auto h-4 w-4 rounded-full"></div>
-    </div>
-    <div class="text-gray-dark my-auto mx-2 font-bold">
+    <div :class="colorMapper[color]" class="mx-1 h-4 w-4 rounded-full" />
+    <div class="text-gray-dark mx-2 font-bold">
       <slot />
     </div>
   </div>
