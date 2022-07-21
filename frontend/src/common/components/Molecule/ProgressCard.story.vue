6f9286ea2a968429618db0ffb098a5df882f5535
Jaemin Choi <1dotolee@gmail.com>
2022-07-21 00:48:23 +0000

    docs: split components by variant

--- frontend/src/common/components/Molecule/ProgressCard.story.vue
+++ frontend/src/common/components/Molecule/ProgressCard.story.vue
@@ -4,22 +4,25 @@ import ProgressCard from './ProgressCard.vue'
 
 <template>
   <Story>
-    <ProgressCard
-      title="프로그래밍대회"
-      header="updated 3.30"
-      description="프로그래밍대회 기출문제 입니다."
-      :total="10"
-      :complete="6"
-    />
-    <h2 class="mt-8 mb-2 text-lg">Another Example</h2>
-    <ProgressCard
-      title="프로그래밍대회"
-      header="updated 3.30"
-      description="프로그래밍대회 기출문제 입니다."
-      :total="100"
-      :complete="80"
-      progress-text="problems"
-      color="#FFC3DA"
-    />
+    <Variant title="Default">
+      <ProgressCard
+        title="프로그래밍대회"
+        header="updated 3.30"
+        description="프로그래밍대회 기출문제 입니다."
+        :total="10"
+        :complete="6"
+      />
+    </Variant>
+    <Variant title="Custom">
+      <ProgressCard
+        title="프로그래밍대회"
+        header="updated 3.30"
+        description="프로그래밍대회 기출문제 입니다."
+        :total="100"
+        :complete="80"
+        progress-text="problems"
+        color="#FFC3DA"
+      />
+    </Variant>
   </Story>
 </template>
