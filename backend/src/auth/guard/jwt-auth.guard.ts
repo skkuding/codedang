6126ebc298948f92e4c23ba3015272e91f2920f4
minseo999 <alstj778@gmail.com>
2022-07-21 09:47:22 +0000

    fix(be): add return type to canActivate method - canActivate 타입 추론 에러를 해결

--- backend/src/auth/guard/jwt-auth.guard.ts
+++ backend/src/auth/guard/jwt-auth.guard.ts
@@ -1,6 +1,7 @@
 import { ExecutionContext, Injectable } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { AuthGuard } from '@nestjs/passport'
+import { Observable } from 'rxjs'
 import { IS_PUBLIC_KEY } from 'src/common/decorator/public.decorator'
 
 @Injectable()
@@ -9,7 +10,9 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     super()
   }
 
-  canActivate(context: ExecutionContext) {
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
       context.getHandler(),
       context.getClass()
