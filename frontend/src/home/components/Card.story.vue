e83cccdd12bc166484dfa3e72e06a96db3f4186b
djc06048 <gsafe1213@gmail.com>
2022-07-22 15:11:22 +0000

    fix(fe): update card story

--- frontend/src/home/components/Card.story.vue
+++ frontend/src/home/components/Card.story.vue
@@ -21,11 +21,11 @@ import FaSolidMedal from '~icons/fa-solid/medal'
         }
       ]"
     >
-      <template #next-to-text-icon>
-        <a href="#"><MaterialSymbolsArrowForwardIosRounded /></a>
-      </template>
       <GridiconsNoticeOutline class="text-text-title text-xl" />
       <span class="ml-2">Notice</span>
+      <template #notice-icon>
+        <a href="#"><MdiDotsHorizontal /></a>
+      </template>
     </Card>
 
     <h1 class="m-4 text-xl">Another Example</h1>
@@ -34,23 +34,29 @@ import FaSolidMedal from '~icons/fa-solid/medal'
       :noticelist="[
         {
           title: 'SKKU Coding Platform 모의대회',
-          date: 'Jan 9,2021'
+          date: 'Jan 9,2021',
+          state: 'ongoing'
         },
         {
           title: 'SKKU Coding Platform 1차 대회',
-          date: 'Dec 31,2020'
+          date: 'Dec 31,2020',
+          state: 'prearranged'
         },
         {
           title: 'SKKU Coding Platform 2차 대회',
-          date: 'Dec 31,2020'
+          date: 'Dec 31,2020',
+          state: 'prearranged'
         }
       ]"
     >
-      <template #next-to-text-icon>
-        <a href="#"><MdiDotsHorizontal /></a>
-      </template>
       <FaSolidMedal class="text-text-title text-xl" />
       <span class="ml-2">Current/Upcoming Contests</span>
+      <template #contest-icon-ongoing>
+        <a href="#"><MdiDotsHorizontal /></a>
+      </template>
+      <template #contest-icon-prearranged>
+        <a href="#"><Fa6RegularCalendar /></a>
+      </template>
     </Card>
   </Story>
 </template>
