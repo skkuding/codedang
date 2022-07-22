269b99dc60d09e97f4f1941e5f2fed9d6ad6a5a7
djc06048 <gsafe1213@gmail.com>
2022-07-22 11:36:16 +0000

    fix(fe): update modal story

--- frontend/src/common/components/Molecule/Modal.story.vue
+++ frontend/src/common/components/Molecule/Modal.story.vue
@@ -4,13 +4,21 @@ import ModalVue from './Modal.vue'
 
 <template>
   <Story>
-    <ModalVue title="skkuding coding platform" title-color="green">
+    <ModalVue
+      title="skkuding coding platform"
+      title-color="green"
+      class="h-[767px] w-[360px]"
+    >
       <template #modal-image>
         <img src="@/common/assets/logo.svg" class="w-20" alt="" />
       </template>
       <template #modal-content>this is modal</template>
     </ModalVue>
 
-    <ModalVue title="skkuding coding platform" title-color="green" />
+    <ModalVue
+      title="skkuding coding platform"
+      title-color="gray"
+      class="h-[767px] w-[360px]"
+    />
   </Story>
 </template>
