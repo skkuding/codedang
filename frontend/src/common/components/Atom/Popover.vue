7ccd3dcf5da2d01a910ee4bd6d7094ff6bd6a049
SOLBI1028 <hagkthfql@naver.com>
2022-07-23 17:25:28 +0000

    feat(fe): add popover component

---
+++ frontend/src/common/components/Atom/Popover.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const props = defineProps<{
+  text: string
+  contentId: string
+  hover?: boolean
+}>()
+
+const mounted = ref(false)
+onMounted(() => {
+  mounted.value = true
+})
+
+const open = ref(false)
+const toggle = () => {
+  if (!props.hover) open.value = !open.value
+}
+const setHover = () => {
+  if (props.hover) open.value = true
+}
+const setHoverFalse = () => {
+  if (props.hover) open.value = false
+}
+</script>
+
+<template>
+  <div class="relative">
+    <span @click="toggle" @mouseover="setHover" @mouseleave="setHoverFalse">
+      <slot name="content" />
+    </span>
+    <teleport v-if="mounted && open" :to="contentId">
+      <span
+        class="border-t-gray absolute bottom-full ml-2 border-x-8 border-t-8 border-b-0 border-solid border-x-transparent"
+      />
+      <span
+        class="bg-gray absolute bottom-full my-2 whitespace-nowrap rounded px-2 py-1"
+      >
+        {{ text }}
+      </span>
+    </teleport>
+  </div>
+</template>
