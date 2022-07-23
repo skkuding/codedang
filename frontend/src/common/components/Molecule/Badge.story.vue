1401d15dc24ba50cbadecf3f2a53063099c5bfab
kee711 <59683761+kee711@users.noreply.github.com>
2022-07-23 18:08:59 +0900

    feat(fe): add badge component (#121)
    * feat(fe): add badge component
    
    * feat(fe): create story for Badge
    
    * chore(fe): reflect feedback
    
    * fix: update props of badge story
    
    * fix: create explicit color mapper
    
    TailwindCSS에서 template string으로 생성된 class를 인식하지 못하기 때문에
    사용될 class가 미리 정의된 mapper를 사용한다.
    
    * chore(fe): reflect feedback
    
    * chore: remove slot name
    
    * chore: add story examples
    
    * style: shorten code
    
    Co-authored-by: Jaemin Choi <1dotolee@gmail.com>

---
+++ frontend/src/common/components/Molecule/Badge.story.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Badge from './Badge.vue'
+</script>
+
+<template>
+  <Story :layout="{ type: 'grid' }">
+    <Variant title="green">
+      <Badge color="green">Badge</Badge>
+    </Variant>
+    <Variant title="red">
+      <Badge color="red">Compile Error</Badge>
+    </Variant>
+    <Variant title="blue">
+      <Badge color="blue">Passed</Badge>
+    </Variant>
+  </Story>
+</template>
