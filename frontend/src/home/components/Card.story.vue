3aabf639057cf54345435ac92a5c5e035f7c9f79
djc06048 <gsafe1213@gmail.com>
2022-07-24 05:10:38 +0000

    fix(fe): update card story

--- frontend/src/home/components/Card.story.vue
+++ frontend/src/home/components/Card.story.vue
@@ -5,57 +5,67 @@ import MaterialSymbolsArrowForwardIosRounded from '~icons/material-symbols/arrow
 import MdiDotsHorizontal from '~icons/mdi/dots-horizontal'
 import Fa6RegularCalendar from '~icons/fa6-regular/calendar'
 import FaSolidMedal from '~icons/fa-solid/medal'
+import { computed } from 'vue'
+interface data {
+  title: string
+  date: string
+  state?: string
+}
+const noticeitemlist: data[] = [
+  {
+    title: 'SKKU Coding Platform 모의대회 결과 및 솔루션',
+    date: 'Jan 9,2021'
+  },
+  {
+    title: 'SKKU Coding Platform 모의대회 결과',
+    date: 'Dec 31,2020'
+  }
+]
+const contestitemlist: data[] = [
+  {
+    title: 'SKKU Coding Platform 모의대회',
+    date: 'Jan 9,2021',
+    state: 'ongoing'
+  },
+  {
+    title: 'SKKU Coding Platform 1차 대회',
+    date: 'Dec 31,2020',
+    state: 'prearranged'
+  },
+  {
+    title: 'SKKU Coding Platform 2차 대회',
+    date: 'Dec 31,2020',
+    state: 'prearranged'
+  }
+]
 </script>
 
 <template>
   <Story>
-    <Card
-      :noticelist="[
-        {
-          title: 'SKKU Coding Platform 모의대회 결과 및 솔루션',
-          date: 'Jan 9,2021'
-        },
-        {
-          title: 'SKKU Coding Platform 모의대회 결과',
-          date: 'Dec 31,2020'
-        }
-      ]"
-    >
-      <GridiconsNoticeOutline class="text-text-title text-xl" />
-      <span class="ml-2">Notice</span>
-      <template #notice-icon>
+    <Card :carditemlist="noticeitemlist">
+      <template #title>
+        <GridiconsNoticeOutline class="text-text-title text-xl" />
+        <span class="ml-2">Notice</span>
+      </template>
+
+      <template #icon>
         <a href="#"><MdiDotsHorizontal /></a>
       </template>
     </Card>
 
     <h1 class="m-4 text-xl">Another Example</h1>
 
-    <Card
-      :noticelist="[
-        {
-          title: 'SKKU Coding Platform 모의대회',
-          date: 'Jan 9,2021',
-          state: 'ongoing'
-        },
-        {
-          title: 'SKKU Coding Platform 1차 대회',
-          date: 'Dec 31,2020',
-          state: 'prearranged'
-        },
-        {
-          title: 'SKKU Coding Platform 2차 대회',
-          date: 'Dec 31,2020',
-          state: 'prearranged'
-        }
-      ]"
-    >
-      <FaSolidMedal class="text-text-title text-xl" />
-      <span class="ml-2">Current/Upcoming Contests</span>
-      <template #contest-icon-ongoing>
-        <a href="#"><MdiDotsHorizontal /></a>
+    <Card :carditemlist="contestitemlist">
+      <template #title>
+        <FaSolidMedal class="text-text-title text-xl" />
+        <span class="ml-2">Current/Upcoming Contests</span>
       </template>
-      <template #contest-icon-prearranged>
-        <a href="#"><Fa6RegularCalendar /></a>
+
+      <template #icon="item">
+        <a href="#">
+          <MdiDotsHorizontal v-if="item.item === 'ongoing'" />
+          <Fa6RegularCalendar v-else />
+        </a>
       </template>
     </Card>
   </Story>
