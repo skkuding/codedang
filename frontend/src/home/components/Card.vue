cc204fc2320f91a67c9fc937ba0cd721843395ab
djc06048 <gsafe1213@gmail.com>
2022-07-24 05:06:42 +0000

    fix(fe): update tag style and contests card

--- frontend/src/home/components/Card.vue
+++ frontend/src/home/components/Card.vue
@@ -10,50 +10,34 @@ interface data {
 }
 
 const props = defineProps<{
-  noticelist: data[]
+  carditemlist: data[]
 }>()
 </script>
 
 <template>
-  <div class="mb-3 max-w-[610px] rounded-lg p-5 shadow-xl">
-    <div class="relative w-full">
-      <div class="flex flex-row align-middle">
-        <table class="mb-3 w-full">
-          <tr>
-            <th class="mb-3 flex flex-row align-middle">
-              <slot></slot>
-              <div class="absolute right-0">
-                <a href="#"><MaterialSymbolsMenu /></a>
-              </div>
-            </th>
-          </tr>
-          <ul>
-            <tr
-              v-for="item in noticelist"
-              :key="item.title"
-              class="mt-2 flex flex-row align-middle"
-            >
-              <td class="mx-1">
-                <slot name="notice-icon"></slot>
-                <slot
-                  v-if="item.state === 'ongoing'"
-                  name="contest-icon-ongoing"
-                ></slot>
-                <slot
-                  v-if="item.state === 'prearranged'"
-                  name="contest-icon-prearranged"
-                ></slot>
-              </td>
-              <td class="w-2/3">
-                <a href="#">{{ item.title }}</a>
-              </td>
-              <td class="w-1/3 text-right">
-                <a href="#">{{ item.date }}</a>
-              </td>
-            </tr>
-          </ul>
-        </table>
+  <div class="mb-3 w-full max-w-[610px] rounded-lg p-5 shadow-xl">
+    <div class="flex w-full flex-col">
+      <div class="mb-3 flex w-full flex-row justify-between align-middle">
+        <div class="flex"><slot name="title"></slot></div>
+        <a href="#"><MaterialSymbolsMenu /></a>
       </div>
+      <ul>
+        <li
+          v-for="item in carditemlist"
+          :key="item.title"
+          class="mt-2 flex flex-row align-middle"
+        >
+          <div class="mx-1">
+            <slot name="icon" :item="item.state"></slot>
+          </div>
+          <div class="w-2/3">
+            <a href="#">{{ item.title }}</a>
+          </div>
+          <div class="w-1/3 text-right">
+            <a href="#">{{ item.date }}</a>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
