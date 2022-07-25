4653a3629b32da1c3c3edfe2493380869af08f8a
hbsongk <68498323+hbsongk@users.noreply.github.com>
2022-07-25 11:13:01 +0000

    feat(fe): add Sidebar

---
+++ frontend/src/common/components/Organism/Sidebar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="bg-gray-light h-screen w-[200px]">
+    <img src="@/../codingPlatformLogo.png" />
+    <div>
+      <div class="mb-40">
+        <ul>
+          <li class="border p-4">Notice</li>
+          <li class="border p-4">Contest</li>
+          <li class="border p-4">Problem</li>
+          <li class="border p-4">Problem Pool</li>
+        </ul>
+      </div>
+      <div class="mb-40">
+        <ul>
+          <li class="border p-4">Member</li>
+          <li class="border p-4">Submission</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
