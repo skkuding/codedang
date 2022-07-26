567617dff3ad9ed127f596beed22637cbec0cf69
aintbe <drwho@g.skku.edu>
2022-07-26 08:56:34 +0000

    test(be): add get&delete tests

--- backend/src/notice/notice.service.spec.ts
+++ backend/src/notice/notice.service.spec.ts
@@ -8,10 +8,10 @@ import {
   UnprocessableDataException
 } from 'src/common/exception/business.exception'
 
-const noticeId = 1
+const noticeId = 2
 const userId = 1
 
-const noticeData: RequestNoticeDto = {
+const noticeDto: RequestNoticeDto = {
   group_id: 1,
   title: 'Title',
   content: 'Content',
@@ -19,7 +19,29 @@ const noticeData: RequestNoticeDto = {
   fixed: true
 }
 
-const groupData: Group = {
+const notice: Notice = {
+  id: noticeId,
+  created_by_id: userId,
+  group_id: noticeDto.group_id,
+  title: noticeDto.title,
+  content: noticeDto.content,
+  visible: noticeDto.visible,
+  fixed: noticeDto.fixed,
+  create_time: new Date(),
+  update_time: new Date()
+}
+
+const noticePrev: Notice = {
+  ...notice,
+  id: 1
+}
+
+const noticeNext: Notice = {
+  ...notice,
+  id: 3
+}
+
+const group: Group = {
   id: 1,
   created_by_id: 1,
   group_name: 'group_name',
@@ -30,26 +52,17 @@ const groupData: Group = {
   update_time: new Date()
 }
 
-const noticeResult: Notice = {
-  id: noticeId,
-  created_by_id: userId,
-  group_id: noticeData.group_id,
-  title: noticeData.title,
-  content: noticeData.content,
-  visible: noticeData.visible,
-  fixed: noticeData.fixed,
-  create_time: new Date(),
-  update_time: new Date()
-}
-
 const db = {
   notice: {
-    findUnique: jest.fn().mockResolvedValue(noticeResult),
-    create: jest.fn().mockResolvedValue(noticeResult),
-    update: jest.fn().mockResolvedValue(noticeResult)
+    findMany: jest.fn(),
+    findUnique: jest.fn().mockResolvedValue(notice),
+    findFirst: jest.fn(),
+    create: jest.fn().mockResolvedValue(notice),
+    update: jest.fn().mockResolvedValue(notice),
+    delete: jest.fn()
   },
   group: {
-    findUnique: jest.fn().mockResolvedValue(groupData)
+    findUnique: jest.fn().mockResolvedValue(group)
   }
 }
 
@@ -70,17 +83,143 @@ describe('NoticeService', () => {
 
   describe('createNotice', () => {
     beforeEach(() => {
-      db.group.findUnique.mockResolvedValue(groupData)
+      db.group.findUnique.mockResolvedValue(group)
     })
 
     it('should return new created notice data', async () => {
-      const createResult = await service.createNotice(userId, noticeData)
-      expect(createResult).toEqual(noticeResult)
+      const createResult = await service.createNotice(userId, noticeDto)
+      expect(createResult).toEqual(notice)
     })
 
     it('should throw error when given group does not exist', async () => {
       db.group.findUnique.mockResolvedValue(null)
-      await expect(service.createNotice(userId, noticeData)).rejects.toThrow(
+      await expect(service.createNotice(userId, noticeDto)).rejects.toThrow(
+        EntityNotExistException
+      )
+    })
+  })
+
+  describe('getNoticesByGroupId', () => {
+    const noticeArray = [
+      {
+        id: noticePrev.id,
+        title: noticePrev.title,
+        create_time: noticePrev.create_time
+      },
+      {
+        id: notice.id,
+        title: notice.title,
+        create_time: notice.create_time
+      },
+      {
+        id: noticeNext.id,
+        title: noticeNext.title,
+        create_time: noticeNext.create_time
+      }
+    ]
+
+    it('should return notice list of the group', async () => {
+      db.notice.findMany.mockResolvedValue(noticeArray)
+
+      const getNoticesByGroupId = await service.getNoticesByGroupId(group.id, 1)
+      expect(getNoticesByGroupId).toEqual(noticeArray)
+    })
+  })
+
+  describe('getNotice', () => {
+    const userNotice = {
+      current: {
+        title: notice.title,
+        content: notice.content,
+        create_time: notice.create_time,
+        update_time: notice.update_time
+      },
+      prev: {
+        id: noticePrev.id,
+        title: noticePrev.title
+      },
+      next: {
+        id: noticeNext.id,
+        title: noticeNext.title
+      }
+    }
+
+    it('should return a notice and previews', async () => {
+      db.notice.findFirst
+        .mockResolvedValueOnce(userNotice.current)
+        .mockResolvedValueOnce(userNotice.prev)
+        .mockResolvedValueOnce(userNotice.next)
+
+      const getNotice = await service.getNotice(noticeId, group.id)
+      expect(getNotice).toEqual(userNotice)
+    })
+
+    it('should throw error when the notice does not exist', async () => {
+      db.notice.findFirst.mockResolvedValue(null)
+
+      await expect(service.getNotice(noticeId, group.id)).rejects.toThrow(
+        EntityNotExistException
+      )
+    })
+  })
+
+  describe('getAdminNotices', () => {
+    const noticeArray = [
+      {
+        id: noticePrev.id,
+        group: group,
+        title: noticePrev.title,
+        create_time: noticePrev.create_time,
+        visible: noticePrev.visible
+      },
+      {
+        id: notice.id,
+        group: group,
+        title: notice.title,
+        create_time: notice.create_time,
+        visible: notice.visible
+      },
+      {
+        id: noticeNext.id,
+        group: group,
+        title: noticeNext.title,
+        create_time: noticeNext.create_time,
+        visible: noticeNext.visible
+      }
+    ]
+
+    it('should return notice list of the group', async () => {
+      db.notice.findMany.mockResolvedValue(noticeArray)
+
+      const getNoticesByGroupId = await service.getAdminNotices(userId, 1)
+      expect(getNoticesByGroupId).toEqual(noticeArray)
+    })
+  })
+
+  describe('getAdminNotice', () => {
+    const adminNotice = {
+      group: {
+        id: group.id,
+        group_name: group.group_name
+      },
+      title: notice.title,
+      content: notice.content,
+      visible: notice.visible,
+      fixed: notice.fixed
+    }
+    it('should return a notice', async () => {
+      db.notice.findUnique.mockResolvedValueOnce(adminNotice)
+
+      const getNotice = await service.getAdminNotice(noticeId)
+      expect(getNotice).toEqual(adminNotice)
+    })
+
+    it('should throw error when given notice does not exist', async () => {
+      db.notice.findUnique.mockRejectedValueOnce(
+        new EntityNotExistException('notice')
+      )
+
+      await expect(service.getAdminNotice(noticeId)).rejects.toThrow(
         EntityNotExistException
       )
     })
@@ -88,16 +227,16 @@ describe('NoticeService', () => {
 
   describe('updateNotice', () => {
     beforeEach(() => {
-      db.notice.findUnique.mockResolvedValue(noticeResult)
+      db.notice.findUnique.mockResolvedValue(notice)
     })
 
     it('should return updated Notice', async () => {
-      const updateResult = await service.updateNotice(noticeId, noticeData)
-      expect(updateResult).toEqual(noticeResult)
+      const updateResult = await service.updateNotice(noticeId, noticeDto)
+      expect(updateResult).toEqual(notice)
     })
 
     it('should throw error when group_id change', async () => {
-      const invalidNoticeData: RequestNoticeDto = {
+      const invalidnoticeDto: RequestNoticeDto = {
         group_id: 2,
         title: 'Title',
         content: 'Content',
@@ -106,15 +245,39 @@ describe('NoticeService', () => {
       }
 
       await expect(
-        service.updateNotice(noticeId, invalidNoticeData)
+        service.updateNotice(noticeId, invalidnoticeDto)
       ).rejects.toThrow(UnprocessableDataException)
     })
 
     it('should throw error when given notice does not exist', async () => {
-      db.notice.findUnique.mockResolvedValue(null)
-      await expect(service.updateNotice(noticeId, noticeData)).rejects.toThrow(
+      db.notice.findUnique.mockRejectedValue(
+        new EntityNotExistException('notice')
+      )
+      await expect(service.updateNotice(noticeId, noticeDto)).rejects.toThrow(
         EntityNotExistException
       )
+    })
+  })
+
+  describe('deleteContest', () => {
+    afterEach(() => {
+      db.notice.delete.mockClear()
+    })
+
+    it('should successfully delete given notice', async () => {
+      await service.deleteNotice(noticeId)
+      expect(db.notice.delete).toBeCalledTimes(1)
+    })
+
+    it('should throw error when given notice does not exist', async () => {
+      db.notice.findUnique.mockRejectedValue(
+        new EntityNotExistException('notice')
+      )
+
+      await expect(service.deleteNotice(noticeId)).rejects.toThrow(
+        EntityNotExistException
+      )
+      expect(db.notice.delete).toBeCalledTimes(0)
     })
   })
 })
