ba7a67fb265754d610423ef6258bed975b5b561e
Jaemin Choi <1dotolee@gmail.com>
2022-07-26 13:41:22 +0000

    feat: bind modelValue for v-model usage
    ref: https://vuejs.org/guide/components/events.html#usage-with-v-model

--- frontend/src/common/components/Molecule/Switch.story.vue
+++ frontend/src/common/components/Molecule/Switch.story.vue
@@ -1,17 +1,13 @@
 <script setup lang="ts">
-import Switch from '@/common/components/Molecule/Switch.vue'
+import Switch from './Switch.vue'
 import { ref } from 'vue'
 
-var dat1 = ref(false)
-var dat2 = ref(false)
+const data = ref(false)
 </script>
 
 <template>
   <Story>
-    <Switch label="Label" @switchdata="(i) => (dat1 = i)" />
-    <p>Emmited value : {{ dat1 }}</p>
-    <h2 class="mt-8 mb-2 text-lg">Another Example</h2>
-    <Switch label="Private" @switchdata="(i) => (dat2 = i)" />
-    <p>Emmited value : {{ dat2 }}</p>
+    <Switch v-model="data" label="Toggle me!" />
+    <p>Current value: {{ data }}</p>
   </Story>
 </template>
--- frontend/src/common/components/Molecule/Switch.vue
+++ frontend/src/common/components/Molecule/Switch.vue
@@ -1,25 +1,23 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue'
-defineProps<{
-  label: string
+import { ref } from 'vue'
+
+const props = defineProps<{
+  label?: string
+  modelValue: boolean
 }>()
 
-const switchdata = ref(false)
+defineEmits<{
+  (e: 'update:modelValue', value: boolean): void
+}>()
 
-const emit = defineEmits(['switchdata'])
-watch(
-  () => switchdata.value,
-  (newValue) => {
-    emit('switchdata', newValue)
-  }
-)
+const data = ref(props.modelValue)
 </script>
 
 <template>
   <label class="flex items-center gap-2">
     <div class="text-text-title font-bold">{{ label }}</div>
     <input
-      v-model="switchdata"
+      v-model="data"
       type="checkbox"
       role="switch"
       class="border-gray before:bg-gray checked:before:bg-green checked:border-green grid h-6 w-12 cursor-pointer appearance-none rounded-full border px-1 before:my-auto before:h-4 before:w-4 before:rounded-full before:transition checked:before:translate-x-5"
