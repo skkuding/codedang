0549f48dbfe1771af8f747b4c6603f6eca13fff5
djc06048 <gsafe1213@gmail.com>
2022-07-27 07:03:01 +0000

    fix(fe): add background color, change modal style

--- frontend/src/common/components/Molecule/Modal.vue
+++ frontend/src/common/components/Molecule/Modal.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { reactive, computed } from 'vue'
 import IconoirCancel from '~icons/iconoir/cancel'
 import { ref } from 'vue'
 
 const props = defineProps<{
   title: string
-  titleColor: string
+  titleColor?: string
 }>()
+const emit = defineEmits(['close'])
 
 const ModalStyle = computed(() => {
   if (props.titleColor == 'green') {
@@ -15,35 +16,27 @@ const ModalStyle = computed(() => {
     return 'text-center text-xl text-gray'
   }
 })
-const iconStyle = computed(() => {
-  if (props.titleColor == 'green') {
-    return 'text-green'
-  } else {
-    return 'text-gray'
-  }
-})
-
-const isModalVisible = ref(true)
-
-function close() {
-  isModalVisible.value = false
-}
+const isModalVisible = ref(false)
 </script>
 
 <template>
-  <div v-show="isModalVisible" class="relative rounded-lg p-2 text-center">
-    <div class="my-2 flex justify-center">
-      <slot name="modal-image"></slot>
-      <IconoirCancel
-        class="absolute right-0 float-right cursor-pointer"
-        :class="iconStyle"
-        @click="close"
-      />
-    </div>
-    <span :class="ModalStyle">{{ title }}</span>
+  <div class="ml-auto mr-auto">
+    <div class="fixed top-0 left-0 h-screen w-screen bg-black opacity-25"></div>
 
-    <div>
-      <slot name="modal-content"></slot>
+    <div
+      class="relative z-50 h-full w-full rounded-lg bg-white p-2 text-center"
+    >
+      <div class="my-2 flex justify-center">
+        <slot name="modal-image"></slot>
+        <IconoirCancel
+          class="text-gray absolute right-0 cursor-pointer"
+          @click="$emit('close')"
+        />
+      </div>
+      <span :class="ModalStyle">{{ title }}</span>
+      <div>
+        <slot name="modal-content">default content</slot>
+      </div>
     </div>
   </div>
 </template>
