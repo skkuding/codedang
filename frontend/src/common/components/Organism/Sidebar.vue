0b64cab6de3b37c10211a7a3a7c6513d10c19a71
goo314 <rn0731@g.skku.edu>
2022-07-27 11:17:48 +0000

    chore(fe): simplify code and remove img by adding icons

--- frontend/src/common/components/Organism/Sidebar.vue
+++ frontend/src/common/components/Organism/Sidebar.vue
@@ -1,98 +1,85 @@
+<script setup lang="ts">
+import BiHouse from '~icons/bi/house'
+import File from '~icons/bi/file-text'
+import Trophy from '~icons/bi/trophy'
+import User from '~icons/fa6-regular/user'
+import Brain from '~icons/fluent/brain-circuit-24-regular'
+import Box from '~icons/bi/box'
+import Code from '~icons/bi/code-square'
+import Book from '~icons/bi/journals'
+</script>
+
 <template>
-  <div class="bg-gray-light h-screen w-[200px]">
+  <div
+    class="bg-gray-light fixed top-0 bottom-0 left-0 h-full w-[200px] overflow-auto"
+  >
     <div class="align-center flex justify-center">
       <img src="@/../codingPlatformLogo.png" class="m-6 h-[77px] w-[155px]" />
     </div>
-    <div>
-      <div>
-        <ul class="text-gray-dark">
-          <router-link to="/">
-            <li
-              class="border-gray flex cursor-pointer border-y border-opacity-25 p-2 font-bold"
-            >
-              <div class="ml-6 flex">
-                <img src="@/../Home.svg" class="mr-3" />
-                <span>SKKUDING</span>
-              </div>
-            </li>
-          </router-link>
-          <router-link to="/notice">
-            <li
-              class="border-gray flex cursor-pointer border-y border-opacity-25 p-2 font-bold"
-            >
-              <div class="ml-6 flex">
-                <img src="@/../Notice.svg" class="mr-3" />
-                <span>Notice</span>
-              </div>
-            </li>
-          </router-link>
-          <router-link to="/">
-            <li
-              class="border-gray flex border-y border-opacity-25 p-2 font-bold"
-            >
-              <div class="ml-6 flex">
-                <img src="@/../Contest.svg" class="mr-3" />
-                <span>Contest</span>
-              </div>
-            </li>
-          </router-link>
+    <div class="text-gray-dark">
+      <router-link
+        class="border-gray flex justify-center border-y border-opacity-25 p-2 font-bold"
+        to="/"
+      >
+        <BiHouse class="mr-2" />
+        SKKUDING
+      </router-link>
 
-          <router-link to="/">
-            <li
-              class="border-gray flex border-y border-opacity-25 p-2 font-bold"
-            >
-              <div class="ml-6 flex">
-                <img src="@/../Workbook.svg" class="mr-3" />
-                <span>Workbook</span>
-              </div>
-            </li>
-          </router-link>
+      <router-link
+        class="border-gray flex justify-center border-y border-opacity-25 p-2 font-bold"
+        to="/notice"
+      >
+        <File class="mr-2" />
+        Notice
+      </router-link>
 
-          <router-link to="/">
-            <li
-              class="border-gray flex border-y border-opacity-25 p-2 font-bold"
-            >
-              <div class="ml-6 flex">
-                <img src="@/../Problem.svg" class="mr-3" />
-                <span>Problem</span>
-              </div>
-            </li>
-          </router-link>
+      <router-link
+        class="border-gray flex justify-center border-y border-opacity-25 p-2 font-bold"
+        to="/contest"
+      >
+        <Trophy class="mr-2" />
+        Contest
+      </router-link>
 
-          <router-link to="/">
-            <li
-              class="border-gray flex border-y border-opacity-25 p-2 font-bold"
-            >
-              <div class="ml-6 flex">
-                <img src="@/../Problem Pool.svg" class="mr-3" />
-                <span>Problem Pool</span>
-              </div>
-            </li>
-          </router-link>
+      <router-link
+        class="border-gray flex justify-center border-y border-opacity-25 p-2 font-bold"
+        to="/workbook"
+      >
+        <Book class="mr-2" />
+        Workbook
+      </router-link>
 
-          <router-link to="/">
-            <li
-              class="border-gray flex border-y border-opacity-25 p-2 font-bold"
-            >
-              <div class="ml-6 flex">
-                <img src="@/../Member.svg" class="mr-3" />
-                <span>Member</span>
-              </div>
-            </li>
-          </router-link>
+      <router-link
+        class="border-gray flex justify-center border-y border-opacity-25 p-2 font-bold"
+        to="/problem"
+      >
+        <Brain class="mr-2" />
+        Problem
+      </router-link>
 
-          <router-link to="/">
-            <li
-              class="border-gray flex border-y border-opacity-25 p-2 font-bold"
-            >
-              <div class="ml-[18px] flex">
-                <img src="@/../Submission.svg" class="mr-[9px]" />
-                <span>Submission</span>
-              </div>
-            </li>
-          </router-link>
-        </ul>
-      </div>
+      <router-link
+        class="border-gray flex justify-center border-y border-opacity-25 p-2 font-bold"
+        to="/"
+      >
+        <Box class="mr-2" />
+        Problem Pool
+      </router-link>
+
+      <router-link
+        class="border-gray flex justify-center border-y border-opacity-25 p-2 font-bold"
+        to="/"
+      >
+        <User class="mr-2" />
+        Member
+      </router-link>
+
+      <router-link
+        class="border-gray flex justify-center border-y border-opacity-25 p-2 font-bold"
+        to="/"
+      >
+        <Code class="mr-2" />
+        Submission
+      </router-link>
     </div>
   </div>
 </template>
