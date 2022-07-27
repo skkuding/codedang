201a666e643c87d37966d8a35691a177377a7feb
djc06048 <gsafe1213@gmail.com>
2022-07-27 07:03:57 +0000

    fix(fe): change modal story, add transition effect

--- frontend/src/common/components/Molecule/Modal.story.vue
+++ frontend/src/common/components/Molecule/Modal.story.vue
@@ -1,24 +1,56 @@
 <script setup lang="ts">
 import ModalVue from './Modal.vue'
+import ButtonVue from '../Atom/Button.vue'
+import { ref } from 'vue'
+const isModalVisible = ref(false)
+function close() {
+  isModalVisible.value = false
+  console.log(isModalVisible)
+}
 </script>
 
 <template>
   <Story>
-    <ModalVue
-      title="skkuding coding platform"
-      title-color="green"
-      class="h-[767px] w-[360px]"
-    >
-      <template #modal-image>
-        <img src="@/common/assets/logo.svg" class="w-20" alt="" />
-      </template>
-      <template #modal-content>this is modal</template>
-    </ModalVue>
-
-    <ModalVue
-      title="skkuding coding platform"
-      title-color="gray"
-      class="h-[767px] w-[360px]"
-    />
+    <span>Click the button, and it shows modal!</span>
+    <br />
+    <ButtonVue @click="isModalVisible = true">showmodal</ButtonVue>
+    <Transition name="modal">
+      <ModalVue
+        v-if="isModalVisible"
+        title="skkuding coding platform"
+        title-color="green"
+        class="h-[767px] w-[360px]"
+        @close="close"
+      >
+        <template #modal-image>
+          <img src="@/common/assets/logo.svg" class="w-20" alt="" />
+        </template>
+        <template #modal-content>this is modal</template>
+      </ModalVue>
+    </Transition>
   </Story>
 </template>
+<style scoped>
+.modal-enter-from {
+  opacity: 0;
+}
+.modal-enter-active {
+  transition: all 1s;
+}
+
+.modal-enter-to {
+  opacity: 1;
+}
+
+.modal-leave-from {
+  opacity: 1;
+}
+
+.modal-leave-active {
+  transition: all 1s;
+}
+
+.modal-leave-to {
+  opacity: 0;
+}
+</style>
