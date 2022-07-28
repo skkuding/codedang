d071d7f3ec3ecc34bcdb87fec403647e9128a61c
SOLBI1028 <hagkthfql@naver.com>
2022-07-28 06:03:15 +0000

    refactor: refactor style code

--- frontend/src/common/components/Molecule/Modal.vue
+++ frontend/src/common/components/Molecule/Modal.vue
@@ -8,26 +8,28 @@ const props = defineProps<{
 }>()
 defineEmits(['close'])
 
-const ModalStyle = computed(() => {
+const titleStyle = computed(() => {
   return props.titleColor === 'green' ? 'text-green' : 'text-gray'
 })
 </script>
 
 <template>
-  <div class="ml-auto mr-auto">
+  <div class="m-auto">
     <div class="fixed top-0 left-0 h-screen w-screen bg-black opacity-25"></div>
-
     <div
       class="relative z-50 h-full w-full rounded-lg bg-white p-2 text-center"
     >
       <IconoirCancel
-        class="text-gray absolute right-0 cursor-pointer"
+        class="text-gray absolute right-4 top-4 cursor-pointer"
         @click="$emit('close')"
       />
-    </div>
-    <span :class="ModalStyle">{{ title }}</span>
-    <div>
-      <slot name="modal-content">default content</slot>
+      <div class="absolute top-0 bottom-0 right-0 left-0 m-auto h-fit w-fit">
+        <div class="mx-auto my-2 w-fit"><slot name="modal-image" /></div>
+        <div class="text-bold mb-4 text-center text-xl" :class="titleStyle">
+          {{ title }}
+        </div>
+        <slot name="modal-content" />
+      </div>
     </div>
   </div>
 </template>
