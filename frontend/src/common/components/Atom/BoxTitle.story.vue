fd0964d40e4ad95712efa5afc07874118d972c93
goo314 <rn0731@g.skku.edu>
2022-07-28 07:34:31 +0000

    chore(fe): reflect feedback in box-title story

--- frontend/src/common/components/Atom/BoxTitle.story.vue
+++ frontend/src/common/components/Atom/BoxTitle.story.vue
@@ -2,20 +2,25 @@
 import BoxTitle from './BoxTitle.vue'
 </script>
 <template>
-  <Story>
-    <BoxTitle>
-      <template #title>Problems</template>
-      <template #subtitle>
-        Find problems with problem set and filters, and solve it!
-      </template>
-    </BoxTitle>
-    <h2 class="mt-8 mb-2 text-lg">Another Example</h2>
-    <BoxTitle>
-      <template #title>
-        🏆 SKKU Coding Platform
-        <span class="text-[#FEB144]">Contests</span>
-      </template>
-      <template #subtitle>Compete with schoolmates & win the prizes!</template>
-    </BoxTitle>
+  <Story :layout="{ type: 'grid', width: '100%' }">
+    <Variant title="default">
+      <BoxTitle>
+        <template #title>Problems</template>
+        <template #subtitle>
+          Find problems with problem set and filters, and solve it!
+        </template>
+      </BoxTitle>
+    </Variant>
+    <Variant title="Another Example">
+      <BoxTitle>
+        <template #title>
+          🏆 SKKU Coding Platform
+          <span class="text-[#FEB144]">Contests</span>
+        </template>
+        <template #subtitle>
+          Compete with schoolmates & win the prizes!
+        </template>
+      </BoxTitle>
+    </Variant>
   </Story>
 </template>
