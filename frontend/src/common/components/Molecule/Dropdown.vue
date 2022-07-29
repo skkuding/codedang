0e8c5a34ec95de0dac51489f1995ad7e54af3fc8
Jaemin Choi <1dotolee@gmail.com>
2022-07-29 14:52:08 +0000

    chore: close dropdown if outside clicked

--- frontend/src/common/components/Molecule/Dropdown.vue
+++ frontend/src/common/components/Molecule/Dropdown.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import { OnClickOutside } from '@vueuse/components'
 
 defineProps<{
   items: string[]
@@ -10,20 +11,22 @@ const show = ref(false)
 
 <template>
   <div class="relative inline-block">
-    <button class="cursor-pointer" @click="show = !show">
+    <button class="cursor-pointer" @click="show = true">
       <slot name="dropdown-icon" />
     </button>
-    <ul
-      v-show="show"
-      class="border-gray-light absolute right-0 flex w-max flex-col gap-1 rounded-lg border bg-white p-2 shadow-lg"
-    >
-      <li
-        v-for="item in items"
-        :key="item"
-        class="text-text-title hover:bg-gray/30 active:bg-gray/50 w-full cursor-pointer rounded-lg px-2 py-1 font-medium"
+    <OnClickOutside @trigger="show = false">
+      <ul
+        v-show="show"
+        class="border-gray-light absolute right-0 flex w-max flex-col gap-1 rounded-lg border bg-white p-2 shadow-lg"
       >
-        {{ item }}
-      </li>
-    </ul>
+        <li
+          v-for="item in items"
+          :key="item"
+          class="text-text-title hover:bg-gray/30 active:bg-gray/50 w-full cursor-pointer rounded-lg px-2 py-1 font-medium"
+        >
+          {{ item }}
+        </li>
+      </ul>
+    </OnClickOutside>
   </div>
 </template>
