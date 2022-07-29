3db387b7e060ab59b3c39208aa497677ee9d9587
Jaemin Choi <1dotolee@gmail.com>
2022-07-29 14:00:31 +0000

    style: modify header design
    글씨 크기를 줄이고 header 내에 반응형 디자인을 적용한다.

--- frontend/src/common/components/Organism/Header.vue
+++ frontend/src/common/components/Organism/Header.vue
@@ -2,49 +2,37 @@
 import Dropdown from '../Molecule/Dropdown.vue'
 import SignatureLogo from '../Atom/SignatureLogo.vue'
 import Fa6RegularUser from '~icons/fa6-regular/user'
-const content = ['Management', 'Settings', 'Logout']
 </script>
+
 <template>
-  <header
-    class="border-b-gray sticky top-0 z-50 flex h-14 w-full min-w-[540px] content-center items-center justify-between border-b bg-white px-12 py-4 font-medium opacity-95"
-  >
-    <router-link to="/">
-      <SignatureLogo class="hidden h-12 md:block" />
-    </router-link>
-    <nav>
-      <router-link
-        class="text-text-title hover:text-green mr-4 text-2xl font-medium hover:cursor-pointer hover:no-underline"
-        active-class="text-green"
-        to="/announcement"
-      >
-        Notice
+  <header class="border-b-gray grid h-14 place-items-center border-b px-8">
+    <div class="flex w-full max-w-7xl items-center justify-between">
+      <router-link to="/">
+        <SignatureLogo
+          class="hidden w-40 hover:opacity-60 active:opacity-40 md:block"
+        />
       </router-link>
-      <router-link
-        class="text-text-title hover:text-green mr-4 text-2xl font-medium hover:cursor-pointer hover:no-underline"
-        active-class="text-green"
-        to="/contest"
-      >
-        Contest
-      </router-link>
-      <router-link
-        class="text-text-title hover:text-green mr-4 text-2xl font-medium hover:cursor-pointer hover:no-underline"
-        active-class="text-green"
-        to="/problem"
-      >
-        Problem
-      </router-link>
-      <router-link
-        class="text-text-title hover:text-green mr-8 text-2xl font-medium hover:cursor-pointer hover:no-underline"
-        active-class="text-green"
-        to="/group"
-      >
-        Group
-      </router-link>
-    </nav>
-    <Dropdown :content="content">
-      <template #dropdown-icon>
-        <Fa6RegularUser class="text-text-title hover:text-green" />
-      </template>
-    </Dropdown>
+      <nav class="text-text-title flex gap-4">
+        <router-link
+          v-for="{ to, name } in [
+            { to: '/notice', name: 'Notice' },
+            { to: '/contest', name: 'Contest' },
+            { to: '/problem', name: 'Problem' },
+            { to: '/group', name: 'Group' }
+          ]"
+          :key="name"
+          class="cursor-pointer text-lg font-semibold hover:opacity-60 active:opacity-40"
+          active-class="text-green hover:opacity-70 active:opacity-50"
+          :to="to"
+        >
+          {{ name }}
+        </router-link>
+      </nav>
+      <Dropdown :content="['Management', 'Settings', 'Logout']">
+        <template #dropdown-icon>
+          <Fa6RegularUser class="text-text-title hover:text-green" />
+        </template>
+      </Dropdown>
+    </div>
   </header>
 </template>
