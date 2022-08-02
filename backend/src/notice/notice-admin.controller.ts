eeb207a66f3216a7135e3a1180d0ff5b746f1dfb
aintbe <drwho@g.skku.edu>
2022-08-02 10:51:27 +0000

    fix(be): use role guard for admin page

--- backend/src/notice/notice-admin.controller.ts
+++ backend/src/notice/notice-admin.controller.ts
@@ -14,14 +14,18 @@ import {
   NotFoundException
 } from '@nestjs/common'
 import { NoticeService } from './notice.service'
-import { Notice } from '@prisma/client'
+import { Notice, Role } from '@prisma/client'
 import { GroupManagerGuard } from 'src/group/guard/group-manager.guard'
 import { AuthenticatedRequest } from 'src/auth/interface/authenticated-request.interface'
 import { UpdateNoticeDto } from './dto/update-notice.dto'
 import { CreateNoticeDto } from './dto/create-notice.dto'
 import { EntityNotExistException } from 'src/common/exception/business.exception'
+import { Roles } from 'src/common/decorator/roles.decorator'
+import { RolesGuard } from 'src/user/guard/roles.guard'
 
 @Controller('admin/notice')
+@Roles(Role.GroupAdmin)
+@UseGuards(RolesGuard)
 export class NoticeAdminController {
   constructor(private readonly noticeService: NoticeService) {}
 
