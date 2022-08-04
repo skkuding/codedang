8642187a62327cad2fa69e1eabbc1a6cc3c37962
SOLBI1028 <75013515+SOLBI1028@users.noreply.github.com>
2022-08-04 10:04:14 +0900

    chore: add histoire plugin (#175)
    * chore: add histoire plugin
    
    * chore: fix lint error

--- frontend/histoire.config.ts
+++ frontend/histoire.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'histoire'
+import { HstVue } from '@histoire/plugin-vue'
 
 export default defineConfig({
+  plugins: [HstVue()],
   setupFile: '/src/histoire.setup.ts',
   theme: {
     title: 'SKKUding Histoire'
