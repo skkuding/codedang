771b9aed0767a88aade8918017524752f9a2ed45
cho-to <nicole2974@g.skku.edu>
2022-08-04 15:49:25 +0000

    feat(be): add getGroupById

--- backend/src/group/group.controller.ts
+++ backend/src/group/group.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  InternalServerErrorException,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Req,
+  UnauthorizedException
+} from '@nestjs/common'
+import { Group } from '@prisma/client'
+import { AuthenticatedRequest } from 'src/auth/interface/authenticated-request.interface'
+import {
+  EntityNotExistException,
+  InvalidUserException
+} from 'src/common/exception/business.exception'
 import { GroupService } from './group.service'
 
 @Controller('group')
 export class GroupController {
   constructor(private readonly groupService: GroupService) {}
+
+  @Get(':group_id')
+  async getGroupById(
+    @Req() req: AuthenticatedRequest,
+    @Param('group_id', ParseIntPipe) groupId: number
+  ): Promise<Partial<Group>> {
+    try {
+      return await this.groupService.getGroupById(req.user.id, groupId)
+    } catch (error) {
+      if (error instanceof EntityNotExistException) {
+        throw new NotFoundException(error.message)
+      }
+      if (error instanceof InvalidUserException) {
+        throw new UnauthorizedException(error.message)
+      }
+      throw new InternalServerErrorException()
+    }
+  }
 }
--- backend/src/group/group.service.ts
+++ backend/src/group/group.service.ts
@@ -1,11 +1,44 @@
 import { Injectable } from '@nestjs/common'
 import { UserGroup } from '@prisma/client'
+import {
+  EntityNotExistException,
+  InvalidUserException
+} from 'src/common/exception/business.exception'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { UserGroupData } from './interface/user-group-data.interface'
+
+function returnIsNotAllowed(userId: number, groupId: number): string {
+  return `Group ${groupId} is not allowed to User ${userId}`
+}
 
 @Injectable()
 export class GroupService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async getGroupById(userId: number, groupId: number) {
+    const group = await this.prisma.group.findUnique({
+      where: {
+        id: groupId
+      },
+      select: {
+        group_name: true,
+        description: true
+      },
+      rejectOnNotFound: () => new EntityNotExistException('group')
+    })
+
+    await this.prisma.userGroup.findFirst({
+      where: {
+        user_id: userId,
+        group_id: groupId,
+        is_registered: true
+      },
+      rejectOnNotFound: () =>
+        new InvalidUserException(returnIsNotAllowed(userId, groupId))
+    })
+
+    return group
+  }
 
   async getUserGroupMembershipInfo(userId: number, groupId: number) {
     return await this.prisma.userGroup.findFirst({
