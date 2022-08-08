6b11d6355be3bfbd746e1c02c0b2e643a5404e8b
coc0a25 <cranemont@gmail.com>
2022-08-08 06:45:57 +0000

    chore(be): udpate problem module imports

--- backend/src/problem/problem.module.ts
+++ backend/src/problem/problem.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common'
-import { ProblemController } from './problem.controller'
+import { ContestModule } from 'src/contest/contest.module'
+import { GroupModule } from 'src/group/group.module'
+import { WorkbookModule } from 'src/workbook/workbook.module'
+import {
+  PublicProblemController,
+  PublicContestProblemController,
+  PublicWorkbookProblemController,
+  GroupContestProblemController,
+  GroupWorkbookProblemController
+} from './problem.controller'
+import { ProblemRepository } from './problem.repository'
 import { ProblemService } from './problem.service'
 
 @Module({
-  controllers: [ProblemController],
-  providers: [ProblemService]
+  imports: [GroupModule, ContestModule, WorkbookModule],
+  controllers: [
+    PublicProblemController,
+    PublicContestProblemController,
+    PublicWorkbookProblemController,
+    GroupContestProblemController,
+    GroupWorkbookProblemController
+  ],
+  providers: [ProblemService, ProblemRepository]
 })
 export class ProblemModule {}
