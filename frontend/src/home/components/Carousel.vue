78ea41432ca050af37e920b1fb9f13522df1c0d0
Jaemin Choi <1dotolee@gmail.com>
2022-08-08 23:27:50 +0000

    chore: add blurred background

--- frontend/src/home/components/Carousel.vue
+++ frontend/src/home/components/Carousel.vue
@@ -21,38 +21,41 @@ const { pause, resume } = useIntervalFn(() => {
 </script>
 
 <template>
-  <div class="flex justify-center">
-    <div class="relative h-[600px] w-full overflow-hidden">
-      <div class="absolute left-1/2 bottom-5 z-10 -translate-x-1/2">
-        <button
-          v-for="index in slides.length"
-          :key="index"
-          class="m-1 h-4 w-4 cursor-pointer rounded-full border-none bg-white opacity-50"
-          :class="currentSlide === index - 1 ? 'opacity-100' : ''"
-          @click="switchSlide(index - 1)"
-          @mouseenter="pause"
-          @mouseout="resume"
-        ></button>
-      </div>
-      <transition
-        class="absolute inset-0"
-        enter-active-class="transition-transform duration-1000"
-        leave-active-class="transition-transform duration-1000"
-        :enter-from-class="
-          direction === 'right' ? 'translate-x-full' : '-translate-x-full'
-        "
-        :leave-to-class="
-          direction === 'right' ? '-translate-x-full' : 'translate-x-full'
-        "
+  <div class="bg-gray/50 relative h-[600px] w-full overflow-hidden">
+    <div class="absolute left-1/2 bottom-5 z-10 flex -translate-x-1/2 gap-2">
+      <button
+        v-for="index in slides.length"
+        :key="index"
+        class="h-4 w-4 cursor-pointer rounded-full border-none"
+        :class="currentSlide === index - 1 ? 'bg-white' : 'bg-white/50'"
+        @click="switchSlide(index - 1)"
         @mouseenter="pause"
         @mouseout="resume"
+      />
+    </div>
+    <transition
+      class="absolute inset-0"
+      enter-active-class="transition-transform duration-1000"
+      leave-active-class="transition-transform duration-1000"
+      :enter-from-class="
+        direction === 'right' ? 'translate-x-full' : '-translate-x-full'
+      "
+      :leave-to-class="
+        direction === 'right' ? '-translate-x-full' : 'translate-x-full'
+      "
+      @mouseenter="pause"
+      @mouseout="resume"
+    >
+      <div
+        :key="currentSlide"
+        class="bg-cover bg-center"
+        :style="{ backgroundImage: `url(${slides[currentSlide]})` }"
       >
         <img
-          :key="currentSlide"
-          class="h-full w-full"
+          class="h-full w-full object-contain backdrop-blur"
           :src="slides[currentSlide]"
         />
-      </transition>
-    </div>
+      </div>
+    </transition>
   </div>
 </template>
