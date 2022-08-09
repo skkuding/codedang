675161e8e6881133425aede209c611eba98ae012
minseo999 <alstj778@gmail.com>
2022-08-09 16:31:52 +0000

    fix(be): import usermodule in contest module

--- backend/src/contest/contest.module.ts
+++ backend/src/contest/contest.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { GroupModule } from 'src/group/group.module'
 import { GroupService } from 'src/group/group.service'
+import { UserModule } from 'src/user/user.module'
 import {
   ContestAdminController,
   GroupContestAdminController
@@ -9,13 +10,13 @@ import { ContestController, GroupContestController } from './contest.controller'
 import { ContestService } from './contest.service'
 
 @Module({
-  imports: [GroupModule],
+  imports: [GroupModule, UserModule],
   controllers: [
     ContestController,
     GroupContestController,
     ContestAdminController,
     GroupContestAdminController
   ],
-  providers: [ContestService, GroupService]
+  providers: [ContestService]
 })
 export class ContestModule {}
