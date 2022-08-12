5866981eb69f48b85d3beabde19d79f0c82d8e5c
goo314 <rn0731@g.skku.edu>
2022-08-12 05:58:12 +0000

    chore(fe): minor change

--- frontend/src/workbook/pages/[id].vue
+++ frontend/src/workbook/pages/[id].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import WorkbookTitle from '../components/WorkbookTitle.vue'
 import PaginationTable from '@/common/components/Organism/PaginationTable.vue'
+import IconCheck from '~icons/fa6-regular/circle-check'
 
 defineProps<{
   id: number
@@ -24,36 +25,87 @@ const colorMapper = {
   7: 'bg-level-7'
 }
 
-// example items
 const items = [
-  { id: 1, title: '가파른 경사', level: 1, submissions: 132, rate: '92.14%' },
+  {
+    id: 1,
+    title: '가파른 경사',
+    level: 1,
+    submissions: 132,
+    rate: '92.14%',
+    result: 'accepted'
+  },
   {
     id: 1006,
     title: '습격자 호루라기',
     level: 2,
     submissions: 561,
-    rate: '0.61%'
+    rate: '0.61%',
+    result: 'accepted'
   },
-  { id: 10, title: '아싸 홍삼', level: 1, submissions: 100, rate: '90%' },
+  {
+    id: 10,
+    title: '아싸 홍삼',
+    level: 1,
+    submissions: 100,
+    rate: '90%',
+    result: 'wrong answer'
+  },
   {
     id: 11,
     title: '에브리바디 홍상',
     level: 2,
     submissions: 100,
-    rate: '83%'
+    rate: '83%',
+    result: 'wrong answer'
   },
-  { id: 12, title: '나는 토깽이', level: 3, submissions: 100, rate: '72%' },
-  { id: 13, title: '나는 거부깅', level: 4, submissions: 100, rate: '65%' },
-  { id: 14, title: '토깽이 둘', level: 5, submissions: 100, rate: '52%' },
-  { id: 15, title: '토깽이 토깽이', level: 6, submissions: 100, rate: '1%' },
+  {
+    id: 12,
+    title: '나는 토깽이',
+    level: 3,
+    submissions: 100,
+    rate: '72%',
+    result: ''
+  },
+  {
+    id: 13,
+    title: '나는 거부깅',
+    level: 4,
+    submissions: 100,
+    rate: '65%',
+    result: ''
+  },
+  {
+    id: 14,
+    title: '토깽이 둘',
+    level: 5,
+    submissions: 100,
+    rate: '52%',
+    result: ''
+  },
+  {
+    id: 15,
+    title: '토깽이 토깽이',
+    level: 6,
+    submissions: 100,
+    rate: '1%',
+    result: 'accepted'
+  },
   {
     id: 16,
     title: '아싸 토깽 에브리바디 토깽',
     level: 7,
     submissions: 100,
-    rate: '1%'
+    rate: '1%',
+    result: ''
   },
-  { id: 17, title: '토깽이 토깽이', level: 7, submissions: 100, rate: '1%' }
+  {
+    id: 17,
+    title: '토깽이 토깽이',
+    level: 7,
+    submissions: 100,
+    rate: '1%',
+    result: ''
+  }
 ]
 
 const clickRow = (row: any) => {
@@ -63,15 +115,20 @@ const clickRow = (row: any) => {
 
 <template>
   <div class="my-16">
-    <WorkbookTitle text="CATS 대비 문제집 Level 1" color="red" class="mb-7" />
+    <WorkbookTitle text="CATS 대비 문제집 Level 1" color="red" class="mb-10" />
     <PaginationTable
       :fields="fields"
       :items="items"
-      placeholder="keywords"
       :number-of-pages="1"
       no-search-bar
       @row-clicked="clickRow"
     >
+      <template #title="{ row }">
+        <div class="flex items-center gap-2">
+          {{ row.title }}
+          <IconCheck v-if="row.result === 'accepted'" class="text-green" />
+        </div>
+      </template>
       <template #level="{ row }">
         <div class="flex items-center gap-2">
           <span class="h-5 w-5 rounded-full" :class="colorMapper[row.level]" />
