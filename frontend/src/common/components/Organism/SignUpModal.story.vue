ed79da7258ffc4e7b514ea71744a1b31e6dcd085
djc06048 <gsafe1213@gmail.com>
2022-08-13 14:39:10 +0000

    feat(fe): Add SignUpModal- Organism

---
+++ frontend/src/common/components/Organism/SignUpModal.story.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import SignUpModal from './SignUpModal.vue'
+const isSignUpModalVisible = ref(false)
+
+function close() {
+  isSignUpModalVisible.value = false
+}
+</script>
+
+<template>
+  <story>
+    <button @click="isSignUpModalVisible = true">회원가입 모달</button>
+    <SignUpModal
+      v-if="isSignUpModalVisible"
+      class="text-green h-[687px] w-[360px]"
+      @close="close"
+    ></SignUpModal>
+  </story>
+</template>
---
+++ frontend/src/common/components/Organism/SignUpModal.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import Modal from '../Molecule/Modal.vue'
+import InputItem from '../Atom/InputItem.vue'
+import Button from '../Atom/Button.vue'
+import MajesticonsSend from '~icons/majesticons/send'
+import Fa6SolidCheck from '~icons/fa6-solid/check'
+
+defineEmits<{
+  (e: 'open: SignUpModal', value: string): boolean
+}>()
+</script>
+
+<template>
+  <Modal>
+    <template #modal-title>
+      <div class="text-green text-2xl font-semibold">
+        <div>Welcome to</div>
+        <div>SKKU Coding Platform</div>
+      </div>
+    </template>
+    <template #modal-content>
+      <ul class="m-9 space-y-4 p-2">
+        <InputItem shadow placeholder="User ID" class="h-[36px] w-[260px]" />
+        <InputItem shadow placeholder="Student ID" class="h-[36px] w-[260px]" />
+        <InputItem shadow placeholder="Real Name" class="h-[36px] w-[260px]" />
+        <InputItem shadow placeholder="PassWord" class="h-[36px] w-[260px]" />
+        <InputItem
+          shadow
+          placeholder="PassWord Again"
+          class="h-[36px] w-[260px]"
+        />
+        <div class="flex justify-between">
+          <InputItem
+            :notice="false"
+            shadow
+            placeholder="Email Address"
+            class="h-[36px] w-[240px]"
+          />
+          <Button color="green" c>
+            <MajesticonsSend />
+          </Button>
+        </div>
+
+        <div class="flex justify-between">
+          <InputItem
+            shadow
+            placeholder="Vertification code"
+            class="h-[36px] w-[240px]"
+          />
+          <Button color="green">
+            <Fa6SolidCheck />
+          </Button>
+        </div>
+
+        <InputItem shadow placeholder="Captcha" class="h-[36px] w-[130px]" />
+      </ul>
+      <Button color="green" class="mb-2">register</Button>
+      <p class="text-gray-dark">Already have account?</p>
+      <!-- TODO: 아래 border -->
+      <button
+        class="border-b-1 text-gray-dark border-black"
+        @click="$emit('open: SignUpModal', true)"
+      >
+        Sign in
+      </button>
+    </template>
+  </Modal>
+</template>
