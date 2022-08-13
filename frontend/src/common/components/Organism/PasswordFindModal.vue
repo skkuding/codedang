32bd4d5e70575551e9e26025a599f09f49e95a04
djc06048 <gsafe1213@gmail.com>
2022-08-13 14:37:54 +0000

    feat(fe): Add PassWordFindModal- Organism

---
+++ frontend/src/common/components/Organism/PasswordFindModal.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import Modal from '../Molecule/Modal.vue'
+import InputItem from '../Atom/InputItem.vue'
+import Button from '../Atom/Button.vue'
+import MajesticonsSend from '~icons/majesticons/send'
+import Fa6SolidCheck from '~icons/fa6-solid/check'
+defineEmits<{
+  (e: 'open: logInModal', value: string): boolean
+}>()
+</script>
+
+<template>
+  <Modal>
+    <template #modal-title>
+      <div class="text-green text-2xl font-semibold">PassWord Recovery</div>
+    </template>
+    <template #modal-content>
+      <ul class="m-9 space-y-4 p-2">
+        <div class="flex justify-between">
+          <InputItem
+            :notice="false"
+            shadow
+            placeholder="Email Address"
+            class="h-[36px] w-[240px]"
+          />
+          <Button color="green" c>
+            <MajesticonsSend />
+          </Button>
+        </div>
+
+        <div class="flex justify-between">
+          <InputItem
+            shadow
+            placeholder="Vertification code"
+            class="h-[36px] w-[240px]"
+          />
+          <Button color="green">
+            <Fa6SolidCheck />
+          </Button>
+        </div>
+
+        <InputItem shadow placeholder="Captcha" class="h-[36px] w-[130px]" />
+      </ul>
+
+      <Button color="green" class="mb-2">Send Password Reset Email</Button>
+      <div>
+        <button
+          class="border-b-1 text-gray-dark border-black"
+          @click="$emit('open: logInModal', true)"
+        >
+          Back to Sign in
+        </button>
+      </div>
+    </template>
+  </Modal>
+</template>
