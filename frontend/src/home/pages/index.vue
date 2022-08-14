7fa9539c948bb97d5480acf826d58bbfbd06cee6
djc06048 <gsafe1213@gmail.com>
2022-08-14 08:54:42 +0000

    chore(fe): carousel 추가 및 header 컴포넌트 수정

--- frontend/src/home/pages/index.vue
+++ frontend/src/home/pages/index.vue
@@ -4,7 +4,7 @@ import Header from '@/common/components/Organism/Header.vue'
 import Card from '../components/Card.vue'
 import Modal from '@/common/components/Molecule/Modal.vue'
 import Button from '@/common/components/Atom/Button.vue'
-
+import Carousel from '../components/Carousel.vue'
 import InputItem from '@/common/components/Atom/InputItem.vue'
 import { ref } from 'vue'
 import IconInfo from '~icons/fa6-solid/circle-info'
@@ -47,183 +47,44 @@ const contestItems = [
     state: 'prearranged'
   }
 ]
-const auth = ref(false)
-const isSignUpModalVisible = ref(false)
-const isLogInModalVisible = ref(false)
-const isPasswordFindModalVisible = ref(false)
-function close() {
-  isSignUpModalVisible.value = false
-  isLogInModalVisible.value = false
-  isPasswordFindModalVisible.value = false
-}
+const slides = [
+  'https://picsum.photos/id/1032/900/400',
+  'https://picsum.photos/id/1033/900/400',
+  'https://picsum.photos/id/1037/900/400',
+  'https://picsum.photos/id/1035/900/400',
+  'https://picsum.photos/id/1036/900/400'
+]
 </script>
 
 <template>
-  <Header class="fixed top-0 w-full">
-    <template #SignUp>
-      <Button
-        color="gray-dark"
-        class="w-20"
-        @click="isSignUpModalVisible = true"
-      >
-        Sign Up
-      </Button>
-    </template>
-    <template #LogIn>
-      <Button
-        color="gray-dark"
-        class="w-16"
-        @click="isLogInModalVisible = true"
-      >
-        Log In
-      </Button>
-    </template>
-  </Header>
+  <Header class="fixed top-0 z-40 w-full"></Header>
+  <body class="h-screen">
+    <Carousel :slides="slides" class="z-10 mt-[56px]"></Carousel>
+    <div
+      class="inline-flex-col relative z-10 mx-auto w-full lg:flex lg:justify-around"
+    >
+      <Card href="#" :items="noticeItems" class="mx-auto max-w-xl">
+        <template #title>
+          <IconInfo />
+          <h2 class="ml-2">Notice</h2>
+        </template>
 
-  <Modal
-    v-if="isSignUpModalVisible"
-    title="Welcome to SKKU Coding Platform"
-    title-color="green"
-    @close="close"
-  >
-    <template #modal-content>
-      <ul class="m-9 p-2">
-        <InputItem shadow placeholder="User ID" />
-        <InputItem shadow placeholder="Student ID" />
-        <InputItem shadow placeholder="Real Name" />
-        <InputItem shadow placeholder="PassWord" />
-        <InputItem shadow placeholder="PassWord Again" />
-        <div class="flex">
-          <InputItem
-            :notice="false"
-            size="small"
-            shadow
-            placeholder="Email Address"
-          />
-          <Button color="green">
-            <MajesticonsSend />
-          </Button>
-        </div>
+        <template #icon>
+          <IconAngleRight />
+        </template>
+      </Card>
+      <Card href="#" :items="contestItems" class="mx-auto max-w-xl">
+        <template #title>
+          <IconMedal />
+          <h2 class="ml-2">Current/Upcoming Contests</h2>
+        </template>
 
-        <div class="flex">
-          <InputItem shadow placeholder="Vertification code" />
-          <Button color="green">
-            <Fa6SolidCheck />
-          </Button>
-        </div>
-
-        <InputItem shadow placeholder="Captcha" size="small" />
-      </ul>
-      <Button color="green" class="mt-8">register</Button>
-      <p>Already have account?</p>
-      <!-- TODO: 아래 border -->
-      <button
-        class="border-b-1 border-black"
-        @click="isLogInModalVisible = true"
-      >
-        Sign in
-      </button>
-    </template>
-  </Modal>
-  <Modal
-    v-if="isLogInModalVisible"
-    title="Skku coding platform"
-    title-color="green"
-    @close="close"
-  >
-    <template #modal-image>
-      <img src="@/common/assets/logo.svg" class="w-20" alt="" />
-    </template>
-    <template #modal-content>
-      <ul class="m-9 p-2">
-        <InputItem shadow placeholder="User ID" />
-        <InputItem shadow placeholder="Password" />
-      </ul>
-      <!-- TODO: sign in 눌렀을 경우 아이콘으로 바뀌기-->
-      <Button color="green" class="mt-8" @click="$emit('isauth', true)">
-        Sign In
-      </Button>
-      <div class="bottom-0 mt-8 flex justify-around">
-        <button
-          class="border-b-1 border-black"
-          @click="isSignUpModalVisible = true"
-        >
-          Register now
-        </button>
-        <button
-          class="border-b-1 border-black"
-          @click="isPasswordFindModalVisible = true"
-        >
-          Forgot Password?
-        </button>
-      </div>
-    </template>
-  </Modal>
-  <Modal
-    v-if="isPasswordFindModalVisible"
-    title="Password Recovery"
-    title-color="green"
-    @close="close"
-  >
-    <template #modal-content>
-      <ul class="m-9 p-2">
-        <div class="flex">
-          <InputItem
-            :notice="false"
-            size="small"
-            shadow
-            placeholder="Email Address"
-          />
-          <Button color="green">
-            <MajesticonsSend />
-          </Button>
-        </div>
-
-        <div class="flex">
-          <InputItem shadow placeholder="Vertification code" />
-          <Button color="green">
-            <Fa6SolidCheck />
-          </Button>
-        </div>
-        <InputItem shadow placeholder="Captcha" />
-      </ul>
-
-      <Button color="green" class="mt-8">Send Password Reset Email</Button>
-      <div>
-        <button
-          class="border-b-1 border-black"
-          @click="isLogInModalVisible = true"
-        >
-          Back to Sign in
-        </button>
-      </div>
-    </template>
-  </Modal>
-  <!-- FIXME: relative 속성일 때 가운데 정렬 mx:auto? -->
-  <div
-    class="inline-flex-col relative mx-auto mt-[56px] w-full lg:flex lg:justify-around"
-  >
-    <Card href="#" :items="noticeItems" class="max-w-xl">
-      <template #title>
-        <IconInfo />
-        <h2 class="ml-2">Notice</h2>
-      </template>
-
-      <template #icon>
-        <IconAngleRight />
-      </template>
-    </Card>
-    <Card href="#" :items="contestItems" class="max-w-xl">
-      <template #title>
-        <IconMedal />
-        <h2 class="ml-2">Current/Upcoming Contests</h2>
-      </template>
-
-      <template #icon="item">
-        <IconEllipsis v-if="item.item === 'ongoing'" />
-        <IconCalendar v-else />
-      </template>
-    </Card>
-  </div>
-  <Footer class="fixed bottom-0" />
+        <template #icon="item">
+          <IconEllipsis v-if="item.item === 'ongoing'" />
+          <IconCalendar v-else />
+        </template>
+      </Card>
+    </div>
+  </body>
+  <Footer class="fixed bottom-0 z-30" />
 </template>
