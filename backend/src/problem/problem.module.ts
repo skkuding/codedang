bfe163f255a7f79b2ae9c3623655e7b466661cf3
coc0a25 <cranemont@gmail.com>
2022-08-14 14:26:22 +0000

    fix(be): import usermodule in problem module

--- backend/src/problem/problem.module.ts
+++ backend/src/problem/problem.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ContestModule } from 'src/contest/contest.module'
 import { GroupModule } from 'src/group/group.module'
+import { UserModule } from 'src/user/user.module'
 import { WorkbookModule } from 'src/workbook/workbook.module'
 import {
   PublicProblemController,
@@ -13,7 +14,7 @@ import { ProblemRepository } from './problem.repository'
 import { ProblemService } from './problem.service'
 
 @Module({
-  imports: [GroupModule, ContestModule, WorkbookModule],
+  imports: [GroupModule, UserModule, ContestModule, WorkbookModule],
   controllers: [
     PublicProblemController,
     PublicContestProblemController,
