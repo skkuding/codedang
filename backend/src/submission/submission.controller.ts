3b1e1d5b5491dcc99b182542497791d76e8a04ce
minseo999 <alstj778@gmail.com>
2022-08-15 15:36:42 +0000

    feat(be): implement create public problem submission

--- backend/src/submission/submission.controller.ts
+++ backend/src/submission/submission.controller.ts
@@ -1,7 +1,31 @@
-import { Controller } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  InternalServerErrorException,
+  Post,
+  Req
+} from '@nestjs/common'
+import { AuthenticatedRequest } from 'src/auth/interface/authenticated-request.interface'
+import { CreatePublicProblemSubmissionDto } from './dto/createPublicProblemSubmission.dto'
 import { SubmissionService } from './submission.service'
 
-@Controller('submission')
+@Controller()
 export class SubmissionController {
   constructor(private readonly submissionService: SubmissionService) {}
+
+  @Post('problem/:problem_id/submission')
+  async createPublicProblemSubmission(
+    @Req() req: AuthenticatedRequest,
+    @Body() createPublicProblemSubmissionDto: CreatePublicProblemSubmissionDto
+  ) {
+    try {
+      await this.submissionService.createPublicProblemSubmission(
+        req.user.id,
+        req.ip.slice(7),
+        createPublicProblemSubmissionDto
+      )
+    } catch (error) {
+      throw new InternalServerErrorException()
+    }
+  }
 }
--- backend/src/submission/submission.service.ts
+++ backend/src/submission/submission.service.ts
@@ -1,7 +1,43 @@
 import { Injectable } from '@nestjs/common'
+import { UnprocessableDataException } from 'src/common/exception/business.exception'
 import { PrismaService } from 'src/prisma/prisma.service'
+import { CreatePublicProblemSubmissionDto } from './dto/createPublicProblemSubmission.dto'
 
 @Injectable()
 export class SubmissionService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async createPublicProblemSubmission(
+    userId: number,
+    ip: string,
+    createPublicProblemSubmissionDto: CreatePublicProblemSubmissionDto
+  ) {
+    //TODO: use problem repository(is_public=true, group_id=1)
+    const { languages } = await this.prisma.problem.findUnique({
+      where: { id: createPublicProblemSubmissionDto.problemId },
+      select: { languages: true }
+    })
+
+    if (!languages.includes(createPublicProblemSubmissionDto.language)) {
+      throw new UnprocessableDataException(
+        `${createPublicProblemSubmissionDto.language} is not allowed`
+      )
+    }
+
+    const submission = this.prisma.submission.create({
+      data: {
+        user: {
+          connect: { id: userId }
+        },
+        problem: {
+          connect: { id: createPublicProblemSubmissionDto.problemId }
+        },
+        code: createPublicProblemSubmissionDto.code,
+        language: createPublicProblemSubmissionDto.language,
+        ip_addr: ip
+      }
+    })
+
+    return submission
+  }
 }
