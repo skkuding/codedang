6788ea4917a74a81108bbf3e6cb2d29c38d85077
hbsongk <68498323+hbsongk@users.noreply.github.com>
2022-08-16 09:10:52 +0000

    chore(fe): simplify problemlistwithouttag

--- frontend/src/problem/pages/index.vue
+++ frontend/src/problem/pages/index.vue
@@ -10,25 +10,18 @@ import Switch from '@/common/components/Molecule/Switch.vue'
 import Button from '@/common/components/Atom/Button.vue'
 import { computed } from 'vue'
 import { ref } from 'vue'
+import { onMounted } from 'vue'
 
 defineProps<{
   id: number
 }>()
-const fieldsWithTags = [
+const fields = [
   { key: 'id', label: '#' },
   { key: 'title' },
   { key: 'level' },
   { key: 'submissions' },
   { key: 'rate', label: 'AC Rate' },
   { key: 'tag' }
-]
-
-const fields = [
-  { key: 'id', label: '#' },
-  { key: 'title' },
-  { key: 'level' },
-  { key: 'submissions' },
-  { key: 'rate', label: 'AC Rate' }
 ]
 
 const colorMapper = {
@@ -40,6 +33,108 @@ const colorMapper = {
   6: 'bg-level-6',
   7: 'bg-level-7'
 }
+
+const problemListWithoutTag: any = []
+
+const problemList = [
+  {
+    id: 1,
+    title: '가파른 경사',
+    level: 1,
+    submissions: 132,
+    rate: '92.14%',
+    tag: 'A'
+  },
+  {
+    id: 1006,
+    title: '습격자 호루라기',
+    level: 2,
+    submissions: 561,
+    rate: '70%',
+    tag: 'B'
+  },
+  {
+    id: 10,
+    title: '아싸 홍삼',
+    level: 1,
+    submissions: 100,
+    rate: '90%',
+    tag: 'E'
+  },
+  {
+    id: 11,
+    title: '에브리바디 홍상',
+    level: 2,
+    submissions: 100,
+    rate: '83%',
+    tag: 'C'
+  },
+  {
+    id: 12,
+    title: '나는 토깽이',
+    level: 3,
+    submissions: 100,
+    rate: '72%',
+    tag: 'D'
+  },
+  {
+    id: 13,
+    title: '나는 거부깅',
+    level: 4,
+    submissions: 100,
+    rate: '65%',
+    tag: 'F'
+  },
+  {
+    id: 14,
+    title: '토깽이 둘',
+    level: 5,
+    submissions: 100,
+    rate: '52%',
+    tag: 'G'
+  },
+  {
+    id: 15,
+    title: '토깽이 토깽이',
+    level: 6,
+    submissions: 100,
+    rate: '1%',
+    tag: 'H'
+  },
+  {
+    id: 16,
+    title: '아싸 토깽 에브리바디 토깽',
+    level: 7,
+    submissions: 100,
+    rate: '1%',
+    tag: 'I'
+  },
+  {
+    id: 17,
+    title: '토깽이 토깽이',
+    level: 7,
+    submissions: 100,
+    rate: '1%',
+    tag: 'J'
+  }
+]
+
+onMounted(() => {
+  for (let i = 0; i < problemList.length; i++) {
+    const { tag, ...other } = problemList[i]
+    problemListWithoutTag.push(other)
+  }
+})
+
+const clickRow = (row: any) => {
+  window.location.href = '/problem/' + row.id
+}
+
+const connectSwitchData = (data: any) => {
+  isSwitchOn.value = data.value
+}
+
+const isSwitchOn = ref(true)
 
 const cardItems = [
   {
@@ -129,173 +224,6 @@ const clickMore = () => {
     }
   }
 }
-
-const problemListWithTags = [
-  {
-    id: 1,
-    title: '가파른 경사',
-    level: 1,
-    submissions: 132,
-    rate: '92.14%',
-    tag: 'A'
-  },
-  {
-    id: 1006,
-    title: '습격자 호루라기',
-    level: 2,
-    submissions: 561,
-    rate: '70%',
-    tag: 'B'
-  },
-  {
-    id: 10,
-    title: '아싸 홍삼',
-    level: 1,
-    submissions: 100,
-    rate: '90%',
-    tag: 'E'
-  },
-  {
-    id: 11,
-    title: '에브리바디 홍상',
-    level: 2,
-    submissions: 100,
-    rate: '83%',
-    tag: 'C'
-  },
-  {
-    id: 12,
-    title: '나는 토깽이',
-    level: 3,
-    submissions: 100,
-    rate: '72%',
-    tag: 'D'
-  },
-  {
-    id: 13,
-    title: '나는 거부깅',
-    level: 4,
-    submissions: 100,
-    rate: '65%',
-    tag: 'F'
-  },
-  {
-    id: 14,
-    title: '토깽이 둘',
-    level: 5,
-    submissions: 100,
-    rate: '52%',
-    tag: 'G'
-  },
-  {
-    id: 15,
-    title: '토깽이 토깽이',
-    level: 6,
-    submissions: 100,
-    rate: '1%',
-    tag: 'H'
-  },
-  {
-    id: 16,
-    title: '아싸 토깽 에브리바디 토깽',
-    level: 7,
-    submissions: 100,
-    rate: '1%',
-    tag: 'I'
-  },
-  {
-    id: 17,
-    title: '토깽이 토깽이',
-    level: 7,
-    submissions: 100,
-    rate: '1%',
-    tag: 'J'
-  }
-]
-
-const problemList = [
-  {
-    id: 1,
-    title: '가파른 경사',
-    level: 1,
-    submissions: 132,
-    rate: '92.14%'
-  },
-  {
-    id: 1006,
-    title: '습격자 호루라기',
-    level: 2,
-    submissions: 561,
-    rate: '70%'
-  },
-  {
-    id: 10,
-    title: '아싸 홍삼',
-    level: 1,
-    submissions: 100,
-    rate: '90%'
-  },
-  {
-    id: 11,
-    title: '에브리바디 홍상',
-    level: 2,
-    submissions: 100,
-    rate: '83%'
-  },
-  {
-    id: 12,
-    title: '나는 토깽이',
-    level: 3,
-    submissions: 100,
-    rate: '72%'
-  },
-  {
-    id: 13,
-    title: '나는 거부깅',
-    level: 4,
-    submissions: 100,
-    rate: '65%'
-  },
-  {
-    id: 14,
-    title: '토깽이 둘',
-    level: 5,
-    submissions: 100,
-    rate: '52%'
-  },
-  {
-    id: 15,
-    title: '토깽이 토깽이',
-    level: 6,
-    submissions: 100,
-    rate: '1%'
-  },
-  {
-    id: 16,
-    title: '아싸 토깽 에브리바디 토깽',
-    level: 7,
-    submissions: 100,
-    rate: '1%'
-  },
-  {
-    id: 17,
-    title: '토깽이 토깽이',
-    level: 7,
-    submissions: 100,
-    rate: '1%'
-  }
-]
-
-const clickRow = (row: any) => {
-  window.location.href = '/problem/' + row.id
-}
-
-const connectSwitchData = (data: any) => {
-  isSwitchOn.value = data.value
-  console.log(isSwitchOn.value)
-}
-
-const isSwitchOn = ref(true)
 </script>
 
 <template>
@@ -310,8 +238,8 @@ const isSwitchOn = ref(true)
     <PageSubtitle text="All Problem" class="mt-10 mb-7" />
     <PaginationTable
       v-if="isSwitchOn"
-      :fields="fieldsWithTags"
-      :items="problemListWithTags"
+      :fields="fields"
+      :items="problemList"
       placeholder="keywords"
       :number-of-pages="1"
       @row-clicked="clickRow"
@@ -332,8 +260,8 @@ const isSwitchOn = ref(true)
     </PaginationTable>
     <PaginationTable
       v-else
-      :fields="fields"
-      :items="problemList"
+      :fields="fields.slice(0, -1)"
+      :items="problemListWithoutTag"
       placeholder="keywords"
       :number-of-pages="1"
       @row-clicked="clickRow"
@@ -372,7 +300,6 @@ const isSwitchOn = ref(true)
         class="!w-[95%]"
       />
     </div>
-
     <Button class="text-gray-dark mt-8 w-full" color="white" @click="clickMore">
       More
     </Button>
