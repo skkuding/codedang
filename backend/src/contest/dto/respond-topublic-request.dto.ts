910f9625658ed4bcdee35b77b1325b0fadf2ca74
minseo999 <alstj778@gmail.com>
2022-08-19 10:45:22 +0000

    chore(be): change enum value request status

--- backend/src/contest/dto/respond-topublic-request.dto.ts
+++ backend/src/contest/dto/respond-topublic-request.dto.ts
@@ -2,7 +2,7 @@ import { RequestStatus } from '@prisma/client'
 import { IsEnum, IsNotEmpty } from 'class-validator'
 
 export class RespondContestToPublicRequestDto {
-  @IsEnum({ Accept: RequestStatus.Accept, Reject: RequestStatus.Reject })
+  @IsEnum({ Accept: RequestStatus.Accepted, Reject: RequestStatus.Rejected })
   @IsNotEmpty()
   readonly request_status: RequestStatus
 }
