ec6e2e1e552d3230228448df6e9695d4c62a3373
hbsongk <68498323+hbsongk@users.noreply.github.com>
2022-08-22 17:16:02 +0000

    feat(fe): add contest-list

--- frontend/src/contest/pages/index.vue
+++ frontend/src/contest/pages/index.vue
@@ -1,8 +1,113 @@
 <script setup lang="ts">
-import MyComponent from '../components/MyComponent.vue'
+import BoxTitle from '../../common/components/Atom/BoxTitle.vue'
+import PageSubtitle from '../../common/components/Atom/PageSubtitle.vue'
+import CardItem from '../../common/components/Molecule/CardItem.vue'
+import Footer from '../../common/components/Organism/Footer.vue'
+import Trophy from '~icons/fxemoji/trophy'
+
+// ongoing contest, register now, upcoming contests, finished contest 별로 array
+// 선언하고 splice 형태로 각 특성 분류
+
+// Icon이랑 색감 BoxTitle 색감 어떻게 줄까
+const contestList = [
+  {
+    img: '@/../skku.svg',
+    title: '2021 Spring SKKU 프로그래밍 대회 - 참여중 대회',
+    additionalText: 'Member: 23',
+    description: 'SKKU 코딩 플랫폼 개발 동아리입니다.',
+    coloredText: 'Created by 하설빙',
+    coloredTextShort: 'By 하설빙'
+  },
+  {
+    img: '@/../skku.svg',
+    title: '2021 Spring SKKU 프로그래밍 대회 - 참여중 대회',
+    additionalText: 'Member: 23',
+    description: 'SKKU 코딩 플랫폼 개발 동아리입니다.',
+    coloredText: 'Created by 하설빙',
+    coloredTextShort: 'By 하설빙'
+  },
+  {
+    img: '@/../skku.svg',
+    title: '2021 Spring SKKU 프로그래밍 대회 - 참여중 대회',
+    additionalText: 'Member: 23',
+    description: 'SKKU 코딩 플랫폼 개발 동아리입니다.',
+    coloredText: 'Created by 하설빙',
+    coloredTextShort: 'By 하설빙'
+  },
+  {
+    img: '@/../skku.svg',
+    title: '2021 Spring SKKU 프로그래밍 대회 - 참여중 대회',
+    additionalText: 'Member: 23',
+    description: 'SKKU 코딩 플랫폼 개발 동아리입니다.',
+    coloredText: 'Created by 하설빙',
+    coloredTextShort: 'By 하설빙'
+  },
+  {
+    img: '@/../skku.svg',
+    title: '2021 Spring SKKU 프로그래밍 대회 - 참여중 대회',
+    additionalText: 'Member: 23',
+    description: 'SKKU 코딩 플랫폼 개발 동아리입니다.',
+    coloredText: 'Created by 하설빙',
+    coloredTextShort: 'By 하설빙'
+  }
+]
 </script>
 
 <template>
-  <div>This is contest list page</div>
-  <MyComponent msg="contest list!" />
+  <Header />
+  <BoxTitle>
+    <template #title>
+      <Trophy class="inline-block" />
+      SKKU Coding Platform
+      <span class="!text-blue">Contests</span>
+    </template>
+    <template #subtitle>Compete with schoolmates & win the prizes!</template>
+  </BoxTitle>
+  <div class="m-24">
+    <PageSubtitle text="Ongoing Contest > >" />
+    <CardItem
+      v-for="(items, index) in contestList"
+      :key="index"
+      :img="items.img"
+      :title="items.title"
+      :description="items.description"
+      :additional-text="items.additionalText"
+      :colored-text="items.coloredText"
+      :colored-text-short="items.coloredTextShort"
+    />
+    <PageSubtitle text="Register Now > >" />
+    <CardItem
+      v-for="(items, index) in contestList"
+      :key="index"
+      :img="items.img"
+      :title="items.title"
+      :description="items.description"
+      :additional-text="items.additionalText"
+      :colored-text="items.coloredText"
+      :colored-text-short="items.coloredTextShort"
+    />
+    <PageSubtitle text="Upcoming Contests > >" />
+    <CardItem
+      v-for="(items, index) in contestList"
+      :key="index"
+      :img="items.img"
+      :title="items.title"
+      :description="items.description"
+      :additional-text="items.additionalText"
+      :colored-text="items.coloredText"
+      :colored-text-short="items.coloredTextShort"
+    />
+    <PageSubtitle text="Finished Contests" />
+    <CardItem
+      v-for="(items, index) in contestList"
+      :key="index"
+      :img="items.img"
+      :title="items.title"
+      :description="items.description"
+      :additional-text="items.additionalText"
+      :colored-text="items.coloredText"
+      :colored-text-short="items.coloredTextShort"
+    />
+  </div>
+  <Footer />
 </template>
