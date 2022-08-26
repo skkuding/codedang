050e946b264de52156d07919f00d8e9e5523a789
kee711 <kee711@naver.com>
2022-08-26 11:57:29 +0000

    feat(fe): add group namebox component

---
+++ frontend/src/group/components/Namebox.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import PageSubtitle from '@/common/components/Atom/PageSubtitle.vue'
+
+interface User {
+  id: number
+  username: string
+  studentId: string
+  role: string
+}
+
+const props = defineProps<{
+  title: string
+  creator: number
+  userList: User[]
+}>()
+
+const isCreator = (id: number) => {
+  if (id === props.creator) return 'text-green'
+}
+</script>
+
+<template>
+  <PageSubtitle :text="title" />
+  <div
+    class="border-gray grid grid-cols-3 gap-y-3.5 rounded-md border p-6 lg:grid-cols-5"
+  >
+    <div v-for="user in userList" :key="user.id" :class="isCreator(user.id)">
+      {{ user.username + '(' + user.studentId + ')' }}
+    </div>
+  </div>
+</template>
