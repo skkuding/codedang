3fac0fddd659192fa1fbd4f98e658ceb0c719939
SOLBI1028 <hagkthfql@naver.com>
2022-08-26 12:53:09 +0000

    feat(fe): add radio button component

---
+++ frontend/src/common/components/Molecule/RadioButton.story.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import RadioButton from './RadioButton.vue'
+import { ref } from 'vue'
+
+const texts = ['Latest', 'Ongoing']
+const selected = ref()
+const setSelected = (index: number) => {
+  selected.value = index
+}
+</script>
+
+<template>
+  <Story :layout="{ type: 'grid' }">
+    <Variant title="default">
+      <RadioButton :texts="texts" @selected="setSelected" />
+    </Variant>
+  </Story>
+</template>
---
+++ frontend/src/common/components/Molecule/RadioButton.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import Button from '../Atom/Button.vue'
+import { ref, watch } from 'vue'
+
+defineProps<{
+  texts: string[]
+}>()
+
+const selected = ref(0)
+const emit = defineEmits(['selected'])
+watch(selected, (value) => {
+  emit('selected', value)
+})
+
+const setSelected = (index: number) => {
+  selected.value = index
+}
+
+const setColor = (index: number) => {
+  if (selected.value === index) return 'green'
+  return 'gray'
+}
+</script>
+
+<template>
+  <div class="flex gap-x-2">
+    <Button
+      v-for="(text, index) in texts"
+      :key="text"
+      rounded
+      :color="setColor(index)"
+      @click="setSelected(index)"
+    >
+      {{ text }}
+    </Button>
+  </div>
+</template>
