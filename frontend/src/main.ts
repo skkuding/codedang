52401bf90950026b250b8b7fec0ff5e733c304c8
Jaemin Choi <1dotolee@gmail.com>
2022-08-30 13:40:57 +0000

    feat: add page progress bar

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import { createPinia } from 'pinia'
+import NProgress from 'nprogress'
 import routes from 'virtual:generated-pages'
 import App from './App.vue'
 
@@ -11,6 +12,14 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
+
+router.beforeEach(() => {
+  NProgress.start()
+})
+router.afterEach(() => {
+  NProgress.done()
+})
+
 app.use(router)
 app.use(createPinia())
 app.mount('#app')
