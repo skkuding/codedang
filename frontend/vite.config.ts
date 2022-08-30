73e2f43366628ac0592450362a5a150caec1c924
Jaemin Choi <1dotolee@gmail.com>
2022-08-30 08:16:00 +0000

    chore: add proxy url for api call

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -24,5 +24,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
