2d800f3431d6c6d4b7329657057a8034eb9ef915
kee711 <kee711@naver.com>
2022-09-02 10:51:37 +0000

    feat(fe): create group member 페이지

--- frontend/src/group/pages/[id]/member.vue
+++ frontend/src/group/pages/[id]/member.vue
@@ -1,11 +1,68 @@
 <script setup lang="ts">
-import MyComponent from '../../components/MyComponent.vue'
+import Namebox from '../../components/Namebox.vue'
+import Button from '@/common/components/Atom/Button.vue'
+import Modal from '@/common/components/Molecule/Modal.vue'
+import { ref } from 'vue'
+
 defineProps<{
-  groupId: number
+  id: number
+  createdBy: number
 }>()
+
+const isModalVisible = ref(false)
+function close() {
+  isModalVisible.value = false
+}
+// TODO : Yes 눌렀을때 API 호출하도록 변경 필요
+
+const groupAdmin = [
+  { id: 1, username: '하솔비', studentId: '201831xxxx', role: 'group_admin' },
+  { id: 3, username: '하솔비', studentId: '201833xxxx', role: 'group_admin' },
+  { id: 7, username: '하솔비', studentId: '201837xxxx', role: 'group_admin' },
+  { id: 8, username: '하솔비', studentId: '201838xxxx', role: 'group_admin' },
+  { id: 9, username: '하솔비', studentId: '201839xxxx', role: 'group_admin' },
+  { id: 11, username: '하솔비', studentId: '201831xxxx', role: 'group_admin' }
+]
+
+const groupMember = [
+  { id: 2, username: '하솔비', studentId: '201832xxxx', role: 'user' },
+  { id: 4, username: '하솔비', studentId: '201834xxxx', role: 'user' },
+  { id: 5, username: '하솔비', studentId: '201835xxxx', role: 'user' },
+  { id: 6, username: '하솔비', studentId: '201836xxxx', role: 'user' },
+  { id: 10, username: '하솔비', studentId: '201832xxxx', role: 'user' }
+]
 </script>
 
 <template>
-  <div>This is group member page : id = {{ groupId }}</div>
-  <MyComponent msg="group member!" />
+  <div class="relative mx-auto mb-32">
+    <Namebox
+      class="mb-12"
+      title="Manager"
+      :user-list="groupAdmin"
+      :creator="createdBy"
+    />
+    <Namebox
+      class="mb-28"
+      title="Member"
+      :user-list="groupMember"
+      :creator="createdBy"
+    />
+    <Button class="absolute right-0 -bottom-20" @click="isModalVisible = true">
+      Leave Group
+    </Button>
+    <Modal
+      v-if="isModalVisible"
+      title="Skku coding platform"
+      title-color="green"
+      class="h-48 w-80"
+      @close="close"
+    >
+      <template #modal-title>Leave Group</template>
+      <template #modal-content>
+        Do you really want to leave group?
+        <Button class="m-4 mt-6 w-20" @click="close">Yes</Button>
+        <Button class="m-4 mt-6 w-20" @click="close">No</Button>
+      </template>
+    </Modal>
+  </div>
 </template>
