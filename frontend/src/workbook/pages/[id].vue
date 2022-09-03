0391e36fbee87da68e640d919f947aea0a90b3ba
goo314 <rn0731@g.skku.edu>
2022-09-03 09:32:37 +0000

    chore: show title, display id, tags only

--- frontend/src/workbook/pages/[id].vue
+++ frontend/src/workbook/pages/[id].vue
@@ -1,17 +1,15 @@
 <script setup lang="ts">
 import WorkbookTitle from '../components/WorkbookTitle.vue'
 import PaginationTable from '@/common/components/Organism/PaginationTable.vue'
+import Switch from '@/common/components/Molecule/Switch.vue'
 import IconCheck from '~icons/fa6-regular/circle-check'
 import { onBeforeMount, ref } from 'vue'
-import { useRouter } from 'vue-router'
 
 // TODO: define interface in separte file
 interface Problem {
   id: number
   title: string
-  level: number
-  submissions: number
-  rate: string
+  tags: string
   result: string
 }
 
@@ -19,151 +17,104 @@ defineProps<{
   id: number
 }>()
 
-const fields = [
-  { key: 'id', label: '#' },
-  { key: 'title' },
-  { key: 'level' },
-  { key: 'submissions' },
-  { key: 'rate', label: 'AC Rate' }
-]
-
-const colorMapper = (level: number) => {
-  switch (level) {
-    case 1:
-      return 'bg-level-1'
-    case 2:
-      return 'bg-level-2'
-    case 3:
-      return 'bg-level-3'
-    case 4:
-      return 'bg-level-4'
-    case 5:
-      return 'bg-level-5'
-    case 6:
-      return 'bg-level-6'
-    case 7:
-      return 'bg-level-7'
-    default:
-      return 'bg-gray'
-  }
-}
+const fields = ref([{ key: 'id', label: '#' }, { key: 'title' }])
 
 const workbookProblemList = ref<Problem[]>([])
 
 // TODO: use API to get workbook-problem list of initial page
-onBeforeMount(() => {
-  workbookProblemList.value = [
-    {
-      id: 1,
-      title: '가파른 경사',
-      level: 1,
-      submissions: 132,
-      rate: '92.14%',
-      result: 'accepted'
-    },
-    {
-      id: 1006,
-      title: '습격자 호루라기',
-      level: 2,
-      submissions: 561,
-      rate: '0.61%',
-      result: 'accepted'
-    },
-    {
-      id: 10,
-      title: '아싸 홍삼',
-      level: 1,
-      submissions: 100,
-      rate: '90%',
-      result: 'wrong answer'
-    },
-    {
-      id: 11,
-      title: '에브리바디 홍상',
-      level: 2,
-      submissions: 100,
-      rate: '83%',
-      result: 'wrong answer'
-    },
-    {
-      id: 12,
-      title: '나는 토깽이',
-      level: 3,
-      submissions: 100,
-      rate: '72%',
-      result: ''
-    },
-    {
-      id: 13,
-      title: '나는 거부깅',
-      level: 4,
-      submissions: 100,
-      rate: '65%',
-      result: ''
-    },
-    {
-      id: 14,
-      title: '토깽이 둘',
-      level: 5,
-      submissions: 100,
-      rate: '52%',
-      result: ''
-    },
-    {
-      id: 15,
-      title: '토깽이 토깽이',
-      level: 6,
-      submissions: 100,
-      rate: '1%',
-      result: 'accepted'
-    },
-    {
-      id: 16,
-      title: '아싸 토깽 에브리바디 토깽',
-      level: 7,
-      submissions: 100,
-      rate: '1%',
-      result: ''
-    },
-    {
-      id: 17,
-      title: '토깽이 토깽이',
-      level: 7,
-      submissions: 100,
-      rate: '1%',
-      result: ''
-    }
-  ]
-})
+
+workbookProblemList.value = [
+  {
+    id: 1,
+    title: '가파른 경사',
+    tags: 'if-statement',
+    result: 'accepted'
+  },
+  {
+    id: 1006,
+    title: '습격자 호루라기',
+    tags: 'dfs',
+    result: 'accepted'
+  },
+  {
+    id: 10,
+    title: '아싸 홍삼',
+    tags: 'dynamic programming',
+    result: 'wrong answer'
+  },
+  {
+    id: 11,
+    title: '에브리바디 홍상',
+    tags: 'for-loop',
+    result: 'wrong answer'
+  },
+  {
+    id: 12,
+    title: '나는 토깽이',
+    tags: 'greedy',
+    result: ''
+  },
+  {
+    id: 13,
+    title: '나는 거부깅',
+    tags: 'brute force',
+    result: ''
+  },
+  {
+    id: 14,
+    title: '토깽이 둘',
+    tags: 'binary tree',
+    result: ''
+  },
+  {
+    id: 15,
+    title: '토깽이 토깽이',
+    tags: 'bfs',
+    result: 'accepted'
+  },
+  {
+    id: 16,
+    title: '아싸 토깽 에브리바디 토깽',
+    tags: 'dfs',
+    result: ''
+  },
+  {
+    id: 17,
+    title: '토깽이 토깽이',
+    tags: 'if-statement',
+    result: ''
+  }
+]
+
+const showTag = ref(false)
 
 // TODO: implement change-page function to reset workbookProblemList using API when click pagination buttons
-
-const router = useRouter()
-const clickRow = (row: Problem) => {
-  router.push('/problem/' + row.id)
-}
 </script>
 
 <template>
-  <div class="my-16">
-    <WorkbookTitle text="CATS 대비 문제집 Level 1" color="red" class="mb-10" />
+  <div class="my-10">
+    <WorkbookTitle text="CATS 대비 문제집 Level 1" color="red" class="mb-2" />
     <PaginationTable
       :fields="fields"
       :items="workbookProblemList"
       :number-of-pages="1"
       no-search-bar
-      @row-clicked="clickRow"
+      @row-clicked="({ id }) => $router.push('/problem/' + id)"
     >
+      <template #option>
+        <Switch
+          v-model="showTag"
+          label="Tags"
+          @update:model-value="
+            (value) => (value ? fields.push({ key: 'tags' }) : fields.pop())
+          "
+        />
+      </template>
+
       <template #title="{ row }">
         <div class="flex items-center gap-2">
           {{ row.title }}
           <IconCheck v-if="row.result === 'accepted'" class="text-green" />
-        </div>
-      </template>
-      <template #level="{ row }">
-        <div class="flex items-center gap-2">
-          <span class="h-5 w-5 rounded-full" :class="colorMapper(row.level)" />
-          Level {{ row.level }}
         </div>
       </template>
     </PaginationTable>
