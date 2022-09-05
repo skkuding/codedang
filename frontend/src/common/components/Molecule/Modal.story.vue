9fd88430bd376d9cb3fb3f88e99eca30f39b89bd
Jaemin Choi <1dotolee@gmail.com>
2022-09-05 14:19:35 +0000

    chore: adjust modal size to content

--- frontend/src/common/components/Molecule/Modal.story.vue
+++ frontend/src/common/components/Molecule/Modal.story.vue
@@ -11,7 +11,10 @@ const show = ref(false)
     <main class="h-screen w-screen">
       <div>Click the button, and it shows modal!</div>
       <Button @click="show = true">Show</Button>
-      <Modal v-model="show" class="flex items-center justify-center font-bold">
+      <Modal
+        v-model="show"
+        class="flex items-center justify-center p-12 text-xl font-bold"
+      >
         Modal opened!
       </Modal>
     </main>
