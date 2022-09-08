aebcc61714ee42cf3a451622041b48851c544d3e
Kohminchae <mc0jooo@gmail.com>
2022-09-08 09:32:28 +0000

    feat(fe): add notice list and detail

--- frontend/src/notice/pages/[id].vue
+++ frontend/src/notice/pages/[id].vue
@@ -1,11 +1,85 @@
 <script setup lang="ts">
-import MyComponent from '../components/MyComponent.vue'
+import Header from '@/common/components/Organism/Header.vue'
+import FaSolidBars from '~icons/fa-solid/bars'
+import Fa6SolidAngleUp from '~icons/fa6-solid/angle-up'
+import Fa6SolidAngleDown from '~icons/fa6-solid/angle-down'
+import { useRouter } from 'vue-router'
+
 defineProps<{
-  noticeId: number
+  id: string
 }>()
+const router = useRouter()
+const noticeItem = {
+  title: '1111',
+  date: '2022-04-01',
+  content: 'this is notice',
+  update: '2022-04-11',
+  id: router.currentRoute.value.params.id
+}
+const preNotice = {
+  title: '0000',
+  date: '2022-04-012',
+  content: 'this is preNotice',
+  update: '2022-04-12',
+  id: '1'
+}
+const nextNotice = {
+  title: '3333',
+  date: '2022-04-03',
+  content: 'this is nextNotice',
+  update: '2022-04-13',
+  id: '3'
+}
+const goDetail = (nextId: string) => {
+  return '/notice/' + nextId
+}
 </script>
 
 <template>
-  <div>This is notice detail page : id = {{ noticeId }}</div>
-  <MyComponent msg="notice detail!" />
+  <Header></Header>
+  <div :key="router.currentRoute.value.fullPath" class="mx-auto mt-10 w-[70%]">
+    <div class="flex items-center justify-end">
+      <router-link to="/notice">
+        <FaSolidBars />
+      </router-link>
+      <router-link to="/notice" class="ml-2 hidden sm:block">List</router-link>
+    </div>
+    <div
+      class="bg-gray-light border-gray mt-4 flex h-12 w-full items-center border-y-[1px]"
+    >
+      <div class="ml-4 mr-auto">
+        {{ noticeItem.id }}
+        {{ noticeItem.title }}
+      </div>
+      <div class="mr-4 hidden text-right sm:block">
+        {{ noticeItem.date }}
+      </div>
+    </div>
+    <div class="mt-2 hidden w-full justify-end pr-4 text-right md:block">
+      Last update: {{ noticeItem.update }}
+    </div>
+    <div class="mx-4 mt-2 h-96 w-full max-w-full break-all">
+      {{ noticeItem.content }}
+    </div>
+    <div class="w-full">
+      <router-link
+        v-if="preNotice"
+        class="border-gray flex h-12 items-center border-y-[1px]"
+        :to="goDetail(preNotice.id)"
+      >
+        <Fa6SolidAngleUp class="ml-2" />
+        <div class="ml-4 hidden w-20 sm:block">Previous</div>
+        <div class="ml-2">{{ preNotice.title }}</div>
+      </router-link>
+      <router-link
+        v-if="nextNotice"
+        class="border-gray flex h-12 items-center border-b-[1px]"
+        :to="goDetail(nextNotice.id)"
+      >
+        <Fa6SolidAngleDown class="ml-2" />
+        <div class="ml-4 hidden w-20 sm:block">Next</div>
+        <div class="ml-2">{{ nextNotice.title }}</div>
+      </router-link>
+    </div>
+  </div>
 </template>
--- frontend/src/notice/pages/index.vue
+++ frontend/src/notice/pages/index.vue
@@ -1,8 +1,123 @@
 <script setup lang="ts">
-import MyComponent from '../components/MyComponent.vue'
+import PaginationTable from '@/common/components/Organism/PaginationTable.vue'
+import PageTitle from '@/common/components/Atom/PageTitle.vue'
+import Header from '@/common/components/Organism/Header.vue'
+import { useRouter } from 'vue-router'
+import { ref, computed } from 'vue'
+
+interface field {
+  key: string
+  label?: string
+  custom?: boolean
+}
+interface item {
+  [key: string]: any
+}
+const router = useRouter()
+const goDetail = (item: any) => {
+  router.push({
+    name: 'notice-id',
+    params: { id: item.id }
+  })
+}
+const noticeField: field[] = [
+  {
+    key: 'title',
+    label: 'Title'
+  },
+  {
+    key: 'date',
+    label: 'Date'
+  }
+]
+const noticeItem: item[] = [
+  {
+    title: '111111',
+    date: '2022-05-06',
+    id: 1
+  },
+  {
+    title: '222222',
+    date: '2022-05-07',
+    id: 2
+  },
+  {
+    title: '222222',
+    date: '2022-05-07',
+    id: 3
+  },
+  {
+    title: '222222',
+    date: '2022-05-07',
+    id: 4
+  },
+  {
+    title: '222222',
+    date: '2022-05-07',
+    id: 5
+  },
+  {
+    title: '222222',
+    date: '2022-05-07',
+    id: 6
+  },
+  {
+    title: '222222',
+    date: '2022-05-07',
+    id: 7
+  },
+  {
+    title: '222222',
+    date: '2022-05-07',
+    id: 8
+  },
+  {
+    title: '222222',
+    date: '2022-05-07',
+    id: 9
+  }
+]
+
+const perPage = 5
+const shownPages = ref(
+  noticeItem.length % perPage === 0
+    ? noticeItem.length / perPage
+    : Math.floor(noticeItem.length / perPage) + 1
+)
+const shownItems = ref(noticeItem)
+const currentPage = ref(1)
+
+let currentItems = computed(() =>
+  shownItems.value.slice(
+    (currentPage.value - 1) * perPage,
+    currentPage.value * perPage
+  )
+)
+
+const search = (keyword: string) => {
+  const total = noticeItem.filter((i) => i.title.includes(keyword))
+  shownItems.value = total
+  shownPages.value = shownItems.value.length / perPage
+  currentPage.value = 1
+}
+
+const changeItems = (page: number) => {
+  currentPage.value = page
+}
 </script>
 
 <template>
-  <div>This is notice list page</div>
-  <MyComponent msg="notice list!" />
+  <Header></Header>
+  <div class="mx-auto w-[70%]">
+    <PageTitle text="Notice" />
+    <PaginationTable
+      :fields="noticeField"
+      :items="currentItems"
+      text="No Notice"
+      :number-of-pages="shownPages"
+      @search="search"
+      @change-page="changeItems"
+      @row-clicked="goDetail"
+    ></PaginationTable>
+  </div>
 </template>
