f3cc938f4976dea0626d3af7511e723df47d041e
jimin9038 <jimin9038@g.skku.edu>
2022-09-08 06:26:29 +0000

    feat: use bootstrap icon for texteditor options

--- frontend/src/common/components/Organism/TextEditor.vue
+++ frontend/src/common/components/Organism/TextEditor.vue
@@ -8,14 +8,18 @@ import IconItalic from '~icons/fa6-solid/italic'
 import IconStrikethrough from '~icons/fa6-solid/strikethrough'
 import IconCode from '~icons/fa6-solid/code'
 import IconTextSlash from '~icons/fa6-solid/text-slash'
-// import IconH1 from '~icons/fa6-solid/H1'
-// import IconH2 from '~icons/fa6-solid/H2'
+import IconH1 from '~icons/bi/type-h2'
+import IconH2 from '~icons/bi/type-h1'
 import IconParagraph from '~icons/fa6-solid/paragraph'
 import IconListUl from '~icons/fa6-solid/list-ul'
 import IconList from '~icons/fa6-solid/list'
-import IconLaptopCode from '~icons/fa6-solid/laptop-code'
+import IconCodeSquare from '~icons/bi/code-square'
 import IconReturn from '~icons/ic/round-keyboard-return'
 import IconRoundBlockQuote from '~icons/ic/round-format-quote'
+import IconHr from '~icons/bi/hr'
+import IconArrowCounterClockwise from '~icons/bi/arrow-counterclockwise'
+import IconArrowClockwise from '~icons/bi/arrow-clockwise'
+
 const editor = useEditor({
   content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
   extensions: [StarterKit]
@@ -25,29 +29,25 @@ const editor = useEditor({
 <template>
   <ButtonGroup class="mr-2">
     <Button
-      class="EditorButton"
-      :class="{ 'is-active': editor?.isActive('bold') }"
+      :pressed="editor?.isActive('bold')"
       @click="editor?.chain().focus().toggleBold().run()"
     >
       <IconBold />
     </Button>
     <Button
-      class="EditorButton"
-      :class="{ 'is-active': editor?.isActive('italic') }"
+      :pressed="editor?.isActive('italic')"
       @click="editor?.chain().focus().toggleItalic().run()"
     >
       <IconItalic />
     </Button>
     <Button
-      :class="{ 'is-active': editor?.isActive('strike') }"
-      class="EditorButton"
+      :pressed="editor?.isActive('strike')"
       @click="editor?.chain().focus().toggleStrike().run()"
     >
       <IconStrikethrough />
     </Button>
     <Button
-      class="EditorButton"
-      :class="{ 'is-active': editor?.isActive('code') }"
+      :pressed="editor?.isActive('code')"
       @click="editor?.chain().focus().toggleCode().run()"
     >
       <IconCode />
@@ -58,46 +58,40 @@ const editor = useEditor({
   </ButtonGroup>
   <ButtonGroup class="mx-2">
     <Button
-      class="EditorButton"
-      :class="{ 'is-active': editor?.isActive('heading', { level: 3 }) }"
+      :pressed="editor?.isActive('heading', { level: 3 })"
       @click="editor?.chain().focus().toggleHeading({ level: 3 }).run()"
     >
       <IconH1 />
     </Button>
     <Button
-      class="EditorButton"
-      :class="{ 'is-active': editor?.isActive('heading', { level: 4 }) }"
+      :pressed="editor?.isActive('heading', { level: 4 })"
       @click="editor?.chain().focus().toggleHeading({ level: 4 }).run()"
     >
       <IconH2 />
     </Button>
     <Button
-      class="EditorButton"
-      :class="{ 'is-active': editor?.isActive('paragraph') }"
+      :pressed="editor?.isActive('paragraph')"
       @click="editor?.chain().focus().setParagraph().run()"
     >
       <IconParagraph />
     </Button>
     <Button
-      class="EditorButton"
-      :class="{ 'is-active': editor?.isActive('bulletList') }"
+      :pressed="editor?.isActive('bulletList')"
       @click="editor?.chain().focus().toggleBulletList().run()"
     >
       <IconListUl />
     </Button>
     <Button
-      class="EditorButton"
-      :class="{ 'is-active': editor?.isActive('orderedList') }"
+      :pressed="editor?.isActive('orderedList')"
       @click="editor?.chain().focus().toggleOrderedList().run()"
     >
       <IconList />
     </Button>
     <Button
-      class="EditorButton"
-      :class="{ 'is-active': editor?.isActive('codeBlock') }"
+      :pressed="editor?.isActive('codeBlock')"
       @click="editor?.chain().focus().toggleCodeBlock().run()"
     >
-      <IconLaptopCode />
+      <IconCodeSquare />
     </Button>
   </ButtonGroup>
   <div class="divider"></div>
@@ -106,35 +100,22 @@ const editor = useEditor({
       <IconReturn />
     </Button>
     <Button
-      class="EditorButton"
-      :class="{ 'is-active': editor?.isActive('blockquote') }"
+      :pressed="editor?.isActive('blockquote')"
       @click="editor?.chain().focus().toggleBlockquote().run()"
     >
       <IconRoundBlockQuote />
     </Button>
     <Button @click="editor?.chain().focus().setHorizontalRule().run()">
-      <icon icon="distribute-vertical"></icon>
+      <IconHr />
     </Button>
   </ButtonGroup>
   <div class="divider"></div>
   <ButtonGroup class="mx-2">
-    <Button
-      size="sm"
-      title="Undo"
-      type="Button"
-      variant="outline-secondary"
-      @click="editor?.chain().focus().undo().run()"
-    >
-      <icon icon="arrow-counterclockwise"></icon>
+    <Button @click="editor?.chain().focus().undo().run()">
+      <IconArrowCounterClockwise />
     </Button>
-    <Button
-      size="sm"
-      title="Redo"
-      type="Button"
-      variant="outline-secondary"
-      @click="editor?.chain().focus().redo().run()"
-    >
-      <icon icon="arrow-clockwise"></icon>
+    <Button @click="editor?.chain().focus().redo().run()">
+      <IconArrowClockwise />
     </Button>
   </ButtonGroup>
   <div class="divider"></div>
