skkuding/next

feat(be): add create-contest-record api

backend/src/contest/contest.controller.ts
@@ -1,7 +1,39 @@
-import { Controller } from '@nestjs/common'
+import {
+  Controller,
+  ForbiddenException,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+  UseGuards
+} from '@nestjs/common'
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard'
+import { AuthenticatedRequest } from 'src/auth/interface/authenticated-request.interface'
+import { EntityNotExistException } from 'src/common/exception/business.exception'
+import { GroupMemberGuard } from 'src/group/guard/group-member.guard'
 import { ContestService } from './contest.service'
 
+@Controller('group/:group_id/contest')
+@UseGuards(JwtAuthGuard, GroupMemberGuard)
 @Controller()
 export class ContestController {
   constructor(private readonly contestService: ContestService) {}
+
+  @Post(':id/participation')
+  async createContestRecord(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', ParseIntPipe) contestId: number,
+    @Param('group_id', ParseIntPipe) groupId: number
+  ): Promise<null | Error> {
+    try {
+      this.contestService.createContestRecord(req.user.id, contestId, groupId)
+      return
+    } catch (error) {
+      if (error instanceof EntityNotExistException) {
+        throw new NotFoundException(error.message)
+      }
+      throw new ForbiddenException(error.message)
+    }
+  }
 }

backend/src/contest/contest.service.ts
@@ -2,11 +2,18 @@ import { Injectable } from '@nestjs/common'
 import { Contest } from '@prisma/client'
 import {
   EntityNotExistException,
+  InvalidUserException,
   UnprocessableDataException
 } from 'src/common/exception/business.exception'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { CreateContestDto } from './dto/create-contest.dto'
 import { UpdateContestDto } from './dto/update-contest.dto'
+
+const PUBLIC = 1
+
+function returnTextIsNotAllowed(user_id: number, contest_id: number): string {
+  return `Contest ${contest_id} is not allowed to User ${user_id}`
+}
 
 @Injectable()
 export class ContestService {
@@ -99,5 +106,64 @@ export class ContestService {
         id: contestId
       }
     })
+  }
+
+  // Todo: issue #90
+  async createContestRecord(
+    user_id: number,
+    contest_id: number,
+    group_id: number
+  ): Promise<null | Error> {
+    //contest 존재 여부
+    const contest = await this.prisma.contest.findUnique({
+      where: { id: contest_id },
+      select: { start_time: true, end_time: true, type: true }
+    })
+    if (!contest) {
+      throw new EntityNotExistException(`Contest ${contest_id}`)
+    }
+
+    //중복 참여 확인 in contestRecord
+    const isAlreadyRecord = await this.prisma.contestRecord.findFirst({
+      where: { user_id, contest_id },
+      select: { id: true }
+    })
+    if (isAlreadyRecord) {
+      throw new InvalidUserException(
+        `User ${user_id} is already participated in Contest ${contest_id}`
+      )
+    }
+
+    //contest private여부 확인
+    if (group_id !== PUBLIC) {
+      //user group인지 확인
+      const isUserInGroup = await this.prisma.userGroup.findFirst({
+        where: { user_id, group_id: group_id, is_registered: true },
+        select: { id: true }
+      })
+      //contest group 확인
+      if (!isUserInGroup) {
+        throw new InvalidUserException(
+          returnTextIsNotAllowed(user_id, contest_id)
+        )
+      }
+    }
+
+    //contest start 전 or contest end 후 -> throw
+    const now = new Date()
+    if (now < contest.start_time || now >= contest.end_time) {
+      throw new InvalidUserException(
+        returnTextIsNotAllowed(user_id, contest_id)
+      )
+    }
+    //contest type ACM -> create contest rank acm record
+    if (contest.type === 'ACM') {
+      await this.prisma.contestRankACM.create({
+        data: { contest_id, user_id }
+      })
+    }
+    // Todo: other contest type -> create other contest record table
+
+    return
   }
 }

skkuding/codedang

backend/src/contest/contest.controller.ts
@@ -1,24 +1,25 @@
 import {
   Controller,
   ForbiddenException,
-  Get,
-  InternalServerErrorException,
   NotFoundException,
   Param,
   ParseIntPipe,
+  Post,
   Req,
-  UseGuards
+  UseGuards,
+  Get,
+  InternalServerErrorException
 } from '@nestjs/common'
-import { Contest } from '@prisma/client'
 import { AuthenticatedRequest } from 'src/auth/interface/authenticated-request.interface'
+import { GroupMemberGuard } from 'src/group/guard/group-member.guard'
+import { ContestService } from './contest.service'
+import { Contest } from '@prisma/client'
 import { Public } from 'src/common/decorator/public.decorator'
 import {
   EntityNotExistException,
   ForbiddenAccessException
 } from 'src/common/exception/business.exception'
-import { GroupMemberGuard } from 'src/group/guard/group-member.guard'
 import { RolesGuard } from 'src/user/guard/roles.guard'
-import { ContestService } from './contest.service'
 
 @Controller('contest')
 @Public()
@@ -76,6 +77,24 @@ export class GroupContestController {
         throw new ForbiddenException(error.message)
       }
       throw new InternalServerErrorException()
+    }
+  }
+
+  @Post(':id/participation')
+  @UseGuards(RolesGuard, GroupMemberGuard)
+  async createContestRecord(
+    @Req() req: AuthenticatedRequest,
+    @Param('id', ParseIntPipe) contestId: number,
+    @Param('groupId', ParseIntPipe) groupId: number
+  ): Promise<null | Error> {
+    try {
+      this.contestService.createContestRecord(req.user.id, contestId)
+      return
+    } catch (error) {
+      if (error instanceof EntityNotExistException) {
+        throw new NotFoundException(error.message)
+      }
+      throw new ForbiddenException(error.message)
     }
   }
 }

backend/src/contest/contest.service.ts
@@ -3,6 +3,7 @@ import { Contest } from '@prisma/client'
 import {
   EntityNotExistException,
   ForbiddenAccessException,
+  InvalidUserException,
   UnprocessableDataException
 } from 'src/common/exception/business.exception'
 import { GroupService } from 'src/group/group.service'
@@ -82,7 +83,7 @@ export class ContestService {
       data: {
         title: contestDto.title,
         description: contestDto.description,
-        descriptionSummary: contestDto.description,
+        descriptionSummary: contestDto.descriptionSummary,
         startTime: contestDto.startTime,
         endTime: contestDto.endTime,
         visible: contestDto.visible,
@@ -235,5 +236,45 @@ export class ContestService {
       where: { groupId: groupId },
       select: { ...this.contestSelectOption, visible: true }
     })
+  }
+
+  // Todo: issue #90
+  async createContestRecord(
+    userId: number,
+    contestId: number
+  ): Promise<null | Error> {
+    //contest 존재 여부
+    const contest = await this.prisma.contest.findUnique({
+      where: { id: contestId },
+      select: { startTime: true, endTime: true, type: true }
+    })
+    if (!contest) {
+      throw new EntityNotExistException(`Contest ${contestId}`)
+    }
+
+    //중복 참여 확인 in contestRecord
+    const isAlreadyRecord = await this.prisma.contestRecord.findFirst({
+      where: { userId, contestId },
+      select: { id: true }
+    })
+    if (isAlreadyRecord) {
+      throw new ForbiddenAccessException(
+        `User ${userId} is already participated in Contest ${contestId}`
+      )
+    }
+    //contest start 전 or contest end 후
+    const now = new Date()
+    if (now < contest.startTime || now >= contest.endTime) {
+      throw new ForbiddenAccessException(`Contest ${contestId} is not ongoing`)
+    }
+    //contest type ACM -> create contest rank acm record
+    if (contest.type === 'ACM') {
+      await this.prisma.contestRankACM.create({
+        data: { contestId, userId }
+      })
+    }
+    // Todo: other contest type -> create other contest record table
+
+    return
   }
 }