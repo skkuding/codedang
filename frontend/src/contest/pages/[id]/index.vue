585b62eda2291dad5e874eedba0e2095c5e1fa53
hbsongk <68498323+hbsongk@users.noreply.github.com>
2022-09-14 13:47:13 +0000

    chore(fe): complement code details

--- frontend/src/contest/pages/[id]/index.vue
+++ frontend/src/contest/pages/[id]/index.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
 import PageTitle from '../../../common/components/Atom/PageTitle.vue'
-import { computed } from 'vue'
+import { ref } from 'vue'
 import Badge from '../../../common/components/Molecule/Badge.vue'
 import Tab from '../../../common/components/Molecule/Tab.vue'
 import top from '../../components/top.vue'
 import problem from '../../components/problem.vue'
 import ranking from '../../components/ranking.vue'
 import notice from '../../components/notice.vue'
-import { useTimestamp } from '@vueuse/core'
+import { useNow, useDateFormat } from '@vueuse/core'
 
-const timestamp = useTimestamp()
-const currentTime = computed(() => new Date(timestamp.value))
+const formatter = ref('YYYY-MM-DD HH:mm:ss')
+const time = useDateFormat(useNow(), formatter)
 </script>
 
 <template>
-  <div class="mx-auto mt-10 h-[760px] w-10/12">
+  <div class="mx-auto mt-10">
     <div class="mb-5 flex justify-between">
       <PageTitle text="SKKU Coding Platform 2차 모의대회" class="mb-14" />
-      <Badge color="green">{{ currentTime }}</Badge>
+      <Badge color="green">{{ time }}</Badge>
     </div>
-    <Tab :items="['top', 'problem', 'notice', 'ranking']" class="!font-bold">
+    <Tab :items="['top', 'problem', 'notice', 'ranking']" class="font-bold">
       <template #top><top /></template>
       <template #notice><notice /></template>
       <template #problem><problem /></template>
