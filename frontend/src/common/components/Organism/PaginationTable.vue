4e21d0b12ef13dc13a56c0cb22a85a7c8b449ede
goo314 <rn0731@g.skku.edu>
2022-09-18 14:31:37 +0000

    chore(fe): add subhead and reset responsive style 화면 크기가 작아질때 subhead가 없을 경우 3개의 column만 보여준다. 화면 크기가 작아질때 subhead가 있는 경우 가로로 scroll한다.

--- frontend/src/common/components/Organism/PaginationTable.vue
+++ frontend/src/common/components/Organism/PaginationTable.vue
@@ -3,12 +3,18 @@ import { computed, ref, watch } from 'vue'
 import SearchBar from '../Molecule/SearchBar.vue'
 import Pagination from '../Molecule/Pagination.vue'
 
-defineProps<{
-  fields: {
-    key: string
-    label?: string
-    width?: string
-  }[]
+type subfieldType = {
+  key: string
+  label?: string
+}
+
+type fieldType = subfieldType & {
+  width?: string
+  subfields?: subfieldType[]
+}
+
+const props = defineProps<{
+  fields: fieldType[]
   items: {
     [key: string]: unknown
   }[]
@@ -23,6 +29,21 @@ defineProps<{
 
 const emit = defineEmits(['row-clicked', 'change-page', 'search'])
 
+const subhead = computed(() => {
+  return props.fields.reduce((prev: subfieldType[], cur: fieldType) => {
+    return prev.concat(cur.subfields || [])
+  }, [])
+})
+
+const entries = computed(() => {
+  return props.fields.reduce(
+    (prev: (subfieldType | fieldType)[], cur: fieldType) => {
+      return prev.concat(cur.subfields || cur)
+    },
+    []
+  )
+})
+
 const headerColor = {
   light: 'text-text-title bg-[#F9F9F9]',
   secondary: 'bg-transparent text-white',
@@ -33,6 +54,14 @@ const rowColor = {
   light: 'hover:bg-gray-light',
   secondary: 'hover:bg-[#204A60] text-white',
   dark: 'hover:bg-[#212529] text-white'
+}
+
+const responsiveStyle = (index: number) => {
+  return subhead.value.length > 0 ? '' : index > 2 ? 'hidden md:table-cell' : ''
+}
+
+const capitalize = (key: string) => {
+  return key.charAt(0).toUpperCase() + key.slice(1)
 }
 
 const currentPage = ref(1)
@@ -59,55 +88,82 @@ watch(currentPage, (value) => {
         @search="search"
       ></SearchBar>
     </div>
-    <table
-      class="w-full table-fixed break-all"
-      :class="!noSearchBar ? 'mt-5' : ''"
-    >
-      <thead v-if="!noHeader">
-        <tr
-          class="border-gray border-b-2"
-          :class="headerColor[mode || 'light']"
-        >
-          <th
-            v-for="(field, index) in fields"
-            :key="index"
-            class="p-2.5 pl-4 text-left"
-            :class="index > 2 ? 'hidden md:table-cell' : ''"
-            :style="field.width ? 'width:' + field.width : ''"
-          >
-            <span v-if="Object.keys(field).includes('label')">
-              {{ field.label }}
-            </span>
-            <span v-else>
-              {{ field.key.charAt(0).toUpperCase() + field.key.slice(1) }}
-            </span>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-if="items.length === 0">
-          <td class="p-2.5 pl-4">{{ text }}</td>
-        </tr>
-        <template v-else>
+    <div class="min-w-full overflow-x-scroll">
+      <table
+        class="table-fixed break-normal md:w-full md:break-all"
+        :class="!noSearchBar ? 'mt-5' : ''"
+      >
+        <thead v-if="!noHeader">
           <tr
-            v-for="(row, index) in items"
-            :key="index"
-            class="border-gray cursor-pointer border-y"
-            :class="rowColor[mode || 'light']"
-            @click="$emit('row-clicked', row)"
+            :class="[
+              { 'border-gray border-b-2': subhead.length === 0 },
+              headerColor[mode || 'light']
+            ]"
           >
-            <td
-              v-for="(field, idx) in fields"
-              :key="idx"
-              class="p-2.5 pl-4"
-              :class="idx > 2 ? 'hidden md:table-cell' : ''"
+            <th
+              v-for="(field, index) in fields"
+              :key="index"
+              class="p-2.5 pl-4 text-left"
+              :class="responsiveStyle(index)"
+              :rowspan="field.subfields ? undefined : 2"
+              :colspan="field.subfields ? field.subfields.length : undefined"
+              :style="field.width ? 'width:' + field.width : ''"
             >
-              <slot :name="field.key" :row="row">{{ row[field.key] }}</slot>
+              <span v-if="Object.keys(field).includes('label')">
+                {{ field.label }}
+              </span>
+              <span v-else>
+                {{ capitalize(field.key) }}
+              </span>
+            </th>
+          </tr>
+          <!-- additional head row for subhead -->
+          <tr
+            v-if="subhead.length > 0"
+            class="border-gray border-b-2 text-sm"
+            :class="headerColor[mode || 'light']"
+          >
+            <th
+              v-for="(subfield, index) in subhead"
+              :key="index"
+              class="p-2.5 pl-4 text-left"
+            >
+              <span v-if="Object.keys(subfield).includes('label')">
+                {{ subfield.label }}
+              </span>
+              <span v-else>
+                {{ capitalize(subfield.key) }}
+              </span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-if="items.length === 0">
+            <td class="p-2.5 pl-4" :class="{ 'text-white': mode !== 'light' }">
+              {{ text || 'No Data' }}
             </td>
           </tr>
-        </template>
-      </tbody>
-    </table>
+          <template v-else>
+            <tr
+              v-for="(row, index) in items"
+              :key="index"
+              class="border-gray cursor-pointer border-y"
+              :class="rowColor[mode || 'light']"
+              @click="$emit('row-clicked', row)"
+            >
+              <td
+                v-for="(entry, idx) in entries"
+                :key="idx"
+                class="p-2.5 pl-4"
+                :class="responsiveStyle(idx)"
+              >
+                <slot :name="entry.key" :row="row">{{ row[entry.key] }}</slot>
+              </td>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+    </div>
     <div class="flex justify-center">
       <Pagination
         v-if="!noPagination"
