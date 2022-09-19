bfb786994ccaec6531bb0958e5b86cef63a709a9
goo314 <rn0731@g.skku.edu>
2022-09-19 11:48:58 +0000

    docs(fe): reflect mode and subhead change in story

--- frontend/src/common/components/Organism/PaginationTable.story.vue
+++ frontend/src/common/components/Organism/PaginationTable.story.vue
@@ -1,49 +1,122 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, watch } from 'vue'
 import PaginationTable from './PaginationTable.vue'
 
-const fields = [{ key: 'name', label: 'Object' }, { key: 'color' }]
+const fields = [
+  {
+    key: 'index',
+    label: '#',
+    width: '60px'
+  },
+  {
+    key: 'color',
+    width: '20%'
+  },
+  {
+    key: 'name',
+    label: 'Object'
+  },
+  { key: 'description', width: '40%' }
+]
+
+const withSubfields = [
+  {
+    key: 'index',
+    label: '#',
+    width: '60px'
+  },
+  {
+    key: 'name',
+    label: 'Object'
+  },
+  { key: 'description', width: '40%' },
+  { key: 'more', subfields: [{ key: 'color' }, { key: 'count' }] }
+]
 
 interface Item {
+  index: number
   name: string
+  description: string
   color: string
+  count: number
 }
 
 // initial items
-const items: Item[][] = [
+const items = [
   [
-    { name: 'Apple', color: 'red' },
-    { name: 'Banana', color: 'yellow' },
-    { name: 'Car', color: 'blue' }
+    {
+      index: 0,
+      name: 'Apple',
+      description: 'It is my favorite fruit',
+      color: 'red',
+      count: 100
+    },
+    {
+      index: 1,
+      name: 'Banana',
+      description: 'It is my favorite fruit',
+      color: 'yellow',
+      count: 123
+    },
+    {
+      index: 2,
+      name: 'Car',
+      description: 'It is my favorite item',
+      color: 'blue',
+      count: 687
+    }
   ],
   [
-    { name: 'Dog', color: 'brown' },
-    { name: 'Elephant', color: 'gray' },
-    { name: 'Fox', color: 'orange' }
+    {
+      index: 3,
+      name: 'Dog',
+      description: 'It is my favorite animal',
+      color: 'brown',
+      count: 290
+    },
+    {
+      index: 4,
+      name: 'Elephant',
+      description: 'It is my favorite animal',
+      color: 'gray',
+      count: 102
+    },
+    {
+      index: 5,
+      name: 'Fox',
+      description: 'It is my favorite animal',
+      color: 'orange',
+      count: 203
+    }
   ],
   [
-    { name: 'Grape', color: 'purple' },
-    { name: 'Hamster', color: 'yellow' }
+    {
+      index: 6,
+      name: 'Grape',
+      description: 'It is my favorite fruit',
+      color: 'purple',
+      count: 304
+    }
   ]
 ]
 
-const shownItems = ref(items)
+const shownItems = ref(items.map((item) => item.slice()))
 const shownPages = ref(items.length)
 
-// items in current page
 const currentPage = ref(1)
-const currentItems = computed(() => shownItems.value[currentPage.value - 1])
+const currentItems = ref(items[0])
+
+watch(currentPage, (page) => {
+  currentItems.value =
+    shownItems.value.length > 0 ? shownItems.value[page - 1] : []
+})
 
 const filter = (keyword: string) => {
   const total = []
   for (const item of items) {
-    total.push(
-      ...item.filter(
-        (value) => value.name.includes(keyword) || value.color.includes(keyword)
-      )
-    )
+    total.push(...item.filter((value) => value.name.includes(keyword)))
   }
-  shownItems.value.splice(0, shownItems.value.length)
+  shownItems.value.splice(0, shownPages.value)
   while (total.length > 0) {
     shownItems.value.push(total.splice(0, 3))
   }
@@ -65,36 +138,94 @@ const clickRow = (row: Item) => {
 
 <template>
   <Story>
-    <Variant title="Basic">
-      <PaginationTable
-        :fields="fields"
-        :items="currentItems"
-        placeholder="keywords"
-        :number-of-pages="shownPages"
-        @search="filter"
-        @change-page="changeItems"
-        @row-clicked="clickRow"
-      >
-        <template #color="{ row }">
-          <div class="flex items-center gap-2">
-            <span
-              class="h-5 w-5 rounded-full"
-              :style="'background:' + row.color"
-            />
-            {{ row.color }}
-          </div>
-        </template>
-      </PaginationTable>
+    <Variant title="Light">
+      <div class="p-10">
+        <h2 class="text-lg">Default</h2>
+        <PaginationTable
+          :fields="fields"
+          :items="currentItems"
+          placeholder="keywords"
+          :number-of-pages="shownPages"
+          @search="filter"
+          @change-page="changeItems"
+          @row-clicked="clickRow"
+        >
+          <template #color="{ row }">
+            <div class="flex items-center gap-2">
+              <span
+                class="h-5 w-5 rounded-full"
+                :style="'background:' + row.color"
+              />
+              {{ row.color }}
+            </div>
+          </template>
+        </PaginationTable>
+        <div class="text-gray-dark mt-10 text-sm">
+          Click item : {{ selected }}
+        </div>
 
-      <div class="mt-10">Click item : {{ selected }}</div>
+        <hr class="my-10" />
+        <h2 class="mb-5 text-lg">With Subheading</h2>
+
+        <PaginationTable
+          :fields="withSubfields"
+          :items="items[0]"
+          placeholder="keywords"
+          :number-of-pages="5"
+        >
+          <template #color="{ row }">
+            <div class="flex items-center gap-2">
+              <span
+                class="h-5 w-5 rounded-full"
+                :style="'background:' + row.color"
+              />
+              {{ row.color }}
+            </div>
+          </template>
+        </PaginationTable>
+      </div>
     </Variant>
-    <Variant title="Empty">
-      <PaginationTable
-        :fields="[]"
-        :items="[]"
-        :number-of-pages="1"
-        text="No data"
-      />
+    <Variant title="Secondary">
+      <div class="bg-[#1A3E51] p-10">
+        <PaginationTable
+          :fields="fields"
+          :items="items[0]"
+          mode="secondary"
+          placeholder="keywords"
+          :number-of-pages="5"
+        >
+          <template #color="{ row }">
+            <div class="flex items-center gap-2">
+              <span
+                class="h-5 w-5 rounded-full"
+                :style="'background:' + row.color"
+              />
+              {{ row.color }}
+            </div>
+          </template>
+        </PaginationTable>
+      </div>
+    </Variant>
+    <Variant title="Dark">
+      <div class="bg-black p-10">
+        <PaginationTable
+          :fields="fields"
+          :items="items[1]"
+          mode="dark"
+          placeholder="keywords"
+          :number-of-pages="5"
+        >
+          <template #color="{ row }">
+            <div class="flex items-center gap-2">
+              <span
+                class="h-5 w-5 rounded-full"
+                :style="'background:' + row.color"
+              />
+              {{ row.color }}
+            </div>
+          </template>
+        </PaginationTable>
+      </div>
     </Variant>
   </Story>
 </template>
