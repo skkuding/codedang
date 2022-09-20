ddc0d9d9e1f9c6f085e3ae5cf8918642120b4acf
Jaemin Choi <1dotolee@gmail.com>
2022-09-20 18:20:40 +0900

    feat: add email template for email verification (#243)
    * feat: add email template for password reset
    
    * feat: add email change and register email template
    
    * chore: format templates

--- backend/src/email/mailerConfig.service.ts
+++ backend/src/email/mailerConfig.service.ts
@@ -1,6 +1,7 @@
 import { MailerOptions, MailerOptionsFactory } from '@nestjs-modules/mailer'
 import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter'
 
 @Injectable()
 export class MailerConfigService implements MailerOptionsFactory {
@@ -17,6 +18,12 @@ export class MailerConfigService implements MailerOptionsFactory {
       },
       defaults: {
         from: `"SKKU CODING PLATFORM" <${this.config.get('NODEMAILER_USER')}>`
+      },
+      template: {
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true
+        }
       }
     }
   }
