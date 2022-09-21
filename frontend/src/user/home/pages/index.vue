ee21d7157aa4f73f54a708d553ff6b48d70e0183
HyeLim_.p <62535887+djc06048@users.noreply.github.com>
2022-09-21 21:03:25 +0900

    feat(fe): add home page (#199)
    * feat(fe): 입력값이 없을 시 보여지는 경고창을 무시할 수 있는 props추가
    
    * fix(fe): modal의 배경과 모달 창 위치 수정
    
    * feat(fe): 모달 창 구현 위한 slot 추가
    
    * feat(fe): Home 페이지
    
    * chore(fe): set position of modal
    
    * feat(fe): Add PassWordFindModal- Organism
    
    * feat(fe): Add LogInModal- Organism
    
    * feat(fe): Add SignUpModal- Organism
    
    * chore(fe): 컴포넌트 수정
    
    * chore(fe): carousel 추가
    
    * Revert "chore(fe): carousel 추가"
    
    This reverts commit 70cf370ddf51e4345c72b52fe907a29a9f94b946.
    
    * chore(fe): modal 컴포넌트 완성
    
    * chore(fe): 헤더에 logIn, passwordFind, signUp 컴포넌트 추가
    
    * feat(fe): Organism에 LogInModal추가
    
    * feat(fe): Organism에 PasswordFindModal 추가
    
    * feat(fe): Organism에 SignUpModal 추가
    
    * chore(fe): carousel 추가 및 header 컴포넌트 수정
    
    * chore(fe): 사용되지 않는 import 제거
    
    * chore(fe): x 버튼 안눌리는 문제 해결 및 props제거
    
    * chore(fe): 모달 코드 위치 조정
    
    * chore(fe): 모달 크기에 tailwindcss 적용 및 스타일 조정
    
    * chore(fe): carousel과 card사이 간격 추가
    
    * chore(fe): 4px 단위로 수정
    
    * chore(fe): change name of props
    
    * chore(fe): delete unnecessary code
    
    * chore(fe): change border style
    
    * chore(fe): reflect  props name of Inputitem
    
    * chore(fe): change card layout
    
    * chore: remove duplicated header and footer
    
    #193 에서 적용된 layout에 따라 페이지 내에서는 header와 footer를 추가하지 않는다.
    
    * fix: define type refrences of vite plugins
    
    * chore: add link to card
    
    * style: add responsive style to cards
    
    * style: reimplement modal
    
    재사용성을 높이기 위해 modal 전체를 slot으로 둔다.
    v-model을 이용해 modal을 여는 interface를 간단히 한다.
    열고 닫는 애니메이션 효과와 배경에 blur를 적용한다.
    
    * chore: adjust modal size to content
    
    * fix: close modal only if outside is clicked
    
    * feat: add login modal
    
    * style: add page transition inside auth modal`
    
    * style: add signup modal
    
    * style: add password reset modal
    
    * feat: separate each modal content to single file
    
    각 페이지 내용을 async하게 보여주기 위해 각각 async component로 정의한다.
    
    * feat: add authmodal to header
    
    * style: add outline style to button
    
    * style: remove outline of content changer
    
    * chore: reduce transparency of mobile header menu
    
    * chore: create logo svg component
    
    페이지를 불러오는 시점에 svg를 synchronous하게 불러오기 위해서 component로 정의한다.
    Asynchronous하게 불러오려면 `AsyncComponent`를 활용하면 된다.
    
    * chore: reduce upper margin of password reset title
    
    * chore: add email verification interface to signup
    
    * style: add thin border for clear division
    
    * chore: align cards to top
    
    * chore: change fa-solid iconset to fa6
    
    * chore: replacee `sign in` term to `log in`
    
    * fix: show dropdown menu on carousel
    
    * chore: change fa-solid iconset to fa6
    
    * fix: hide mobile header menu on large screen
    
    * chore(ci): run ci on every pull request
    
    * fix: use string type for authmodal v-model
    
    * fix: use outline props of buttons in texteditor
    
    * fix: do not add transparency to pressed button
    
    * docs: add outlined & pressed buttons
    
    Co-authored-by: SOLBI1028 <hagkthfql@naver.com>
    Co-authored-by: Jaemin Choi <1dotolee@gmail.com>

--- frontend/src/user/home/pages/index.vue
+++ frontend/src/user/home/pages/index.vue
@@ -1,3 +1,82 @@
+<script setup lang="ts">
+import Card from '../components/Card.vue'
+import Carousel from '../components/Carousel.vue'
+import IconInfo from '~icons/fa6-solid/circle-info'
+import IconAngleRight from '~icons/fa6-solid/angle-right'
+import IconMedal from '~icons/fa6-solid/medal'
+import IconEllipsis from '~icons/fa6-solid/ellipsis'
+import IconCalendar from '~icons/fa6-solid/calendar'
+
+const noticeItems = [
+  {
+    title: 'SKKU Coding Platform 모의대회 결과 및 솔루션',
+    date: 'Jan 9,2021',
+    href: '/'
+  },
+  {
+    title: 'SKKU Coding Platform 모의대회 결과',
+    date: 'Dec 31,2020',
+    href: '/'
+  }
+]
+
+const contestItems = [
+  {
+    title: 'SKKU Coding Platform 모의대회',
+    date: 'Jan 9,2021',
+    href: '/',
+    state: 'ongoing'
+  },
+  {
+    title: 'SKKU Coding Platform 1차 대회',
+    date: 'Dec 31,2020',
+    href: '/',
+    state: 'prearranged'
+  },
+  {
+    title: 'SKKU Coding Platform 2차 대회',
+    date: 'Dec 31,2020',
+    href: '/',
+    state: 'prearranged'
+  }
+]
+
+const slides = [
+  'https://picsum.photos/id/1032/900/400',
+  'https://picsum.photos/id/1033/900/400',
+  'https://picsum.photos/id/1037/900/400',
+  'https://picsum.photos/id/1035/900/400',
+  'https://picsum.photos/id/1036/900/400'
+]
+</script>
+
 <template>
-  <div>This is Main page</div>
+  <main>
+    <Carousel :slides="slides" />
+    <div
+      class="my-20 flex flex-col items-center justify-center gap-12 lg:flex-row lg:items-start"
+    >
+      <Card href="/notice" :items="noticeItems" class="w-[36rem] max-w-full">
+        <template #title>
+          <IconInfo />
+          <h2 class="ml-2">Notice</h2>
+        </template>
+
+        <template #icon>
+          <IconAngleRight />
+        </template>
+      </Card>
+      <Card href="/contest" :items="contestItems" class="w-[36rem] max-w-full">
+        <template #title>
+          <IconMedal />
+          <h2 class="ml-2">Current/Upcoming Contests</h2>
+        </template>
+
+        <template #icon="item">
+          <IconEllipsis v-if="item.item === 'ongoing'" />
+          <IconCalendar v-else />
+        </template>
+      </Card>
+    </div>
+  </main>
 </template>
