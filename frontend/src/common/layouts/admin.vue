41b9c93476467e4125f2c33e5e7710c502317317
Jaemin Choi <1dotolee@gmail.com>
2022-09-22 12:08:09 +0000

    feat: add admin layout

---
+++ frontend/src/common/layouts/admin.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Sidebar from '../components/Organism/Sidebar.vue'
+</script>
+
+<template>
+  <div class="flex w-fit">
+    <Sidebar />
+    <router-view />
+  </div>
+</template>
