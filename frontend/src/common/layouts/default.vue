f6fc9f7514417a0896dab396c3fb7379ae7fa92a
Jaemin Choi <1dotolee@gmail.com>
2022-09-22 11:49:37 +0000

    feat: add page layout
    vite-plugin-vue-layouts plugin을 이용해서 layout을 만든다.

---
+++ frontend/src/common/layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Header from '../components/Organism/Header.vue'
+import Footer from '../components/Organism/Footer.vue'
+</script>
+
+<template>
+  <Header />
+  <router-view class="mx-auto w-4/5 max-w-7xl" />
+  <Footer />
+</template>
