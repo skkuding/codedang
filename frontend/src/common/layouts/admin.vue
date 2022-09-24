03734412bcec8dfc022e51adf062b682e9db04a4
Jaemin Choi <1dotolee@gmail.com>
2022-09-24 06:49:42 +0000

    feat: toggle if sidebar is for group
    page 내에서 emit을 통해 group 기능을 사용할지를 전달한다.
    https://github.com/JohnCampionJr/vite-plugin-vue-layouts#data-dynamically-from-page-to-layout

--- frontend/src/common/layouts/admin.vue
+++ frontend/src/common/layouts/admin.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import Sidebar from '../components/Organism/Sidebar.vue'
+
+const group = ref(false)
 </script>
 
 <template>
   <div class="flex w-fit">
-    <Sidebar />
-    <router-view />
+    <Sidebar :group="group" />
+    <router-view @toggle-group="(value: boolean) => (group = value)" />
   </div>
 </template>
