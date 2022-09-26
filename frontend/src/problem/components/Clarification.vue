4445315b2fcc21b2749b237dbf889fe7a25903e9
SongHongBin <hbsongk@naver.com>
2022-09-26 10:59:33 +0000

    feat(fe): add draggable attribute

--- frontend/src/problem/components/Clarification.vue
+++ frontend/src/problem/components/Clarification.vue
@@ -1,25 +1,30 @@
 <script setup lang="ts">
-import { computed } from 'vue'
 import { ref } from 'vue'
-import IconSound from '~icons/quill/sound'
+import IconSoundFilled from '~icons/ant-design/sound-filled'
+import { useDraggable } from '@vueuse/core'
 
 const visible = ref(false)
+const el = ref<HTMLElement | null>(null)
+
+const { x, y, style } = useDraggable(el, {
+  initialValue: { x: 40, y: 40 }
+})
 </script>
 
 <template>
   <button
-    class="item bg-gray h-[200px] w-[200px] rounded-full"
+    ref="el"
+    :style="style"
+    class="item bg-gray fixed h-[200px] w-[200px] rounded-full"
     @click="visible = !visible"
   >
-    <div class="relative">
-      <button
-        class="bg-red absolute top-0.5 right-0.5 h-[50px] w-[50px] rounded-full font-bold text-white"
-      >
-        1
-      </button>
-    </div>
+    <button
+      class="bg-red absolute top-0.5 right-0.5 h-[50px] w-[50px] rounded-full font-bold text-white"
+    >
+      1
+    </button>
     <div class="flex justify-center">
-      <IconSound class="text-7xl" />
+      <IconSoundFilled class="text-7xl text-white" />
     </div>
   </button>
   <div v-if="visible" class="bg-gray fixed h-96 w-96 overflow-auto">
