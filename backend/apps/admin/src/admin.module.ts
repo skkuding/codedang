d3ed3cc9ae2f26336b969c0ddad11c0be24249eb
Jaemin Choi <1dotolee@gmail.com>
2022-10-02 14:27:25 +0000

    feat: add prisma module

--- backend/apps/admin/src/admin.module.ts
+++ backend/apps/admin/src/admin.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { AdminController } from './admin.controller'
 import { AdminService } from './admin.service'
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
       sortSchema: true
-    })
+    }),
+    PrismaModule
   ],
   controllers: [AdminController],
   providers: [AdminService]
