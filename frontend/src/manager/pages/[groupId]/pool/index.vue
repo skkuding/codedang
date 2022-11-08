6fbb524a7aaffa277eaba9b86eff3e36f0a4d235
goo314 <rn0731@g.skku.edu>
2022-11-08 12:35:59 +0000

    feat: add logic in buttons create 버튼을 누르면 detail 페이지로 이동한다. api 연결로 빈 부분을 매꿀 수 있다.

--- frontend/src/manager/pages/[groupId]/pool/index.vue
+++ frontend/src/manager/pages/[groupId]/pool/index.vue
@@ -10,6 +10,9 @@ import Modal from '@/common/components/Molecule/Modal.vue'
 import IconTrashCan from '~icons/fa6-solid/trash-can'
 import IconPaperPlane from '~icons/fa6-solid/paper-plane'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 const fields = [
   { key: 'id' },
@@ -21,14 +24,15 @@ const fields = [
   { key: 'options' }
 ]
 
-const items: { [key: string]: any }[] = [
+const items = ref([
   {
     id: 1,
     name: '문제set1',
     problems: 20,
     createdtime: '2021-12-31 18:20:29',
     createdby: '하솔비',
-    sharedgroup: ['skkuding', 'npc']
+    sharedgroup: ['skkuding', 'npc'],
+    authority: { share: true, delete: true }
   },
   {
     id: 2,
@@ -36,7 +40,8 @@ const items: { [key: string]: any }[] = [
     problems: 20,
     createdtime: '2021-12-31 18:20:29',
     createdby: '박민서',
-    sharedgroup: ['skkuding', 'npc']
+    sharedgroup: ['skkuding', 'npc'],
+    authority: { share: true, delete: false }
   },
   {
     id: 3,
@@ -44,7 +49,8 @@ const items: { [key: string]: any }[] = [
     problems: 20,
     createdtime: '2021-12-31 18:20:29',
     createdby: '구성현',
-    sharedgroup: ['skkuding', 'npc']
+    sharedgroup: ['skkuding', 'npc'],
+    authority: { share: true, delete: false }
   },
   {
     id: 4,
@@ -52,35 +58,54 @@ const items: { [key: string]: any }[] = [
     problems: 20,
     createdtime: '2021-12-31 18:20:29',
     createdby: '김학산',
-    sharedgroup: ['skkuding', 'npc']
+    sharedgroup: ['skkuding', 'npc'],
+    authority: { share: true, delete: true }
   }
-]
-
-const addOptions = () => {
-  items.forEach((item) => {
-    if (item.createdby === '하솔비')
-      item['options'] = [IconTrashCan, IconPaperPlane]
-    else item['options'] = [IconPaperPlane]
-  })
-}
-addOptions()
+])
 
 // for create problem pool modal
-const showCreate = ref(false)
-const createName = ref('')
+const showCreateModal = ref(false)
+const poolName = ref('')
+
+const createPool = () => {
+  if (poolName.value.length === 0) return
+  // TODO: get pool id from post api
+  const id = 1
+  router.push('./pool/' + id)
+}
 
 // for share problem pool modal
-const showSharing = ref<boolean>(false)
-const sharedGroupField = [{ key: 'name' }, { key: 'delete' }]
-const sharedGroup = [{ name: 'NPC 중급반' }]
-
+const showSharingModal = ref<boolean>(false)
 const showCheckSharing = ref<boolean>(false)
+const inviteGroup = ref()
+const sharedGroupField = [{ key: 'name' }, { key: 'delete' }]
+const sharedGroup = ref([{ name: 'NPC 중급반' }])
+
+const findGroup = (code: string) => {
+  if (code.length === 0) {
+    inviteGroup.value = undefined
+    return
+  }
+  inviteGroup.value = {
+    name: 'mini SKKUDING',
+    description: 'small version of skkuding',
+    creator: '홍길동',
+    img: 'https://www.skku.edu/_res/skku/img/skku_s.png'
+  }
+}
+
+const appendSharedGroup = () => {
+  if (!sharedGroup.value.find((x) => x.name === inviteGroup.value.name))
+    sharedGroup.value.push(inviteGroup.value)
+  showCheckSharing.value = false
+  inviteGroup.value = undefined
+}
 </script>
 
 <template>
   <div class="mb-6 flex">
     <PageTitle text="Problem Pool List" />
-    <Button class="ml-4" @click="showCreate = true">+ Create</Button>
+    <Button class="ml-4" @click="showCreateModal = true">+ Create</Button>
   </div>
 
   <PaginationTable
@@ -104,19 +129,21 @@ const showCheckSharing = ref<boolean>(false)
 
     <template #options="{ row }">
       <Button
-        v-if="row.createdby === '하솔비'"
+        v-if="row.authority.delete"
         class="mr-1 aspect-square rounded-lg"
         outline
         color="gray-dark"
+        @click="() => (items = items.filter((x) => x.id !== row.id))"
       >
         <IconTrashCan />
       </Button>
       <Button
-        :class="row.createdby === '하솔비' ? 'ml-1' : 'ml-4'"
+        v-if="row.authority.share"
+        :class="row.authority.delete ? 'ml-2' : 'ml-12'"
         class="aspect-square rounded-lg"
         outline
         color="gray-dark"
-        @click="showSharing = true"
+        @click="showSharingModal = true"
       >
         <IconPaperPlane />
       </Button>
@@ -124,40 +151,45 @@ const showCheckSharing = ref<boolean>(false)
   </PaginationTable>
 
   <!-- Create Problem Pool Modal -->
-  <Modal v-model="showCreate" class="h-[16rem] w-[45rem] p-10">
+  <Modal v-model="showCreateModal" class="h-[16rem] w-[45rem] p-10">
     <PageSubtitle class="text-center" text="Create Problem Pool" />
     <div class="my-6 flex flex-col gap-y-2 px-6">
       <div class="text-lg font-bold">Problem Pool Name</div>
       <InputItem
-        v-model="createName"
+        v-model="poolName"
         shadow
+        required
         placeholder="Problem Pool Name"
         class="block w-full"
+        @keyup.enter="createPool"
       />
     </div>
-    <Button
-      class="absolute bottom-6 right-6"
-      @click="() => $router.push('./pool/1')"
-    >
+    <Button class="absolute bottom-6 right-6" @click="createPool">
       Create
     </Button>
   </Modal>
 
   <!-- Share Problem Pool Modal -->
-  <Modal v-model="showSharing" class="w-[45rem] p-10">
+  <Modal v-model="showSharingModal" class="w-[45rem] p-10">
     <PageSubtitle class="text-center" text="Share Problem Pool" />
     <div class="text-green my-4 text-center text-base">그래프 문제 set</div>
     <div v-if="!showCheckSharing" class="my-6 flex flex-col gap-y-2 px-6">
       <div class="text-lg font-bold">Invitation code of shared group</div>
-      <SearchBar placeholder="Invitation Code" class="place-self-end" />
+      <SearchBar
+        id="search-group"
+        placeholder="Invitation Code"
+        class="place-self-end"
+        @search="findGroup"
+      />
       <CardItem
-        title="NPC 초급반"
-        img="https://www.skku.edu/_res/skku/img/skku_s.png"
-        description="NPC 초급반 그룹"
-        colored-text="구성현, 송홍빈"
-        colored-text-short="구성현, 송홍빈"
+        v-if="inviteGroup !== undefined"
+        :title="inviteGroup.name"
+        :img="inviteGroup.img"
+        :description="inviteGroup.description"
+        :colored-text="inviteGroup.creator"
         @click="showCheckSharing = true"
       />
+      <div v-else class="py-10 text-center">No Group Found</div>
       <div class="mt-6 text-lg font-bold">Shared Group</div>
       <PaginationTable
         :fields="sharedGroupField"
@@ -167,24 +199,35 @@ const showCheckSharing = ref<boolean>(false)
         no-header
         no-search-bar
       >
-        <template #delete>
-          <Button outline color="gray-dark" class="aspect-square rounded-lg">
+        <template #delete="{ row }">
+          <Button
+            outline
+            color="gray-dark"
+            class="aspect-square rounded-lg"
+            @click="
+              () =>
+                (sharedGroup = sharedGroup.filter((x) => x.name !== row.name))
+            "
+          >
             <IconTrashCan />
           </Button>
         </template>
       </PaginationTable>
-      <Button class="absolute bottom-6 right-6" @click="showSharing = false">
+      <Button
+        class="absolute bottom-6 right-6"
+        @click="showSharingModal = false"
+      >
         Save
       </Button>
     </div>
     <div v-else class="my-6 flex flex-col items-center justify-center gap-y-4">
       <div class="my-6">
         Do you really want to share
-        <span class="font-bold">'NPC 중급반'</span>
+        <span class="font-bold">{{ inviteGroup.name }}</span>
         ?
       </div>
       <div class="flex gap-x-10">
-        <Button class="h-10 w-20">Yes</Button>
+        <Button class="h-10 w-20" @click="appendSharedGroup">Yes</Button>
         <Button class="h-10 w-20" @click="showCheckSharing = false">No</Button>
       </div>
     </div>
