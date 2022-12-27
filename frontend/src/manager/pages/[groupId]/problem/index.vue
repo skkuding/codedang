a959887600e29d1f5ccbf15a21097dcb8c97a44b
SongHongBin <hbsongk@naver.com>
2022-12-27 08:11:58 +0000

    feat(fe): add manager problem list page

--- frontend/src/manager/pages/[groupId]/problem/index.vue
+++ frontend/src/manager/pages/[groupId]/problem/index.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts">
-// write script code
+import PaginationTable from '@/common/components/Organism/PaginationTable.vue'
+import Sidebar from '@/common/components/Organism/Sidebar.vue'
+import PageTitle from '@/common/components/Atom/PageTitle.vue'
+import PageSubtitle from '@/common/components/Atom/PageSubtitle.vue'
+import Button from '@/common/components/Atom/Button.vue'
 </script>
 
 <template>
-  <div>write template code</div>
+  <div class="flex">
+    <Sidebar />
+    <main>
+      <PageTitle text="Problem" />
+      <div class="flex">
+        <div>
+          <Button>+ Create</Button>
+          <Button>Import</Button>
+        </div>
+        <div><PageSubtitle text="created by" /></div>
+      </div>
+    </main>
+  </div>
 </template>
