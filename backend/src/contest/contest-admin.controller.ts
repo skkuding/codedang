312176d68e95606ad7226ee5ef63ef9ec7c80f59
Park Minseo <alstj778@gmail.com>
2022-12-28 20:22:51 +0900

    feat(be): implement a group contest switch to public request (#179)
    * chore(be): add contestToPublicRequest table
    - public으로 전환 요청을 위한 테이블 추가
    - contest가 전환되었음을 표시하기 위한 is_public 필드 추가
    
    * feat(be): implement group manager request a contest to be public
    
    * feat(be): implement groupadmin cancel the request
    
    * chore(be): rename createContestToPublicRequest feature
    
    * chore(be): fix typo and create new migration of contesttopublicrequest table
    
    * feat(be): implement getContestToPublicRequest for group admin
    
    * chore(be): specify return type of getContestToPublicRequest
    
    * feat(be): implement admin update response of contestToPublicRequest
    
    * feat(be): implement admin get ContestToPublicRequest list
    
    * feat(be): implement admin get contestToPublicRequest
    
    * chore(be): add update contest isPublic logic to respond
    
    * fix(be): add is_public to mock contest data
    
    * refactor(be): reorder contest admin controller
    
    * refactor(be): add deleteUnacceptedRequest method
    
    * fix(be): fix typo of method name
    
    * test(be): add initial test case for contestToPublicRequest services
    
    * test(be): add test case for get service method
    
    * refactor(be): separate contestToPublicRequest controllers
    
    * chore(be): change enum value request status
    
    * chore(be): apply change enum value
    
    * refactor(be): remove useless else statement
    
    * fix(be): fix minor problem
    
    * chore(be): add actionNotAllowedException
    
    * chore(be): change relation topulic request vs contest
    
    * chore(be): change route path
    
    * chore(be): change implementation for group manager
    
    * chore(be): change super manager feature implementation
    
    * chore(be): apply camelCase rule for codes
    
    * chore: change name of contest publicizing request
    
    `ContestToPublicRequest`가 다소 모호한 표현일 수 있어 명확한 표현의 이름으로 변경한다.
    
    * chore: change url path for publicizing request
    
    URL 경로에 명확한 표현을 사용한다.
    
    * chore: change name of contest updating action
    
    * chore: erase unused URL parameter
    
    * chore: change URL parameter name
    
    * refactor: change URL endpoint
    
    Gather the common element of each API's URL address and erase it.
    Add the element to ContestPublicizingRequestAdminController's URL endpoint.
    
    * chore: delete unused URL parameter
    
    contestId is not used in deleteContestPublicizingRequest and getContestPublicizingRequest.
    
    Co-authored-by: Jaemin Choi <1dotolee@gmail.com>
    Co-authored-by: SH9480P <realsehwan@gmail.com>

--- backend/src/contest/contest-admin.controller.ts
+++ backend/src/contest/contest-admin.controller.ts
@@ -4,6 +4,7 @@ import {
   Delete,
   Get,
   InternalServerErrorException,
+  MethodNotAllowedException,
   NotFoundException,
   Param,
   ParseIntPipe,
@@ -16,8 +17,9 @@ import {
 import { ContestService } from './contest.service'
 import { CreateContestDto } from './dto/create-contest.dto'
 import { UpdateContestDto } from './dto/update-contest.dto'
-import { Contest, Role } from '@prisma/client'
+import { Contest, ContestPublicizingRequest, Role } from '@prisma/client'
 import {
+  ActionNotAllowedException,
   EntityNotExistException,
   UnprocessableDataException
 } from 'src/common/exception/business.exception'
@@ -25,6 +27,7 @@ import { AuthenticatedRequest } from 'src/auth/interface/authenticated-request.i
 import { GroupLeaderGuard } from 'src/group/guard/group-leader.guard'
 import { RolesGuard } from 'src/user/guard/roles.guard'
 import { Roles } from 'src/common/decorator/roles.decorator'
+import { RespondContestPublicizingRequestDto } from './dto/respond-publicizing-request.dto'
 
 @Controller('admin/contest')
 @UseGuards(RolesGuard)
@@ -107,6 +110,7 @@ export class GroupContestAdminController {
       if (error instanceof EntityNotExistException) {
         throw new NotFoundException(error.message)
       }
+
       throw new InternalServerErrorException()
     }
   }
@@ -116,5 +120,144 @@ export class GroupContestAdminController {
     @Param('groupId', ParseIntPipe) groupId: number
   ): Promise<Partial<Contest>[]> {
     return await this.contestService.getAdminContestsByGroupId(groupId)
+  }
+}
+
+@Controller('admin/contest/publicizing-request')
+@UseGuards(RolesGuard)
+@Roles(Role.SuperManager)
+export class ContestPublicizingRequestAdminController {
+  constructor(private readonly contestService: ContestService) {}
+
+  @Patch('/:requestId')
+  async respondContestPublicizingRequest(
+    @Param('requestId', ParseIntPipe) requestId: number,
+    @Body()
+    respondContestPublicizingRequestDto: RespondContestPublicizingRequestDto
+  ): Promise<ContestPublicizingRequest> {
+    try {
+      return await this.contestService.respondContestPublicizingRequest(
+        requestId,
+        respondContestPublicizingRequestDto
+      )
+    } catch (error) {
+      if (error instanceof ActionNotAllowedException) {
+        throw new MethodNotAllowedException(error.message)
+      }
+      if (error instanceof EntityNotExistException) {
+        throw new NotFoundException(error.message)
+      }
+
+      throw new InternalServerErrorException()
+    }
+  }
+
+  @Get('/pending')
+  async getPendingContestPublicizingRequests(): Promise<
+    Partial<ContestPublicizingRequest>[]
+  > {
+    try {
+      return await this.contestService.getPendingContestPublicizingRequests()
+    } catch (error) {
+      throw new InternalServerErrorException()
+    }
+  }
+
+  @Get('/responded')
+  async getRespondedContestPublicizingRequests(): Promise<
+    Partial<ContestPublicizingRequest>[]
+  > {
+    try {
+      return await this.contestService.getRespondedContestPublicizingRequests()
+    } catch (error) {
+      throw new InternalServerErrorException()
+    }
+  }
+
+  @Get('/:requestId')
+  async getContestPublicizingRequest(
+    @Param('requestId', ParseIntPipe) requestId: number
+  ): Promise<Partial<ContestPublicizingRequest>> {
+    try {
+      return await this.contestService.getAdminContestPublicizingRequest(
+        requestId
+      )
+    } catch (error) {
+      if (error instanceof EntityNotExistException) {
+        throw new NotFoundException(error.message)
+      }
+
+      throw new InternalServerErrorException()
+    }
+  }
+}
+
+@Controller('admin/group/:groupId/contest/:contestId/publicizing-request')
+@UseGuards(RolesGuard, GroupManagerGuard)
+export class ContestPublicizingRequestController {
+  constructor(private readonly contestService: ContestService) {}
+
+  @Post()
+  async createContestPublicizingRequest(
+    @Req() req: AuthenticatedRequest,
+    @Body()
+    createContestPublicizingRequestDto: CreateContestPublicizingRequestDto
+  ) {
+    try {
+      return await this.contestService.createContestPublicizingRequest(
+        req.user.id,
+        createContestPublicizingRequestDto
+      )
+    } catch (error) {
+      if (error instanceof ActionNotAllowedException) {
+        throw new MethodNotAllowedException(error.message)
+      }
+
+      throw new InternalServerErrorException()
+    }
+  }
+
+  @Delete('/:requestId')
+  async deleteContestPublicizingRequest(
+    @Param('requestId', ParseIntPipe) requestId: number
+  ) {
+    try {
+      await this.contestService.deleteContestPublicizingRequest(requestId)
+    } catch (error) {
+      if (error instanceof ActionNotAllowedException) {
+        throw new MethodNotAllowedException(error.message)
+      }
+      if (error instanceof EntityNotExistException) {
+        throw new NotFoundException(error.message)
+      }
+
+      throw new InternalServerErrorException()
+    }
+  }
+
+  @Get()
+  async getContestPublicizingRequests(
+    @Param('contestId', ParseIntPipe) contestId: number
+  ): Promise<Partial<ContestPublicizingRequest>[]> {
+    try {
+      return await this.contestService.getContestPublicizingRequests(contestId)
+    } catch (error) {
+      throw new InternalServerErrorException()
+    }
+  }
+
+  @Get('/:requestId')
+  async getContestPublicizingRequest(
+    @Param('requestId', ParseIntPipe) requestId: number
+  ): Promise<Partial<ContestPublicizingRequest>> {
+    try {
+      return await this.contestService.getContestPublicizingRequest(requestId)
+    } catch (error) {
+      if (error instanceof EntityNotExistException) {
+        throw new NotFoundException(error.message)
+      }
+
+      throw new InternalServerErrorException()
+    }
   }
 }
