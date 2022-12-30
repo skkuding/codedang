c257cd8af1344fd35c747f8277ea8c35d0a69ab1
goo314 <rn0731@g.skku.edu>
2022-12-30 13:53:42 +0000

    chore: rename admin to manager

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         { dir: 'src/user/contest/pages', baseRoute: 'contest' },
         { dir: 'src/user/group/pages', baseRoute: 'group' },
         { dir: 'src/user/workbook/pages', baseRoute: 'workbook' },
-        { dir: 'src/admin/pages', baseRoute: 'admin' }
+        { dir: 'src/manager/pages', baseRoute: 'manager' }
       ]
     }),
     layouts({
