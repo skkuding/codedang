bba4f813c127fcd360c42d1e77b41bd2e61988f6
SH9480P <realsehwan@gmail.com>
2022-12-31 15:38:57 +0000

    chore: register WorkbookAdminController to module
    add missing controller to its module

--- backend/src/workbook/workbook.module.ts
+++ backend/src/workbook/workbook.module.ts
@@ -3,10 +3,11 @@ import { WorkbookController } from './workbook.controller'
 import { WorkbookService } from './workbook.service'
 import { UserModule } from 'src/user/user.module'
 import { GroupModule } from 'src/group/group.module'
+import { WorkbookAdminController } from './workbook-admin.controller'
 
 @Module({
   imports: [UserModule, GroupModule],
-  controllers: [WorkbookController],
+  controllers: [WorkbookController, WorkbookAdminController],
   providers: [WorkbookService]
 })
 export class WorkbookModule {}
