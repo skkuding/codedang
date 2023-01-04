7ce7d07124496de5014bc8a4edfc578284c00de6
Jaemin Choi <1dotolee@gmail.com>
2023-01-04 03:06:45 +0000

    fix: move nestjs server port
    5000번은 MacOS에서 충돌이 발생해 4000번으로 포트를 옮긴다.
    
    Ref: https://developer.apple.com/forums/thread/693768

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:4000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
