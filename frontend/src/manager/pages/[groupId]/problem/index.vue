f6f8f64a1ad907a9fed0780865c5d8e047628ac8
SongHongBin <hbsongk@naver.com>
2023-01-05 08:08:15 +0000

    chore(fe): add pagination table

--- frontend/src/manager/pages/[groupId]/problem/index.vue
+++ frontend/src/manager/pages/[groupId]/problem/index.vue
@@ -1,23 +1,71 @@
 <script setup lang="ts">
 import PaginationTable from '@/common/components/Organism/PaginationTable.vue'
-import Sidebar from '@/common/components/Organism/Sidebar.vue'
 import PageTitle from '@/common/components/Atom/PageTitle.vue'
 import PageSubtitle from '@/common/components/Atom/PageSubtitle.vue'
 import Button from '@/common/components/Atom/Button.vue'
+import { ref } from 'vue'
+
+const fields = [
+  { key: 'id' },
+  { key: 'title' },
+  { key: 'difficulty' },
+  { key: 'lastupdate' },
+  { key: 'delete' }
+]
+const items = ref([
+  {
+    id: 1,
+    title: '가파른 경사',
+    difficulty: 'Level1',
+    lastupdate: '2021-12-31 08:30:41'
+  },
+  {
+    id: 2,
+    title: '개구리',
+    difficulty: 'Level2',
+    lastupdate: '2021-12-31 08:30:42'
+  },
+  {
+    id: 3,
+    title: '올바른 괄호 문자열',
+    difficulty: 'Level3',
+    lastupdate: '2021-12-31 08:30:43'
+  },
+  {
+    id: 4,
+    title: '러브매칭',
+    difficulty: 'Level4',
+    lastupdate: '2021-12-31 08:30:44'
+  },
+  {
+    id: 5,
+    title: '많이 튼튼한 금고테스트',
+    difficulty: 'Level5',
+    lastupdate: '2021-12-31 08:30:45'
+  }
+])
 </script>
 
 <template>
-  <div class="flex">
-    <Sidebar />
-    <main>
-      <PageTitle text="Problem" />
-      <div class="flex">
-        <div>
-          <Button>+ Create</Button>
-          <Button>Import</Button>
-        </div>
-        <div><PageSubtitle text="created by" /></div>
+  <main>
+    <PageTitle text="Problem" class="pt-3" />
+    <div class="flex gap-4 pl-44">
+      <div class="flex gap-4">
+        <Button>+ Create</Button>
+        <Button>Import</Button>
       </div>
-    </main>
-  </div>
+      <PageSubtitle class="ml-14 text-base" text="created by" />
+    </div>
+  </main>
+  <PaginationTable
+    :fields="fields"
+    :items="items"
+    :number-of-pages="5"
+    text="No data"
+  ></PaginationTable>
 </template>
+
+<route lang="yaml">
+meta:
+  layout: admin
+</route>
