956a8e509bcdf3bb184d63620bf667afbff7b3fd
SongHongBin <hbsongk@naver.com>
2023-01-12 11:44:16 +0000

    chore(fe): delete unnecessary codes

--- frontend/src/user/contest/pages/index.vue
+++ frontend/src/user/contest/pages/index.vue
@@ -7,7 +7,6 @@ import IconCaretUp from '~icons/fa6-solid/caret-up'
 import { useTimeAgo } from '@vueuse/core'
 import { onMounted } from 'vue'
 import axios from 'axios'
-
 import { ref } from 'vue'
 
 interface Group {
@@ -25,100 +24,14 @@ interface Contest {
 }
 
 const items = ref<{ [key: string]: Contest[] }>({
-  ongoing: [
-    {
-      id: 1,
-      title: '2021 Summer SKKU 프로그래밍 대회',
-      startTime: new Date('2022-08-17T00:00:00'),
-      endTime: new Date('2022-12-18T11:59:00'),
-      type: 'ACM',
-      group: {
-        id: 1,
-        groupName: 'SKKUDING'
-      }
-    }
-  ],
-  registerNow: [
-    {
-      id: 1,
-      title: '2021 Summer SKKU 프로그래밍 대회',
-      startTime: new Date('2022-08-17T00:00:00'),
-      endTime: new Date('2022-12-18T11:59:00'),
-      type: 'ACM',
-      group: {
-        id: 1,
-        groupName: 'SKKUDING'
-      }
-    },
-    {
-      id: 2,
-      title: '2021 Summer SKKU 프로그래밍 대회',
-      startTime: new Date('2022-08-17T00:00:00'),
-      endTime: new Date('2022-12-18T11:59:00'),
-      type: 'ACM',
-      group: {
-        id: 2,
-        groupName: 'SKKUDING'
-      }
-    }
-  ],
-  upcoming: [
-    {
-      id: 1,
-      title: '2021 Summer SKKU 프로그래밍 대회',
-      startTime: new Date('2022-08-17T00:00:00'),
-      endTime: new Date('2022-12-18T11:59:00'),
-      type: 'ACM',
-      group: {
-        id: 1,
-        groupName: 'SKKUDING'
-      }
-    },
-    {
-      id: 2,
-      title: '2021 Summer SKKU 프로그래밍 대회',
-      startTime: new Date('2022-08-17T00:00:00'),
-      endTime: new Date('2022-12-18T11:59:00'),
-      type: 'ACM',
-      group: {
-        id: 2,
-        groupName: 'SKKUDING'
-      }
-    }
-  ],
-  finished: [
-    {
-      id: 1,
-      title: '2021 Summer SKKU 프로그래밍 대회',
-      startTime: new Date('2022-08-17T00:00:00'),
-      endTime: new Date('2022-12-18T11:59:00'),
-      type: 'ACM',
-      group: {
-        id: 1,
-        groupName: 'SKKUDING'
-      }
-    },
-    {
-      id: 2,
-      title: '2021 Summer SKKU 프로그래밍 대회',
-      startTime: new Date('2022-08-17T00:00:00'),
-      endTime: new Date('2022-12-18T11:59:00'),
-      type: 'ACM',
-      group: {
-        id: 2,
-        groupName: 'SKKUDING'
-      }
-    }
-  ]
+  ongoing: [],
+  upcoming: [],
+  finished: []
 })
+
 onMounted(async () => {
-  try {
-    const response = await axios.get('/api/contest')
-    items.value = response.data
-    console.log(items.value)
-  } catch (err) {
-    console.log('Error: ', err)
-  }
+  const response = await axios.get('/api/contest')
+  items.value = response.data
 })
 const coloredText = (id: string, item: Contest) => {
   if (id === 'ongoing' || id === 'registerNow')
@@ -140,7 +53,6 @@ const showFinished = ref(false)
   <div
     v-for="({ title, id }, index) in [
       { title: 'Ongoing Contests', id: 'ongoing' },
-      { title: 'Register Now Contests', id: 'registerNow' },
       { title: 'Upcoming Contests', id: 'upcoming' },
       { title: 'Finished Contests', id: 'finished' }
     ]"
