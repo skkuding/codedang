8d1558ce9128c1f74680f4643264aa71bea3ed95
SongHongBin <hbsongk@naver.com>
2023-01-12 10:09:39 +0000

    feat(fe): connect contest api

--- frontend/src/user/contest/pages/index.vue
+++ frontend/src/user/contest/pages/index.vue
@@ -5,93 +5,121 @@ import IconAnglesRight from '~icons/fa6-solid/angles-right'
 import IconCaretDown from '~icons/fa6-solid/caret-down'
 import IconCaretUp from '~icons/fa6-solid/caret-up'
 import { useTimeAgo } from '@vueuse/core'
+import { onMounted } from 'vue'
+import axios from 'axios'
 
 import { ref } from 'vue'
 
-interface Contest {
+interface Group {
   id: number
-  img: string
-  title: string
-  description: string
-  startTime: Date
-  endTime: Date
+  groupName: string
 }
 
-const items: { [key: string]: Contest[] } = {
+interface Contest {
+  id: number
+  title: string
+  startTime: Date
+  endTime: Date
+  type: string
+  group: Group
+}
+
+const items = ref<{ [key: string]: Contest[] }>({
   ongoing: [
     {
       id: 1,
-      img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
       title: '2021 Summer SKKU 프로그래밍 대회',
-      description: '2021년 여름학기 SKKU 프로그래밍 대회입니다.',
       startTime: new Date('2022-08-17T00:00:00'),
-      endTime: new Date('2022-12-18T11:59:00')
+      endTime: new Date('2022-12-18T11:59:00'),
+      type: 'ACM',
+      group: {
+        id: 1,
+        groupName: 'SKKUDING'
+      }
     }
   ],
   registerNow: [
     {
       id: 1,
-      img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
       title: '2021 Summer SKKU 프로그래밍 대회',
-      description: '2021년 여름학기 SKKU 프로그래밍 대회입니다.',
       startTime: new Date('2022-08-17T00:00:00'),
-      endTime: new Date('2022-12-18T11:59:00')
+      endTime: new Date('2022-12-18T11:59:00'),
+      type: 'ACM',
+      group: {
+        id: 1,
+        groupName: 'SKKUDING'
+      }
     },
     {
       id: 2,
-      img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-      title: '2021 Fall SKKU 프로그래밍 대회',
-      description: '2021년 2학기 SKKU 프로그래밍 대회입니다.',
-      startTime: new Date('2022-09-01T00:00:00'),
-      endTime: new Date('2022-09-17T11:59:00')
+      title: '2021 Summer SKKU 프로그래밍 대회',
+      startTime: new Date('2022-08-17T00:00:00'),
+      endTime: new Date('2022-12-18T11:59:00'),
+      type: 'ACM',
+      group: {
+        id: 2,
+        groupName: 'SKKUDING'
+      }
     }
   ],
   upcoming: [
     {
-      id: 3,
-      img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-      title: '2023 Spring SKKU 프로그래밍 대회',
-      description: '2023년 1학기 SKKU 프로그래밍 대회입니다.',
-      startTime: new Date('2022-09-20T00:00:00'),
-      endTime: new Date('2022-12-17T11:59:00')
+      id: 1,
+      title: '2021 Summer SKKU 프로그래밍 대회',
+      startTime: new Date('2022-08-17T00:00:00'),
+      endTime: new Date('2022-12-18T11:59:00'),
+      type: 'ACM',
+      group: {
+        id: 1,
+        groupName: 'SKKUDING'
+      }
     },
     {
-      id: 4,
-      img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-      title: '2022 Fall SKKU 프로그래밍 대회',
-      description: '2023년 2학기 SKKU 프로그래밍 대회입니다.',
-      startTime: new Date('2022-09-21T00:00:00'),
-      endTime: new Date('2022-12-20T11:59:00')
-    },
-    {
-      id: 5,
-      img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-      title: '2023 Winter SKKU 프로그래밍 대회',
-      description: '2023년 겨울학기 SKKU 프로그래밍 대회입니다.',
-      startTime: new Date('2022-09-22T00:00:00'),
-      endTime: new Date('2022-12-15T11:59:00')
+      id: 2,
+      title: '2021 Summer SKKU 프로그래밍 대회',
+      startTime: new Date('2022-08-17T00:00:00'),
+      endTime: new Date('2022-12-18T11:59:00'),
+      type: 'ACM',
+      group: {
+        id: 2,
+        groupName: 'SKKUDING'
+      }
     }
   ],
   finished: [
     {
-      id: 6,
-      img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
+      id: 1,
       title: '2021 Summer SKKU 프로그래밍 대회',
-      description: '2021년 여름학기 SKKU 프로그래밍 대회입니다.',
-      startTime: new Date('2020-09-22T00:00:00'),
-      endTime: new Date('2020-12-15T11:59:00')
+      startTime: new Date('2022-08-17T00:00:00'),
+      endTime: new Date('2022-12-18T11:59:00'),
+      type: 'ACM',
+      group: {
+        id: 1,
+        groupName: 'SKKUDING'
+      }
     },
     {
-      id: 7,
-      img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-      title: '2021 Fall SKKU 프로그래밍 대회',
-      description: '2021년 2학기 SKKU 프로그래밍 대회입니다.',
-      startTime: new Date('2020-09-22T00:00:00'),
-      endTime: new Date('2021-12-15T11:59:00')
+      id: 2,
+      title: '2021 Summer SKKU 프로그래밍 대회',
+      startTime: new Date('2022-08-17T00:00:00'),
+      endTime: new Date('2022-12-18T11:59:00'),
+      type: 'ACM',
+      group: {
+        id: 2,
+        groupName: 'SKKUDING'
+      }
     }
   ]
-}
-
+})
+onMounted(async () => {
+  try {
+    const response = await axios.get('/api/contest')
+    items.value = response.data
+    console.log(items.value)
+  } catch (err) {
+    console.log('Error: ', err)
+  }
+})
 const coloredText = (id: string, item: Contest) => {
   if (id === 'ongoing' || id === 'registerNow')
     return 'Started ' + useTimeAgo(item.startTime).value
@@ -134,16 +162,14 @@ const showFinished = ref(false)
         />
       </div>
     </div>
-
-    <div v-if="items[id].length === 0" class="text-gray-dark p-2.5 pl-4">
+    <div v-if="items[id]?.length === 0" class="text-gray-dark p-2.5 pl-4">
       No Contest
     </div>
     <div v-else-if="id !== 'finished' || showFinished">
       <div v-for="(item, idx) in items[id]" :key="idx">
         <CardItem
-          :img="item.img"
+          :img="'https://www.skku.edu/_res/skku/img/skku_s.png'"
           :title="item.title"
-          :description="item.description"
           :colored-text="coloredText(id, item)"
           :colored-text-short="coloredTextShort(id, item)"
           class="mt-4"
