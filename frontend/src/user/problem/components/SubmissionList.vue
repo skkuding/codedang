384ce9dad1f0991ec6379bb9396c7d6b4bed7ba7
jimin9038 <jimin9038@g.skku.edu>
2023-01-17 07:18:20 +0000

    feat(fe): implement submissionlist

--- frontend/src/user/problem/components/SubmissionList.vue
+++ frontend/src/user/problem/components/SubmissionList.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import PageTitle from '@/common/components/Atom/PageTitle.vue'
 import PaginationTable from '@/common/components/Organism/PaginationTable.vue'
+import Switch from '@/common/components/Molecule/Switch.vue'
+import SearchBar from '@/common/components/Molecule/SearchBar.vue'
+import Dropdown from '@/common/components/Molecule/Dropdown.vue'
+import ListItem from '@/common/components/Atom/ListItem.vue'
+import IconDown from '~icons/fa6-solid/angle-down'
 import { ref, watch } from 'vue'
-interface Item {
+
+const showOnlyMine = true
+
+interface Submission {
   id: number
   user: string
   userId: number
@@ -9,7 +18,7 @@ interface Item {
   language: string
   result: string
 }
-const items = [
+const submissionList: Submission[][] = [
   [
     {
       id: 19715,
@@ -113,36 +122,80 @@ const fields = [
     label: 'Result'
   }
 ]
-const shownItems = ref(items.map((item) => item.slice()))
-const shownPages = ref(items.length)
+const shownSubmissionList = ref(submissionList.map((s) => s.slice()))
+const shownPages = ref(submissionList.length)
 
 const currentPage = ref(1)
-const currentItems = ref(items[0])
+const currentSubmissionList = ref(submissionList[0])
 
 watch(currentPage, (page) => {
-  currentItems.value =
-    shownItems.value.length > 0 ? shownItems.value[page - 1] : []
+  currentSubmissionList.value =
+    shownSubmissionList.value.length > 0
+      ? shownSubmissionList.value[page - 1]
+      : []
 })
 
-const changeItems = (page: number) => {
+const changeSubmissionList = (page: number) => {
   currentPage.value = page
 }
 
 const selected = ref()
 
-const clickRow = (row: Item) => {
+const clickRow = (row: Submission) => {
   selected.value = row.id
 }
 </script>
 
 <template>
-  <PaginationTable
-    :fields="fields"
-    :items="currentItems"
-    placeholder="keywords"
-    :number-of-pages="shownPages"
-    :no-search-bar="true"
-    @change-page="changeItems"
-    @row-clicked="clickRow"
-  ></PaginationTable>
+  <div class="bg-slate-700">
+    <PaginationTable
+      :fields="fields"
+      :items="currentSubmissionList"
+      placeholder="keywords"
+      :number-of-pages="shownPages"
+      :no-search-bar="true"
+      mode="dark"
+      @change-page="changeSubmissionList"
+      @row-clicked="clickRow"
+    >
+      <template #option>
+        <div class="block space-x-3 space-y-3 md:flex md:items-baseline">
+          <Switch
+            v-model="showOnlyMine"
+            label="Only Mine"
+            class="ml-3"
+          ></Switch>
+          <SearchBar placeholder="User"></SearchBar>
+          <Dropdown color="white">
+            <template #button>
+              <div
+                class="flex h-9 w-fit flex-row items-center gap-x-2 rounded-md bg-slate-500 px-3 text-white"
+              >
+                <span>All Languages</span>
+                <IconDown class="h-4 w-4" />
+              </div>
+            </template>
+            <template #items>
+              <ListItem>C++</ListItem>
+              <ListItem>Python</ListItem>
+            </template>
+          </Dropdown>
+          <Dropdown color="white">
+            <template #button>
+              <div
+                class="mr-3 flex h-9 w-fit flex-row items-center gap-x-2 rounded-md bg-slate-500 px-3 text-white"
+              >
+                <span>All Result</span>
+                <IconDown class="h-4 w-4" />
+              </div>
+            </template>
+            <template #items>
+              <ListItem>Accepted</ListItem>
+              <ListItem>Wrong Answer</ListItem>
+            </template>
+          </Dropdown>
+        </div>
+      </template>
+    </PaginationTable>
+  </div>
 </template>
