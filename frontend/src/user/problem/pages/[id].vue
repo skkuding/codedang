2c140b95fd12191024d5caecc441882e62e41ee1
jimin9038 <jimin9038@g.skku.edu>
2023-01-17 07:18:49 +0000

    feat(fe): add fsubmission list to problem detail

--- frontend/src/user/problem/pages/[id].vue
+++ frontend/src/user/problem/pages/[id].vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import SubmissionList from '../components/SubmissionList.vue'
+
 defineProps<{
   id: number
 }>()
@@ -6,4 +8,10 @@ defineProps<{
 
 <template>
   <div>This is problem detail page : id = {{ id }}</div>
+  <SubmissionList></SubmissionList>
 </template>
+
+<route lang="yaml">
+meta:
+  layout: empty
+</route>
