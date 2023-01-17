7f3f68053f87c43472b283b0f6180f72639c82ea
jimin9038 <jimin9038@g.skku.edu>
2023-01-17 08:28:58 +0000

    feat(fe): add submissiondetail

---
+++ frontend/src/user/problem/components/SubmissionDetail.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import CodeEditor from '@/common/components/Organism/CodeEditor.vue'
+
+defineProps<{
+  lang: 'cpp' | 'python' | 'javascript' | 'java'
+}>()
+const code = ref('ee')
+</script>
+
+<template>
+  <div>
+    <CodeEditor v-model="code" :lang="lang"></CodeEditor>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: empty
+</route>
