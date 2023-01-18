824cab31cb0bdb6eacb0aa7cc573bc0a7f349835
Se Hwan <realsehwan@gmail.com>
2023-01-18 08:10:53 +0000

    chore: add IsNotEmpty class validator
    UpdateWorkbookDto에 IsNotEmpty를 추가합니다.

--- backend/src/workbook/dto/update-workbook.dto.ts
+++ backend/src/workbook/dto/update-workbook.dto.ts
@@ -1,24 +1,30 @@
 import { Type } from 'class-transformer'
-import { IsString, IsDate, IsBoolean } from 'class-validator'
+import { IsString, IsDate, IsBoolean, IsNotEmpty } from 'class-validator'
 
 export class UpdateWorkbookDto {
   @IsString()
+  @IsNotEmpty()
   title: string
 
   @IsString()
+  @IsNotEmpty()
   description: string
 
   @IsDate()
+  @IsNotEmpty()
   @Type(() => Date)
   startTime: Date
 
   @IsDate()
+  @IsNotEmpty()
   @Type(() => Date)
   endTime: Date
 
   @IsBoolean()
+  @IsNotEmpty()
   visible: boolean
 
   @IsBoolean()
+  @IsNotEmpty()
   allowPartialScore: boolean
 }
