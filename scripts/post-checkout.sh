444c1a85ca6f30af268989bc4eadd8a98e7231b7
Jaemin Choi <1dotolee@gmail.com>
2023-01-19 18:11:51 +0000

    fix: compare migration and database

--- scripts/post-checkout.sh
+++ scripts/post-checkout.sh
@@ -8,10 +8,14 @@ pnpm install
 # delete empty migration folders git has left
 find $BASEDIR/backend/prisma/migrations -empty -type d -delete
 
-if pnpm --filter backend exec prisma migrate diff \
-  --from-schema-datasource prisma/schema.prisma \
-  --to-schema-datamodel prisma/schema.prisma \
+# load database url
+source $BASEDIR/backend/.env
+
+if ! pnpm --filter backend exec prisma migrate diff \
+  --from-url $DATABASE_URL \
+  --to-migrations prisma/migrations \
+  --shadow-database-url $DATABASE_URL \
   --exit-code
 then
-  pnpm --filter backend exec prisma migrate reset
+  pnpm --filter backend exec prisma migrate reset -f
 fi
