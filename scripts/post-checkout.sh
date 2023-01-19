74087bad7f608a98a416bf4c62fe39070b7f6713
Jaemin Choi <1dotolee@gmail.com>
2023-01-19 18:24:40 +0000

    fix: hide error from prisma migrate diff

--- scripts/post-checkout.sh
+++ scripts/post-checkout.sh
@@ -11,7 +11,7 @@ find $BASEDIR/backend/prisma/migrations -empty -type d -delete
 # load database url
 source $BASEDIR/backend/.env
 
-if ! pnpm --filter backend exec prisma migrate diff \
+if ! pnpm --filter backend --silent exec prisma migrate diff \
   --from-url $DATABASE_URL \
   --to-migrations prisma/migrations \
   --shadow-database-url $DATABASE_URL \
