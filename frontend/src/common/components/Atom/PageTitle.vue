5eb943f5c85c782d670a1dda02c14992baacc1da
jimin9038 <jimin9038@g.skku.edu>
2023-01-23 16:33:40 +0000

    feat(fe): add white color on pageTitle

--- frontend/src/common/components/Atom/PageTitle.vue
+++ frontend/src/common/components/Atom/PageTitle.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   text: string
+  color?: string
 }>()
+const className = props.color == 'white' ? 'text-white' : 'text-text-title'
 </script>
 
 <template>
-  <div class="text-text-title text-3xl font-bold">{{ text }}</div>
+  <div :class="className" class="text-3xl font-bold">{{ text }}</div>
 </template>
