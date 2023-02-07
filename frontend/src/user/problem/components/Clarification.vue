2b528235f75eb145a26e7291c9a4b8a44f8564e8
Jaemin Choi <1dotolee@gmail.com>
2023-02-07 07:53:49 +0000

    feat: close clarification when outside clicked

--- frontend/src/user/problem/components/Clarification.vue
+++ frontend/src/user/problem/components/Clarification.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import IconSoundFilled from '~icons/ant-design/sound-filled'
-import { useDraggable } from '@vueuse/core'
+import { useDraggable, useWindowSize } from '@vueuse/core'
+import { OnClickOutside } from '@vueuse/components'
 import PageSubtitle from '@/common/components/Atom/PageSubtitle.vue'
 import Button from '@/common/components/Atom/Button.vue'
-import { useWindowSize } from '@vueuse/core'
-import { watch } from 'vue'
 
 const el = ref<HTMLElement | null>(null)
 
@@ -61,52 +60,54 @@ const noticeList = [
 </script>
 
 <template>
-  <button
-    ref="el"
-    :style="style"
-    class="item bg-gray-dark fixed z-10 h-[54px] w-[54px] rounded-full shadow-lg"
-    @pointerdown="
-      (e) => {
-        from.x = e.pageX
-        from.y = e.pageY
-      }
-    "
-    @pointerup="
-      (e) => {
-        if (from.x === e.pageX && from.y === e.pageY) {
-          visible = !visible
+  <OnClickOutside @trigger="visible = false">
+    <button
+      ref="el"
+      :style="style"
+      class="item bg-gray-dark fixed z-10 h-[54px] w-[54px] rounded-full shadow-lg"
+      @pointerdown="
+        (e) => {
+          from.x = e.pageX
+          from.y = e.pageY
         }
-      }
-    "
-  >
-    <div
-      class="bg-red absolute top-0.5 right-0.5 h-[15px] w-[15px] rounded-full text-xs font-bold text-white"
+      "
+      @pointerup="
+        (e) => {
+          if (from.x === e.pageX && from.y === e.pageY) {
+            visible = !visible
+          }
+        }
+      "
     >
-      1
-    </div>
-    <div class="flex justify-center">
-      <IconSoundFilled class="hover:text-red text-xl text-white" />
-    </div>
-  </button>
-  <div
-    v-if="visible"
-    class="border-gray fixed z-50 m-auto h-96 w-1/2 overflow-auto rounded-md border border-solid bg-white py-2 px-6"
-  >
-    <div class="mb-3">
-      <PageSubtitle text="Notice" class="ml-12" />
-    </div>
+      <div
+        class="bg-red absolute top-0.5 right-0.5 h-[15px] w-[15px] rounded-full text-xs font-bold text-white"
+      >
+        1
+      </div>
+      <div class="flex justify-center">
+        <IconSoundFilled class="hover:text-red text-xl text-white" />
+      </div>
+    </button>
     <div
-      v-for="(item, index) in noticeList.slice().reverse()"
-      :key="index"
-      class="mb-4 flex gap-6 text-sm"
+      v-if="visible"
+      class="border-gray fixed z-50 m-auto h-96 w-1/2 overflow-auto rounded-md border border-solid bg-white py-2 px-6"
     >
-      <IconSoundFilled v-if="index == 0" class="text-red text-xl" />
-      <IconSoundFilled v-else class="text-xl text-black" />
-      <div class="mr-auto w-72">{{ item.title }}</div>
-      <div class="m-auto hidden md:inline-block">{{ date }}</div>
+      <div class="mb-3">
+        <PageSubtitle text="Notice" class="ml-12" />
+      </div>
+      <div
+        v-for="(item, index) in noticeList.slice().reverse()"
+        :key="index"
+        class="mb-4 flex gap-6 text-sm"
+      >
+        <IconSoundFilled v-if="index == 0" class="text-red text-xl" />
+        <IconSoundFilled v-else class="text-xl text-black" />
+        <div class="mr-auto w-72">{{ item.title }}</div>
+        <div class="m-auto hidden md:inline-block">{{ date }}</div>
+      </div>
+      <Button class="w-full" outline color="gray-dark" @click="visible = false">
+        Close
+      </Button>
     </div>
-    <Button class="w-full" outline color="gray-dark" @click="visible = false">
-      Close
-    </Button>
-  </div>
+  </OnClickOutside>
 </template>
