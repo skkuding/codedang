27551d0866be45c20887e1822b1b06071245197e
Kang Eunbi <jenabill@naver.com>
2023-02-16 13:28:30 +0000

    refactor(fe): update profile component style

--- frontend/src/user/group/components/Profile.vue
+++ frontend/src/user/group/components/Profile.vue
@@ -10,18 +10,18 @@ defineProps<{
 </script>
 
 <template>
-  <div class="flex items-end">
+  <div class="flex items-end gap-4">
     <img
       class="h-20 w-20 rounded-lg object-contain shadow-[0_4px_4px_0px_rgba(0,0,0,0.25)]"
       src="@/common/assets/logo.png"
       alt="Group img"
     />
     <!-- <img :src="`data:image/png;base64,${groupImage}`" /> API 호출 시 이미지를 base-64로 가져와서 추후 해당 코드로 적용 예정. 현재 임의로 로고 집어넣음-->
-    <div class="pl-4">
-      <div class="flex items-end">
+    <div class="flex flex-col gap-1">
+      <div class="flex items-baseline gap-2">
         <PageTitle :text="groupName" />
-        <Button color="gray-dark" class="ml-2 px-1">
-          <IconGear class="h-6 w-6" />
+        <Button color="gray-dark" class="px-1">
+          <IconGear class="h-3 w-3" />
         </Button>
       </div>
       <div class="text-black">{{ description }}</div>
