ed2d460a2f71cbd28e7f379b208b7a2d1cdfbb1e
jspark2000 <idpjs2000@gmail.com>
2023-02-17 12:15:56 +0000

    fix(be): delete duplicate prisma migrations

--- backend/prisma/migrations/20230203103637_delete_is_registered/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `is_registered` on the `user_group` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "user_group" DROP COLUMN "is_registered";
