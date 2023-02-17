7be07c8644c9d047a6b1a660e8413b66b01e741b
Kang Eunbi <jenabill@naver.com>
2023-02-17 12:16:15 +0000

    refactor(fe): change group list section props and style

--- frontend/src/user/group/pages/index.vue
+++ frontend/src/user/group/pages/index.vue
@@ -11,117 +11,15 @@ import { ref } from 'vue'
 const noInvitationCode = ref(false)
 const joinModalVisible = ref(false)
 const invitationCode = ref('')
-
-type Group = {
-  id: number
-  createdBy: number
-  groupName: string
-  groupAdmin: string
-  groupManager: string
-  private: boolean
-  invitationCode: string
-  description: string
-  member: number
-  createdUser: string
-}
-
-//getMyGroups
-const myGroupList: Group[] = [
-  {
-    id: 1,
-    createdBy: 1,
-    groupName: 'SKKUDING',
-    groupAdmin: 'Prof. Kim',
-    groupManager: '홍길동',
-    private: false,
-    invitationCode: 'abcde',
-    description: '성균관대학교 개발동아리입니다',
-    member: 23,
-    createdUser: '구성현'
-  },
-  {
-    id: 2,
-    createdBy: 1,
-    groupName: 'TSS',
-    groupAdmin: 'Prof. Kim',
-    groupManager: '홍길동',
-    private: false,
-    invitationCode: 'cdefg',
-    description: '성균관대학교 개발동아리입니다',
-    member: 10,
-    createdUser: '구성현'
-  }
-]
-
-//getNonPrivateGroups
-const groupList: Group[] = [
-  {
-    id: 1,
-    createdBy: 1,
-    groupName: 'SKKUDING',
-    groupAdmin: 'Prof. Kim',
-    groupManager: '홍길동',
-    private: false,
-    invitationCode: 'abcde',
-    description: '성균관대학교 개발동아리입니다',
-    member: 10,
-    createdUser: '구성현'
-  },
-  {
-    id: 2,
-    createdBy: 1,
-    groupName: 'TSS',
-    groupAdmin: 'Prof. Kim',
-    groupManager: '홍길동',
-    private: false,
-    invitationCode: 'cdefg',
-    description: '성균관대학교 개발동아리입니다',
-    member: 10,
-    createdUser: '구성현'
-  },
-  {
-    id: 3,
-    createdBy: 1,
-    groupName: 'TSS',
-    groupAdmin: 'Prof. Kim',
-    groupManager: '홍길동',
-    private: false,
-    invitationCode: 'cdefg',
-    description: '성균관대학교 개발동아리입니다',
-    member: 10,
-    createdUser: '구성현'
-  },
-  {
-    id: 4,
-    createdBy: 1,
-    groupName: 'TSS',
-    groupAdmin: 'Prof. Kim',
-    groupManager: '홍길동',
-    private: false,
-    invitationCode: 'cdefg',
-    description: '성균관대학교 개발동아리입니다',
-    member: 10,
-    createdUser: '구성현'
-  }
-]
 </script>
 
 <template>
-  <div class="mt-10 flex flex-col">
+  <div class="mt-10 flex flex-col gap-4">
     <Button class="self-end" @click="joinModalVisible = true">
       Join Group
     </Button>
-    <GroupListSection
-      :group-list="myGroupList"
-      title="My Group"
-      :my-group="true"
-    />
-    <GroupListSection
-      :group-list="groupList"
-      title="All Group"
-      pagination
-      :my-group="false"
-    />
+    <GroupListSection title="My Group" :my-group="true" />
+    <GroupListSection title="All Group" pagination :my-group="false" />
     <Modal v-model="joinModalVisible" class="px-4 shadow-md">
       <div class="flex flex-col items-center gap-2 py-12 px-4">
         <h1 class="mb-2 text-center text-lg font-bold">
@@ -131,10 +29,12 @@ const groupList: Group[] = [
           <InputItem v-model="invitationCode" placeholder="Invitation Code" />
           <Button class="py-2"><IconSendFilled /></Button>
         </div>
-        <p v-if="noInvitationCode" class="text-red">
+        <p v-if="noInvitationCode" class="text-red text-center">
           Group not found! Please check your code again
         </p>
-        <p>If you have invitation code, enter the code here.</p>
+        <p class="text-center">
+          If you have invitation code, enter the code here.
+        </p>
       </div>
     </Modal>
   </div>
