1865feb2a95416b791bd91f0c95819422257b3fc
jspark2000 <idpjs2000@gmail.com>
2023-02-22 06:01:55 +0000

    feat(be): add test code for method join-group-by-id

--- backend/src/group/group.service.spec.ts
+++ backend/src/group/group.service.spec.ts
@@ -1,23 +1,41 @@
 import { CACHE_MANAGER } from '@nestjs/common'
 import { Test, type TestingModule } from '@nestjs/testing'
 import { expect } from 'chai'
-import { fake } from 'sinon'
+import { groups, userGroups } from './mock/group.mock'
+import { stub } from 'sinon'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { GroupService } from './group.service'
+import { Cache } from 'cache-manager'
+import { UserGroup } from '@prisma/client'
 
-const mockPrismaService = {
+const db = {
+  user: {
+    findMany: stub(),
+    findFirst: stub(),
+    findUnique: stub()
+  },
+  group: {
+    findMany: stub(),
+    findFirst: stub(),
+    findUnique: stub()
+  },
   userGroup: {
-    findFirst: fake()
+    create: stub(),
+    delete: stub(),
+    findFirst: stub(),
+    findMany: stub(),
+    findUnique: stub()
   }
 }
 
 describe('GroupService', () => {
   let service: GroupService
+  let cache: Cache
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         GroupService,
-        { provide: PrismaService, useValue: mockPrismaService },
+        { provide: PrismaService, useValue: db },
         {
           provide: CACHE_MANAGER,
           useFactory: () => ({
@@ -28,9 +46,67 @@ describe('GroupService', () => {
       ]
     }).compile()
     service = module.get<GroupService>(GroupService)
+    cache = module.get<Cache>(CACHE_MANAGER)
   })
 
   it('should be defined', () => {
     expect(service).to.be.ok
+  })
+
+  describe('joinGroupById', () => {
+    it('should return {isJoined: true} when group not set as requireApprovalBeforeJoin', async () => {
+      //given
+      const userId = 3
+      const groupId = 1
+      const fakeUserGroup: UserGroup = {
+        userId,
+        groupId,
+        isGroupLeader: false,
+        createTime: new Date('2023-02-22T00:00:00.000Z'),
+        updateTime: new Date('2023-02-22T0:00:00.000Z')
+      }
+      db.group.findFirst.resolves({
+        config: groups[0].config,
+        userGroup: userGroups.filter(
+          (userGroup) => userGroup.groupId === groupId
+        )
+      })
+      stub(service, 'createUserGroup').resolves(fakeUserGroup)
+
+      //when
+      const result = await service.joinGroupById(userId, groupId)
+
+      //then
+      expect(result).to.deep.equal({
+        userGroupData: fakeUserGroup,
+        isJoined: true
+      })
+    })
+
+    it('should return {isJoined: false} when group set as requireApprovalBeforeJoin', async () => {
+      //given
+      const userId = 3
+      const groupId = 2
+      db.group.findFirst.resolves({
+        config: groups[1].config,
+        userGroup: userGroups.filter(
+          (userGroup) => userGroup.groupId === groupId
+        )
+      })
+      const cacheSpy = stub(cache, 'get').resolves(undefined)
+
+      //when
+      const result = await service.joinGroupById(userId, groupId)
+
+      //then
+      expect(cacheSpy.calledOnce).to.be.true
+      expect(result).to.deep.equal({
+        userGroupData: {
+          userId,
+          groupId
+        },
+        isJoined: false
+      })
+    })
   })
 })
---
+++ backend/src/group/mock/group.mock.ts
@@ -0,0 +1,98 @@
+import { Group, Role, User, UserGroup } from '@prisma/client'
+
+export const groups: Group[] = [
+  {
+    id: 1,
+    config: {
+      showOnList: true,
+      allowJoinFromSearch: true,
+      allowJoinWithURL: false,
+      requireApprovalBeforeJoin: false
+    },
+    description: 'mock public group with no approval',
+    createdById: 1,
+    groupName: 'mock public group',
+    isDeleted: false,
+    createTime: new Date('2023-02-22T00:00:00.000Z'),
+    updateTime: new Date('2023-02-22T:00:00.000Z')
+  },
+  {
+    id: 2,
+    config: {
+      showOnList: true,
+      allowJoinFromSearch: true,
+      allowJoinWithURL: false,
+      requireApprovalBeforeJoin: true
+    },
+    description: 'mock public group with approval',
+    createdById: 1,
+    groupName: 'mock public group 2',
+    isDeleted: false,
+    createTime: new Date('2023-02-22T00:00:00.000Z'),
+    updateTime: new Date('2023-02-22T10:00:00.000Z')
+  }
+]
+
+export const users: User[] = [
+  {
+    id: 1,
+    role: Role.Manager,
+    password: '1234',
+    username: 'manager',
+    createTime: new Date('2023-02-22T00:00:00.000Z'),
+    updateTime: new Date('2023-02-22T10:00:00.000Z'),
+    email: 'manager@skkuding.dev',
+    lastLogin: new Date('2023-02-22T10:00:00.000Z')
+  },
+  {
+    id: 2,
+    role: Role.User,
+    password: '1234',
+    username: 'user01',
+    createTime: new Date('2023-02-22T00:00:00.000Z'),
+    updateTime: new Date('2023-02-22T10:00:00.000Z'),
+    email: 'user01@skkuding.dev',
+    lastLogin: new Date('2023-02-22T10:00:00.000Z')
+  },
+  {
+    id: 3,
+    role: Role.User,
+    password: '1234',
+    username: 'user02',
+    createTime: new Date('2023-02-22T00:00:00.000Z'),
+    updateTime: new Date('2023-02-22T10:00:00.000Z'),
+    email: 'user02@skkuding.dev',
+    lastLogin: new Date('2023-02-22T10:00:00.000Z')
+  }
+]
+
+export const userGroups: UserGroup[] = [
+  {
+    groupId: 1,
+    userId: 1,
+    createTime: new Date('2023-02-22T00:00:00.000Z'),
+    updateTime: new Date('2023-02-22T10:00:00.000Z'),
+    isGroupLeader: true
+  },
+  {
+    groupId: 1,
+    userId: 2,
+    createTime: new Date('2023-02-22T00:00:00.000Z'),
+    updateTime: new Date('2023-02-22T10:00:00.000Z'),
+    isGroupLeader: false
+  },
+  {
+    groupId: 2,
+    userId: 1,
+    createTime: new Date('2023-02-22T00:00:00.000Z'),
+    updateTime: new Date('2023-02-22T10:00:00.000Z'),
+    isGroupLeader: true
+  },
+  {
+    groupId: 2,
+    userId: 2,
+    createTime: new Date('2023-02-22T00:00:00.000Z'),
+    updateTime: new Date('2023-02-22T10:00:00.000Z'),
+    isGroupLeader: true
+  }
+]
