5e8fdfb59a7405470fdb0ba0a8820bafe98d8068
Eunbi Kang <jenabill@naver.com>
2023-02-28 11:04:19 +0000

    feat(fe): connect group contest list get api

--- frontend/src/user/group/components/Contest.vue
+++ frontend/src/user/group/components/Contest.vue
@@ -1,83 +1,99 @@
 <script setup lang="ts">
+import PageSubtitle from '@/common/components/Atom/PageSubtitle.vue'
 import RadioButton from '@/common/components/Molecule/RadioButton.vue'
 import SearchBar from '@/common/components/Molecule/SearchBar.vue'
 import CardItem from '@/common/components/Molecule/CardItem.vue'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+import { useAuthStore } from '@/common/store/auth'
+import { useNow, useTimeAgo } from '@vueuse/core'
 
-defineProps<{
+const props = defineProps<{
   id: number
 }>()
 
 const filters = ['Latest', 'Ongoing']
 const selectedFilter = ref(filters[0])
 
-// dummy data
-const data = [
-  {
-    id: 1,
-    img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-    title: 'SKKU 프로그래밍 대회 2023',
-    description: 'description',
-    startedBefore: '00:07:05',
-    participation: true
-  },
-  {
-    id: 2,
-    img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-    title: 'SKKU 프로그래밍 대회 2023',
-    description: 'description',
-    startedBefore: '00:07:05',
-    participation: false
-  },
-  {
-    id: 3,
-    img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-    title: 'SKKU 프로그래밍 대회 2023',
-    description: 'description',
-    startedBefore: '00:07:05',
-    participation: false
-  },
-  {
-    id: 4,
-    img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-    title: 'SKKU 프로그래밍 대회 2023',
-    description: 'description',
-    startedBefore: '00:07:05',
-    participation: true
-  },
-  {
-    id: 5,
-    img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-    title: 'SKKU 프로그래밍 대회 2023',
-    description: 'description',
-    startedBefore: '00:07:05',
-    participation: false
-  },
-  {
-    id: 6,
-    img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-    title: 'SKKU 프로그래밍 대회 2023',
-    description: 'description',
-    startedBefore: '00:07:05',
-    participation: false
-  },
-  {
-    id: 7,
-    img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-    title: 'SKKU 프로그래밍 대회 2023',
-    description: 'description',
-    startedBefore: '00:07:05',
-    participation: false
-  },
-  {
-    id: 8,
-    img: 'https://www.skku.edu/_res/skku/img/skku_s.png',
-    title: 'SKKU 프로그래밍 대회 2023',
-    description: 'description',
-    startedBefore: '00:07:05',
-    participation: false
+// server data
+type ContestList = {
+  id: number
+  title: string
+  description: string
+  startTime: string
+  endTime: string
+  group: {
+    id: number
+    groupName: string
   }
-]
+}[]
+type Response = {
+  registeredOngoing: ContestList
+  registeredUpcoming: ContestList
+  ongoing: ContestList
+  upcoming: ContestList
+  finished: ContestList
+}
+type State = Pick<Response, 'finished' | 'ongoing' | 'upcoming'> & {
+  registered: ContestList
+}
+const contentList = ref<State | null>(null)
+const store = useAuthStore()
+onMounted(async () => {
+  // 임시 코드
+  if (!store.isLoggedIn) {
+    await store.login('user10', 'Useruser')
+  }
+
+  const { data } = await axios.get<Response>(`/api/group/${props.id}/contest`)
+  contentList.value = {
+    registered: data.registeredOngoing.concat(...data.registeredUpcoming),
+    upcoming: data.upcoming,
+    ongoing: data.ongoing,
+    finished: data.finished
+  }
+})
+const padStart = (
+  targetLength: number,
+  padString: string,
+  str: string
+): string => {
+  return str.length >= targetLength
+    ? str
+    : new Array(targetLength - str.length + 1).join(padString) + str
+}
+const now = useNow()
+const DAY = 1000 * 60 * 60 * 24
+const HOUR = 1000 * 60 * 60
+const MINUTE = 1000 * 60
+const getTimeInfo = (key: keyof State, date: Date) => {
+  const diff = now.value.getTime() - date.getTime()
+  if (key === 'finished') {
+    return `Finished ${useTimeAgo(date).value} ago`
+  } else if (diff < -DAY) {
+    return `Start After ${useTimeAgo(date).value}`
+  } else if (diff <= 0) {
+    const hour = padStart(2, '0', String(Math.floor(-diff / HOUR)))
+    const min = padStart(2, '0', String(Math.floor((-diff % HOUR) / MINUTE)))
+    const sec = padStart(
+      2,
+      '0',
+      String(Math.floor(((-diff % HOUR) % MINUTE) / 1000))
+    )
+    return `Start After ${hour}:${min}:${sec}`
+  } else if (diff < DAY) {
+    const hour = padStart(2, '0', String(Math.floor(diff / HOUR)))
+    const min = padStart(2, '0', String(Math.floor((diff % HOUR) / MINUTE)))
+    const sec = padStart(
+      2,
+      '0',
+      String(Math.floor(((diff % HOUR) % MINUTE) / 1000))
+    )
+    return `Started Before ${hour}:${min}:${sec}`
+  } else {
+    return `Started Before ${useTimeAgo(date).value}`
+  }
+}
 </script>
 
 <template>
@@ -85,27 +101,39 @@ const data = [
     class="mb-8 flex flex-col items-end justify-end gap-y-2 gap-x-4 lg:flex-row"
   >
     <RadioButton v-model="selectedFilter" :texts="filters" />
-    <SearchBar placeholder="keywords" />
+    <SearchBar placeholder="원하는 대회를 검색해보세요!" />
   </div>
-  <div class="flex flex-col justify-center gap-4">
-    <CardItem
-      v-for="{
-        id: contestId,
-        img,
-        title,
-        description,
-        participation,
-        startedBefore
-      } in data"
-      :key="contestId"
-      :img="img"
-      :title="title"
-      :description="description"
-      :additional-text="participation ? '#참여' : ''"
-      :colored-text="`Started Before ${startedBefore}`"
-      :border-color="participation ? 'green' : 'gray'"
-      class="w-full bg-white drop-shadow-md"
-      @click="$router.push(`/contest/${contestId}`)"
-    />
+  <div v-if="contentList !== null" class="flex flex-col gap-16">
+    <section
+      v-for="key in ['registered', 'ongoing', 'upcoming', 'finished'] as const"
+      :key="key"
+      class="flex flex-col justify-center gap-4"
+    >
+      <PageSubtitle :text="`${key} Contest`" class="capitalize" />
+      <p
+        v-if="contentList[key].length === 0"
+        class="text-gray-dark font-semibold"
+      >
+        No Contest
+      </p>
+      <CardItem
+        v-for="{
+          id: contestId,
+          title,
+          startTime,
+          endTime,
+          group
+        } in contentList[key]"
+        :key="contestId"
+        :title="title"
+        :description="`For ${group.groupName}`"
+        :colored-text="
+          getTimeInfo(key, new Date(key === 'finished' ? endTime : startTime))
+        "
+        :border-color="key === 'registered' ? 'green' : 'gray'"
+        class="w-full bg-white capitalize drop-shadow-md"
+        @click="$router.push(`/contest/${contestId}`)"
+      />
+    </section>
   </div>
 </template>
