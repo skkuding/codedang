06aabddc94baa5994321cd43fb8a1a859c7e59d4
jspark2000 <idpjs2000@gmail.com>
2023-05-02 11:53:20 +0000

    fix(be): delete unused code

--- backend/apps/admin/src/auth/guard/jwt-auth.guard.ts
+++ backend/apps/admin/src/auth/guard/jwt-auth.guard.ts
@@ -3,7 +3,6 @@ import { Reflector } from '@nestjs/core'
 import { GqlExecutionContext } from '@nestjs/graphql'
 import { AuthGuard } from '@nestjs/passport'
 import { type Observable } from 'rxjs'
-import { IS_AUTH_NOT_NEEDED_KEY } from '@client/common/decorator/auth-ignore.decorator'
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
@@ -14,13 +13,6 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(
     context: ExecutionContext
   ): boolean | Promise<boolean> | Observable<boolean> {
-    const isAuthNotNeeded = this.reflector.getAllAndOverride<boolean>(
-      IS_AUTH_NOT_NEEDED_KEY,
-      [context.getHandler(), context.getClass()]
-    )
-    if (isAuthNotNeeded) {
-      return true
-    }
     return super.canActivate(context)
   }
 
