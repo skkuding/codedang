1c801c648a8bed5a91d68f5ab948d57ca4f53e81
Sori Lim <aint.imsorry@gmail.com>
2023-05-09 15:25:57 +0000

    fix(be): fix seed file accordingly

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -6434,7 +6434,7 @@ const createSubmissions = async () => {
   submissions.push(
     await prisma.submission.create({
       data: {
-        hash: generateHash(),
+        id: generateHash(),
         userId: users[0].id,
         problemId: problems[0].id,
         contestId: contest.id,
@@ -6458,7 +6458,7 @@ int main(void) {
   submissions.push(
     await prisma.submission.create({
       data: {
-        hash: generateHash(),
+        id: generateHash(),
         userId: users[1].id,
         problemId: problems[1].id,
         contestId: contest.id,
@@ -6482,7 +6482,7 @@ int main(void) {
   submissions.push(
     await prisma.submission.create({
       data: {
-        hash: generateHash(),
+        id: generateHash(),
         userId: users[2].id,
         problemId: problems[2].id,
         contestId: contest.id,
@@ -6506,7 +6506,7 @@ int main(void) {
   submissions.push(
     await prisma.submission.create({
       data: {
-        hash: generateHash(),
+        id: generateHash(),
         userId: users[3].id,
         problemId: problems[3].id,
         contestId: contest.id,
@@ -6526,7 +6526,7 @@ int main(void) {
   submissions.push(
     await prisma.submission.create({
       data: {
-        hash: generateHash(),
+        id: generateHash(),
         userId: users[4].id,
         problemId: problems[4].id,
         contestId: contest.id,
@@ -6550,7 +6550,7 @@ int main(void) {
   submissions.push(
     await prisma.submission.create({
       data: {
-        hash: generateHash(),
+        id: generateHash(),
         userId: users[5].id,
         problemId: problems[5].id,
         workbookId: workbooks[0].id,
@@ -6574,7 +6574,7 @@ int main(void) {
   submissions.push(
     await prisma.submission.create({
       data: {
-        hash: generateHash(),
+        id: generateHash(),
         userId: users[6].id,
         problemId: problems[6].id,
         workbookId: workbooks[0].id,
