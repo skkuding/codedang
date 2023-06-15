6beccaf4f1c4b1094961d9aa89b4f2a5be4e3284
Hyeongrok1 <jhrhello0323@gmail.com>
2023-06-15 11:51:06 +0000

    feat(be): delete admin user interface folder

--- backend/apps/admin/src/user/interface/create-userprofile.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface CreateUserProfileData {
-  userId: number
-  realName: string
-}
--- backend/apps/admin/src/user/interface/jwt.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface EmailAuthJwtPayload {
-  email: string
-}
-
-export interface EmailAuthJwtObject extends EmailAuthJwtPayload {
-  iat: number
-  exp: number
-  iss: string
-}
