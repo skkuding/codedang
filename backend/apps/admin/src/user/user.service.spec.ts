80c94b65ea5ccf065f8b4698ea5f2959529ce771
Hyeongrok1 <jhrhello0323@gmail.com>
2023-06-17 06:24:16 +0000

    feat(be): add prisma to userService test

--- backend/apps/admin/src/user/user.service.spec.ts
+++ backend/apps/admin/src/user/user.service.spec.ts
@@ -3,6 +3,43 @@ import { expect } from 'chai'
 import { UserService } from './user.service'
 import { PrismaModule, PrismaService } from '@libs/prisma'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { stub } from 'sinon'
+
+const ID = 1
+const EMAIL_ADDRESS = 'email@email.com'
+const user = {
+  id: ID,
+  username: 'user',
+  password: 'thisIsPassword',
+  role: 'User',
+  email: EMAIL_ADDRESS,
+  lastLogin: undefined,
+  createTime: undefined,
+  updateTime: undefined
+}
+const profile = {
+  id: ID,
+  userId: ID,
+  realName: 'real name',
+  createTime: undefined,
+  updateTime: undefined
+}
+const db = {
+  user: {
+    create: stub().resolves(user),
+    findUnique: stub(),
+    update: stub().resolves(user),
+    delete: stub()
+  },
+  userProfile: {
+    create: stub(),
+    findUnique: stub(),
+    update: stub().resolves({ ...profile, realName: 'new name' })
+  },
+  userGroup: {
+    create: stub()
+  }
+}
 
 describe('UserService', () => {
   let service: UserService
@@ -13,7 +50,7 @@ describe('UserService', () => {
       providers: [
         UserService,
         ConfigService,
-        { provide: PrismaService, useValue: {} }
+        { provide: PrismaService, useValue: db }
       ]
     }).compile()
 
