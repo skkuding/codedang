be69595bb027fa8bc507b2744813259c9be17a93
Hyeongrok1 <jhrhello0323@gmail.com>
2023-06-17 05:36:38 +0000

    feat(be): add prisma dependency to userService test

--- backend/apps/admin/src/user/user.service.spec.ts
+++ backend/apps/admin/src/user/user.service.spec.ts
@@ -1,13 +1,14 @@
 import { Test, type TestingModule } from '@nestjs/testing'
 import { expect } from 'chai'
 import { UserService } from './user.service'
+import { PrismaService } from '@libs/prisma'
 
 describe('UserService', () => {
   let service: UserService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService]
+      providers: [UserService, PrismaService]
     }).compile()
 
     service = module.get<UserService>(UserService)
