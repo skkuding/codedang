5ccd4a33619523f73e1fb7edf366fb226ffbe7f4
Kohminchae <mc0jooo@gmail.com>
2023-06-22 10:29:15 +0000

    fix(fe): fix router-view key

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 import Toast from '@/common/components/Atom/Toast.vue'
 import { useAuthStore } from './common/store/auth'
-import { useRouter } from 'vue-router'
 
 // check if refresh token is valid
 useAuthStore().reissue()
-const router = useRouter()
 </script>
 
 <template>
-  <router-view :key="router.currentRoute.value.fullPath" />
+  <router-view :key="$route.fullPath" />
   <Toast />
 </template>
