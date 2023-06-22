eb1c5981305f51632e00d0151a0c94c0f2001499
Eunbi Kang <jenabill@naver.com>
2023-06-22 07:00:59 +0000

    feat(fe): add custom naive ui theme

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import Toast from '@/common/components/Atom/Toast.vue'
 import { useAuthStore } from './common/store/auth'
+import { NConfigProvider, type GlobalThemeOverrides } from 'naive-ui'
+
+const themeOverrides: GlobalThemeOverrides = {
+  DataTable: {
+    thTextColor: '#7C7A7B',
+    thFontWeight: 'bold',
+    tdTextColor: '#212529'
+  }
+}
 
 // check if refresh token is valid
 useAuthStore().reissue()
 </script>
 
 <template>
-  <router-view />
-  <Toast />
+  <n-config-provider :theme-overrides="themeOverrides">
+    <router-view />
+    <Toast />
+  </n-config-provider>
 </template>
