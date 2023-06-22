72948cfedc5f1abd08470a8cf8354d7cd50413a2
Eunbi Kang <jenabill@naver.com>
2023-06-22 07:01:53 +0000

    feat(fe): add admin contest page

--- frontend/src/admin/pages/contest.vue
+++ frontend/src/admin/pages/contest.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
-import { h } from 'vue'
-import { NButton, NSwitch, NIcon } from 'naive-ui'
+import { h, ref } from 'vue'
+import { NButton, NSwitch, NIcon, NDataTable } from 'naive-ui'
 import FaRegularTrashAlt from '~icons/fa-regular/trash-alt'
+import PageTitle from '@/common/components/Atom/PageTitle.vue'
+import SearchBar from '@/common/components/Molecule/SearchBar.vue'
 
 interface Contest {
   id: number
@@ -17,26 +19,21 @@ const deleteContest = (row: Contest) => {
 }
 
 const toggleVisible = (row: Contest) => {
-  console.log('toggle visible', row)
+  row.visible = !row.visible
 }
 
 const columns = [
   {
     title: 'ID',
-    key: 'id',
-    width: 50
+    key: 'id'
   },
   {
     title: 'Group',
-    key: 'group',
-    resizable: true
+    key: 'group'
   },
   {
     title: 'Type',
-    key: 'type',
-    resizable: true,
-    minWidth: 50,
-    maxWidth: 100
+    key: 'type'
   },
   {
     title: 'Title',
@@ -44,15 +41,18 @@ const columns = [
   },
   {
     title: 'Period',
-    key: 'period'
+    key: 'period',
+    width: 250
   },
   {
     title: 'Visible',
     key: 'visible',
+    align: 'center' as const,
     render(row: Contest) {
       return h(NSwitch, {
         strong: true,
         tertiary: true,
+        value: row.visible,
         size: 'small',
         onClick: () => toggleVisible(row)
       })
@@ -61,11 +61,12 @@ const columns = [
   {
     title: 'Delete',
     key: 'delete',
+    align: 'center' as const,
     render(row: Contest) {
       return h(NButton, {
         strong: true,
-        tertiary: true,
         size: 'small',
+        bordered: true,
         onClick: () => deleteContest(row),
         renderIcon() {
           return h(NIcon, null, {
@@ -76,44 +77,41 @@ const columns = [
     }
   }
 ]
-
-const data = [
-  {
-    id: 1,
-    group: 'NPC 초급반',
-    type: 'ACM',
-    title: '2차 모의대회',
-    period: '2022-08-22',
-    visible: false
-  },
-  {
-    id: 2,
-    group: 'NPC 초급반',
-    type: 'ACM',
-    title: '2차 모의대회',
-    period: '2022-08-22',
-    visible: false
-  },
-  {
-    id: 3,
-    group: 'NPC 초급반',
-    type: 'ACM',
-    title: '2차 모의대회',
-    period: '2022-08-22',
-    visible: false
-  }
-]
+const loading = ref(false)
+const data = ref(
+  Array(104)
+    .fill({
+      id: 1,
+      group: 'NPC 초급반',
+      type: 'ACM',
+      title: 'SKKU Coding Platform 2차모의대회',
+      period: '2022-08-28 18:00:00 ~ 2022-08-28 22:00:00',
+      visible: false
+    })
+    .map((item, index) => ({
+      ...item,
+      id: item.id + index,
+      visible: index % 2 ? false : true
+    }))
+)
 </script>
 
 <template>
-  <n-data-table
-    :columns="columns"
-    :data="data"
-    :pagination="{
-      pageSize: 3
-    }"
-    :bordered="false"
-  />
+  <main class="flex flex-col">
+    <PageTitle text="Contest List" class="mb-5" />
+    <SearchBar placeholder="keywords" class="mb-5 self-end" />
+    <n-data-table
+      class="text-xl"
+      :columns="columns"
+      :data="data"
+      :pagination="{
+        pageSize: 5
+      }"
+      :loading="loading"
+      :bordered="false"
+      row-class-name="font-bold text-base"
+    />
+  </main>
 </template>
 <route lang="yaml">
 meta:
