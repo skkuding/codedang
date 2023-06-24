0671b4136352b5e17147abf3f6a988779faec940
dayongkr <dayongkr@gmail.com>
2023-06-24 12:38:53 +0000

    fix(fe): connect API in a contest detail page

--- frontend/src/user/contest/components/Notice.vue
+++ frontend/src/user/contest/components/Notice.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import PaginationTable from '@/common/components/Organism/PaginationTable.vue'
+import { useDateFormat } from '@vueuse/core'
+import axios from 'axios'
+import { ref } from 'vue'
 
-defineProps<{
+const props = defineProps<{
   id: number
 }>()
 
@@ -10,19 +13,20 @@ const fields = [
   { key: 'time', label: 'Time' },
   { key: 'description', label: 'Description' }
 ]
+const formatter = ref('YYYY-MM-DD HH:mm:ss')
 
-const noticeList = [
-  {
-    id: 'A',
-    time: '2021-12-27 18:22:05',
-    description: '문제 A번에 들어간 사진이 잘못 되어 수정하였습니다.'
-  },
-  {
-    id: 'C',
-    time: '2021-12-27 18:22:05',
-    description: '문제 C번의 설명을 수정하였습니다.'
-  }
-]
+const noticeList = ref([])
+axios.get(`/api/contest/${props.id}/clarification`).then((res) => {
+  noticeList.value = res.data.map(
+    (notice: { id: string; updateTime: string; content: string }) => {
+      return {
+        id: notice.id,
+        time: useDateFormat(notice.updateTime, formatter).value,
+        description: notice.content
+      }
+    }
+  )
+})
 </script>
 
 <template>
