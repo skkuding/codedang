69a1a522f6f190ed059d453973b0d2e44aff995f
Jaemin Choi <1dotolee@gmail.com>
2023-06-26 13:05:51 +0000

    fix: do not use port 22

--- infra/main.tf
+++ infra/main.tf
@@ -108,14 +108,6 @@ resource "aws_security_group" "allow_web" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
