bf41366596b7c27c35054eb370379cd638d4b6cd
Sori Lim <aint.imsorry@gmail.com>
2023-06-29 12:55:35 +0000

    feat(be): remove dependency of group guard on roles guard

--- backend/libs/auth/src/roles/group-leader.guard.ts
+++ backend/libs/auth/src/roles/group-leader.guard.ts
@@ -3,6 +3,7 @@ import {
   type CanActivate,
   type ExecutionContext
 } from '@nestjs/common'
+import { type GqlContextType, GqlExecutionContext } from '@nestjs/graphql'
 import type { AuthenticatedRequest } from '../authenticated-request.interface'
 import type { AuthenticatedUser } from '../authenticated-user.class'
 import { RolesService } from './roles.service'
@@ -12,15 +13,24 @@ export class GroupLeaderGuard implements CanActivate {
   constructor(private readonly service: RolesService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request: AuthenticatedRequest = context.switchToHttp().getRequest()
-    const user: AuthenticatedUser = request.user
+    let request: AuthenticatedRequest
+    if (context.getType<GqlContextType>() === 'graphql') {
+      request = GqlExecutionContext.create(context).getContext().req
+    } else {
+      request = context.switchToHttp().getRequest()
+    }
 
+    const user: AuthenticatedUser = request.user
+    if (!user.role) {
+      const userRole = (await this.service.getUserRole(user.id)).role
+      user.role = userRole
+    }
     if (user.isAdmin() || user.isSuperAdmin()) {
       return true
     }
 
     const groupId: number = parseInt(request.params.groupId)
-    const userId: number = request.user.id
+    const userId: number = user.id
 
     const userGroup = await this.service.getUserGroup(userId, groupId)
     const isGroupLeader: boolean = userGroup && userGroup.isGroupLeader
