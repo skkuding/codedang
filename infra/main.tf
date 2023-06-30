aa6448bc753b92389efe713033c196069c51eef9
Jaemin Choi <1dotolee@gmail.com>
2023-06-30 18:47:52 +0000

    feat: add cloudfront in front of frontend s3

--- infra/main.tf
+++ infra/main.tf
@@ -124,6 +124,60 @@ resource "aws_security_group" "allow_web" {
   }
 }
 
+############## Cloudfront ##############
+
+resource "aws_cloudfront_origin_access_control" "main" {
+  name                              = "Codedang-Cloudfront-S3-OAI"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "main" {
+  origin {
+    domain_name              = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id                = aws_s3_bucket.frontend.id
+    origin_access_control_id = aws_cloudfront_origin_access_control.main.id
+  }
+
+  enabled             = true
+  comment             = "Codedang Cloudfront"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id       = aws_s3_bucket.frontend.id
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none" # Allow cache from all countries
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true # Enforce HTTPS
+  }
+
+  # Redirect non-root path to root path (need for SPA)
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
+  }
+}
+
 ############## ECS - Proxy ##############
 
 resource "aws_subnet" "proxy1" {
@@ -244,4 +298,27 @@ resource "aws_s3_bucket_website_configuration" "frontend" {
   error_document {
     key = "index.html"
   }
+}
+
+data "aws_iam_policy_document" "cloudfront_s3" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.frontend.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.main.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "frontend" {
+  bucket = aws_s3_bucket.frontend.id
+  policy = data.aws_iam_policy_document.cloudfront_s3.json
 }
