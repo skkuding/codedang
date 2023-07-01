2ebc502118b92ef54cb0b75e23d8a9350e203318
Sori Lim <aint.imsorry@gmail.com>
2023-07-01 15:16:50 +0000

    test(be): test group admin apis

--- backend/apps/admin/src/group/group.resolver.spec.ts
+++ backend/apps/admin/src/group/group.resolver.spec.ts
@@ -1,13 +1,19 @@
 import { Test, type TestingModule } from '@nestjs/testing'
 import { expect } from 'chai'
+import { RolesService } from '@libs/auth'
 import { GroupResolver } from './group.resolver'
+import { GroupService } from './group.service'
 
 describe('GroupResolver', () => {
   let resolver: GroupResolver
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [GroupResolver]
+      providers: [
+        GroupResolver,
+        { provide: GroupService, useValue: {} },
+        { provide: RolesService, useValue: {} }
+      ]
     }).compile()
 
     resolver = module.get<GroupResolver>(GroupResolver)
