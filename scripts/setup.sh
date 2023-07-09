3090eea43170a472fc3c64210a34d1b46d82d71c
Jaemin Choi <1dotolee@gmail.com>
2023-07-09 14:10:26 +0000

    chore: optimize setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -80,9 +80,7 @@ fi
 # Apply database migration
 for i in {1..5}
 do
-  pnpm --filter backend exec prisma generate \
-    && pnpm --filter backend exec prisma migrate dev \
-    && break # break if migration succeed
+  pnpm --filter backend exec prisma migrate dev && break # break if migration succeed
   echo -e '\n⚠️ Failed to migrate. Waiting for db to be ready...\n'
   sleep 5
 done
