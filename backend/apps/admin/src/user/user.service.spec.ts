151f2295a1be01a4690b3f07f74f869505ad4e62
Hyeongrok1 <jhrhello0323@gmail.com>
2023-07-13 12:27:35 +0000

    test(be): modify admin user.service.spec.ts

--- backend/apps/admin/src/user/user.service.spec.ts
+++ backend/apps/admin/src/user/user.service.spec.ts
@@ -1,3 +1,4 @@
+import { CACHE_MANAGER } from '@nestjs/cache-manager'
 import { Test, type TestingModule } from '@nestjs/testing'
 import { expect } from 'chai'
 import { PrismaService } from '@libs/prisma'
@@ -5,13 +6,24 @@ import { UserService } from './user.service'
 
 describe('UserService', () => {
   let service: UserService
-
+  let cache: Cache
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService, { provide: PrismaService, useValue: {} }]
+      providers: [
+        UserService,
+        { provide: PrismaService, useValue: {} },
+        {
+          provide: CACHE_MANAGER,
+          useFactory: () => ({
+            set: () => [],
+            get: () => []
+          })
+        }
+      ]
     }).compile()
 
     service = module.get<UserService>(UserService)
+    cache = module.get<Cache>(CACHE_MANAGER)
   })
 
   it('should be defined', () => {
