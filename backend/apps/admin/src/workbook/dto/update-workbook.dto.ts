d0c29f8c9f2c36defc96a24e7b0ac90ad6b9aa85
gyunrinkle <rbstj0311@naver.com>
2023-07-13 18:03:20 +0000

    chore(be): delete old DTO files
    전에 만들었던 DTO 파일들을 삭제했습니다.

--- backend/apps/admin/src/workbook/dto/update-workbook.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsNumber } from 'class-validator'
-import { CreateWorkbookDto } from './create-workbook.dto'
-
-export class UpdateWorkbookDto extends CreateWorkbookDto {
-  @IsNumber()
-  @IsNotEmpty()
-  id: number
-}
