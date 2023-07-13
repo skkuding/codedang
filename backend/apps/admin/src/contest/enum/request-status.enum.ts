bd59f0302dd11f3e627c2affbdc2618d05325ce2
Jaehyeon1020 <kjhkk1020@naver.com>
2023-07-13 14:20:37 +0000

    chore(be): delete request-status enum - 사용하지 않는 RequestStatus Enum 삭제

--- backend/apps/admin/src/contest/enum/request-status.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum RequestStatus {
-  pending = 'PENDING', // public 요청 후 대기 중
-  accept = 'ACCEPT', // public 요청 승인됨
-  reject = 'REJECT', // publice 요청 거절됨
-  none = 'NONE' // public 전환을 요청하지 않은 contest
-}
