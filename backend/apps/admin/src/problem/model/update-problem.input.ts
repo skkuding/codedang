d2a54a187e02da0d50709402394e131758e55253
JungangHighschool <lhs109802@g.skku.edu>
2023-07-13 23:14:49 +0900

    fix(be): modify error handling

--- backend/apps/admin/src/problem/model/update-problem.input.ts
+++ backend/apps/admin/src/problem/model/update-problem.input.ts
@@ -1,4 +1,8 @@
-import { InputType, OmitType } from '@nestjs/graphql'
+import { Field, InputType, OmitType } from '@nestjs/graphql'
+import {
+  ProblemTagUncheckedUpdateManyWithoutProblemNestedInput,
+  ProblemTestcaseUncheckedUpdateManyWithoutProblemNestedInput
+} from '@admin/@generated'
 import { ProblemUncheckedUpdateInput } from '@admin/@generated/problem/problem-unchecked-update.input'
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -14,4 +18,18 @@ export class UpdateProblemInput extends OmitType(ProblemUncheckedUpdateInput, [
   'submission',
   'problemTestcase',
   'problemTag'
-]) {}
+]) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-nocheck
+  @Field(() => ProblemTestcaseUncheckedUpdateManyWithoutProblemNestedInput, {
+    nullable: false
+  })
+  problemTestcase: ProblemTestcaseUncheckedUpdateManyWithoutProblemNestedInput
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  //@ts-nocheck
+  @Field(() => ProblemTagUncheckedUpdateManyWithoutProblemNestedInput, {
+    nullable: false
+  })
+  problemTag: ProblemTagUncheckedUpdateManyWithoutProblemNestedInput
+}
