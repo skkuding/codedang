0c8a1f1f468a77a48e2463f46c000982442d0656
Jaehyeon1020 <kjhkk1020@naver.com>
2023-07-13 14:30:49 +0000

    fix(be): remove improper inline type specifiers

--- backend/apps/admin/src/auth/interface/authenticated-request.interface.ts
+++ backend/apps/admin/src/auth/interface/authenticated-request.interface.ts
@@ -1,5 +1,5 @@
-import { type Request } from 'express'
-import { type AuthenticatedUser } from '../class/authenticated-user.class'
+import type { Request } from 'express'
+import type { AuthenticatedUser } from '../class/authenticated-user.class'
 
 export interface AuthenticatedRequest extends Request {
   user: AuthenticatedUser
