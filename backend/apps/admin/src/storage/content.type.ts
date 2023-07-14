b3d2b7ee5211029020610e57d24c3d3bfb386505
Sori Lim <aint.imsorry@gmail.com>
2023-07-14 09:57:23 +0000

    chore(be): upload with specified content type

---
+++ backend/apps/admin/src/storage/content.type.ts
@@ -0,0 +1,5 @@
+export const ContentType = {
+  json: 'application/json',
+  txt: 'text/plain'
+}
+export type Extension = keyof typeof ContentType
--- backend/apps/admin/src/storage/storage.service.ts
+++ backend/apps/admin/src/storage/storage.service.ts
@@ -1,6 +1,11 @@
 import { Inject, Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client
+} from '@aws-sdk/client-s3'
+import { ContentType, type Extension } from './content.type'
 
 @Injectable()
 export class StorageService {
@@ -9,12 +14,13 @@ export class StorageService {
     @Inject('S3_CLIENT') private readonly client: S3Client
   ) {}
 
-  async uploadObject(path: string, contents: string) {
+  async uploadObject(path: string, content: string, extension: Extension) {
     await this.client.send(
       new PutObjectCommand({
         Bucket: this.config.get('TESTCASE_BUCKET'),
-        Key: path,
-        Body: contents
+        Key: `${path}.${extension}`,
+        Body: content,
+        ContentType: ContentType[extension]
       })
     )
   }
