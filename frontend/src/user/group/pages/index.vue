7557702899157215e47758f57aa2cc21964ec135
Seonghyeon Cho <67233988+st42597@users.noreply.github.com>
2023-07-14 21:14:38 +0900

    Revert "feat(fe): add group create modal & new inivitation code submission (#623)"
    This reverts commit f3d10911fd2e16e164762e32af95e867a4c9034f.

--- frontend/src/user/group/pages/index.vue
+++ frontend/src/user/group/pages/index.vue
@@ -2,82 +2,40 @@
 import Button from '@/common/components/Atom/Button.vue'
 import InputItem from '@/common/components/Atom/InputItem.vue'
 import Modal from '@/common/components/Molecule/Modal.vue'
-import Switch from '@/common/components/Molecule/Switch.vue'
-import { NUpload, NButton, NColorPicker } from 'naive-ui'
 import { ref } from 'vue'
-import IconLock from '~icons/bi/lock'
-import IconUnlock from '~icons/bi/unlock'
 import IconPaperPlane from '~icons/fa6-solid/paper-plane'
 import GroupListSection from '../components/GroupListSection.vue'
 
 //TODO: invitation 검색 API 연결 후 noInvitation 값을 변경하는 function 구현 필요
+const noInvitationCode = ref(false)
+const joinModalVisible = ref(false)
 const invitationCode = ref('')
-const createModal = ref(false)
-const groupName = ref('')
-const groupPrivate = ref(false)
-const groupDescription = ref('')
 </script>
 
 <template>
   <div class="mt-10 flex flex-col gap-4">
-    <div class="flex items-start justify-end gap-2">
-      <InputItem v-model="invitationCode" placeholder="Invitation Code" />
-      <Button class="py-2"><IconPaperPlane /></Button>
-    </div>
+    <Button class="self-end" @click="joinModalVisible = true">
+      Join Group
+    </Button>
     <GroupListSection title="My Group" :is-my-group="true" />
-    <div class="flex justify-end">
-      <Button class="py-2" @click="createModal = true">+ Create Group</Button>
-    </div>
     <GroupListSection title="All Group" pagination :is-my-group="false" />
+    <Modal v-model="joinModalVisible" class="shadow-md">
+      <div class="flex flex-col items-center gap-2 px-8 py-12">
+        <h1 class="mb-1 text-center text-lg font-bold">
+          Join Group by Invitation Code
+        </h1>
+        <p class="text-center">
+          If you have invitation code, enter the code here.
+        </p>
+        <div class="flex items-start gap-2">
+          <InputItem v-model="invitationCode" placeholder="Invitation Code" />
+          <Button class="py-2"><IconPaperPlane /></Button>
+        </div>
+
+        <p v-if="noInvitationCode" class="text-red text-center">
+          Group not found! Please check your code again
+        </p>
+      </div>
+    </Modal>
   </div>
-  <Modal v-model="createModal" class="shadow-md">
-    <div class="flex flex-col px-8 py-12">
-      <h1 class="text-gray-dark mb-2 text-2xl font-bold">Create Group</h1>
-      <div class="bg-gray-light mb-6 h-[1px] w-full"></div>
-      <div class="mb-6">
-        <div class="mb-2 flex">
-          <h2 class="mr-60 text-lg font-semibold">Group Name</h2>
-          <h2 class="text-lg font-semibold">Public / Private</h2>
-        </div>
-        <div class="flex items-center">
-          <InputItem v-model="groupName" class="mr-36 shadow" />
-          <div class="flex" :class="[groupPrivate ? '' : 'text-green']">
-            <IconUnlock class="h-5 w-5" />
-            <span>Public</span>
-          </div>
-          <Switch v-model="groupPrivate" class="mx-2" />
-          <div class="flex" :class="[groupPrivate ? 'text-green' : '']">
-            <IconLock class="h-5 w-5" />
-            <span>Private</span>
-          </div>
-        </div>
-      </div>
-      <div class="mb-6">
-        <h2 class="mb-2 text-lg font-semibold">
-          Description ({{ groupDescription.length }}/50)
-        </h2>
-        <InputItem v-model="groupDescription" class="w-full shadow" />
-      </div>
-      <div>
-        <div class="flex">
-          <h2 class="mr-60 text-lg font-semibold">Group Image</h2>
-          <h2 class="text-lg font-semibold">Group Color</h2>
-        </div>
-        <div class="flex">
-          <NUpload
-            action="https://www.mocky.io/v2/5e4bafc63100007100d8b70f"
-            :headers="{
-              'naive-info': 'hello!'
-            }"
-            :data="{
-              'naive-data': 'cool! naive!'
-            }"
-          >
-            <NButton>Upload File</NButton>
-          </NUpload>
-          <NColorPicker />
-        </div>
-      </div>
-    </div>
-  </Modal>
 </template>
