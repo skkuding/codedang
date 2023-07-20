37318e0d9838ab9569c56bc0038c388330b6ea2c
Jaehyeon1020 <kjhkk1020@naver.com>
2023-07-20 13:03:11 +0000

    feat(be): add admin contest service test code - admin contest service 테스트 코드 추가

--- backend/apps/admin/src/contest/contest.service.spec.ts
+++ backend/apps/admin/src/contest/contest.service.spec.ts
@@ -6,42 +6,48 @@ import { PrismaService } from '@libs/prisma'
 import { Contest } from '@admin/@generated/contest/contest.model'
 import { ContestService } from './contest.service'
 
-// const userId = 1
-// const groupId = 2
-// const contestId = 1
-// const take = 3
-// const cursor = 0
+const contestId = 1
+const userId = 1
+const groupId = 1
 
-// const input = {
-//   title: 'title',
-//   description: 'description',
-//   startTime: undefined,
-//   endTime: undefined,
-//   config: {
-//     isVisible: false,
-//     isRankVisible: false
-//   }
-// }
+const contest: Contest = {
+  id: contestId,
+  createdById: userId,
+  groupId: groupId,
+  title: 'title',
+  description: 'description',
+  startTime: undefined,
+  endTime: undefined,
+  config: {
+    isVisible: true,
+    isRankVisible: true
+  },
+  createTime: undefined,
+  updateTime: undefined
+}
 
-// const updateInput = {
-//   id: 1,
-//   ...input
-// }
+const input = {
+  title: 'test title10',
+  description: 'test description',
+  startTime: undefined,
+  endTime: undefined,
+  config: {
+    isVisible: false,
+    isRankVisible: false
+  }
+}
 
-// const contest: Contest = {
-//   id: 1,
-//   createdById: 1,
-//   groupId: 1,
-//   updateTime: undefined,
-//   createTime: undefined,
-//   ...input
-// }
-
-// const publicizingRequest: PublicizingRequest = {
-//   contest: 1,
-//   user: 1,
-//   createTime: undefined
-// }
+const updateInput = {
+  id: 1,
+  title: 'test title10',
+  description: 'test description',
+  startTime: undefined,
+  endTime: undefined,
+  config: {
+    isVisible: false,
+    isRankVisible: false
+  }
+}
 
 const db = {
   contest: {
@@ -50,7 +56,7 @@ const db = {
     findMany: stub().resolves([Contest]),
     create: stub().resolves(Contest),
     update: stub().resolves(Contest),
-    delete: stub()
+    delete: stub().resolves(Contest)
   }
 }
 
@@ -83,59 +89,64 @@ describe('ContestService', () => {
     expect(service).to.be.ok
   })
 
-  // describe('getContests', () => {
-  //   it('should return an array of contests', async () => {
-  //     const res = await service.getContests(5, 2, 0)
-  //     expect(res).to.deep.equal([contest])
-  //   })
-  // })
+  describe('getContests', () => {
+    it('should return an array of contests', async () => {
+      db.contest.findMany.resolves([contest])
 
-  // describe('getPublicRequests', () => {
-  //   it('should return an array of PublicizingRequest', async () => {
-  //     const res = await service.getPublicRequests()
-  //     expect(res).to.deep.equal([publicizingRequest])
-  //   })
-  // })
+      const res = await service.getContests(5, 2, 0)
+      expect(res).to.deep.equal([contest])
+    })
+  })
 
-  // describe('createContest', () => {
-  //   it('should return created contest', async () => {
-  //     const res = await service.createContest(groupId, userId, input)
-  //     expect(res).to.deep.equal(contest)
-  //   })
-  // })
+  describe('getPublicRequests', () => {
+    it('should return an array of PublicizingRequest', async () => {
+      const res = await service.getPublicRequests()
+      expect(res).to.deep.equal([])
+    })
+  })
 
-  // describe('updateContest', () => {
-  //   it('should return updated contest', async () => {
-  //     const res = await service.updateContest(groupId, updateInput)
-  //     expect(res).to.deep.equal(contest)
-  //   })
-  // })
+  describe('createContest', () => {
+    it('should return created contest', async () => {
+      db.contest.create.resolves(contest)
 
-  // describe('deleteContest', () => {
-  //   it('should return deleted contest', async () => {
-  //     const res = await service.deleteContest(groupId, contestId)
-  //     expect(res).to.deep.equal(contest)
-  //   })
-  // })
+      const res = await service.createContest(groupId, userId, input)
+      expect(res).to.deep.equal(contest)
+    })
+  })
 
-  // describe('acceptPublic', () => {
-  //   it('should return accepted contest', async () => {
-  //     const res = await service.acceptPublic(groupId, contestId)
-  //     expect(res).to.deep.equal(contest)
-  //   })
-  // })
+  describe('updateContest', () => {
+    it('should return updated contest', async () => {
+      db.contest.update.resolves(contest)
 
-  // describe('rejectPublic', () => {
-  //   it('should return rejected contest', async () => {
-  //     const res = await service.rejectPublic(groupId, contestId)
-  //     expect(res).to.deep.equal(contest)
-  //   })
-  // })
+      const res = await service.updateContest(groupId, updateInput)
+      expect(res).to.deep.equal(contest)
+    })
+  })
 
-  // describe('requestToPublic', () => {
-  //   it('should return requested contest', async () => {
-  //     const res = await service.requestToPublic(groupId, contestId)
-  //     expect(res).to.deep.equal(contest)
-  //   })
-  // })
+  describe('deleteContest', () => {
+    it('should return deleted contest', async () => {
+      db.contest.findFirst.resolves(contest)
+
+      const res = await service.deleteContest(groupId, contestId)
+      expect(res).to.deep.equal(contest)
+    })
+  })
+
+  describe('acceptPublic', () => {
+    it('should return accepted contest', async () => {
+      db.contest.update.resolves(contest)
+
+      const res = await service.acceptPublic(groupId, contestId)
+      expect(res).to.deep.equal(contest)
+    })
+  })
+
+  describe('rejectPublic', () => {
+    it('should return rejected contest', async () => {
+      db.contest.findUnique.resolves(contest)
+
+      const res = await service.rejectPublic(groupId, contestId)
+      expect(res).to.deep.equal(contest)
+    })
+  })
 })
