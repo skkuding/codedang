54c5374b8222efde8f09dd65c471bd6a59f68e76
dayongkr <dayongkr@gmail.com>
2023-07-25 13:58:13 +0900

    fix(fe): resolve review

--- frontend/src/common/components/Organism/Header.vue
+++ frontend/src/common/components/Organism/Header.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import Logo from '@/common/assets/codedang_logo.png'
+import Logo from '@/common/assets/skkudingLogo.png'
 import { useAuthStore } from '@/common/store/auth'
 import { OnClickOutside } from '@vueuse/components'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import IconArrowRightFromBracket from '~icons/fa6-solid/arrow-right-from-bracket'
 import IconBars from '~icons/fa6-solid/bars'
 import IconSliders from '~icons/fa6-solid/sliders'
@@ -14,23 +15,21 @@ import Dropdown from '../Molecule/Dropdown.vue'
 import AuthModal from './AuthModal.vue'
 
 const auth = useAuthStore()
+const router = useRouter()
 const isMenuOpen = ref(false)
 const modalContent = ref<'login' | 'signup' | 'password' | 'close'>('close')
 </script>
 
 <template>
-  <OnClickOutside
-    class="sticky top-0 z-[9999] bg-white"
-    @trigger="isMenuOpen = false"
-  >
-    <header class="border-b-gray grid h-20 place-items-center border-b px-8">
+  <OnClickOutside @trigger="isMenuOpen = false">
+    <header
+      class="border-b-gray grid h-16 place-items-center border-b bg-white px-8"
+    >
       <div class="flex w-full max-w-7xl items-center justify-between gap-5">
         <div class="flex items-center gap-32">
-          <RouterLink to="/">
-            <div class="flex items-center gap-1">
-              <img :src="Logo" alt="logo" width="45" />
-              <h1 class="text-2xl font-bold">Codedang</h1>
-            </div>
+          <RouterLink to="/" class="flex items-center gap-1 hover:opacity-80">
+            <img :src="Logo" alt="logo" width="45" />
+            <h1 class="text-2xl font-bold">Codedang</h1>
           </RouterLink>
           <nav class="hidden gap-10 capitalize md:flex">
             <RouterLink
@@ -41,8 +40,10 @@ const modalContent = ref<'login' | 'signup' | 'password' | 'close'>('close')
                 { to: '/group', name: 'group' }
               ]"
               :key="name"
-              active-class="text-green"
               class="cursor-pointer text-lg hover:opacity-60 active:opacity-40"
+              :class="{
+                'text-green': router.currentRoute.value.fullPath.startsWith(to)
+              }"
               :to="to"
             >
               {{ name }}
@@ -75,13 +76,13 @@ const modalContent = ref<'login' | 'signup' | 'password' | 'close'>('close')
           <div v-else class="ml-2 hidden items-center gap-2 md:flex">
             <Button
               color="white"
-              class="whitespace-nowrap"
+              class="whitespace-nowrap hover:opacity-80"
               @click="modalContent = 'login'"
             >
               Sign in
             </Button>
             <button
-              class="whitespace-nowrap rounded-md border border-slate-100 bg-transparent px-3 py-2 font-bold text-black hover:bg-slate-100 hover:bg-opacity-20"
+              class="whitespace-nowrap rounded-md border border-slate-100 bg-transparent px-2 py-1 font-bold text-black hover:bg-slate-100 hover:bg-opacity-20"
               @click="modalContent = 'signup'"
             >
               Sign up
