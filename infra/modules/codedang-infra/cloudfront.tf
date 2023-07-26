93e5a48b453144f444f28c395a636295c088c58d
Jaemin Choi <1dotolee@gmail.com>
2023-07-26 13:54:10 +0000

    chore: set up cname record for cloudfront

--- infra/modules/codedang-infra/cloudfront.tf
+++ infra/modules/codedang-infra/cloudfront.tf
@@ -40,6 +40,8 @@ resource "aws_cloudfront_distribution" "main" {
   comment             = "Codedang Cloudfront"
   default_root_object = "index.html"
 
+  aliases = ["codedang.com"]
+
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD", "OPTIONS"]
