fc875827fe551df1ff561f53082d037874dbb3e8
Dayong Lee <dayongkr@gmail.com>
2023-07-27 20:29:22 +0900

    feat(fe): make a new carousel component (#666)
    * feat(fe): make a new carousel component
    
    * fix(fe): remove blink
    
    * fix(fe): change log name
    
    * fix(fe): change logo path
    
    * fix(fe): remove unused css style
    
    * fix(fe): use useIntervalFn
    
    * feat(fe): add router link
    
    * feat(fe): remove comment
    
    * fix(fe): remove mix-blend
    
    * feat(fe): change design
    
    * fix(fe): align problem
    
    * fix(fe): remove unused margin top
    
    * fix(fe): fix some gap issue
    
    * fix(fe): decrease padding
    
    * fix(fe): decrease z-index
    
    * feat: update carousel content
    
    * fix: load carousel synchronously
    
    ---------
    
    Co-authored-by: Jaemin Choi <1dotolee@gmail.com>

--- frontend/src/common/components/Organism/Header.vue
+++ frontend/src/common/components/Organism/Header.vue
@@ -23,15 +23,15 @@ const modalContent = ref<'login' | 'signup' | 'password' | 'close'>('close')
 <template>
   <OnClickOutside @trigger="isMenuOpen = false">
     <header
-      class="border-b-gray grid h-16 place-items-center border-b bg-white px-8"
+      class="border-b-gray grid h-16 place-items-center border-b bg-white px-5"
     >
       <div class="flex w-full max-w-7xl items-center justify-between gap-5">
-        <div class="flex items-center gap-32">
+        <div class="flex w-1/2 min-w-fit items-center justify-between gap-8">
           <RouterLink to="/" class="flex items-center gap-1">
             <img :src="Logo" alt="logo" width="45" />
             <h1 class="text-2xl font-bold">Codedang</h1>
           </RouterLink>
-          <nav class="hidden gap-10 capitalize md:flex">
+          <nav class="hidden gap-8 capitalize md:flex">
             <RouterLink
               v-for="{ to, name } in [
                 { to: '/notice', name: 'notice' },
