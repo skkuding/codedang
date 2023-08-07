76e50b838337b9db30f8efb59189fb3bf25b2c71
Jaemin Choi <1dotolee@gmail.com>
2023-08-07 13:29:55 +0000

    feat: add iam plicy to send email in ecs

--- infra/modules/codedang-infra/ecs.tf
+++ infra/modules/codedang-infra/ecs.tf
@@ -13,6 +13,27 @@ data "aws_iam_policy_document" "ecs_task_execution_role" {
   }
 }
 
+# IAM Policy to allow sending emails via SES
+resource "aws_iam_policy" "ses_send_email" {
+  name        = "AllowSESSendEmail"
+  description = "Allows sending emails via SES"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "ses:SendEmail",
+          "ses:SendRawEmail"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
 resource "aws_iam_role" "ecs_task_execution_role" {
   name               = "Codedang-Api-Task-Execution-Role"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
@@ -21,4 +42,9 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_ses" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = aws_iam_policy.ses_send_email.arn
 }
