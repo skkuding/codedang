47246096eeb0bebf8cb2b49eca4c0630bcf6da4d
Jisoo Park <102132161+jspark2000@users.noreply.github.com>
2023-08-10 17:04:38 +0900

    feat(be): implement submission post api for user (#497)
    * feat(be): add baseline codes
    
    * feat(be): add constants on enums
    
    * fix: pnpm-lock file
    
    * feat(be): implement submission services
    
    * fix(be): modify submission service
    
    * feat(be): implements get submission result service
    
    * feat(be): implement submission controller
    
    * feat(be): add basic thunder-client test
    
    * feat(be): add baseline test code
    
    * feat(be): implements test code
    
    * fix(be): pnpm-lock file
    
    * fix(be): modify controller return type
    
    * fix(be): modify submission result return type
    
    * fix(ci): delete vscode prettier extension settings
    
    * feat(be): implement guards
    
    * style(be): formatting code
    
    * fix(be): change publish type
    
    * fix(be): change package import dir
    
    * feat(be): implements amqp subscription method
    
    * fix(be): modfiy imports
    
    * feat(be): implement test code
    
    * feat(be): add thunder client test
    
    * fix(be): packages.json
    
    * feat(be): save commit
    
    * chore(be): add newly required fields in prisma schema
    
    * chore(be): add new constants to libs
    
    * feat(be): specify submission controllers
    
    * feat(be): implement submission get & post apis
    
    * fix(be): fix mocking files to match revised schema
    
    * chore(be): revise field in snippet
    
    * fix(be): fix mock and seed files
    
    * fix(be): fix submission service
    
    * fix(be): fix controllers to handle exceptions
    
    * test(be): test clinet submission apis
    
    * fix(be): fix contest mock files
    
    * chore: fix lint error
    
    * chore: fix lockfile
    
    * chore: use secure cookie on production
    
    * chore: log unexpected exception
    
    * fix(be): fix prisma error handling
    
    * fix: use 127.0.0.1 instead of localhost
    
    * feat: read example testcase from files
    
    * fix: handle not found problem with submission
    
    * chore: remove rxjs (unused deps)
    
    * fix: handle prisma notfound error
    
    * chore: remove arrow function as method
    
    * chore: remove unnecessary console log
    
    * feat: log judge result handling error
    
    * fix: always enable subscriber
    
    * chore: add type import
    
    * fix: remove redundant parameter
    
    problemId를 DTO와 param 모두에서 넘기는 대신, param에서만 전달한다.
    
    * fix: use amqps only if specified
    
    * feat: set rabbitmq env on production env
    
    * fix: update submission dto
    
    * docs: update thunder client
    
    * fix: update test
    
    ---------
    
    Co-authored-by: Sori Lim <aint.imsorry@gmail.com>
    Co-authored-by: Jaemin Choi <1dotolee@gmail.com>

--- testcase-server/init-testcase.ts
+++ testcase-server/init-testcase.ts
@@ -5,6 +5,7 @@ import {
   PutBucketPolicyCommand,
   PutObjectCommand
 } from '@aws-sdk/client-s3'
+import { readdir, readFile } from 'fs/promises'
 
 const main = async () => {
   const client = new S3Client({
@@ -12,8 +13,8 @@ const main = async () => {
     endpoint: process.env.TESTCASE_ENDPOINT_URL,
     forcePathStyle: true, // required for minio
     credentials: {
-      accessKeyId: process.env.TESTCASE_ACCESS_KEY,
-      secretAccessKey: process.env.TESTCASE_SECRET_KEY
+      accessKeyId: process.env.TESTCASE_ACCESS_KEY || '',
+      secretAccessKey: process.env.TESTCASE_SECRET_KEY || ''
     }
   })
 
@@ -51,43 +52,20 @@ const main = async () => {
     console.log('Created bucket', bucketName)
   }
 
-  const data = [
-    {
-      id: '1:1',
-      input: '1\n',
-      output: '1\n'
-    },
-    {
-      id: '1:2',
-      input: '22\n',
-      output: '22\n'
-    },
-    {
-      id: '1:3',
-      input: '333\n',
-      output: '333\n'
-    },
-    {
-      id: '1:4',
-      input: '4444\n',
-      output: '4444\n'
-    },
-    {
-      id: '1:5',
-      input: '55555\n',
-      output: '55555\n'
-    }
-  ]
-
-  await client.send(
-    new PutObjectCommand({
-      Bucket: bucketName,
-      Key: '1.json',
-      Body: JSON.stringify(data),
-      ContentType: 'application/json'
-    })
-  )
-  console.log('Uploaded example testcase')
+  // upload example testcase files
+  const files = await readdir('./data')
+  for (const file of files) {
+    const data = await readFile(`./data/${file}`, { encoding: 'utf-8' })
+    await client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: file,
+        Body: data,
+        ContentType: 'application/json'
+      })
+    )
+    console.log('Uploaded', file)
+  }
 }
 
 main()
