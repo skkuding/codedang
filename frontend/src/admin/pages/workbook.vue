747278e89b1c9f6769e329eee105094f25fc249a
EliseCho13 <dbwlspdlqj@naver.com>
2023-08-17 11:38:47 +0000

    feat(fe): changed naive ui datatable to pagination table

--- frontend/src/admin/pages/workbook.vue
+++ frontend/src/admin/pages/workbook.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
+import Button from '@/common/components/Atom/Button.vue'
 import PageTitle from '@/common/components/Atom/PageTitle.vue'
-import SearchBar from '@/common/components/Molecule/SearchBar.vue'
-import { NButton, NSwitch, NIcon, NDataTable } from 'naive-ui'
-import { h, ref } from 'vue'
-import Fa6RegularTrashCan from '~icons/fa6-regular/trash-can'
+import Switch from '@/common/components/Molecule/Switch.vue'
+import PaginationTable from '@/common/components/Organism/PaginationTable.vue'
+import { ref } from 'vue'
+import IconTrash from '~icons/fa/trash-o'
 
 interface Workbook {
   id: number
   group: string
-  title: string
+  label: string
   period: string
   visible: boolean
 }
 
+//TODO: 워크북 삭제 및 visible 토글 기능 넣기
 const deleteWorkbook = (row: Workbook) => {
   console.log('delete Workbook', row)
 }
@@ -20,65 +22,59 @@ const deleteWorkbook = (row: Workbook) => {
 const toggleVisible = (row: Workbook) => {
   row.visible = !row.visible
 }
+const onoff = ref(true)
 
 const columns = [
   {
-    title: '#',
+    label: '#',
     key: 'id',
-    minWidth: 80
+    width: '8%'
   },
   {
-    title: 'Group',
+    label: 'Group',
     key: 'group',
-    minWidth: 150
+    width: '15%'
   },
   {
-    title: 'Title',
-    key: 'title'
+    label: 'Title',
+    key: 'title',
+    width: '30%'
   },
   {
-    title: 'Period',
+    label: 'Period',
     key: 'period',
-    width: 220
+    width: '30%'
   },
   {
-    title: 'Visible',
+    label: 'Visible',
     key: 'visible',
-    align: 'center' as const,
-    width: 120,
-    render(row: Workbook) {
-      return h(NSwitch, {
-        strong: true,
-        tertiary: true,
-        value: row.visible,
-        size: 'small',
-        onClick: () => toggleVisible(row)
-      })
-    }
+    width: '8%'
   },
   {
-    title: 'Delete',
+    label: 'Delete',
     key: 'delete',
-    align: 'center' as const,
-    width: 120,
-    render(row: Workbook) {
-      return h(NButton, {
-        strong: true,
-        size: 'small',
-        bordered: true,
-        onClick: () => deleteWorkbook(row),
-        renderIcon() {
-          return h(NIcon, null, {
-            default: () => h(Fa6RegularTrashCan)
-          })
-        }
-      })
-    }
+    width: '8%'
   }
 ]
-const loading = ref(false)
+
+const pageSlot = 5
+const curWorkbookPage = ref(1)
+const totalPageWorkbook = ref(1)
+
+//TODO: api 연결 후 개수 컷하기. 현재 id가 같은 걸 어레이로 반복 뿌리기만 해서 아이디로 자를 수 없음.
+const changeWorkbook = (page: number) => {
+  let q = Math.floor((curWorkbookPage.value - 1) / 5) * pageSlot
+  if (q < page && page <= q + pageSlot) {
+    curWorkbookPage.value = page
+    // currentWorkbook.value = contestList.value[(page - 1) % pageSlot]
+  } else {
+    curWorkbookPage.value = page
+    // getWorkbook(Math.floor((page - 1) / pageSlot) * perPage * pageSlot)
+  }
+}
+
 const data = ref(
-  Array(104)
+  Array(10)
     .fill({
       id: 1,
       group: 'NPC 초급반',
@@ -92,24 +88,33 @@ const data = ref(
       visible: index % 2 ? false : true
     }))
 )
+// <!-- key, label, width -->
 </script>
 
 <template>
   <div class="flex flex-col">
     <PageTitle text="Workbook List" class="mb-5" />
-    <SearchBar placeholder="keywords" class="mb-5 self-end" />
-    <n-data-table
-      :columns="columns"
-      :data="data"
-      :loading="loading"
-      :pagination="{
-        pageSize: 5
-      }"
-      :scroll-x="1100"
-      :bordered="false"
-      class="text-xl"
-      row-class-name="text-base"
-    />
+    <!-- <SearchBar placeholder="keywords" class="mb-5 self-end" /> -->
+    <PaginationTable
+      :fields="columns"
+      :items="data"
+      placeholder="keywords"
+      :number-of-pages="totalPageWorkbook"
+      @change-page="changeWorkbook"
+      @row-clicked="() => $router.push(`/admin/`)"
+    >
+      <template #visible="{}">
+        <Switch v-model="onoff" @click.stop="toggleVisible" />
+      </template>
+      <template #delete="{}">
+        <Button
+          class="flex h-[32px] w-[32px] items-center justify-center"
+          @click.stop="deleteWorkbook"
+        >
+          <IconTrash />
+        </Button>
+      </template>
+    </PaginationTable>
   </div>
 </template>
 
