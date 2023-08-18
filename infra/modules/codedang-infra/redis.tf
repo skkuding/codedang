51e1b345fc3185c9264e8cb2c37be6aa65b73b74
k1g99 <100272259+k1g99@users.noreply.github.com>
2023-08-18 17:49:24 +0900

    fix(infra): disable redis cluster mode (#802)
    fix: disable redis cluster mode

--- infra/modules/codedang-infra/redis.tf
+++ infra/modules/codedang-infra/redis.tf
@@ -29,9 +29,7 @@ resource "aws_elasticache_replication_group" "db_cache" {
   port                     = var.redis_port
   apply_immediately        = true
   snapshot_retention_limit = 0 # no backup
-
-  num_node_groups         = 1 # number of shards
-  replicas_per_node_group = 1 # replicas per shard
+  replicas_per_node_group  = 1 # replicas per shard
 
   security_group_ids = [aws_security_group.redis.id]
   subnet_group_name  = aws_elasticache_subnet_group.redis_subnet_group.name
@@ -43,14 +41,42 @@ resource "aws_elasticache_replication_group" "db_cache" {
     log_format       = "text"
     log_type         = "slow-log"
   }
-
-  log_delivery_configuration {
-    destination      = "/elasticache/redis"
-    destination_type = "cloudwatch-logs"
-    log_format       = "text"
-    log_type         = "engine-log"
-  }
 }
+
+# resource "aws_elasticache_replication_group" "db_cache" {
+#   automatic_failover_enabled = true
+#   # preferred_cache_cluster_azs = [var.availability_zones[0], var.availability_zones[1]]
+#   replication_group_id     = "elasticache-redis-codedang-1"
+#   description              = "Redis for codedang"
+#   node_type                = "cache.t3.micro"
+#   engine                   = "redis"
+#   parameter_group_name     = "default.redis7.cluster.on"
+#   engine_version           = "7.0"
+#   port                     = var.redis_port
+#   apply_immediately        = true
+#   snapshot_retention_limit = 0 # no backup
+
+#   num_node_groups         = 1 # number of shards
+#   replicas_per_node_group = 1 # replicas per shard
+
+#   security_group_ids = [aws_security_group.redis.id]
+#   subnet_group_name  = aws_elasticache_subnet_group.redis_subnet_group.name
+
+
+#   log_delivery_configuration {
+#     destination      = "/elasticache/redis"
+#     destination_type = "cloudwatch-logs"
+#     log_format       = "text"
+#     log_type         = "slow-log"
+#   }
+
+#   log_delivery_configuration {
+#     destination      = "/elasticache/redis"
+#     destination_type = "cloudwatch-logs"
+#     log_format       = "text"
+#     log_type         = "engine-log"
+#   }
+# }
 
 resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   name       = "Redis-subnet-group"
