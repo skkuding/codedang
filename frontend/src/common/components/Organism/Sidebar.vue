1817ea5eca4ed8097efbfd0da4ed77bc6d0b5f1b
Dayong Lee <dayongkr@gmail.com>
2023-08-24 20:13:24 +0900

    feat(fe): add main tab and change logo link (#822)

--- frontend/src/common/components/Organism/Sidebar.vue
+++ frontend/src/common/components/Organism/Sidebar.vue
@@ -4,7 +4,7 @@ import { computed } from 'vue'
 import { useRoute } from 'vue-router'
 import IconCode from '~icons/bi/code-square'
 import IconFile from '~icons/bi/file-text'
-import IconBiHouse from '~icons/bi/house'
+import IconHome from '~icons/bi/house'
 import IconBook from '~icons/bi/journals'
 import IconTrophy from '~icons/bi/trophy'
 import IconUser from '~icons/fa6-regular/user'
@@ -13,6 +13,7 @@ import IconBrain from '~icons/fluent/brain-circuit-24-regular'
 const route = useRoute()
 
 const commonItems = [
+  { to: '/admin', name: 'Main', icon: IconHome },
   { to: '/admin/notice', name: 'Notice', icon: IconFile },
   { to: '/admin/contest', name: 'Contest', icon: IconTrophy },
   { to: '/admin/workbook', name: 'Workbook', icon: IconBook },
@@ -20,7 +21,7 @@ const commonItems = [
 ]
 
 const groupItems = (id: number) => [
-  { to: `/admin/${id}`, name: 'Main', icon: IconBiHouse },
+  { to: `/admin/${id}`, name: 'Main', icon: IconHome },
   { to: `/admin/${id}/notice`, name: 'Notice', icon: IconFile },
   { to: `/admin/${id}/contest`, name: 'Contest', icon: IconTrophy },
   { to: `/admin/${id}/workbook`, name: 'Workbook', icon: IconBook },
@@ -38,7 +39,7 @@ const items = computed(() =>
 
 <template>
   <div class="bg-gray-light text-gray-dark h-screen w-48 overflow-auto">
-    <RouterLink to="/admin" class="align-center flex justify-center">
+    <RouterLink to="/" class="align-center flex justify-center">
       <img :src="Logo" class="my-8 w-32" />
     </RouterLink>
     <hr class="bg-gray h-0.5 border-none opacity-25" />
