c38c1b898df48bfa0e6267a3ec7f05addd5c33da
k1g99 <100272259+k1g99@users.noreply.github.com>
2023-08-29 00:21:12 +0900

    fix: fix target group cloudinit_config data source (#837)

--- infra/modules/codedang-infra/ecs-iris-asg.tf
+++ infra/modules/codedang-infra/ecs-iris-asg.tf
@@ -67,7 +67,7 @@ resource "aws_launch_template" "ec2_template_iris" {
 
   # 미리 만들어 놓아야 합니다.
   key_name  = "codedang-ecs-iris-instance"
-  user_data = data.template_cloudinit_config.iris_config.rendered
+  user_data = data.cloudinit_config.iris_config.rendered
 
   # Block device mappings for the instance
   # block_device_mappings {
