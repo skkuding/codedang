4c40573dabfac7f9d48ff08e8281b46a938e3f16
k1g99 <idmatthew9546@gmail.com>
2023-08-30 08:12:34 +0000

    fix: use task role to send email

--- infra/modules/codedang-infra/ecs.tf
+++ infra/modules/codedang-infra/ecs.tf
@@ -40,7 +40,7 @@ resource "aws_iam_instance_profile" "ecs_task_execution_role" {
 }
 
 resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "Codedang-Api-Task-Execution-Role-Test"
+  name               = "Codedang-Api-Task-Execution-Role"
   assume_role_policy = data.aws_iam_policy_document.ecs_task_execution_role.json
 }
 data "aws_iam_policy_document" "ecs_task_execution_role" {
@@ -83,27 +83,38 @@ resource "aws_iam_policy" "ecs_task_execution_role" {
 }
 
 # IAM Policy to allow sending emails via SES
+resource "aws_iam_role" "ecs_task_role" {
+  name               = "Codedang-API-Task-Role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_role.json
+}
+
+data "aws_iam_policy_document" "ecs_task_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_ses" {
-  role       = aws_iam_role.ecs_task_execution_role.name
+  role       = aws_iam_role.ecs_task_role.name
   policy_arn = aws_iam_policy.ses_send_email.arn
 }
-resource "aws_iam_policy" "ses_send_email" {
-  name        = "AllowSESSendEmail"
-  description = "Allows sending emails via SES"
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = [
-          "ses:SendEmail",
-          "ses:SendRawEmail"
-        ],
-        Effect   = "Allow",
-        Resource = "*"
-      }
-    ]
-  })
+resource "aws_iam_policy" "ses_send_email" {
+  name   = "Codedang-Api-Ses-Send-Email"
+  policy = data.aws_iam_policy_document.ses_send_email.json
+}
+
+data "aws_iam_policy_document" "ses_send_email" {
+  statement {
+    actions   = ["ses:SendEmail", "ses:SendRawEmail"]
+    effect    = "Allow"
+    resources = ["*"]
+  }
 }
 
 ################# cloudinit config #################
