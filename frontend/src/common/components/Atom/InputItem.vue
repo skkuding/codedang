e1b4fc3fad1ba622091b3a0af2c8c29e8672b1cd
Jaemin Choi <1dotolee@gmail.com>
2023-09-22 17:52:28 +0000

    feat(fe): use vee-validate in sign up form

--- frontend/src/common/components/Atom/InputItem.vue
+++ frontend/src/common/components/Atom/InputItem.vue
@@ -1,16 +1,18 @@
-<script setup lang="ts" generic="T extends string | number">
+<script setup lang="ts">
 import { computed } from 'vue'
+
+defineOptions({ inheritAttrs: false })
 
 const props = defineProps<{
   placeholder?: string
   shadow?: boolean
   required?: boolean
   error?: string
-  modelValue?: T
+  modelValue?: string
 }>()
 
 defineEmits<{
-  (e: 'update:modelValue', value: T): void
+  (e: 'update:modelValue', value: string): void
 }>()
 
 const shadowClass = computed(() =>
@@ -23,20 +25,22 @@ const errorClass = computed(() => (props.error ? 'border-red' : ''))
 </script>
 
 <template>
-  <input
-    v-bind="$attrs"
-    :value="modelValue"
-    :placeholder="placeholder"
-    :class="shadowClass + ' ' + errorClass"
-    class="placeholder-gray rounded px-3 py-1 outline-none"
-    @input="
-      $emit('update:modelValue', ($event.target as HTMLInputElement).value)
-    "
-  />
-  <p v-show="required && modelValue" class="text-red text-xs font-bold">
-    {{ placeholder + ' is required' }}
-  </p>
-  <p v-show="error" class="text-red mt-1 text-xs">
-    {{ error }}
-  </p>
+  <div class="rounded-lg px-1">
+    <input
+      v-bind="$attrs"
+      :value="modelValue"
+      :placeholder="placeholder"
+      :class="shadowClass + ' ' + errorClass"
+      class="placeholder-gray w-full rounded px-3 py-1 outline-none"
+      @input="
+        $emit('update:modelValue', ($event.target as HTMLInputElement).value)
+      "
+    />
+    <p v-show="required && modelValue" class="text-red text-xs font-bold">
+      {{ placeholder + ' is required' }}
+    </p>
+    <p v-show="error" class="text-red mt-1 text-xs">
+      {{ error }}
+    </p>
+  </div>
 </template>
