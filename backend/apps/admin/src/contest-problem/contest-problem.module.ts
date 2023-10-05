da9ce27f4ead37f1172a97674d206f2ecbf1adfc
Gyunseo Lee <rbstj0311@naver.com>
2023-10-05 04:40:18 +0000

    chore(be): delete unused res contest-problem res와 workbook-problem res 삭제

--- backend/apps/admin/src/contest-problem/contest-problem.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { ContestProblemResolver } from './contest-problem.resolver'
-import { ContestProblemService } from './contest-problem.service'
-
-@Module({
-  providers: [ContestProblemResolver, ContestProblemService]
-})
-export class ContestProblemModule {}
--- backend/apps/admin/src/workbook-problem/workbook-problem.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { WorkbookProblemResolver } from './workbook-problem.resolver'
-import { WorkbookProblemService } from './workbook-problem.service'
-
-@Module({
-  providers: [WorkbookProblemResolver, WorkbookProblemService]
-})
-export class WorkbookProblemModule {}
