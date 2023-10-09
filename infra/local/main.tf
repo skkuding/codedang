de4ca7646520cc8238d096c72f17e8a0f8d9f80d
k1g99 <idmatthew9546@gmail.com>
2023-10-09 15:28:51 +0000

    chore(infra): add env variables for github login

--- infra/local/main.tf
+++ infra/local/main.tf
@@ -8,14 +8,16 @@ terraform {
 }
 
 module "codedang-infra" {
-  source            = "../modules/codedang-infra"
-  region            = var.region
-  s3_bucket         = var.s3_bucket
-  postgres_username = var.postgres_username
-  postgres_port     = var.postgres_port
-  redis_port        = var.redis_port
-  rabbitmq_username = var.rabbitmq_username
-  rabbitmq_port     = var.rabbitmq_port
+  source               = "../modules/codedang-infra"
+  region               = var.region
+  s3_bucket            = var.s3_bucket
+  postgres_username    = var.postgres_username
+  postgres_port        = var.postgres_port
+  redis_port           = var.redis_port
+  rabbitmq_username    = var.rabbitmq_username
+  rabbitmq_port        = var.rabbitmq_port
+  github_client_id     = var.github_client_id
+  github_client_secret = var.github_client_secret
 }
 
 module "codedang-tf-backend" {
