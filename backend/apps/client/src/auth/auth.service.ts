bd54145a95865aa982bedd6a4175ab286ed237ab
Jaehyeon1020 <kjhkk1020@naver.com>
2023-11-02 13:03:29 +0000

    chore(be): change github signup - github 회원가입 페이지로 redirection하던 로직 - 회원가입 페이지 url만 만들어서 반환하는 것으로 수정

--- backend/apps/client/src/auth/auth.service.ts
+++ backend/apps/client/src/auth/auth.service.ts
@@ -121,10 +121,11 @@ export class AuthService {
     })
 
     if (!userOAuth) {
-      // 소셜 회원가입 페이지로 이동
+      // 소셜 회원가입 페이지 url 전달
       // TODO: 소셜 회원가입 페이지 url 확정되면 여기에 삽입
-      const signUpUrl = `https://codedang.com/social-signup?provider=github&id=${githubUser.githubId}&username=${githubUser.username}`
-      return res.redirect(signUpUrl)
+      return {
+        signUpUrl: `https://codedang.com/social-signup?provider=github&id=${githubUser.githubId}}`
+      }
     }
 
     const user = await this.prisma.user.findFirst({
