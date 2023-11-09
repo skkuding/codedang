b0ad6734bd5f6770d76c48adf21b732c2833da3f
dayongkr <dayongkr@gmail.com>
2023-11-09 23:23:20 +0900

    feat(fe): add docker file for nextjs

--- frontend-client/app/notice/page.tsx
+++ frontend-client/app/notice/page.tsx
@@ -1,3 +1,26 @@
-export default function Notice() {
-  return <main>notice</main>
+interface Notice {
+  id: number
+  title: string
+  createTime: string
+  isFixed: boolean
+  createdBy: string
+}
+
+export default async function Notice() {
+  // const notices = await fetch('http://localhost:5525/api/notice?take=5').then(
+  //   (res) => res.json()
+  // )
+  return (
+    <main>
+      {/* {notices.map((notice: Notice) => {
+        return (
+          <div key={notice.id}>
+            <h1>{notice.title}</h1>
+            <p>{notice.createTime}</p>
+            <p>{notice.createdBy}</p>
+          </div>
+        )
+      })} */}
+    </main>
+  )
 }
--- frontend-client/next.config.js
+++ frontend-client/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://dev.codedang.com/api/:path*',
+        permanent: true
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
