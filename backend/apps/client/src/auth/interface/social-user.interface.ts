788377892c771f5a434537c03f813036cf492b7d
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2023-11-23 20:36:18 +0900

    feat(be): add github login (#897)
    * feat(be): add github login controller
    - 깃허브 로그인 페이지로 redirection하는 controller 생성
    - 이후 로직은 구현 필요
    
    * feat(be): add github login
    - github 로그인 구현
    - 회원가입 로직 추가 필요
    
    * feat(be): add redirect url with username
    
    * feat(be): add github login
    - github login 추가
    - github를 이용한 회원가입 api는 따로 구현
    
    * chore(be): remove email field from social user
    - email은 social user에게 필수적이지 않으므로 회원가입시에 가져오지 않도록 변경
    
    * feat(be): add social sign-up
    - 소셜 회원가입 api
    
    * fix(be): modify social(github) login logic
    - 소셜 유저는 비밀번호를 입력해 로그인하는 것이 아니므로 jwt 발급 로직에서
    - 비밀번호를 확인하는 과정 우회하도록 수정
    
    * chore(be): modify social sign-up url
    
    * chore(infra): add env variables for github login
    
    * chore(be): update pnpm-lock
    
    * fix(be): remove duplicate code
    
    * chore: combine github oauth keys
    
    * chore(be): change github signup
    - github 회원가입 페이지로 redirection하던 로직
    - 회원가입 페이지 url만 만들어서 반환하는 것으로 수정
    
    * chore(be): modify githubStrategy
    - github 로그인으로부터 이메일 받도록 수정
    - 그에 따라 회원가입 시 유저 정보에 이메일 저장
    - 회원가입 페이지 url 쿼리에도 이메일 정보 추가
    
    ---------
    
    Co-authored-by: k1g99 <idmatthew9546@gmail.com>

---
+++ backend/apps/client/src/auth/interface/social-user.interface.ts
@@ -0,0 +1,8 @@
+export interface GithubUser extends Express.User {
+  githubId: string
+  username: string
+  email: string
+}
+
+export interface KakaoUser extends Express.User {}
+export interface GoogleUser extends Express.User {}
---
+++ backend/libs/constants/src/oauth.constants.ts
@@ -0,0 +1,5 @@
+// common
+export const HOMEPAGE_URL = 'http://localhost:4000'
+
+// github
+export const GITHUB_CALLBACK_URL = 'http://localhost:4000/auth/github-callback'
