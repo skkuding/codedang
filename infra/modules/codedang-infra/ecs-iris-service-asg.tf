1746bb3b4e245e2ff00f068aca176b7cc1436ee3
k1g99 <idmatthew9546@gmail.com>
2023-12-19 10:06:20 +0000

    feat: add cloudwatch metric for iris service

--- infra/modules/codedang-infra/ecs-iris-service-asg.tf
+++ infra/modules/codedang-infra/ecs-iris-service-asg.tf
@@ -1,3 +1,42 @@
+###################### cloudwatch alert ######################
+resource "aws_cloudwatch_metric_alarm" "ecs_iris_scale-down" {
+  alarm_name          = "Codedang-Iris-Service-Scale-Down-Alert"
+  comparison_operator = "LessThanThreshold"
+  datapoints_to_alarm = 15
+  evaluation_periods  = 15
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 45
+  alarm_description   = "This metric monitors ec2 cpu utilization and scale down ecs service"
+  alarm_actions       = [aws_appautoscaling_policy.service_asp_iris_scale_down.arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.iris.name
+    ServiceName = aws_ecs_service.iris.name
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "ecs_iris_scale-up" {
+  alarm_name          = "Codedang-Iris-Service-Scale-Up-Alert"
+  comparison_operator = "GreaterThanThreshold"
+  datapoints_to_alarm = 1
+  evaluation_periods  = 1
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 60
+  alarm_description   = "This metric monitors ec2 cpu utilization and scale up ecs service"
+  alarm_actions       = [aws_appautoscaling_policy.service_asp_iris_scale_up.arn]
+
+  dimensions = {
+    ClusterName = aws_ecs_cluster.iris.name
+    ServiceName = aws_ecs_service.iris.name
+  }
+}
+
 ###################### Service Auto Scaling #####################
 resource "aws_appautoscaling_target" "service_asg_iris" {
   max_capacity       = 8
@@ -6,8 +45,6 @@ resource "aws_appautoscaling_target" "service_asg_iris" {
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
-
-
 
 resource "aws_appautoscaling_policy" "service_asp_iris_scale_up" {
   name               = "scale-up"
