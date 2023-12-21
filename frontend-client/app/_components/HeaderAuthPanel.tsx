bddf99577ed45593d66f099cab05b782b7d66634
Jaemin Choi <1dotolee@gmail.com>
2023-12-21 20:50:13 +0900

    feat(fe): refactor header and auth modal (#1029)
    * style(fe): declare component in function style
    
    * refactor(fe): move local components into app
    
    한 곳에서만 사용되는 component들은 page와 같은 경로에 둬 코드 응집성을 높인다.
    
    * chore: use esm module in tailwindcss config
    
    * chore(fe): replace png with svg
    
    성능 최적화를 위해 SVG 대체 가능한 경우 SVG를 사용합니다.
    
    * feat(fe): resize signin modal width
    
    * feat(fe): add more transparency to dialog background
    
    Auth 모달 배경이 다소 불투명한 것 같아 투명도를 조절하였습니다.
    
    * feat(fe): remove input ring offset
    
    * feat(fe): remove sign in sign
    
    로그인 페이지에 'sign in' 문구를 없애고 로고 크기를 조절합니다.
    
    * feat(fe): modify labels
    
    안내 문구에 대문자를 적용하고, 이해를 위해 기호를 일부 추가합니다.
    
    * feat(fe): add hover effect to third-party login buttons
    
    * feat(fe): add padding and margins
    
    * fix: change tailwind config file ext for shadcn
    
    * feat(fe): organize components under folder
    
    ---------
    
    Co-authored-by: Dayong Lee <dayongkr@gmail.com>

--- frontend-client/app/_components/HeaderAuthPanel.tsx
+++ frontend-client/app/_components/HeaderAuthPanel.tsx
@@ -1,11 +1,11 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 import { RxHamburgerMenu } from 'react-icons/rx'
-import { Button } from '../ui/button'
-import { Dialog, DialogContent, DialogTrigger } from '../ui/dialog'
-import Signin from './Signin'
+import SignIn from './SignIn'
 
-const SignInUpButton = () => {
+export default function HeaderAuthPanel() {
   return (
     <div className="ml-2 flex items-center gap-2">
       <Dialog>
@@ -14,11 +14,11 @@ const SignInUpButton = () => {
             variant={'outline'}
             className="hidden border-none px-3 py-1 text-base font-semibold md:block"
           >
-            Sign in
+            Sign In
           </Button>
         </DialogTrigger>
-        <DialogContent className="max-w-sm">
-          <Signin />
+        <DialogContent className="max-w-[22rem]">
+          <SignIn />
         </DialogContent>
       </Dialog>
       <Dialog>
@@ -27,16 +27,14 @@ const SignInUpButton = () => {
             variant={'outline'}
             className="hidden px-3 py-1 text-base font-bold md:block"
           >
-            Sign up
+            Sign Up
           </Button>
         </DialogTrigger>
-        <DialogContent className="max-w-sm">
-          <Signin />
+        <DialogContent className="max-w-[22rem]">
+          <SignIn />
         </DialogContent>
       </Dialog>
       <RxHamburgerMenu size="30" className="md:hidden" />
     </div>
   )
 }
-
-export default SignInUpButton
