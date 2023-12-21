953a45314bb433d32dba90919d0b0a5a0749d8e1
JungangHighschool <lhs109802@g.skku.edu>
2023-12-21 07:37:13 +0000

    fix(be): prisma referential action modification

---
+++ backend/prisma/migrations/20231221073406_/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "submssion_result" DROP CONSTRAINT "submssion_result_problem_test_case_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "submssion_result" ADD CONSTRAINT "submssion_result_problem_test_case_id_fkey" FOREIGN KEY ("problem_test_case_id") REFERENCES "problem_testcase"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
