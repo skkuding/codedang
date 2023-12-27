582ea124cf21f1d488e91a835bb562b4331ae895
Sori Lim <aint.imsorry@gmail.com>
2023-12-27 17:32:59 +0000

    feat: apply border only to card-footer

--- frontend-client/app/_components/ContestCard.tsx
+++ frontend-client/app/_components/ContestCard.tsx
@@ -5,6 +5,7 @@ import {
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
+import { cn } from '@/lib/utils'
 import type { Contest } from '@/types/type'
 import dayjs from 'dayjs'
 import Badge from './Badge'
@@ -33,33 +34,32 @@ export default function ContestCard({ contest }: Props) {
   const endTime = dayjs(contest.endTime).format('MMM DD, YYYY')
 
   return (
-    <div className="w-1/3 px-2 py-5">
-      <Card
-        className={`flex h-56 flex-col justify-between bg-gray-500 ${
-          variants.style[contest.badge]
-        }`}
-      >
-        <Badge badge={contest.badge}></Badge>
-        <div>
-          <CardHeader className="p-5">
-            <CardDescription className="font-bold text-white">
-              {variants.description[contest.badge]}
-            </CardDescription>
-            <CardTitle className="overflow-hidden text-ellipsis whitespace-nowrap text-3xl text-white">
-              {contest.title}
-            </CardTitle>
-          </CardHeader>
-          <CardFooter className="justify-between rounded-b-3xl bg-white text-sm">
-            <p>{`${startTime} - ${endTime}`}</p>
-            {contest.badge == 'ongoing' && (
-              <div className="flex gap-1 text-red-500">
-                <p>-</p>
-                <TimeDiff timeRef={contest.endTime}></TimeDiff>
-              </div>
-            )}
-          </CardFooter>
-        </div>
-      </Card>
-    </div>
+    <Card
+      className={cn(
+        'flex h-56 flex-col justify-between border-none bg-gray-500',
+        variants.style[contest.badge]
+      )}
+    >
+      <Badge badge={contest.badge}></Badge>
+      <div>
+        <CardHeader className="p-5">
+          <CardDescription className="font-bold text-white">
+            {variants.description[contest.badge]}
+          </CardDescription>
+          <CardTitle className="overflow-hidden text-ellipsis whitespace-nowrap text-3xl text-white">
+            {contest.title}
+          </CardTitle>
+        </CardHeader>
+        <CardFooter className="justify-between rounded-b-3xl border border-gray-300 bg-white px-5 text-sm">
+          <p>{`${startTime} - ${endTime}`}</p>
+          {contest.badge == 'ongoing' && (
+            <div className="flex gap-1 text-red-500">
+              <p>-</p>
+              <TimeDiff timeRef={contest.endTime}></TimeDiff>
+            </div>
+          )}
+        </CardFooter>
+      </div>
+    </Card>
   )
 }
--- frontend-client/app/_components/ProblemCard.tsx
+++ frontend-client/app/_components/ProblemCard.tsx
@@ -8,22 +8,18 @@ interface Props {
 
 export default function ProblemCard({ problem }: Props) {
   return (
-    <div className="w-1/3 px-2 py-5">
-      <Card className="flex h-56 flex-col justify-between bg-gray-500">
-        <Badge badge={problem.difficulty} />
-        <div>
-          <CardHeader className="p-5">
-            <CardTitle className="overflow-hidden text-ellipsis whitespace-nowrap text-3xl text-white">
-              {problem.title}
-            </CardTitle>
-          </CardHeader>
-          <CardFooter className="rounded-b-3xl bg-white text-sm">
-            <p>
-              Solved Rate: {/* TODO: 백엔드 API 변경되면 AC Rate 값 추가 */}
-            </p>
-          </CardFooter>
-        </div>
-      </Card>
-    </div>
+    <Card className="flex h-56 flex-col justify-between border-none bg-gray-500">
+      <Badge badge={problem.difficulty} />
+      <div>
+        <CardHeader className="p-5">
+          <CardTitle className="overflow-hidden text-ellipsis whitespace-nowrap text-3xl text-white">
+            {problem.title}
+          </CardTitle>
+        </CardHeader>
+        <CardFooter className="rounded-b-3xl border border-gray-300 bg-white text-sm">
+          <p>Solved Rate: {/* TODO: 백엔드 API 변경되면 AC Rate 값 추가 */}</p>
+        </CardFooter>
+      </div>
+    </Card>
   )
 }
