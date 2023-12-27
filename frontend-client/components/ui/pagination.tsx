04d519c4adc3cf3f3436ef6695a0d792ab93ffef
dayongkr <dayongkr@gmail.com>
2023-12-28 00:21:07 +0900

    feat(fe): remove icon in pagination next & prev button

--- frontend-client/components/ui/pagination.tsx
+++ frontend-client/components/ui/pagination.tsx
@@ -66,11 +66,10 @@ const PaginationPrevious = ({
   <PaginationLink
     aria-label="Go to previous page"
     size="default"
-    className={cn('gap-1 pl-2.5', className)}
+    className={className}
     {...props}
   >
     <ChevronLeft className="h-4 w-4" />
-    <span>Previous</span>
   </PaginationLink>
 )
 PaginationPrevious.displayName = 'PaginationPrevious'
@@ -82,10 +81,9 @@ const PaginationNext = ({
   <PaginationLink
     aria-label="Go to next page"
     size="default"
-    className={cn('gap-1 pr-2.5', className)}
+    className={className}
     {...props}
   >
-    <span>Next</span>
     <ChevronRight className="h-4 w-4" />
   </PaginationLink>
 )
