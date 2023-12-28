05016937ed2d61c45e8da7dd17d2a7c39e00444e
dayongkr <dayongkr@gmail.com>
2023-12-28 11:28:35 +0900

    feat(fe): add tailwind typography

--- frontend-client/tailwind.config.ts
+++ frontend-client/tailwind.config.ts
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/naming-convention */
+import typography from '@tailwindcss/typography'
 import type { Config } from 'tailwindcss'
 import animate from 'tailwindcss-animate'
 
@@ -38,5 +39,5 @@ export default {
       }
     }
   },
-  plugins: [animate]
+  plugins: [animate, typography]
 } satisfies Config
