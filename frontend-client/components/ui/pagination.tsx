a5be5eb8f282bfb1e4e5454c97be06ed9aabf286
Sori Lim <aint.imsorry@gmail.com>
2023-12-28 07:00:53 +0000

    feat: import carousel component from shadcn

--- frontend-client/components/ui/pagination.tsx
+++ frontend-client/components/ui/pagination.tsx
@@ -1,4 +1,4 @@
-import { ButtonProps, buttonVariants } from '@/components/ui/button'
+import { type ButtonProps, buttonVariants } from '@/components/ui/button'
 import { cn } from '@/lib/utils'
 import { ChevronLeft, ChevronRight, MoreHorizontal } from 'lucide-react'
 import * as React from 'react'
