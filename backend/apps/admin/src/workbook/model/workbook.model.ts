ed8dec11d6b871424cbf290933e33e4b52040eca
Gyunseo Lee <rbstj0311@naver.com>
2024-01-01 16:50:37 +0000

    chore(be): change comment

--- backend/apps/admin/src/workbook/model/workbook.model.ts
+++ backend/apps/admin/src/workbook/model/workbook.model.ts
@@ -1,7 +1,7 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql'
 import { Workbook } from '@admin/@generated'
 
-// TODO: e2e 테스트 오류로 인해 Workbook을 Extend하지 않고, 직접 WorkbookModel ObjectType을 정의해야 함
+// TODO: github action 테스트 오류로 인해 Workbook을 Extend하지 않고, 직접 WorkbookModel ObjectType을 정의해야 함
 @ObjectType()
 export class WorkbookModel extends Workbook {
   @Field(() => Int, { nullable: false })
