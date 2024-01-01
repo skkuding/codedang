3dbb5500d1b0d52394934ef3e4dff54e09d1f393
Gyunseo Lee <rbstj0311@naver.com>
2024-01-01 08:23:43 +0000

    fix(be): fix workbookdetail object type for getworkbook query

--- backend/apps/admin/src/workbook/model/workbook.output.ts
+++ backend/apps/admin/src/workbook/model/workbook.output.ts
@@ -1,12 +1,13 @@
 import { ObjectType, Field } from '@nestjs/graphql'
+import { Submission } from '@admin/@generated'
 import { Problem } from '@admin/@generated/problem/problem.model'
-import { Submission } from '@admin/@generated/submission/submission.model'
-import { Workbook } from '@admin/@generated/workbook/workbook.model'
+import { WorkbookModel } from './workbook.model'
 
 @ObjectType()
-export class WorkbookDetail extends Workbook {
-  @Field(() => [Problem], { nullable: false })
-  problems!: [Problem]
-  @Field(() => [Submission], { nullable: false })
-  submissions!: [Submission]
+export class WorkbookDetail extends WorkbookModel {
+  @Field(() => [Problem], { nullable: true })
+  problems: Problem[]
+
+  @Field(() => [Submission], { nullable: true })
+  submissions: Submission[]
 }
