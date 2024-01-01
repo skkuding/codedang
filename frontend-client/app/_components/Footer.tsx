b76b8fd17bd1b64c20a247835fedadb5436f8e0f
youznn <leejin914418@gmail.com>
2024-01-01 14:46:20 +0900

    feat(fe): add hover color , new tab link and delete toaster

--- frontend-client/app/_components/Footer.tsx
+++ frontend-client/app/_components/Footer.tsx
@@ -1,9 +1,6 @@
 'use client'
 
-import { Toaster } from '@/components/ui/toaster'
 import { useToast } from '@/components/ui/use-toast'
-import Link from 'next/link'
-import { useState } from 'react'
 import { IoIosLink } from 'react-icons/io'
 import { RiGithubFill } from 'react-icons/ri'
 import { RiKakaoTalkFill } from 'react-icons/ri'
@@ -11,11 +8,9 @@ import { TbMailFilled } from 'react-icons/tb'
 
 export default function Footer() {
   const { toast } = useToast()
-  const [copied, setCopied] = useState(false)
   const copyToClipboard = async () => {
     try {
-      await navigator.clipboard.writeText('skkucodingplatform@gmail.com/')
-      setCopied(true)
+      await navigator.clipboard.writeText('skkucodingplatform@gmail.com')
       toast({
         description: 'Email Copied!',
         variant: 'success'
@@ -26,38 +21,50 @@ export default function Footer() {
   }
   return (
     <footer className=" items-center py-7">
-      <div className="flex h-20 w-full flex-col items-center justify-center gap-2 bg-gray-100">
+      <div className="flex h-20 w-full flex-col items-center justify-center gap-2 bg-gray-100 text-gray-500">
         <div className="flex flex-row items-center justify-center gap-4">
-          <Link href="https://pf.kakao.com/_UKraK/chat">
+          <a
+            href="https://pf.kakao.com/_UKraK/chat"
+            rel="noreferrer noopener"
+            target="_blank"
+          >
             <RiKakaoTalkFill
-              className="cursor-pointer text-gray-500"
+              className="cursor-pointer hover:text-gray-400"
               size="24"
             />
-          </Link>
+          </a>
           <TbMailFilled
             onClick={copyToClipboard}
-            className="cursor-pointer text-gray-500"
+            className="cursor-pointer hover:text-gray-400"
             size="24"
           />
-          <Link href="https://github.com/skkuding/">
-            <RiGithubFill className="cursor-pointer text-gray-500" size="24" />
-          </Link>
-          <Link href="https://skkuding.dev/">
-            <IoIosLink className="cursor-pointer text-gray-500" size="23" />
-          </Link>
+          <a
+            rel="noreferrer noopener"
+            target="_blank"
+            href="https://github.com/skkuding/"
+          >
+            <RiGithubFill
+              className="cursor-pointer hover:text-gray-400"
+              size="24"
+            />
+          </a>
+          <a
+            rel="noreferrer noopener"
+            target="_blank"
+            href="https://skkuding.dev/"
+          >
+            <IoIosLink
+              className="cursor-pointer hover:text-gray-400"
+              size="23"
+            />
+          </a>
         </div>
         <div>
-          <p className="text-sm font-bold text-gray-500">
+          <p className="text-sm font-bold">
             (c) SKKUDING &nbsp;&nbsp;/ Since 2021
           </p>
         </div>
       </div>
-      {copied && (
-        <>
-          <Toaster />
-          {/* todo: 다른 토스터에서 반응하지 않게 조정*/}
-        </>
-      )}
     </footer>
   )
 }
