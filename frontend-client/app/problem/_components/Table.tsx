313c386a838f7d899e7ce0c4390408b9705d1c64
youznn <leejin914418@gmail.com>
2024-01-02 22:03:07 +0900

    feat(fe): set fixed width, add responsiveness, include hover effect

--- frontend-client/app/problem/_components/Table.tsx
+++ frontend-client/app/problem/_components/Table.tsx
@@ -24,7 +24,6 @@ interface Problem {
   acceptedRate: number
   tags?: string[]
   info?: string
-  createTime?: string
 }
 
 interface ProblemProps {
@@ -55,7 +54,9 @@ export default function Problem({ data, currentPage }: ProblemProps) {
       accessorKey: 'title',
       cell: ({ row }) => {
         return (
-          <span className="text-sm md:text-base">{row.original.title}</span>
+          <p className="text-left">
+            <span className="text-sm md:text-base">{row.original.title}</span>
+          </p>
         )
       }
     },
@@ -64,16 +65,19 @@ export default function Problem({ data, currentPage }: ProblemProps) {
       accessorKey: 'difficulty',
       cell: ({ row }) => {
         return (
-          <span className="flex items-center justify-between text-sm  md:text-base">
+          <span className="flex items-center justify-center gap-1 text-center text-sm  md:gap-3 md:text-base">
             <div
               className={cn(
                 'h-3 w-3 rounded-full',
                 LevelColors[row.original.difficulty as keyof typeof LevelColors]
               )}
             ></div>
-            {row.original.difficulty.slice(0, 5) +
-              ' ' +
-              row.original.difficulty.slice(5)}
+            <div className="hidden md:block">
+              {'Level ' + row.original.difficulty.slice(5)}
+            </div>
+            <div className="md:hidden">
+              {'Lv ' + row.original.difficulty.slice(5)}
+            </div>
           </span>
         )
       }
@@ -116,14 +120,22 @@ export default function Problem({ data, currentPage }: ProblemProps) {
   })
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
+    <div className="rounded-md">
+      <Table className="table-fixed">
+        <TableHeader className="border-b-2 border-gray-700 text-gray-500">
           {table.getHeaderGroups().map((headerGroup) => (
             <TableRow key={headerGroup.id}>
               {headerGroup.headers.map((header) => {
                 return (
-                  <TableHead key={header.id}>
+                  <TableHead
+                    className={cn(
+                      'font-bold',
+                      header.column.columnDef.header === 'Title'
+                        ? 'w-[40%] text-sm md:w-[50%] md:text-base'
+                        : 'w-[20%] text-center text-sm md:w-[25%] md:text-base'
+                    )}
+                    key={header.id}
+                  >
                     {header.isPlaceholder
                       ? null
                       : flexRender(
@@ -142,14 +154,14 @@ export default function Problem({ data, currentPage }: ProblemProps) {
               <TableRow
                 key={row.id}
                 data-state={row.getIsSelected() && 'selected'}
-                className="cursor-pointer"
+                className="cursor-pointer text-center text-gray-700 hover:font-bold "
                 onClick={() => {
                   console.log(row.original.id)
                   console.log(currentPage)
                 }}
               >
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
+                  <TableCell className="px-1" key={cell.id}>
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
