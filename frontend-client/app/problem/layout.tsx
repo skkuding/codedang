00bd3cfd06a6c2167ed2b8da9c1a846277b074e3
Sori Lim <aint.imsorry@gmail.com>
2024-01-02 07:03:47 +0000

    feat: set up page

---
+++ frontend-client/app/problem/layout.tsx
@@ -0,0 +1,12 @@
+import Cover from '@/components/Cover'
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Cover title="Problem" description="All the problems by CODEDANG" />
+      <div className="flex w-full max-w-5xl flex-col gap-5 p-5 py-8 md:py-12">
+        {children}
+      </div>
+    </>
+  )
+}
--- frontend-client/app/problem/page.tsx
+++ frontend-client/app/problem/page.tsx
@@ -1,3 +1,11 @@
+import { Pagination } from '@/components/ui/pagination'
+import Table from './_components/Table'
+
 export default function Problem() {
-  return <h2>problem</h2>
+  return (
+    <>
+      <Table></Table>
+      <Pagination></Pagination>
+    </>
+  )
 }
