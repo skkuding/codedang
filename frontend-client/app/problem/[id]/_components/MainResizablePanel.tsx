b34f82888c6530267366fa764f9f759900f67e9e
dayongkr <dayongkr@gmail.com>
2024-01-03 17:41:15 +0900

    fix: remove input output section

--- frontend-client/app/problem/[id]/_components/MainResizablePanel.tsx
+++ frontend-client/app/problem/[id]/_components/MainResizablePanel.tsx
@@ -11,13 +11,7 @@ export default function MainResizablePanel() {
     <ResizablePanelGroup direction="horizontal" className="h-full">
       <ResizablePanel defaultSize={35}></ResizablePanel>
       <ResizableHandle withHandle />
-      <ResizablePanel defaultSize={65}>
-        <ResizablePanelGroup direction="vertical">
-          <ResizablePanel defaultSize={75}></ResizablePanel>
-          <ResizableHandle withHandle />
-          <ResizablePanel defaultSize={25}></ResizablePanel>
-        </ResizablePanelGroup>
-      </ResizablePanel>
+      <ResizablePanel defaultSize={65}></ResizablePanel>
     </ResizablePanelGroup>
   )
 }
