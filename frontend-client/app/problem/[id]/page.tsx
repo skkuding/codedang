398a530dcf099543f9391717f32c3a5f333c3dc4
dayongkr <dayongkr@gmail.com>
2024-01-03 17:47:49 +0900

    chore: refactor

--- frontend-client/app/problem/[id]/page.tsx
+++ frontend-client/app/problem/[id]/page.tsx
@@ -6,14 +6,17 @@ import MainResizablePanel from './_components/MainResizablePanel'
 export default async function ProblemEditor() {
   return (
     <div className="flex h-dvh w-full flex-col bg-slate-700 text-white">
-      <header className="flex h-12 shrink-0 justify-between bg-slate-800">
-        <div className="flex items-center justify-center text-slate-500">
-          <Image src={icon} alt="코드당" width={30} className="mx-7" />
-          Problem <IoIosArrowForward className="size-6" />
-          {/* Problem Name 임의 */}
-          <span className="text-white">가파른 경사</span>
+      <header className="flex h-12 shrink-0 justify-between bg-slate-800 px-5">
+        <div className="flex items-center justify-center gap-7 font-bold text-slate-500">
+          <Image src={icon} alt="코드당" width={30} />
+          <div className="flex items-center gap-1">
+            <p>Problem</p>
+            <IoIosArrowForward className="size-6" />
+            {/* Problem Name 임의 */}
+            <h1 className="text-lg text-white">가파른 경사</h1>
+          </div>
         </div>
-        <div className="mr-4 flex items-center justify-center">
+        <div className="flex items-center justify-center">
           <span className="text-slate-500">ENG</span>
           <span className="mx-2">l</span>
         </div>
