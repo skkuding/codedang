4e6391078408a3e25e72772b6976b99482a8ede1
EliseCho <dbwlspdlqj@naver.com>
2024-01-03 13:28:14 +0900

    feat(fe): change signup modal to new design (#1104)
    * feat(fe): add signup page0
    
    * fix(fe): remove email inputs, change placeholder texts
    
    * style(fe): move logo, add margin
    
    * style(fe): change password input width, change gaps
    
    * feat(fe): added first page of signUp
    
    * feat(fe): added signup email verification modal
    
    * fix(fe): change error message display to show only on focus
    
    * style(fe): add input shadow
    
    * fix(fe): added validation trigger, made to verify code upon change
    
    * fix(fe): fixed nextModal to be function
    
    * fix(fe): email verify, welcome in signup, signupregister not done
    
    * feat(fe): add input condition, change button color based on condition
    
    * fix(fe): remove toast
    
    * chore(fe): removed comments, console logs
    
    * fix(fe): change zod schema password to 8-20
    
    * feat(fe): added error messages, not done, waiting for backend to fix
    
    * style(fe): added back arrow, fixed css of login in signup
    
    * style(fe): styling of signup welcome modal done
    
    * fix(fe): fixed margin of SignUpWelcome
    
    * style(fe): finished design of SignUpEmailVerify
    
    * fix(fe): made email content displayable in code verification
    
    * chore(fe): add console log in username check button
    
    * fix(fe): hid email sent msg after email verification code is right
    
    * feat(fe): added signup register
    
    * fix(fe): add signup text, change submit button condition
    
    * fix(fe): add realname condition
    
    * fix(fe): change input condition fixed when invalid and unfocused
    
    * fix(fe): remove unnecessary div wrapper around signupregister
    
    * fix(fe): removed console logs, declared types
    
    * fix(fe): declared type in SignUpRegister
    
    * fix(fe): change 4 boolean states to 1 number state
    
    * fix(fe): fixed error msg overriding in email verification
    
    * fix(fe): removed validation states, added trigger onChange
    
    * fix(fe): removed duplicated error msgs, removed click states
    
    * fix(fe): removed unused modalForm, specified type of formData
    
    * fix(fe): removed unused codes, removed comments
    
    * fix(fe): removed console logs, removed type any in welcome, declared use client
    
    * fix(fe): removed unused var error
    
    * fix(fe): removed unused imports
    
    * feat(fe): make modal to be closed after submitting, added toast upon signup success and fail
    
    * fix(fe): fixed error toast description to title
    
    * fix(fe): removed unnecessary props and errors
    
    * fix(fe): assign validation for each inputs
    
    * fix(fe): added username disable state
    
    * fix(fe): removed realname available check msg
    
    * fix(fe): change register button disabled condition
    
    * fix(fe): change to show available text after checking username
    
    * fix(fe): simplified data used in sign up
    
    * fix(fe): move toaster to root layout
    
    Co-authored-by: EliseCho13 <dbwlspdlqj@naver.com>
    
    * fix(fe): added disable feature in signupregister buttons, added disable color
    
    * fix(fe): removed register input shadow
    
    * style(fe): change hex color to tailwind
    
    * fix(fe): merge main to 1068, remove toast
    
    * feat(fe): username check api connected
    
    * chore(fe): pnpm lock change
    
    * fix(fe): removed unused shadcn toast
    
    * fix(fe): add disableUsername condition to register button
    
    * fix(fe): fixed prettier error in layout
    
    * fix: pnpm lock
    
    * fix(fe): remove default toaster
    
    * fix(fe): remove * from error messages
    
    * fix(fe): remove unneccesary fragment
    
    * fix(fe): specify content type in the headers when fetching
    
    * fix(fe): change codedang logo alt to english
    
    * fix(fe): remove unnecessary div in signupwelcome
    
    * style(fe): remove margin bottom of social login wrapper
    
    * fix(fe): change log in to sign in
    
    * fix(fe): combine 3 fa icons import to 1
    
    * fix(fe): specify root folder path using alias
    
    * fix(fe): import formdata from signup
    
    * fix(fe): move refine conditions for username and realname to object in zod
    
    * fix(fe): add type in formdata import
    
    ---------
    
    Co-authored-by: b0xercat <seojin3154@naver.com>

--- frontend-client/app/_components/SignUp.tsx
+++ frontend-client/app/_components/SignUp.tsx
@@ -1,290 +1,67 @@
 'use client'
 
-import { baseUrl } from '@/lib/vars'
 import CodedangLogo from '@/public/codedang.svg'
-import { zodResolver } from '@hookform/resolvers/zod'
 import Image from 'next/image'
 import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
-import { FaPaperPlane } from 'react-icons/fa'
-import { FaCheck } from 'react-icons/fa'
-import { FaEyeSlash } from 'react-icons/fa'
-import { FaEye } from 'react-icons/fa'
-import { toast } from 'sonner'
-import { z } from 'zod'
-import { Button } from '../../components/ui/button'
-import { Input } from '../../components/ui/input'
+import { IoMdArrowBack } from 'react-icons/io'
+import SignUpEmailVerify from './SignUpEmailVerify'
+import SignUpRegister from './SignUpRegister'
+import SignUpWelcome from './SignUpWelcome'
 
-interface SignUpFormInput {
-  username: string
+export interface FormData {
   email: string
   verificationCode: string
-  realName: string
-  password: string
-  passwordAgain: string
+  headers: {
+    'email-auth': string
+  }
 }
 
-const schema = z
-  .object({
-    username: z.string().min(3).max(10),
-    email: z.string().email(),
-    verificationCode: z.string().min(6).max(6),
-    realName: z.string().min(1).max(20),
-    password: z.string().min(8).max(32),
-    passwordAgain: z.string().min(8).max(32)
-  })
-  .refine(
-    (data: { password: string; passwordAgain: string }) =>
-      data.password === data.passwordAgain,
-    {
-      message: 'Passwords do not match',
-      path: ['passwordAgain']
-    }
-  )
-  .refine(
-    (data: { username: string }) => /^[a-zA-Z0-9]+$/.test(data.username),
-    {
-      message: 'Username can only contain alphabets and numbers',
-      path: ['username']
-    }
-  )
-  .refine((data: { realName: string }) => /^[a-zA-Z\s]+$/.test(data.realName), {
-    message: 'Real name can only contain alphabets',
-    path: ['realName']
-  })
-
 export default function SignUp() {
-  const [sentEmail, setSentEmail] = useState<boolean>(false)
-  const [emailVerified, setEmailVerified] = useState<boolean>(false)
-  const [emailAuthToken, setEmailAuthToken] = useState<string>('')
-  const [passwordShow, setPasswordShow] = useState<boolean>(false)
-  const [passwordAgainShow, setPasswordAgainShow] = useState<boolean>(false)
-
-  const {
-    handleSubmit,
-    register,
-    getValues,
-    formState: { errors }
-  } = useForm<SignUpFormInput>({
-    resolver: zodResolver(schema)
+  const [modalPage, setModalPage] = useState(0)
+  const [formData, setFormData] = useState({
+    email: '',
+    verificationCode: '',
+    headers: {
+      'email-auth': ''
+    }
   })
 
-  const onSubmit = async (data: {
-    username: string
-    email: string
-    realName: string
-    password: string
-  }) => {
-    try {
-      await fetch(baseUrl + '/user/sign-up', {
-        method: 'POST',
-        headers: {
-          'email-auth': emailAuthToken,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          username: data.username,
-          email: data.email,
-          realName: data.realName,
-          password: data.password
-        })
-      })
-      toast.success('Sign up succeed!')
-    } catch (error) {
-      toast.error('Sign up failed!')
-    }
+  const nextModal = () => {
+    setModalPage(modalPage + 1)
   }
 
-  const sendCodeToEmail = async (email: string) => {
-    if (!sentEmail) {
-      await fetch(baseUrl + '/email-auth/send-email/register-new', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          email: email
-        })
-      })
-        .then((res) => {
-          if (res.status === 409) {
-            toast.error('You have already signed up!')
-          } else if (res.status === 201) {
-            setSentEmail(true)
-          }
-        })
-        .catch(() => {
-          toast.error('Sending verification code failed!')
-        })
-    } else {
-      toast.error('You have already sent an email!')
-    }
-  }
-
-  const verifyCode = async (email: string, verificationCode: string) => {
-    if (!emailVerified) {
-      try {
-        const response = await fetch(baseUrl + '/email-auth/verify-pin', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            pin: verificationCode,
-            email: email
-          })
-        })
-        if (response.status === 200) {
-          setEmailVerified(true)
-          setEmailAuthToken(response.headers.get('email-auth') || '')
-        } else {
-          toast.error('Verification code is not valid!')
-        }
-      } catch (error) {
-        toast.error('Email verification failed!')
-      }
-    } else {
-      toast.error('You have already verified code!')
-    }
+  const backModal = () => {
+    setModalPage(modalPage - 1)
   }
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <Image src={CodedangLogo} alt="코드당" width={70} className="mb-5" />
+      {!(modalPage === 0) && (
+        <button
+          onClick={backModal}
+          className="absolute left-4 top-4 h-4 w-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-gray-100 data-[state=open]:text-gray-500 dark:ring-offset-gray-950 dark:focus:ring-gray-300 dark:data-[state=open]:bg-gray-800 dark:data-[state=open]:text-gray-400"
+        >
+          <IoMdArrowBack />
+        </button>
+      )}
+      <Image
+        src={CodedangLogo}
+        alt="codedang"
+        width={70}
+        className="absolute left-10 top-10"
+      />
 
-      <form
-        className="flex w-60 flex-col gap-4"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div>
-          <Input placeholder="User Id" {...register('username')} />
-          {errors.username && (
-            <p className="mt-1 text-xs text-red-500">
-              {errors.username?.message}
-            </p>
-          )}
-        </div>
-
-        <div>
-          <div className="flex gap-2">
-            <Input
-              id="email"
-              type="email"
-              placeholder="Email Address"
-              {...register('email')}
-            />
-            <Button
-              onClick={() => {
-                const { email } = getValues()
-                sendCodeToEmail(email)
-              }}
-              className="flex aspect-square w-12 items-center justify-center rounded-md bg-[#2279FD]"
-            >
-              <FaPaperPlane className="text-white" size="20" />
-            </Button>
-          </div>
-          {errors.email && (
-            <p className="mt-1 text-xs text-red-500">{errors.email?.message}</p>
-          )}
-          {sentEmail && (
-            <p className="mt-1 text-xs text-blue-500">
-              Email verification code has been sent!
-            </p>
-          )}
-        </div>
-
-        <div>
-          <div className="flex gap-1">
-            <Input
-              type="number"
-              placeholder="Verification Code"
-              {...register('verificationCode')}
-            />
-            <Button
-              onClick={() => {
-                const { email } = getValues()
-                const { verificationCode } = getValues()
-                verifyCode(email, verificationCode)
-              }}
-              className="flex aspect-square w-12 items-center justify-center rounded-md bg-[#2279FD]"
-            >
-              <FaCheck className="text-white" size="20" />
-            </Button>
-          </div>
-          {errors.verificationCode && (
-            <p className="mt-1 text-xs text-red-500">
-              {errors.verificationCode?.message}
-            </p>
-          )}
-          {emailVerified && (
-            <p className="text-blue text-xs font-bold">
-              Email has been verified!
-            </p>
-          )}
-        </div>
-
-        <div>
-          <Input placeholder="Real Name" {...register('realName')} />
-          {errors.realName && (
-            <p className="mt-1 text-xs text-red-500">
-              {errors.realName?.message}
-            </p>
-          )}
-        </div>
-
-        <div>
-          <div className="flex justify-between gap-2">
-            <Input
-              className="w-52"
-              placeholder="Password"
-              {...register('password')}
-              type={passwordShow ? 'text' : 'password'}
-            />
-            <span
-              className="mt-3"
-              onClick={() => setPasswordShow(!passwordShow)}
-            >
-              {passwordShow ? <FaEyeSlash /> : <FaEye />}
-            </span>
-          </div>
-          {errors.password && (
-            <p className="mt-1 text-xs text-red-500">
-              {errors.password?.message}
-            </p>
-          )}
-        </div>
-
-        <div>
-          <div className="flex justify-between gap-2">
-            <Input
-              className="w-52"
-              {...register('passwordAgain')}
-              placeholder="Password Check"
-              type={passwordAgainShow ? 'text' : 'password'}
-            />
-            <span
-              className="mt-3"
-              onClick={() => setPasswordAgainShow(!passwordAgainShow)}
-            >
-              {passwordAgainShow ? <FaEyeSlash /> : <FaEye />}
-            </span>
-          </div>
-          {errors.passwordAgain && (
-            <p className="mt-1 text-xs text-red-500">
-              {errors.passwordAgain?.message}
-            </p>
-          )}
-        </div>
-
-        <Button color="blue" onClick={() => {}} type="submit">
-          Register
-        </Button>
-      </form>
-      <div className="text-gray-dark mt-6 flex flex-col items-center text-sm">
+      {modalPage === 0 && <SignUpWelcome nextModal={nextModal} />}
+      {modalPage === 1 && (
+        <SignUpEmailVerify nextModal={nextModal} setFormData={setFormData} />
+      )}
+      {modalPage === 2 && <SignUpRegister formData={formData} />}
+      <div className="flex items-center text-sm text-gray-500">
         Already have an account?
-        <a className="text-gray-dark hover:text-gray-dark/80 active:text-gray-dark/60 w-fit cursor-pointer text-sm underline">
-          Log In
+        <a className="ml-5 w-fit cursor-pointer text-sm underline hover:text-black active:text-black">
+          Sign In
         </a>
       </div>
     </div>
   )
 }
-
-// 모달 전환하는 방식으로 수정 예정
-// 모달 컴포넌트 분리 예정
