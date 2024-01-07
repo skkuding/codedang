599e93bdf84f3258dd4370d695490cd6388bb8db
b0xercat <seojin3154@naver.com>
2024-01-07 16:38:23 +0900

    feat(fe): connet contest api

--- frontend-client/app/(main)/contest/page.tsx
+++ frontend-client/app/(main)/contest/page.tsx
@@ -1,3 +1,24 @@
-export default function Contest() {
-  return <h2>contest</h2>
+import { baseUrl } from '@/lib/vars'
+import type { Contest } from '@/types/type'
+import ContestTable from './_components/ContestTable'
+
+export default async function Contest() {
+  const unfinishedRes = await fetch(baseUrl + '/contest')
+  const unfinishedData = await unfinishedRes.json()
+
+  const finishedRes = await fetch(baseUrl + '/contest/finished?take=10')
+  const finishedData = await finishedRes.json()
+
+  const contests: Contest[] = [
+    ...unfinishedData.upcoming,
+    ...unfinishedData.ongoing,
+    ...finishedData.finished
+  ]
+
+  return (
+    <>
+      {/* TODO: Add search bar */}
+      <ContestTable data={contests} />
+    </>
+  )
 }
