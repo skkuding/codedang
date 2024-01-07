3b3b11935f37769ae2e84f7a8c65b6c48d6b44a9
Peorge <realsehwan@gmail.com>
2024-01-07 17:09:45 +0000

    feat: add prisma logging

--- backend/libs/logger/src/pino-option.logger.ts
+++ backend/libs/logger/src/pino-option.logger.ts
@@ -2,12 +2,13 @@ import type { Params } from 'nestjs-pino'
 import type { PrettyOptions } from 'pino-pretty'
 
 // TODO: include context in message format of pino-pretty
+// TODO: add customPrettiers option to refine logs, such as query
 const pinoPrettyOptions: PrettyOptions = {}
 
 // TODO: change log level to nestjs-style. e.g. INFO -> LOG
 export const pinoLoggerModuleOption: Params = {
   pinoHttp: {
-    level: 'info',
+    level: 'trace', // TODO: in production mode, set log level as 'info'
     autoLogging: true,
     formatters: {
       level(label) {
