094f3169d723af9c8782887e07b152fada41685a
Gyunseo Lee <rbstj0311@naver.com>
2024-01-07 17:34:19 +0000

    fix(be): delete previous migration scripts to squash migrations scripts into one file

--- backend/prisma/migrations/20231226040805_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `id` column on the `user_problem` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "user_problem" DROP COLUMN "id",
-ADD COLUMN     "id" SERIAL NOT NULL;
