e301b7c635cf046edd62d6a67428799bf757af6e
b0xercat <seojin3154@naver.com>
2024-01-07 17:26:24 +0900

    feat(fe): add filter in contest table

--- frontend-client/app/(main)/contest/_components/ContestTable.tsx
+++ frontend-client/app/(main)/contest/_components/ContestTable.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
 import {
   Table,
   TableBody,
@@ -8,7 +9,6 @@ import {
   TableHeader,
   TableRow
 } from '@/components/ui/table'
-import { cn } from '@/lib/utils'
 import type { Contest } from '@/types/type'
 import type { ColumnDef } from '@tanstack/react-table'
 import {
@@ -17,12 +17,31 @@ import {
   useReactTable
 } from '@tanstack/react-table'
 import dayjs from 'dayjs'
+import { useState } from 'react'
 
 interface ContestTableProps {
   data: Contest[]
 }
 
 export default function ContestTable({ data }: ContestTableProps) {
+  const [filteredData, setFilteredData] = useState<Contest[]>(data)
+
+  const filterContests = (filterType: string) => {
+    if (filterType === 'all') {
+      setFilteredData(data)
+    } else if (filterType === 'upcoming') {
+      const upcomingContests = data.filter(
+        (contest) => contest.status === 'upcoming'
+      )
+      setFilteredData(upcomingContests)
+    } else if (filterType === 'finished') {
+      const finishedContests = data.filter(
+        (contest) => contest.status === 'finished'
+      )
+      setFilteredData(finishedContests)
+    }
+  }
+
   const columns: ColumnDef<Contest>[] = [
     {
       header: 'Name',
@@ -30,14 +49,7 @@ export default function ContestTable({ data }: ContestTableProps) {
       cell: ({ row }) => {
         return (
           <div className="flex items-center justify-start gap-3 md:gap-4">
-            <span
-              className={cn(
-                row.original.title && 'font-semibold',
-                'text-sm md:text-base'
-              )}
-            >
-              {row.original.title}
-            </span>
+            <span className="text-sm md:text-base">{row.original.title}</span>
           </div>
         )
       }
@@ -82,11 +94,12 @@ export default function ContestTable({ data }: ContestTableProps) {
           <span>Participants</span>
         </p>
       ),
-      accessorKey: 'startTime',
-      cell: () => {
+      accessorKey: 'participants',
+      cell: ({ row }) => {
         return (
           <p className="text-center">
-            <span className="text-xs md:text-sm">1</span>
+            {/* TODO participant로 변경 */}
+            <span className="text-xs md:text-sm">{row.original.id}</span>
           </p>
         )
       }
@@ -94,62 +107,87 @@ export default function ContestTable({ data }: ContestTableProps) {
   ]
 
   const table = useReactTable({
-    data,
+    data: filteredData,
     columns,
     getCoreRowModel: getCoreRowModel()
   })
 
   return (
-    <Table className="table-fixed">
-      <TableHeader>
-        {table.getHeaderGroups().map((headerGroup) => (
-          <TableRow key={headerGroup.id}>
-            {headerGroup.headers.map((header) => {
-              return (
-                <TableHead
-                  key={header.id}
-                  className={
-                    header.column.columnDef.header === 'Name'
-                      ? 'w-[55%] md:w-[61%]'
-                      : 'w-[15%] md:w-[13%]'
-                  }
-                >
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </TableHead>
-              )
-            })}
-          </TableRow>
-        ))}
-      </TableHeader>
-      <TableBody>
-        {table.getRowModel().rows?.length ? (
-          table.getRowModel().rows.map((row) => (
-            <TableRow
-              key={row.id}
-              data-state={row.getIsSelected() && 'selected'}
-              className="cursor-pointer"
-              onClick={() => {}}
-            >
-              {row.getVisibleCells().map((cell) => (
-                <TableCell key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </TableCell>
-              ))}
+    <>
+      <div className="mt-4 flex items-center gap-8">
+        <Button
+          variant={'link'}
+          className="h-5 w-fit p-0 py-2 text-xl font-semibold text-gray-500"
+          onClick={() => filterContests('all')}
+        >
+          All
+        </Button>
+        <Button
+          variant={'link'}
+          className="h-5 w-fit p-0 py-2 text-xl font-semibold text-gray-500"
+          onClick={() => filterContests('upcoming')}
+        >
+          Upcoming
+        </Button>
+        <Button
+          variant={'link'}
+          className="h-5 w-fit p-0 py-2 text-xl font-semibold text-gray-500"
+          onClick={() => filterContests('finished')}
+        >
+          Finished
+        </Button>
+      </div>
+      <Table className="table-fixed">
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead
+                    key={header.id}
+                    className={
+                      header.column.columnDef.header === 'Name'
+                        ? 'w-[55%] md:w-[61%]'
+                        : 'w-[15%] md:w-[13%]'
+                    }
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                )
+              })}
             </TableRow>
-          ))
-        ) : (
-          <TableRow>
-            <TableCell colSpan={columns.length} className="h-24 text-center">
-              No results.
-            </TableCell>
-          </TableRow>
-        )}
-      </TableBody>
-    </Table>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && 'selected'}
+                className="cursor-pointer"
+                onClick={() => {}}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </>
   )
 }
