e53efb3649ae8b006375483f9c6522521869c34c
b0xercat <seojin3154@naver.com>
2024-01-08 11:12:20 +0900

    fix(fe): add status to contest api

--- frontend-client/app/(main)/contest/page.tsx
+++ frontend-client/app/(main)/contest/page.tsx
@@ -6,14 +6,28 @@ export default async function Contest() {
   const unfinishedRes = await fetch(baseUrl + '/contest')
   const unfinishedData = await unfinishedRes.json()
 
-  const finishedRes = await fetch(baseUrl + '/contest/finished?take=10')
+  const finishedRes = await fetch(baseUrl + '/contest/finished?take=3', {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
   const finishedData = await finishedRes.json()
 
+  unfinishedData.ongoing.forEach((contest: { status: string }) => {
+    contest.status = 'ongoing'
+  })
+  unfinishedData.upcoming.forEach((contest: { status: string }) => {
+    contest.status = 'upcoming'
+  })
+  finishedData.finished.forEach((contest: { status: string }) => {
+    contest.status = 'finished'
+  })
   const contests: Contest[] = [
     ...unfinishedData.upcoming,
     ...unfinishedData.ongoing,
     ...finishedData.finished
   ]
+  console.log(contests)
 
   return (
     <>
