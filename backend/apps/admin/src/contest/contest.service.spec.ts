d2489e8a006098e0004e07ffb3ce92f005c488e4
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2024-01-08 16:23:42 +0900

    feat(be): implement api add problems to contest (#1105)
    * feat(be): implement add-problems api
    
    * chore(be): remove unused log
    
    * chore(be): modify contest service add-problems
    - problem을 contest에 추가할 때 exposeTime을 contest의 endTime으로 설정하도록 변경
    
    * chore(be): add exception handling logic to create-contest
    - createContest에 group이 존재하지 않는 경우의 예외 처리를 추가
    
    * chore(be): modify import-problems exception handling logic
    - importProblems에서 이미 추가되어있는 Problem을 다시 추가하려고 할 때
    - 예외를 발생시키는 것이 아닌 해당 문제를 무시하고 넘어가는 것으로 처리하도록 수정
    
    * docs(be): add admin contest module api docs
    
    * test(be): modify create-contest test code
    
    * test(be): add add-contest-problems test code
    - addContestProblems 테스트 코드 추가
    - prisma $transaction mocking 코드 추가
    - 사용하지 않는 stub() 제거
    
    * chore(be): modify function name
    - addProblems => addContestProblems로 함수명 변경
    
    * chore(be): modify add-contest-problems to use transaction
    - prisma transaction을 사용하도록 로직 변경
    
    * docs(be): modify directory name
    - Add Problems => Add Contest Problems로 이름 변경
    - problem id가 없는 경우에 대한 설명 docs에 추가
    
    * docs(be): modify add-contest-problems api docs
    - `groupId` 인자를 추가로 받게 됨에 따른 변경
    
    * test(be): modify add-contest-problems test code
    
    * chore(be): apply group-leader-guard
    - addContestProblems api에 Group Leader 이상 권한만 접근 가능하도록 변경
    
    * fix(be): modify import-problems to set expose-time
    - problem이 contest에 추가될 때 expose time을 설정하도록 수정
    - update, create를 transaction으로 한번에 처리
    
    * docs(be): modify import-problems test code
    - transaction 사용에 따른 테스트 코드 수정
    
    * docs(be): modify import-problems api docs
    
    * chore(be): integrate import-problems and add-contest-problems
    - 기능 상 유사한 함수 통합
    - 함수명 importProblemsToContest로 변경
    
    * docs(be): modify function name
    
    * test(be): modify test code

--- backend/apps/admin/src/contest/contest.service.spec.ts
+++ backend/apps/admin/src/contest/contest.service.spec.ts
@@ -1,11 +1,14 @@
 import { CACHE_MANAGER } from '@nestjs/cache-manager'
 import { Test, type TestingModule } from '@nestjs/testing'
 import { faker } from '@faker-js/faker'
+import { Prisma } from '@prisma/client'
 import type { Cache } from 'cache-manager'
 import { expect } from 'chai'
 import { stub } from 'sinon'
 import { EntityNotExistException } from '@libs/exception'
 import { PrismaService } from '@libs/prisma'
+import { ContestProblem, Group } from '@admin/@generated'
+import { Problem } from '@admin/@generated'
 import { Contest } from '@admin/@generated/contest/contest.model'
 import { ContestService } from './contest.service'
 import type {
@@ -17,6 +20,7 @@ import type { PublicizingRequest } from './model/publicizing-request.model'
 const contestId = 1
 const userId = 1
 const groupId = 1
+const problemId = 2
 
 const contest: Contest = {
   id: contestId,
@@ -30,6 +34,54 @@ const contest: Contest = {
     isVisible: true,
     isRankVisible: true
   },
+  createTime: faker.date.past(),
+  updateTime: faker.date.past()
+}
+
+const group: Group = {
+  id: groupId,
+  groupName: 'groupName',
+  description: 'description',
+  config: {
+    showOnList: true,
+    allowJoinFromSearch: true,
+    allowJoinWithURL: false,
+    requireApprovalBeforeJoin: true
+  },
+  createTime: faker.date.past(),
+  updateTime: faker.date.past()
+}
+
+const problem: Problem = {
+  id: problemId,
+  createdById: 2,
+  groupId: 2,
+  title: 'test problem',
+  description: 'thisistestproblem',
+  inputDescription: 'inputdescription',
+  outputDescription: 'outputdescription',
+  hint: 'hint',
+  template: [],
+  languages: ['C'],
+  timeLimit: 10000,
+  memoryLimit: 100000,
+  difficulty: 'Level1',
+  source: 'source',
+  exposeTime: faker.date.past(),
+  createTime: faker.date.past(),
+  updateTime: faker.date.past(),
+  inputExamples: ['input'],
+  outputExamples: ['input'],
+  submissionCount: 0,
+  acceptedCount: 0,
+  acceptedRate: 0
+}
+
+const contestProblem: ContestProblem = {
+  order: 0,
+  contestId,
+  problemId,
+  score: 50,
   createTime: faker.date.past(),
   updateTime: faker.date.past()
 }
@@ -72,6 +124,20 @@ const db = {
     update: stub().resolves(Contest),
     delete: stub().resolves(Contest)
   },
+  contestProblem: {
+    create: stub().resolves(ContestProblem)
+  },
+  problem: {
+    update: stub().resolves(Problem)
+  },
+  group: {
+    findUnique: stub().resolves(Group)
+  },
+  $transaction: stub().callsFake(async () => {
+    const updatedProblem = await db.problem.update()
+    const newContestProblem = await db.contestProblem.create()
+    return [updatedProblem, newContestProblem]
+  }),
   getPaginator: PrismaService.prototype.getPaginator
 }
 
@@ -129,6 +195,7 @@ describe('ContestService', () => {
   describe('createContest', () => {
     it('should return created contest', async () => {
       db.contest.create.resolves(contest)
+      db.group.findUnique.resolves(group)
 
       const res = await service.createContest(groupId, userId, input)
       expect(res).to.deep.equal(contest)
@@ -189,6 +256,43 @@ describe('ContestService', () => {
       expect(service.handlePublicizingRequest(3, true)).to.be.rejectedWith(
         EntityNotExistException
       )
+    })
+  })
+
+  describe('importProblemsToContest', () => {
+    it('should return created ContestProblems', async () => {
+      db.contest.findUnique.resolves(contest)
+      db.problem.update.resolves(problem)
+      db.contestProblem.create.resolves(contestProblem)
+
+      const res = await Promise.all(
+        await service.importProblemsToContest(groupId, contestId, [problemId])
+      )
+
+      expect(res).to.deep.equal([contestProblem])
+    })
+
+    it('should return an empty array when the problem already exists in contest', async () => {
+      db.contest.findUnique.resolves(contest)
+      db.problem.update.resolves(problem)
+      db.contestProblem.create.throws(
+        new Prisma.PrismaClientKnownRequestError(
+          'ContestProblem already exists',
+          { code: 'P2002', clientVersion: 'version' }
+        )
+      )
+
+      const res = await service.importProblemsToContest(groupId, contestId, [
+        problemId
+      ])
+
+      expect(res).to.deep.equal([])
+    })
+
+    it('should throw error when the contestId not exist', async () => {
+      expect(
+        service.importProblemsToContest(groupId, 9999, [problemId])
+      ).to.be.rejectedWith(EntityNotExistException)
     })
   })
 })
