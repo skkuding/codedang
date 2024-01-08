036f38bcd36a0545d2defe9551377a4a2e0762d6
dayongkr <dayongkr@gmail.com>
2024-01-08 20:33:10 +0900

    feat: delete cookie

--- frontend-client/middleware.ts
+++ frontend-client/middleware.ts
@@ -1,13 +1,30 @@
-import { withAuth } from 'next-auth/middleware'
+import { getToken } from 'next-auth/jwt'
+// import { withAuth } from 'next-auth/middleware'
+import { NextResponse, type NextRequest } from 'next/server'
 
-export { withAuth } from 'next-auth/middleware'
-export default withAuth({
-  callbacks: {
-    authorized({ token }) {
-      return token?.role === 'Admin'
-    }
+// export default withAuth({
+//   callbacks: {
+//     authorized({ token }) {
+//       return token?.role === 'Admin'
+//     }
+//   }
+// })
+// export const config = {
+//   matcher: ['/admin/:path*']
+// }
+
+export const middleware = async (req: NextRequest) => {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  const res = NextResponse.next()
+  if (token && (Date.now() >= token.refreshTokenExpires || !token.username)) {
+    // If refresh token is expired
+    res.cookies.delete('next-auth.session-token')
+    res.cookies.delete('__Secure-next-auth.session-token')
   }
-})
-export const config = {
-  matcher: ['/admin/:path*']
+  if (
+    req.nextUrl.pathname.startsWith('/admin') &&
+    (!token || token.role !== 'Admin')
+  )
+    return NextResponse.redirect(new URL('/', req.url))
+  return res
 }
