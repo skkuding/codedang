bb11a1a97145110607a97da15fa12a34bcff24c2
EliseCho13 <dbwlspdlqj@naver.com>
2024-01-09 20:09:13 +0900

    fix(fe): removed console logs and dummy data

--- frontend-client/app/(main)/_components/ContestCardList.tsx
+++ frontend-client/app/(main)/_components/ContestCardList.tsx
@@ -14,59 +14,6 @@ const getContests = async () => {
     ongoing: Contest[]
     upcoming: Contest[]
   }>('/contest')
-  //dummy test
-  data.ongoing.push({
-    id: 2,
-    title: '대회',
-    startTime: '2023-12-05T08:47:24.799Z',
-    endTime: '2024-02-03T08:47:24.800Z',
-    group: {
-      id: 1,
-      groupName: 'Example Group'
-    }
-  })
-  data.upcoming.push(
-    {
-      id: 12,
-      title: '2대회',
-      startTime: '2023-01-25T08:47:24.799Z',
-      endTime: '2024-02-03T08:47:24.800Z',
-      group: {
-        id: 1,
-        groupName: 'Example Group'
-      }
-    },
-    {
-      id: 3,
-      title: 'test 대회',
-      startTime: '2023-02-02T08:47:24.799Z',
-      endTime: '2024-03-03T08:47:24.800Z',
-      group: {
-        id: 1,
-        groupName: 'Example Group'
-      }
-    }
-    // {
-    //   id: 38,
-    //   title: 'testing',
-    //   startTime: '2024-12-02T08:47:24.799Z',
-    //   endTime: '2024-12-03T08:47:24.800Z',
-    //   group: {
-    //     id: 1,
-    //     groupName: 'Example Group'
-    //   }
-    // }
-    // {
-    //   id: 28,
-    //   title: 'yesdjkfj',
-    //   startTime: '2024-04-02T08:47:24.799Z',
-    //   endTime: '2024-04-03T08:47:24.800Z',
-    //   group: {
-    //     id: 1,
-    //     groupName: 'Example Group'
-    //   }
-    // }
-  )
   data.ongoing.forEach((contest) => {
     contest.status = 'ongoing'
   })
@@ -94,15 +41,10 @@ export default function ContestCardList({ type }: { type: string }) {
   }, [])
 
   const handleArrowClick = (direction: 'next' | 'prev') => {
-    console.log('current index is ', currentIndex)
     if (
       direction === 'next' &&
       currentIndex < Math.floor((contests.length - 2) / contestsPerPage) + 1
     ) {
-      console.log(
-        'result is ',
-        Math.floor((contests.length - 2) / contestsPerPage) + 2
-      )
       setCurrentIndex(currentIndex + 1)
     } else if (direction === 'prev' && currentIndex > 0) {
       setCurrentIndex(currentIndex - 1)
