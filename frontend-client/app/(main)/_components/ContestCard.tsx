17347167fc3c134b7004e01ec538c5b00d7eaad4
EliseCho <dbwlspdlqj@naver.com>
2024-01-15 19:07:29 +0900

    feat(fe): contest card, problem card design change (#1193)
    * fix(fe): changed day format to always show minute and seconds
    
    * fix(fe): fixed hour when its more than a day
    
    * feat(fe): changed badge style to match new design
    
    * feat(fe): changed card style to match new design
    
    * fix(fe): removed unused responsive md
    
    * style(fe): changed design of more button in main
    
    * fix(fe): removed icons of contest, recommendation in main, adjusted font size and color
    
    * fix(fe): added level colors, changed round of badge
    
    * feat(fe): fixed design of problem card
    
    * fix: change style
    
    * chore: typo error
    
    * chore: add gap
    
    ---------
    
    Co-authored-by: dayongkr <dayongkr@gmail.com>

--- frontend-client/app/(main)/_components/ContestCard.tsx
+++ frontend-client/app/(main)/_components/ContestCard.tsx
@@ -2,13 +2,15 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { cn } from '@/lib/utils'
 import type { Contest } from '@/types/type'
 import dayjs from 'dayjs'
+import { FaRegClock } from 'react-icons/fa'
+import { FaRegCalendarAlt } from 'react-icons/fa'
 import Badge from './Badge'
 import TimeDiff from './TimeDiff'
 
 const variants = {
-  ongoing: 'bg-gradient-to-br from-[#9BE299] to-[#82D4E7]',
-  upcoming: 'bg-gradient-to-br from-[#A6B8FF] to-[#86C8DE]',
-  finished: 'bg-gradient-to-br from-[#D1C9D9] to-[#B9C8E6]'
+  ongoing: 'bg-gradient-to-br from-blue-500 to-blue-950',
+  upcoming: 'bg-gradient-to-br from-emerald-600 to-emerald-900',
+  finished: 'bg-gray-500'
 }
 
 interface Props {
@@ -28,25 +30,31 @@ export default function ContestCard({ contest }: Props) {
   return (
     <Card
       className={cn(
-        'my-2 flex h-40 w-60 flex-col justify-between border-0 shadow-md transition hover:scale-105 hover:opacity-80 md:h-44 md:w-72',
+        'flex w-full flex-col justify-between gap-1 rounded-md border-0 text-white transition hover:scale-105 hover:opacity-80',
         variants[contest.status]
       )}
     >
-      <CardHeader className="m-4 space-y-0 p-0 text-sm">
-        {contest.status == 'ongoing' ? (
-          <Badge type={contest.status}>
-            <div className="text-red-500">
-              <TimeDiff timeRef={contest.endTime}></TimeDiff>
-            </div>
-          </Badge>
-        ) : (
-          <p className="text-right text-blue-50">{`${startTime} - ${endTime}`}</p>
-        )}
+      <CardHeader className="pb-0">
+        <Badge type={contest.status}>
+          <p>{contest.status}</p>
+        </Badge>
+
+        <CardTitle className="text-lg font-semibold">{contest.title}</CardTitle>
       </CardHeader>
-      <CardContent className="p-5 pt-0">
-        <CardTitle className="line-clamp-2 overflow-hidden break-keep text-xl tracking-normal text-white md:text-2xl">
-          {contest.title}
-        </CardTitle>
+
+      <CardContent className="flex items-center gap-1 text-xs text-white opacity-80">
+        {contest.status === 'finished' ? (
+          <>
+            <FaRegCalendarAlt />
+            {startTime} - {endTime}
+          </>
+        ) : (
+          <>
+            <FaRegClock />
+            {contest.status === 'ongoing' ? 'Ends in' : 'Starts in'}
+            <TimeDiff timeRef={contest.endTime}></TimeDiff>
+          </>
+        )}
       </CardContent>
     </Card>
   )
