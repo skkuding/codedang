40ba6bd38dfc23087b7d99d859f1d34c32c2efce
dayongkr <dayongkr@gmail.com>
2024-01-15 17:25:51 +0900

    chore: rollback

--- frontend-client/next.config.js
+++ frontend-client/next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // TODO: vercel doesn't support experimental features yet
-  // experimental: {
-  //   typedRoutes: true
-  // },
+  experimental: {
+    typedRoutes: true
+  },
   output: 'standalone',
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL
