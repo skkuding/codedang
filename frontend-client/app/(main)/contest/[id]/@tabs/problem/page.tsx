b3addfb9e1d42874103a09d415c62f205f0200b0
b0xercat <seojin3154@naver.com>
2024-01-16 14:22:31 +0900

    feat(fe): add temporary problem table

--- frontend-client/app/(main)/contest/[id]/@tabs/problem/page.tsx
+++ frontend-client/app/(main)/contest/[id]/@tabs/problem/page.tsx
@@ -1,3 +1,18 @@
-export default function ContestProblem() {
-  return <h1>Problem</h1>
+import ProblemTable from '@/app/(main)/problem/_components/ProblemTable'
+import { fetcher } from '@/lib/utils'
+import { ContestDetailProps } from '../../layout'
+
+export default async function ContestProblem({
+  params
+}: {
+  params: ContestDetailProps['params']
+}) {
+  const { id } = params
+  const contest = await fetcher.get(`contest/${id}/problem?take=10`).json()
+  contest.forEach((problem: { id: string; problemId: string }) => {
+    problem.id = problem.problemId
+  })
+  return (
+    <ProblemTable data={contest} isLoading={!contest} isTagChecked={false} />
+  )
 }
