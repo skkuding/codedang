6270e16bd6f8254808a0b5d18ab4b9681f1091f3
b0xercat <seojin3154@naver.com>
2024-01-17 02:50:50 +0900

    fix(fe): componentize contest tabs

--- frontend-client/app/(main)/contest/[id]/@tabs/layout.tsx
+++ frontend-client/app/(main)/contest/[id]/@tabs/layout.tsx
@@ -1,8 +1,4 @@
-'use client'
-
-import { Button } from '@/components/ui/button'
-import { cn } from '@/lib/utils'
-import { useRouter, usePathname } from 'next/navigation'
+import ContestTabs from '../../_components/ContestTabs'
 import type { ContestDetailProps } from '../layout'
 
 export default function Layout({
@@ -13,66 +9,9 @@ export default function Layout({
   params: ContestDetailProps['params']
 }) {
   const { id } = params
-  const router = useRouter()
-  const pathname = usePathname()
-
-  const isCurrentTab = (tab: string) => {
-    if (tab === '') return pathname === `/contest/${id}`
-    return pathname.startsWith(`/contest/${id}/${tab}`)
-  }
-
   return (
     <>
-      <div className="mb-1 flex justify-center gap-4">
-        <Button
-          variant={'link'}
-          onClick={() => {
-            router.push(`/contest/${id}`)
-          }}
-          className={cn(
-            'text-lg text-gray-400',
-            isCurrentTab('') && 'text-primary'
-          )}
-        >
-          Top
-        </Button>
-        <Button
-          variant={'link'}
-          onClick={() => {
-            router.push(`/contest/${id}/problem`)
-          }}
-          className={cn(
-            'text-lg text-gray-400',
-            isCurrentTab('problem') && 'text-primary'
-          )}
-        >
-          Problem
-        </Button>
-        <Button
-          variant={'link'}
-          onClick={() => {
-            router.push(`/contest/${id}/clarification`)
-          }}
-          className={cn(
-            'text-lg text-gray-400',
-            isCurrentTab('clarification') && 'text-primary'
-          )}
-        >
-          Clarification
-        </Button>
-        <Button
-          variant={'link'}
-          onClick={() => {
-            router.push(`/contest/${id}/standings`)
-          }}
-          className={cn(
-            'text-lg text-gray-400',
-            isCurrentTab('standings') && 'text-primary'
-          )}
-        >
-          Standings
-        </Button>
-      </div>
+      <ContestTabs contestId={id}></ContestTabs>
       {children}
     </>
   )
---
+++ frontend-client/app/(main)/contest/_components/ContestTabs.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+export default function ContestTabs({ contestId }: { contestId: string }) {
+  const id = contestId
+  const pathname = usePathname()
+
+  const isCurrentTab = (tab: string) => {
+    if (tab === '') return pathname === `/contest/${id}`
+    return pathname.startsWith(`/contest/${id}/${tab}`)
+  }
+
+  return (
+    <div className="mb-4 flex justify-center gap-12">
+      <Link
+        href={`/contest/${id}`}
+        className={cn(
+          'text-lg text-gray-400',
+          isCurrentTab('') && 'text-primary'
+        )}
+      >
+        Top
+      </Link>
+      <Link
+        href={`/contest/${id}/problem`}
+        className={cn(
+          'text-lg text-gray-400',
+          isCurrentTab('problem') && 'text-primary'
+        )}
+      >
+        Problem
+      </Link>
+      <Link
+        href={`/contest/${id}/clarification`}
+        className={cn(
+          'text-lg text-gray-400',
+          isCurrentTab('clarification') && 'text-primary'
+        )}
+      >
+        Clarification
+      </Link>
+      <Link
+        href={`/contest/${id}/standings`}
+        className={cn(
+          'text-lg text-gray-400',
+          isCurrentTab('standings') && 'text-primary'
+        )}
+      >
+        Standings
+      </Link>
+    </div>
+  )
+}
