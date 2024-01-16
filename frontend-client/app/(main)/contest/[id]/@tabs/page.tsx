f26e08aa5d0ea0d22913d3b3aabf4b596e5a93fb
b0xercat <seojin3154@naver.com>
2024-01-16 11:25:53 +0900

    chore(fe): add register button

--- frontend-client/app/(main)/contest/[id]/@tabs/page.tsx
+++ frontend-client/app/(main)/contest/[id]/@tabs/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import { baseUrl } from '@/lib/vars'
 import { sanitize } from 'isomorphic-dompurify'
 import type { ContestDetailProps } from '../layout'
@@ -13,9 +14,15 @@ export default async function ContestTop({
     res.json()
   )
   return (
-    <main
-      className="prose w-full max-w-full border-b border-t border-b-gray-400 border-t-gray-400 p-5 py-12"
-      dangerouslySetInnerHTML={{ __html: sanitize(description) }}
-    />
+    <>
+      <main
+        className="prose w-full max-w-full border-b border-t border-b-gray-400 border-t-gray-400 p-5 py-12"
+        dangerouslySetInnerHTML={{ __html: sanitize(description) }}
+      />
+      {/* TODO: Participate Contest API */}
+      <div className="mt-10 flex justify-center">
+        <Button className="px-12 py-6 text-lg font-light">Register</Button>
+      </div>
+    </>
   )
 }
