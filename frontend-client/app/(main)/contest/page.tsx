6e8a4cbd5974aadfcc6c9bf4a9d6cd01ec59d300
b0xercat <seojin3154@naver.com>
2024-01-17 16:15:25 +0900

    fix(fe): change contest list page to server component

--- frontend-client/app/(main)/contest/page.tsx
+++ frontend-client/app/(main)/contest/page.tsx
@@ -1,31 +1,15 @@
-'use client'
-
 import { fetcher } from '@/lib/utils'
 import type { Contest } from '@/types/type'
-import { useEffect, useState } from 'react'
 import ContestCardList from '../_components/ContestCardList'
 import ContestTable from './_components/ContestTable'
 
-const getFinished = async () => {
+export default async function Contest() {
   const data: {
     finished: Contest[]
   } = await fetcher.get('contest/finished?take=51').json()
   data.finished.forEach((contest) => {
     contest.status = 'finished'
   })
-  return data.finished
-}
-
-export default function Contest() {
-  const [finished, setFinished] = useState<Contest[]>([])
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getFinished()
-      setFinished(data)
-    }
-    fetchData()
-  }, [])
 
   return (
     <>
@@ -34,7 +18,7 @@ export default function Contest() {
 
       <p className="text-xl font-bold md:text-2xl">Finished</p>
       {/* TODO: Add search bar */}
-      <ContestTable data={finished} />
+      <ContestTable data={data.finished} />
     </>
   )
 }
