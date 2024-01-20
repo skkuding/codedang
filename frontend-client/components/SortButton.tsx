607b137b18d030d17b7a1e91faf7d68ba2651094
Dayong Lee <dayongkr@gmail.com>
2024-01-20 21:37:31 +0900

    refactor(fe): integrate data table and add search bar (#1214)
    * refactor: notice table
    
    * feat: add link
    
    * refactor: delete contest table and cell style
    
    * refactor: contest data table
    
    * feat: add searchbar in notice
    
    * fix: prettier error
    
    * feat(fe): rollback tsconfig option
    
    * chore: rollback tsconfig lib option

---
+++ frontend-client/components/SortButton.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import { cn } from '@/lib/utils'
+import type { Route } from 'next'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+import { FaSortDown, FaSortUp } from 'react-icons/fa'
+import { Button } from './ui/button'
+
+interface SortButtonProps {
+  children: React.ReactNode
+  order: string
+}
+
+export default function SortButton({ children, order }: SortButtonProps) {
+  const param = useSearchParams()
+  const router = useRouter()
+  const pathname = usePathname()
+  const currentOrder = param.get('order')
+  return (
+    <Button
+      variant="ghost"
+      onClick={() => {
+        const newParam = new URLSearchParams()
+        // set all searchParam to newParam
+        param.forEach((value, key) => newParam.set(key, value))
+        if (currentOrder == `${order}-asc`)
+          newParam.set('order', `${order}-desc`)
+        else newParam.set('order', `${order}-asc`)
+        router.push(`${pathname}?${newParam.toString()}` as Route)
+      }}
+      className="items-center justify-center gap-1 md:text-base"
+    >
+      {children}
+      <div className="text-gray-300">
+        <FaSortUp
+          className={cn(
+            'absolute',
+            currentOrder == `${order}-asc` && 'text-gray-900'
+          )}
+        />
+        <FaSortDown
+          className={currentOrder == `${order}-desc` && 'text-gray-900'}
+        />
+      </div>
+    </Button>
+  )
+}
