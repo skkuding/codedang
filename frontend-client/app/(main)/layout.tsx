e19e351f70a0cff7b4c4bc7d2f8a83c5dd9670fc
Jaemin Choi <1dotolee@gmail.com>
2024-01-24 09:22:41 +0000

    chore: remove header from main layout

--- frontend-client/app/(main)/layout.tsx
+++ frontend-client/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from './_components/Footer'
-import Header from './_components/Header'
+
+// import Header from './_components/Header'
 
 export default function MainLayout({
   children
@@ -8,7 +9,7 @@ export default function MainLayout({
 }) {
   return (
     <div className="flex min-h-dvh w-screen flex-col items-center overflow-x-hidden">
-      <Header />
+      {/* <Header /> */}
       <main className="flex w-full max-w-7xl flex-1 flex-col items-center px-5">
         {children}
       </main>
