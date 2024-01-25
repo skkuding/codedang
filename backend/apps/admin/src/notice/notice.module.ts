fd4e91e318c1fe73cb4afb3c4eac6402bdf6481a
Peorge <realsehwan@gmail.com>
2024-01-25 16:23:19 +0000

    feat: add field resolvers
    - Notice에서 접근 가능한 createdBy(User)와 group 필드에 대한 resolver를 추가

--- backend/apps/admin/src/notice/notice.module.ts
+++ backend/apps/admin/src/notice/notice.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
 import { RolesModule } from '@libs/auth'
+import { GroupModule } from '@admin/group/group.module'
+import { UserModule } from '@admin/user/user.module'
 import { NoticeResolver } from './notice.resolver'
 import { NoticeService } from './notice.service'
 
 @Module({
-  imports: [RolesModule],
+  imports: [RolesModule, UserModule, GroupModule],
   providers: [NoticeService, NoticeResolver]
 })
 export class NoticeModule {}
