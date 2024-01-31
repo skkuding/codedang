e1ed6e20d1e5d967faccbcf763e0a959b6691d5e
Dayong Lee <dayongkr@gmail.com>
2024-01-31 17:15:37 +0900

    feat(fe): problem editor (#1276)
    * feat: start
    
    * feat: edit description design
    
    * feat(fe): implement description loading skeleton
    
    * fix(fe): delete test page
    
    * feat(fe): apply programming language to code editor
    
    * feat(fe): making submit button
    
    * feat: save with error
    
    * feat: add copyclipboard toaster
    
    * feat(fe): apply submit button
    
    * refactor: change tag design, flex layout to grid layout
    
    * fix: lint error
    
    ---------
    
    Co-authored-by: jiho <jihopark09@naver.com>
    Co-authored-by: YooJin Lee <leejin914418@gmail.com>

--- frontend-client/app/problem/[id]/layout.tsx
+++ frontend-client/app/problem/[id]/layout.tsx
@@ -1,5 +1,6 @@
 import HeaderEditor from '@/app/problem/[id]/_components/HeaderEditor'
-import { baseUrl } from '@/lib/vars'
+import { fetcher } from '@/lib/utils'
+import type { ProblemDetail } from '@/types/type'
 import MainResizablePanel from './_components/MainResizablePanel'
 
 export default async function layout({
@@ -10,16 +11,12 @@ export default async function layout({
   children: React.ReactNode
 }) {
   const { id } = params
-  const response = await fetch(baseUrl + '/problem/' + id)
-  const data = await response.json()
-  // Specific information for editor main page
+  const data: ProblemDetail = await fetcher(`problem/${id}`).json()
 
   return (
-    <div className="flex h-dvh w-full min-w-[1000px] flex-col overflow-x-auto bg-slate-700 text-white">
+    <div className="grid-rows-editor grid h-dvh w-full min-w-[1000px] overflow-x-auto bg-slate-700 text-white">
       <HeaderEditor id={data.id} title={data.title} />
-      <main className="flex h-full flex-col overflow-hidden border border-slate-600">
-        <MainResizablePanel data={data} tabs={children} />
-      </main>
+      <MainResizablePanel data={data}>{children}</MainResizablePanel>
     </div>
   )
 }
