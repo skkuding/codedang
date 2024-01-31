7053b3ea0bd177230a2ee11217a7a4dfd17aac09
b0xercat <seojin3154@naver.com>
2024-02-01 01:34:25 +0900

    feat(fe): show register button only if contest is upcoming

--- frontend-client/app/(main)/contest/[id]/@tabs/page.tsx
+++ frontend-client/app/(main)/contest/[id]/@tabs/page.tsx
@@ -5,6 +5,7 @@ import ParticipateButton from './_components/ParticipateButton'
 
 interface ContestTop {
   description: string
+  startTime: string
 }
 
 interface ContestTopProps {
@@ -18,6 +19,8 @@ export default async function ContestTop({ params }: ContestTopProps) {
   const session = await auth()
   const { id } = params
   const data: ContestTop = await fetcher.get(`contest/${id}`).json()
+  const startTime = new Date(data.startTime)
+  const currentTime = new Date()
 
   return (
     <>
@@ -25,7 +28,7 @@ export default async function ContestTop({ params }: ContestTopProps) {
         className="prose w-full max-w-full border-b-2 border-b-gray-300 p-5 py-12"
         dangerouslySetInnerHTML={{ __html: sanitize(data.description) }}
       />
-      {session && (
+      {session && currentTime < startTime && (
         <div className="mt-10 flex justify-center">
           <ParticipateButton id={id} />
         </div>
