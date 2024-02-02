884dd237cf04fb11a7d7fd9bb663d4b6dd3d7e74
Jaemin Choi <1dotolee@gmail.com>
2024-02-02 15:53:36 +0000

    chore: remove unused security group

--- infra/modules/codedang-infra/security-group.tf
+++ infra/modules/codedang-infra/security-group.tf
@@ -188,29 +188,3 @@ resource "aws_security_group" "redis" {
     Name = "Codedang-SG-Redis"
   }
 }
-
-resource "aws_security_group" "mq" {
-  name        = "Codedang-SG-MQ"
-  description = "Allow Message inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "MQ"
-    from_port   = var.rabbitmq_port
-    to_port     = var.rabbitmq_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "Codedang-SG-MQ"
-  }
-}
