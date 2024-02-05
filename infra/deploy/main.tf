1d7c858b65d1bcdab2fc7220bcd6a1394172b767
Jaemin Choi <1dotolee@gmail.com>
2024-02-05 17:22:26 +0900

    feat(infra): deploy new frontend with nextjs (#1311)
    * feat: change staging server origin to vercel
    
    * chore: fix caddyfile typo
    
    * feat: add amplify origin to cloudfront
    
    * feat: remove frontend s3
    
    * feat(cd): remove frontend build job
    
    * style(cd): change gha secret names
    
    * fix: load env var kakao_client_id
    
    * chore: remove unused security group

--- infra/deploy/main.tf
+++ infra/deploy/main.tf
@@ -20,7 +20,6 @@ module "codedang-infra" {
   source = "../modules/codedang-infra"
 
   region               = var.region
-  s3_bucket            = var.s3_bucket
   postgres_username    = var.postgres_username
   postgres_port        = var.postgres_port
   redis_port           = var.redis_port
