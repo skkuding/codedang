1240b9beea540aad2b4f9cc3e60959a362f2969b
b0xercat <seojin3154@naver.com>
2024-02-06 19:07:41 +0900

    feat(fe): add zod and react hook form

--- frontend-client/app/admin/problem/create/_components/ExampleTextarea.tsx
+++ frontend-client/app/admin/problem/create/_components/ExampleTextarea.tsx
@@ -1,4 +1,5 @@
 import { Textarea } from '@/components/ui/textarea'
+import { cn } from '@/lib/utils'
 import { RxCross2 } from 'react-icons/rx'
 
 interface ExampleTextareaProps {
@@ -6,12 +7,14 @@ interface ExampleTextareaProps {
   onRemove: (id: number) => void
   onInputChange: (input: string) => void
   onOutputChange: (output: string) => void
+  className?: string
 }
 export default function ExampleTextarea({
   id,
   onRemove,
   onInputChange,
-  onOutputChange
+  onOutputChange,
+  className
 }: ExampleTextareaProps) {
   const handleInputChange = (event: { target: { value: string } }) => {
     onInputChange(event.target.value)
@@ -22,7 +25,12 @@ export default function ExampleTextarea({
   }
 
   return (
-    <div className="relative flex h-[120px] w-full rounded-md border border-gray-200 bg-gray-50 py-3 shadow-sm">
+    <div
+      className={cn(
+        'relative flex h-[120px] w-full rounded-md border border-gray-200 bg-gray-50 py-3 shadow-sm',
+        className
+      )}
+    >
       <RxCross2
         className="absolute right-2 top-2 w-3 cursor-pointer p-0 text-gray-400 "
         onClick={() => onRemove(id)}
--- frontend-client/app/admin/problem/create/page.tsx
+++ frontend-client/app/admin/problem/create/page.tsx
@@ -6,32 +6,97 @@ import { Input } from '@/components/ui/input'
 import { Switch } from '@/components/ui/switch'
 import { Textarea } from '@/components/ui/textarea'
 import { cn } from '@/lib/utils'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useState, useRef } from 'react'
+import { useForm } from 'react-hook-form'
 import { FaAngleLeft } from 'react-icons/fa6'
 import { IoMdCheckmarkCircleOutline } from 'react-icons/io'
+import { PiWarningBold } from 'react-icons/pi'
 import { toast } from 'sonner'
+import { z } from 'zod'
 import ExampleTextarea from './_components/ExampleTextarea'
 import Label from './_components/Lable'
 
 const inputStyle =
   'border-gray-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950'
+const errorBorderStyle = 'border-red-500 focus-visible:ring-red-500'
 
-interface Example {
+interface ExampleTextarea {
   id: number
   input: string
   output: string
   type: 'sample' | 'testcase'
 }
 
-// savepoint
+// TODO: language tags string[]로 변경
+interface Info {
+  level: string
+  language: string
+  tags: string
+}
+
+interface Example {
+  input: string
+  output: string
+}
+
+interface Limit {
+  time: number
+  memory: number
+}
+
+interface FormData {
+  title: string
+  info: Info
+  description: string
+  inputDescription: string
+  outputDescription: string
+  sample: Example[]
+  testcase: Example[]
+  limit: Limit
+  hint?: string
+  source?: string
+}
+
+// TODO: language tags z.array(z.string())로 변경
+const schema = z.object({
+  title: z.string().min(1).max(25),
+  info: z.object({
+    level: z.string().min(1),
+    language: z.string().min(1),
+    tags: z.string().min(1)
+  }),
+  description: z.string().min(1),
+  inputDescription: z.string().min(1),
+  outputDescription: z.string().min(1),
+  sample: z.array(z.object({ input: z.string(), output: z.string() })).min(1),
+  testcase: z.array(z.object({ input: z.string(), output: z.string() })).min(1),
+  limit: z.object({ time: z.number().min(0), memory: z.number().min(0) }),
+  hint: z.string().optional(),
+  source: z.string().optional()
+})
+
 export default function Page() {
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    formState: { errors }
+  } = useForm<FormData>({
+    resolver: zodResolver(schema)
+  })
+
+  const onSubmit = async (data: FormData) => {
+    console.log(data)
+  }
+
   const [showHint, setShowHint] = useState<boolean>(false)
   const [showSource, setShowSource] = useState<boolean>(false)
 
-  const [samples, setSamples] = useState<Example[]>([
+  const [samples, setSamples] = useState<ExampleTextarea[]>([
     { id: 1, input: '', output: '', type: 'sample' }
   ])
-  const [testcases, setTestcases] = useState<Example[]>([
+  const [testcases, setTestcases] = useState<ExampleTextarea[]>([
     { id: 1, input: '', output: '', type: 'testcase' }
   ])
   const sampleCounter = useRef(1)
@@ -99,15 +164,31 @@ export default function Page() {
         <span className="text-4xl font-bold">Create Problem</span>
       </div>
 
-      <form action="" className="flex w-[760px] flex-col gap-6">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex w-[760px] flex-col gap-6"
+      >
         <div className="flex flex-col gap-1">
           <Label>Title</Label>
           <Input
             id="title"
             type="text"
             placeholder="Name your problem"
-            className={cn(inputStyle, 'w-[380px]')}
+            className={cn(
+              inputStyle,
+              'w-[380px]',
+              errors.title && errorBorderStyle
+            )}
+            {...register('title')}
           />
+          {errors.title && (
+            <div className="flex items-center gap-1 text-xs text-red-500">
+              <PiWarningBold />
+              {getValues('title').length === 0
+                ? 'required'
+                : errors.title.message}
+            </div>
+          )}
         </div>
 
         {/* TODO: Info Component로 변경 */}
@@ -119,18 +200,22 @@ export default function Page() {
               type="text"
               placeholder="level"
               className={cn(inputStyle, 'w-[115px]')}
+              {...register('info.level')}
             />
             <Input
               id="language"
               type="text"
               placeholder="language"
               className={cn(inputStyle, 'w-[115px]')}
+              {...register('info.language')}
             />
+            <p>{errors.info?.language?.message}</p>
             <Input
               id="tags"
               type="text"
               placeholder="tags"
               className={cn(inputStyle, 'w-[115px]')}
+              {...register('info.tags')}
             />
           </div>
         </div>
@@ -142,6 +227,7 @@ export default function Page() {
             id="description"
             placeholder="Enter a description..."
             className="h-[200px] resize-none"
+            {...register('description')}
           />
         </div>
 
@@ -152,16 +238,36 @@ export default function Page() {
               <Textarea
                 id="inputDescription"
                 placeholder="Enter a description..."
-                className="h-[120px] w-[360px] resize-none"
+                className={cn(
+                  'h-[120px] w-[360px] resize-none',
+                  errors.inputDescription && errorBorderStyle
+                )}
+                {...register('inputDescription')}
               />
+              {errors.inputDescription && (
+                <div className="flex items-center gap-1 text-xs text-red-500">
+                  <PiWarningBold />
+                  required
+                </div>
+              )}
             </div>
             <div className="flex flex-col gap-1">
               <Label>Output Description</Label>
               <Textarea
                 id="outputDescription"
                 placeholder="Enter a description..."
-                className="h-[120px] w-[360px] resize-none"
+                className={cn(
+                  'h-[120px] w-[360px] resize-none',
+                  errors.outputDescription && errorBorderStyle
+                )}
+                {...register('outputDescription')}
               />
+              {errors.outputDescription && (
+                <div className="flex items-center gap-1 text-xs text-red-500">
+                  <PiWarningBold />
+                  required
+                </div>
+              )}
             </div>
           </div>
         </div>
@@ -178,17 +284,26 @@ export default function Page() {
           </div>
           <div className="flex flex-col gap-2">
             {samples.map(({ id, input, output }) => (
-              <ExampleTextarea
-                key={id}
-                id={id}
-                onRemove={() => removeExample('sample', id)}
-                onInputChange={(newInput: string) =>
-                  updateExample('sample', id, newInput, output)
-                }
-                onOutputChange={(newOutput: string) =>
-                  updateExample('sample', id, input, newOutput)
-                }
-              />
+              <>
+                <ExampleTextarea
+                  key={id}
+                  id={id}
+                  onRemove={() => removeExample('sample', id)}
+                  onInputChange={(newInput: string) =>
+                    updateExample('sample', id, newInput, output)
+                  }
+                  onOutputChange={(newOutput: string) =>
+                    updateExample('sample', id, input, newOutput)
+                  }
+                  className={errors.sample && errorBorderStyle}
+                />
+                {errors.sample && (
+                  <div className="flex items-center gap-1 text-xs text-red-500">
+                    <PiWarningBold />
+                    required
+                  </div>
+                )}
+              </>
             ))}
           </div>
         </div>
@@ -205,17 +320,26 @@ export default function Page() {
           </div>
           <div className="flex flex-col gap-2">
             {testcases.map(({ id, input, output }) => (
-              <ExampleTextarea
-                key={id}
-                id={id}
-                onRemove={() => removeExample('testcase', id)}
-                onInputChange={(newInput: string) =>
-                  updateExample('testcase', id, newInput, output)
-                }
-                onOutputChange={(newOutput: string) =>
-                  updateExample('testcase', id, input, newOutput)
-                }
-              />
+              <>
+                <ExampleTextarea
+                  key={id}
+                  id={id}
+                  onRemove={() => removeExample('testcase', id)}
+                  onInputChange={(newInput: string) =>
+                    updateExample('testcase', id, newInput, output)
+                  }
+                  onOutputChange={(newOutput: string) =>
+                    updateExample('testcase', id, input, newOutput)
+                  }
+                  className={errors.testcase && errorBorderStyle}
+                />
+                {errors.testcase && (
+                  <div className="flex items-center gap-1 text-xs text-red-500">
+                    <PiWarningBold />
+                    required
+                  </div>
+                )}
+              </>
             ))}
           </div>
         </div>
@@ -223,23 +347,58 @@ export default function Page() {
         <div className="flex flex-col gap-1">
           <Label>Limit</Label>
           <div className="flex gap-8">
-            <div className="flex items-center gap-2">
-              <Input
-                id="time"
-                type="text"
-                placeholder="Time"
-                className={cn(inputStyle, 'h-[36px] w-[112px]')}
-              />
-              <p className="text-sm font-bold text-gray-600">ms</p>
+            <div>
+              <div className="flex items-center gap-2">
+                <Input
+                  id="time"
+                  type="number"
+                  placeholder="Time"
+                  className={cn(
+                    inputStyle,
+                    'h-[36px] w-[112px]',
+                    errors.limit?.time && errorBorderStyle
+                  )}
+                  {...register('limit.time', {
+                    setValueAs: (value: string) => parseInt(value, 10)
+                  })}
+                />
+                <p className="text-sm font-bold text-gray-600">ms</p>
+              </div>
+              {errors.limit?.time && (
+                <div className="flex items-center gap-1 text-xs text-red-500">
+                  <PiWarningBold />
+                  {Number.isNaN(getValues('limit.time'))
+                    ? 'required'
+                    : errors.limit?.time?.message}
+                </div>
+              )}
             </div>
-            <div className="flex items-center gap-2">
-              <Input
-                id="memory"
-                type="text"
-                placeholder="Memory"
-                className={cn(inputStyle, 'h-[36px] w-[112px]')}
-              />
-              <p className="text-sm font-bold text-gray-600">MB</p>
+
+            <div>
+              <div className="flex items-center gap-2">
+                <Input
+                  id="memory"
+                  type="number"
+                  placeholder="Memory"
+                  className={cn(
+                    inputStyle,
+                    'h-[36px] w-[112px]',
+                    errors.limit?.memory && errorBorderStyle
+                  )}
+                  {...register('limit.memory', {
+                    setValueAs: (value: string) => parseInt(value, 10)
+                  })}
+                />
+                <p className="text-sm font-bold text-gray-600">MB</p>
+              </div>
+              {errors.limit?.memory && (
+                <div className="flex items-center gap-1 text-xs text-red-500">
+                  <PiWarningBold />
+                  {Number.isNaN(getValues('limit.memory'))
+                    ? 'required'
+                    : errors.limit?.memory?.message}
+                </div>
+              )}
             </div>
           </div>
         </div>
@@ -257,6 +416,7 @@ export default function Page() {
               id="hint"
               placeholder="Enter a hint"
               className="h-[120px] w-[760px] resize-none"
+              {...register('hint')}
             />
           )}
         </div>
@@ -275,6 +435,7 @@ export default function Page() {
               type="text"
               placeholder="Enter a source"
               className={cn(inputStyle, 'h-[36px] w-[380px]')}
+              {...register('source')}
             />
           )}
         </div>
@@ -282,11 +443,6 @@ export default function Page() {
         <Button
           type="submit"
           className="mt-6 flex h-[36px] w-[100px] gap-2 bg-black hover:bg-black/80"
-          onClick={(e) => {
-            e.preventDefault()
-            console.log(samples)
-            console.log(testcases)
-          }}
         >
           <IoMdCheckmarkCircleOutline fontSize={18} />
           Create
