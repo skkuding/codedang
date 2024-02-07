634675ca37f52a75e9fc46603147db6cb36161bc
Kwon Seo Jin <97675977+B0XERCAT@users.noreply.github.com>
2024-02-07 15:00:50 +0900

    feat(fe): add graphql fetcher (#1315)
    * feat(fe): add fetchergql to utils
    
    * chore(fe): comment out group select popover
    
    * fix: fix pnpm lock

--- frontend-client/app/admin/layout.tsx
+++ frontend-client/app/admin/layout.tsx
@@ -1,15 +1,23 @@
+import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
 import type { Route } from 'next'
 import Link from 'next/link'
 import { FaArrowRightFromBracket } from 'react-icons/fa6'
-import GroupSelect from './_components/GroupSelect'
+// import GroupSelect from './_components/GroupSelect'
 import SideBar from './_components/SideBar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-dvh">
       <nav className="flex w-60 flex-col p-2 pt-8 text-sm font-medium">
-        <GroupSelect />
+        {/* Todo: Group 기능 추가 시, Public Button 대신 GroupSelect 컴포넌트로 변경 */}
+        {/* <GroupSelect /> */}
+        <Button
+          variant="ghost"
+          className="h-fit justify-between text-wrap py-2 text-left text-lg font-bold text-slate-800"
+        >
+          Public
+        </Button>
         <Separator className="my-4 transition" />
         <SideBar />
         <Link
