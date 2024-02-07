79063fab71e3efd12f9ad4e4e1235cd8b9c8feea
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2024-02-07 17:37:56 +0900

    feat(be): add get tags api (#1341)

--- backend/apps/admin/src/problem/problem.service.ts
+++ backend/apps/admin/src/problem/problem.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common'
 import { Language } from '@generated'
-import type { ContestProblem, WorkbookProblem } from '@generated'
+import type { ContestProblem, Tag, WorkbookProblem } from '@generated'
 import { Level } from '@generated'
 import type { ProblemWhereInput } from '@generated'
 import { Workbook } from 'exceljs'
@@ -577,5 +577,9 @@ export class ProblemService {
     })
 
     return await this.prisma.$transaction(queries)
+  }
+
+  async getTags(): Promise<Partial<Tag>[]> {
+    return await this.prisma.tag.findMany()
   }
 }
