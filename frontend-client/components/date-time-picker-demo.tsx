b111903acf836e95d98ba2de6797c5f61f80034e
YooJin Lee <leejin914418@gmail.com>
2024-02-14 06:17:47 +0000

    feat: add calender and time picker

---
+++ frontend-client/components/date-time-picker-demo.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { TimePickerDemo } from '@/components/time-picker-demo'
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover'
+import { cn } from '@/lib/utils'
+import { format } from 'date-fns'
+import { Calendar as CalendarIcon } from 'lucide-react'
+import * as React from 'react'
+
+export function DateTimePickerDemo() {
+  const [date, setDate] = React.useState<Date>()
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={'outline'}
+          className={cn(
+            'w-[280px] justify-start text-left font-normal',
+            !date && 'text-muted-foreground'
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? format(date, 'PPP HH:mm:ss') : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+        <div className="border-border border-t p-3">
+          <TimePickerDemo setDate={setDate} date={date} />
+        </div>
+      </PopoverContent>
+    </Popover>
+  )
+}
