d02c29f8fab336bbb6067f708f27ae11e3222a93
Jaemin Choi <1dotolee@gmail.com>
2024-02-14 14:02:38 +0900

    feat(fe): add apollo vscode ext settings

---
+++ apollo.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: {
+    includes: ['./frontend-client/**/*.ts', './frontend-client/**/*.tsx'],
+    service: {
+      name: 'codedang-graphql-app',
+      url: 'https://dev.codedang.com/graphql'
+    }
+  }
+}
