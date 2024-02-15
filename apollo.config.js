9b84d7d9d4e564740c151498983def4bf2d57a25
Jaemin Choi <1dotolee@gmail.com>
2024-02-15 14:26:45 +0900

    fix: ignore generated files for apollo extension

--- apollo.config.js
+++ apollo.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   client: {
     includes: ['./frontend-client/**/*.ts', './frontend-client/**/*.tsx'],
+    excludes: ['**/__generated__/**'],
     service: {
       name: 'codedang-graphql-app',
       url: 'https://dev.codedang.com/graphql'
