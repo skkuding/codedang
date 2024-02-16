1eef65cb9df382ae5170e5ccf6ac1b23301f18dd
Peorge <realsehwan@gmail.com>
2024-02-16 05:28:10 +0000

    feat: add field resolvers
    - problem -> problemTag
    - problem -> problemTestcase
    - problemTag -> tag

--- backend/apps/admin/src/problem/problem.module.ts
+++ backend/apps/admin/src/problem/problem.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { StorageModule } from '@admin/storage/storage.module'
 import { ProblemResolver } from './problem.resolver'
 import { ProblemService } from './problem.service'
+import { ProblemTagResolver, TagResolver } from './problemTag.resolver'
 
 @Module({
   imports: [StorageModule],
-  providers: [ProblemResolver, ProblemService]
+  providers: [ProblemResolver, ProblemTagResolver, TagResolver, ProblemService]
 })
 export class ProblemModule {}
