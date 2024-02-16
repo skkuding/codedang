bac2800e75639acecf0e093cdf7678d49e9a6cd4
Peorge <realsehwan@gmail.com>
2024-02-16 07:25:56 +0000

    refactor: add PR review comments

--- backend/apps/admin/src/problem/problem.module.ts
+++ backend/apps/admin/src/problem/problem.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common'
 import { StorageModule } from '@admin/storage/storage.module'
+import { ProblemTagResolver, TagResolver } from './problem-tag.resolver'
 import { ProblemResolver } from './problem.resolver'
 import { ProblemService } from './problem.service'
-import { ProblemTagResolver, TagResolver } from './problemTag.resolver'
 
 @Module({
   imports: [StorageModule],
