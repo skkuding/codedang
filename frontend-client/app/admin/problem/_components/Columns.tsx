3f74495cadb88f9fd711584ad1c04e0ef06cedc4
Jiho Park <59248080+jihorobert@users.noreply.github.com>
2024-02-17 22:23:57 +0900

    feat(fe): header for admin problem list page (#1395)
    * feat(fe): header for admin problem list page
    
    * fix: fix href for edit page
    
    * feat(fe): add language filter and update column
    
    * fix(fe): fix css style
    
    * fix: disable switch button for later

--- frontend-client/app/admin/problem/_components/Columns.tsx
+++ frontend-client/app/admin/problem/_components/Columns.tsx
@@ -1,12 +1,10 @@
-'use client'
-
 import { DataTableColumnHeader } from '@/components/DataTableColumnHeader'
 import { Badge } from '@/components/ui/badge'
 import { Checkbox } from '@/components/ui/checkbox'
 import { Switch } from '@/components/ui/switch'
-import type { Problem } from '@/types/type'
+// import { fetcherGql } from '@/lib/utils'
+// import { gql } from '@apollo/client'
 import type { ColumnDef } from '@tanstack/react-table'
-import { useState } from 'react'
 import { FiEyeOff } from 'react-icons/fi'
 import { FiEye } from 'react-icons/fi'
 
@@ -15,25 +13,29 @@ interface Tag {
   name: string
 }
 
-// No api for hidden (임시 hiddenCell 만듦)
-function HiddenCell() {
-  const [visible, setVisible] = useState(false)
-
-  return (
-    <div className="flex space-x-2">
-      <Switch id="hidden-mode" checked={visible} onCheckedChange={setVisible} />
-      <div className="flex items-center justify-center">
-        {visible ? (
-          <FiEye className="text-primary h-[14px] w-[14px]" />
-        ) : (
-          <FiEyeOff className="h-[14px] w-[14px] text-gray-400" />
-        )}
-      </div>
-    </div>
-  )
+interface DataTableProblem {
+  id: number
+  title: string
+  updateTime: string
+  difficulty: string
+  submissionCount: number
+  acceptedRate: number
+  isVisible: boolean
+  languages: string[]
+  problemTag: { id: number; tag: Tag }[]
 }
 
-export const columns: ColumnDef<Problem>[] = [
+// const EDIT_VISIBLE = gql`
+//   mutation UpdateProblem($groupId: Int!, $input: UpdateProblemInput!) {
+//     updateProblem(groupId: $groupId, input: $input) {
+//       id
+//       title
+//       isVisible
+//     }
+//   }
+// `
+
+export const columns: ColumnDef<DataTableProblem>[] = [
   {
     id: 'select',
     header: ({ table }) => (
@@ -44,7 +46,7 @@ export const columns: ColumnDef<Problem>[] = [
         }
         onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
         aria-label="Select all"
-        className="translate-y-[2px]"
+        className="translate-y-[2px] bg-white"
       />
     ),
     cell: ({ row }) => (
@@ -52,7 +54,7 @@ export const columns: ColumnDef<Problem>[] = [
         checked={row.getIsSelected()}
         onCheckedChange={(value) => row.toggleSelected(!!value)}
         aria-label="Select row"
-        className="translate-y-[2px]"
+        className="translate-y-[2px] bg-white"
       />
     ),
     enableSorting: false,
@@ -63,58 +65,85 @@ export const columns: ColumnDef<Problem>[] = [
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Title" />
     ),
-    cell: ({ row }) => (
-      <div className="flex-col">
-        {row.getValue('title')}
-        <div className="flex">
-          <div>
-            {row.original.tags?.map((tag) => (
-              <Badge
-                key={tag.id}
-                variant="secondary"
-                className="mr-1 whitespace-nowrap rounded-md border-gray-400 bg-gray-300 px-1 font-normal"
-              >
-                {tag.name}
-              </Badge>
-            ))}
+    cell: ({ row }) => {
+      return (
+        <div className="flex-col">
+          {row.getValue('title')}
+          <div className="flex">
+            <div>
+              {row.original.problemTag?.map((tag) => (
+                <Badge
+                  key={tag.id}
+                  variant="secondary"
+                  className="mr-1 whitespace-nowrap rounded-md border-gray-400 bg-gray-300 px-1 font-normal"
+                >
+                  {tag.tag.name}
+                </Badge>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    ),
+      )
+    },
     enableSorting: false,
     enableHiding: false
   },
   /**
    * @description
-   * mad this column for filtering tags
+   * made this column for filtering languages
    * doesn't show in datatable
    */
   {
-    accessorKey: 'tags',
+    accessorKey: 'languages',
     header: () => {},
     cell: () => {},
-
     filterFn: (row, id, value) => {
-      const tags = row.original.tags
+      const languages = row.original.languages
+      if (!languages?.length) {
+        return false
+      }
 
+      const langValue: string[] = row.getValue(id)
+      const valueArray = value as string[]
+      const result = langValue.every((language) =>
+        valueArray.includes(language)
+      )
+      return result
+    }
+  },
+  /**
+   * @description
+   * made this column for filtering languages
+   * doesn't show in datatable
+   */
+  {
+    accessorKey: 'problemTag',
+    header: () => {},
+    cell: () => {},
+    filterFn: (row, id, value) => {
+      const tags = row.original.problemTag
       if (!tags?.length) {
-        return false // 만약 태그가 없는 경우 false 반환
+        return false
       }
 
       const tagValue = row.getValue(id)
-      const valueArray = value as number[] // value 배열로 타입 변환
-      const tagIdArray = (tagValue as Tag[]).map((tag) => tag.id) // tagValue의 id로 이루어진 배열 생성
-      const result = tagIdArray.every((tagId) => valueArray.includes(tagId)) // 모든 tagId가 valueArray에 포함되어 있는지 확인
-
+      const valueArray = value as number[]
+      const tagIdArray = (tagValue as { id: number; tag: Tag }[]).map(
+        (tag) => tag.tag.id
+      )
+      const result = tagIdArray.every((tagId) => valueArray.includes(tagId))
       return result
     }
   },
   {
-    accessorKey: 'date',
+    accessorKey: 'updateTime',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Update" />
     ),
-    cell: ({ row }) => row.getValue('date')
+    cell: ({ row }) => {
+      const updateTime: string = row.getValue('updateTime')
+      return <div>{updateTime.substring(2, 10)}</div>
+    }
   },
   {
     accessorKey: 'difficulty',
@@ -146,10 +175,47 @@ export const columns: ColumnDef<Problem>[] = [
     }
   },
   {
-    accessorKey: 'hidden',
+    accessorKey: 'isVisible',
     header: ({ column }) => (
       <DataTableColumnHeader column={column} title="Visible" />
     ),
-    cell: HiddenCell
+    cell: ({ row }) => {
+      const isVisible: boolean = row.getValue('isVisible')
+
+      // const updateVisible = async (data: {
+      //   id: number
+      //   title: string
+      //   isVisible: boolean
+      // }) => {
+      //   const res = await fetcherGql(EDIT_VISIBLE, {
+      //     groupId: 1,
+      //     input: data
+      //   })
+      //   console.log('res: ', res)
+      // }
+
+      return (
+        <div className="flex space-x-2">
+          <Switch
+            id="hidden-mode"
+            checked={isVisible}
+            // onCheckedChange={() => {
+            //   updateVisible({
+            //     id: row.original.id,
+            //     title: row.original.title,
+            //     isVisible: !isVisible
+            //   })
+            // }}
+          />
+          <div className="flex items-center justify-center">
+            {isVisible ? (
+              <FiEye className="text-primary h-[14px] w-[14px]" />
+            ) : (
+              <FiEyeOff className="h-[14px] w-[14px] text-gray-400" />
+            )}
+          </div>
+        </div>
+      )
+    }
   }
 ]
--- frontend-client/app/admin/problem/page.tsx
+++ frontend-client/app/admin/problem/page.tsx
@@ -1,96 +1,124 @@
+'use client'
+
 import { DataTableAdmin } from '@/components/DataTableAdmin'
-import { fetcher } from '@/lib/utils'
-import type { Problem } from '@/types/type'
+import { Button } from '@/components/ui/button'
+import { fetcherGql } from '@/lib/utils'
+import { gql } from '@apollo/client'
+import { PlusCircleIcon } from 'lucide-react'
+import Link from 'next/link'
 import * as React from 'react'
+import { useEffect, useState } from 'react'
 import { columns } from './_components/Columns'
 
-// const GET_PROBLEMS = gql`
-//   query GetProblems($groupId: Int!, $cursor: Int, $take: Int!, $input: FilterProblemsInput!) {
-//     getProblems(groupId: $groupId, cursor: $cursor, take: $take, input: $input) {
-//       id
-//       title
-//       difficulty
-//       submissionCount
-//       acceptedRate
-//       languages
-//       problemTag{
-//         tag{
-//           id
-//           name
-//         }
-//       }
-//     }
-//   }
-// `
+interface Tag {
+  id: number
+  name: string
+}
 
-// interface DataTableProblem {
-//   id: string
-//   title: string
-//   difficulty: string
-//   submissionCount: number
-//   acceptedRate: number
-//   languages: string[]
-//   problemTag: Tag[]
-// }
+interface DataTableProblem {
+  id: number
+  title: string
+  updateTime: string
+  difficulty: string
+  submissionCount: number
+  acceptedRate: number
+  isVisible: boolean
+  languages: string[]
+  problemTag: { id: number; tag: Tag }[]
+}
 
-// const GET_PROBLEMS = gql`
-//   query GetProblems($groupId: Int!, $cursor: Int, $take: Int!, $input: FilterProblemsInput!) {
-//     getProblems(groupId: $groupId, cursor: $cursor, take: $take, input: $input) {
-//       id
-//       title
-//       difficulty
-//       submissionCount
-//       acceptedRate
-//       languages
-//       problemTag{
-//         tag{
-//           id
-//           name
-//         }
-//       }
-//     }
-//   }
-// `
+const GET_PROBLEMS = gql`
+  query GetProblems(
+    $groupId: Int!
+    $cursor: Int
+    $take: Int!
+    $input: FilterProblemsInput!
+  ) {
+    getProblems(
+      groupId: $groupId
+      cursor: $cursor
+      take: $take
+      input: $input
+    ) {
+      id
+      title
+      updateTime
+      difficulty
+      submissionCount
+      acceptedRate
+      isVisible
+      languages
+      problemTag {
+        id
+        tag {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
 export const dynamic = 'force-dynamic'
 
-export default async function Page() {
-  // const [problems, setProblems] = useState<Problem[]>([])
-  // useEffect(() => {
-  //   fetcherGql(GET_PROBLEMS, {
-  //     groupId: 1,
-  //     cursor: 1,
-  //     take: 10,
-  //     input: {
-  //       difficulty: ["Level1", "Level2", "Level3", "Level4", "Level5"],
-  //       languages: ["C", "Cpp", "Java", "Python3"],
-  //     }
+export default function Page() {
+  const [problems, setProblems] = useState<DataTableProblem[]>([])
 
-  //   }).then((data) => {
-  //     const transformedData = data.getProblems.map(
-  //       (problem: { id: string; title: string, difficulty: string, submissionCount: number, acceptedRate: number, languages: string[], problemTag: Tag[] }) => ({
-  //         ...problem,
-  //         id: Number(problem.id),
-  //         problemTag: problem.problemTag.map((tag: Tag) => ({
-  //           ...tag,
-  //           id: Number(tag.id)
-  //         }))
-  //       })
-  //     )
-  //     setProblems(transformedData)
-  //   })
-  // }, [])
-
-  // TODO:  Codedang Admin Get Problems API에 problemTags내용이 추가되면 Admin에서 가져오기 (현재는 problemTag 전부 null로 돼있음)
-  const { problems }: { problems: Problem[] } = await fetcher
-    .get('problem', {
-      searchParams: {
-        take: 15
+  useEffect(() => {
+    fetcherGql(GET_PROBLEMS, {
+      groupId: 1,
+      cursor: 1,
+      take: 8,
+      input: {
+        difficulty: ['Level1', 'Level2', 'Level3', 'Level4', 'Level5'],
+        languages: ['C', 'Cpp', 'Java', 'Python3']
       }
+    }).then((data) => {
+      const transformedData = data.getProblems.map(
+        (problem: {
+          id: string
+          title: string
+          updateTime: string
+          difficulty: string
+          submissionCount: number
+          acceptedRate: number
+          languages: string[]
+          problemTag: { id: string; tag: Tag }[]
+        }) => ({
+          ...problem,
+          id: Number(problem.id),
+          problemTag: problem.problemTag.map(
+            (tag: { id: string; tag: Tag }) => ({
+              ...tag,
+              id: Number(tag.id),
+              tag: {
+                ...tag.tag,
+                id: Number(tag.tag.id)
+              }
+            })
+          )
+        })
+      )
+      setProblems(transformedData)
     })
-    .json()
+  }, [])
 
   return (
-    <div className="container mx-auto py-10">
+    <div className="container mx-auto space-y-5 py-10">
+      <div className="flex justify-between">
+        <div>
+          <p className="text-4xl font-bold">Problem List</p>
+          <p className="flex text-lg text-slate-500">
+            Here&apos;s a list you made
+          </p>
+        </div>
+        <Link href="/admin/problem/create">
+          <Button variant="default">
+            <PlusCircleIcon className="mr-2 h-4 w-4" />
+            Create
+          </Button>
+        </Link>
+      </div>
       <DataTableAdmin columns={columns} data={problems} />
     </div>
   )
