f113fa5cc503bf72d5004d22e1ae4b83bc5841c7
Jaehyeon1020 <kjhkk1020@naver.com>
2024-02-17 03:40:21 +0000

    chore(be): remove useless comment

--- backend/apps/client/src/problem/problem.service.ts
+++ backend/apps/client/src/problem/problem.service.ts
@@ -16,8 +16,6 @@ import { RelatedProblemsResponseDto } from './dto/related-problems.response.dto'
 import type { ProblemOrder } from './enum/problem-order.enum'
 import { ProblemRepository } from './problem.repository'
 
-// import type { ProblemOrder } from './schema/problem-order.schema'
-
 @Injectable()
 export class ProblemService {
   constructor(private readonly problemRepository: ProblemRepository) {}
