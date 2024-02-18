bd0ca88c785bae208dc6aff81ba599421b9f9e7c
LEE HAESUNG <102472529+JungangHighschool@users.noreply.github.com>
2024-02-18 17:23:45 +0900

    feat(be): admin create problem with sample (#1373)
    * fix(be): add sample model
    
    * fix(be): mocking and bruno modification
    
    * fix(be): test code type check
    
    * fix(be): spec code
    
    * refactor(be): upload problem method
    
    * fix(be): remove comments
    
    * fix(be): remove comments
    
    * fix(be): bruno
    
    * Update schema.prisma
    
    주석 제거
    
    * fix(be): add isVisible
    
    * fix(be): remove comments
    
    * fix: resolve invalid type
    
    * fix: include samples in return data
    
    ---------
    
    Co-authored-by: Jaemin Choi <1dotolee@gmail.com>

--- backend/apps/admin/src/problem/model/testcase.input.ts
+++ backend/apps/admin/src/problem/model/testcase.input.ts
@@ -1,13 +1,16 @@
 import { Field, InputType, Int } from '@nestjs/graphql'
 
 @InputType()
-export class Testcase {
+export class Sample {
   @Field(() => String, { nullable: false })
   input!: string
 
   @Field(() => String, { nullable: false })
   output!: string
+}
 
+@InputType()
+export class Testcase extends Sample {
   @Field(() => Int, { nullable: true })
   scoreWeight?: number
 }
