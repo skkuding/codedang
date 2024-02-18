4cc98804e1c92869c87fc14254adbae519cb9123
Jaemin Choi <1dotolee@gmail.com>
2024-02-18 20:28:02 +0900

    feat(fe): add apollo vscode ext settings (#1379)
    * feat(fe): add apollo vscode ext settings
    
    * feat(fe): add apollo codegen
    
    * fix: ignore generated files for apollo extension
    
    * feat: use useClient hook
    
    * chore: only ban importing gql from apollo/client
    
    * chore: remove unused dockerfile
    
    * chore: upgrade pnpm action

--- frontend-client/app/admin/problem/utils.ts
+++ frontend-client/app/admin/problem/utils.ts
@@ -1,12 +1,13 @@
+import { gql } from '@generated'
 import type { Language } from '@/types/type'
-import { gql } from '@apollo/client'
+import { Language as TLanguage } from '@generated/graphql'
 
 export const inputStyle =
   'border-gray-200 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950'
 
 export const levels = ['Level1', 'Level2', 'Level3', 'Level4', 'Level5']
 
-export const languageOptions = [
+export const languageOptions: Language[] = [
   'C',
   'Cpp',
   'Golang',
@@ -15,16 +16,25 @@ export const languageOptions = [
   'Python3'
 ]
 
+export const languageMapper: Record<Language, TLanguage> = {
+  C: TLanguage.C,
+  Cpp: TLanguage.Cpp,
+  Golang: TLanguage.Golang,
+  Java: TLanguage.Java,
+  Python2: TLanguage.Python2,
+  Python3: TLanguage.Python3
+}
+
 export interface TemplateLanguage {
   language: Language
   isVisible: boolean
 }
 
-export const GET_TAGS = gql`
+export const GET_TAGS = gql(`
   query GetTags {
     getTags {
       id
       name
     }
   }
-`
+`)
