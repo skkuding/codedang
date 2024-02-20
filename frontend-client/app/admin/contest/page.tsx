7c8fb10526194e27178689122deaa6cad242de98
b0xercat <seojin3154@naver.com>
2024-02-20 22:14:38 +0900

    fix(fe): change button using asChild

--- frontend-client/app/admin/contest/page.tsx
+++ frontend-client/app/admin/contest/page.tsx
@@ -35,12 +35,12 @@ export default function Page() {
           <p className="text-lg text-slate-500">Here&apos;s a list you made</p>
         </div>
         {/* TODO: 주소를 /admin/contest/create로 수정하기 */}
-        <Link href="/admin/problem/create">
-          <Button variant="default">
+        <Button variant="default" asChild>
+          <Link href="/admin/problem/create">
             <PlusCircleIcon className="mr-2 h-4 w-4" />
             Create
-          </Button>
-        </Link>
+          </Link>
+        </Button>
       </div>
       <DataTableAdmin columns={columns} data={contests} />
     </div>
