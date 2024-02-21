4300c79c82c781a2a87307a4db8529adf97d6d87
Kwon Seo Jin <97675977+B0XERCAT@users.noreply.github.com>
2024-02-21 20:15:05 +0900

    feat(fe): add submission skeleton (#1462)
    * feat(fe): add submission skeleton
    
    * feat(fe): add submission detail skeleton
    
    * fix(fe): refactor loading components
    
    * chore(fe): show only one skeleton per table row
    
    * feat(fe): show loading skeleton on component basis
    
    ---------
    
    Co-authored-by: Sori Lim <aint.imsorry@gmail.com>

---
+++ frontend-client/app/problem/[problemId]/submission/loading.tsx
@@ -0,0 +1,17 @@
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function Loading() {
+  return (
+    <div className="flex h-fit flex-col gap-5 p-6 text-lg">
+      <Skeleton className="h-12 w-full rounded-xl bg-slate-900" />
+      <div className="flex flex-col gap-4">
+        {[...Array(5)].map((_, i) => (
+          <Skeleton
+            key={i}
+            className="flex h-8 w-full rounded-xl bg-slate-900"
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
