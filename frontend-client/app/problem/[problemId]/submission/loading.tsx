01d67ef43eab2a51714b32de5fd481bb76664e2e
Sori Lim <aint.imsorry@gmail.com>
2024-02-21 19:39:50 +0900

    chore(fe): show only one skeleton per table row

--- frontend-client/app/problem/[problemId]/submission/loading.tsx
+++ frontend-client/app/problem/[problemId]/submission/loading.tsx
@@ -2,18 +2,14 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export default function Loading() {
   return (
-    <div className="flex h-fit flex-col gap-4 p-6 text-lg">
-      <Skeleton className="h-10 w-full bg-slate-900" />
-      <div className="flex flex-col gap-2">
-        {[...Array(16)].map((_, i) => (
-          <div key={i} className="flex gap-2">
-            <Skeleton className="h-8 w-1/12 bg-slate-900" />
-            <Skeleton className="h-8 w-1/12 bg-slate-900" />
-            <Skeleton className="h-8 w-3/12 bg-slate-900" />
-            <Skeleton className="h-8 w-2/12 bg-slate-900" />
-            <Skeleton className="h-8 w-3/12 bg-slate-900" />
-            <Skeleton className="h-8 w-2/12 bg-slate-900" />
-          </div>
+    <div className="flex h-fit flex-col gap-5 p-6 text-lg">
+      <Skeleton className="h-12 w-full rounded-xl bg-slate-900" />
+      <div className="flex flex-col gap-4">
+        {[...Array(5)].map((_, i) => (
+          <Skeleton
+            key={i}
+            className="flex h-8 w-full rounded-xl bg-slate-900"
+          />
         ))}
       </div>
     </div>
