9432a76183f9bf5c9942e20134f4d3b8d751d009
b0xercat <seojin3154@naver.com>
2024-02-21 16:44:52 +0900

    fix(fe): refactor loading components

--- frontend-client/app/problem/[problemId]/submission/[submissionId]/loading.tsx
+++ frontend-client/app/problem/[problemId]/submission/[submissionId]/loading.tsx
@@ -1,5 +1,11 @@
-import SubmissionDetailLoading from './_components/SubmissionDetailLoading'
+import { Skeleton } from '@/components/ui/skeleton'
 
 export default function Loading() {
-  return <SubmissionDetailLoading />
+  return (
+    <div className="flex h-fit flex-col gap-4 p-2 text-lg">
+      <Skeleton className="h-20 w-full bg-slate-900" />
+      <Skeleton className="h-8 w-3/12 bg-slate-900" />
+      <Skeleton className="h-32 w-full bg-slate-900" />
+    </div>
+  )
 }
