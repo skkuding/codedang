709148c10ce8b324ee0f9ffe5e1e7cdfa3c36fb9
Jaemin Choi <1dotolee@gmail.com>
2024-02-22 00:13:13 +0900

    chore: change env name

--- frontend-client/next.config.js
+++ frontend-client/next.config.js
@@ -24,7 +24,7 @@ module.exports = withSentryConfig(
     // Suppresses source map uploading logs during build
     silent: true,
     org: 'skkuding',
-    project: process.env.SENTRY_PROJECT_NAME,
+    project: process.env.SENTRY_PROJECT,
     url: 'https://sentry.codedang.com'
   },
   {
