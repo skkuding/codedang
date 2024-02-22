3afb483853a27bcb9adfaf0c207ca622400d020b
Gyunseo Lee <rbstj0311@naver.com>
2024-02-22 17:33:48 +0900

    feat(be): implement the management of the user's sessions when using multiple clients (#1387)
    * feat(be): implement the management of the user's sessions, using multiple clients
    
    * fix(be): fix the wrong logic of checking the specific refresh token in the refresh token list
    
    * test(be): fix the service test code
    
    * fix(be): delete the unnecessary comment
    
    * fix(be): fix the auth service code
    
    * fix(be): fix the auth controller
    
    * Revert "fix(be): fix the auth controller"
    
    This reverts commit c3853dd74ef74a357aaf2cee7fc5e154937102b3.
    
    * Revert "test(be): fix the service test code"
    
    This reverts commit c2704c1f98e50d0604a69ccb449c69d5336ac0fb.
    
    * revert: this reverts commits from e661643a to 08fb6ad6
    
    * fix(be): change the refresh token cache key
    
    * fix(be): change the params of refresh token cache key func and logic of cache validation
    
    * fix(be): add the refresh token param to logout endpoint
    
    * test(be): fix the service test code
    
    * fix(be): fix the logout logic when the refresh token is non-existent

--- backend/libs/cache/src/keys.ts
+++ backend/libs/cache/src/keys.ts
@@ -1,5 +1,5 @@
-export const refreshTokenCacheKey = (userId: number) =>
-  `user:${userId}:refresh_token`
+export const refreshTokenCacheKey = (userId: number, refreshToken: string) =>
+  `user:${userId}:${refreshToken}`
 
 export const emailAuthenticationPinCacheKey = (email: string) =>
   `email:${email}:email-auth`
