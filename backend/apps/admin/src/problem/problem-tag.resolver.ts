c09184bccae6b1946711157afdd7585e7312fd1d
Gyunseo Lee <rbstj0311@naver.com>
2024-02-26 13:33:29 +0000

    fix(be): add a update and delete mutation

--- backend/apps/admin/src/problem/problem-tag.resolver.ts
+++ backend/apps/admin/src/problem/problem-tag.resolver.ts
@@ -12,7 +12,10 @@ import {
   Args
 } from '@nestjs/graphql'
 import { ProblemTag, Tag } from '@generated'
-import { DuplicateFoundException } from '@libs/exception'
+import {
+  DuplicateFoundException,
+  EntityNotExistException
+} from '@libs/exception'
 import { ProblemService } from './problem.service'
 
 @Resolver(() => ProblemTag)
@@ -55,6 +58,54 @@ export class TagResolver {
       return await this.problemService.createTags(formattedTagNames)
     } catch (error) {
       if (error instanceof DuplicateFoundException) {
+        throw error.convert2HTTPException()
+      }
+      this.logger.error(error)
+      throw new InternalServerErrorException()
+    }
+  }
+
+  @Mutation(() => Tag)
+  async updateTag(
+    @Args('oldTagName', { type: () => String })
+    oldTagName: string,
+    @Args('newTagName', { type: () => String })
+    newTagName: string
+  ) {
+    try {
+      // 각 태그 이름을 원하는 형식으로 변환
+      const formattedOldTagName = oldTagName
+        .replace(/\s+/g, '-') // 띄어쓰기를 '-'로 대체
+        .toLowerCase() // 모든 영문자를 소문자로 변환
+      const formattedNewTagName = newTagName
+        .replace(/\s+/g, '-') // 띄어쓰기를 '-'로 대체
+        .toLowerCase() // 모든 영문자를 소문자로 변환
+
+      return await this.problemService.updateTag(
+        formattedOldTagName,
+        formattedNewTagName
+      )
+    } catch (error) {
+      if (
+        error instanceof EntityNotExistException ||
+        error instanceof DuplicateFoundException
+      ) {
+        throw error.convert2HTTPException()
+      }
+      this.logger.error(error)
+      throw new InternalServerErrorException()
+    }
+  }
+
+  @Mutation(() => Tag)
+  async deleteTag(@Args('tagName', { type: () => String }) tagName: string) {
+    try {
+      const formattedTagName = tagName
+        .replace(/\s+/g, '-') // 띄어쓰기를 '-'로 대체
+        .toLowerCase() // 모든 영문자를 소문자로 변환
+      return await this.problemService.deleteTag(formattedTagName)
+    } catch (error) {
+      if (error instanceof EntityNotExistException) {
         throw error.convert2HTTPException()
       }
       this.logger.error(error)
