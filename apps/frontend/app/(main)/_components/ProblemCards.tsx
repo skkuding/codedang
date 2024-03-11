65b344a638cce7e30f4217bbd69e9c81806050bf
jiho <jihopark09@naver.com>
2024-03-11 22:31:08 +0900

    fix: comment

--- apps/frontend/app/(main)/_components/ProblemCards.tsx
+++ apps/frontend/app/(main)/_components/ProblemCards.tsx
@@ -13,7 +13,6 @@ const getProblems = async () => {
       }
     })
     .json()
-
   return problems
 }
 
--- apps/frontend/components/auth/SignUpWelcome.tsx
+++ apps/frontend/components/auth/SignUpWelcome.tsx
@@ -2,11 +2,12 @@
 
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
-import KakaotalkLogo from '@/public/kakaotalk.svg'
+// import KakaotalkLogo from '@/public/kakaotalk.svg'
 import useSignUpModalStore from '@/stores/signUpModal'
-import Image from 'next/image'
-import { FaGithub } from 'react-icons/fa'
-import { FcGoogle } from 'react-icons/fc'
+
+// import Image from 'next/image'
+// import { FaGithub } from 'react-icons/fa'
+// import { FcGoogle } from 'react-icons/fc'
 
 export default function SignUpWelcome() {
   const { nextModal } = useSignUpModalStore((state) => state)
@@ -25,7 +26,7 @@ export default function SignUpWelcome() {
         </p>
         <Separator className="flex-1" />
       </div>
-      <div className="flex w-full items-center justify-center gap-5">
+      {/* <div className="flex w-full items-center justify-center gap-5">
         <div className="flex aspect-square w-12 cursor-pointer items-center justify-center rounded-full bg-[#FEE500] hover:opacity-80">
           <Image src={KakaotalkLogo} alt="카카오톡" width={20} />
         </div>
@@ -35,7 +36,7 @@ export default function SignUpWelcome() {
         <div className="flex aspect-square w-12 cursor-pointer items-center justify-center rounded-full bg-[#212528] hover:opacity-80">
           <FaGithub className="text-white" size="22" />
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
