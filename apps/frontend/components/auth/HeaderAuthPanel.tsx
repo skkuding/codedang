46a8bcc0309362770920891d247764ce7495463a
jiho <jihopark09@naver.com>
2024-03-11 23:22:53 +0900

    fix(fe): add hamburger

--- apps/frontend/components/auth/HeaderAuthPanel.tsx
+++ apps/frontend/components/auth/HeaderAuthPanel.tsx
@@ -6,12 +6,14 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuTrigger,
-  DropdownMenuItem
+  DropdownMenuItem,
+  DropdownMenuSeparator
 } from '@/components/ui/dropdown-menu'
 import { cn } from '@/lib/utils'
 import useAuthModalStore from '@/stores/authModal'
 import useSignUpModalStore from '@/stores/signUpModal'
 import { LogOut, User, UserRoundCog } from 'lucide-react'
+import type { Route } from 'next'
 import type { Session } from 'next-auth'
 import { signOut } from 'next-auth/react'
 import Link from 'next/link'
@@ -41,7 +43,7 @@ export default function HeaderAuthPanel({
     <div className="ml-2 flex items-center gap-2">
       {session ? (
         <DropdownMenu>
-          <DropdownMenuTrigger className="flex gap-2 px-4 py-1">
+          <DropdownMenuTrigger className="hidden gap-2 px-4 py-1 md:flex">
             <p
               className={
                 group === 'editor'
@@ -110,7 +112,93 @@ export default function HeaderAuthPanel({
           </DialogContent>
         </Dialog>
       )}
-      <RxHamburgerMenu size="30" className="md:hidden" />
+      {session ? (
+        <DropdownMenu>
+          <DropdownMenuTrigger className="flex gap-2 px-4 py-1 md:hidden">
+            <RxHamburgerMenu size="30" className="md:hidden" />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {session?.user.role !== 'User' && (
+              <Link href="/admin">
+                <DropdownMenuItem className="flex cursor-pointer items-center gap-1 font-semibold">
+                  <UserRoundCog className="size-4" /> Management
+                </DropdownMenuItem>
+              </Link>
+            )}
+            <DropdownMenuItem
+              className="text-primary flex cursor-pointer items-center gap-1 font-semibold"
+              onClick={() => {
+                signOut()
+              }}
+            >
+              {session?.user.username}
+            </DropdownMenuItem>
+            <DropdownMenuSeparator className="bg-gray-300" />
+            <Link href="/notice">
+              <DropdownMenuItem className="flex cursor-pointer items-center gap-1 font-semibold">
+                Notice
+              </DropdownMenuItem>
+            </Link>
+            <Link href="/contest">
+              <DropdownMenuItem className="flex cursor-pointer items-center gap-1 font-semibold">
+                Contest
+              </DropdownMenuItem>
+            </Link>
+            <Link href="/problem">
+              <DropdownMenuItem className="flex cursor-pointer items-center gap-1 font-semibold">
+                Problem
+              </DropdownMenuItem>
+            </Link>
+            <DropdownMenuSeparator className="bg-gray-300" />
+            <DropdownMenuItem
+              className="flex cursor-pointer items-center gap-1 font-semibold"
+              onClick={() => {
+                signOut()
+              }}
+            >
+              <LogOut className="size-4" /> Log Out
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <Dialog open={currentModal !== ''} onOpenChange={hideModal}>
+          <DialogTrigger asChild>
+            <Button
+              onClick={() => showSignIn()}
+              variant={variants[group]}
+              className={cn(
+                'flex border-none px-3 py-1 text-base md:hidden ',
+                group === 'editor' ? 'font-medium' : 'font-semibold'
+              )}
+            >
+              Log In
+            </Button>
+          </DialogTrigger>
+          <DialogTrigger asChild>
+            <Button
+              onClick={() => {
+                showSignUp()
+                setModalPage(0)
+              }}
+              variant={variants[group]}
+              className={cn(
+                'flex border-none px-3 py-1 text-base md:hidden ',
+                group === 'editor' ? 'font-medium' : 'font-bold'
+              )}
+            >
+              Sign Up
+            </Button>
+          </DialogTrigger>
+          <DialogContent
+            onOpenAutoFocus={(e) => {
+              e.preventDefault()
+            }}
+            className="min-h-[30rem] max-w-[20.5rem]"
+          >
+            <AuthModal />
+          </DialogContent>
+        </Dialog>
+      )}
     </div>
   )
 }
