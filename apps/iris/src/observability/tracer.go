0a1510004b09a95bb855caf2927c46b6df913f48
김일건 <100272259+k1g99@users.noreply.github.com>
2024-03-12 22:53:03 +0900

    feat(infra): add opentelemetry trace instrumentation (#1540)
    * chore: install otel libraries
    
    * feat: add codes for automatic instrumentation in client api
    
    * chore: remove otel libiraries
    
    * chore: reinstall otel libraries
    
    * feat: add otel http and express instrumentation
    
    * feat: add otel prisma instrumentation
    postgres instrumentation이 있어서 시도해봤지만, 설정이 안되어서 prisma instrumentation을 적용하였습니다.
    
    * chore: send traces is batches in production mode
    
    * chore: add nestjs-otel package
    
    * feat: add tracing span for submission(MQ, service)
    
    Submission 모듈에 Service, Mq 에 대한 tracing span을 추가했습니다.
    
    * feat: add iris otel default
    
    * feat: add iris tracing code
    
    judge-handler.go 파일의 compile, getTest ase, runner.run 함수 부분에 Tracing 코드를 추가했습니다.
    
    * chore: change JUDGE Handler span attribute type
    
    JUDGE Handler의 span attribute submissionId의 타입을 int로 통일합니다
    
    * chore: remove iris build file
    
    * feat: keep otel_endpoint secret
    
    * chore: update go module and tidy
    
    * chore: tracer initialization from main file
    
    * chore: use batchspanprocessor only
    
    * chore: change iris info msg for dev mode
    
    * fix: fix merge errors
    
    * chore: resolve dependencies for submissionservice test
    
    ---------
    
    Co-authored-by: Goathoon <ownest2@gmail.com>

---
+++ apps/iris/src/observability/tracer.go
@@ -0,0 +1,78 @@
+package observability
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/skkuding/codedang/iris/src/utils"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+)
+
+func InitTracer(ctx context.Context) func() {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+
+	// Resource
+	res, err := newResource(ctx)
+	reportErr(err, "failed to create resource")
+
+	// Exporter
+	traceExporter, err := newExporter(ctx)
+	reportErr(err, "failed to create exporter")
+
+	// TraceProvider
+	batchSpanProcessor := sdktrace.NewBatchSpanProcessor(traceExporter)
+	tracerProvider := newTraceProvider(res, batchSpanProcessor)
+	otel.SetTracerProvider(tracerProvider)
+
+	return func() {
+		// Shutdown will flush any remaining spans and shut down the exporter.
+		reportErr(tracerProvider.Shutdown(ctx), "failed to shutdown TracerProvider")
+		cancel()
+	}
+
+}
+
+func newTraceProvider(res *resource.Resource, bsp sdktrace.SpanProcessor) *sdktrace.TracerProvider {
+	tracerProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	return tracerProvider
+}
+
+func newResource(ctx context.Context) (*resource.Resource, error) {
+	// Create a new resource with a service name and the service version.
+	r, err := resource.New(ctx,
+		resource.WithAttributes(
+			// the service name used to display traces in backends
+			semconv.ServiceNameKey.String("IRIS"),
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return r, nil
+}
+
+func newExporter(ctx context.Context) (*otlptrace.Exporter, error) {
+	oltpEndpoint := utils.Getenv("OTEL_EXPORTER_OTLP_ENDPOINT", "")
+	exp, err := otlptracehttp.New(ctx, otlptracehttp.WithEndpoint(oltpEndpoint), otlptracehttp.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	return exp, nil
+}
+
+func reportErr(err error, message string) {
+	if err != nil {
+		// print error message
+		fmt.Print(message)
+	}
+}
