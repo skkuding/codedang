91084f7b2440b365813da5aa9633ce7e7457d9da
YooJin Lee <leejin914418@gmail.com>
2024-03-15 05:13:59 +0000

    chore: change provider location

--- apps/frontend/app/(main)/layout.tsx
+++ apps/frontend/app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import Footer from './_components/Footer'
 import Header from './_components/Header'
-import Provider from './providers'
 
 export default function MainLayout({
   children
@@ -11,7 +10,7 @@ export default function MainLayout({
     <div className="flex min-h-dvh w-screen flex-col items-center overflow-x-hidden">
       <Header />
       <main className="flex w-full max-w-7xl flex-1 flex-col items-center px-5">
-        <Provider>{children}</Provider>
+        {children}
       </main>
       <Footer />
     </div>
--- apps/frontend/app/(main)/problem/page.tsx
+++ apps/frontend/app/(main)/problem/page.tsx
@@ -1,39 +1,48 @@
+'use client'
+
 import { Skeleton } from '@/components/ui/skeleton'
 import type { Problem } from '@/types/type'
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
 import { Suspense } from 'react'
 import ProblemInfiniteTable from './_components/ProblemInfiniteTable'
 
 export default function Problem() {
+  const queryClient = new QueryClient()
   return (
     <>
-      <Suspense
-        fallback={
-          <>
-            <div className="mt-4 flex">
-              <span className="w-5/12">
-                <Skeleton className="h-6 w-20" />
-              </span>
-              <span className="w-2/12">
-                <Skeleton className="mx-auto h-6 w-20" />
-              </span>
-              <span className="w-2/12">
-                <Skeleton className="mx-auto h-6 w-20" />
-              </span>
-              <span className="w-2/12">
-                <Skeleton className="mx-auto h-6 w-20" />
-              </span>
-              <span className="w-1/12">
-                <Skeleton className="mx-auto h-6 w-12" />
-              </span>
-            </div>
-            {[...Array(5)].map((_, i) => (
-              <Skeleton key={i} className="my-2 flex h-12 w-full rounded-xl" />
-            ))}
-          </>
-        }
-      >
-        <ProblemInfiniteTable />
-      </Suspense>
+      <QueryClientProvider client={queryClient}>
+        <Suspense
+          fallback={
+            <>
+              <div className="mt-4 flex">
+                <span className="w-5/12">
+                  <Skeleton className="h-6 w-20" />
+                </span>
+                <span className="w-2/12">
+                  <Skeleton className="mx-auto h-6 w-20" />
+                </span>
+                <span className="w-2/12">
+                  <Skeleton className="mx-auto h-6 w-20" />
+                </span>
+                <span className="w-2/12">
+                  <Skeleton className="mx-auto h-6 w-20" />
+                </span>
+                <span className="w-1/12">
+                  <Skeleton className="mx-auto h-6 w-12" />
+                </span>
+              </div>
+              {[...Array(5)].map((_, i) => (
+                <Skeleton
+                  key={i}
+                  className="my-2 flex h-12 w-full rounded-xl"
+                />
+              ))}
+            </>
+          }
+        >
+          <ProblemInfiniteTable />
+        </Suspense>
+      </QueryClientProvider>
     </>
   )
 }
