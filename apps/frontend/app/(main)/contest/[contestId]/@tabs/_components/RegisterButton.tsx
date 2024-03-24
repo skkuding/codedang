07c159dd7ac7299f4013369f717bce26d35244d6
youznn <youznn49@gmail.com>
2024-03-25 02:15:40 +0900

    feat: remove isRegistered state and add refreshing

--- apps/frontend/app/(main)/contest/[contestId]/@tabs/_components/RegisterButton.tsx
+++ apps/frontend/app/(main)/contest/[contestId]/@tabs/_components/RegisterButton.tsx
@@ -1,28 +1,11 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { Skeleton } from '@/components/ui/skeleton'
 import { fetcherWithAuth } from '@/lib/utils'
 import { useRouter } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { toast } from 'sonner'
-
-const clickRegister = async (contestId: string) => {
-  await fetcherWithAuth
-    .post(`contest/${contestId}/participation`, {
-      searchParams: { groupId: 1 }
-    })
-    .then((res) => res.json())
-    .catch((err) => console.log(err))
-}
-
-const clickDeregister = async (contestId: string) => {
-  await fetcherWithAuth
-    .delete(`contest/${contestId}/participation`, {
-      searchParams: { groupId: 1 }
-    })
-    .then((res) => res.json())
-    .catch((err) => console.log(err))
-}
 
 const getFirstProblemId = async (contestId: string) => {
   const { problems }: { problems: { id: string }[] } = await fetcherWithAuth
@@ -42,66 +25,87 @@ export default function RegisterButton({
   registered: boolean
   state: string
 }) {
-  const [isRegistered, setIsRegistered] = useState(registered)
   const [firstProblemId, setFirstProblemId] = useState('')
-  const buttonColor = isRegistered ? 'bg-secondary' : 'bg-primary'
+  const buttonColor = registered ? 'bg-secondary' : 'bg-primary'
   const router = useRouter()
+  const clickRegister = async (contestId: string) => {
+    await fetcherWithAuth
+      .post(`contest/${contestId}/participation`, {
+        searchParams: { groupId: 1 }
+      })
+      .then((res) => {
+        res.json()
+        router.refresh()
+      })
+      .catch((err) => console.log(err))
+  }
+  const clickDeregister = async (contestId: string) => {
+    await fetcherWithAuth
+      .delete(`contest/${contestId}/participation`, {
+        searchParams: { groupId: 1 }
+      })
+      .then((res) => {
+        res.json()
+        router.refresh()
+      })
+      .catch((err) => console.log(err))
+  }
+
   useEffect(() => {
     async function fetchFirstProblemId() {
       const firstId =
-        isRegistered && state === 'Ongoing' ? await getFirstProblemId(id) : ''
+        registered && state === 'Ongoing' ? await getFirstProblemId(id) : ''
       firstId && setFirstProblemId(firstId)
     }
     fetchFirstProblemId()
-  }, [isRegistered])
+  }, [registered])
   return (
     <>
-      {state === 'Upcoming' ? (
-        <Button
-          className={`px-12 py-6 text-lg font-light ${buttonColor} hover:${buttonColor}`}
-          onClick={() => {
-            if (isRegistered) {
-              clickDeregister(id)
-              setIsRegistered(false)
-              toast.success('Deregistered Upcoming test successfully')
-            } else {
-              clickRegister(id)
-              setIsRegistered(true)
-              toast.success('Registered Upcoming test successfully')
-            }
-          }}
-        >
-          {isRegistered ? 'Deregister' : 'Register'}
-        </Button>
-      ) : (
-        <>
-          {!isRegistered ? (
-            <Button
-              className={`px-12 py-6 text-lg font-light ${buttonColor} hover:${buttonColor}`}
-              onClick={() => {
+      <Suspense fallback={<Skeleton className="px-12 py-6"></Skeleton>}>
+        {state === 'Upcoming' ? (
+          <Button
+            className={`px-12 py-6 text-lg font-light ${buttonColor} hover:${buttonColor}`}
+            onClick={() => {
+              if (registered) {
+                clickDeregister(id)
+                toast.success('Deregistered Upcoming test successfully')
+              } else {
                 clickRegister(id)
-                setIsRegistered(true)
-                toast.success('Registered Ongoing test successfully')
-              }}
-            >
-              Register
-            </Button>
-          ) : (
-            <>
-              {firstProblemId && (
-                <Button
-                  className={`px-12 py-6 text-lg font-light ${buttonColor} hover:${buttonColor}`}
-                  onClick={() =>
-                    router.push(`/contest/${id}/problem/${firstProblemId}`)
-                  }
-                >
-                  Go To First Problem!
-                </Button>
-              )}
-            </>
-          )}
-        </>
-      )}
+                toast.success('Registered Upcoming test successfully')
+              }
+            }}
+          >
+            {registered ? 'Deregister' : 'Register'}
+          </Button>
+        ) : (
+          <>
+            {!registered ? (
+              <Button
+                className={`px-12 py-6 text-lg font-light ${buttonColor} hover:${buttonColor}`}
+                onClick={() => {
+                  clickRegister(id)
+                  toast.success('Registered Ongoing test successfully')
+                }}
+              >
+                Register
+              </Button>
+            ) : (
+              <>
+                {firstProblemId && (
+                  <Button
+                    className={`px-12 py-6 text-lg font-light ${buttonColor} hover:${buttonColor}`}
+                    onClick={() =>
+                      router.push(`/contest/${id}/problem/${firstProblemId}`)
+                    }
+                  >
+                    Go To First Problem!
+                  </Button>
+                )}
+              </>
+            )}
+          </>
+        )}
+      </Suspense>
     </>
   )
 }
