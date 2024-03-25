6d9d4d82f7ad11d42aa3458a887cd5bebf0c3956
cho-to <nicole2974@g.skku.edu>
2024-03-25 16:49:08 +0000

    chore(be): remove unused migration files

--- backend/prisma/migrations/20240222083824_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `isVisible` on the `contest` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "contest" DROP COLUMN "isVisible",
-ADD COLUMN     "is_visible" BOOLEAN NOT NULL DEFAULT true;
