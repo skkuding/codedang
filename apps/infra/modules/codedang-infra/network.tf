1ffe9e2dccc7061d066ac5ca32eb4df0147d11ec
cona <rn0731@g.skku.edu>
2024-03-28 20:19:03 +0900

    fix(infra): change incorrect argument in private route table (#1619)

--- apps/infra/modules/codedang-infra/network.tf
+++ apps/infra/modules/codedang-infra/network.tf
@@ -89,8 +89,8 @@ resource "aws_route_table" "private-ecs" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat.id
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
   }
 
   # route {
