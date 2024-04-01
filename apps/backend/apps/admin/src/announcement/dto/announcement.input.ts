826f1291b5289cadb057c7ae27290c8dcb7391b3
Lee-won-hyeok <won2283@naver.com>
2024-04-01 12:51:16 +0000

    docs: update bruno docs

--- apps/backend/apps/admin/src/announcement/dto/announcement.input.ts
+++ apps/backend/apps/admin/src/announcement/dto/announcement.input.ts
@@ -2,7 +2,7 @@ import { InputType, Int, Field } from '@nestjs/graphql'
 
 @InputType()
 export class AnnouncementInput {
-  @Field({ nullable: true })
+  @Field(() => Int, { nullable: true })
   problemId?: number
 
   @Field(() => Int)
