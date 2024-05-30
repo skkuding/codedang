e1dc00273e4122c71ed6cfb4e7a732a1fc8df05b
cona <rn0731@g.skku.edu>
2024-05-30 14:14:33 +0900

    refactor(infra): create new terraform project to prepare refactoring (#1714)
    * refactor(infra): move codedang-tf-backend module to project
    
    * refactor(infra): delete unused terraform project
    
    * chore(infra): prepare refactoring codedang project
    
    * chore(infra): change tfstate file name
    
    * feat(infra): add terraform format in lefthook
    
    * refactor(infra): add projects network and storage
    
    * chore(infra): change dir name from projects to production

---
+++ apps/infra/production/storage/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "codedang-tf-state"
+    key            = "terraform/storage.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
