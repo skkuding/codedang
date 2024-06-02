966a8fe252ca449a615a0bfa947acab74a1ff8d4
k1g99 <idmatthew9546@gmail.com>
2024-06-02 04:52:10 +0000

    refactor: move security_group to storage proj

--- apps/infra/production/storage/redis.tf
+++ apps/infra/production/storage/redis.tf
@@ -7,6 +7,7 @@ resource "aws_subnet" "private_redis1" {
     Name = "Codedang_Redis-Subnet1"
   }
 }
+
 resource "aws_subnet" "private_redis2" {
   vpc_id            = data.aws_vpc.main.id
   cidr_block        = "10.0.32.0/24"
@@ -16,6 +17,33 @@ resource "aws_subnet" "private_redis2" {
     Name = "Codedang_Redis-Subnet2"
   }
 }
+
+resource "aws_security_group" "redis" {
+  name        = "Codedang-SG-Redis"
+  description = "Allow Redis inbound traffic"
+  vpc_id      = data.aws_vpc.main.id
+
+  ingress {
+    description = "Redis"
+    from_port   = var.redis_port
+    to_port     = var.redis_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "Codedang-SG-Redis"
+  }
+}
+
 resource "aws_elasticache_cluster" "db_cache" {
   cluster_id               = "elasticache-redis-codedang-1"
   engine                   = "redis"
@@ -37,6 +65,7 @@ resource "aws_elasticache_cluster" "db_cache" {
     log_type         = "slow-log"
   }
 }
+
 resource "aws_elasticache_subnet_group" "redis_subnet_group" {
   name       = "Redis-subnet-group"
   subnet_ids = [aws_subnet.private_redis1.id, aws_subnet.private_redis2.id]
