a4a47544ef93be660371722f9ecdd6f9d7e6d6f8
goo314 <rn0731@g.skku.edu>
2024-06-03 18:30:34 +0000

    chore(infra): set provider region as string

--- apps/infra/production/codedang/main.tf
+++ apps/infra/production/codedang/main.tf
@@ -16,5 +16,5 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region = "ap-northeast-2"
 }
--- apps/infra/production/codedang/variables.tf
+++ apps/infra/production/codedang/variables.tf
@@ -1,5 +1,1 @@
-variable "region" {
-  type        = string
-  description = "The region for provider"
-  default     = "ap-northeast-2"
-}
+
