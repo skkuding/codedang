7788eeb4fae3e01253426b074731cacdeb55077f
k1g99 <idmatthew9546@gmail.com>
2024-07-03 13:54:04 +0000

    refactor: add iam user for s3 buckets

---
+++ apps/infra/production/storage/s3_testcase_iam_user.tf
@@ -0,0 +1,21 @@
+# TODO: do not create IAM user, use EC2 instance profile instead
+resource "aws_iam_user" "testcase" {
+  name = "user-codedang-testcase"
+}
+
+data "aws_iam_policy_document" "testcase_get_put_delete_object" {
+  statement {
+    actions   = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"]
+    resources = ["${aws_s3_bucket.testcase.arn}/*"]
+  }
+}
+
+resource "aws_iam_user_policy" "testcase" {
+  name   = "codedang-testcase-s3"
+  user   = aws_iam_user.testcase.name
+  policy = data.aws_iam_policy_document.testcase_get_put_delete_object.json
+}
+
+resource "aws_iam_access_key" "testcase" {
+  user = aws_iam_user.testcase.name
+}
