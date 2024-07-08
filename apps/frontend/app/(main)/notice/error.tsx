2a2772fb1d5796ac7cf7bbd0aac6c8e394a1f056
jwoojin9 <jwoojin9@naver.com>
2024-07-08 08:37:49 +0000

    fix(fe): change notice error handling

---
+++ apps/frontend/app/(main)/notice/error.tsx
@@ -0,0 +1,36 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import ErrorImg from '@/public/error.webp'
+import { captureException } from '@sentry/nextjs'
+import Image from 'next/image'
+import { useEffect } from 'react'
+
+interface Props {
+  error: Error & { digest?: string }
+  reset: () => void
+}
+
+export default function Error({ error }: Props) {
+  useEffect(() => {
+    captureException(error)
+  }, [error])
+
+  return (
+    <div className="flex h-full w-full flex-1 flex-col items-center justify-center gap-3 py-12">
+      <p className="mt-8 text-2xl font-extrabold">Failed to Load Notice!</p>
+      <p className="mb-4 max-w-[36rem] text-lg font-semibold">
+        {error.message || 'Unknown Error'}
+      </p>
+      <Button variant="outline" onClick={() => window.location.reload()}>
+        Reload
+      </Button>
+      <Image
+        src={ErrorImg}
+        alt="Unexpected Error"
+        height={240}
+        className="mt-8"
+      />
+    </div>
+  )
+}
