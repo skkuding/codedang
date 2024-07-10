d45c7ff8347f82e2333b889dcf04d92f0125776b
jiho <jihopark09@naver.com>
2024-07-10 23:35:22 +0900

    fix(fe): other approach to change node env

--- apps/frontend/middleware.ts
+++ apps/frontend/middleware.ts
@@ -3,7 +3,6 @@ import { encode, getToken } from 'next-auth/jwt'
 import { parseCookie } from 'next/dist/compiled/@edge-runtime/cookies'
 import { NextResponse, type NextRequest } from 'next/server'
 import { baseUrl } from './lib/constants'
-import { isDevelopmentEnv } from './lib/utils'
 
 const getAuthToken = (res: Response) => {
   const Authorization = res.headers.get('authorization') as string
@@ -66,7 +65,7 @@ export const middleware = async (req: NextRequest) => {
       res.cookies.set(sessionCookieName, newToken, {
         // for client setCookie
         maxAge: 24 * 60 * 60,
-        secure: !isDevelopmentEnv(),
+        secure: process.env.NODE_ENV !== 'development',
         httpOnly: true,
         sameSite: 'lax'
       })
