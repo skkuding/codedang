aad42e8204dfe21dd6bc7ba48652221dfcf3f6ba
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2024-07-15 05:21:10 +0000

    chore(be): add default value on enable-copy-paste field

--- apps/backend/prisma/migrations/20240704112531_add_contestproblem_enable_copy_paste_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `enable_copy_paste` to the `contest_problem` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "contest_problem" ADD COLUMN     "enable_copy_paste" BOOLEAN NOT NULL;
--- apps/backend/prisma/migrations/20240704120342_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `enable_copy_paste` on the `contest_problem` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "contest_problem" DROP COLUMN "enable_copy_paste";
