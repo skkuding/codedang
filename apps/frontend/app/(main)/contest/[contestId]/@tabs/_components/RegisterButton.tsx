8fceaefcc3a071f1d97117ab28da2d13a6d2c8b7
b0xercat <seojin3154@naver.com>
2024-07-21 17:07:23 +0900

    feat(fe): add register code dialog, split register logic based on invitationCodeExists

--- apps/frontend/app/(main)/contest/[contestId]/@tabs/_components/RegisterButton.tsx
+++ apps/frontend/app/(main)/contest/[contestId]/@tabs/_components/RegisterButton.tsx
@@ -1,10 +1,29 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { fetcherWithAuth } from '@/lib/utils'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger
+} from '@/components/ui/dialog'
+import { Input } from '@/components/ui/input'
+import { cn, fetcherWithAuth } from '@/lib/utils'
+import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
+import { z } from 'zod'
+
+interface RegisterCodeInput {
+  invitationCode: string
+}
+
+const schema = z.object({
+  invitationCode: z.string().length(6)
+})
 
 const getFirstProblemId = async (contestId: string) => {
   const { problems }: { problems: { id: string }[] } = await fetcherWithAuth
@@ -18,90 +37,168 @@ const getFirstProblemId = async (contestId: string) => {
 export default function RegisterButton({
   id,
   registered,
-  state
+  state,
+  title,
+  invitationCodeExists
 }: {
   id: string
   registered: boolean
   state: string
+  title: string
+  invitationCodeExists: boolean
 }) {
   const [firstProblemId, setFirstProblemId] = useState('')
-  const buttonColor = registered ? 'bg-secondary' : 'bg-primary'
+  // const buttonColor = registered ? 'bg-secondary' : 'bg-primary'
   const router = useRouter()
   const clickRegister = async (contestId: string) => {
     await fetcherWithAuth
       .post(`contest/${contestId}/participation`, {
-        searchParams: { groupId: 1 }
+        searchParams: invitationCodeExists
+          ? {
+              groupId: 1,
+              invitationCode: getValues('invitationCode')
+            }
+          : { groupId: 1 }
       })
       .then((res) => {
-        res.json()
+        if (!res.ok) {
+          toast.error(
+            invitationCodeExists ? 'Invalid code' : 'Failed to register'
+          )
+        } else {
+          toast.success(`Registered ${state} test successfully`)
+        }
         router.refresh()
       })
       .catch((err) => console.log(err))
   }
-  const clickDeregister = async (contestId: string) => {
-    await fetcherWithAuth
-      .delete(`contest/${contestId}/participation`, {
-        searchParams: { groupId: 1 }
-      })
-      .then((res) => {
-        res.json()
-        router.refresh()
-      })
-      .catch((err) => console.log(err))
-  }
+  // const clickDeregister = async (contestId: string) => {
+  //   await fetcherWithAuth
+  //     .delete(`contest/${contestId}/participation`, {
+  //       searchParams: { groupId: 1 }
+  //     })
+  //     .then((res) => {
+  //       res.json()
+  //       router.refresh()
+  //     })
+  //     .catch((err) => console.log(err))
+  // }
 
   useEffect(() => {
     async function fetchFirstProblemId() {
-      const firstId =
-        registered && state === 'Ongoing' ? await getFirstProblemId(id) : ''
-      firstId && setFirstProblemId(firstId)
+      if (registered && state === 'Ongoing') {
+        const firstId = await getFirstProblemId(id)
+        setFirstProblemId(firstId ?? '')
+      } else {
+        setFirstProblemId('')
+      }
     }
     fetchFirstProblemId()
-  }, [registered])
+  }, [registered, state, id])
+
+  const {
+    handleSubmit,
+    register,
+    trigger,
+    getValues,
+    formState: { errors }
+  } = useForm<RegisterCodeInput>({
+    resolver: zodResolver(schema)
+  })
+
+  const onSubmit = async () => {
+    clickRegister(id)
+  }
+
   return (
     <>
-      {state === 'Upcoming' ? (
-        <Button
-          className={`px-12 py-6 text-lg font-light ${buttonColor} hover:${buttonColor}`}
-          onClick={() => {
-            if (registered) {
-              clickDeregister(id)
-              toast.success('Deregistered Upcoming test successfully')
-            } else {
-              clickRegister(id)
-              toast.success('Registered Upcoming test successfully')
-            }
-          }}
-        >
-          {registered ? 'Deregister' : 'Register'}
-        </Button>
-      ) : (
+      {registered ? (
+        // Case 1) User registered for the contest
         <>
-          {!registered ? (
+          {/* {state === 'Upcoming' ? (
             <Button
               className={`px-12 py-6 text-lg font-light ${buttonColor} hover:${buttonColor}`}
               onClick={() => {
-                clickRegister(id)
-                toast.success('Registered Ongoing test successfully')
+                clickDeregister(id)
+                toast.success('Deregistered Upcoming test successfully')
               }}
             >
-              Register
+              Deregister
             </Button>
           ) : (
-            <>
-              {firstProblemId && (
-                <Button
-                  className={`px-12 py-6 text-lg font-light ${buttonColor} hover:${buttonColor}`}
-                  onClick={() =>
-                    router.push(`/contest/${id}/problem/${firstProblemId}`)
-                  }
-                >
-                  Go To First Problem!
-                </Button>
-              )}
-            </>
+            <> */}
+          {firstProblemId && (
+            <Button
+              className="px-12 py-6 text-lg font-light"
+              onClick={() =>
+                router.push(`/contest/${id}/problem/${firstProblemId}`)
+              }
+            >
+              Go To First Problem!
+            </Button>
           )}
+          {/* </>
+          )} */}
         </>
+      ) : !invitationCodeExists ? (
+        // Case 2) User not registered and no invitation code required
+        <Button
+          className="px-12 py-6 text-lg disabled:bg-gray-300 disabled:text-gray-600"
+          disabled={state === 'Upcoming'}
+          onClick={() => {
+            clickRegister(id)
+          }}
+        >
+          Register Now
+        </Button>
+      ) : (
+        // Case 3) User not registered and invitation code required
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              className="px-12 py-6 text-lg disabled:bg-gray-300 disabled:text-gray-600"
+              disabled={state === 'Upcoming'}
+            >
+              Register Now
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="flex w-[416px] flex-col gap-6 p-10">
+            <DialogHeader>
+              <DialogTitle className="line-clamp-2 text-xl">
+                {title}
+              </DialogTitle>
+            </DialogHeader>
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="flex flex-col gap-6"
+            >
+              <div className="flex flex-col gap-1">
+                <Input
+                  placeholder="Register Code"
+                  {...register('invitationCode', {
+                    onChange: () => trigger('invitationCode')
+                  })}
+                  type="number"
+                  className={cn(
+                    'h-12 w-full [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none',
+                    errors.invitationCode &&
+                      'border-red-500 focus-visible:ring-red-500'
+                  )}
+                />
+                {errors.invitationCode && (
+                  <p className="text-xs text-red-500">
+                    Register Code must be a 6-digit number
+                  </p>
+                )}
+              </div>
+              <div className="flex justify-center">
+                <Button type="submit" className="w-24">
+                  Register
+                </Button>
+              </div>
+            </form>
+          </DialogContent>
+        </Dialog>
       )}
     </>
   )
