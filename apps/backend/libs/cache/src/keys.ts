304b6ffe9b291bc0f7a508f6db0d1337347a62c3
dayongkr <dayongkr@gmail.com>
2024-07-22 21:46:16 +0900

    chore(ci): add change for test

--- apps/backend/libs/cache/src/keys.ts
+++ apps/backend/libs/cache/src/keys.ts
@@ -8,3 +8,8 @@ export const joinGroupCacheKey = (groupId: number) => `group:${groupId}`
 
 export const invitationCodeKey = (code: string) => `invite:${code}`
 export const invitationGroupKey = (groupId: number) => `invite:to:${groupId}`
+const userCacheKey = function (userId: number) {
+  return `user:${userId}`
+}
+
+const hello = "world"
