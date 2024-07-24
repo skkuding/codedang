b51dfcb29c3d7d39014aefeccb18ff2d5c3ad89d
b0xercat <seojin3154@naver.com>
2024-07-24 16:01:20 +0900

    feat(fe): add contest detail in contest overall page

--- apps/frontend/app/admin/contest/[id]/page.tsx
+++ apps/frontend/app/admin/contest/[id]/page.tsx
@@ -2,11 +2,36 @@
 
 import { Button } from '@/components/ui/button'
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area'
+import { GET_CONTEST } from '@/graphql/contest/queries'
+import { renderKatex } from '@/lib/renderKatex'
+import { dateFormatter } from '@/lib/utils'
+import Period from '@/public/period.svg'
+import { useQuery } from '@apollo/client'
+import Image from 'next/image'
 import Link from 'next/link'
+import { useEffect, useRef } from 'react'
 import { FaAngleLeft, FaPencil } from 'react-icons/fa6'
 
 export default function Page({ params }: { params: { id: string } }) {
   const { id } = params
+
+  const contestData = useQuery(GET_CONTEST, {
+    variables: {
+      contestId: Number(id)
+    }
+  }).data?.getContest
+
+  const katexRef = useRef<HTMLDivElement>(null)!
+  useEffect(() => {
+    renderKatex(contestData?.description, katexRef)
+  }, [contestData?.description, katexRef])
+
+  const katexContent = (
+    <div
+      className="prose mb-12 w-full max-w-full border-y-2 border-y-gray-300 p-5 py-12"
+      ref={katexRef}
+    />
+  )
 
   return (
     <ScrollArea className="shrink-0">
@@ -16,7 +41,7 @@ export default function Page({ params }: { params: { id: string } }) {
             <Link href="/admin/contest">
               <FaAngleLeft className="h-12 hover:text-gray-700/80" />
             </Link>
-            <span className="text-4xl font-bold">{id}</span>
+            <span className="text-4xl font-bold">{contestData?.title}</span>
           </div>
           <Link href={`/admin/contest/${id}/edit`}>
             <Button variant="default">
@@ -25,6 +50,19 @@ export default function Page({ params }: { params: { id: string } }) {
             </Button>
           </Link>
         </div>
+        <div className="flex justify-between">
+          <p className="text-primary font-bold">
+            Invitation code: {contestData?.invitationCode}
+          </p>
+          <div className="flex items-center gap-2">
+            <Image src={Period} alt="period" width={22} />
+            <p className="font-semibold">
+              {dateFormatter(contestData?.startTime, 'YY-MM-DD HH:mm')} ~{' '}
+              {dateFormatter(contestData?.endTime, 'YY-MM-DD HH:mm')}
+            </p>
+          </div>
+        </div>
+        {katexContent}
       </main>
       <ScrollBar orientation="horizontal" />
     </ScrollArea>
