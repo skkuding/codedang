73adc58121c099bf9581a7338e57ae9cf5bf823c
Jimin Ha <jimin9038@g.skku.edu>
2024-07-26 07:34:16 +0000

    refactor(fe): simplify sentry init logic

--- apps/frontend/instrumentation.ts
+++ apps/frontend/instrumentation.ts
@@ -1,18 +1,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 export const register = () => {
-  if (process.env.NEXT_RUNTIME === 'nodejs') {
-    Sentry.init({
-      dsn: 'https://10bd959e91328cfb75c274af2faa5fac@sentry.codedang.com/10',
-
-      // Adjust this value in production, or use tracesSampler for greater control
-      tracesSampleRate: 1,
-
-      // Setting this option to true will print useful information to the console while you're setting up Sentry.
-      debug: false
-    })
-  }
-  if (process.env.NEXT_RUNTIME === 'edge') {
+  if (['nodejs', 'edge'].includes(process.env.NEXT_RUNTIME ?? '')) {
     Sentry.init({
       dsn: 'https://10bd959e91328cfb75c274af2faa5fac@sentry.codedang.com/10',
 
