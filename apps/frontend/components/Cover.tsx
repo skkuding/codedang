4ebb3cb3559b845ab42121969009f26915913d82
youznn <leejin914418@gmail.com>
2024-08-02 23:56:36 +0900

    feat(fe): redesign banner

--- apps/frontend/components/Cover.tsx
+++ apps/frontend/components/Cover.tsx
@@ -3,24 +3,30 @@ import { cn } from '@/lib/utils'
 interface CoverProps {
   title: string
   description: string
-  bgColor: string
 }
+
+const bgImage: { [key: string]: string } = {
+  problem: 'bg-[url(/problembanner.svg)]',
+  notice: 'bg-[url(/noticebanner.svg)]',
+  contest: 'bg-[url(/contestbanner.svg)]'
+}
+
 /**
  * @param title - title text
  * @param description - description text
  * @param bgColor - tailwindcss color class (e.g. "bg-gray-500")
  */
-export default function Cover({ title, description, bgColor }: CoverProps) {
+export default function Cover({ title, description }: CoverProps) {
   return (
     <div
       className={cn(
-        bgColor,
-        'relative flex h-[200px] w-screen items-center justify-center overflow-hidden'
+        bgImage[title.toLowerCase()],
+        'relative bottom-[55px] z-[-10] mb-[-55px] flex h-[285px] w-screen items-center justify-center bg-cover bg-center bg-no-repeat'
       )}
     >
-      <div className="relative z-10 flex w-full items-start justify-center">
-        <h2 className="text-4xl font-bold text-white">{title}</h2>
-        <p className="absolute mt-12 w-fit whitespace-nowrap text-base text-gray-50">
+      <div className="w-full flex-col justify-center text-center">
+        <h2 className="font-mono text-[56px] font-bold text-white">{title}</h2>
+        <p className="whitespace-nowrap text-base text-white/80">
           {description}
         </p>
       </div>
