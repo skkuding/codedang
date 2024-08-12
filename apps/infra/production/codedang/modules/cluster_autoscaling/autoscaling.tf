8d3e01e87cad299fec1d7b192839cba770e6bdd1
ssupecial <kanges802@g.skku.edu>
2024-08-12 03:32:07 +0000

    feat(infra): add managed scaling option to ecs capacity provider

--- apps/infra/production/codedang/modules/cluster_autoscaling/autoscaling.tf
+++ apps/infra/production/codedang/modules/cluster_autoscaling/autoscaling.tf
@@ -66,5 +66,12 @@ resource "aws_ecs_capacity_provider" "this" {
   auto_scaling_group_provider {
     auto_scaling_group_arn         = aws_autoscaling_group.this.arn
     managed_termination_protection = "ENABLED"
+
+    managed_scaling {
+      maximum_scaling_step_size = 5
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 90
+    }
   }
 }
