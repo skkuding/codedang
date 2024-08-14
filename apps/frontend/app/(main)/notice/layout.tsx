17b7a7d22831327879f25ca0f195d4a893e82057
b0xercat <seojin3154@naver.com>
2024-08-14 15:22:38 +0900

    chore: increase max width of notice contest problem contents

--- apps/frontend/app/(main)/notice/layout.tsx
+++ apps/frontend/app/(main)/notice/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         title="NOTICE"
         description="Events and announcements of CODEDANG"
       />
-      <div className="flex w-full max-w-5xl flex-col gap-5 p-5 py-8">
+      <div className="flex w-full max-w-7xl flex-col gap-5 p-5 py-8">
         {children}
       </div>
     </>
--- apps/frontend/app/(main)/problem/layout.tsx
+++ apps/frontend/app/(main)/problem/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <Cover title="PROBLEM" description="All the problems of CODEDANG" />
-      <div className="flex w-full max-w-5xl flex-col gap-5 p-5 py-8">
+      <div className="flex w-full max-w-7xl flex-col gap-5 p-5 py-8">
         {children}
       </div>
     </>
