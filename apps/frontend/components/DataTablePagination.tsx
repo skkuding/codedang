b446cd93b0ef166e3f09d0c3bcaf8e6cf3bc3a1f
jwoojin9 <jwoojin9@naver.com>
2024-08-17 17:08:36 +0000

    chore(fe): reduce rows per page size

--- apps/frontend/components/DataTablePagination.tsx
+++ apps/frontend/components/DataTablePagination.tsx
@@ -87,7 +87,7 @@ export function DataTablePagination<TData>({
               table.setPageSize(Number(value))
             }}
           >
-            <SelectTrigger className="h-8 w-[70px]">
+            <SelectTrigger className="h-6 w-14 bg-white text-xs">
               <SelectValue placeholder={table.getState().pagination.pageSize} />
             </SelectTrigger>
             <SelectContent side="top" className="bg-white">
