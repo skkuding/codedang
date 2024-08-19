46525d89875dffa174aa9aa17c6d42656f8d6fca
Jaehyeon Kim <kjhkk1020@naver.com>
2024-08-19 23:45:39 +0900

    feat(be): add admin submission module (#1924)
    * feat(be): add admin submission module
    
    * feat(be): use user-ip
    
    * chore(be): handle exceptions
    
    * fix(be): add type
    
    * fix(be): typo
    
    * fix(be): rename into getcontestsubmissions
    
    ---------
    
    Co-authored-by: Jimin Ha <jimin9038@g.skku.edu>

---
+++ apps/backend/apps/admin/src/submission/submission.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '@libs/prisma'
+import type { Language, ResultStatus } from '@admin/@generated'
+import type { GetContestSubmissionsInput } from './model/get-contest-submission.input'
+
+@Injectable()
+export class SubmissionService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getContestSubmissions(
+    input: GetContestSubmissionsInput,
+    take: number,
+    cursor: number | null
+  ) {
+    const paginator = this.prisma.getPaginator(cursor)
+
+    const { contestId, problemId } = input
+    const contestSubmissions = await this.prisma.submission.findMany({
+      ...paginator,
+      take,
+      where: {
+        contestId,
+        problemId
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            username: true,
+            studentId: true,
+            userProfile: {
+              select: {
+                realName: true
+              }
+            }
+          }
+        },
+        problem: {
+          select: {
+            title: true
+          }
+        }
+      }
+    })
+
+    const results = contestSubmissions.map((c) => {
+      return {
+        title: c.problem.title,
+        studentId: c.user?.studentId ?? 'Unknown',
+        realname: c.user?.userProfile?.realName ?? 'Unknown',
+        username: c.user?.username ?? 'Unknown',
+        result: c.result as ResultStatus,
+        language: c.language as Language,
+        submissionTime: c.createTime,
+        codeSize: c.codeSize ?? null,
+        ip: c.userIp ?? 'Unknown'
+      }
+    })
+
+    return results
+  }
+}
