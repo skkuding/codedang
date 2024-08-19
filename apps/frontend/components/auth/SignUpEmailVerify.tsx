4418ec4300bd48ab5b0d6e8293f3b5521f69c19b
jwoojin9 <jwoojin9@naver.com>
2024-08-19 19:49:25 +0000

    chore(fe): change signup fields ring to border

--- apps/frontend/components/auth/SignUpEmailVerify.tsx
+++ apps/frontend/components/auth/SignUpEmailVerify.tsx
@@ -159,14 +159,14 @@ export default function SignUpEmailVerify() {
             id="email"
             type="email"
             className={cn(
-              'focus-visible:ring-primary w-full focus-visible:ring-1',
+              'focus-visible:border-primary w-full focus-visible:ring-0',
               (emailError || errors.email) &&
-                'ring-1 ring-red-500 focus-visible:ring-red-500'
+                'border-red-500 focus-visible:border-red-500'
             )}
             placeholder="example@g.skku.edu"
             {...register('email')}
-            onChange={() => clearErrors('email')}
-            onKeyDown={(e) => {
+            onFocus={() => clearErrors('email')}
+            onKeyDown={async (e) => {
               if (e.key === 'Enter' && !sendButtonDisabled) {
                 e.preventDefault()
                 setSendButtonDisabled(true)
@@ -194,9 +194,9 @@ export default function SignUpEmailVerify() {
             type="number"
             className={cn(
               'mt-2 [appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none',
-              'focus-visible:ring-primary w-full focus-visible:ring-1',
+              'focus-visible:border-primary w-full focus-visible:ring-0',
               (errors.verificationCode || expired || codeError) &&
-                'ring-1 ring-red-500 focus-visible:ring-red-500'
+                'border-red-500 focus-visible:border-red-500'
             )}
             placeholder="Verification Code"
             {...register('verificationCode', {
