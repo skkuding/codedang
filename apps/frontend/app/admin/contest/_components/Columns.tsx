d8dce2c24469f5dff4f769010c519217c76347ff
Jimin Ha <jimin9038@g.skku.edu>
2024-08-19 04:45:19 +0000

    fix(fe): checkbox stop propagation

--- apps/frontend/app/admin/contest/_components/Columns.tsx
+++ apps/frontend/app/admin/contest/_components/Columns.tsx
@@ -127,6 +127,7 @@ export const columns: ColumnDef<DataTableContest>[] = [
     ),
     cell: ({ row }) => (
       <Checkbox
+        onClick={(e) => e.stopPropagation()}
         checked={row.getIsSelected()}
         onCheckedChange={(value) => row.toggleSelected(!!value)}
         aria-label="Select row"
