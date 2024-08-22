7e9d9c281318caef665fdd9fbd33c5122f04fa8b
Jimin Ha <jimin9038@g.skku.edu>
2024-08-22 10:41:23 +0000

    chore(fe): make safeFetcher that throws http error

--- apps/frontend/lib/utils.ts
+++ apps/frontend/lib/utils.ts
@@ -14,6 +14,7 @@ export const fetcher = ky.create({
   prefixUrl: baseUrl,
   retry: 0,
   timeout: 5000,
+  throwHttpErrors: false,
   hooks: {
     beforeError: [
       (error) => {
@@ -27,6 +28,51 @@ export const fetcher = ky.create({
 })
 
 export const fetcherWithAuth = fetcher.extend({
+  hooks: {
+    beforeRequest: [
+      async (request) => {
+        // Add access token to request header if user is logged in.
+        const session = await auth()
+        if (session)
+          request.headers.set('Authorization', session.token.accessToken)
+      }
+    ],
+    afterResponse: [
+      // Retry option is not working, so we use this workaround.
+      async (request, options, response) => {
+        if (response.status === 401) {
+          const session = await auth()
+          if (session) {
+            request.headers.set('Authorization', session.token.accessToken)
+            fetcher(request, {
+              ...options,
+              hooks: {} // Remove hooks to prevent infinite loop.
+            })
+          }
+        }
+      }
+    ]
+  }
+})
+
+// difference with fetcher: "throws" http error (must handle error when using)
+export const safeFetcher = ky.create({
+  prefixUrl: baseUrl,
+  retry: 0,
+  timeout: 5000,
+  hooks: {
+    beforeError: [
+      (error) => {
+        if (error instanceof TimeoutError) {
+          toast.error('Request timed out. Please try again later.')
+        }
+        return error
+      }
+    ]
+  }
+})
+
+export const safeFetcherWithAuth = safeFetcher.extend({
   hooks: {
     beforeRequest: [
       async (request) => {
