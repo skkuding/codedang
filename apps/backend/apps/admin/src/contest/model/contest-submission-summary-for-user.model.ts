c10bcb3bc5a75499a78625ba92618188298e0e89
Jimin Ha <jimin9038@g.skku.edu>
2024-08-27 13:52:51 +0000

    fix(be): fix ContestSubmissionSummaryForOne model

--- apps/backend/apps/admin/src/contest/model/contest-submission-summary-for-user.model.ts
+++ apps/backend/apps/admin/src/contest/model/contest-submission-summary-for-user.model.ts
@@ -16,25 +16,25 @@ export class ContestSubmissionSummaryForUser {
  */
 @ObjectType({ description: 'ContestSubmissionSummaryForOne' })
 export class ContestSubmissionSummaryForOne {
-  @Field(() => Int, { nullable: true })
+  @Field(() => Int, { nullable: false })
   contestId: number
 
-  @Field(() => String, { nullable: true })
+  @Field(() => String, { nullable: false })
   problemTitle: string
 
-  @Field(() => String, { nullable: true })
+  @Field(() => String, { nullable: false })
   username: string
 
-  @Field(() => String, { nullable: true })
+  @Field(() => String, { nullable: false })
   studentId: string
 
-  @Field(() => ResultStatus, { nullable: true })
+  @Field(() => ResultStatus, { nullable: false })
   submissionResult: ResultStatus // Accepted, RuntimeError, ...
 
-  @Field(() => Language, { nullable: true })
+  @Field(() => Language, { nullable: false })
   language: Language
 
-  @Field(() => String, { nullable: true })
+  @Field(() => String, { nullable: false })
   submissionTime: Date
 
   @Field(() => Int, { nullable: true })
