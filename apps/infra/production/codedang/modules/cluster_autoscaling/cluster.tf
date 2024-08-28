b3572fdc82a109e0ec4008015a4571af37ad7228
ssupecial <kanges802@g.skku.edu>
2024-08-28 00:04:50 +0000

    feat(infra): add capacity provider strategy to ecs cluster

--- apps/infra/production/codedang/modules/cluster_autoscaling/cluster.tf
+++ apps/infra/production/codedang/modules/cluster_autoscaling/cluster.tf
@@ -5,4 +5,10 @@ resource "aws_ecs_cluster" "this" {
 resource "aws_ecs_cluster_capacity_providers" "this" {
   cluster_name       = aws_ecs_cluster.this.name
   capacity_providers = [aws_ecs_capacity_provider.this.name]
+
+  default_capacity_provider_strategy {
+    capacity_provider = aws_ecs_capacity_provider.this.name
+    weight            = 100
+    base              = 1
+  }
 }
