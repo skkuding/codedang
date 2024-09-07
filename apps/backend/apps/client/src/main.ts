6049db7e316fedce1d3f3eaad04d4beb1ed945bc
jspark2000 <idpjs2000@gmail.com>
2024-09-07 15:31:29 +0000

    fix(be): apply swc on nest

--- apps/backend/apps/client/src/main.ts
+++ apps/backend/apps/client/src/main.ts
@@ -1,7 +1,7 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import * as cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser'
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino'
 import { AppModule } from './app.module'
 import startMetricsExporter from './metric'
