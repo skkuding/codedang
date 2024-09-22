6f803827730cf92de26d4529b0d4e4cf0dd4d906
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2024-09-22 12:55:35 +0000

    feat(be): handle prisma exception in global exception filter

--- apps/backend/libs/exception/src/filter.ts
+++ apps/backend/libs/exception/src/filter.ts
@@ -3,10 +3,15 @@ import {
   type ArgumentsHost,
   Logger,
   HttpException,
-  InternalServerErrorException
+  InternalServerErrorException,
+  ConflictException,
+  NotFoundException,
+  BadRequestException,
+  UnprocessableEntityException
 } from '@nestjs/common'
 import { BaseExceptionFilter } from '@nestjs/core'
 import type { GqlExceptionFilter } from '@nestjs/graphql'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { BusinessException } from './business.exception'
 
 @Catch()
@@ -17,6 +22,9 @@ export class AdminExceptionFilter implements GqlExceptionFilter {
     if (exception instanceof BusinessException) {
       this.logger.log(exception)
       return exception.convert2HTTPException()
+    } else if (exception instanceof PrismaClientKnownRequestError) {
+      this.logger.log(exception)
+      return convertPrismaError2HttpException(exception)
     } else if (exception instanceof HttpException) {
       this.logger.log(exception)
       return exception
@@ -34,6 +42,9 @@ export class ClientExceptionFilter extends BaseExceptionFilter {
     if (exception instanceof BusinessException) {
       this.logger.log(exception)
       super.catch(exception.convert2HTTPException(), host)
+    } else if (exception instanceof PrismaClientKnownRequestError) {
+      this.logger.log(exception)
+      super.catch(convertPrismaError2HttpException(exception), host)
     } else if (exception instanceof HttpException) {
       this.logger.log(exception)
       super.catch(exception, host)
@@ -41,5 +52,20 @@ export class ClientExceptionFilter extends BaseExceptionFilter {
       this.logger.error(exception)
       super.catch(new InternalServerErrorException(), host)
     }
+  }
+}
+
+const convertPrismaError2HttpException = (
+  exception: PrismaClientKnownRequestError
+) => {
+  switch (exception.code) {
+    case 'P2002':
+      return new ConflictException(exception.message)
+    case 'P2025':
+      return new NotFoundException(exception.message)
+    case 'P2003':
+      return new UnprocessableEntityException(exception.message)
+    default:
+      return new BadRequestException(exception.message)
   }
 }
