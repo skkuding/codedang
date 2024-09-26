8f488b24c2c3e018df46a6db2936fe268e424d87
Jimin Ha <jimin9038@g.skku.edu>
2024-09-26 07:58:29 +0000

    fix: wefwef

--- apps/frontend/app/(main)/problem/layout.tsx
+++ apps/frontend/app/(main)/problem/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <>
       <Cover title="PROBLEM" description="All the problems of CODEDANG" />
       <div className="flex w-full max-w-7xl flex-col gap-5 p-5 py-8">
-        e{children}de
+        e{children}def
       </div>
     </>
   )
