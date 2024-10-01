05060033074808a92852ac8a77c7f8326aae8163
b0xercat <seojin3154@naver.com>
2024-10-02 00:23:54 +0900

    refactor(fe): componentize TestcaseTab TestSummary TestResultDetail

--- apps/frontend/components/TestcasePanel.tsx
+++ apps/frontend/components/TestcasePanel.tsx
@@ -28,16 +28,6 @@ export default function TestcasePanel({ testResult }: TestcasePanelProps) {
     .filter((index) => index !== -1)
   const tabLength = testcaseTabList.length
 
-  function handleDelete(testcase: TestcaseResult) {
-    const updatedList = testcaseTabList.filter(
-      (item) => item.id !== testcase.id
-    )
-    setTestcaseTabList(updatedList)
-    if (currentTab === testcase.id) {
-      setCurrentTab(0)
-    }
-  }
-
   return dataWithIndex.length !== 0 ? (
     <>
       <div className="flex h-12">
@@ -45,7 +35,9 @@ export default function TestcasePanel({ testResult }: TestcasePanelProps) {
           className={cn(
             'w-44 content-center text-center',
             currentTab === 0 ? 'bg-[#222939]' : 'bg-[#121728]',
-            currentTab === testcaseTabList[0].id && 'rounded-br-xl'
+            tabLength > 0 &&
+              currentTab === testcaseTabList[0].id &&
+              'rounded-br-xl'
           )}
           onClick={() => setCurrentTab(0)}
         >
@@ -53,32 +45,15 @@ export default function TestcasePanel({ testResult }: TestcasePanelProps) {
         </div>
         {testcaseTabList.map((testcase, index) => {
           return (
-            <div
+            <TestcaseTab
+              testcaseTabList={testcaseTabList}
+              setTestcaseTabList={setTestcaseTabList}
+              currentTab={currentTab}
+              setCurrentTab={setCurrentTab}
+              testcase={testcase}
+              index={index}
               key={testcase.id}
-              className={cn(
-                'relative w-44 border-l border-[#222939] bg-[#121728]',
-                currentTab === 0 && index == 0 && 'rounded-bl-xl',
-                currentTab === testcaseTabList[index - 1]?.id &&
-                  'rounded-bl-xl',
-                currentTab === testcaseTabList[index + 1]?.id && 'rounded-br-xl'
-              )}
-            >
-              <IoMdClose
-                className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer text-gray-300"
-                size={18}
-                onClick={() => handleDelete(testcase)}
-              />
-              <div
-                key={testcase.id}
-                className={cn(
-                  'h-full content-center pr-6 text-center',
-                  currentTab === testcase.id && 'bg-[#222939]'
-                )}
-                onClick={() => setCurrentTab(testcase.id)}
-              >
-                {tabLength < 7 ? 'Samples' : 'S'} #{testcase.id}
-              </div>
-            </div>
+            />
           )
         })}
         <div
@@ -93,28 +68,11 @@ export default function TestcasePanel({ testResult }: TestcasePanelProps) {
       <ScrollArea className="h-full">
         {currentTab === 0 ? (
           <div className="flex flex-col gap-6 p-5">
-            <table className="min-w-full">
-              <tbody>
-                <tr>
-                  <td className="py-1 text-slate-400">Correct Testcase</td>
-                  <td className="py-1 text-white">
-                    {acceptedCount}/{total}
-                  </td>
-                </tr>
-                {notAcceptedIndexes.length > 0 && (
-                  <tr>
-                    <td className="min-w-52 py-1 align-top text-slate-400">
-                      Wrong Testcase Number
-                    </td>
-                    <td className="py-1 text-white">
-                      {notAcceptedIndexes
-                        .map((index) => `Sample #${index + 1}`)
-                        .join(', ')}
-                    </td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
+            <TestSummary
+              acceptedCount={acceptedCount}
+              total={total}
+              notAcceptedIndexes={notAcceptedIndexes}
+            />
             <TestcaseTable
               data={dataWithIndex}
               testcaseTabList={testcaseTabList}
@@ -123,36 +81,67 @@ export default function TestcasePanel({ testResult }: TestcasePanelProps) {
             />
           </div>
         ) : (
-          <div className="px-8 pt-5">
-            <div className="flex w-full gap-4 rounded-md bg-[#121728] px-6 py-3 font-light text-neutral-400">
-              Result
-              <span
-                className={cn(
-                  dataWithIndex[currentTab - 1].result === 'Accepted'
-                    ? 'text-green-500'
-                    : dataWithIndex[currentTab - 1].result === 'Judging'
-                      ? 'text-neutral-400'
-                      : 'text-red-500'
-                )}
-              >
-                {dataWithIndex[currentTab - 1].result}
-              </span>
-            </div>
-            <div className="flex gap-4">
-              <LabeledField
-                label="Input"
-                text={dataWithIndex[currentTab - 1].input}
-              />
-              <LabeledField
-                label="Expected Output"
-                text={dataWithIndex[currentTab - 1].output}
-              />
-            </div>
-          </div>
+          <TestResultDetail
+            dataWithIndex={dataWithIndex}
+            currentTab={currentTab}
+          />
         )}
       </ScrollArea>
     </>
   ) : null
+}
+
+function TestcaseTab({
+  testcaseTabList,
+  setTestcaseTabList,
+  currentTab,
+  setCurrentTab,
+  testcase,
+  index
+}: {
+  testcaseTabList: TestcaseResult[]
+  setTestcaseTabList: (data: TestcaseResult[]) => void
+  currentTab: number
+  setCurrentTab: (data: number) => void
+  testcase: TestcaseResult
+  index: number
+}) {
+  function handleDelete(testcase: TestcaseResult) {
+    const updatedList = testcaseTabList.filter(
+      (item) => item.id !== testcase.id
+    )
+    setTestcaseTabList(updatedList)
+    if (currentTab === testcase.id) {
+      setCurrentTab(0)
+    }
+  }
+
+  return (
+    <div
+      className={cn(
+        'relative w-44 border-l border-[#222939] bg-[#121728]',
+        currentTab === 0 && index == 0 && 'rounded-bl-xl',
+        currentTab === testcaseTabList[index - 1]?.id && 'rounded-bl-xl',
+        currentTab === testcaseTabList[index + 1]?.id && 'rounded-br-xl'
+      )}
+    >
+      <IoMdClose
+        className="absolute right-2 top-1/2 -translate-y-1/2 cursor-pointer text-gray-300"
+        size={18}
+        onClick={() => handleDelete(testcase)}
+      />
+      <div
+        key={testcase.id}
+        className={cn(
+          'h-full content-center pr-6 text-center',
+          currentTab === testcase.id && 'bg-[#222939]'
+        )}
+        onClick={() => setCurrentTab(testcase.id)}
+      >
+        {testcaseTabList.length < 7 ? 'Samples' : 'S'} #{testcase.id}
+      </div>
+    </div>
+  )
 }
 
 function LabeledField({ label, text }: { label: string; text: string }) {
@@ -166,6 +155,78 @@ function LabeledField({ label, text }: { label: string; text: string }) {
           __html: sanitize(text)
         }}
       />
+    </div>
+  )
+}
+
+function TestSummary({
+  acceptedCount,
+  total,
+  notAcceptedIndexes
+}: {
+  acceptedCount: number
+  total: number
+  notAcceptedIndexes: number[]
+}) {
+  return (
+    <table className="min-w-full">
+      <tbody>
+        <tr>
+          <td className="py-1 text-slate-400">Correct Testcase</td>
+          <td className="py-1 text-white">
+            {acceptedCount}/{total}
+          </td>
+        </tr>
+        {notAcceptedIndexes.length > 0 && (
+          <tr>
+            <td className="min-w-52 py-1 align-top text-slate-400">
+              Wrong Testcase Number
+            </td>
+            <td className="py-1 text-white">
+              {notAcceptedIndexes
+                .map((index) => `Sample #${index + 1}`)
+                .join(', ')}
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  )
+}
+
+function TestResultDetail({
+  dataWithIndex,
+  currentTab
+}: {
+  dataWithIndex: TestcaseResult[]
+  currentTab: number
+}) {
+  return (
+    <div className="px-8 pt-5">
+      <div className="flex w-full gap-4 rounded-md bg-[#121728] px-6 py-3 font-light text-neutral-400">
+        Result
+        <span
+          className={cn(
+            dataWithIndex[currentTab - 1].result === 'Accepted'
+              ? 'text-green-500'
+              : dataWithIndex[currentTab - 1].result === 'Judging'
+                ? 'text-neutral-400'
+                : 'text-red-500'
+          )}
+        >
+          {dataWithIndex[currentTab - 1].result}
+        </span>
+      </div>
+      <div className="flex gap-4">
+        <LabeledField
+          label="Input"
+          text={dataWithIndex[currentTab - 1].input}
+        />
+        <LabeledField
+          label="Expected Output"
+          text={dataWithIndex[currentTab - 1].output}
+        />
+      </div>
     </div>
   )
 }
