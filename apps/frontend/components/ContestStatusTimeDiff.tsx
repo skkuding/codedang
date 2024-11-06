bd81a8513aa6895e4dcb0ed581d7d5f8c53f897b
Jimin Ha <jimin9038@g.skku.edu>
2024-11-06 04:34:14 +0000

    chore(fe): suppress exhausted deps since we use useinterval

--- apps/frontend/components/ContestStatusTimeDiff.tsx
+++ apps/frontend/components/ContestStatusTimeDiff.tsx
@@ -78,6 +78,7 @@ export default function ContestStatusTimeDiff({
 
   useEffect(() => {
     updateContestStatus()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   useInterval(() => {
