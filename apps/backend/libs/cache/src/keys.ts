f9be17cc7159f990a00b2e426a85e70c253732df
donghun1214 <acekdhh@g.skku.edu>
2024-11-07 13:44:39 +0000

    fix(be): change testKey for race condition

--- apps/backend/libs/cache/src/keys.ts
+++ apps/backend/libs/cache/src/keys.ts
@@ -9,4 +9,6 @@ export const joinGroupCacheKey = (groupId: number) => `group:${groupId}`
 export const invitationCodeKey = (code: string) => `invite:${code}`
 export const invitationGroupKey = (groupId: number) => `invite:to:${groupId}`
 
-export const testKey = (userId: number) => `test:user:${userId}`
+export const testKey = (userId: number, testcaseId: number) =>
+  `test:user:${userId}:testcase:${testcaseId}`
+export const userTestcasesKey = (userId: number) => `test:user:${userId}`
