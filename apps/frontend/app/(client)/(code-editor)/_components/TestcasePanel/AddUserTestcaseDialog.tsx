22e1df493bfc13c0bbf9ec07165bbbf371194700
eunnbi <jenabill@naver.com>
2024-11-16 01:37:38 +0900

    fix(fe): fix user testcases form

--- apps/frontend/app/(client)/(code-editor)/_components/TestcasePanel/AddUserTestcaseDialog.tsx
+++ apps/frontend/app/(client)/(code-editor)/_components/TestcasePanel/AddUserTestcaseDialog.tsx
@@ -10,38 +10,34 @@ import {
 import { ScrollArea, ScrollBar } from '@/components/shadcn/scroll-area'
 import { Textarea } from '@/components/shadcn/textarea'
 import { cn } from '@/lib/utils'
-import type { TestcaseItem } from '@/types/type'
 import { AlertTriangle, X } from 'lucide-react'
-import { useState, type ChangeEvent } from 'react'
-import { useForm } from 'react-hook-form'
+import { useState } from 'react'
 import { CiSquarePlus } from 'react-icons/ci'
 import { FaPlus } from 'react-icons/fa'
 import { FaCircleCheck } from 'react-icons/fa6'
 import { IoIosClose } from 'react-icons/io'
 import CopyButton from '../CopyButton'
 import { useTestcaseStore } from '../context/TestcaseStoreProvider'
-import { useUserTestcase } from './useUserTestcase'
+import { useUserTestcasesForm } from './useUserTestcasesForm'
 
 export default function AddUserTestcaseDialog() {
   const [open, setOpen] = useState(false)
   const sampleTestcases = useTestcaseStore((state) => state.sampleTestcases)
 
   const {
+    formState,
     testcases,
+    onSubmit,
+    register,
     addTestcase,
-    editTestcase,
-    deleteTestcase,
-    saveUserTestcases
-  } = useUserTestcase()
-
-  const onClickSaveButton = () => {
-    saveUserTestcases()
-    setOpen(false)
-  }
+    removeTestcase
+  } = useUserTestcasesForm({
+    onSubmit: () => setOpen(false)
+  })
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger className="my-2 flex h-8 w-[148px] items-center justify-center gap-2 rounded-[5px] bg-slate-600 p-2 text-white">
+      <DialogTrigger className="flex h-8 w-[148px] items-center justify-center gap-2 rounded-[5px] bg-slate-600 p-2 text-white">
         <CiSquarePlus size={24} />
         Add Testcase
       </DialogTrigger>
@@ -56,119 +52,93 @@ export default function AddUserTestcaseDialog() {
         </DialogClose>
         <ScrollArea className="h-[600px] px-14">
           <ScrollBar />
-          <DialogHeader className="mt-[70px]">
-            <DialogTitle className="text-white">Add User Testcase</DialogTitle>
-          </DialogHeader>
+          <form onSubmit={onSubmit}>
+            <DialogHeader className="mt-[70px]">
+              <DialogTitle className="text-white">
+                Add User Testcase
+              </DialogTitle>
+            </DialogHeader>
 
-          <div className="my-8 flex flex-col gap-6">
-            {sampleTestcases.map((testcase, index) => (
-              <div key={testcase.id} className="flex flex-col gap-4">
-                <p className="text-[#C4CACC]">
-                  Sample #{(index + 1).toString().padStart(2, '0')}
-                </p>
-                <SampleTestcaseItem
-                  input={testcase.input}
-                  output={testcase.output}
-                />
-              </div>
-            ))}
+            <div className="my-8 flex flex-col gap-6">
+              {sampleTestcases.map((testcase, index) => (
+                <div key={testcase.id} className="flex flex-col gap-4">
+                  <p className="text-[#C4CACC]">
+                    Sample #{(index + 1).toString().padStart(2, '0')}
+                  </p>
+                  <SampleTestcaseItem
+                    input={testcase.input}
+                    output={testcase.output}
+                  />
+                </div>
+              ))}
 
-            {testcases.map((testcase, index) => (
-              <div key={testcase.id} className="flex flex-col gap-4">
-                <p className="text-primary-light">
-                  User Testcase #{(index + 1).toString().padStart(2, '0')}
-                </p>
-                <UserTestcaseItem
-                  id={testcase.id}
-                  input={testcase.input}
-                  output={testcase.output}
-                  editTestcase={editTestcase}
-                  deleteTestcase={deleteTestcase}
-                />
-              </div>
-            ))}
-
-            <div className="flex flex-col gap-4">
-              <p className="text-primary-light">
-                User Testcase #
-                {(testcases.length + 1).toString().padStart(2, '0')}
-              </p>
-              <UserTestcaseForm addTestcase={addTestcase} />
-            </div>
-          </div>
-          <div className="mb-[40px] flex flex-row justify-end gap-3">
-            <DialogClose asChild>
-              <Button className="h-[40px] w-[78px] bg-[#DCE3E5] text-[#787E80] hover:bg-[#C4CACC] hover:text-[#5F6566] active:bg-[#C4CACC] active:text-[#5F6566]">
-                Cancel
+              {testcases.map((testcase, index) => (
+                <div key={testcase.id} className="flex flex-col">
+                  <p className="text-primary-light mb-4">
+                    User Testcase #{(index + 1).toString().padStart(2, '0')}
+                  </p>
+                  <div
+                    className={cn(
+                      'relative flex min-h-[80px] w-full rounded-md border border-[#DFDFDF] bg-white py-3 font-mono text-[#C2C2C2] shadow-sm',
+                      formState.errors?.testcases?.[index] && 'border-error'
+                    )}
+                  >
+                    <Textarea
+                      {...register(`testcases.${index}.input`, {
+                        required: true
+                      })}
+                      className="z-10 resize-none border-0 px-4 py-0 text-black shadow-none placeholder:text-[#3333334D] focus-visible:ring-0"
+                    />
+                    <Textarea
+                      {...register(`testcases.${index}.output`, {
+                        required: true
+                      })}
+                      className="z-10 min-h-[80px] rounded-none border-l border-transparent border-l-gray-200 px-4 py-0 text-black shadow-none placeholder:text-[#3333334D] focus-visible:ring-0"
+                    />
+                    <button
+                      type="button"
+                      className="absolute right-4 z-20 text-[#9B9B9B]"
+                      onClick={() => removeTestcase(index)}
+                    >
+                      <IoIosClose size={25} />
+                    </button>
+                  </div>
+                  {formState.errors?.testcases?.[index] && (
+                    <div className="text-error mt-2 flex items-center gap-1 text-[11px] font-medium leading-[14px]">
+                      <AlertTriangle size={12} />
+                      Required
+                    </div>
+                  )}
+                </div>
+              ))}
+              <Button
+                type="button"
+                onClick={addTestcase}
+                className="mt-6 flex w-full gap-2 bg-[#555C66] text-[#C4CACC] hover:bg-[#222939] active:bg-[#222939]"
+              >
+                <FaPlus />
+                Add
               </Button>
-            </DialogClose>
-            <Button
-              className="flex h-[40px] w-[95px] gap-2 text-white hover:text-neutral-200 active:text-neutral-200"
-              onClick={onClickSaveButton}
-            >
-              <FaCircleCheck />
-              Save
-            </Button>
-          </div>
+            </div>
+
+            <div className="mb-[40px] flex flex-row justify-end gap-3">
+              <DialogClose asChild>
+                <Button className="h-[40px] w-[78px] bg-[#DCE3E5] text-[#787E80] hover:bg-[#C4CACC] hover:text-[#5F6566] active:bg-[#C4CACC] active:text-[#5F6566]">
+                  Cancel
+                </Button>
+              </DialogClose>
+              <Button
+                type="submit"
+                className="flex h-[40px] w-[95px] gap-2 text-white hover:text-neutral-200 active:text-neutral-200"
+              >
+                <FaCircleCheck />
+                Save
+              </Button>
+            </div>
+          </form>
         </ScrollArea>
       </DialogContent>
     </Dialog>
-  )
-}
-
-function UserTestcaseForm({
-  addTestcase
-}: {
-  addTestcase: (value: TestcaseItem) => void
-}) {
-  const { register, handleSubmit, formState, reset } = useForm<{
-    input: string
-    output: string
-  }>()
-
-  const hasError = formState.errors.input || formState.errors.output
-
-  const onSubmit = handleSubmit((value) => {
-    addTestcase({
-      id: new Date().getTime(),
-      ...value
-    })
-    reset()
-  })
-
-  return (
-    <form onSubmit={onSubmit}>
-      <div
-        className={cn(
-          'flex min-h-[80px] w-full rounded-md border border-[#DFDFDF] bg-white py-3 font-mono text-[#C2C2C2] shadow-sm',
-          hasError && 'border-error'
-        )}
-      >
-        <Textarea
-          placeholder="Input"
-          className="z-10 resize-none border-0 px-4 py-0 text-black shadow-none placeholder:text-[#3333334D] focus-visible:ring-0"
-          {...register('input', { required: true })}
-        />
-        <Textarea
-          placeholder="Output"
-          className="z-10 min-h-[80px] rounded-none border-l border-transparent border-l-gray-200 px-4 py-0 text-black shadow-none placeholder:text-[#3333334D] focus-visible:ring-0"
-          {...register('output', { required: true })}
-        />
-      </div>
-      {hasError && (
-        <div className="text-error mt-2 flex items-center gap-1 text-[11px] font-medium leading-[14px]">
-          <AlertTriangle size={12} />
-          Required
-        </div>
-      )}
-      <Button
-        type="submit"
-        className="mt-6 flex w-full gap-2 bg-[#555C66] text-[#C4CACC] hover:bg-[#222939] active:bg-[#222939]"
-      >
-        <FaPlus />
-        Add
-      </Button>
-    </form>
   )
 }
 
@@ -211,52 +181,6 @@ function SampleTestcaseItem({
           className="absolute right-4 top-3 z-20 size-6 text-[#AAB1B2]"
         />
       </div>
-    </div>
-  )
-}
-
-function UserTestcaseItem({
-  id,
-  input,
-  output,
-  editTestcase,
-  deleteTestcase
-}: {
-  id: number
-  input: string
-  output: string
-  editTestcase: (testcase: TestcaseItem) => void
-  deleteTestcase: (id: number) => void
-}) {
-  const onChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    const { name, value } = e.currentTarget
-    editTestcase({ id, input, output, [name]: value })
-  }
-  return (
-    <div
-      className={cn(
-        'relative flex min-h-[80px] w-full rounded-md border border-[#DFDFDF] bg-white py-3 font-mono text-[#C2C2C2] shadow-sm'
-      )}
-    >
-      <Textarea
-        name="input"
-        value={input}
-        onChange={onChange}
-        className="z-10 resize-none border-0 px-4 py-0 text-black shadow-none placeholder:text-[#3333334D] focus-visible:ring-0"
-      />
-      <Textarea
-        name="output"
-        value={output}
-        onChange={onChange}
-        className="z-10 min-h-[80px] rounded-none border-l border-transparent border-l-gray-200 px-4 py-0 text-black shadow-none placeholder:text-[#3333334D] focus-visible:ring-0"
-      />
-      <button
-        type="button"
-        className="absolute right-4 z-20 text-[#9B9B9B]"
-        onClick={() => deleteTestcase(id)}
-      >
-        <IoIosClose size={25} />
-      </button>
     </div>
   )
 }
---
+++ apps/frontend/app/(client)/(code-editor)/_components/TestcasePanel/useUserTestcasesForm.ts
@@ -0,0 +1,92 @@
+import type { TestcaseItem } from '@/types/type'
+import { useEffect } from 'react'
+import { useFieldArray, useForm } from 'react-hook-form'
+import { useTestcaseStore } from '../context/TestcaseStoreProvider'
+
+interface EditableTestcaseItem extends TestcaseItem {
+  originalInput: string
+  originalOutput: string
+}
+
+const isEdited = (item: EditableTestcaseItem) =>
+  item.input !== item.originalInput || item.output !== item.originalOutput
+
+const getEmptyTestcase = () => ({
+  id: new Date().getTime(),
+  input: '',
+  output: '',
+  originalInput: '',
+  originalOutput: ''
+})
+
+const getDefaultTestcases = (userTestcases: TestcaseItem[]) => {
+  if (userTestcases.length > 0) {
+    return userTestcases.map((item) => ({
+      ...item,
+      originalInput: item.input,
+      originalOutput: item.output
+    }))
+  }
+  return [getEmptyTestcase()]
+}
+
+export const useUserTestcasesForm = (options: { onSubmit?: () => void }) => {
+  const userTestcases = useTestcaseStore((state) => state.userTestcases)
+  const setUserTestcases = useTestcaseStore((state) => state.setUserTestcases)
+
+  const { control, register, setValue, formState, handleSubmit } = useForm<{
+    testcases: EditableTestcaseItem[]
+  }>({
+    defaultValues: {
+      testcases: getDefaultTestcases(userTestcases)
+    }
+  })
+
+  const {
+    fields: testcases,
+    append,
+    remove,
+    replace
+  } = useFieldArray({
+    control,
+    name: 'testcases'
+  })
+
+  const addTestcase = () => {
+    append(getEmptyTestcase())
+  }
+
+  const removeTestcase = (index: number) => {
+    if (testcases.length === 1) {
+      replace(getEmptyTestcase())
+    } else {
+      remove(index)
+    }
+  }
+
+  const onSubmit = handleSubmit(({ testcases }) => {
+    let baseId = new Date().getTime()
+    setUserTestcases(
+      testcases.map((testcase) => ({
+        // NOTE: testcase가 수정된 경우 이전 테스트 결과를 초기화하기 위해 새로운 아이디를 부여합니다.
+        id: isEdited(testcase) ? baseId++ : testcase.id,
+        input: testcase.input,
+        output: testcase.output
+      }))
+    )
+    options.onSubmit?.()
+  })
+
+  useEffect(() => {
+    setValue('testcases', getDefaultTestcases(userTestcases))
+  }, [userTestcases])
+
+  return {
+    formState,
+    testcases,
+    onSubmit,
+    register,
+    addTestcase,
+    removeTestcase
+  }
+}
