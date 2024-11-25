c927c0a3acb5e96aa59b9293247d8f610f651cfa
eunnbi <jenabill@naver.com>
2024-11-25 17:47:50 +0900

    refactor(fe): change problem cards component to server component

--- apps/frontend/app/(client)/(main)/_components/ProblemCards.tsx
+++ apps/frontend/app/(client)/(main)/_components/ProblemCards.tsx
@@ -1,78 +1,36 @@
-'use client'
-
-import { Skeleton } from '@/components/shadcn/skeleton'
-import { fetcher } from '@/libs/utils'
-import type { WorkbookProblem } from '@/types/type'
-import type { Route } from 'next'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { getProblemList } from '../../_libs/apis/problem'
 import ProblemCard from './ProblemCard'
 
-interface ProblemCardsProps {
-  data: WorkbookProblem[]
-  total: number
-}
-
-const getProblems = async () => {
-  const problemRes: ProblemCardsProps = await fetcher
-    .get('problem', {
-      searchParams: {
-        take: 3,
-        order: 'submit-desc'
-        // workbookId: 1
-      }
-    })
-    .json()
-
-  problemRes.data ?? console.error('4.getProblem', problemRes)
-  return problemRes.data ?? problemRes
-}
-
-export default function ProblemCards() {
-  const [problems, setProblems] = useState<WorkbookProblem[]>([])
-  const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    getProblems().then((res) => {
-      setProblems(res)
-      setLoading(false)
-    })
-  }, [])
+export default async function ProblemCards() {
+  const { data: problems } = await getProblemList({
+    take: 3,
+    order: 'submit-desc'
+  })
 
   return (
     <>
       <div className="flex justify-start gap-5 md:hidden">
-        {loading
-          ? [...Array(2)].map((_, i) => (
-              <Skeleton key={i} className="flex h-[120px] w-full rounded-xl" />
-            ))
-          : problems.slice(0, 2).map((problem) => {
-              return (
-                <Link
-                  key={problem.id}
-                  href={`/problem/${problem.id}` as Route}
-                  className="inline-block w-1/2"
-                >
-                  <ProblemCard problem={problem} />
-                </Link>
-              )
-            })}
+        {problems.slice(0, 2).map((problem) => (
+          <Link
+            key={problem.id}
+            href={`/problem/${problem.id}`}
+            className="inline-block w-1/2"
+          >
+            <ProblemCard problem={problem} />
+          </Link>
+        ))}
       </div>
       <div className="hidden justify-start gap-5 md:flex">
-        {loading
-          ? [...Array(3)].map((_, i) => (
-              <Skeleton key={i} className="flex h-[120px] w-full rounded-xl" />
-            ))
-          : problems.map((problem) => {
-              return (
-                <Link
-                  key={problem.id}
-                  href={`/problem/${problem.id}` as Route}
-                  className="inline-block w-1/3"
-                >
-                  <ProblemCard problem={problem} />
-                </Link>
-              )
-            })}
+        {problems.map((problem) => (
+          <Link
+            key={problem.id}
+            href={`/problem/${problem.id}`}
+            className="inline-block w-1/3"
+          >
+            <ProblemCard problem={problem} />
+          </Link>
+        ))}
       </div>
     </>
   )
