a590506d52ce2919f62e50913be8b9b04d19cae3
eunnbi <jenabill@naver.com>
2024-11-26 16:50:07 +0900

    refactor(fe): refactor problem infinite table

--- apps/frontend/app/(client)/(main)/problem/page.tsx
+++ apps/frontend/app/(client)/(main)/problem/page.tsx
@@ -1,38 +1,12 @@
-'use client'
-
-import { Skeleton } from '@/components/shadcn/skeleton'
-import type { Problem } from '@/types/type'
 import { Suspense } from 'react'
-import ProblemInfiniteTable from './_components/ProblemInfiniteTable'
+import ProblemInfiniteTable, {
+  ProblemInfiniteTableFallback
+} from './_components/ProblemInfiniteTable'
 
-export default function Problem() {
+export default function ProblemListPage() {
   return (
-    <Suspense
-      fallback={
-        <>
-          <div className="mt-4 flex">
-            <span className="w-5/12">
-              <Skeleton className="h-6 w-20" />
-            </span>
-            <span className="w-2/12">
-              <Skeleton className="mx-auto h-6 w-20" />
-            </span>
-            <span className="w-2/12">
-              <Skeleton className="mx-auto h-6 w-20" />
-            </span>
-            <span className="w-2/12">
-              <Skeleton className="mx-auto h-6 w-20" />
-            </span>
-            <span className="w-1/12">
-              <Skeleton className="mx-auto h-6 w-12" />
-            </span>
-          </div>
-          {[...Array(5)].map((_, i) => (
-            <Skeleton key={i} className="my-2 flex h-12 w-full rounded-xl" />
-          ))}
-        </>
-      }
-    >
+    /**TODO: add error boundary */
+    <Suspense fallback={<ProblemInfiniteTableFallback />}>
       <ProblemInfiniteTable />
     </Suspense>
   )
