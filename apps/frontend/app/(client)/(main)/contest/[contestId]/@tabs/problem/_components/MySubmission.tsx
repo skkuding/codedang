bf5a48cc73d8f1af12fccce47d72dfd2881ab378
Kimhyojung0810 <jenny0308@g.skku.edu>
2024-11-29 03:24:38 +0900

    refactor(fe): refactor MySubmission component

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/problem/_components/MySubmission.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/problem/_components/MySubmission.tsx
@@ -1,3 +1,8 @@
+'use client'
+
+import { submissionQueries } from '@/app/(client)/_libs/queries/submission'
+//import FetchErrorFallback from '@/components/FetchErrorFallback'
+//import { TanstackQueryErrorBoundary } from '@/components/TanstackQueryErrorBoundary'
 import {
   Dialog,
   DialogTrigger,
@@ -10,54 +15,54 @@ import {
   TooltipProvider,
   TooltipTrigger
 } from '@/components/shadcn/tooltip'
-import { fetcherWithAuth } from '@/libs/utils'
 import seeSubmissionIcon from '@/public/icons/see-submission.svg'
-import type { SubmissionDetail, Submission, ContestProblem } from '@/types/type'
+import type { ContestProblem } from '@/types/type'
 import * as TooltipPrimitive from '@radix-ui/react-tooltip'
+import { useQuery } from '@tanstack/react-query'
 import Image from 'next/image'
 import { useParams } from 'next/navigation'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import SubmissionDetailContent from './SubmissionDetailContent'
-
-interface SubmissionsResponse {
-  data: Submission[]
-  total: number
-}
 
 export default function MySubmission({ problem }: { problem: ContestProblem }) {
   const [isTooltipOpen, setIsTooltipOpen] = useState(false)
-  const [submission, setSubmission] = useState<SubmissionDetail | null>(null)
-  const [submissionId, setSubmissionId] = useState<number | null>(null)
-  const { contestId } = useParams()
+  const { contestId: contestIdString } = useParams()
+  const contestId = Number(contestIdString) //submissionQueries에서 contestId number로 정의
 
-  useEffect(() => {
-    const getSubmission = async () => {
-      const submissions: SubmissionsResponse = await fetcherWithAuth
-        .get(`contest/${contestId}/submission`, {
-          searchParams: {
-            take: 1,
-            problemId: problem.id
-          }
-        })
-        .json()
-      const firstSubmission = submissions.data[0]
-      setSubmissionId(firstSubmission.id)
+  const { data: latestSubmissionData, isLoading: isLoadingLatest } = useQuery(
+    submissionQueries.list({
+      contestId,
+      problemId: problem.id,
+      take: 1
+    })
+  )
 
-      const submission: SubmissionDetail = await fetcherWithAuth
-        .get(
-          `submission/${firstSubmission.id}?problemId=${problem.id}&contestId=${contestId}`
-        )
-        .json()
-      setSubmission(submission)
-    }
-    getSubmission()
-  }, [contestId, problem.id])
+  const latestSubmission = latestSubmissionData?.data?.[0]
+  const latestSubmissionId = latestSubmission?.id ?? 0 //타입 에러 떠서 latestSubmissionId가 undefined일 경우 기본값 0으로 설정
 
-  if (!submission || !submissionId) {
+  // 최신 제출물의 상세정보 가져오기
+  const { data: submission, isLoading: isLoadingDetail } = useQuery({
+    ...submissionQueries.detail({
+      contestId,
+      submissionId: latestSubmissionId,
+      problemId: problem.id
+    }),
+    enabled: latestSubmissionId !== 0, // 의존적 쿼리, 기본값이 아닌 경우(undefined가 아닌 경우)에만 실행
+    throwOnError: true
+  })
+
+  // 로딩 상태 처리
+  if (isLoadingLatest || isLoadingDetail) {
     return <Skeleton className="size-[25px]" />
   }
 
+  if (!submission) {
+    return null
+  }
+
+  //ToDo: Error Handling
   return (
+    //<TanstackQueryErrorBoundary fallback={FetchErrorFallback}>
     <>
       <Dialog onOpenChange={() => setIsTooltipOpen(false)}>
         <TooltipProvider>
@@ -92,7 +97,7 @@ export default function MySubmission({ problem }: { problem: ContestProblem }) {
           <DialogContent className="max-h-[620px] max-w-[800px] justify-center">
             <SubmissionDetailContent
               submission={submission}
-              submissionId={submissionId}
+              submissionId={latestSubmissionId}
               problem={problem}
             />
           </DialogContent>
