68d949c647fb167f91e28c2c0c59b224a2d28193
Kwon Seo Jin <97675977+B0XERCAT@users.noreply.github.com>
2024-11-28 18:43:24 +0900

    feat(fe): implement notice create and detail page (#2248)
    * feat(fe): add create notice gql mutation
    
    * feat(fe): implement notice create page
    
    * feat(fe): add simple notice detail page
    
    * fix(fe): replace sanitize with KatexContent
    
    * chore(fe): route to detail page after creating notice

---
+++ apps/frontend/app/admin/notice/_components/FixedForm.tsx
@@ -0,0 +1,54 @@
+'use client'
+
+import { useFormContext, useController } from 'react-hook-form'
+import { TbPinned, TbPinnedOff } from 'react-icons/tb'
+import ErrorMessage from '../../_components/ErrorMessage'
+
+export default function FixedForm({
+  blockEdit = false
+}: {
+  blockEdit?: boolean
+}) {
+  const {
+    control,
+    formState: { errors }
+  } = useFormContext()
+
+  const { field: isFixedField } = useController({
+    name: 'isFixed',
+    control,
+    defaultValue: true
+  })
+
+  return (
+    <>
+      <div className="mt-3 flex items-center gap-2">
+        <div className="flex gap-14">
+          <label className="flex gap-2">
+            <input
+              type="radio"
+              onBlur={isFixedField.onBlur}
+              onChange={() => isFixedField.onChange(true)}
+              checked={isFixedField.value === true}
+              className="text-primary-light"
+              disabled={blockEdit}
+            />
+            <TbPinned className="text-gray-400" size={20} />
+          </label>
+          <label className="flex gap-2">
+            <input
+              type="radio"
+              onBlur={isFixedField.onBlur}
+              onChange={() => isFixedField.onChange(false)}
+              checked={isFixedField.value === false}
+              className="text-primary-light"
+              disabled={blockEdit}
+            />
+            <TbPinnedOff className="text-gray-400" size={20} />
+          </label>
+        </div>
+      </div>
+      {errors.isFixed && <ErrorMessage message="required" />}
+    </>
+  )
+}
