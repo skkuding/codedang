1f878f7483226174fc8714f084b5fb3673167d13
Kwon Seo Jin <97675977+B0XERCAT@users.noreply.github.com>
2024-11-28 20:34:51 +0900

    feat(fe): apply error boundary for main directory (#2241)
    * feat(fe): install suspensive, implement ErrorFallback component, apply ErrorBoundary to home page
    
    * chore(fe): fix pnpm-lock.yaml
    
    * feat(fe): add retry button in FetchErrorFallback
    
    * feat(fe): apply ErrorBoundary
    
    * chore(fe): remove console logging
    
    * chore(fe): specify importing type ErrorBoundaryFallbackProps
    
    * chore(fe): narrow the scope of ErrorBoundary in contest page

--- apps/frontend/app/(client)/(main)/contest/page.tsx
+++ apps/frontend/app/(client)/(main)/contest/page.tsx
@@ -1,6 +1,8 @@
+import FetchErrorFallback from '@/components/FetchErrorFallback'
 import { Separator } from '@/components/shadcn/separator'
 import { Skeleton } from '@/components/shadcn/skeleton'
 import { auth } from '@/libs/auth'
+import { ErrorBoundary } from '@suspensive/react'
 import { redirect } from 'next/navigation'
 import { Suspense } from 'react'
 import SearchBar from '../_components/SearchBar'
@@ -64,25 +66,30 @@ export default async function Contest({ searchParams }: ContestProps) {
   return (
     <>
       <div className="mb-12 flex flex-col gap-12">
-        <Suspense fallback={<ContestCardListFallback />}>
-          <ContestCardList
-            title="Join the contest now!"
-            type="Ongoing"
-            session={session}
-          />
-        </Suspense>
-        <Suspense fallback={<ContestCardListFallback />}>
-          <ContestCardList
-            title="Check out upcoming contests"
-            type="Upcoming"
-            session={session}
-          />
-        </Suspense>
+        <ErrorBoundary fallback={FetchErrorFallback}>
+          <Suspense fallback={<ContestCardListFallback />}>
+            <ContestCardList
+              title="Join the contest now!"
+              type="Ongoing"
+              session={session}
+            />
+          </Suspense>
+        </ErrorBoundary>
+        <ErrorBoundary fallback={FetchErrorFallback}>
+          <Suspense fallback={<ContestCardListFallback />}>
+            <ContestCardList
+              title="Check out upcoming contests"
+              type="Upcoming"
+              session={session}
+            />
+          </Suspense>
+        </ErrorBoundary>
       </div>
       <div className="flex-col">
         <h1 className="mb-6 text-2xl font-bold text-gray-700">
           List of Contests
         </h1>
+
         <Suspense fallback={<FinishedContestTableFallback />}>
           {session ? (
             <TableSwitchButton registered={registered} />
@@ -92,14 +99,16 @@ export default async function Contest({ searchParams }: ContestProps) {
             </p>
           )}
           <Separator className="mb-3" />
-          <div className="flex justify-end py-8">
-            <SearchBar className="w-60" />
-          </div>
-          {session && registered ? (
-            <RegisteredContestTable search={search} />
-          ) : (
-            <FinishedContestTable search={search} session={session} />
-          )}
+          <ErrorBoundary fallback={FetchErrorFallback}>
+            <div className="flex justify-end py-8">
+              <SearchBar className="w-60" />
+            </div>
+            {session && registered ? (
+              <RegisteredContestTable search={search} />
+            ) : (
+              <FinishedContestTable search={search} session={session} />
+            )}
+          </ErrorBoundary>
         </Suspense>
       </div>
     </>
