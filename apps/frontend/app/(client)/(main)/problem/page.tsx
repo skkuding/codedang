4ab66a9018154438445f003c2b20df4bb3dbabc6
Eunbi Kang <jenabill@naver.com>
2024-11-29 22:00:45 +0900

    refactor(fe): refactor infinite scroll with tanstack query (#2244)
    * feat(fe): add problem infinite list query option
    
    * feat(fe): add intersection area component
    
    * refactor(fe): refactor problem infinite table
    
    * feat(fe): add tanstack query error boundary
    
    * fix(fe): pass itemsPerPage parameter
    
    * chore(fe): use common error fallback ui

--- apps/frontend/app/(client)/(main)/problem/page.tsx
+++ apps/frontend/app/(client)/(main)/problem/page.tsx
@@ -1,39 +1,16 @@
-'use client'
-
-import { Skeleton } from '@/components/shadcn/skeleton'
-import type { Problem } from '@/types/type'
+import FetchErrorFallback from '@/components/FetchErrorFallback'
+import { TanstackQueryErrorBoundary } from '@/components/TanstackQueryErrorBoundary'
 import { Suspense } from 'react'
-import ProblemInfiniteTable from './_components/ProblemInfiniteTable'
+import ProblemInfiniteTable, {
+  ProblemInfiniteTableFallback
+} from './_components/ProblemInfiniteTable'
 
-export default function Problem() {
+export default function ProblemListPage() {
   return (
-    <Suspense
-      fallback={
-        <>
-          <div className="mt-4 flex">
-            <span className="w-5/12">
-              <Skeleton className="h-6 w-20" />
-            </span>
-            <span className="w-2/12">
-              <Skeleton className="mx-auto h-6 w-20" />
-            </span>
-            <span className="w-2/12">
-              <Skeleton className="mx-auto h-6 w-20" />
-            </span>
-            <span className="w-2/12">
-              <Skeleton className="mx-auto h-6 w-20" />
-            </span>
-            <span className="w-1/12">
-              <Skeleton className="mx-auto h-6 w-12" />
-            </span>
-          </div>
-          {[...Array(5)].map((_, i) => (
-            <Skeleton key={i} className="my-2 flex h-12 w-full rounded-xl" />
-          ))}
-        </>
-      }
-    >
-      <ProblemInfiniteTable />
-    </Suspense>
+    <TanstackQueryErrorBoundary fallback={FetchErrorFallback}>
+      <Suspense fallback={<ProblemInfiniteTableFallback />}>
+        <ProblemInfiniteTable />
+      </Suspense>
+    </TanstackQueryErrorBoundary>
   )
 }
