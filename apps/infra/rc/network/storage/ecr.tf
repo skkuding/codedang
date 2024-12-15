be3e2796d4f5a5b68c44978661ba6d1040b9401e
bocklag <khouu@naver.com>
2024-12-15 16:12:34 +0000

    feat(infra): rc-server(re-factoring terraform)

---
+++ apps/infra/rc/network/storage/ecr.tf
@@ -0,0 +1,28 @@
+data "aws_ecr_repository" "repositories" {
+  for_each = toset(var.repository_names)
+  name = each.value
+}
+
+resource "aws_ecr_lifecycle_policy" "repository_policy" {
+  for_each = data.aws_ecr_repository.repositories
+
+  repository = each.value.name
+  policy     = <<EOF
+    {
+        "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Keep the last 2 multi-architecture sets (1 image index, 2 architecture images).",
+            "selection": {
+            "tagStatus": "any",
+            "countType": "imageCountMoreThan",
+            "countNumber": 6
+            },
+            "action": {
+            "type": "expire"
+            }
+        }
+        ]
+    }
+    EOF
+}
