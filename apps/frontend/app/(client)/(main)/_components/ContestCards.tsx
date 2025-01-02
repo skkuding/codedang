77f8ce147234714e74b3c79f1f3ea8837d207d98
SuminJeon <jeonsumin2002@gmail.com>
2025-01-02 16:20:51 +0900

    style: delete the title if no data

--- apps/frontend/app/(client)/(main)/_components/ContestCards.tsx
+++ apps/frontend/app/(client)/(main)/_components/ContestCards.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/shadcn/button'
 import { fetcher } from '@/libs/utils'
 import type { Contest } from '@/types/type'
 import type { Route } from 'next'
@@ -25,33 +26,45 @@ export default async function ContestCards() {
   const contests = await getContests()
 
   return (
-    <>
-      <div className="flex justify-start gap-5 md:hidden">
-        {contests.slice(0, 2).map((contest) => {
-          return (
-            <Link
-              key={contest.id}
-              href={`/contest/${contest.id}` as Route}
-              className="inline-block w-1/2"
-            >
-              <ContestCard contest={contest} />
+    contests.length > 0 && (
+      <>
+        <div className="flex w-full flex-col gap-6">
+          <div className="flex items-center justify-between text-gray-700">
+            <p className="text-2xl font-bold">Contest 🏆</p>
+            <Link href={'/contest'}>
+              <Button variant="ghost" className="h-8 px-3">
+                See More
+              </Button>
             </Link>
-          )
-        })}
-      </div>
-      <div className="hidden justify-start gap-5 md:flex">
-        {contests.map((contest) => {
-          return (
-            <Link
-              key={contest.id}
-              href={`/contest/${contest.id}` as Route}
-              className="inline-block w-1/3"
-            >
-              <ContestCard contest={contest} />
-            </Link>
-          )
-        })}
-      </div>
-    </>
+          </div>
+          <div className="flex justify-start gap-5 md:hidden">
+            {contests.slice(0, 2).map((contest) => {
+              return (
+                <Link
+                  key={contest.id}
+                  href={`/contest/${contest.id}` as Route}
+                  className="inline-block w-1/2"
+                >
+                  <ContestCard contest={contest} />
+                </Link>
+              )
+            })}
+          </div>
+          <div className="hidden justify-start gap-5 md:flex">
+            {contests.map((contest) => {
+              return (
+                <Link
+                  key={contest.id}
+                  href={`/contest/${contest.id}` as Route}
+                  className="inline-block w-1/3"
+                >
+                  <ContestCard contest={contest} />
+                </Link>
+              )
+            })}
+          </div>
+        </div>
+      </>
+    )
   )
 }
