3b68a6829aca3489b628dae31962e9befcef83fe
RyuRaseul <ryudb00@g.skku.edu>
2025-01-18 08:05:25 +0000

    feat(be): add test submission model

---
+++ apps/backend/prisma/migrations/20250117202041_add_test_submission_result_model/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "testcase_submission_result" (
+    "id" SERIAL NOT NULL,
+    "testcase_submission_id" INTEGER NOT NULL,
+    "is_user_test" BOOLEAN NOT NULL DEFAULT false,
+    "problem_test_case_id" INTEGER,
+    "user_input" TEXT,
+    "user_output" TEXT,
+    "result" "ResultStatus" NOT NULL,
+    "cpu_time" BIGINT,
+    "memory_usage" INTEGER,
+    "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "update_time" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "testcase_submission_result_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "testcase_submission_result" ADD CONSTRAINT "testcase_submission_result_testcase_submission_id_fkey" FOREIGN KEY ("testcase_submission_id") REFERENCES "testcase_submission"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "testcase_submission_result" ADD CONSTRAINT "testcase_submission_result_problem_test_case_id_fkey" FOREIGN KEY ("problem_test_case_id") REFERENCES "problem_testcase"("id") ON DELETE CASCADE ON UPDATE CASCADE;
