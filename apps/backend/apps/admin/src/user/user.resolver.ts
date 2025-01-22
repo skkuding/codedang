64e337f641260b76722374216b2c9b765eb75c84
hwantae <73492261+hwantae@users.noreply.github.com>
2025-01-22 19:28:52 +0900

    docs(be): add jsdoc at admin user and client auth (#2321)

--- apps/backend/apps/admin/src/user/user.resolver.ts
+++ apps/backend/apps/admin/src/user/user.resolver.ts
@@ -10,6 +10,15 @@ import { UserService } from './user.service'
 export class UserResolver {
   constructor(private readonly userService: UserService) {}
 
+  /**
+   * 특정 그룹의 멤버를 페이지네이션과 필터링 조건에 따라 조회함.
+   *
+   * @param {number} groupId - 그룹의 ID. 기본 값은 OPEN_SPACE_ID.
+   * @param {number | null} cursor - 페이지네이션을 위한 커서. 선택적 매개변수임
+   * @param {number} take - 가져올 그룹 멤버의 수. 기본값은 10.
+   * @param {boolean} leaderOnly - 그룹의 리더만 필터링 해주는 플래그. 기본 값은 false
+   * @returns {Promise<GroupMember[]>} - 그룹 멤버 리스트를 리턴함.
+   */
   @Query(() => [GroupMember])
   async getGroupMembers(
     @Args(
@@ -36,6 +45,13 @@ export class UserResolver {
     )
   }
 
+  /**
+   * 특정 그룹에서 사용자 ID를 기반으로 그룹 멤버를 조회함.
+   *
+   * @param {number} groupId - 그룹의 ID. 기본 값은 OPEN_SPACE_ID.
+   * @param {number} userId - 조회할 사용자의 ID.
+   * @returns {Promise<GroupMember>} 그룹 멤버 객체를 리턴함.
+   */
   @Query(() => GroupMember)
   async getGroupMember(
     @Args(
@@ -50,6 +66,14 @@ export class UserResolver {
     return await this.userService.getGroupMember(groupId, userId)
   }
 
+  /**
+   * 특정 그룹 멤버를 리더로 업데이트함
+   *
+   * @param {number} userId - 역할을 업데이트 할 사용자의 ID.
+   * @param {number} groupId - 업데이트 대상 그룹의 ID.
+   * @param {boolean} toGroupLeader - 사용자를 리더로 설정할지에 대한 플래그
+   * @returns {Promise<UserGroup>} 업데이트된 그룹 멤버 객체를 리턴.
+   */
   @Mutation(() => UserGroup)
   async updateGroupMember(
     @Args('userId', { type: () => Int }, new RequiredIntPipe('userId'))
@@ -64,6 +88,13 @@ export class UserResolver {
     )
   }
 
+  /**
+   * 특정 사용자를 그룹에서 제거.
+   *
+   * @param {number} userId - 제거할 사용자의 id
+   * @param {number} groupId - 대상 그룹의 id
+   * @returns {Promise<UserGroup>} 삭제된 그룹 멤버 객체를 리턴.
+   */
   @Mutation(() => UserGroup)
   async deleteGroupMember(
     @Args('userId', { type: () => Int }, new RequiredIntPipe('userId'))
@@ -73,6 +104,11 @@ export class UserResolver {
     return await this.userService.deleteGroupMember(userId, groupId)
   }
 
+  /**
+   * 특정 그룹에 가입 요청한 유저 리스트를 조회.
+   * @param {number} groupId - 특정 그룹의 id.
+   * @returns {Promise<User[]>} 가입 요청한 유저 리스트를 리턴.
+   */
   @Query(() => [User])
   async getJoinRequests(
     @Args('groupId', { type: () => Int }, GroupIDPipe) groupId: number
@@ -80,6 +116,14 @@ export class UserResolver {
     return await this.userService.getJoinRequests(groupId)
   }
 
+  /**
+   * 그룹 가입 요청을 처리.
+   *
+   * @param {number} groupId
+   * @param {number} userId
+   * @param {boolean} isAccept - 요청 승인에 대한 플래그.
+   * @returns {Promise<UserGroup>}
+   */
   @Mutation(() => UserGroup)
   async handleJoinRequest(
     @Args('groupId', { type: () => Int }, GroupIDPipe) groupId: number,
