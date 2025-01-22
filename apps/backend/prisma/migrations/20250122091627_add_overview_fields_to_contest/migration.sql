bdeee1f3f6d4ee469ec13f922fcd8468699fc925
Junhyeok Song <02hyeok@hdmedi.co.kr>
2025-01-22 12:31:00 +0000

    feat(be): add migration sql about modified contest schema

---
+++ apps/backend/prisma/migrations/20250122091627_add_overview_fields_to_contest/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "contest" ADD COLUMN     "benefits" TEXT,
+ADD COLUMN     "competitionMethod" TEXT,
+ADD COLUMN     "participationTarget" TEXT,
+ADD COLUMN     "posterUrl" TEXT,
+ADD COLUMN     "problemFormat" TEXT,
+ADD COLUMN     "rankingMethod" TEXT;
