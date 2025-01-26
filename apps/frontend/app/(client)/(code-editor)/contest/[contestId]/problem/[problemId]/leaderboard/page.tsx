7a253f68a62c8d9bb57dc0163b74d6d84353c24d
hyuntori <hyuntori@hyuntoriui-MacBookPro.local>
2025-01-26 21:21:28 +0900

    fix(fe): adjust eslint rule

--- apps/frontend/app/(client)/(code-editor)/contest/[contestId]/problem/[problemId]/leaderboard/page.tsx
+++ apps/frontend/app/(client)/(code-editor)/contest/[contestId]/problem/[problemId]/leaderboard/page.tsx
@@ -4,7 +4,7 @@ import { Paginator } from '@/components/Paginator'
 import { usePagination } from '@/libs/hooks/usePagination'
 import type { LeaderboardItem } from '@/types/type'
 import { columns } from './_components/Columns'
-import DataTable from './_components/DataTable'
+import { DataTable } from './_components/DataTable'
 
 // import dummyData from './temp_data.json'
 
