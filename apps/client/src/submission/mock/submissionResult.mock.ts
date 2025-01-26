6035eb288d90d857ff3fbbb5441056b1ddb61a7e
rlaxodnr <taandyoon7@naver.com>
2025-01-26 15:25:10 +0000

    feat(be): improve result code consumer logic

---
+++ apps/client/src/submission/mock/submissionResult.mock.ts
@@ -0,0 +1,60 @@
+import { ResultStatus, type SubmissionResult } from '@prisma/client'
+import type { JudgerResponse } from '../class/judger-response.dto'
+
+export const submissionResults: SubmissionResult[] = [
+  {
+    id: 1,
+    createTime: new Date('2023-01-01'),
+    problemTestcaseId: 1,
+    result: ResultStatus.Accepted,
+    submissionId: 1,
+    updateTime: new Date('2023-01-01'),
+    cpuTime: BigInt(12345),
+    memoryUsage: 12345
+  },
+  {
+    id: 2,
+    createTime: new Date('2023-01-01'),
+    problemTestcaseId: 2,
+    result: ResultStatus.Accepted,
+    submissionId: 2,
+    updateTime: new Date('2023-01-01'),
+    cpuTime: BigInt(12345),
+    memoryUsage: 12345
+  },
+  {
+    id: 3,
+    createTime: new Date('2023-01-01'),
+    problemTestcaseId: 3,
+    result: ResultStatus.WrongAnswer,
+    submissionId: 1,
+    updateTime: new Date('2023-01-01'),
+    cpuTime: BigInt(12345),
+    memoryUsage: 12345
+  },
+  {
+    id: 4,
+    createTime: new Date('2023-01-01'),
+    problemTestcaseId: 4,
+    result: ResultStatus.CompileError,
+    submissionId: 1,
+    updateTime: new Date('2023-01-01'),
+    cpuTime: BigInt(12345),
+    memoryUsage: 12345
+  }
+]
+
+export const judgerResponse: JudgerResponse = {
+  resultCode: 6,
+  submissionId: 1,
+  error: '',
+  judgeResult: {
+    testcaseId: 1,
+    cpuTime: 12345,
+    realTime: 20000,
+    memory: 12345,
+    signal: 0,
+    exitCode: 0,
+    errorCode: 0
+  }
+}
