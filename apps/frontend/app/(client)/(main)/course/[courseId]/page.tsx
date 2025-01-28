815c0a37085ad0d2bd821c92594e35259c32b52b
mingyuseong <mingyu27@naver.com>
2025-01-28 17:54:41 +0900

    feat(fe): create recent notice component

--- apps/frontend/app/(client)/(main)/course/[courseId]/page.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/page.tsx
@@ -1,15 +1,19 @@
 import { Separator } from '@/components/shadcn/separator'
 import { AssignmentOverview } from './_components/AssignmentOverview'
-import { RecentAnouncement } from './_components/RecentAnouncement'
+import { RecentNotice } from './_components/RecentNotice'
 import { RecentUpdates } from './_components/RecentUpdates'
 
 export default function Dashboard() {
   return (
     <div className="flex flex-col">
-      <div className="grid grid-rows-1 items-center gap-4">
-        <RecentAnouncement />
+      <div className="grid grid-cols-[1fr_auto_1fr] items-center">
+        <div className="p-10">
+          <RecentNotice />
+        </div>
         <Separator orientation="vertical" className="h-full bg-gray-300" />
-        <RecentUpdates />
+        <div className="p-10">
+          <RecentUpdates />
+        </div>
       </div>
       <Separator />
       <AssignmentOverview />
