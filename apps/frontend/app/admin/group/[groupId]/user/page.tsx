4d4f9335c86c3a3aa8d21f3a2348376396a7f2ca
jwoojin9 <jwoojin9@naver.com>
2025-01-31 03:10:39 +0900

    feat(fe): add group user table

--- apps/frontend/app/admin/group/[groupId]/user/page.tsx
+++ apps/frontend/app/admin/group/[groupId]/user/page.tsx
@@ -1,4 +1,31 @@
-export default function Page({ params }: { params: { groupId: string } }) {
+import { FetchErrorFallback } from '@/components/FetchErrorFallback'
+import { Button } from '@/components/shadcn/button'
+import { ErrorBoundary } from '@suspensive/react'
+import { PlusCircleIcon } from 'lucide-react'
+import { Suspense } from 'react'
+import {
+  GroupUserTable,
+  GroupUserTableFallback
+} from './_components/GroupUserTable'
+
+export const dynamic = 'force-dynamic'
+
+export default function User({ params }: { params: { groupId: string } }) {
   const { groupId } = params
-  return <div>{groupId} User Page</div>
+  return (
+    <div className="container mx-auto space-y-5 py-10">
+      <div className="flex justify-between">
+        <h1 className="text-4xl font-bold">User List</h1>
+        <Button variant="default">
+          <PlusCircleIcon className="mr-2 h-4 w-4" />
+          Invite
+        </Button>
+      </div>
+      <ErrorBoundary fallback={FetchErrorFallback}>
+        <Suspense fallback={<GroupUserTableFallback />}>
+          <GroupUserTable groupId={groupId} />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  )
 }
