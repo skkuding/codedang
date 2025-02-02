94e072ccf4a2df872d9154e7cad496fe5f4906f4
jspark2000 <idpjs2000@gmail.com>
2025-02-02 15:40:22 +0000

    fix(iris): remove unused files

--- apps/iris/src/handler/adaptor.go
+++
@@ -1,8 +0,0 @@
-package handler
-
-type SandboxAdapter interface {
-	Compile(source string) error
-	Run(executable string) error
-	GetConfig(language string) (interface{}, error)
-	GetExecutionEnv(language string) (interface{}, error)
-}
