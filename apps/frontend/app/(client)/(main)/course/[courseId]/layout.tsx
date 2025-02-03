5e8524c99d0c7bb96dbf987ee517c7f90d23e0ee
mingyuseong <mingyu27@naver.com>
2025-02-03 21:50:39 +0900

    feat(fe): create course info box in Course Detail Page

--- apps/frontend/app/(client)/(main)/course/[courseId]/layout.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/layout.tsx
@@ -1,8 +1,6 @@
 import { Separator } from '@/components/shadcn/separator'
-import codedangLogo from '@/public/logos/codedang-with-text.svg'
-import Image from 'next/image'
-import Link from 'next/link'
 import { Cover } from '../_components/Cover'
+import { CourseInfoBox } from './_components/CourseInfoBox'
 import { Sidebar } from './_components/Sidebar'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -12,12 +10,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="flex h-dvh w-full flex-col">
         <div className="flex flex-row">
           <nav className="w-auto bg-white p-2 px-6 pb-6 pt-20 text-sm font-medium">
-            <div className="mb-4 h-[165px] w-[305px] flex-col rounded bg-blue-200 p-5">
-              <p>[SWE301_41]</p>
-              <p>강의명은최대열세글자까지</p>
-              <p>2025 spring</p>
-              <p>prof. 박진영</p>
-            </div>
+            <CourseInfoBox />
             <Sidebar />
           </nav>
           <Separator orientation="vertical" />
