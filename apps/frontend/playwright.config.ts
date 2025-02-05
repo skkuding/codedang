4fff0dc83b67c2f69fda4c483910d9ec55b6524e
eunnbi <jenabill@naver.com>
2025-02-05 17:44:09 +0900

    chore(fe): update playwright config

--- apps/frontend/playwright.config.ts
+++ apps/frontend/playwright.config.ts
@@ -1,26 +1,74 @@
-import { defineConfig, devices } from '@playwright/test'
+import {
+  defineConfig,
+  devices,
+  type PlaywrightTestConfig
+} from '@playwright/test'
 
 export default defineConfig({
-  timeout: 30000,
-  retries: 3,
-  reporter: [['list'], ['json', { outputFile: 'test-results.json' }]],
   testDir: './playwright/tests/',
+  retries: 3,
+  reporter: [
+    ['list'],
+    ['json', { outputFile: 'playwright/test-results.json' }]
+  ],
+  webServer: {
+    command: 'pnpm dev',
+    url: 'http://localhost:5525',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000
+  },
+  use: {
+    baseURL: 'http://localhost:5525'
+  },
   projects: [
+    // Setup projects
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] }
+      name: 'Admin Setup',
+      testDir: './playwright/tests/admin',
+      testMatch: /.*\.setup\.ts/
     },
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
+      name: 'User Setup',
+      testDir: './playwright/tests/user',
+      testMatch: /.*\.setup\.ts/
     },
-    {
-      name: 'Google Chrome',
-      use: { ...devices['Desktop Chrome'] }
-    },
-    {
-      name: 'Microsoft Edge',
-      use: { ...devices['Desktop Edge'] }
-    }
+    ...getProjectsByBrowser()
   ]
 })
+
+function getProjectsByBrowser(): NonNullable<PlaywrightTestConfig['projects']> {
+  const browsers = [
+    'Desktop Safari',
+    'Desktop Firefox',
+    'Desktop Chrome',
+    'Desktop Edge'
+  ] as const
+
+  return browsers.flatMap((browser) => [
+    {
+      name: browser,
+      testDir: './playwright/tests/non-auth',
+      use: {
+        ...devices[browser]
+      }
+    },
+    {
+      name: `${browser} (User)`,
+      testDir: './playwright/tests/user',
+      dependencies: ['User Setup'],
+      use: {
+        ...devices[browser],
+        storageState: 'playwright/.auth/user.json'
+      }
+    },
+    {
+      name: `${browser} (Admin)`,
+      testDir: './playwright/tests/admin',
+      dependencies: ['Admin Setup'],
+      use: {
+        ...devices[browser],
+        storageState: 'playwright/.auth/admin.json'
+      }
+    }
+  ])
+}
