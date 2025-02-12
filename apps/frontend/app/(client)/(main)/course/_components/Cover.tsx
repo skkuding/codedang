81bebf8893f917e92b49bc69e9986c0738dba534
mingyuseong <mingyu27@naver.com>
2025-02-12 22:52:17 +0900

    fix(fe): add responsive design for cover component

--- apps/frontend/app/(client)/(main)/course/_components/Cover.tsx
+++ apps/frontend/app/(client)/(main)/course/_components/Cover.tsx
@@ -31,29 +31,44 @@ export function Cover({ title, description }: CoverProps) {
       <div
         className={cn(
           bgColors[title.toLowerCase()],
-          'z-[-10] flex h-[440px] w-full items-center justify-center'
+          'z-[-10] flex h-[440px] items-center justify-center gap-20'
         )}
       >
-        <div className="flex h-full flex-col justify-between">
-          <Image src={codedangBannerTop} alt="코드당" width={64} />
-          <Image src={codedangBannerBottom} alt="코드당" width={224} />
+        <div className="flex h-full w-auto flex-shrink-0 flex-col justify-between max-md:hidden">
+          <Image src={codedangBannerTop} alt="코드당" width={64} height={64} />
+          <Image
+            src={codedangBannerBottom}
+            alt="코드당"
+            width={224}
+            height={224}
+          />
         </div>
         <div className="flex flex-col">
-          <Image src={codedangBannerMiddle} alt="코드당" width={70} />
+          <Image
+            src={codedangBannerMiddle}
+            alt="코드당"
+            width={70}
+            height={70}
+            className="flex-shrink-0 max-md:hidden"
+          />
           <div className="flex-col text-center">
-            <h2 className="py-5 font-mono text-4xl font-bold text-white md:text-[56px]">
+            <h2 className="flex-shrink-0 py-5 font-mono text-4xl font-bold text-white md:text-[56px]">
               {title}
             </h2>
-            <div className="rounded-full bg-white px-16 py-1">
-              <span className={cn(textColors[title.toLowerCase()])}>
+            <div className="flex-shrink-0 rounded-full bg-white px-16 py-1">
+              <span
+                className={cn(
+                  'min-w-max whitespace-nowrap text-lg',
+                  textColors[title.toLowerCase()]
+                )}
+              >
                 {description}
               </span>
             </div>
           </div>
         </div>
-        <div className="px-32" />
 
-        <div className="relative flex h-screen items-center justify-center max-md:hidden">
+        <div className="relative flex h-screen flex-shrink-0 items-center justify-center max-md:hidden">
           <Image
             src={icons[title.toLowerCase()]}
             width={208}
@@ -62,7 +77,7 @@ export function Cover({ title, description }: CoverProps) {
             className="absolute right-[50%] top-[40%] flex-shrink-0 rotate-[19.478deg] max-md:hidden"
           />
 
-          <div className="relative z-10 text-center">
+          <div className="relative z-10 text-center max-md:hidden">
             <h1 className="text-6xl font-extrabold text-white">CODEDANG</h1>
             <p className="mt-4 text-2xl text-white">
               SKKU Educational Coding Website
