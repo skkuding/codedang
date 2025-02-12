a2f75750f64b02a7b73b224b5b233d0cbd7273be
jiho <jihopark09@naver.com>
2025-02-12 21:49:02 +0900

    feat(fe): make ranking info modal dialog first draft

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/leaderboard/page.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/leaderboard/page.tsx
@@ -2,6 +2,7 @@ import { SearchBar } from '@/app/(client)/(main)/_components/SearchBar'
 import information from '@/public/icons/information.svg'
 import Image from 'next/image'
 import { LeaderboardTable } from './_components/LeaderboardTable'
+import { RankInfoDialog } from './_components/RankInfoDialog'
 
 export default function ContestLeaderBoard({
   searchParams,
@@ -19,7 +20,8 @@ export default function ContestLeaderBoard({
         <h1 className="text-2xl font-semibold leading-8">
           CHECK YOUR RANKING!
         </h1>
-        <Image src={information} alt="information" />
+        <RankInfoDialog />
+        {/* <Image src={information} alt="information" /> */}
       </div>
       {/* NOTE: branch t1205-contest-main-page-new 머지되면 searchbar 디자인 바뀔 예정*/}
       <SearchBar className="mb-12 w-60" />
