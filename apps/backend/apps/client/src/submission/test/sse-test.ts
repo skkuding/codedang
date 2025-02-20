3612683fa42ae2c2a6b6406cdb4dd8adc1569b4c
Jaehyeon Kim <65964601+Jaehyeon1020@users.noreply.github.com>
2025-02-20 02:33:53 +0000

    chore(be): integrate sse test scripts

--- apps/backend/apps/client/src/submission/test/sse-test.ts
+++ apps/backend/apps/client/src/submission/test/sse-test.ts
@@ -3,22 +3,28 @@
 /**
  * SSE 테스트 스크립트 사용법
  * 1. Bruno로 로그인 API 호출 해 로그인 한 후 Authorization 헤더에 세팅되는 Bearer Token 값 복사 후 jwtToken 변수에 붙여넣기
- * 2. submission 생성(제출) 후 반환되는 submission ID 설정
+ * 2. submission 생성(제출) 후 반환되는 submission ID 설정 OR key 설정
+ * 3. 테스트 할 API에 따라 API Path 설정
  */
 
-const submissionId = 1 // 테스트할 submissionId
-const jwtToken = 'Bearer abcdef' // JWT Token
+const jwtToken = 'Bearer token'
 
-async function startSSE() {
+const submissionId = 1 // (Get Submission Result 용) submissionId
+const key = 'key' // (Get Test|User-Test Result 용) key
+
+const paths = {
+  getSubmissionResult: `http://localhost:4000/submission/submission-result/${submissionId}`,
+  getTestResult: `http://localhost:4000/submission/test-result/${key}`
+}
+
+async function runSSE() {
   try {
-    const response = await fetch(
-      `http://localhost:4000/submission/result/${submissionId}`,
-      {
-        headers: {
-          Authorization: jwtToken
-        }
+    // 테스트 할 API에 따라 API Path를 설정 해 주세요.
+    const response = await fetch(paths.getSubmissionResult, {
+      headers: {
+        Authorization: jwtToken
       }
-    )
+    })
 
     if (!response.ok) {
       throw new Error(`Failed to connect: ${response.statusText}`)
@@ -41,4 +47,4 @@ async function startSSE() {
   }
 }
 
-startSSE()
+runSSE()
