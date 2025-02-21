344cd5c14938cbe0bf39d1e2a3e3a9c9e0d4b8fb
juhyeong0505 <80684730+juhyeong0505@users.noreply.github.com>
2025-02-21 23:08:53 +0900

    feat(fe): make table files

--- apps/frontend/app/(client)/(main)/course/[courseId]/assignment/page.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/assignment/page.tsx
@@ -1,29 +1,13 @@
-import { Separator } from '@/components/shadcn/separator'
 import { auth } from '@/libs/auth'
 import { AssignmentTable } from '../_components/AssignmentTable'
-import { AssignmentTableSwitchButton } from '../_components/AssignmentTableSwitchButton'
-import { FinishedAssignmentTable } from '../_components/FinishedAssignmentTable'
-import { OngoingAssignmentTable } from '../_components/OngoingAssignmentTable'
-import { UpcomingAssignmentTable } from '../_components/UpcomingAssignmentTable'
 
-interface AssignmentProps {
-  searchParams: {
-    type: string
-  }
-}
-export default async function Assignment({ searchParams }: AssignmentProps) {
-  const type = searchParams.type
+export default async function Assignment() {
   const session = await auth()
   return (
-    <div className="mt-[78px] flex flex-col">
+    <div className="mt-[67px] flex flex-col">
       <div>
-        <AssignmentTableSwitchButton type={type} />
-        <Separator className="mb-3" />
         <div className="mb-12 w-full">
-          {type === 'showAll' && <AssignmentTable session={session} />}
-          {type === 'ongoing' && <OngoingAssignmentTable />}
-          {type === 'upcoming' && <UpcomingAssignmentTable />}
-          {type === 'finished' && <FinishedAssignmentTable session={session} />}
+          <AssignmentTable session={session} />
         </div>
       </div>
     </div>
