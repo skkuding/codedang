a19bf34fb03ea8a5597f6d26c683fdec5f3c905e
b0xercat <seojin3154@naver.com>
2025-02-22 21:15:52 +0900

    feat(fe): impl AssignmentAccordion

---
+++ apps/frontend/app/(client)/(main)/course/[courseId]/_components/AssignmentAccordion.tsx
@@ -0,0 +1,46 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from '@/components/shadcn/accordion'
+
+interface AssignmentAccordionProps {
+  week: number
+}
+
+export function AssignmentAccordion({ week }: AssignmentAccordionProps) {
+  return (
+    <>
+      {Array.from({ length: week }, (_, i) => i + 1).map((week: number) => (
+        <AssignmentAccordionItem key={week} week={week} />
+      ))}
+    </>
+  )
+}
+
+interface AssignmentAccordionItemProps {
+  week: number
+}
+
+function AssignmentAccordionItem({ week }: AssignmentAccordionItemProps) {
+  return (
+    <Accordion type="single" collapsible className="w-[1208px]">
+      <AccordionItem value={week.toString()} className="mx-6 border-b-0">
+        <AccordionTrigger className="my-2 flex h-[68px] rounded-2xl px-8 text-lg font-semibold shadow-lg">
+          <div className="relative text-left">
+            <p className="text-primary absolute top-0 w-32 -translate-y-1/2">
+              [Week {week}]
+            </p>
+            <p className="absolute left-32 top-0 w-32 -translate-y-1/2">
+              {week}주차
+            </p>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent className="mb-10 pb-0 pt-[3px] text-base text-[#00000080]">
+          Assignment Contents
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  )
+}
--- apps/frontend/app/(client)/(main)/course/[courseId]/assignment/page.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/assignment/page.tsx
@@ -1,15 +1,26 @@
-import { auth } from '@/libs/auth'
-import { AssignmentTable } from '../_components/AssignmentTable'
+import { safeFetcherWithAuth } from '@/libs/utils'
+import type { Course } from '@/types/type'
+import { AssignmentAccordion } from '../_components/AssignmentAccordion'
 
-export default async function Assignment() {
-  const session = await auth()
+export default async function Assignment({
+  params
+}: {
+  params: { courseId: string }
+}) {
+  const { courseId } = params
+
+  const course = await getCourse(courseId)
+  const week = course.courseInfo.week
+
   return (
-    <div className="mt-[67px] flex flex-col">
-      <div>
-        <div className="mb-12 w-full">
-          <AssignmentTable session={session} />
-        </div>
-      </div>
+    <div className="mb-12 mt-[67px] flex w-full flex-col">
+      <AssignmentAccordion week={week} />
     </div>
   )
+}
+
+const getCourse = async (courseId: string) => {
+  const response = await safeFetcherWithAuth.get(`course/${courseId}`)
+  const data = await response.json<Course>()
+  return data
 }
