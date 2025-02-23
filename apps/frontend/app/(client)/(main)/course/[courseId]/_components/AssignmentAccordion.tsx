eac0ab45b3404d30d1014f772c5ee72d818d62e0
b0xercat <seojin3154@naver.com>
2025-02-23 16:00:09 +0900

    chore(fe): add CountBadge

--- apps/frontend/app/(client)/(main)/course/[courseId]/_components/AssignmentAccordion.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/_components/AssignmentAccordion.tsx
@@ -26,7 +26,7 @@ const dummyAssignmentList = [
     week: 1,
     participants: 1,
     problemCount: 3,
-    solvedProblemCount: 1
+    submittedProblemCount: 1
   },
   {
     id: 1,
@@ -43,7 +43,7 @@ const dummyAssignmentList = [
     week: 1,
     participants: 13,
     problemCount: 3,
-    solvedProblemCount: 3
+    submittedProblemCount: 3
   },
   {
     id: 3,
@@ -60,7 +60,7 @@ const dummyAssignmentList = [
     week: 2,
     participants: 1,
     problemCount: 3,
-    solvedProblemCount: 2
+    submittedProblemCount: 2
   },
   {
     id: 1,
@@ -77,7 +77,7 @@ const dummyAssignmentList = [
     week: 3,
     participants: 13,
     problemCount: 5,
-    solvedProblemCount: 2
+    submittedProblemCount: 2
   },
   {
     id: 3,
@@ -94,7 +94,7 @@ const dummyAssignmentList = [
     week: 4,
     participants: 1,
     problemCount: 3,
-    solvedProblemCount: 3
+    submittedProblemCount: 3
   },
   {
     id: 1,
@@ -111,17 +111,17 @@ const dummyAssignmentList = [
     week: 4,
     participants: 13,
     problemCount: 7,
-    solvedProblemCount: 2
+    submittedProblemCount: 2
   }
 ]
 
 export function AssignmentAccordion({ week }: AssignmentAccordionProps) {
   return (
-    <>
+    <div className="mt-3">
       {Array.from({ length: week }, (_, i) => i + 1).map((week: number) => (
         <AssignmentAccordionItem key={week} week={week} />
       ))}
-    </>
+    </div>
   )
 }
 
@@ -135,17 +135,17 @@ function AssignmentAccordionItem({ week }: AssignmentAccordionItemProps) {
   )
 
   return (
-    <Accordion type="single" collapsible className="w-[1208px]">
+    <Accordion type="single" collapsible className="w-full">
       <AccordionItem value={week.toString()} className="mx-6 border-b-0">
         <AccordionTrigger
           className={cn(
-            'mt-4 flex rounded-2xl bg-white px-8 py-6 text-lg font-semibold shadow-md',
+            'mt-4 flex rounded-2xl bg-white px-8 py-4 text-lg font-semibold shadow-md',
             'border border-transparent',
             'data-[state=open]:border-primary data-[state=open]:-mb-6'
           )}
           iconSize="w-7 h-7"
         >
-          <div className="relative text-left">
+          <div className="relative text-left text-sm">
             <p className="text-primary absolute top-0 w-32 -translate-y-1/2">
               [Week {week}]
             </p>
@@ -158,36 +158,48 @@ function AssignmentAccordionItem({ week }: AssignmentAccordionItemProps) {
           <div className="overflow-hidden rounded-2xl border">
             <div className="h-6 bg-[#F8F8F8]" />
             {assignments.length > 0 ? (
-              <>
-                {assignments.map((assignment) => (
-                  <div
-                    key={assignment.id}
-                    className="flex gap-10 border-b bg-[#F8F8F8] p-4"
-                  >
-                    <p className="text-sm font-medium text-black">
-                      {assignment.title}
-                    </p>
-                    <p className="text-sm text-slate-500">
-                      {dateFormatter(
-                        assignment.startTime,
-                        'YYYY-MM-DD HH:mm:ss'
-                      )}
-                      {'      '}-{'      '}
-                      {dateFormatter(assignment.endTime, 'YYYY-MM-DD HH:mm:ss')}
-                    </p>
-                  </div>
-                ))}
-              </>
+              assignments.map((assignment) => (
+                <div
+                  key={assignment.id}
+                  className="flex items-center gap-10 border-b bg-[#F8F8F8] px-12 py-4"
+                >
+                  <span className="bg-primary h-2 w-2 rounded-full" />
+                  <p className="line-clamp-1 w-64 text-sm font-medium text-black">
+                    {assignment.title}
+                  </p>
+                  <p className="text-sm text-slate-500">
+                    {dateFormatter(assignment.startTime, 'YYYY-MM-DD HH:mm:ss')}{' '}
+                    - {dateFormatter(assignment.endTime, 'YYYY-MM-DD HH:mm:ss')}
+                  </p>
+                  <CountBadge
+                    solvedProblemCount={assignment.submittedProblemCount}
+                    problemCount={assignment.problemCount}
+                  />
+                </div>
+              ))
             ) : (
-              <div className="flex gap-10 border-b bg-[#F8F8F8] p-4">
-                <p className="text-sm font-medium text-black">
-                  과제가 없습니다
-                </p>
+              <div className="bg-[#F8F8F8] px-8 py-4">
+                <p className="text-sm text-slate-500">과제가 없습니다</p>
               </div>
             )}
           </div>
         </AccordionContent>
       </AccordionItem>
     </Accordion>
+  )
+}
+
+interface CountBadgeProps {
+  solvedProblemCount: number
+  problemCount: number
+}
+
+function CountBadge({ solvedProblemCount, problemCount }: CountBadgeProps) {
+  const bgColor =
+    solvedProblemCount === problemCount ? 'bg-primary' : 'bg-[#C4C4C4]'
+  return (
+    <div className={cn('rounded-full px-3 py-0.5 text-xs text-white', bgColor)}>
+      {solvedProblemCount}/{problemCount}
+    </div>
   )
 }
