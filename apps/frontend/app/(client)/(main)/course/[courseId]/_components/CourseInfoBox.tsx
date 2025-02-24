25787ec834a9809fbb0b2f6f12f30466452fef4b
b0xercat <seojin3154@naver.com>
2025-02-24 17:08:23 +0900

    feat(fe): modify CourseInfoBox ui

--- apps/frontend/app/(client)/(main)/course/[courseId]/_components/CourseInfoBox.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/_components/CourseInfoBox.tsx
@@ -1,9 +1,9 @@
 import { fetcherWithAuth } from '@/libs/utils'
-import QuarterEllipse from '@/public/icons/quarter-ellipse.svg'
+import calendarFillIcon from '@/public/icons/calendar-fill.svg'
+import ongoingIcon from '@/public/icons/ongoing.svg'
+import personFillIcon from '@/public/icons/person-fill.svg'
 import type { Course } from '@/types/type'
 import Image from 'next/image'
-import { BsPersonFill } from 'react-icons/bs'
-import { FaCalendar } from 'react-icons/fa'
 
 interface CourseInfoBoxProps {
   courseId: string
@@ -15,34 +15,38 @@ export async function CourseInfoBox({ courseId }: CourseInfoBoxProps) {
     const course: Course = await res.json()
 
     return (
-      <div className="relative m-4 flex h-[153px] w-[281px] flex-col justify-between rounded-xl p-7 shadow">
-        <div className="flex flex-col text-sm font-semibold">
-          <span>
-            [{`${course.courseInfo.courseNum}_${course.courseInfo.classNum}`}]
-          </span>
-          <span className="line-clamp-2">{course.groupName}</span>
+      <div className="flex flex-col gap-3 py-6">
+        <div className="flex gap-1">
+          <Image src={ongoingIcon} alt="calendar-fill" width={20} height={20} />
+          {/* FIXME: 하드코딩된 ONGOING 대신 데이터를 받아와주세요 */}
+          <p className="text-primary">ONGOING</p>
         </div>
-        <div>
-          <div className="flex flex-col gap-1 text-xs">
-            <div className="flex">
-              <FaCalendar className="ml-[2px]" />
-              <span className="ml-1 font-light">
-                {course.courseInfo.semester}
-              </span>
-            </div>
-            <div className="flex">
-              <BsPersonFill size={19} />
-              <span className="ml-1 font-light">
-                Prof. {course.courseInfo.professor}
-              </span>
-            </div>
+        <p className="line-clamp-2 w-52 text-base font-semibold">
+          {`[${course.courseInfo.courseNum}_${course.courseInfo.classNum}] ${course.groupName}`}
+        </p>
+        <div className="flex flex-col gap-2">
+          <div className="flex gap-2">
+            <Image
+              src={calendarFillIcon}
+              alt="calendar-fill"
+              width={16}
+              height={16}
+            />
+            <p className="font-medium text-neutral-600">
+              {course.courseInfo.semester}
+            </p>
           </div>
-          <Image
-            src={QuarterEllipse}
-            alt="Quarter Ellipse"
-            layout="intrinsic"
-            className="absolute bottom-0 right-0"
-          />
+          <div className="flex gap-2">
+            <Image
+              src={personFillIcon}
+              alt="person-fill"
+              width={16}
+              height={16}
+            />
+            <p className="font-medium text-neutral-600">
+              {course.courseInfo.professor} 교수
+            </p>
+          </div>
         </div>
       </div>
     )
