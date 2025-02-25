1fe8b4f2caf6df10a3e085e3166b910ad8c9d58b
b0xercat <seojin3154@naver.com>
2025-02-25 16:03:10 +0900

    chore(fe): move CourseInfo into CourseSidebar

--- apps/frontend/app/(client)/(main)/course/[courseId]/_components/CourseSidebar.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/_components/CourseSidebar.tsx
@@ -7,12 +7,13 @@ import noticeIcon from '@/public/icons/notice.svg'
 import qnaIcon from '@/public/icons/qna.svg'
 import Image from 'next/image'
 import Link from 'next/link'
+import { CourseInfoBox } from './CourseInfoBox'
 
-interface SidebarProps {
+interface CourseSidebarProps {
   courseId: string
 }
 
-export function Sidebar({ courseId }: SidebarProps) {
+export function CourseSidebar({ courseId }: CourseSidebarProps) {
   const navItems = [
     { name: 'Home', path: `/course/${courseId}` as const, icon: homeIcon },
     {
@@ -43,25 +44,29 @@ export function Sidebar({ courseId }: SidebarProps) {
   ]
 
   return (
-    <div className="flex flex-col gap-2">
-      {navItems.map((item) => (
-        <Link
-          key={item.name}
-          href={item.path}
-          className="rounded px-4 py-2 transition"
-        >
-          <Image
-            src={item.icon}
-            alt={item.name}
-            width={20}
-            className="mr-2 inline-block"
-          />
-          <span className="font-pretendard text-[1rem] font-bold leading-[1rem] text-[#21272A]">
-            {item.name}
-          </span>
-          <Separator className="mt-2" />
-        </Link>
-      ))}
+    <div className="flex flex-col">
+      <CourseInfoBox courseId={courseId} />
+      <Separator />
+      <nav className="flex flex-col gap-2">
+        {navItems.map((item) => (
+          <Link
+            key={item.name}
+            href={item.path}
+            className="rounded px-4 py-2 transition"
+          >
+            <Image
+              src={item.icon}
+              alt={item.name}
+              width={20}
+              className="mr-2 inline-block"
+            />
+            <span className="font-pretendard text-[1rem] font-bold leading-[1rem] text-[#21272A]">
+              {item.name}
+            </span>
+            <Separator className="mt-2" />
+          </Link>
+        ))}
+      </nav>
     </div>
   )
 }
--- apps/frontend/app/(client)/(main)/course/[courseId]/layout.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/layout.tsx
@@ -1,6 +1,5 @@
-import { CourseInfoBox } from './_components/CourseInfoBox'
+import { CourseSidebar } from './_components/CourseSidebar'
 import { Cover } from './_components/Cover'
-import { Sidebar } from './_components/Sidebar'
 
 interface CourseLayoutProps {
   children: React.ReactNode
@@ -13,11 +12,8 @@ export default function Layout({ children, params }: CourseLayoutProps) {
     <>
       <Cover title="COURSE" description="Check your course" />
       <div className="flex h-full w-full flex-col">
-        <div className="flex flex-row">
-          <nav className="w-auto border-r border-r-slate-200 bg-white p-2 px-6 pt-20 text-sm font-medium">
-            <CourseInfoBox courseId={courseId} />
-            <Sidebar courseId={courseId} />
-          </nav>
+        <div className="flex flex-row bg-red-50">
+          <CourseSidebar courseId={courseId} />
           <article className="w-full">
             <div>{children}</div>
           </article>
