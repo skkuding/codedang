9409b3ff7f833a4124a6c4cb66915d2e2823946e
b0xercat <seojin3154@naver.com>
2025-02-25 16:27:03 +0900

    chore: comment out unused link

--- apps/frontend/app/(client)/(main)/course/[courseId]/_components/CourseSidebar.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/_components/CourseSidebar.tsx
@@ -1,10 +1,10 @@
 import { Separator } from '@/components/shadcn/separator'
 import assignmentIcon from '@/public/icons/assignment.svg'
-import examIcon from '@/public/icons/exam.svg'
+// import examIcon from '@/public/icons/exam.svg'
 import gradeIcon from '@/public/icons/grade.svg'
-import homeIcon from '@/public/icons/home.svg'
-import noticeIcon from '@/public/icons/notice.svg'
-import qnaIcon from '@/public/icons/qna.svg'
+// import homeIcon from '@/public/icons/home.svg'
+// import noticeIcon from '@/public/icons/notice.svg'
+// import qnaIcon from '@/public/icons/qna.svg'
 import Image from 'next/image'
 import Link from 'next/link'
 import { CourseInfoBox } from './CourseInfoBox'
@@ -15,17 +15,17 @@ interface CourseSidebarProps {
 
 export function CourseSidebar({ courseId }: CourseSidebarProps) {
   const navItems = [
-    { name: 'Home', path: `/course/${courseId}` as const, icon: homeIcon },
-    {
-      name: 'Notice',
-      path: `/course/${courseId}/notice` as const,
-      icon: noticeIcon
-    },
-    {
-      name: 'Exam',
-      path: `/course/${courseId}/exam` as const,
-      icon: examIcon
-    },
+    // { name: 'Home', path: `/course/${courseId}` as const, icon: homeIcon },
+    // {
+    //   name: 'Notice',
+    //   path: `/course/${courseId}/notice` as const,
+    //   icon: noticeIcon
+    // },
+    // {
+    //   name: 'Exam',
+    //   path: `/course/${courseId}/exam` as const,
+    //   icon: examIcon
+    // },
     {
       name: 'Assignment',
       path: `/course/${courseId}/assignment` as const,
@@ -35,12 +35,12 @@ export function CourseSidebar({ courseId }: CourseSidebarProps) {
       name: 'Grade',
       path: `/course/${courseId}/grade` as const,
       icon: gradeIcon
-    },
-    {
-      name: 'Q&A',
-      path: `/course/${courseId}/qna` as const,
-      icon: qnaIcon
     }
+    // {
+    //   name: 'Q&A',
+    //   path: `/course/${courseId}/qna` as const,
+    //   icon: qnaIcon
+    // }
   ]
 
   return (
@@ -52,7 +52,7 @@ export function CourseSidebar({ courseId }: CourseSidebarProps) {
           <Link
             key={item.name}
             href={item.path}
-            className="flex gap-2 rounded-full bg-red-50 px-4 py-2"
+            className="flex gap-2 rounded-full px-4 py-2"
           >
             <Image src={item.icon} alt={item.name} width={16} height={16} />
             <span className="font-pretendard text-base font-medium text-[#474747]">
