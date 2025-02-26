a2030a64cdd194a8d6d58137d588ae490b704a8f
Jimin Ha <jimin9038@g.skku.edu>
2025-02-26 03:03:45 +0000

    fix(be): fix grade feature

--- apps/backend/prisma/migrations/20250225181924_add_grade_fields/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "assignment" ADD COLUMN     "auto_finalize_score" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "is_final_score_visible" BOOLEAN NOT NULL DEFAULT false;
-
--- AlterTable
-ALTER TABLE "assignment_problem_record" ADD COLUMN     "final_score" INTEGER NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "assignment_record" ADD COLUMN     "final_score" INTEGER NOT NULL DEFAULT 0;
