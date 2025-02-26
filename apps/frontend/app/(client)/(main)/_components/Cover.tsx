ff7dccbac656b9681563d979aa25c7ebb05faabd
SuminJeon <jeonsumin2002@gmail.com>
2025-02-26 17:12:43 +0900

    feat(fe): put banner letters into appropriate position

--- apps/frontend/app/(client)/(main)/_components/Cover.tsx
+++ apps/frontend/app/(client)/(main)/_components/Cover.tsx
@@ -27,36 +27,43 @@ const icons: { [key: string]: string } = {
  */
 export function Cover({ title, description }: CoverProps) {
   return (
-    <div className="mt-14 w-screen">
-      <div className="absolute left-0 top-0 z-[10] h-14 w-full bg-white" />
+    <div className="w-screen">
+      <div className="absolute left-0 top-0 z-[10] h-14 w-full" />
       <div
         className={cn(
           bgColors[title.toLowerCase()],
-          'z-[-10] flex h-[200px] w-full items-center justify-center'
+          'z-[-10] flex h-[344px] w-full justify-center'
         )}
       >
-        <Image
+        {/* <Image
           src={icons[title.toLowerCase()]}
           width={280}
           height={280}
           alt={title}
           className="max-md:hidden"
-        />
-        <div className="flex-col text-center md:px-20">
-          <h2 className="py-5 font-mono text-4xl font-bold text-white md:text-[56px]">
-            {title}
-          </h2>
-          <p className="whitespace-nowrap text-sm text-white/80 md:text-base">
-            {description}
-          </p>
+        /> */}
+        <div className="relative justify-center pb-[91px] pt-[157px] text-center md:w-[1440px] md:flex-col md:items-center md:pl-[240.12px] md:pr-[838px] md:text-left">
+          <Image
+            src={'/logos/codedang-rotated.svg'}
+            width={92.6}
+            height={70.14}
+            alt={title}
+            className="absolute top-1/3 md:left-[194px] md:top-[115]"
+          />
+          <div>
+            <h2 className="text-[50px] font-bold text-white">{title}</h2>
+            <p className="whitespace-nowrap text-xl font-medium text-white/80">
+              {description}
+            </p>
+          </div>
         </div>
-        <Image
+        {/* <Image
           src={icons[title.toLowerCase()]}
           width={280}
           height={280}
           alt={title}
           className="rotate-180 max-md:hidden"
-        />
+        /> */}
       </div>
       <div className="h-16 w-full bg-white" />
     </div>
