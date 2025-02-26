3320746f0c4cdd7da96bf97422f8d0f2be0f9de2
b0xercat <seojin3154@naver.com>
2025-02-26 22:24:47 +0900

    feat(fe): add CompleteBadge

--- apps/frontend/app/(client)/(main)/course/[courseId]/_components/AssignmentAccordion.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/_components/AssignmentAccordion.tsx
@@ -57,25 +57,38 @@ function AssignmentAccordionItem({
   const filteredAssignments = assignments.filter(
     (assignment) => assignment.week === week
   )
+  const isCompleted =
+    filteredAssignments.length === 0
+      ? false
+      : filteredAssignments.every((assignment) => {
+          return (
+            assignment.submittedNumber === assignment.problemNumber &&
+            assignment.problemNumber !== 0
+          )
+        })
 
   return (
     <Accordion type="single" collapsible className="w-full">
       <AccordionItem value={week.toString()} className="mx-6 border-b-0">
         <AccordionTrigger
           className={cn(
-            'mt-4 flex rounded-2xl bg-white px-8 py-4 text-lg font-semibold shadow-md',
+            'mt-4 flex rounded-2xl bg-white px-8 py-5 text-lg font-semibold shadow-md',
             'border border-transparent',
             'data-[state=open]:border-primary data-[state=open]:-mb-6'
           )}
           iconSize="w-7 h-7"
         >
-          <div className="relative text-left text-sm">
+          <div className="relative w-full text-left text-base">
             <p className="text-primary absolute top-0 w-32 -translate-y-1/2">
               [Week {week}]
             </p>
             <p className="absolute left-32 top-0 w-32 -translate-y-1/2">
               {week}주차
             </p>
+            <CompleteBadge
+              className="absolute right-4 top-0 -translate-y-1/2"
+              isCompleted={isCompleted}
+            />
           </div>
         </AccordionTrigger>
         <AccordionContent className="-mb-4">
@@ -137,6 +150,29 @@ function CountBadge({ solvedProblemCount, problemCount }: CountBadgeProps) {
   return (
     <div className={cn('rounded-full px-3 py-0.5 text-xs text-white', bgColor)}>
       {solvedProblemCount}/{problemCount}
+    </div>
+  )
+}
+
+interface CompleteBadgeProps {
+  className: string
+  isCompleted: boolean
+}
+
+function CompleteBadge({ className, isCompleted }: CompleteBadgeProps) {
+  const badgeStyle = isCompleted
+    ? 'border-primary text-primary'
+    : 'border-[#C4C4C4] text-[#C4C4C4]'
+  const text = isCompleted ? 'Complete' : 'Incomplete'
+  return (
+    <div
+      className={cn(
+        'flex h-[34px] w-[121px] items-center justify-center rounded-full border',
+        badgeStyle,
+        className
+      )}
+    >
+      <p className="text-[16px] font-medium">{text}</p>
     </div>
   )
 }
