8524e649410eaa218c70ed35dd4c3e5a68206e6a
b0xercat <seojin3154@naver.com>
2025-02-27 11:04:21 +0900

    chore: get courseId from params

--- apps/frontend/app/(client)/(main)/course/[courseId]/grade/page.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/grade/page.tsx
@@ -1,10 +1,18 @@
 import { GradeAccordion } from '../_components/GradeAccordion'
 
-export default function Grade() {
+interface GradeProps {
+  params: {
+    courseId: string
+  }
+}
+
+export default function Grade({ params }: GradeProps) {
+  const { courseId } = params
+
   return (
     <div className="mt-6 w-full px-6 py-4">
       <p className="text-2xl font-semibold">Grade</p>
-      <GradeAccordion courseId="2" />
+      <GradeAccordion courseId={courseId} />
     </div>
   )
 }
