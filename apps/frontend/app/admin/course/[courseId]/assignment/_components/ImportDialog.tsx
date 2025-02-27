0b89b7feeb75b37105f1e2b62ad175022d4cd0f5
Woojin Jung <44637040+jwoojin9@users.noreply.github.com>
2025-02-27 20:09:53 +0900

    feat(fe): create instructor assignment (#2428)
    * feat(fe): create instructor assignment
    
    * chore(fe): change contest variables to assignment
    
    * chore(fe): change error message
    
    * fix(fe): change edit assignment ui
    
    * feat(fe): add auto grading
    
    * feat(fe): add autograding feature

---
+++ apps/frontend/app/admin/course/[courseId]/assignment/_components/ImportDialog.tsx
@@ -0,0 +1,95 @@
+'use client'
+
+import { FetchErrorFallback } from '@/components/FetchErrorFallback'
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+  AlertDialogContent,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogCancel,
+  AlertDialogAction
+} from '@/components/shadcn/alert-dialog'
+import { Button } from '@/components/shadcn/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle
+} from '@/components/shadcn/dialog'
+import { ErrorBoundary } from '@suspensive/react'
+import { PlusCircleIcon } from 'lucide-react'
+import { Suspense, useState } from 'react'
+import type { AssignmentProblem } from '../_libs/type'
+import {
+  ImportProblemTable,
+  ImportProblemTableFallback
+} from './ImportProblemTable'
+
+interface ImportDialogProps {
+  problems: AssignmentProblem[]
+  setProblems: (problems: AssignmentProblem[]) => void
+}
+
+export function ImportDialog({ problems, setProblems }: ImportDialogProps) {
+  const [showImportDialog, setShowImportDialog] = useState(false)
+  return (
+    <>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button
+            type="button"
+            className="flex h-[36px] w-48 items-center gap-2 px-0"
+          >
+            <PlusCircleIcon className="h-4 w-4" />
+            <div className="mb-[2px] text-sm">Import · Edit problem</div>
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent className="p-8">
+          <AlertDialogHeader className="gap-2">
+            <AlertDialogTitle>Importing from Problem List</AlertDialogTitle>
+            {/* 정책 결정 후 문구 확정 예정 */}
+            <AlertDialogDescription>
+              If assignment problems are imported from the ‘All Problem List’,
+              the problems will automatically become{' '}
+              <span className="font-bold">invisible</span> state.
+              <br />
+              <br />
+              These problems will be shared with your course’s instructors.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className="rounded-md px-4 py-2">
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction asChild>
+              <Button type="button" onClick={() => setShowImportDialog(true)}>
+                Ok
+              </Button>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <Dialog open={showImportDialog} onOpenChange={setShowImportDialog}>
+        <DialogContent className="w-[1280px] max-w-[1280px]">
+          <DialogHeader>
+            <DialogTitle>Import Problem</DialogTitle>
+          </DialogHeader>
+          <ErrorBoundary fallback={FetchErrorFallback}>
+            <Suspense fallback={<ImportProblemTableFallback />}>
+              <ImportProblemTable
+                checkedProblems={problems}
+                onSelectedExport={(problems) => {
+                  setProblems(problems)
+                  setShowImportDialog(false)
+                }}
+              />
+            </Suspense>
+          </ErrorBoundary>
+        </DialogContent>
+      </Dialog>
+    </>
+  )
+}
---
+++ apps/frontend/app/admin/course/[courseId]/assignment/create/_components/CreateAssignmentForm.tsx
@@ -0,0 +1,128 @@
+'use client'
+
+import { useConfirmNavigationContext } from '@/app/admin/_components/ConfirmNavigation'
+import {
+  CREATE_ASSIGNMENT,
+  IMPORT_PROBLEMS_TO_ASSIGNMENT
+} from '@/graphql/assignment/mutations'
+// import { UPDATE_ASSIGNMENT_PROBLEMS_ORDER } from '@/graphql/problem/mutations'
+import { useMutation } from '@apollo/client'
+import type { CreateAssignmentInput } from '@generated/graphql'
+import { valibotResolver } from '@hookform/resolvers/valibot'
+import type { Route } from 'next'
+import { useRouter } from 'next/navigation'
+import type { ReactNode } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { createSchema } from '../../_libs/schemas'
+import type { AssignmentProblem } from '../../_libs/type'
+
+interface CreateAssignmentFormProps {
+  groupId: string
+  children: ReactNode
+  problems: AssignmentProblem[]
+  setIsCreating: (isCreating: boolean) => void
+}
+
+export function CreateAssignmentForm({
+  groupId,
+  children,
+  problems,
+  setIsCreating
+}: CreateAssignmentFormProps) {
+  const methods = useForm<CreateAssignmentInput>({
+    resolver: valibotResolver(createSchema),
+    defaultValues: {
+      isRankVisible: true,
+      isVisible: true,
+      enableCopyPaste: false,
+      isJudgeResultVisible: false
+    }
+  })
+
+  const { setShouldSkipWarning } = useConfirmNavigationContext()
+  const router = useRouter()
+
+  const [createAssignment, { error }] = useMutation(CREATE_ASSIGNMENT)
+  const [importProblemsToAssignment] = useMutation(
+    IMPORT_PROBLEMS_TO_ASSIGNMENT
+  )
+  // const [updateAssignmentProblemsOrder] = useMutation(
+  //   UPDATE_ASSIGNMENT_PROBLEMS_ORDER
+  // )
+
+  const isSubmittable = (input: CreateAssignmentInput) => {
+    if (input.startTime >= input.endTime) {
+      toast.error('Start time must be less than end time')
+      return
+    }
+
+    if (
+      new Set(problems.map((problem) => problem.order)).size !== problems.length
+    ) {
+      toast.error('Duplicate problem order found')
+      return
+    }
+    onSubmit()
+  }
+
+  const onSubmit = async () => {
+    const input = methods.getValues()
+    setIsCreating(true)
+
+    const { data } = await createAssignment({
+      variables: {
+        groupId: Number(groupId),
+        input
+      }
+    })
+
+    const assignmentId = Number(data?.createAssignment.id)
+
+    if (error) {
+      toast.error('Failed to create assignment')
+      setIsCreating(false)
+      return
+    }
+
+    await importProblemsToAssignment({
+      variables: {
+        groupId: Number(groupId),
+        assignmentId,
+        problemIdsWithScore: problems.map((problem) => {
+          return {
+            problemId: problem.id,
+            score: problem.score
+          }
+        })
+      }
+    })
+
+    // TODO: 백엔드 작업 완료되면 Assignment problem order 변경 기능 추가
+    // const orderArray = problems
+    //   .sort((a, b) => a.order - b.order)
+    //   .map((problem) => problem.id)
+
+    // await updateAssignmentProblemsOrder({
+    //   variables: {
+    //     groupId: 1,
+    //     assignmentId,
+    //     orders: orderArray
+    //   }
+    // })
+
+    setShouldSkipWarning(true)
+    toast.success('Assignment created successfully')
+    router.push(`/admin/course/${groupId}/assignment` as const)
+    router.refresh()
+  }
+
+  return (
+    <form
+      className="flex w-[760px] flex-col gap-6"
+      onSubmit={methods.handleSubmit(isSubmittable)}
+    >
+      <FormProvider {...methods}>{children}</FormProvider>
+    </form>
+  )
+}
