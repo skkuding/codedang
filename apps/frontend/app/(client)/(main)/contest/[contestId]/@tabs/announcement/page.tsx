fd7fff8ceb5ea3d51bce88153a93f5c292b092bb
SuminJeon <jeonsumin2002@gmail.com>
2025-02-27 17:01:12 +0900

    chore(fe): delete text-black

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/announcement/page.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/announcement/page.tsx
@@ -20,9 +20,7 @@ export default async function ContestAnnouncement({
     .json()
   return (
     <div className="pb-[120px]">
-      <p className="my-20 text-left text-2xl font-semibold text-black">
-        ANNOUNCEMENT
-      </p>
+      <p className="my-20 text-left text-2xl font-semibold">ANNOUNCEMENT</p>
       <DataTable
         data={contestAnnouncements}
         columns={columns}
