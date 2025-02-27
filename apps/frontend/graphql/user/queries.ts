c45407e67e3cf77b1da33c6b15aca010b1b18108
Jimin Ha <jimin9038@g.skku.edu>
2025-02-28 02:05:02 +0900

    fix(fe): little design fix

--- apps/frontend/graphql/user/queries.ts
+++ apps/frontend/graphql/user/queries.ts
@@ -17,15 +17,14 @@ const GET_GROUP_MEMBER =
 const GET_GROUP_MEMBERS = gql(`
   query GetGroupMembers($groupId: Int!, $cursor: Int, $take: Int!, $leaderOnly: Boolean!) {
     getGroupMembers(groupId: $groupId, cursor: $cursor, take: $take, leaderOnly: $leaderOnly) {
-  	username
-    userId
-    name
-    email
-    username
-    major
-    studentId
-    role
-    isGroupLeader
+      userId
+      name
+      email
+      username
+      major
+      studentId
+      role
+      isGroupLeader
     }
   }
 `)
