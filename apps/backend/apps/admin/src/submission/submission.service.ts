db9be05f2547665b39bb8038c16956107f1299e7
Kim Woojoo <117765232+w0uldy0u@users.noreply.github.com>
2025-02-28 00:14:01 +0900

    feat(be): get assignment submission by user id (#2437)
    * feat(be): add get assignment submission by userId
    
    * feat(be): include assignment data in submission detail

--- apps/backend/apps/admin/src/submission/submission.service.ts
+++ apps/backend/apps/admin/src/submission/submission.service.ts
@@ -203,6 +203,30 @@ export class SubmissionService {
     return results
   }
 
+  async getAssignmentSubmission(
+    assignmentId: number,
+    userId: number,
+    problemId: number
+  ) {
+    const submissionId = await this.prisma.submission.findFirst({
+      where: {
+        assignmentId,
+        userId,
+        problemId
+      },
+      orderBy: { updateTime: 'desc' },
+      select: {
+        id: true
+      }
+    })
+
+    if (!submissionId) {
+      throw new EntityNotExistException('Submission')
+    }
+
+    return this.getSubmission(submissionId.id)
+  }
+
   getOrderBy(
     order: SubmissionOrder
   ): Prisma.SubmissionOrderByWithRelationInput {
@@ -243,6 +267,7 @@ export class SubmissionService {
         },
         problem: true,
         contest: true,
+        assignment: true,
         submissionResult: true
       }
     })
