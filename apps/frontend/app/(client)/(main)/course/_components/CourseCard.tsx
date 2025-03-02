3d28aa1821d1cb7d4d2e09b2cbef098bbf1c9036
jwoojin9 <jwoojin9@naver.com>
2025-03-03 07:02:34 +0900

    chore(fe): change some gap

--- apps/frontend/app/(client)/(main)/course/_components/CourseCard.tsx
+++ apps/frontend/app/(client)/(main)/course/_components/CourseCard.tsx
@@ -18,27 +18,27 @@ export function CourseCard({ course, color }: CourseCardProps) {
   return (
     <div className="flex h-[300px] w-[310px] flex-col justify-between rounded-lg border border-gray-200 shadow-none">
       <div className={cn('h-[108px] rounded-t-lg', color)} />
-      <div className="flex h-[192px] w-full flex-col justify-between px-6 py-6">
+      <div className="flex h-[192px] w-full flex-col justify-between gap-3 px-6 pb-8 pt-[26px]">
         <StatusBadge variant={'ongoing'} />
-        <div className="my-1 line-clamp-1 h-6 w-[347px] text-ellipsis whitespace-pre-wrap text-lg font-semibold leading-tight text-black">
+        <div className="text-ellipsis whitespace-pre-wrap text-lg font-semibold leading-tight tracking-[-0.54px] text-black">
           [{course?.courseInfo?.courseNum}_{course?.courseInfo?.classNum}]{' '}
           {course.groupName}
         </div>
-        <div className="flex flex-col gap-1">
-          <div className="inline-flex items-center gap-2 whitespace-nowrap text-xs text-gray-800">
+        <div className="flex flex-col gap-1 pt-1">
+          <div className="inline-flex items-center gap-[14px] whitespace-nowrap">
             <Image src={calendarIcon} alt="calendar" width={16} height={16} />
-            <p className="my-2 text-sm font-medium text-[#8A8A8A]">
+            <p className="my-2 text-sm font-medium tracking-[-0.42px] text-[#8A8A8A]">
               {course?.courseInfo?.semester}
             </p>
           </div>
-          <div className="inline-flex items-center gap-2 whitespace-nowrap text-xs text-gray-800">
+          <div className="inline-flex items-center gap-[14px] whitespace-nowrap">
             <Image
               src={personFillIcon}
               alt="person-fill"
               width={16}
               height={16}
             />
-            <p className="text-sm font-medium text-[#8A8A8A]">
+            <p className="text-sm font-medium tracking-[-0.42px] text-[#8A8A8A]">
               Prof. {course?.courseInfo?.professor}{' '}
             </p>
           </div>
