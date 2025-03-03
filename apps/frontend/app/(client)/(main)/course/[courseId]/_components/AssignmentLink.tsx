e00fde8389cb1f35cdfb4d20e85566e9d5a0ae6b
Jimin Ha <jimin9038@g.skku.edu>
2025-03-03 15:50:10 +0900

    fix(fe): check if assignment is upcoming before participating an assignment

--- apps/frontend/app/(client)/(main)/course/[courseId]/_components/AssignmentLink.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/_components/AssignmentLink.tsx
@@ -1,9 +1,13 @@
 'use client'
 
 import { fetcherWithAuth, dateFormatter } from '@/libs/utils'
-import type { Assignment } from '@/types/type'
+import { cn } from '@/libs/utils'
+import type { Assignment, AssignmentStatus } from '@/types/type'
+import dayjs from 'dayjs'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import { useInterval } from 'react-use'
 import { toast } from 'sonner'
 import { CountBadge } from './AssignmentAccordion'
 
@@ -14,16 +18,34 @@ interface AssignmentLinkProps {
 
 export function AssignmentLink({ assignment, courseId }: AssignmentLinkProps) {
   const router = useRouter()
+  const [assignmentStatus, setAssignmentStatus] =
+    useState<AssignmentStatus>('upcoming')
+
+  const updateAssignmentStatus = () => {
+    // TODO: change to use server date
+    const now = dayjs()
+    if (now.isAfter(assignment.endTime)) {
+      setAssignmentStatus('finished')
+    } else if (now.isAfter(assignment.startTime)) {
+      setAssignmentStatus('ongoing')
+    } else {
+      setAssignmentStatus('upcoming')
+    }
+  }
+
+  useEffect(() => {
+    updateAssignmentStatus()
+  }, [])
+
+  useInterval(() => {
+    updateAssignmentStatus()
+  }, 1000)
 
   const handleClick = async (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault()
 
-    // TODO: should use server date !!
-    const now = new Date()
-    const startTime = new Date(assignment.startTime)
-
-    if (now < startTime) {
-      toast.error('Cannot participate in upcoming assignment')
+    if (assignmentStatus === 'upcoming') {
+      toast.error('This assignment is not started yet!')
       return
     }
 
@@ -49,12 +71,31 @@ export function AssignmentLink({ assignment, courseId }: AssignmentLinkProps) {
     >
       <div className="flex items-center justify-between border-b bg-[#F8F8F8] px-12 py-6">
         <div className="flex gap-6">
-          <span className="bg-primary mt-[7px] h-[10px] w-[10px] rounded-full" />
+          <span
+            className={cn(
+              'mt-[7px] h-[10px] w-[10px] rounded-full',
+              assignmentStatus === 'upcoming' ? 'bg-[#C4C4C4]' : 'bg-primary'
+            )}
+          />
           <div className="flex flex-col gap-[6px]">
-            <p className="line-clamp-1 w-96 text-base font-medium text-black">
+            <p
+              className={cn(
+                'line-clamp-1 w-96 text-base font-medium',
+                assignmentStatus === 'upcoming'
+                  ? 'text-neutral-500'
+                  : 'text-black'
+              )}
+            >
               {assignment.title}
             </p>
-            <p className="text-base text-slate-500">
+            <p
+              className={cn(
+                'text-base',
+                assignmentStatus === 'upcoming'
+                  ? 'text-neutral-500'
+                  : 'text-slate-500'
+              )}
+            >
               {dateFormatter(assignment.startTime, 'YYYY-MM-DD HH:mm:ss')} -{' '}
               {dateFormatter(assignment.endTime, 'YYYY-MM-DD HH:mm:ss')}
             </p>
