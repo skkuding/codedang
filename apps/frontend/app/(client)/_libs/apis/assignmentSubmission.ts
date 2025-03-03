f95bec3e0bbf1e7d587a7a68fe11fb937635c40a
Mingyu Seong <112471084+mingyu27@users.noreply.github.com>
2025-03-04 00:05:24 +0900

    feat(fe): create course detail grade assignment modal (#2431)
    * fix(fe): udpate dependencies
    
    * feat(fe): integrate dialog with grade accordion
    
    * chore(fe): remove unused stuff
    
    * chore(fe): rebuild pnpm-lock file
    
    * fix(fe): handle error from creating course
    
    * feat(fe): integrate test case result api
    
    * fix(fe): remove 'use client'
    
    * feat(fe): integrate api
    
    * chore(fe): apply comments
    
    * chore(fe): modify separator color

--- apps/frontend/app/(client)/_libs/apis/assignmentSubmission.ts
+++ apps/frontend/app/(client)/_libs/apis/assignmentSubmission.ts
@@ -47,3 +47,153 @@ export const getAssignmentSubmissionDetail = async ({
   const data = await response.json<SubmissionDetail>()
   return data
 }
+
+interface AnonymizedScore {
+  assignmentId: number
+  title: string
+  totalParticipants: number
+  autoFinalizeScore: boolean
+  isFinalScoreVisible: boolean
+  scores?: number[]
+  finalScores?: number[]
+}
+
+export interface GetAnonymizedScoresRequest {
+  assignmentId: number
+  courseId: number
+}
+
+export const getAnonymizedScores = async ({
+  assignmentId,
+  courseId
+}: GetAnonymizedScoresRequest) => {
+  const response = await safeFetcherWithAuth.get(
+    `assignment/${assignmentId}/score`,
+    {
+      searchParams: { anonymized: true, groupId: courseId }
+    }
+  )
+
+  const data = await response.json<AnonymizedScore>()
+  console.log('Fetched data:', data)
+  return data
+}
+
+export interface GetProblemSubmissionResultsRequest {
+  assignmentId: number
+  problemId: number
+}
+
+export interface ProblemSubmissionResultsResponse {
+  data: ProblemSubmissionResult[]
+  total: number
+}
+export interface ProblemSubmissionResult {
+  id: number
+  user: {
+    username: string
+  }
+  createTime: string
+  language: string
+  result: string
+  codeSize: number
+}
+
+export const getProblemSubmissionResults = async ({
+  assignmentId,
+  problemId
+}: GetProblemSubmissionResultsRequest) => {
+  const response = await safeFetcherWithAuth.get(
+    `assignment/${assignmentId}/submission`,
+    {
+      searchParams: { problemId }
+    }
+  )
+
+  const data = await response.json<ProblemSubmissionResultsResponse>()
+  console.log('Fetched data:', data)
+  return data
+}
+
+export interface GetTestResultRequest {
+  assignmentId: number
+  problemId: number
+  submissionId: number
+}
+
+export interface SubmissionResponse {
+  problemId: number
+  username: string
+  code: string
+  language: string
+  createTime: string
+  result: string
+  testcaseResult: TestcaseResult[]
+}
+export interface TestcaseResult {
+  id: number
+  submissionId: number
+  problemTestcaseId: number
+  result: string
+  cpuTime: number | null
+  memoryUsage: number | null
+  createTime: string
+  updateTime: string
+}
+
+export const getTestResult = async ({
+  assignmentId,
+  problemId,
+  submissionId
+}: GetTestResultRequest) => {
+  const response = await safeFetcherWithAuth.get(`submission/${submissionId}`, {
+    searchParams: { problemId, assignmentId }
+  })
+
+  const data = await response.json<SubmissionResponse>()
+  console.log('Fetched data:', data)
+  return data
+}
+
+export interface GetAssignmentGradesRequest {
+  groupId: number
+}
+
+export type GetAssignmentGradesResponse = AssignmentGrade[]
+
+export interface AssignmentGrade {
+  id: number
+  title: string
+  endTime: string
+  autoFinalizeScore: boolean
+  isFinalScoreVisible: boolean
+  isJudgeResultVisible: boolean
+  week: number
+  userAssignmentFinalScore: number | null
+  userAssignmentJudgeScore: number | null
+  assignmentPerfectScore: number
+  problems: ProblemGrade[]
+}
+export interface ProblemGrade {
+  id: number
+  title: string
+  order: number
+  maxScore: number
+  problemRecord: ProblemRecord | null
+}
+
+export interface ProblemRecord {
+  finalScore: number
+  score: number
+  isSubmitted: boolean
+  comment: string
+}
+
+export const getAssignmentGrades = async ({
+  groupId
+}: GetAssignmentGradesRequest) => {
+  const response = await safeFetcherWithAuth.get(`course/${groupId}/grade`)
+  const data = await response.json<GetAssignmentGradesResponse>()
+  console.log('Fetched data:', data)
+  return data
+}
