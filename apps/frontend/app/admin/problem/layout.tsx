be062ca80bdf1172c231d008a18358e2af22fc67
Jimin Ha <jimin9038@g.skku.edu>
2025-03-04 12:40:47 +0900

    chore(fe): remove unused codes

--- apps/frontend/app/admin/problem/layout.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { ProblemTabs } from './_components/ProblemTabs'
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="flex h-dvh flex-col bg-neutral-50">{children}</div>
-}
--- apps/frontend/app/admin/problem/page.tsx
+++ apps/frontend/app/admin/problem/page.tsx
@@ -2,38 +2,15 @@
 
 import { FetchErrorFallback } from '@/components/FetchErrorFallback'
 import { Button } from '@/components/shadcn/button'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogClose
-} from '@/components/shadcn/dialog'
 import { ErrorBoundary } from '@suspensive/react'
 import { PlusCircleIcon } from 'lucide-react'
 import Link from 'next/link'
-import { useState, useEffect, Suspense } from 'react'
+import { Suspense } from 'react'
 import { ProblemTable, ProblemTableFallback } from './_components/ProblemTable'
 import { ProblemTabs } from './_components/ProblemTabs'
 import { UploadDialog } from './_components/UploadDialog'
 
-export default function Page({
-  searchParams
-}: {
-  searchParams: { import: boolean | undefined }
-}) {
-  const [openDialog, setOpenDialog] = useState<boolean>(false)
-
-  useEffect(() => {
-    if (searchParams.import) {
-      setOpenDialog(true)
-    }
-  }, [searchParams.import])
-
-  const importProblem = searchParams.import
-
+export default function Page() {
   return (
     <div className="container mx-auto py-10">
       <div className="flex justify-between">
@@ -43,17 +20,15 @@ export default function Page({
             Here&apos;s a list you made
           </p>
         </div>
-        {importProblem ? null : (
-          <div className="flex gap-2">
-            <UploadDialog />
-            <Link href="/admin/problem/create">
-              <Button variant="default">
-                <PlusCircleIcon className="mr-2 h-4 w-4" />
-                Create
-              </Button>
-            </Link>
-          </div>
-        )}
+        <div className="flex gap-2">
+          <UploadDialog />
+          <Link href="/admin/problem/create">
+            <Button variant="default">
+              <PlusCircleIcon className="mr-2 h-4 w-4" />
+              Create
+            </Button>
+          </Link>
+        </div>
       </div>
       <ProblemTabs />
       <ErrorBoundary fallback={FetchErrorFallback}>
