a070a55d861471fe266e0bc618788c107d5ee242
Sumin Jeon <jeonsumin2002@gmail.com>
2025-03-22 22:05:14 +0900

    feat(fe): put contest Update History

--- apps/frontend/app/admin/contest/[contestId]/(overall)/@tabs/announcement/page.tsx
+++ apps/frontend/app/admin/contest/[contestId]/(overall)/@tabs/announcement/page.tsx
@@ -13,7 +13,9 @@ import {
 } from '@/components/shadcn/select'
 import { Textarea } from '@/components/shadcn/textarea'
 import { CREATE_CONTEST_ANNOUNCEMENT } from '@/graphql/contest/mutations'
+import { GET_CONTEST_UPDATE_HISTORIES } from '@/graphql/contest/queries'
 import { GET_CONTEST_PROBLEMS } from '@/graphql/problem/queries'
+import { convertToLetter, dateFormatter } from '@/libs/utils'
 import { useApolloClient, useMutation, useQuery } from '@apollo/client'
 import type { CreateAnnouncementInput } from '@generated/graphql'
 import { valibotResolver } from '@hookform/resolvers/valibot'
@@ -21,6 +23,7 @@ import { usePathname } from 'next/navigation'
 import { useState } from 'react'
 import { useForm, type SubmitHandler } from 'react-hook-form'
 import { toast } from 'sonner'
+import { number } from 'valibot'
 import { ConfirmNavigation } from '../../../../../_components/ConfirmNavigation'
 import { ErrorMessage } from '../../../../../_components/ErrorMessage'
 
@@ -31,6 +34,9 @@ export default function Page() {
   const client = useApolloClient()
   const [announcetext, setAnnouncetext] = useState<string>('')
   const { data: problemData } = useQuery(GET_CONTEST_PROBLEMS, {
+    variables: { contestId: contestId }
+  })
+  const { data: updateHistory } = useQuery(GET_CONTEST_UPDATE_HISTORIES, {
     variables: { contestId: contestId }
   })
   console.log('problemData:', problemData)
@@ -73,6 +79,35 @@ export default function Page() {
       <ScrollArea className="w-full">
         <main className="flex flex-col gap-6 px-20 py-16">
           <div>Update History</div>
+          <div className="w-full border">
+            {!updateHistory?.getContestUpdateHistories ||
+              (updateHistory.getContestUpdateHistories.updateHistories
+                .length === 0 && <p>no result.</p>)}
+            {updateHistory?.getContestUpdateHistories.updateHistories.map(
+              (history) => (
+                <div key={history.updatedAt}>
+                  <div className="flex">
+                    <p>
+                      Problem{' '}
+                      {history.order !== null
+                        ? convertToLetter(Number(history.order))
+                        : ''}
+                      {' : '}
+                    </p>
+                    <p> </p>
+                    <div className="flex">
+                      {history.updatedInfo.map((current, index) => (
+                        <p key={current.updatedField}>
+                          {current.current}
+                          {index < history.updatedInfo.length - 1 && ' & '}
+                        </p>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
           <form onSubmit={handleSubmit(onSubmitAnnouncement)}>
             <p>Post New Announcement</p>
             <div className="flex flex-col gap-2">
@@ -117,6 +152,7 @@ export default function Page() {
               {errors.content && <ErrorMessage />}
             </div>
             <Button type="submit">Submit</Button>
+            <p>Posted announcement cannot be edited.</p>
           </form>
         </main>
       </ScrollArea>
