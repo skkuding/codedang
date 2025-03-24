47d991f369a55b3beb8fe7a0a3365ba0731140d5
hyuntori <hyuntori@hyuntori.local>
2025-03-24 19:20:52 +0900

    feat(fe): add leaderboard freeze component (only html with css)

--- apps/frontend/app/admin/contest/[contestId]/(overall)/@tabs/leaderboard/page.tsx
+++ apps/frontend/app/admin/contest/[contestId]/(overall)/@tabs/leaderboard/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Input } from '@/components/shadcn/input'
+import { Switch } from '@/components/shadcn/switch'
 import { GET_CONTEST_LEADERBOARD } from '@/graphql/leaderboard/queries'
 import SearchIcon from '@/public/icons/search.svg'
 import { useSuspenseQuery } from '@apollo/client'
@@ -54,27 +55,47 @@ export default function ContestLeaderBoard() {
   }, [data])
 
   return (
-    <div className="relative ml-[116px] w-screen pb-[120px]">
-      <div className="relative mb-[62px] mt-[30px]">
-        <Image
-          src={SearchIcon}
-          alt="search"
-          className="absolute left-5 top-1/2 -translate-y-1/2 cursor-pointer"
-          onClick={() => {
-            handleSearch(searchText)
-          }}
-        />
-        <Input
-          placeholder="Search"
-          className="w-[600px] pl-[52px] text-[18px] font-normal"
-          onChange={(e) => setSearchText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              handleSearch(searchText)
-            }
-          }}
-        />
+    <div className="relative mt-9 w-screen pb-[120px]">
+      <div className="h-[93px] w-[1002px] rounded-xl border border-[#619CFB] pl-[30px] pt-[21px]">
+        <div className="flex flex-row">
+          <div className="mr-3 text-xl font-semibold text-[#3581FA]">
+            Unfreeze Leaderboard
+          </div>
+          <Switch
+            className="h-[24px] w-[46px]"
+            thumbClassName="w-[18px] h-[18px] data-[state=checked]:translate-x-[22px] data-[state=unchecked]:translate-x-[2px]"
+          />
+        </div>
+        <div className="font-[14px] font-normal text-[#9B9B9B]">
+          The leaderboard can only be unfrozen after the contest has finished.
+        </div>
       </div>
+      <div className="mb-[62px] mt-[60px] flex flex-row">
+        <div className="mr-[167px] text-2xl font-semibold text-black">
+          83/89 Participants
+        </div>
+        <div className="relative">
+          <Image
+            src={SearchIcon}
+            alt="search"
+            className="absolute left-5 top-1/2 -translate-y-1/2 cursor-pointer"
+            onClick={() => {
+              handleSearch(searchText)
+            }}
+          />
+          <Input
+            placeholder="Search"
+            className="w-[600px] pl-[52px] text-[18px] font-normal"
+            onChange={(e) => setSearchText(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleSearch(searchText)
+              }
+            }}
+          />
+        </div>
+      </div>
+
       <div>
         <LeaderboardTable
           problemSize={problemSize}
