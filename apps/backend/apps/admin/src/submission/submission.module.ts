8c483d43148d62e635c6c7bef7cc2656d7b21748
RyuRaseul <ryudb00@g.skku.edu>
2025-03-30 07:15:09 +0000

    chore(be): remove storage service from submission module

--- apps/backend/apps/admin/src/submission/submission.module.ts
+++ apps/backend/apps/admin/src/submission/submission.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
 import { RolesModule } from '@libs/auth'
-import { StorageModule } from '@admin/storage/storage.module'
 import { SubmissionResolver } from './submission.resolver'
 import { SubmissionService } from './submission.service'
 
 @Module({
-  imports: [RolesModule, StorageModule],
+  imports: [RolesModule],
   providers: [SubmissionResolver, SubmissionService]
 })
 export class SubmissionModule {}
