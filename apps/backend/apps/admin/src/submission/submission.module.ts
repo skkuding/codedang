139c952415a449a1341dc9f833c6a95a53013229
RyuRaseul <ryudb00@g.skku.edu>
2025-03-30 03:16:56 +0000

    feat(be): add storageService to submission module

--- apps/backend/apps/admin/src/submission/submission.module.ts
+++ apps/backend/apps/admin/src/submission/submission.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
 import { RolesModule } from '@libs/auth'
+import { StorageModule } from '@admin/storage/storage.module'
 import { SubmissionResolver } from './submission.resolver'
 import { SubmissionService } from './submission.service'
 
 @Module({
-  imports: [RolesModule],
+  imports: [RolesModule, StorageModule],
   providers: [SubmissionResolver, SubmissionService]
 })
 export class SubmissionModule {}
