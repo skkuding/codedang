d1e0d965e6fff160b24c1de7732463130f135b51
Bang Seunghyeon <144550420+HypePenguin@users.noreply.github.com>
2025-03-31 17:22:16 +0900

    feat(fe): add leaderboard freeze on code editor (#2624)
    * fix(fe): change css
    
    * feat(fe): add freeze tooltip in leaderboard
    
    * feat(fe): add bottom-center icon
    
    * fix(fe): change type dependency
    
    * feat(fe): test
    
    * feat(fe): add leaderboard freeze on code editor and simple css modification
    
    ---------
    
    Co-authored-by: hyuntori <hyuntori@hyuntoriui-MacBookPro.local>
    Co-authored-by: hyuntori <hyuntori@hyuntori.local>

--- apps/frontend/types/type.ts
+++ apps/frontend/types/type.ts
@@ -152,6 +152,7 @@ export interface Contest {
   enableCopyPaste: boolean
   status: ContestStatus
   participants: number
+  freezeTime?: Date
   isRegistered: boolean
   contestProblem: ProblemInContestInterface[]
 }
