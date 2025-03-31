662dc2b7bb61d0961cdf3893a0f04294a0deaf41
강민석 <min.seok@g.skku.edu>
2025-03-31 22:29:08 +0900

    feat(be): contest management pannel  access (#2623)
    * fix(be): make userContestRoles get only userContest
    
    * feat(fe): contest roles
    
    * fix(be): test
    
    * docs(be): request script admin
    
    * feat(be): admin getUserRoles
    
    * docs(be): get contest roles admin
    
    * feat(fe): delete restapi of frontend
    
    ---------
    
    Co-authored-by: Clover229 <jeonsumin2002@gmail.com>

--- apps/backend/apps/admin/src/contest/contest.resolver.ts
+++ apps/backend/apps/admin/src/contest/contest.resolver.ts
@@ -1,5 +1,5 @@
 import { Args, Context, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
-import { Contest, ContestProblem } from '@generated'
+import { Contest, ContestProblem, UserContest } from '@generated'
 import { ContestRole } from '@prisma/client'
 import {
   AuthenticatedRequest,
@@ -173,5 +173,11 @@ export class ContestResolver {
     @Args('contestId', { type: () => Int }) contestId: number
   ) {
     return await this.contestService.getContestUpdateHistories(contestId)
+  }
+
+  @Query(() => [UserContest])
+  @UseDisableContestRolesGuard()
+  async getContestRoles(@Context('req') req: AuthenticatedRequest) {
+    return await this.contestService.getContestRoles(req.user.id)
   }
 }
