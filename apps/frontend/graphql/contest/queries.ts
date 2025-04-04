22d292f80de148e38fbd67fdc698e041fc229729
b0xercat <seojin3154@naver.com>
2025-04-04 17:49:13 +0900

    fix(fe): replace get users with get contest userContest

--- apps/frontend/graphql/contest/queries.ts
+++ apps/frontend/graphql/contest/queries.ts
@@ -18,6 +18,13 @@ const GET_CONTEST = gql(`
       userContest {
         userId
         role
+        user {
+          username
+          email
+          userProfile {
+            realName
+          }
+        }
       }
     }
   }
