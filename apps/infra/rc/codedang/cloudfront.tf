63de15971cc610ba7e398a944c8e6714ab563e5f
Jiwon <124144606+jiwonly@users.noreply.github.com>
2025-04-05 10:01:00 +0900

    feat: add rc amplify terraform (#2670)

--- apps/infra/rc/codedang/cloudfront.tf
+++ apps/infra/rc/codedang/cloudfront.tf
@@ -12,11 +12,8 @@ data "aws_cloudfront_origin_request_policy" "exclude_host_header" {
 
 resource "aws_cloudfront_distribution" "codedang" {
   origin {
-    #TODO : RC서버 Amplify 문제 해결
-
-    domain_name = var.env == "production" ? "amplify.codedang.com" : "main.d6pfxg2cc9yvf.amplifyapp.com"
-
-    origin_id = "frontend" # TODO: Add unique ID of Amplify
+    domain_name = "main.d6vf61i67dhk2.amplifyapp.com"
+    origin_id = "d6vf61i67dhk2"
 
     custom_origin_config {
       http_port              = 80
