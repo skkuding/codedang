f706774b7527a40e88148da2565740808e3a29c6
강민석 <min.seok@g.skku.edu>
2025-04-10 00:34:40 +0900

    feat(be): remove group id from notice (#2654)
    * feat(be): remove groupId from notice
    
    * fix(be): remove groupId from controller
    
    * chore(be): remove unused import
    
    * fix(fe): remove groupId from notice
    
    * chore(be): test

--- apps/backend/apps/admin/src/notice/notice.resolver.ts
+++ apps/backend/apps/admin/src/notice/notice.resolver.ts
@@ -9,20 +9,15 @@ import {
   ResolveField,
   Parent
 } from '@nestjs/graphql'
-import { Group, Notice, User } from '@generated'
-import { AuthenticatedRequest, UseGroupLeaderGuard } from '@libs/auth'
-import {
-  CursorValidationPipe,
-  IDValidationPipe,
-  NullableGroupIDPipe
-} from '@libs/pipe'
+import { Notice, User } from '@generated'
+import { AuthenticatedRequest } from '@libs/auth'
+import { CursorValidationPipe, IDValidationPipe } from '@libs/pipe'
 import { GroupService } from '@admin/group/group.service'
 import { UserService } from '@admin/user/user.service'
 import { CreateNoticeInput, UpdateNoticeInput } from './model/notice.input'
 import { NoticeService } from './notice.service'
 
 @Resolver(() => Notice)
-@UseGroupLeaderGuard()
 export class NoticeResolver {
   private readonly logger = new Logger(NoticeResolver.name)
   constructor(
@@ -34,51 +29,41 @@ export class NoticeResolver {
   @Mutation(() => Notice)
   async createNotice(
     @Args('input') input: CreateNoticeInput,
-    @Args('groupId', { type: () => Int, nullable: true }, NullableGroupIDPipe)
-    groupId: number | null,
     @Context('req') req: AuthenticatedRequest
   ) {
-    return await this.noticeService.createNotice(req.user.id, groupId, input)
+    return await this.noticeService.createNotice(req.user.id, input)
   }
 
   @Mutation(() => Notice)
   async deleteNotice(
-    @Args('groupId', { type: () => Int, nullable: true }, NullableGroupIDPipe)
-    groupId: number | null,
     @Args('noticeId', { type: () => Int }, IDValidationPipe) noticeId: number
   ) {
-    return await this.noticeService.deleteNotice(groupId, noticeId)
+    return await this.noticeService.deleteNotice(noticeId)
   }
 
   @Mutation(() => Notice)
   async updateNotice(
-    @Args('groupId', { type: () => Int, nullable: true }, NullableGroupIDPipe)
-    groupId: number | null,
     @Args('noticeId', { type: () => Int }, IDValidationPipe) noticeId: number,
     @Args('input') input: UpdateNoticeInput
   ) {
-    return await this.noticeService.updateNotice(groupId, noticeId, input)
+    return await this.noticeService.updateNotice(noticeId, input)
   }
 
   @Query(() => Notice)
   async getNotice(
-    @Args('groupId', { type: () => Int, nullable: true }, NullableGroupIDPipe)
-    groupId: number | null,
     @Args('noticeId', { type: () => Int }, IDValidationPipe) noticeId: number
   ) {
-    return await this.noticeService.getNotice(groupId, noticeId)
+    return await this.noticeService.getNotice(noticeId)
   }
 
   @Query(() => [Notice], { nullable: 'items' })
   async getNotices(
-    @Args('groupId', { type: () => Int, nullable: true }, NullableGroupIDPipe)
-    groupId: number | null,
     @Args('cursor', { type: () => Int, nullable: true }, CursorValidationPipe)
     cursor: number | null,
     @Args('take', { type: () => Int, defaultValue: 10 })
     take: number
   ) {
-    return await this.noticeService.getNotices(groupId, cursor, take)
+    return await this.noticeService.getNotices(cursor, take)
   }
 
   @ResolveField('createdBy', () => User, { nullable: true })
@@ -88,14 +73,5 @@ export class NoticeResolver {
       return null
     }
     return this.userService.getUser(createdById)
-  }
-
-  @ResolveField('group', () => Group, { nullable: true })
-  async getGroup(@Parent() notice: Notice) {
-    const { groupId } = notice
-    if (groupId == null) {
-      return null
-    }
-    return this.groupService.getGroupById(groupId)
   }
 }
