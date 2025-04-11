f2b647a9896c8a37824e60e4921c88bffa34ca6e
junhyunlee123 <162781941+junhyunlee123@users.noreply.github.com>
2025-04-11 14:07:37 +0900

    fix(infra): fix acm validation workflow (#2695)
    * fix(infra): workflow fix
    
    * fix(infra): fix2
    
    * fix(infra): fix rc acm validation workflow3
    
    * fix(infra): fix acm val
    
    * fix(infra): fix acm val
    
    * fix(infra): fix acm validation
    
    * fix(infra): fix acm val
    
    * fix(infra): fix acm val
    
    * feat(infra): fix
    
    * feat(infra): fix
    
    * fix(infra): fix
    
    * feat(infra): fix
    
    * fix(infra): fix acm validation workflow

--- apps/infra/rc/acm-validation/main.tf
+++ apps/infra/rc/acm-validation/main.tf
@@ -1,21 +1,68 @@
 # AWS provider for US East 1 (required for ACM)
-provider "aws" {
-  region = "ap-northeast-2"
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.82"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
+  }
+
+  backend "s3" {
+    bucket         = "codedang-tf-state"
+    key            = "terraform/storage.tfstate"
+    region         = "ap-northeast-2"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
 }
 
-# Data source to reference the existing ACM certificate
-data "aws_acm_certificate" "codedang" {
-  provider = aws.us_east_1
-  domain   = "rc.codedang.com"
-  statuses = ["ISSUED", "PENDING_VALIDATION"]
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+data "aws_route53_zone" "main" {
+  name = "rc.codedang.com"
+}
+
+resource "aws_acm_certificate" "codedang" {
+  count       = var.env == "rc" ? 1 : 0
+  domain_name = "rc.codedang.com"
+
+  validation_method = "DNS"
+  provider          = aws.us_east_1
+}
+
+resource "aws_route53_record" "certificate" {
+
+  for_each = {
+    for dvo in aws_acm_certificate.codedang[0].domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      zone_id = data.aws_route53_zone.main.zone_id
+      type    = dvo.resource_record_type
+      value   = dvo.resource_record_value
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.value]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = each.value.zone_id
 }
 
 # Certificate validation
 resource "aws_acm_certificate_validation" "for_all_domains" {
   count           = var.env == "rc" ? 1 : 0
   provider        = aws.us_east_1
-  certificate_arn = data.aws_acm_certificate.codedang.arn
+  certificate_arn = aws_acm_certificate.codedang[0].arn
   validation_record_fqdns = [
-    for dvo in data.aws_acm_certificate.codedang.domain_validation_options : dvo.resource_record_name
+    for record in aws_route53_record.certificate : record.fqdn
   ]
 }
