ae1385dc05550d0d7bc923ef3b0132decc38332b
Jaemin Choi <1dotolee@gmail.com>
2025-04-16 22:44:02 +0900

    feat(infra): parse logs with fluent-bit json parser

--- apps/iris/src/loader/postgres.go
+++ apps/iris/src/loader/postgres.go
@@ -15,22 +15,20 @@ type Postgres struct {
 	client *sql.DB
 }
 
-func NewPostgresDataSource(ctx context.Context) *Postgres {
+func NewPostgresDataSource(ctx context.Context) (*Postgres, error) {
 	// 새로운 ENV 추가 필요
 	connStr := os.Getenv("DATABASE_URL")
 	data := strings.Replace(connStr, "schema=public", "sslmode=disable", 1)
 	db, err := sql.Open("postgres", data)
 
 	if err != nil {
-		panic(fmt.Errorf("cannot access database: %w", err))
+		return nil, fmt.Errorf("failed to access database: %w", err)
 	}
 
-	return &Postgres{ctx, db}
+	return &Postgres{ctx, db}, nil
 }
 
 func (p *Postgres) Get(key string) ([]Element, error) {
-	// logProvider := logger.NewLogger(logger.Console, false)
-
 	rows, err := p.client.Query(`SELECT id, input, output, is_hidden_testcase FROM public.problem_testcase WHERE problem_id = $1`, key)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get key: %w", err)
