2984762e8332a1d61e92912f5855486963054fca
GalaxyBook2 <manamana32321@gmail.com>
2025-04-25 02:30:59 +0900

    fix(infra): normalize header keys to lowercase in convertHeaderCarrierToTable

--- apps/iris/src/connector/rabbitmq/converter.go
+++ apps/iris/src/connector/rabbitmq/converter.go
@@ -2,21 +2,25 @@ package rabbitmq
 
 import (
 	"net/http"
+	"strings"
 
 	amqp "github.com/rabbitmq/amqp091-go"
 	"go.opentelemetry.io/otel/propagation"
 )
+
+// TODO: 좀 더 Semantic한 방식으로 변환
 
 // convertHeaderCarrierToTable converts an OpenTelemetry HeaderCarrier to a RabbitMQ Table.
 func convertHeaderCarrierToTable(carrier propagation.HeaderCarrier) amqp.Table {
 	headers := amqp.Table{}
 	httpHeaders := http.Header(carrier)
 	for key, values := range httpHeaders {
+		lowerKey := strings.ToLower(key)
 		if len(values) > 0 {
 			if len(values) == 1 {
-				headers[key] = values[0]
+				headers[lowerKey] = values[0]
 			} else {
-				headers[key] = values
+				headers[lowerKey] = values[0]
 			}
 		}
 	}
