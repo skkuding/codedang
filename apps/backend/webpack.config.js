fcd011a28a903eff1072271a1a37f7fde2f19629
Jaemin Choi <1dotolee@gmail.com>
2025-05-05 19:16:28 +0900

    fix(be): fix module resolution error in macos

--- apps/backend/webpack.config.js
+++ apps/backend/webpack.config.js
@@ -3,6 +3,19 @@ const swcDefaultConfig =
     .swcOptions
 
 module.exports = {
+  // Somehow loading native binaries(*.node) in MacOS ARM64 is not working
+  // so let's use workaround to load them manually
+  externals: {
+    argon2: 'require("argon2")',
+    '@apollo/gateway': 'require("@apollo/gateway")',
+    '@apollo/subgraph': 'require("@apollo/subgraph")',
+    '@apollo/subgraph/package.json': 'require("@apollo/subgraph/package.json")',
+    '@apollo/subgraph/dist/directives':
+      'require("@apollo/subgraph/dist/directives")',
+    '@as-integrations/fastify': 'require("@as-integrations/fastify")',
+    '@css-inline/css-inline-darwin-arm64':
+      'require("@css-inline/css-inline-darwin-arm64")'
+  },
   module: {
     rules: [
       {
