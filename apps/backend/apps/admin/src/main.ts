cc6c9cd5447b3b7c844fe9ed9b3274d53594e124
GalaxyBook2 <manamana32321@gmail.com>
2025-05-06 09:11:30 +0900

    refactor(infra): improve readability of otlpEndpointUrl assignment

--- apps/backend/apps/admin/src/main.ts
+++ apps/backend/apps/admin/src/main.ts
@@ -10,7 +10,8 @@ const bootstrap = async () => {
     process.env.APP_ENV === 'stage' ||
     process.env.ENABLE_OPENTELEMETRY === 'true'
   ) {
-    const otlpEndpointUrl = process.env.OTEL_EXPORTER_OTLP_ENDPOINT_URL || 'localhost:4317'
+    const otlpEndpointUrl =
+      process.env.OTEL_EXPORTER_OTLP_ENDPOINT_URL || 'localhost:4317'
     const resource = await Instrumentation.getResource('ADMIN-API', '2.2.0')
     await Instrumentation.start(otlpEndpointUrl, resource)
   }
--- apps/backend/apps/client/src/main.ts
+++ apps/backend/apps/client/src/main.ts
@@ -10,11 +10,10 @@ const bootstrap = async () => {
     process.env.APP_ENV === 'stage' ||
     process.env.ENABLE_OPENTELEMETRY === 'true'
   ) {
+    const otlpEndpointUrl =
+      process.env.OTEL_EXPORTER_OTLP_ENDPOINT_URL || 'localhost:4317'
     const resource = await Instrumentation.getResource('CLIENT-API', '2.2.0')
-    await Instrumentation.start(
-      process.env.OTEL_EXPORTER_OTLP_ENDPOINT_URL || 'localhost:4317',
-      resource
-    )
+    await Instrumentation.start(otlpEndpointUrl, resource)
   }
 
   const { AppModule } = await import('./app.module')
