d2468f273252b2e02515982663999bf1789d2b6a
Dohyun Ryu <113527604+RyuRaseul@users.noreply.github.com>
2025-05-09 02:49:06 +0900

    fix(be): set duetime as endtime for existing records (#2740)

--- apps/backend/prisma/migrations/20250506225054_add_duetime/migration.sql
+++ apps/backend/prisma/migrations/20250506225054_add_duetime/migration.sql
@@ -5,4 +5,6 @@
 
 */
 -- AlterTable
-ALTER TABLE "assignment" ADD COLUMN     "due_time" TIMESTAMP(3) NOT NULL;
+ALTER TABLE "assignment" ADD COLUMN       "due_time" TIMESTAMP(3);
+UPDATE "assignment" SET    "due_time" = "end_time" WHERE "due_time" IS NULL;
+ALTER TABLE "assignment" ALTER COLUMN     "due_time"  SET NOT NULL;
