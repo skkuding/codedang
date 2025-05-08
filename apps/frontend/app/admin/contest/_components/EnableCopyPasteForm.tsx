3619c926edc3f2750f35b923635cc8400666f74a
Jiho Park <59248080+jihorobert@users.noreply.github.com>
2025-05-08 14:41:18 +0900

    fix(fe): create edit contest error (#2720)
    * fix(fe): only change selected user count when email exists
    
    * fix(fe): fix rendering error
    
    * fix(fe): complete use selected num and only save valid email
    
    * fix(fe): add enableCopyPaste switch button and refactor edit contest form
    
    * refactor(fe): use defaultValue instead
    
    * fix(fe): resolve input focus problem caused by key conflict
    
    * fix(fe): resolve duplicated emails
    
    * fix(fe): resolve duplicated email problem
    
    * chore(fe): change file path
    
    * chore(fe): change file path

--- apps/frontend/app/admin/contest/_components/EnableCopyPasteForm.tsx
+++ apps/frontend/app/admin/contest/_components/EnableCopyPasteForm.tsx
@@ -4,20 +4,19 @@ import { ErrorMessage } from '@/app/admin/_components/ErrorMessage'
 import { Label } from '@/app/admin/_components/Label'
 import { Switch } from '@/components/shadcn/switch'
 import React from 'react'
-import { useEffect } from 'react'
 import { useController, useFormContext } from 'react-hook-form'
 
-interface SampleTestcaseFormProps {
+interface EnableCopyPasteFormProps {
   name: string
   title: string
   hasValue?: boolean
 }
 
-export function SampleTestcaseForm({
+export function EnableCopyPasteForm({
   name,
   title,
-  hasValue = false
-}: SampleTestcaseFormProps) {
+  hasValue = true
+}: EnableCopyPasteFormProps) {
   const {
     control,
     formState: { errors }
@@ -25,12 +24,9 @@ export function SampleTestcaseForm({
 
   const { field } = useController({
     name,
-    control
+    control,
+    defaultValue: hasValue
   })
-
-  useEffect(() => {
-    field.onChange(hasValue)
-  }, [hasValue])
 
   return (
     <div className="flex items-center gap-3">
