04d6cb301943e3598180b31cab244342ff6f33f4
mnseok <min.seok@g.skku.edu>
2025-05-14 02:45:19 +0000

    fix(be): module import problem

--- apps/backend/apps/admin/src/workbook/workbook.module.ts
+++ apps/backend/apps/admin/src/workbook/workbook.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common'
+import { RolesModule } from '@libs/auth'
 import { ProblemModule } from '@admin/problem/problem.module'
 import { WorkbookProblemResolver } from './workbook-problem.resolver'
 import { WorkbookProblemService } from './workbook-problem.service'
 
 @Module({
-  imports: [ProblemModule],
+  imports: [RolesModule, ProblemModule],
   providers: [WorkbookProblemService, WorkbookProblemResolver]
 })
 export class WorkbookModule {}
