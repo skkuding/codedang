28b5e42501eaa5ca15be52cfbbfa56e7b7d706ea
jwoojin9 <jwoojin9@naver.com>
2025-05-15 08:50:34 +0900

    feat(fe): add solution tab in code editor

--- apps/frontend/app/(client)/(code-editor)/_components/EditorResizablePanel.tsx
+++ apps/frontend/app/(client)/(code-editor)/_components/EditorResizablePanel.tsx
@@ -130,6 +130,8 @@ export function EditorMainResizablePanel({
       pathname.startsWith(`${base}/problem/${problem.id}/leaderboard`)
     ) {
       setTabValue('Leaderboard')
+    } else if (pathname.startsWith(`${base}/problem/${problem.id}/solution`)) {
+      setTabValue('Solution')
     } else {
       setTabValue('Description')
     }
@@ -175,6 +177,19 @@ export function EditorMainResizablePanel({
                     Submissions
                   </TabsTrigger>
                 </Link>
+                {assignmentId && (
+                  <Link
+                    replace
+                    href={`${base}/problem/${problem.id}/solution` as Route}
+                  >
+                    <TabsTrigger
+                      value="Solution"
+                      className="data-[state=active]:text-primary-light rounded-tab-button data-[state=active]:bg-slate-700"
+                    >
+                      Solution
+                    </TabsTrigger>
+                  </Link>
+                )}
                 {contestId && (
                   <Link
                     replace
