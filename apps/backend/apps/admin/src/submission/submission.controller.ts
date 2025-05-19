5ee91aeecd378060d6a3c620a732c8c9ff787857
RyuRaseul <ryudb00@g.skku.edu>
2025-05-19 02:34:02 +0000

    chore: use problemId in download URL

--- apps/backend/apps/admin/src/submission/submission.controller.ts
+++ apps/backend/apps/admin/src/submission/submission.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Req, Res, Get, Param } from '@nestjs/common'
 import { Response } from 'express'
 import { AuthenticatedRequest, UseGroupLeaderGuard } from '@libs/auth'
 import { GroupIDPipe, IDValidationPipe } from '@libs/pipe'
-import type { problemId } from '@admin/problem/mock/mock'
 import { SubmissionService } from './submission.service'
 
 @Controller('submission')
@@ -13,7 +12,7 @@ export class SubmissionController {
    * compressSourceCodes 메서드를 통해 압축된 zipFile을 스트리밍을 통해 다운로드 합니다.
    * filename은 압축된 zipFile의 이름으로 ${assignmentTitle}_${problemId}의 형식으로 저장됩니다.
    */
-  @Get('/download/:groupId/:assignmentId/:filename')
+  @Get('/download/:groupId/:assignmentId/:problemId')
   @UseGroupLeaderGuard()
   async downloadCodes(
     @Param('groupId', GroupIDPipe) groupId: number,
