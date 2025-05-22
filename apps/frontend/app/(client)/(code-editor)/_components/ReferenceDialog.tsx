eeea72001a6443beb3232ad3efbb45662671d8a1
hyuntori <hyuntori@hyuntori.local>
2025-05-22 20:20:30 +0900

    feat(fe): add pypy3 documentation and change document modal design

--- apps/frontend/app/(client)/(code-editor)/_components/ReferenceDialog.tsx
+++ apps/frontend/app/(client)/(code-editor)/_components/ReferenceDialog.tsx
@@ -23,25 +23,30 @@ export function ReferenceDialog() {
         </DialogTrigger>
         <DialogContent
           showDarkOverlay={true}
-          className="rounded-xl border-none bg-slate-900 text-gray-300 sm:max-w-md"
+          className="h-[415px] w-[580px] max-w-none rounded-2xl border-none bg-slate-900 text-gray-300"
         >
           <DialogHeader>
-            <DialogTitle className="font-normal text-white">
+            <DialogTitle className="text-xl font-semibold text-white">
               Compiler Version Document
             </DialogTitle>
           </DialogHeader>
-          <div className="overflow-x-auto rounded border border-slate-600">
-            <table className="min-w-full bg-slate-900 text-left text-sm">
-              <thead className="border-b border-slate-600 bg-slate-800 text-xs">
+          <div className="overflow-x-auto rounded-[10px] border border-slate-600">
+            <table className="min-w-full bg-[#222939] text-left text-sm">
+              <thead className="border-b border-slate-600 bg-slate-800 text-xs font-normal text-[#C4CACC]">
                 <tr>
-                  <th className="px-6 py-3">Language</th>
-                  <th className="px-6 py-3">Compiler Version Document</th>
+                  <th className="py-3 pl-4">Language</th>
+                  <th className="py-3 pl-4">Time Limit</th>
+
+                  <th className="py-3 pl-4">Memory Limit</th>
+                  <th className="px-3 py-3">Compiler Version Document</th>
                 </tr>
               </thead>
               <tbody>
-                <tr className="border-b border-slate-600">
-                  <td className="flex px-6 py-4">C</td>
-                  <td className="px-6 py-4">
+                <tr className="border-b border-slate-600 bg-slate-900">
+                  <td className="py-3 pl-4">C</td>
+                  <td className="py-3 pl-4 text-[#8A8A8A]">-</td>
+                  <td className="py-3 pl-4 text-[#8A8A8A]">-</td>
+                  <td className="px-3 py-3">
                     <div className="flex items-center space-x-2">
                       <a
                         href="https://cplusplus.com/reference/clibrary/"
@@ -62,9 +67,11 @@ export function ReferenceDialog() {
                     </div>
                   </td>
                 </tr>
-                <tr className="border-b border-slate-600">
-                  <td className="flex px-6 py-4">C++</td>
-                  <td className="px-6 py-4">
+                <tr className="border-b border-slate-600 bg-slate-900">
+                  <td className="py-3 pl-4">C++</td>
+                  <td className="py-3 pl-4 text-[#8A8A8A]">-</td>
+                  <td className="py-3 pl-4 text-[#8A8A8A]">-</td>
+                  <td className="px-3 py-3">
                     <div className="flex items-center space-x-2">
                       <a
                         href="https://cplusplus.com/reference/"
@@ -85,9 +92,11 @@ export function ReferenceDialog() {
                     </div>
                   </td>
                 </tr>
-                <tr className="border-b border-slate-600">
-                  <td className="flex px-6 py-4">Java</td>
-                  <td className="px-6 py-4">
+                <tr className="border-b border-slate-600 bg-slate-900">
+                  <td className="py-3 pl-4">Java</td>
+                  <td className="py-3 pl-4 text-[#8A8A8A]">x2+1 sec</td>
+                  <td className="py-3 pl-4 text-[#8A8A8A]">x2+16 MB</td>
+                  <td className="px-3 py-3">
                     <div className="flex items-center space-x-2">
                       <a
                         href="https://docs.oracle.com/en/java/javase/17/docs/api/index.html"
@@ -99,9 +108,11 @@ export function ReferenceDialog() {
                     </div>
                   </td>
                 </tr>
-                <tr>
-                  <td className="flex px-6 py-4">Python</td>
-                  <td className="px-6 py-4">
+                <tr className="border-b border-slate-600 bg-slate-900">
+                  <td className="py-3 pl-4">Python</td>
+                  <td className="py-3 pl-4 text-[#8A8A8A]">x3+2 sec</td>
+                  <td className="py-3 pl-4 text-[#8A8A8A]">x2+32 MB</td>
+                  <td className="px-3 py-3">
                     <div className="flex items-center space-x-2">
                       <a
                         href="https://docs.python.org/3.12/library/index.html"
@@ -110,6 +121,19 @@ export function ReferenceDialog() {
                         <FileText size={18} />
                       </a>
                       <span>python 3.12.3</span>
+                    </div>
+                  </td>
+                </tr>
+                <tr className="bg-slate-900">
+                  <td className="py-3 pl-4">PyPy3</td>
+                  <td className="py-3 pl-4 text-[#8A8A8A]">x3+2 sec</td>
+                  <td className="py-3 pl-4 text-[#8A8A8A]">x2+128 MB</td>
+                  <td className="px-3 py-3">
+                    <div className="flex items-center space-x-2">
+                      <a href="https://pypy.org/" target="_blank">
+                        <FileText size={18} />
+                      </a>
+                      <span>Python 3.1.0.14, PyPy 7.3.17</span>
                     </div>
                   </td>
                 </tr>
