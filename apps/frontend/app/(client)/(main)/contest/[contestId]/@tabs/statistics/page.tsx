f3a2184cd22ad29704373e9525b6fef822f13c6d
Jimin Ha <jimin9038@g.skku.edu>
2025-05-27 21:58:17 +0900

    chore(fe): lint contest statistics page

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/statistics/page.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/statistics/page.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@/components/shadcn/button"
-import { Card, CardContent } from "@/components/shadcn/card"
-import Link from "next/link"
-import { LuArrowRight } from "react-icons/lu"
-import Image from "next/image"
-import pleaseLogo from '@/public/logos/please.png'
+import { Button } from '@/components/shadcn/button'
+import { Card, CardContent } from '@/components/shadcn/card'
 import { safeFetcher } from '@/libs/utils'
 import statisticsChart from '@/public/captures/statistics-chart.gif'
-import statisticsSubmissions from '@/public/captures/statistics-submissions.gif'
 import statisticsLeaderboard from '@/public/captures/statistics-leaderboard.gif'
+import statisticsSubmissions from '@/public/captures/statistics-submissions.gif'
+import pleaseLogo from '@/public/logos/please.png'
+import Image from 'next/image'
+import Link from 'next/link'
+import { LuArrowRight } from 'react-icons/lu'
 
 interface Contest {
   id: number
@@ -22,7 +22,9 @@ interface ContestStatisticsProps {
   }
 }
 
-export default async function ContestStatistics({ params }: ContestStatisticsProps) {
+export default async function ContestStatistics({
+  params
+}: ContestStatisticsProps) {
   const { contestId } = params
 
   // 대회 정보 가져오기
@@ -39,9 +41,15 @@ export default async function ContestStatistics({ params }: ContestStatisticsPro
   if (!isContestFinished) {
     return (
       <div className="flex flex-col items-center justify-center pb-[120px]">
-        <Image className="mt-40" src={pleaseLogo} alt="coming-soon" width={336} />
+        <Image
+          className="mt-40"
+          src={pleaseLogo}
+          alt="coming-soon"
+          width={336}
+        />
         <div className="mt-5 text-lg text-gray-600">
-          Statistics will be available after {new Date(endTimeWithBuffer).toLocaleString('ko-KR')}
+          Statistics will be available after{' '}
+          {new Date(endTimeWithBuffer).toLocaleString('ko-KR')}
         </div>
         <div className="mt-2 text-base text-gray-500">
           You can check the statistics five minutes after the contest ends.
@@ -60,20 +68,24 @@ export default async function ContestStatistics({ params }: ContestStatisticsPro
       <Button className="w-fit text-lg" asChild>
         <Link href="https://leaderboard-statistics.vercel.app/">
           See statistics in new page
-          <LuArrowRight className="inline ml-2" />
+          <LuArrowRight className="ml-2 inline" />
         </Link>
       </Button>
       {/* Statistics Feature Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 my-10">
+      <div className="my-10 grid grid-cols-1 gap-6 md:grid-cols-3">
         <Card className="border-none">
           <CardContent
             style={{ boxShadow: '3px 3px 10px 0px rgba(0, 0, 0, 0.15)' }}
             className="flex aspect-square h-[420px] w-full flex-col items-start justify-around gap-3 rounded-[14px] pt-9 text-2xl"
           >
-            <div className="w-full rounded-lg flex items-center justify-center mb-4">
-              <Image src={statisticsLeaderboard} alt="statistics-leaderboard" width={336}/>
+            <div className="mb-4 flex w-full items-center justify-center rounded-lg">
+              <Image
+                src={statisticsLeaderboard}
+                alt="statistics-leaderboard"
+                width={336}
+              />
             </div>
-            <div className="text-base font-normal text-center text-neutral-600 w-full">
+            <div className="w-full text-center text-base font-normal text-neutral-600">
               You can check the leaderboard over time.
             </div>
           </CardContent>
@@ -84,11 +96,16 @@ export default async function ContestStatistics({ params }: ContestStatisticsPro
             style={{ boxShadow: '3px 3px 10px 0px rgba(0, 0, 0, 0.15)' }}
             className="flex aspect-square h-[420px] w-full flex-col items-start justify-around gap-3 rounded-[14px] pt-9 text-2xl"
           >
-            <div className="w-full rounded-lg flex items-center justify-center mb-4">
-              <Image src={statisticsChart} alt="statistics-capture-2" width={336} />
+            <div className="mb-4 flex w-full items-center justify-center rounded-lg">
+              <Image
+                src={statisticsChart}
+                alt="statistics-capture-2"
+                width={336}
+              />
             </div>
-            <div className="text-base font-normal text-center text-neutral-600 w-full">
-              You can view problem-solving trends and ranking changes for each team over time through graphs.
+            <div className="w-full text-center text-base font-normal text-neutral-600">
+              You can view problem-solving trends and ranking changes for each
+              team over time through graphs.
             </div>
           </CardContent>
         </Card>
@@ -96,18 +113,23 @@ export default async function ContestStatistics({ params }: ContestStatisticsPro
         <Card className="border-none">
           <CardContent
             style={{ boxShadow: '3px 3px 10px 0px rgba(0, 0, 0, 0.15)' }}
-            className="flex aspect-square w-full h-[420px] flex-col items-start justify-around gap-3 rounded-[14px] pt-9 text-2xl"
+            className="flex aspect-square h-[420px] w-full flex-col items-start justify-around gap-3 rounded-[14px] pt-9 text-2xl"
           >
-            <div className="w-full rounded-lg flex items-center justify-center mb-4">
-              <Image src={statisticsSubmissions} alt="statistics-capture-3" width={336} height={300} />
+            <div className="mb-4 flex w-full items-center justify-center rounded-lg">
+              <Image
+                src={statisticsSubmissions}
+                alt="statistics-capture-3"
+                width={336}
+                height={300}
+              />
             </div>
-            <div className="text-base font-normal text-center text-neutral-600 w-full">
-              You can select teams to view or compare each team's problem-solving summary and submission history.
+            <div className="w-full text-center text-base font-normal text-neutral-600">
+              You can select teams to view or compare each team&apos;s
+              problem-solving summary and submission history.
             </div>
           </CardContent>
         </Card>
       </div>
-
     </div>
   )
 }
