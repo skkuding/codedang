ae2aa5efcdd198555b8797187848f73b35ae043f
mingyuseong <mingyu27@naver.com>
2025-06-02 15:16:39 +0900

    feat(fe): create ongoing for assignment status

--- apps/frontend/app/admin/course/[courseId]/_components/AssignmentStatus.tsx
+++ apps/frontend/app/admin/course/[courseId]/_components/AssignmentStatus.tsx
@@ -2,7 +2,9 @@ import { StatusTimeDiff } from '@/components/StatusTimeDiff'
 import { formatDateRange } from '@/libs/utils'
 import calendarIcon from '@/public/icons/calendar.svg'
 import clockIcon from '@/public/icons/clock.svg'
+import dayjs from 'dayjs'
 import Image from 'next/image'
+import { FaCirclePlay } from 'react-icons/fa6'
 
 interface AssignmentStatusProps {
   startTime: Date
@@ -15,11 +17,17 @@ export function AssignmentStatus({
   endTime,
   dueTime
 }: AssignmentStatusProps) {
+  const now = dayjs()
   return (
     <div className="flex flex-col gap-1">
+      {now.isAfter(startTime) && (
+        <div className="text-primary flex items-center gap-2 text-sm font-semibold">
+          <FaCirclePlay size={14} />
+          <p>ONGOING</p>
+        </div>
+      )}
       <div className="flex items-center gap-2 text-sm font-normal">
         <Image src={calendarIcon} alt="calendar" width={14} />
-
         <p>{formatDateRange(startTime, endTime)}</p>
       </div>
       <div className="flex items-center gap-2">
