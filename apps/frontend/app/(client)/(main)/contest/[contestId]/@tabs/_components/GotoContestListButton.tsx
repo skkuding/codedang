cf75e604974391c47bc08b57ff39d3c0749f8c12
root <root@SeoHwan.localdomain>
2025-07-01 17:58:18 +0900

    fix(fe): fix button and text color

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/_components/GotoContestListButton.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/_components/GotoContestListButton.tsx
@@ -5,8 +5,8 @@ export function GotoContestListButton() {
   return (
     <div className="flex w-[1208px] justify-end">
       <Link href={`/contest`}>
-        <Button className="mb-0 mt-5 h-[42px] w-[136px] rounded-full border border-[#80808040] bg-white font-light text-black hover:bg-[#80808014]">
-          <p className="w-[100px] text-center text-[16px] font-medium leading-[22.4px] tracking-[-0.48px]">
+        <Button className="mb-0 mt-5 h-[42px] w-[136px] rounded-full border border-[#D8D8D8] bg-[#F0F0F0] font-light text-black hover:bg-[#80808014]">
+          <p className="w-[100px] text-center text-[14px] font-semibold leading-[22.4px] tracking-[-0.48px]">
             Back to the List
           </p>
         </Button>
