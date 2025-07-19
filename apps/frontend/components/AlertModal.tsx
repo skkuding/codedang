16682832f155b00f495e0cb1597e0f31b1e2b836
Mingyu Seong <112471084+minngyuseong@users.noreply.github.com>
2025-07-19 18:30:53 +0900

    refactor(fe): impl modal unification v2 (#2872)
    * chore(fe): apply prettier again
    
    * feat(fe): impl AlertModal and apply it when deleting course
    
    * chore(fe): remove unused stuff
    
    * chore(fe): remove unused stuff
    
    * chore(fe): remove component from shadcn
    
    * chore(fe): correct import statement
    
    * fix(fe): handle undefined values properly

---
+++ apps/frontend/components/AlertModal.tsx
@@ -0,0 +1,93 @@
+import { Button } from '@/components/shadcn/button'
+import { cn } from '@/libs/utils'
+import infoIcon from '@/public/icons/info.svg'
+import Image from 'next/image'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle
+} from './shadcn/alert-dialog'
+
+interface ButtonProps {
+  text: string
+  onClick: () => void
+  variant?: 'default' | 'outline'
+}
+
+interface AlertModalProps {
+  open: boolean
+  onOpenChange: (open: boolean) => void
+  type: 'confirm' | 'warning'
+  showWarningIcon?: boolean
+  title: string
+  description?: string
+  primaryButton: ButtonProps
+  children?: React.ReactNode
+  onClose?: () => void
+}
+
+export function AlertModal({
+  open,
+  onOpenChange,
+  type,
+  showWarningIcon = true,
+  title,
+  description,
+  primaryButton,
+  children,
+  onClose
+}: AlertModalProps) {
+  return (
+    <AlertDialog open={open} onOpenChange={onOpenChange}>
+      <AlertDialogContent
+        className="flex h-[280px] w-[424px] flex-col items-center justify-between !rounded-2xl p-[40px]"
+        onEscapeKeyDown={onClose}
+      >
+        <AlertDialogHeader className="flex flex-col items-center justify-center">
+          {type === 'warning' && showWarningIcon && (
+            <Image
+              src={infoIcon}
+              alt="info"
+              width={42}
+              height={42}
+              // className="mb-3"
+            />
+          )}
+          <AlertDialogTitle className="text-center text-2xl font-semibold">
+            {title}
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        {children}
+        <p
+          className={cn(
+            'w-full text-center text-sm font-normal text-[#737373]',
+            children && 'text-left'
+          )}
+        >
+          {description}
+        </p>
+        <AlertDialogFooter className="flex w-full justify-center gap-[4px]">
+          <AlertDialogCancel className="h-[46px] w-full">
+            Cancel
+          </AlertDialogCancel>
+          <AlertDialogAction asChild>
+            <Button
+              onClick={primaryButton.onClick}
+              className={cn(
+                'h-[46px] w-full',
+                type === 'warning' ? 'bg-error hover:bg-red-500/90' : null
+              )}
+              variant={primaryButton.variant}
+            >
+              {primaryButton.text}
+            </Button>
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
