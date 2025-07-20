43de837f04361b1d531157ce2795f25b83e27803
sONg20NOW <sgk1004s@naver.com>
2025-07-20 13:08:06 +0900

    style(fe): linting

--- apps/frontend/app/(client)/(main)/_components/MiddleContestBanner.tsx
+++ apps/frontend/app/(client)/(main)/_components/MiddleContestBanner.tsx
@@ -4,48 +4,53 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export function MiddleContestBanner() {
-  // eslint-disable-next-line
   return (
-    <>
-      <div className="relative flex max-h-[578px] w-full max-w-[1860px]">
-        <TextBox className={'z-10'} />
-        <ImageBox />
-      </div>
-    </>
+    <div className="relative flex max-h-[578px] w-full max-w-[1860px]">
+      <TextBox className={'z-10'} />
+      <ImageBox />
+    </div>
   )
 }
 
-function TextBox({ className }: any) {
+function TextBox({ className }: { className: String }) {
   return (
-    <>
-      {/* 전체 검은 박스 => 원색 : #182E56 */}
-      <div
-        className={`flex w-full max-w-[707px] items-center rounded-l-[20px] bg-[#182E56] ${className}`}
-      >
-        {/* 글자 + 버튼 */}
-        <div className="text-background ms-[86px] flex h-full max-h-[264px] w-full max-w-[446px] flex-col justify-between">
-          {/* 글자 부분 */}
-          <div className="flex h-full max-h-[164px] flex-col justify-between">
-            <Text className="max-h-[96px] text-[40px] font-semibold leading-[120%]">
+    <div
+      className={`flex w-full max-w-[707px] items-center rounded-l-[20px] bg-[#182E56] ${className}`}
+    >
+      {/* 글자 + 버튼 */}
+      <div className="text-background ms-[86px] flex h-full max-h-[264px] w-full max-w-[446px] flex-col justify-between">
+        {/* 글자 부분 */}
+        <div className="flex h-full max-h-[164px] flex-col justify-between">
+          <Text className="max-h-[96px] text-[40px] font-semibold leading-[120%]">
+            <p>
+              {' '}
               TAKE PART IN REAL-TIME
               <br />
               CODING CONTEST
-            </Text>
-            <Text className="text-background-alternative max-h-[48px] text-[16px] font-normal leading-[150%]">
+            </p>
+          </Text>
+          <Text className="text-background-alternative max-h-[48px] text-[16px] font-normal leading-[150%]">
+            <p>
               Check your skills, and try to compete in the rankings.
               <br />
               Make it more fun and immersive with real-time rankings!
-            </Text>
-          </div>
-          {/* 버튼 부분 */}
-          <BarButton />
+            </p>
+          </Text>
         </div>
+        {/* 버튼 부분 */}
+        <BarButton />
       </div>
-    </>
+    </div>
   )
 }
 
-function Text({ children, className }: any) {
+function Text({
+  children,
+  className
+}: {
+  children: JSX.Element
+  className: String
+}) {
   return (
     <div
       className={`sans vertical-align: middle; tracking-[-0.03em] ${className}`}
@@ -57,27 +62,25 @@ function Text({ children, className }: any) {
 
 function BarButton() {
   return (
-    <>
-      <Button
-        variant={'outline'}
-        className="h-full max-h-[54px] w-full max-w-[291px] p-0"
+    <Button
+      variant={'outline'}
+      className="h-full max-h-[54px] w-full max-w-[291px] p-0"
+    >
+      <Link
+        // TODO : 버튼 누르면 어디로 이동해야 하죠?
+        href={'/'}
+        className="flex h-full w-full items-center justify-between px-[4px] py-0"
       >
-        <Link
-          // 버튼 누르면 어디로 이동해야 하죠?
-          href={'/'}
-          className="flex h-full w-full items-center justify-between px-[4px] py-0"
-        >
-          <Text className="ms-[26px] max-h-[28px] max-w-[191px] text-[20px] font-medium leading-[140%] text-black">
-            How to host a Contest
-          </Text>
-          <div className="flex h-full max-h-[46px] w-full max-w-[46px] items-center justify-center rounded-full bg-black">
-            <div className="text-background relative flex size-[30px] items-center justify-center">
-              <Image src={RightIcon} alt="Right" fill />
-            </div>
+        <Text className="ms-[26px] max-h-[28px] max-w-[191px] text-[20px] font-medium leading-[140%] text-black">
+          <p>How to host a Contest</p>
+        </Text>
+        <div className="flex h-full max-h-[46px] w-full max-w-[46px] items-center justify-center rounded-full bg-black">
+          <div className="text-background relative flex size-[30px] items-center justify-center">
+            <Image src={RightIcon} alt="Right" fill />
           </div>
-        </Link>
-      </Button>
-    </>
+        </div>
+      </Link>
+    </Button>
   )
 }
 
@@ -99,12 +102,10 @@ function ImageBox() {
   )
 }
 
-function ImageMask({ className }: any) {
+function ImageMask({ className }: { className: String }) {
   return (
-    <>
-      <div
-        className={`from-background/50 to-background/0 absolute right-0 top-[-48px] bg-gradient-to-b ${className}`}
-      ></div>
-    </>
+    <div
+      className={`from-background/50 to-background/0 absolute right-0 top-[-48px] bg-gradient-to-b ${className}`}
+    ></div>
   )
 }
