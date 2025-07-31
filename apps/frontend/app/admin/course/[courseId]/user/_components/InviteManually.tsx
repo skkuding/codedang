bd81ce17113e66a93dcf325f340599fabb2af4bd
seoeun9 <seoeun9@naver.com>
2025-07-31 23:55:33 +0900

    temp

--- apps/frontend/app/admin/course/[courseId]/user/_components/InviteManually.tsx
+++ apps/frontend/app/admin/course/[courseId]/user/_components/InviteManually.tsx
@@ -17,7 +17,7 @@ import { useMutation } from '@apollo/client'
 import { valibotResolver } from '@hookform/resolvers/valibot'
 import { useCallback, useEffect, useState } from 'react'
 import { useForm, type SubmitHandler } from 'react-hook-form'
-import { MdOutlineEmail } from 'react-icons/md'
+import { MdOutlineEmail, MdOutlineAddCircle } from 'react-icons/md'
 import { toast } from 'sonner'
 import { findUserSchema, inviteUserSchema } from '../_libs/schema'
 
@@ -40,6 +40,11 @@ interface UserInfo {
   id: number
 }
 
+type SearchedUserDisplay = {
+  email: string
+  role: 'Instructor' | 'Student'
+}
+
 type InvitedUserDisplay = {
   email: string
   role: 'Instructor' | 'Student'
@@ -47,10 +52,14 @@ type InvitedUserDisplay = {
 
 export function InviteManually({ courseId }: InviteManuallyProps) {
   const roles: MemberRole[] = ['Instructor', 'Student']
+  const [searchedUserId, setSearchedUserId] = useState(0)
   const [userId, setUserId] = useState(0)
 
   const [invitedList, setInvitedList] = useState<InvitedUserDisplay[]>([])
   const [inviteUser] = useMutation(INVITE_USER)
+
+  const [searchedList, setSearchedList] = useState<SearchedUserDisplay[]>([])
+  const [selectedList, setSelectedList] = useState<SearchedUserDisplay[]>([])
 
   const onFind: SubmitHandler<FindUserInput> = async (data) => {
     try {
@@ -60,11 +69,18 @@ export function InviteManually({ courseId }: InviteManuallyProps) {
 
       if (!res.ok) {
         toast.error('Failed to find user')
+        setSearchedList([])
         return
       }
 
       const userInfo: UserInfo = await res.json()
-      setUserId(userInfo.id)
+      setSearchedUserId(userInfo.id)
+      setSearchedList([
+        {
+          email: data.email,
+          role: inviteWatch('isGroupLeader') ? 'Instructor' : 'Student'
+        }
+      ])
     } catch (err) {
       console.error(err)
       toast.error('Unexpected error occurred')
@@ -136,11 +152,11 @@ export function InviteManually({ courseId }: InviteManuallyProps) {
     }
   })
 
-  useEffect(() => {
-    if (userId !== 0) {
-      inviteHandleSubmit(onInvite)()
-    }
-  }, [inviteHandleSubmit, onInvite, userId])
+  // useEffect(() => {
+  //   if (userId !== 0) {
+  //     inviteHandleSubmit(onInvite)()
+  //   }
+  // }, [inviteHandleSubmit, onInvite, userId])
 
   return (
     <form
@@ -153,13 +169,13 @@ export function InviteManually({ courseId }: InviteManuallyProps) {
     >
       <span className="text-lg">Invite Manually</span>
       <div className="flex w-full flex-col gap-4">
-        <div className="flex h-[40px] items-center rounded-full border border-gray-300 px-4 py-2">
+        <div className="flex h-[40px] items-center rounded-full border border-gray-300 px-5 py-2">
           <MdOutlineEmail className="h-5 w-5 text-gray-400" />
 
           <Input
             id="email"
             {...findRegister('email')}
-            placeholder="Email Address"
+            placeholder="Please enter the e-mail"
             className="flex-1 border-none !bg-transparent pl-2 text-sm placeholder:text-gray-400 autofill:!bg-transparent focus:outline-none focus:ring-0 focus-visible:border-none focus-visible:outline-none focus-visible:ring-0"
           />
 
@@ -182,9 +198,33 @@ export function InviteManually({ courseId }: InviteManuallyProps) {
           </Select>
         </div>
 
+        {searchedList.map((user) => (
+          <div className="flex items-center gap-[10px] self-stretch">
+            <div className="flex h-10 flex-[1_0_0] items-center justify-between rounded-full bg-gray-100 px-5">
+              <div className="flex items-center gap-[10px]">
+                <MdOutlineEmail className="h-5 w-5 text-gray-400" />
+                <span className="text-sm">{user.email}</span>
+              </div>
+              <MdOutlineAddCircle
+                className="h-[18px] w-[18px] cursor-pointer text-gray-400"
+                onClick={() =>
+                  selectedList.includes(user)
+                    ? console.log('no')
+                    : setSelectedList((prev) => [...prev, user])
+                }
+              />
+            </div>
+
+            <div className="flex h-10 w-[120px] items-center justify-center gap-1 rounded-full bg-gray-100 px-5">
+              <span className="text-sm">{user.role}</span>
+            </div>
+          </div>
+        ))}
+
         <Button
-          type="submit"
+          type="button"
           className="bg-primary hover:bg-primary-strong h-[44px] w-full rounded-full text-sm font-semibold text-white"
+          onClick={() => inviteHandleSubmit(onInvite)()}
         >
           Invite
         </Button>
