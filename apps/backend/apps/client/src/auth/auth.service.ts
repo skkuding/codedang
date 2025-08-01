0370af4f205ff884ad8cfac6c2cf81257b6f526a
hjkim24 <hjkim4842@naver.com>
2025-08-01 01:37:55 +0000

    feat(be): add userRole in jwt token

--- apps/backend/apps/client/src/auth/auth.service.ts
+++ apps/backend/apps/client/src/auth/auth.service.ts
@@ -49,15 +49,16 @@ export class AuthService {
 
     await this.userService.updateLastLogin(user.username)
 
-    return await this.createJwtTokens(user.id, user.username)
+    return await this.createJwtTokens(user.id, user.username, user.role)
   }
 
   async updateJwtTokens(refreshToken: string) {
-    const { userId, username } = await this.verifyJwtToken(refreshToken)
+    const { userId, username, userRole } =
+      await this.verifyJwtToken(refreshToken)
     if (!(await this.isValidRefreshToken(refreshToken, userId))) {
       throw new InvalidJwtTokenException('Unidentified refresh token')
     }
-    return await this.createJwtTokens(userId, username)
+    return await this.createJwtTokens(userId, username, userRole)
   }
 
   async verifyJwtToken(token: string, options: JwtVerifyOptions = {}) {
@@ -82,8 +83,8 @@ export class AuthService {
     return true
   }
 
-  async createJwtTokens(userId: number, username: string) {
-    const payload: JwtPayload = { userId, username }
+  async createJwtTokens(userId: number, username: string, userRole: string) {
+    const payload: JwtPayload = { userId, username, userRole }
     const accessToken = await this.jwtService.signAsync(payload, {
       expiresIn: ACCESS_TOKEN_EXPIRE_TIME
     })
