1c2b3b3f1cfec0e063cb907ec799f88f99ebc329
eggz6z6 <kazy0606@gmail.com>
2025-08-05 01:11:58 +0900

    feat(fe): modify tailwind values

--- apps/frontend/app/(client)/(main)/_components/ServiceCards.tsx
+++ apps/frontend/app/(client)/(main)/_components/ServiceCards.tsx
@@ -2,15 +2,15 @@
 
 export function ServiceCards() {
   return (
-    <section className="font-pretendard flex w-full flex-col items-center gap-[40px]">
-      <div className="flex w-full max-w-[1184px] flex-col items-start gap-[40px]">
-        <h2 className="text-[30px] font-semibold leading-[36px] tracking-[-0.9px] text-black">
+    <section className="font-pretendard flex w-full flex-col items-center gap-10">
+      <div className="flex w-full max-w-[1208px] flex-col items-start gap-10">
+        <h2 className="text-[30px] font-semibold leading-9 tracking-[-0.9px] text-black">
           SERVICE WE PROVIDE
         </h2>
 
-        <div className="grid w-full auto-rows-[330px] grid-cols-2 gap-[12px] [@media(min-width:1140px)]:grid-cols-[41%_29%_30%]">
+        <div className="grid w-full auto-rows-[330px] grid-cols-2 gap-3 [@media(min-width:1140px)]:grid-cols-[41%_29%_30%]">
           <div className="bg-primary-light relative row-span-2 h-full w-full rounded-[20px] text-white">
-            <div className="absolute left-[30px] top-[30px] flex h-[34px] items-center justify-center rounded-full border border-white px-[12px] py-[4px] text-[16px] font-normal">
+            <div className="absolute left-[30px] top-[30px] flex h-[34px] items-center justify-center rounded-full border border-white px-3 py-1 text-xs font-normal">
               CONTEST
             </div>
             <img
@@ -18,11 +18,11 @@ export function ServiceCards() {
               alt="About Contest"
               className="absolute h-full w-full rounded-[20px] object-cover"
             />
-            <div className="absolute bottom-[40px] left-[30px] right-[30px]">
-              <p className="pb-[14px] text-[30px] font-semibold leading-[36px] tracking-[-0.9px]">
+            <div className="absolute bottom-10 left-[30px] right-[30px]">
+              <p className="pb-[14px] text-[30px] font-semibold leading-9 tracking-[-0.9px]">
                 About Contest
               </p>
-              <p className="text-[16px] font-normal leading-[22.4px] tracking-[-0.48px]">
+              <p className="text-xs font-normal leading-[22.4px] tracking-[-0.48px]">
                 Professors and students can host coding contests,
                 <br /> and rankings help enhance learning and motivation.
               </p>
@@ -30,7 +30,7 @@ export function ServiceCards() {
           </div>
 
           <div className="bg-background-normal relative h-full w-full rounded-[20px]">
-            <div className="border-primary text-primary absolute left-[30px] top-[30px] z-10 flex h-[34px] items-center justify-center rounded-full border px-[12px] py-[4px] text-[16px] leading-[22.4px] tracking-[-0.48px]">
+            <div className="border-primary text-primary absolute left-[30px] top-[30px] z-10 flex h-[34px] items-center justify-center rounded-full border px-3 py-1 text-xs leading-[22.4px] tracking-[-0.48px]">
               NOTICE
             </div>
             <img
@@ -39,17 +39,17 @@ export function ServiceCards() {
               className="absolute inset-0 h-full w-full rounded-[20px] object-cover"
             />
             <div className="absolute left-[30px] right-[30px] top-[78px]">
-              <p className="text-primary-strong pb-[14px] text-[30px] font-semibold leading-[36px] tracking-[-0.9px]">
+              <p className="text-primary-strong pb-[14px] text-[30px] font-semibold leading-9 tracking-[-0.9px]">
                 Stay Informed
               </p>
-              <p className="text-primary text-[16px] font-normal leading-[22.4px] tracking-[-0.48px]">
+              <p className="text-primary text-xs font-normal leading-[22.4px] tracking-[-0.48px]">
                 Explore coding challenges <br /> by level and topic.
               </p>
             </div>
           </div>
 
           <div className="bg-primary relative h-full w-full rounded-[20px] text-white">
-            <div className="absolute left-[30px] top-[30px] z-10 flex h-[34px] items-center justify-center rounded-full border border-white px-[12px] py-[4px] text-[16px] leading-[22.4px] tracking-[-0.48px]">
+            <div className="absolute left-[30px] top-[30px] z-10 flex h-[34px] items-center justify-center rounded-full border border-white px-3 py-1 text-xs leading-[22.4px] tracking-[-0.48px]">
               PROBLEM
             </div>
             <img
@@ -63,11 +63,11 @@ export function ServiceCards() {
               className="absolute bottom-0 right-0 max-h-full max-w-full object-contain"
             />
             <div className="absolute left-[30px] right-[30px] top-[78px]">
-              <p className="pb-[14px] text-[30px] font-semibold leading-[36px] tracking-[-0.9px]">
+              <p className="pb-[14px] text-[30px] font-semibold leading-9 tracking-[-0.9px]">
                 Practice with
                 <br /> Real problems
               </p>
-              <p className="text-[16px] font-normal leading-[22.4px] tracking-[-0.48px]">
+              <p className="text-xs font-normal leading-[22.4px] tracking-[-0.48px]">
                 Explore coding challenges
                 <br /> by level and topic.
               </p>
@@ -75,7 +75,7 @@ export function ServiceCards() {
           </div>
 
           <div className="relative col-span-2 h-full w-full rounded-[20px] bg-[#00183E] text-white">
-            <div className="absolute left-[30px] top-[30px] flex h-[34px] items-center justify-center rounded-full border border-white bg-[#00183E] px-[12px] py-[4px] text-[16px] leading-[22.4px] tracking-[-0.48px]">
+            <div className="absolute left-[30px] top-[30px] flex h-[34px] items-center justify-center rounded-full border border-white bg-[#00183E] px-3 py-1 text-xs leading-[22.4px] tracking-[-0.48px]">
               COURSE
             </div>
             <img
@@ -83,11 +83,11 @@ export function ServiceCards() {
               alt="Learn with Courses"
               className="absolute bottom-0 right-0 rounded-[20px] object-cover"
             />
-            <div className="absolute bottom-[40px] left-[30px] right-[30px]">
-              <p className="pb-[14px] text-[30px] font-semibold leading-[36px] tracking-[-0.9px]">
+            <div className="absolute bottom-10 left-[30px] right-[30px]">
+              <p className="pb-[14px] text-[30px] font-semibold leading-9 tracking-[-0.9px]">
                 Learn with Courses
               </p>
-              <p className="text-[16px] font-normal leading-[22.4px] tracking-[-0.48px]">
+              <p className="text-xs font-normal leading-[22.4px] tracking-[-0.48px]">
                 Access course-linked assignments and exercises.
                 <br /> Learn through professor-curated problem.
               </p>
