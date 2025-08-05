b08e7f9153fb04d7ab6021af3e7f5a9af1253aa3
오종희 <loverduck123@skku.edu>
2025-08-05 15:12:21 +0900

    fix(fe): resolve conflict with arrow-right-white.svg

--- apps/frontend/app/(client)/(main)/_components/NewProblemCards.tsx
+++ apps/frontend/app/(client)/(main)/_components/NewProblemCards.tsx
@@ -68,7 +68,7 @@ function NewProblemCard({ problem }: { problem: Problem }) {
         <div className="flex flex-col items-start gap-10">
           <Badge
             variant={problem.difficulty}
-            className="mb-2 whitespace-nowrap rounded-md px-2 py-1 text-xs font-medium capitalize"
+            className="mb-2 whitespace-nowrap rounded-full px-2 py-1 text-xs font-medium capitalize"
           >
             Level {problem.difficulty.slice(-1)}
           </Badge>
