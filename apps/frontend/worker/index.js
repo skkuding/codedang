1f0b7d489a92612500d009cdd50f3ed5766218ac
jwoojin9 <jwoojin9@naver.com>
2025-08-12 03:21:10 +0900

    feat(fe): add notification click event

--- apps/frontend/worker/index.js
+++ apps/frontend/worker/index.js
@@ -7,3 +7,24 @@ self.addEventListener('push', (event) => {
   }
   event.waitUntil(self.registration.showNotification(title, options))
 })
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close()
+
+  const urlToOpen = event.notification.data.url
+
+  event.waitUntil(
+    clients
+      .matchAll({ type: 'window', includeUncontrolled: true })
+      .then((clientList) => {
+        for (const client of clientList) {
+          if (client.url === urlToOpen && 'focus' in client) {
+            return client.navigate(urlToOpen).then((c) => c?.focus())
+          }
+        }
+        if (clients.openWindow) {
+          return clients.openWindow(urlToOpen)
+        }
+      })
+  )
+})
