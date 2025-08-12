cca1964674a2773bcc243bc4c0f377365874ebdf
mingyuseong <mingyu27@naver.com>
2025-08-12 22:12:51 +0900

    chore(fe): remove unused stuff

--- apps/frontend/app/admin/problem/_components/ProblemUsage.tsx
+++ apps/frontend/app/admin/problem/_components/ProblemUsage.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { Modal } from '@/components/Modal'
+import { ScrollArea } from '@/components/shadcn/scroll-area'
 import { Skeleton } from '@/components/shadcn/skeleton'
 //import { GET_ASSIGNMENT } from '@/graphql/assignment/queries'
 import { GET_BELONGED_ASSIGNMENTS } from '@/graphql/assignment/queries'
@@ -86,110 +87,45 @@ export function ProblemUsage({
 
   return (
     <Modal
-      size={'lg'}
-      type={'custom'}
-      title={'Using this problem'}
-      headerDescription={'This problem is used in the following contexts'}
+      size="lg"
+      type="custom"
+      title="Using this problem"
+      headerDescription="This problem is used in the following contexts"
       trigger={<Image src={fileInfoIcon} alt="fileinfo" />}
-    />
-    // return contestDataResult && assignmentDataResult ? (
-    //   <>
-    //     <TooltipProvider>
-    //       <Tooltip>
-    //         <TooltipTrigger asChild>
-    //           <button
-    //             type="button"
-    //             className="flex items-center justify-center"
-    //             onClick={() => {
-    //               setIsModalOpen(true)
-    //             }}
-    //           >
-    //             <Image src={fileInfoIcon} alt="fileinfo" />
-    //           </button>
-    //         </TooltipTrigger>
-    //         <TooltipContent className="mr-4 bg-white">
-    //           <p className="text-xs text-neutral-900">
-    //             Click to check which contests include this problem.
-    //           </p>
-    //         </TooltipContent>
-    //       </Tooltip>
-    //     </TooltipProvider>
-
-    //     <ScrollArea>
-    //       <Modal
-    //         open={isModalOpen}
-    //         onOpenChange={setIsModalOpen}
-    //         size={'lg'}
-    //         type={'custom'}
-    //         title={'Using this problem'}
-    //         headerDescription={'This problem is used in the following contexts'}
-    //       >
-    //         <ScrollArea className="h-full w-full">
-    //           <div className="border-line flex min-h-full flex-col items-start gap-[30px] self-stretch rounded-[16px] border bg-white p-[30px]">
-    //             <div className="flex flex-col items-start gap-3 self-stretch">
-    //               {assignmentData && <HeaderSection label="Assignment" />}
-    //               {assignmentData && (
-    //                 <ProblemSection
-    //                   contents={
-    //                     (assignmentDataResult?.upcoming,
-    //                     assignmentDataResult?.ongoing,
-    //                     assignmentDataResult?.finished)
-    //                   }
-    //                 />
-    //               )}
-    //             </div>
-    //             <div className="flex flex-col items-start gap-3 self-stretch">
-    //               {contestData && <HeaderSection label="Contest" />}
-    //               {contestData && (
-    //                 <ProblemSection
-    //                   contents={
-    //                     (contestDataResult?.upcoming,
-    //                     contestDataResult?.ongoing,
-    //                     contestDataResult?.finished)
-    //                   }
-    //                 />
-    //               )}
-    //             </div>
-    //             <div className="flex flex-col items-start gap-3 self-stretch">
-    //               <HeaderSection label="Exercise" />
-    //             </div>
-    //           </div>
-
-    //           {/* <ModalSection
-    //             title="Upcoming"
-    //             items={[
-    //               ...(contestDataResult?.upcoming ?? []).map(
-    //                 (item) => item.title
-    //               ),
-    //               ...(assignmentDataResult?.upcoming ?? []).map(
-    //                 (item) => item.title
-    //               )
-    //             ]}
-    //           />
-    //           <ModalSection
-    //             title="Ongoing"
-    //             items={[
-    //               ...(contestDataResult?.ongoing ?? []).map((item) => item.title),
-    //               ...(assignmentDataResult?.ongoing ?? []).map(
-    //                 (item) => item.title
-    //               )
-    //             ]}
-    //           />
-    //           <ModalSection
-    //             title="Finished"
-    //             items={[
-    //               ...(contestDataResult?.finished ?? []).map(
-    //                 (item) => item.title
-    //               ),
-    //               ...(assignmentDataResult?.finished ?? []).map(
-    //                 (item) => item.title
-    //               )
-    //             ]}
-    //           /> */}
-    //         </ScrollArea>
-    //       </Modal>
-    //     </ScrollArea>
-    //   </>
-    // ) : null
+    >
+      <ScrollArea className="h-full w-full">
+        <div className="border-line flex h-full flex-col items-start gap-[30px] self-stretch rounded-[16px] border bg-white p-[30px]">
+          <div className="flex flex-col items-start gap-3 self-stretch">
+            {assignmentData && <HeaderSection label="Assignment" />}
+            {assignmentData && (
+              <ProblemSection
+                contents={
+                  (assignmentDataResult?.upcoming,
+                  assignmentDataResult?.ongoing,
+                  assignmentDataResult?.finished)
+                }
+              />
+            )}
+          </div>
+          {contestData && (
+            <div className="flex flex-col items-start gap-3 self-stretch">
+              {contestData && <HeaderSection label="Contest" />}
+              {contestData && (
+                <ProblemSection
+                  contents={
+                    (contestDataResult?.upcoming,
+                    contestDataResult?.ongoing,
+                    contestDataResult?.finished)
+                  }
+                />
+              )}
+            </div>
+          )}
+          <div className="flex flex-col items-start gap-3 self-stretch">
+            <HeaderSection label="Exercise" />
+          </div>
+        </div>
+      </ScrollArea>
+    </Modal>
   )
 }
