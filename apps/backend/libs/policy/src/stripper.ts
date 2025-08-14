86389b104fd932debbec003f48e220e41d126140
hjkim24 <hjkim4842@naver.com>
2025-08-14 16:53:33 +0000

    fix(be): make regex to const to avoid codeQL check

--- apps/backend/libs/policy/src/stripper.ts
+++ apps/backend/libs/policy/src/stripper.ts
@@ -1,19 +1,26 @@
-/* eslint-disable no-useless-escape */
+const PYTHON_COMMENT = /#.*?(?=\\n|$)/g // Python에서의 한 줄 주석(#) 제거
+const PYTHON_DOCSTRING = /("""[\s\S]*?""")|('''[\s\S]*?''')/g // Python에서의 docstring(""" """, ''' ''') 제거
+const PYTHON_STRING = /("(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*')/g // Python에서의 문자열(" ", ' ') 제거
+
+const C_COMMENT = /\/\/.*?(?=\\n|$)/g // C 계열에서의 한 줄 주석(//) 제거
+const C_BLOCK_COMMENT = /\/\*[\s\S]*?\*\//g // C 계열에서의 Block 주석(/* */) 제거
+const C_STRING = /("(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*')/g // C 계열에서의 문자열(" ", ' ') 제거
+
 export const strip = (language: string, src: string): string => {
   switch (language) {
     case 'Python3':
     case 'PyPy3':
       return src
-        .replace(/#.*?(?=\\n|$)/g, '')
-        .replace(/(\"\"\"[\s\S]*?\"\"\")|(\'\'\'[\s\S]*?\'\'\')/g, '')
-        .replace(/(\"(?:\\.|[^"\\])*\"|\'(?:\\.|[^'\\])*\')/g, '')
+        .replace(PYTHON_COMMENT, '')
+        .replace(PYTHON_DOCSTRING, '')
+        .replace(PYTHON_STRING, '')
     case 'Java':
     case 'C':
     case 'Cpp':
       return src
-        .replace(/\/\/.*?(?=\\n|$)/g, '')
-        .replace(/\/\*[\s\S]*?\*\//g, '')
-        .replace(/(\"(?:\\.|[^"\\])*\"|\'(?:\\.|[^'\\])*\')/g, '')
+        .replace(C_COMMENT, '')
+        .replace(C_BLOCK_COMMENT, '')
+        .replace(C_STRING, '')
     default:
       return src
   }
