8215bc1fbbdc7744248b8e7313f084ba1b03ac78
hjkim24 <hjkim4842@naver.com>
2025-08-14 17:05:14 +0000

    fix(be): unuse lookahead regex prevent ReDoS

--- apps/backend/libs/policy/src/stripper.ts
+++ apps/backend/libs/policy/src/stripper.ts
@@ -1,26 +1,26 @@
-const PYTHON_COMMENT = /#.*?(?=\\n|$)/g // Python에서의 한 줄 주석(#) 제거
 const PYTHON_DOCSTRING = /("""[\s\S]*?""")|('''[\s\S]*?''')/g // Python에서의 docstring(""" """, ''' ''') 제거
-const PYTHON_STRING = /("(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*')/g // Python에서의 문자열(" ", ' ') 제거
-
-const C_COMMENT = /\/\/.*?(?=\\n|$)/g // C 계열에서의 한 줄 주석(//) 제거
 const C_BLOCK_COMMENT = /\/\*[\s\S]*?\*\//g // C 계열에서의 Block 주석(/* */) 제거
-const C_STRING = /("(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*')/g // C 계열에서의 문자열(" ", ' ') 제거
+const STRING = /("(?:\\.|[^"\\])*"|'(?:\\.|[^'\\])*')/g // Python에서의 문자열(" ", ' ') 제거
 
 export const strip = (language: string, src: string): string => {
   switch (language) {
     case 'Python3':
     case 'PyPy3':
       return src
-        .replace(PYTHON_COMMENT, '')
+        .split('\\n')
+        .map((line) => line.split('#')[0]) // # 이후 제거
+        .join('\\n')
         .replace(PYTHON_DOCSTRING, '')
-        .replace(PYTHON_STRING, '')
+        .replace(STRING, '')
     case 'Java':
     case 'C':
     case 'Cpp':
       return src
-        .replace(C_COMMENT, '')
+        .split('\\n')
+        .map((line) => line.split('//')[0]) // // 이후 제거
+        .join('\\n')
         .replace(C_BLOCK_COMMENT, '')
-        .replace(C_STRING, '')
+        .replace(STRING, '')
     default:
       return src
   }
