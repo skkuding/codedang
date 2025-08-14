028dae1354292c618fd66451a61072c863fc64f9
seoeun9 <seoeun9@naver.com>
2025-08-14 14:39:06 +0900

    fix(fe): invite manually email domain

--- apps/frontend/app/admin/course/[courseId]/user/_components/InviteManually.tsx
+++ apps/frontend/app/admin/course/[courseId]/user/_components/InviteManually.tsx
@@ -12,17 +12,17 @@ import {
 } from '@/components/shadcn/select'
 import { INVITE_USER } from '@/graphql/user/mutation'
 import { fetcherWithAuth } from '@/libs/utils'
+import emailIcon from '@/public/icons/email-symbol.svg'
 import plusIcon from '@/public/icons/plus-line.svg'
 import type { MemberRole } from '@/types/type'
 import { useMutation } from '@apollo/client'
 import { valibotResolver } from '@hookform/resolvers/valibot'
 import Image from 'next/image'
-import { useCallback, useState, useEffect } from 'react'
+import { useCallback, useState } from 'react'
 import { useForm, type SubmitHandler } from 'react-hook-form'
 import { IoMdCloseCircle } from 'react-icons/io'
-import { MdOutlineEmail, MdOutlineAddCircle } from 'react-icons/md'
+import { MdOutlineEmail } from 'react-icons/md'
 import { toast } from 'sonner'
-import { email } from 'valibot'
 import { findUserSchema, inviteUserSchema } from '../_libs/schema'
 
 interface InviteUserInput {
@@ -44,41 +44,21 @@ interface UserInfo {
   id: number
 }
 
-type SearchedUserDisplay = {
-  email: string
-  role: 'Instructor' | 'Student'
-  userId: number
-}
-
 type SelectedUserDisplay = {
   email: string
   role: 'Instructor' | 'Student'
   userId: number
 }
 
-type InvitedUserDisplay = {
-  email: string
-  role: 'Instructor' | 'Student'
-}
-
 export function InviteManually({ courseId }: InviteManuallyProps) {
   const roles: MemberRole[] = ['Instructor', 'Student']
   const userId = 0
-
-  const [invitedList, setInvitedList] = useState<InvitedUserDisplay[]>([])
   const [inviteUser] = useMutation(INVITE_USER)
-
-  const [searchedUser, setSearchedUser] = useState<SearchedUserDisplay | null>()
-
   const [selectedList, setSelectedList] = useState<SelectedUserDisplay[]>([])
+  const [emailDomain, setEmailDomain] = useState('@skku.edu')
 
   const onFind: SubmitHandler<FindUserInput> = async (data) => {
-    let emailRevised = data.email
-
-    if (!emailRevised.includes('@')) {
-      emailRevised += '@skku.edu'
-      findSetValue('email', emailRevised)
-    }
+    const emailRevised = data.email + emailDomain
 
     try {
       const res = await fetcherWithAuth('user/email', {
@@ -124,16 +104,6 @@ export function InviteManually({ courseId }: InviteManuallyProps) {
             userId: user.userId
           }
         })
-
-        setInvitedList((prevList) => [
-          ...prevList,
-          {
-            email: result.data?.inviteUser.user.email ?? '',
-            role: result.data?.inviteUser.isGroupLeader
-              ? 'Instructor'
-              : 'Student'
-          }
-        ])
 
         toast.success('Invited Successfully!', {
           style: {
@@ -181,39 +151,62 @@ export function InviteManually({ courseId }: InviteManuallyProps) {
           findHandleSubmit(onFind)()
         }}
         aria-label="Invite user"
-        className="flex flex-col gap-5 rounded-lg border p-[30px]"
+        className="flex flex-col gap-3 rounded-lg border p-[30px]"
       >
         <span className="font-pretendard text-lg">Invite Manually</span>
-        <div className="border-line flex flex-col items-start gap-[10px] self-stretch border-b pb-[30px]">
+        <div className="border-line flex flex-col items-start self-stretch border-b pb-[30px]">
           <div className="flex items-center gap-[10px] self-stretch">
-            <div className="flex h-10 w-full items-center rounded-full border border-gray-300 px-5 py-2">
-              <MdOutlineEmail className="h-5 w-5 text-gray-400" />
-              <Input
-                id="email"
-                {...findRegister('email')}
-                placeholder="Please enter the e-mail"
-                className="flex-1 border-none !bg-transparent pl-2 text-base placeholder:text-gray-400 autofill:!bg-transparent focus:outline-none focus:ring-0 focus-visible:border-none focus-visible:outline-none focus-visible:ring-0"
-              />
-            </div>
+            <div className="flex flex-1 items-center gap-1">
+              <div className="flex h-10 w-full items-center rounded-full border border-gray-300 px-5">
+                <MdOutlineEmail className="h-5 w-5 text-gray-400" />
+                <Input
+                  id="email"
+                  {...findRegister('email')}
+                  placeholder="Enter the e-mail"
+                  className="flex-1 border-none !bg-transparent pl-2 text-base placeholder:text-gray-400 autofill:!bg-transparent focus:outline-none focus:ring-0 focus-visible:border-none focus-visible:outline-none focus-visible:ring-0"
+                />
+              </div>
 
-            <div className="border-line flex h-10 w-[120px] items-start justify-center gap-1 rounded-full border bg-white px-5">
-              <Select
-                value={inviteWatch('isGroupLeader') ? 'Instructor' : 'Student'}
-                onValueChange={(value) => {
-                  inviteSetValue('isGroupLeader', value === 'Instructor')
-                }}
-              >
-                <SelectTrigger className="flex w-auto items-center gap-2 border-none bg-transparent text-base leading-[150%] tracking-[-0.48px] text-black focus:outline-none">
-                  <SelectValue placeholder="Student" />
-                </SelectTrigger>
-                <SelectContent className="rounded-lg bg-white shadow-md">
-                  {roles.map((role) => (
-                    <SelectItem key={role} value={role}>
-                      {role}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+              <div className="border-line flex h-10 flex-1 items-center gap-[6px] rounded-full border bg-white px-[14px]">
+                <Select
+                  value={emailDomain}
+                  onValueChange={setEmailDomain}
+                  defaultValue="@skku.edu"
+                >
+                  <Image src={emailIcon} alt="emailIcon" />
+                  <SelectTrigger className="flex items-center gap-2 border-none bg-transparent text-base leading-[150%] tracking-[-0.48px] text-black focus:outline-none focus:ring-0 focus-visible:border-none">
+                    <SelectValue placeholder="@skku.edu" />
+                  </SelectTrigger>
+                  <SelectContent className="rounded-lg bg-white shadow-md">
+                    <SelectItem value="@gmail.com">gmail.com</SelectItem>
+                    <SelectItem value="@naver.com">naver.com</SelectItem>
+                    <SelectItem value="@daum.com">daum.com</SelectItem>
+                    <SelectItem value="@skku.edu">skku.edu</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+
+              <div className="border-line flex h-10 w-[120px] items-start justify-center rounded-full border bg-white px-5">
+                <Select
+                  value={
+                    inviteWatch('isGroupLeader') ? 'Instructor' : 'Student'
+                  }
+                  onValueChange={(value) => {
+                    inviteSetValue('isGroupLeader', value === 'Instructor')
+                  }}
+                >
+                  <SelectTrigger className="flex w-auto items-center gap-2 border-none bg-transparent text-base leading-[150%] tracking-[-0.48px] text-black focus:outline-none">
+                    <SelectValue placeholder="Student" />
+                  </SelectTrigger>
+                  <SelectContent className="rounded-lg bg-white shadow-md">
+                    {roles.map((role) => (
+                      <SelectItem key={role} value={role}>
+                        {role}
+                      </SelectItem>
+                    ))}
+                  </SelectContent>
+                </Select>
+              </div>
             </div>
             <div
               className="border-primary flex cursor-pointer items-center justify-center gap-1 rounded-full border bg-white px-[22px] py-[10px]"
