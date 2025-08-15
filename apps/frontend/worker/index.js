d59312f82ff2a313322ba87bfa2324212161dce8
w0uldy0u <spacekim01@gmail.com>
2025-08-15 04:37:19 +0000

    chore(fe): modify service worker to use backend sent icon and badge

--- apps/frontend/worker/index.js
+++ apps/frontend/worker/index.js
@@ -3,7 +3,8 @@ self.addEventListener('push', (event) => {
   const title = data.title || 'New Notification'
   const options = {
     body: data.body || 'Check the details.',
-    icon: '/android-chrome-192x192.png'
+    icon: data.icon,
+    badge: data.badge
   }
   event.waitUntil(self.registration.showNotification(title, options))
 })
