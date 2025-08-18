e42286d2f56d1e7a3c27beb00953c23dd576af71
jwoojin9 <jwoojin9@naver.com>
2025-08-19 02:57:22 +0900

    fix(fe): fix notification url

--- apps/frontend/worker/index.js
+++ apps/frontend/worker/index.js
@@ -12,7 +12,7 @@ self.addEventListener('push', (event) => {
 self.addEventListener('notificationclick', (event) => {
   event.notification.close()
 
-  const urlToOpen = event.notification.data.url
+  const urlToOpen = event.notification.data.data.url
 
   event.waitUntil(
     clients
