3a0071c8d437ef4c0569e76064b06f82ff4d97ac
root <root@SeoHwan.localdomain>
2025-08-21 15:06:16 +0900

    fix(fe): connect API

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/qna/page.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/qna/page.tsx
@@ -1,30 +1,18 @@
 'use client'
 
 import { Button } from '@/components/shadcn/button'
-import { useState } from 'react'
-import { DetailQna } from './_components/DetailQna'
+import Link from 'next/link'
 
 export default function QnaPage() {
-  const [showDetailQna, setShowDetailQna] = useState(false)
-
-  const handleBackToList = () => {
-    setShowDetailQna(false)
-  }
-
-  if (showDetailQna) {
-    return <DetailQna onBackToList={handleBackToList} />
-  }
-
   return (
     <div className="flex w-full flex-col items-center justify-center p-8">
       <h2 className="mb-6 text-2xl font-medium text-black">Q&A</h2>
-      <Button
-        onClick={() => setShowDetailQna(true)}
-        className="h-[46px] px-6 py-3"
-      >
-        <span className="text-base font-medium text-white">
-          Post New Question
-        </span>
+      <Button className="h-[46px] px-6 py-3">
+        <Link href="./qna/create">
+          <span className="text-base font-medium text-white">
+            Post New Question
+          </span>
+        </Link>
       </Button>
     </div>
   )
