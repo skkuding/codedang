a1d689bec14e334057ea860c98097da6b4e94a50
seoin2744 <seoin2744@gmail.com>
2025-08-21 08:49:19 +0000

    fix(be): update QnACategory import path to @generated

--- apps/backend/apps/admin/src/contest/model/contest-qna.input.ts
+++ apps/backend/apps/admin/src/contest/model/contest-qna.input.ts
@@ -1,5 +1,5 @@
 import { Field, InputType, Int } from '@nestjs/graphql'
-import { QnACategory } from '@prisma/client'
+import { QnACategory } from '@generated'
 
 @InputType()
 export class UpdateContestQnAInput {
