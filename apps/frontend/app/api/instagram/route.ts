acbbc617f9292ba9f7b66a51f18285ae7be6dd67
Clover229 <jeonsumin2002@gmail.com>
2025-08-22 13:38:51 +0900

    feat(fe): use secret manager for token & make next route for instagram data

---
+++ apps/frontend/app/api/instagram/route.ts
@@ -0,0 +1,12 @@
+// frontend/app/api/instagram/route.ts
+import { fetchInstagramMedia } from '@/libs/instagram'
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  try {
+    const data = await fetchInstagramMedia()
+    return NextResponse.json(data)
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 })
+  }
+}
