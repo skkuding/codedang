144fff75fa520f855e72b6789b065853b1888c20
mingyuseong <mingyu27@naver.com>
2025-08-22 16:53:18 +0900

    fix(fe): roiute to first page when request back at registering page

--- apps/frontend/components/auth/SignUp/SignUp.tsx
+++ apps/frontend/components/auth/SignUp/SignUp.tsx
@@ -9,13 +9,15 @@ import { SignUpVerifyEmail } from './SignUpVerifyEmail'
 import { SignUpWelcome } from './SignUpWelcome'
 
 export function SignUp() {
-  const { modalPage, backModal } = useSignUpModalStore((state) => state)
+  const { modalPage, backModal, setModalPage } = useSignUpModalStore(
+    (state) => state
+  )
 
   return (
     <div className="flex h-full flex-col pb-9 pt-[46px]">
-      {modalPage !== 0 && (
+      {modalPage > 0 && (
         <button
-          onClick={backModal}
+          onClick={() => (modalPage < 2 ? backModal() : setModalPage(0))}
           className="focus:outline-hidden absolute left-5 top-[30px] h-5 w-5 opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-gray-100 data-[state=open]:text-gray-500 dark:ring-offset-gray-950 dark:focus:ring-gray-300 dark:data-[state=open]:bg-gray-800 dark:data-[state=open]:text-gray-400"
         >
           <IoMdArrowBack />
