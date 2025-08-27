51853ca1221e5ecfcb4a0afd154a8a8c1043e022
w0uldy0u <spacekim01@gmail.com>
2025-08-27 19:47:03 +0000

    chore(fe): avoid caching service worker

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -37,6 +37,27 @@ const nextConfig = {
   output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true
+  },
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-cache, no-store, must-revalidate'
+          },
+          { key: 'Pragma', value: 'no-cache' },
+          { key: 'Expires', value: '0' }
+        ]
+      },
+      {
+        source: '/:path*.webmanifest',
+        headers: [
+          { key: 'Cache-Control', value: 'no-cache, no-store, must-revalidate' }
+        ]
+      }
+    ]
   }
 } satisfies NextConfig
 
