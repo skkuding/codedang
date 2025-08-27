062398e05aacc08f229d7ecb2f0a7dad587a0ada
w0uldy0u <spacekim01@gmail.com>
2025-08-27 17:02:10 +0000

    chore(fe): test7

--- apps/frontend/app/layout.tsx
+++ apps/frontend/app/layout.tsx
@@ -53,6 +53,7 @@ export default async function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <link
           rel="apple-touch-startup-image"
+          media="screen and (device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
           href="/apple_splash_screens/iPhone_16_Pro_Max_portrait.png"
         />
         <link
