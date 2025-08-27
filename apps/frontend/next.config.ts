7f1ffac2371cabc88600aa1a14458b62fa95cf8c
w0uldy0u <spacekim01@gmail.com>
2025-08-27 20:28:56 +0000

    chore(fe): testtestestsetestsetest

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -11,21 +11,38 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   disable: process.env.NODE_ENV === 'development',
   customWorkerDir: 'worker',
   skipWaiting: true,
+
   buildExcludes: [/\/_next\/static\/chunks\/app\/.*\/page-.*\.js$/],
+
   workboxOptions: {
     disableDevLogs: true,
     exclude: [
       /sw\.js$/,
-      /workbox-(.)*\.js$/,
+      /workbox-.*\.js$/,
       /\.webmanifest$/,
-      /\/_next\/static\/chunks\/app\/.*\/page-.*\.js$/,
-      /\/worker-.*\.js$/
+      /\/worker-.*\.js$/,
+      /\/_next\/static\/chunks\/app\/.*\/page-.*\.js$/
+    ],
+
+    manifestTransforms: [
+      async (entries: { url: string; revision?: string }[]) => {
+        const re = /\/_next\/static\/chunks\/app\/.*\/page-.*\.js$/
+        const filtered = entries.filter((e) => {
+          // e.url은 %5BcontestId%5D 처럼 인코딩되어 들어올 수 있음
+          const u = decodeURIComponent(e.url)
+          return !re.test(u)
+        })
+        return { manifest: filtered, warnings: [] }
+      }
     ]
   },
+
   runtimeCaching: [
     {
       urlPattern: ({ url }: { url: URL }) =>
-        url.pathname === '/sw.js' || url.pathname.endsWith('.webmanifest'),
+        url.pathname === '/sw.js' ||
+        url.pathname.endsWith('.webmanifest') ||
+        url.pathname.startsWith('/worker-'),
       handler: 'NetworkOnly',
       method: 'GET'
     }
