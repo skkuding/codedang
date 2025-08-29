4d6f32aaeb60b3758ab9bac7a88bfa5cdfa54e2e
Jaemin Choi <1dotolee@gmail.com>
2025-08-29 22:52:25 +0900

    chore: remove prod minio

--- apps/infra/production/dns/minio.tf
+++ apps/infra/production/dns/minio.tf
@@ -1,13 +1,5 @@
 # Purpose: MinIO console UI
 
-resource "aws_route53_record" "minio" {
-  name    = "minio.codedang.com"
-  zone_id = data.aws_route53_zone.codedang.zone_id
-  type    = "A"
-  records = local.prod_cluster_ip
-  ttl     = 300
-}
-
 resource "aws_route53_record" "minio_stage" {
   name    = "minio.stage.codedang.com"
   zone_id = data.aws_route53_zone.codedang.zone_id
