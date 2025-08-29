ef608e925dd1ac914db9761de0ee7a2f79e50b03
Jaemin Choi <1dotolee@gmail.com>
2025-08-30 01:15:44 +0900

    fix(infra): assign send-email role to client-api

--- apps/infra/production/k8s-iam/client-api.tf
+++ apps/infra/production/k8s-iam/client-api.tf
@@ -42,3 +42,19 @@ resource "aws_iam_user_policy" "client_api_s3_read" {
   user   = aws_iam_user.client_api.name
   policy = data.aws_iam_policy_document.client_api_s3_read.json
 }
+
+data "aws_iam_policy_document" "client_api_ses_send" {
+  statement {
+    actions = [
+      "ses:SendEmail",
+      "ses:SendRawEmail",
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_user_policy" "client_api_ses_send" {
+  name   = "codedang-client-api-ses-send"
+  user   = aws_iam_user.client_api.name
+  policy = data.aws_iam_policy_document.client_api_ses_send.json
+}
