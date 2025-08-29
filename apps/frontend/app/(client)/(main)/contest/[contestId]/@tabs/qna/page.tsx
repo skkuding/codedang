b8a2983ba9e9d59336bdd5a5522f6c250122a9f8
오종희 <loverduck123@naver.com>
2025-08-29 14:22:20 +0900

    feat(fe): make qna main page (#3022)
    ### Description
    
    Q&A 메인 페이지를 구현했습니다.
    
    ### Additional context
    
    1. canCreateQnA: **로그인 상태이고**, 해당 대회의 운영진 또는 참여자이거나(Admin, Manager,
    Reviewer, Participant) 해당 대회가 진행 중이지 않다.
    Post 버튼을 클릭했을 때 true이면 질문글 작성 페이지로 이동하고, false이면 권한 없음 모달을 띄운다.
    
    2. isPrivilegedRole: 해당 대회의 운영진 (Admin, Manager, Reviewer)이다.
    - isPrivilegedRole가 true이면 isResolved=false인 게시물에 파란 동그라미 표시
    - isPrivilegedRole가 false이고, 로그인 상태이면 isRead=false인 게시물에 파란 동그라미 표시
    - 비로그인 상태이면 파란 동그라미 미표시
    
    3. Category 열: 해당 대회 문제 목록에 접근 권한이 있는 사용자는 A. 문제제목 / General, 없는 사용자는
    Problem/General로 각 질문글의 카테고리가 표시된다.
    
    ---
    
    + qna 폴더에 figma 디자인 가이드 반영해서 서치바 컴포넌트 수정해놓았는데, 우선 그 폴더에만 남겨둘지 아예 기존 서치바
    컴포넌트를 대체해서 별도의 브랜치에 올릴지 잘 모르겠어요
    
    ---------
    
    Co-authored-by: 오종희 <loverduck123@skku.edu>
    Co-authored-by: Kang Gyu Song <37824301+sONg20NOW@users.noreply.github.com>
    Co-authored-by: Sumin Jeon <jeonsumin2002@gmail.com>

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/qna/page.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/qna/page.tsx
@@ -1,19 +1,118 @@
-'use client'
+import { FetchErrorFallback } from '@/components/FetchErrorFallback'
+import { Skeleton } from '@/components/shadcn/skeleton'
+import { auth } from '@/libs/auth'
+import { fetcherWithAuth } from '@/libs/utils'
+import welcomeLogo from '@/public/logos/welcome.png'
+import type { ContestTop, ProblemDataTop } from '@/types/type'
+import { ErrorBoundary } from '@suspensive/react'
+import Image from 'next/image'
+import { Suspense } from 'react'
+import { LoginButton } from './_components/LoginButton'
+import { QnAMainTable } from './_components/QnAMainTable'
 
-import { Button } from '@/components/shadcn/button'
-import Link from 'next/link'
+interface ContestQnAProps {
+  params: Promise<{ contestId: number }>
+  searchParams: Promise<{
+    registered: string
+    search: string
+    orderBy: string
+    categories: string
+    problemOrders: string
+  }>
+}
 
-export default function QnaPage() {
+export default async function ContestQna(props: ContestQnAProps) {
+  const { contestId } = await props.params
+  const { searchParams } = props
+
+  const session = await auth()
+  const params = await searchParams
+  const search = params.search ?? ''
+  const orderBy = params.orderBy ?? 'desc'
+  const categories = params.categories ?? ''
+  const problemOrders = params.problemOrders ?? ''
+
+  const contest: ContestTop = await fetcherWithAuth
+    .get(`contest/${contestId}`)
+    .json()
+  const contestProblems: ProblemDataTop = await fetcherWithAuth
+    .get(`contest/${contestId}/problem`)
+    .json()
+
+  const state = (() => {
+    const currentTime = new Date()
+    const contestStart = new Date(contest.startTime)
+    const contestEnd = new Date(contest.endTime)
+    if (currentTime >= contestStart && currentTime < contestEnd) {
+      return 'Ongoing'
+    }
+  })()
+
+  const canCreateQnA =
+    session &&
+    (contest.isRegistered || contest.isPrivilegedRole || state !== 'Ongoing')
+  const isPrivilegedRole = contest.isPrivilegedRole
+
+  if (!session && state === 'Ongoing') {
+    return (
+      <div className="flex w-full max-w-7xl flex-col items-center justify-center p-5 py-48">
+        <Image src={welcomeLogo} alt="welcome" />
+        <p className="mt-10 text-2xl font-semibold">Please Login!</p>
+        <div className="mt-2 text-center text-base font-normal text-[#7F7F7F]">
+          <p>This page is only available to logged-in users.</p>
+          <p>Click the button below to login.</p>
+        </div>
+        <LoginButton className="mt-6 flex h-[46px] w-60 items-center justify-center text-base font-bold" />
+        <div className="py-5" />
+      </div>
+    )
+  }
+
   return (
-    <div className="flex w-full flex-col items-center justify-center p-8">
-      <h2 className="mb-6 text-2xl font-medium text-black">Q&A</h2>
-      <Button className="h-[46px] px-6 py-3">
-        <Link href="./qna/create">
-          <span className="text-base font-medium text-white">
-            Post New Question
-          </span>
-        </Link>
-      </Button>
+    <div className="mb-[88px] mt-[80px] max-w-[1440px] px-[116px]">
+      <ErrorBoundary fallback={FetchErrorFallback}>
+        <Suspense fallback={<QnATableFallback />}>
+          <QnAMainTable
+            session={session}
+            contestId={contestId}
+            contestProblems={contestProblems}
+            search={search}
+            orderBy={orderBy}
+            categories={categories}
+            problemOrders={problemOrders}
+            isPrivilegedRole={isPrivilegedRole}
+            canCreateQnA={canCreateQnA}
+          />
+        </Suspense>
+      </ErrorBoundary>
+    </div>
+  )
+}
+
+function QnATableFallback() {
+  return (
+    <div className="w-[1133px]">
+      <Skeleton className="h-[34px] w-[235px] gap-4" />
+      <div className="mt-[140px] flex">
+        <span className="w-2/12">
+          <Skeleton className="h-6 w-20" />
+        </span>
+        <span className="w-2/12">
+          <Skeleton className="mx-auto h-6 w-20" />
+        </span>
+        <span className="w-4/12">
+          <Skeleton className="mx-auto h-6 w-20" />
+        </span>
+        <span className="w-2/12">
+          <Skeleton className="mx-auto h-6 w-20" />
+        </span>
+        <span className="w-2/12">
+          <Skeleton className="mx-auto h-6 w-20" />
+        </span>
+      </div>
+      {[...Array(5)].map((_, i) => (
+        <Skeleton key={i} className="my-10 flex h-8 w-full rounded-xl" />
+      ))}
     </div>
   )
 }
