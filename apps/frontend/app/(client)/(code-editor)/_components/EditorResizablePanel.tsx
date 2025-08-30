dce3640c2e852e100947597df61780bacb8b1d6d
jwoojin9 <jwoojin9@naver.com>
2025-08-31 06:56:32 +0900

    fix(fe): fix assignment code editor default language

--- apps/frontend/app/(client)/(code-editor)/_components/EditorResizablePanel.tsx
+++ apps/frontend/app/(client)/(code-editor)/_components/EditorResizablePanel.tsx
@@ -124,9 +124,9 @@ export function EditorMainResizablePanel({
   const { language, setLanguage } = useLanguageStore(
     problem.id,
     contestId,
+    courseId,
     assignmentId,
-    exerciseId,
-    courseId
+    exerciseId
   )()
   const [tabValue, setTabValue] = useState('Description')
 
