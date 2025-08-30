3ca6302711e2c1aca6c5ef69ca441a9c99e1b46d
Clover229 <jeonsumin2002@gmail.com>
2025-08-30 18:50:12 +0900

    chore(fe): use break-all for very long english words

--- apps/frontend/app/(client)/(code-editor)/contest/[contestId]/problem/[problemId]/qna/_components/QnaAccordion.tsx
+++ apps/frontend/app/(client)/(code-editor)/contest/[contestId]/problem/[problemId]/qna/_components/QnaAccordion.tsx
@@ -61,7 +61,7 @@ export function QnaAccordion({ qnaData }: QnaAccordionProps) {
                   <div className="static flex h-full flex-col bg-[#121728]">
                     <div className="mb-[106px] flex-1 overflow-y-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
                       <div className="bg-[#222939] px-5 pb-5">
-                        <div className="border-1 rounded-lg border-[#FFFFFF33] px-5 py-[14px] text-base">
+                        <div className="border-1 break-all rounded-lg border-[#FFFFFF33] px-5 py-[14px] text-base">
                           {qna.content}
                         </div>
                       </div>
