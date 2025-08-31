752263ae2a58c1efeccb290a80d0dd010e270a4e
Sumin Jeon <jeonsumin2002@gmail.com>
2025-08-31 12:38:49 +0900

    feat(fe): get QnA data desc (#3063)
    ### Description
    
    1. 질문을 최신순으로 볼 수 있게끔 desc로 가져왔습니다.
    2. 배율이 조정되더라도 댓글을 달 수 있는 컴포넌트가 가장 밑으로 올 수 있도록 했습니다.
    3. 접힌 아코디언의 border 등등도 다 삭제
    4. 스크롤 커스터마이징이 안되어서 코드당의 스크롤을 바꿔버렸는데 한 번 확인해주세요..! (맥과 윈도우 스크롤이 달라서
    수정했습니다)
    
    
    ### Additional context
    
    closes TAS-2070
    ---
    
    ### Before submitting the PR, please make sure you do the following
    
    - [ ] Read the [Contributing
    Guidelines](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md)
    - [ ] Read the [Contributing
    Guidelines](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md#pr-and-branch)
    and follow the [Commit
    Convention](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md#commit-convention)
    - [ ] Provide a description in this PR that addresses **what** the PR is
    solving, or reference the issue that it solves (e.g. `fixes #123`).
    - [ ] Ideally, include relevant tests that fail without this PR but pass
    with it.
    
    ---------
    
    Co-authored-by: Kang Gyu Song <37824301+sONg20NOW@users.noreply.github.com>

--- apps/frontend/app/(client)/(code-editor)/contest/[contestId]/problem/[problemId]/qna/_components/QnaAccordion.tsx
+++ apps/frontend/app/(client)/(code-editor)/contest/[contestId]/problem/[problemId]/qna/_components/QnaAccordion.tsx
@@ -20,7 +20,6 @@ export function QnaAccordion({ qnaData }: QnaAccordionProps) {
   const [openAccordion, setOpenAccordion] = useState<string | undefined>(
     undefined
   )
-
   const handleValueChange = (value: string | undefined) => {
     setOpenAccordion(value)
   }
@@ -29,13 +28,13 @@ export function QnaAccordion({ qnaData }: QnaAccordionProps) {
     if (openAccordion === value) {
       return 'h-auto opacity-100'
     } else {
-      return 'h-0 opacity-0'
+      return 'h-0 opacity-0 border-0 p-0 m-0 overflow-hidden'
     }
   }
 
   return (
-    <ScrollArea className="h-full">
-      <div className="flex-1 overflow-hidden">
+    <ScrollArea>
+      <div>
         <Accordion
           type="single"
           collapsible
@@ -58,15 +57,17 @@ export function QnaAccordion({ qnaData }: QnaAccordionProps) {
                 <AccordionTrigger className="px-5 text-[20px] font-semibold">
                   <p>{qna.title}</p>
                 </AccordionTrigger>
-                <AccordionContent className="h-[684px] pb-0">
-                  <div className="flex h-full flex-col">
-                    <div className="border-1 mx-5 mb-5 rounded-lg border-[#FFFFFF33] px-5 py-[14px] text-base">
-                      {qna.content}
-                    </div>
-                    <div className="flex-1 overflow-y-auto">
+                <AccordionContent className="h-[calc(100vh-270px)] bg-[#121728] pb-0">
+                  <div className="static flex h-full flex-col bg-[#121728]">
+                    <div className="mb-[106px] flex-1 overflow-y-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+                      <div className="bg-[#222939] px-5 pb-5">
+                        <div className="border-1 break-all rounded-lg border-[#FFFFFF33] px-5 py-[14px] text-base">
+                          {qna.content}
+                        </div>
+                      </div>
                       <CommentsArea comments={qna.comments} />
                     </div>
-                    <div>
+                    <div className="absolute bottom-0 w-full">
                       <CreateComments qnaOrder={qna.order} />
                     </div>
                   </div>
