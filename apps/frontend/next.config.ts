021bd3dc2cc99e4df2271cbfb1f129cbbfad7e70
seoeun9 <169060897+seoeun9@users.noreply.github.com>
2025-09-01 14:58:38 +0900

    update next.config.ts

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -28,6 +28,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**.cdninstagram.com'
       }
+      {
+        protocol: 'https',
+        hostname: '**.fbcdn.net'
+      }
     ]
   },
   output: 'standalone',
