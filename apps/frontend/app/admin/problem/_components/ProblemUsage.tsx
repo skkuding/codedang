f3c738dea07a8410427695d35e0ea742876f42e8
seoeun9 <seoeun9@naver.com>
2025-09-10 21:05:30 +0900

    fix(fe): apply review comment(1)

--- apps/frontend/app/admin/problem/_components/ProblemUsage.tsx
+++ apps/frontend/app/admin/problem/_components/ProblemUsage.tsx
@@ -5,10 +5,10 @@ import { ScrollArea } from '@/components/shadcn/scroll-area'
 import { Skeleton } from '@/components/shadcn/skeleton'
 import { GET_BELONGED_ASSIGNMENTS } from '@/graphql/assignment/queries'
 import { GET_BELONGED_CONTESTS } from '@/graphql/contest/queries'
-import arrowRight from '@/public/icons/arrow-right.svg'
+import arrowRightIcon from '@/public/icons/arrow-right.svg'
 import fileInfoIcon from '@/public/icons/file-info.svg'
-import filePen from '@/public/icons/file-pen.svg'
-import infoGray from '@/public/icons/info-gray.svg'
+import filePenIcon from '@/public/icons/file-pen.svg'
+import infoGrayIcon from '@/public/icons/info-gray.svg'
 import prize from '@/public/icons/prize.svg'
 import taskComplete from '@/public/icons/task-complete.svg'
 import { useQuery } from '@apollo/client'
@@ -53,7 +53,7 @@ function HeaderSection({ label }: { label: string }) {
   return (
     <div className="flex items-center gap-[6px] self-stretch">
       <div className="bg-primary w-[6px] self-stretch rounded-[1px]" />
-      <span className="font-pretendard text-[18px] font-medium leading-[140%] tracking-[-0.54px] text-black">
+      <span className="text-[18px] font-medium leading-[140%] tracking-[-0.54px] text-black">
         {label}
       </span>
     </div>
@@ -72,16 +72,20 @@ function AssignmentProblemSection({ contents }: ProblemSectionProps) {
             >
               <div className="bg-color-neutral-99 flex items-center self-stretch rounded-[10px] px-5 py-[18px]">
                 <div className="flex items-start gap-[10px]">
-                  <Image src={filePen} alt="filePen" className="h-6 w-6" />
+                  <Image
+                    src={filePenIcon}
+                    alt="filePenIcon"
+                    className="h-6 w-6"
+                  />
 
                   <div className="flex flex-col">
                     <div className="flex items-center gap-[2px]">
-                      <span className="font-pretendard">
+                      <span>
                         [{content.title}] Week {content.week}
                       </span>
                       <Image
-                        src={arrowRight}
-                        alt="arrowRight"
+                        src={arrowRightIcon}
+                        alt="arrowRightIcon"
                         className="text-color-black h-3 w-3"
                       />
                     </div>
@@ -102,7 +106,6 @@ function AssignmentProblemSection({ contents }: ProblemSectionProps) {
 }
 
 function ContestProblemSection({ contents }: ContestSectionProps) {
-  console.log(contents)
   return (
     <div className="flex flex-col gap-[10px]">
       {contents?.map((content) => (
@@ -114,8 +117,8 @@ function ContestProblemSection({ contents }: ContestSectionProps) {
               <div className="flex items-center gap-[2px] self-stretch hover:brightness-110">
                 <span>{content.title}</span>
                 <Image
-                  src={arrowRight}
-                  alt="arrowRight"
+                  src={arrowRightIcon}
+                  alt="arrowRightIcon"
                   className="text-color-black h-3 w-3"
                 />
               </div>
@@ -128,7 +131,6 @@ function ContestProblemSection({ contents }: ContestSectionProps) {
 }
 
 function ExerciseProblemSection({ contents }: ProblemSectionProps) {
-  console.log(contents)
   return (
     <div className="flex flex-col gap-[10px]">
       {contents?.map(
@@ -148,12 +150,12 @@ function ExerciseProblemSection({ contents }: ProblemSectionProps) {
 
                   <div className="flex flex-col">
                     <div className="flex items-center gap-[2px]">
-                      <span className="font-pretendard">
+                      <span>
                         [{content.title}] Week {content.week}
                       </span>
                       <Image
-                        src={arrowRight}
-                        alt="arrowRight"
+                        src={arrowRightIcon}
+                        alt="arrowRightIcon"
                         className="text-color-black h-3 w-3"
                       />
                     </div>
@@ -178,8 +180,8 @@ function NoContentsSection({ label }: { label: string }) {
     <div className="bg-color-neutral-99 flex flex-col items-center justify-center gap-1 self-stretch rounded-lg px-5 pb-10 pt-7">
       <div className="text-color-neutral-80 relative h-7 w-7 overflow-hidden">
         <Image
-          src={infoGray}
-          alt="infoIcon"
+          src={infoGrayIcon}
+          alt="infoGrayIcon"
           className="absolute left-[2.40px] top-[2.40px] h-6 w-6"
         />
       </div>
@@ -244,7 +246,7 @@ export function ProblemUsage({ problemId }: ProblemUsageProps) {
       headerDescription="This problem is used in the following contexts"
       trigger={<Image src={fileInfoIcon} alt="fileinfo" />}
     >
-      <ScrollArea className="h-full w-full">
+      <ScrollArea className="h-full w-full pr-2">
         <div className="border-line flex min-h-0 flex-col items-start gap-[30px] self-stretch rounded-[16px] border bg-white p-[30px]">
           <div className="flex flex-col gap-3 self-stretch">
             <HeaderSection label="Assignment" />
