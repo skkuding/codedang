1a176ccd5da0d152deb633f70a6597b0bd8c3af4
Jaemin Choi <1dotolee@gmail.com>
2025-09-17 06:11:43 +0900

    chore: do not need sudo in user-data script

--- infra/aws/openvpn/bootstrap.sh
+++ infra/aws/openvpn/bootstrap.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 # Update and upgrade the system
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 
 # Install OpenVPN
 export ENDPOINT="${ip_address}"
 export AUTO_INSTALL=y
-curl -fsSL https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh | sudo -E bash
+curl -fsSL https://raw.githubusercontent.com/angristan/openvpn-install/master/openvpn-install.sh | bash
 
 # Push the client.ovpn file to AWS Secrets Manager
-sudo cp /root/client.ovpn $HOME/client.ovpn
-aws secretsmanager put-secret-value --secret-id "${secret_id}" --secret-string "file://$HOME/client.ovpn"
+aws secretsmanager put-secret-value --secret-id "${secret_id}" --secret-string "file://root/client.ovpn"
