c80dab53093c0ae52d13bc2f2833aa543ca05182
오종희 <loverduck123@skku.edu>
2025-09-23 00:06:11 +0900

    fix(fe): cleanup alert modal

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/_components/RegisteredButtonGroup.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/_components/RegisteredButtonGroup.tsx
@@ -1,11 +1,22 @@
 'use client'
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle
+} from '@/components/shadcn/alert-dialog'
 import { Button } from '@/components/shadcn/button'
 import { safeFetcherWithAuth } from '@/libs/utils'
+import warningIcon from '@/public/icons/info.svg'
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { toast } from 'sonner'
-import { AlertModal } from './AlertModal'
 
 interface UnregisteredButtonGroupProps {
   id: string
@@ -44,18 +55,37 @@ export function RegisteredButtonGroup({
       >
         Cancel Registration
       </Button>
-      <AlertModal
-        open={modalOpen}
-        onOpenChange={setModalOpen}
-        title="Cancel Registration?"
-        description={
-          'Do you really want to cancel your registration?\nYou’ll need to register again if you change your mind.'
-        }
-        primaryButton={{
-          text: 'Confirm',
-          onClick: () => clickUnregister(id)
-        }}
-      />
+      <AlertDialog open={modalOpen} onOpenChange={setModalOpen}>
+        <AlertDialogContent className="flex h-[300px] w-[424px] flex-col items-center justify-center rounded-2xl p-[40px]">
+          <AlertDialogHeader className="flex flex-col items-center justify-center">
+            <Image src={warningIcon} alt="warning" width={42} height={42} />
+            <AlertDialogTitle className="w-full text-center text-2xl font-semibold">
+              Cancel Registration?
+            </AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogDescription>
+            <p className="w-full whitespace-pre-wrap text-center text-sm font-normal text-[#737373]">
+              Do you really want to cancel your registration?
+              <br />
+              You’ll need to register again if you change your mind.
+            </p>
+          </AlertDialogDescription>
+          <AlertDialogFooter className="flex w-full justify-center gap-[4px]">
+            <AlertDialogCancel className="h-[46px] w-full">
+              Go Back
+            </AlertDialogCancel>
+            <AlertDialogAction asChild>
+              <Button
+                onClick={() => clickUnregister(id)}
+                className="bg-error h-[46px] w-full hover:bg-red-500/90"
+                variant="default"
+              >
+                Confirm
+              </Button>
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   )
 }
