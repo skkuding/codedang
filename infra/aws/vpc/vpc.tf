74e3b50abd9380eb89ff9912135837a14ebdcfd0
Jaemin Choi <1dotolee@gmail.com>
2025-09-24 22:37:59 +0900

    chore: adjust comment for vpc dns

--- infra/aws/vpc/vpc.tf
+++ infra/aws/vpc/vpc.tf
@@ -1,9 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 
-  # Not a recommended setting for production environment,
-  # but temporarily enabled to put RDS instance in public subnet.
-  # TODO: disable this and use default after setting up VPN
+  # Enabled to resolve RDS instance endpoint to private IPs
   enable_dns_support   = true
   enable_dns_hostnames = true
 
