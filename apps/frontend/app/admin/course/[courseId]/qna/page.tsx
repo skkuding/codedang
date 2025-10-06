92ead2b9203189e90e6258dd0c25a6f7b9e2c7a3
seoeun9 <seoeun9@naver.com>
2025-10-06 19:29:31 +0900

    feat(fe): create table transition tab

--- apps/frontend/app/admin/course/[courseId]/qna/page.tsx
+++ apps/frontend/app/admin/course/[courseId]/qna/page.tsx
@@ -1,12 +1,21 @@
-import { Button } from '@/components/shadcn/button'
-import { Toggle } from '@/components/shadcn/toggle'
-import Link from 'next/link'
-//import { Suspense, useState, use } from 'react'
-import { HiMiniPlusCircle } from 'react-icons/hi2'
+import { FetchErrorFallback } from '@/components/FetchErrorFallback'
+import { ErrorBoundary } from '@suspensive/react'
+import { Suspense } from 'react'
+//import { Toggle } from '@/components/shadcn/toggle'
+import {
+  AssignmentTable,
+  AssignmentTableFallback
+} from '../../_components/AssignmentTable'
+import { CourseQnaAnsweredTab } from '../_components/CourseQnaAnsweredTab'
+
+//임시로 assignmentTable 사용, 추후 QnA Table로 교체 필요
 
 export const dynamic = 'force-dynamic'
 
-export default function Page() {
+export default async function Page(props: {
+  params: Promise<{ courseId: string }>
+}) {
+  const { courseId } = await props.params
   return (
     <div className="container mx-auto space-y-5 py-10">
       <div className="gap-15 flex flex-col items-start">
@@ -15,12 +24,6 @@ export default function Page() {
             <span className="flex flex-1 text-[32px] font-bold">
               Question & Answer
             </span>
-            <Button variant="default" className="w-[97px]" asChild>
-              <Link href={`/admin/course` as const}>
-                <HiMiniPlusCircle className="mr-2 h-5 w-5" />
-                <span className="text-[16px]">Edit</span>
-              </Link>
-            </Button>
           </div>
           <span className="text-color-neutral-70 text-[16px]">
             Assignment와 Exercise 문제와 관련된 질문과 답변을 제공합니다.
@@ -28,9 +31,21 @@ export default function Page() {
         </div>
         <div className="flex flex-col items-start gap-[30ox] self-stretch">
           <span className="text-[26px] font-semibold">Questions</span>
-          <Toggle size="lg">Only show unanswered questions</Toggle>
         </div>
       </div>
+
+      <div className="flex w-full items-start justify-between">
+        <CourseQnaAnsweredTab />
+        <div className="bg-color-commmon-100 text-color-neutral-90 w-[340px] rounded-full border border-[#D8D8D8] px-3 py-[12px]">
+          <span>...Search</span>
+        </div>
+      </div>
+
+      <ErrorBoundary fallback={FetchErrorFallback}>
+        <Suspense fallback={<AssignmentTableFallback />}>
+          <AssignmentTable groupId={courseId} />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   )
 }
