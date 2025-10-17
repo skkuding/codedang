25036732b012fe433956741582daf6803353d56d
Mingyu <minngyuseong@gmail.com>
2025-10-17 20:07:31 +0900

    feat(fe): use Modal

--- apps/frontend/app/admin/problem/[problemId]/edit/_components/ScoreCautionDialog.tsx
+++ apps/frontend/app/admin/problem/[problemId]/edit/_components/ScoreCautionDialog.tsx
@@ -47,101 +47,85 @@ export function ScoreCautionDialog({
   )
 
   return (
-    <Dialog open={isOpen} onOpenChange={onCancel}>
-      <DialogContent className="max-h-[627px] w-[875px] max-w-[875px] gap-6 overflow-y-auto p-10">
-        <DialogHeader>
-          <DialogTitle>Are you sure you want to edit this problem?</DialogTitle>
-          <DialogDescription className="gap-4 whitespace-pre-line pt-4">
-            <ul className="list-decimal space-y-4 pl-4">
-              <li className="marker:text-xs marker:font-bold marker:text-black">
-                <p className="text-xs font-bold text-black">
-                  Editing the problem may affect the{' '}
-                  <span className="underline">accuracy</span> of grading
-                  results.
-                </p>
-                <ul className="list-disc py-2 pl-4 text-xs marker:text-gray-500">
-                  <li>
-                    Future submissions will be graded based on the updated
-                    problem.
-                  </li>
-                  <li>
-                    Previous submissions will retain their original grading
-                    based on the pre-edit version.
-                  </li>
-                </ul>
-              </li>
-              <li className="marker:text-xs marker:font-bold marker:text-black">
-                <p className="text-xs font-bold text-black">
-                  This problem is included in the following assignments.
-                </p>{' '}
-                <p className="text-xs font-bold text-black">
-                  <span className="underline">Please select</span> the
-                  assignments for which you want to{' '}
-                  <span className="underline">rejudge submissions</span> after
-                  editing the problem.
-                </p>
-                <ul className="list-disc py-2 pl-4 text-xs marker:text-gray-500">
-                  <li>
-                    Re-evaluate all submissions for this problem in the selected
-                    assignments.
-                  </li>
-                  <li>
-                    This ensures that submissions are graded based on the
-                    updated problem.
-                  </li>
-                </ul>
-                <ErrorBoundary fallback={FetchErrorFallback}>
-                  <Suspense fallback={<BelongedContestTableFallback />}>
-                    <BelongedContestTable
-                      problemId={problemId}
-                      onSelectedAssignmentsChange={
-                        handleSelectedAssignmentsChange
-                      }
-                    />
-                  </Suspense>
-                </ErrorBoundary>
-                <div className="my-4 border-b" />
-              </li>
-            </ul>
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter className="sm:justify-end">
-          <Button
-            type="button"
-            variant="filter"
-            className="rounded-md px-4 py-2"
-            onClick={onCancel}
-          >
-            Cancel
-          </Button>
-          <Button
-            onClick={async () => {
-              onConfirm()
+    <Modal
+      size={'lg'}
+      type={'custom'}
+      title={'Are you sure you want to edit this problem?'}
+      open={isOpen}
+      onOpenChange={onCancel}
+      primaryButton={{
+        text: 'Confirm',
+        variant: 'default',
+        onClick: async () => {
+          onConfirm()
 
-              if (selectedAssignments.length > 0) {
-                try {
-                  for (const assignment of selectedAssignments) {
-                    await rejudge({
-                      variables: {
-                        groupId: assignment.groupId,
-                        input: {
-                          assignmentId: assignment.id,
-                          problemId,
-                          mode: RejudgeMode.CreateNew
-                        }
-                      }
-                    })
+          if (selectedAssignments.length > 0) {
+            try {
+              for (const assignment of selectedAssignments) {
+                await rejudge({
+                  variables: {
+                    groupId: assignment.groupId,
+                    input: {
+                      assignmentId: assignment.id,
+                      problemId,
+                      mode: RejudgeMode.CreateNew
+                    }
                   }
-                } catch (e) {
-                  console.error('재채점 실패:', e)
-                }
+                })
               }
-            }}
-          >
-            Confirm
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+            } catch (e) {
+              console.error('재채점 실패:', e)
+            }
+          }
+        }
+      }}
+    >
+      <ul className="list-decimal space-y-4 pl-4">
+        <li className="marker:text-xs marker:font-bold marker:text-black">
+          <p className="text-xs font-bold text-black">
+            Editing the problem may affect the{' '}
+            <span className="underline">accuracy</span> of grading results.
+          </p>
+          <ul className="list-disc py-2 pl-4 text-xs marker:text-gray-500">
+            <li>
+              Future submissions will be graded based on the updated problem.
+            </li>
+            <li>
+              Previous submissions will retain their original grading based on
+              the pre-edit version.
+            </li>
+          </ul>
+        </li>
+        <li className="marker:text-xs marker:font-bold marker:text-black">
+          <p className="text-xs font-bold text-black">
+            This problem is included in the following assignments.
+          </p>{' '}
+          <p className="text-xs font-bold text-black">
+            <span className="underline">Please select</span> the assignments for
+            which you want to{' '}
+            <span className="underline">rejudge submissions</span> after editing
+            the problem.
+          </p>
+          <ul className="list-disc py-2 pl-4 text-xs marker:text-gray-500">
+            <li>
+              Re-evaluate all submissions for this problem in the selected
+              assignments.
+            </li>
+            <li>
+              This ensures that submissions are graded based on the updated
+              problem.
+            </li>
+          </ul>
+          <ErrorBoundary fallback={FetchErrorFallback}>
+            <Suspense fallback={<BelongedContestTableFallback />}>
+              <BelongedContestTable
+                problemId={problemId}
+                onSelectedAssignmentsChange={handleSelectedAssignmentsChange}
+              />
+            </Suspense>
+          </ErrorBoundary>
+        </li>
+      </ul>
+    </Modal>
   )
 }
