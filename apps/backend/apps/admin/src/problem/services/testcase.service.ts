709e92b479f10973cc757ebc2146ed5ce44459e6
w0uldy0u <spacekim01@gmail.com>
2025-10-26 10:10:27 +0000

    chore(be): query only id of sharedGroups

--- apps/backend/apps/admin/src/problem/services/testcase.service.ts
+++ apps/backend/apps/admin/src/problem/services/testcase.service.ts
@@ -736,7 +736,7 @@ export class TestcaseService {
       include: {
         problem: {
           select: {
-            sharedGroups: true
+            sharedGroups: { select: { id: true } }
           }
         }
       }
