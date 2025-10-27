30da7286fa9cc512b1068962e608f5b6a10e5b53
seoeun9 <seoeun9@naver.com>
2025-10-28 02:58:27 +0900

    feat(fe): add table skeleton and modal opening

--- apps/frontend/app/admin/course/[courseId]/qna/page.tsx
+++ apps/frontend/app/admin/course/[courseId]/qna/page.tsx
@@ -1,11 +1,9 @@
+//import { Toggle } from '@/components/shadcn/toggle'
+import { QnaTable } from '@/app/admin/course/_components/QnaTable'
 import { FetchErrorFallback } from '@/components/FetchErrorFallback'
 import { ErrorBoundary } from '@suspensive/react'
 import { Suspense } from 'react'
-//import { Toggle } from '@/components/shadcn/toggle'
-import {
-  AssignmentTable,
-  AssignmentTableFallback
-} from '../../_components/AssignmentTable'
+import { AssignmentTableFallback } from '../../_components/AssignmentTable'
 import { CourseQnaAnsweredTab } from '../_components/CourseQnaAnsweredTab'
 
 //임시로 assignmentTable 사용, 추후 QnA Table로 교체 필요
@@ -36,14 +34,15 @@ export default async function Page(props: {
 
       <div className="flex w-full items-start justify-between">
         <CourseQnaAnsweredTab />
-        <div className="bg-color-commmon-100 text-color-neutral-90 w-[340px] rounded-full border border-[#D8D8D8] px-3 py-[12px]">
+
+        {/* <div className="bg-color-commmon-100 text-color-neutral-90 w-[340px] rounded-full border border-[#D8D8D8] px-3 py-[12px]">
           <span>...Search</span>
-        </div>
+        </div> */}
       </div>
 
       <ErrorBoundary fallback={FetchErrorFallback}>
         <Suspense fallback={<AssignmentTableFallback />}>
-          <AssignmentTable groupId={courseId} />
+          <QnaTable groupId={courseId} />
         </Suspense>
       </ErrorBoundary>
     </div>
