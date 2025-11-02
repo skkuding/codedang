86be8f3fddebbc18118f48d990021ff8d24552e0
Choi-Jung-Hyeon <fourmi103@g.skku.edu>
2025-11-02 17:52:09 +0000

    refactor(be): unify category column in course_qna table and drop CourseQnACategory enum

--- apps/backend/prisma/migrations/20250924071743_add_course_qna/migration.sql
+++ apps/backend/prisma/migrations/20250924071743_add_course_qna/migration.sql
@@ -1,5 +1,8 @@
+-- CreateEnum
+CREATE TYPE "public"."CourseQnACategory" AS ENUM ('General', 'Problem');
+
 -- CreateTable
-CREATE TABLE "course_qna" (
+CREATE TABLE "public"."course_qna" (
     "id" SERIAL NOT NULL,
     "order" INTEGER NOT NULL,
     "created_by_id" INTEGER,
@@ -7,17 +10,15 @@ CREATE TABLE "course_qna" (
     "problem_id" INTEGER,
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
-    "category" "QnACategory" NOT NULL DEFAULT 'General',
+    "category" "public"."CourseQnACategory" NOT NULL DEFAULT 'General',
     "is_resolved" BOOLEAN NOT NULL DEFAULT false,
     "is_private" BOOLEAN NOT NULL DEFAULT false,
     "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "read_by" INTEGER[],
-
     CONSTRAINT "course_qna_pkey" PRIMARY KEY ("id")
 );
-
 -- CreateTable
-CREATE TABLE "course_qna_comment" (
+CREATE TABLE "public"."course_qna_comment" (
     "id" SERIAL NOT NULL,
     "order" INTEGER NOT NULL,
     "created_by_id" INTEGER,
@@ -25,27 +26,19 @@ CREATE TABLE "course_qna_comment" (
     "content" TEXT NOT NULL,
     "course_qna_id" INTEGER NOT NULL,
     "create_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
     CONSTRAINT "course_qna_comment_pkey" PRIMARY KEY ("id")
 );
-
 -- CreateIndex
-CREATE UNIQUE INDEX "course_qna_group_id_order_key" ON "course_qna"("group_id", "order");
-
+CREATE UNIQUE INDEX "course_qna_group_id_order_key" ON "public"."course_qna"("group_id", "order");
 -- CreateIndex
-CREATE UNIQUE INDEX "course_qna_comment_course_qna_id_order_key" ON "course_qna_comment"("course_qna_id", "order");
-
+CREATE UNIQUE INDEX "course_qna_comment_course_qna_id_order_key" ON "public"."course_qna_comment"("course_qna_id", "order");
 -- AddForeignKey
-ALTER TABLE "course_qna" ADD CONSTRAINT "course_qna_created_by_id_fkey" FOREIGN KEY ("created_by_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
+ALTER TABLE "public"."course_qna" ADD CONSTRAINT "course_qna_created_by_id_fkey" FOREIGN KEY ("created_by_id") REFERENCES "public"."user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 -- AddForeignKey
-ALTER TABLE "course_qna" ADD CONSTRAINT "course_qna_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
+ALTER TABLE "public"."course_qna" ADD CONSTRAINT "course_qna_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "public"."group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 -- AddForeignKey
-ALTER TABLE "course_qna" ADD CONSTRAINT "course_qna_problem_id_fkey" FOREIGN KEY ("problem_id") REFERENCES "problem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
+ALTER TABLE "public"."course_qna" ADD CONSTRAINT "course_qna_problem_id_fkey" FOREIGN KEY ("problem_id") REFERENCES "public"."problem"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 -- AddForeignKey
-ALTER TABLE "course_qna_comment" ADD CONSTRAINT "course_qna_comment_created_by_id_fkey" FOREIGN KEY ("created_by_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
+ALTER TABLE "public"."course_qna_comment" ADD CONSTRAINT "course_qna_comment_created_by_id_fkey" FOREIGN KEY ("created_by_id") REFERENCES "public"."user"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 -- AddForeignKey
-ALTER TABLE "course_qna_comment" ADD CONSTRAINT "course_qna_comment_courseQnAId_fkey" FOREIGN KEY ("course_qna_id") REFERENCES "course_qna"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."course_qna_comment" ADD CONSTRAINT "course_qna_comment_course_qna_id_fkey" FOREIGN KEY ("course_qna_id") REFERENCES "public"."course_qna"("id") ON DELETE CASCADE ON UPDATE CASCADE;
