edab7307b2d1e76712a3fb59139fb0148dd15cd1
Choi-Jung-Hyeon <fourmi103@g.skku.edu>
2025-11-08 12:07:19 +0000

    refactor(be): remove Course module and related files from admin and client

--- apps/backend/apps/admin/src/course/course.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common'
-import { CourseResolver } from './course.resolver'
-import { CourseService } from './course.service'
-
-@Module({
-  providers: [CourseResolver, CourseService]
-})
-export class CourseModule {}
