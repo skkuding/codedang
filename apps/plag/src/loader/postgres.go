ecc9f0712d56bc7b68fe7826867e9e9ad7e3a47c
Kim Hakjae <37476664+hjkim24@users.noreply.github.com>
2025-11-12 19:45:10 +0900

    fix(be): fix jplag error (#3287)
    ### Description
    
    JSON 파싱 과정에서 이미 JSON 형식으로 들어온 경우 그대로 반환하도록 수정합니다.
    
    ### Additional context
    
    <!-- e.g. is there anything you'd like reviewers to focus on? -->
    
    ---
    
    ### Before submitting the PR, please make sure you do the following
    
    - [ ] Read the [Contributing
    Guidelines](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md)
    - [ ] Read the [Contributing
    Guidelines](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md#pr-and-branch)
    and follow the [Commit
    Convention](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md#commit-convention)
    - [ ] Provide a description in this PR that addresses **what** the PR is
    solving, or reference the issue that it solves (e.g. `fixes #123`).
    - [ ] Ideally, include relevant tests that fail without this PR but pass
    with it.
    
    ---------
    
    Co-authored-by: Dohyun Ryu <113527604+RyuRaseul@users.noreply.github.com>
    Co-authored-by: hj060809 <hjbanf89@gmail.com>

--- apps/plag/src/loader/postgres.go
+++ apps/plag/src/loader/postgres.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
-	"regexp"
 	"strings"
 
 	_ "github.com/lib/pq"
@@ -30,75 +29,38 @@ func NewPostgresDataSource(ctx context.Context) (*Postgres, error) {
 	return &Postgres{ctx, db}, nil
 }
 
-func ParseRawJson(rawCode string) string {
-  cleanedString := rawCode[2 : len(rawCode)-2]
-
-  var jsonString strings.Builder
-  for _, parsedCode := range strings.Split(cleanedString, "text\\\": \\\"") {
-    pCode := strings.Split(parsedCode, "\\\", \\\"locked")
-
-    if len(pCode) == 1 {
-      jsonString.WriteString(strings.ReplaceAll(pCode[0], `\"`, `"`))
-    } else if len(pCode) == 2 {
-      jsonString.WriteString("text\": \"")
-      jsonString.WriteString(pCode[0])
-      jsonString.WriteString("\", \"locked")
-      jsonString.WriteString(strings.ReplaceAll(pCode[1], `\"`, `"`))
-    }
-  }
-
-  return jsonString.String()
-}
-
-func ParseRawCode(rawCode string) string {
-  re := regexp.MustCompile(`\\[ntr"]`)
-	codeWithNewlines := re.ReplaceAllStringFunc(rawCode, func(s string) string { // 보완이 필요합니다.
-		switch s {
-		case `\n`:
-			return "\n"
-		case `\t`:
-			return "\t"
-		case `\r`:
-			return "\r"
-    case `\"`:
-			return "\""
-		default:
-			return s
-		}
-	})
-
-  return codeWithNewlines
-}
-
 func GetAllCodes(rows *sql.Rows, problemId string) ([]Element, error) {
-  var result []Element
+	var result []Element
 
 	for rows.Next() {
 		var id int
 		var userId int
-		var rawCode string
+		var rawCode []byte
 		var createTime string
 
 		if err := rows.Scan(&id, &userId, &rawCode, &createTime); err != nil {
 			return nil, fmt.Errorf("database fetch error: %w", err)
 		}
 
-    if len(rawCode) < 4 {
-      return nil, fmt.Errorf("code length error: json is too short")
-    }
+		if len(rawCode) < 4 {
+			return nil, fmt.Errorf("code length error: json is too short")
+		}
 
-    data := []byte(ParseRawJson(rawCode))
-    var codePiece CodePiece
+		var codePieces []CodePiece
+		err := json.Unmarshal(rawCode, &codePieces)
+		if err != nil {
+			return nil, fmt.Errorf("json unmarshal: %w", err)
+		}
 
-    err := json.Unmarshal(data, &codePiece)
-    if err != nil {
-      return nil, fmt.Errorf("json unmarshal: %e", err)
+    var code strings.Builder
+    for _, codePiece := range codePieces {
+      code.WriteString(codePiece.Text)
     }
 
 		result = append(result, Element{
-			Id: id,
-			UserId: userId,
-			Code: ParseRawCode(codePiece.Text),
+			Id:         id,
+			UserId:     userId,
+			Code:       code.String(),
 			CreateTime: createTime,
 		})
 	}
@@ -111,107 +73,109 @@ func GetAllCodes(rows *sql.Rows, problemId string) ([]Element, error) {
 }
 
 func (p *Postgres) GetRawBaseCode(problemId string, language string) (string, error) {
-  baseCodeRow := p.client.QueryRow(`SELECT template FROM public.problem WHERE id = $1`, problemId)
+	baseCodeRow := p.client.QueryRow(`SELECT COALESCE(to_jsonb(template), '[]'::jsonb) FROM public.problem WHERE id = $1`, problemId)
 
-  var rawBaseCode string
-  err := baseCodeRow.Scan(&rawBaseCode)
+	var rawBaseCode []byte
+	err := baseCodeRow.Scan(&rawBaseCode)
 
-  if err == sql.ErrNoRows{
-    return "", nil
-  } else if err != nil {
-    return "", fmt.Errorf("database fetch error: %w", err)
-  }
-
-  if len(rawBaseCode) < 4 {
-    return "", nil
-  }
-
-  data := []byte(ParseRawJson(rawBaseCode))
-	var result []TemplateItem
-
-	err = json.Unmarshal(data, &result)
-	if err != nil {
-		return "", fmt.Errorf("database json unmarshal: %e", err)
+	if err == sql.ErrNoRows {
+		return "", nil
+	} else if err != nil {
+		return "", fmt.Errorf("database fetch error: %w", err)
 	}
 
-  var codeBuilder strings.Builder
-  for _, r := range result {
-    if r.Language == language {
-      for _, c := range r.Code {
-        codeBuilder.WriteString(ParseRawCode(c.Text))
-        codeBuilder.WriteString("\n")
-      }
-    }
-  }
+	var arrayData []TemplateItem
+	if err := json.Unmarshal(rawBaseCode, &arrayData); err != nil {
+		return "", fmt.Errorf("database json unmarshal: %w", err)
+	}
 
-  return codeBuilder.String(), nil
+	var baseCode strings.Builder
+	for _, item := range arrayData {
+		if item.Language == language {
+			for _, codePiece := range item.Code {
+				baseCode.WriteString(codePiece.Text)
+			}
+			return baseCode.String(), nil
+		}
+	}
+
+	return "", nil
 }
 
 func (p *Postgres) GetAllCodesFromAssignment(problemId string, language string, assignmentId string) (string, []Element, error) {
-  rows, err := p.client.Query(`SELECT DISTINCT ON (user_id) id, user_id, code, create_time FROM public.submission WHERE problem_id = $1 AND language = $2 AND assignment_id = $3 ORDER BY user_id, update_time DESC`, problemId, language, assignmentId)
-  if err != nil {
+	rows, err := p.client.Query(`SELECT id, user_id, COALESCE(to_jsonb(code), '[]'::jsonb), create_time
+                               FROM public.submission
+                               WHERE problem_id = $1 AND language = $2 AND assignment_id = $3
+                               ORDER BY user_id, update_time DESC`, problemId, language, assignmentId)
+	if err != nil {
 		return "", nil, fmt.Errorf("failed to get data: %w", err)
 	}
 
-  defer rows.Close()
+	defer rows.Close()
 
-  codes, err := GetAllCodes(rows, problemId)
+	codes, err := GetAllCodes(rows, problemId)
 
-  if err != nil {
+	if err != nil {
 		return "", nil, fmt.Errorf("failed to get data: %w", err)
 	}
 
-  baseCode, err := p.GetRawBaseCode(problemId, language)
+	baseCode, err := p.GetRawBaseCode(problemId, language)
 
-  if err != nil {
+	if err != nil {
 		return "", nil, fmt.Errorf("failed to get data: %w", err)
 	}
 
-  return baseCode, codes, nil
+	return baseCode, codes, nil
 }
 
 func (p *Postgres) GetAllCodesFromContest(problemId string, language string, contestId string) (string, []Element, error) {
-  rows, err := p.client.Query(`SELECT DISTINCT ON (user_id) id, user_id, code, create_time FROM public.submission WHERE problem_id = $1 AND language = $2 AND contest_id = $3 ORDER BY user_id, update_time DESC`, problemId, language, contestId)
-  if err != nil {
+	rows, err := p.client.Query(`SELECT DISTINCT ON (user_id) id, user_id, COALESCE(to_jsonb(code), '[]'::jsonb), create_time
+                              FROM public.submission
+                              WHERE problem_id = $1 AND language = $2 AND contest_id = $3
+                              ORDER BY user_id, update_time DESC`, problemId, language, contestId)
+	if err != nil {
 		return "", nil, fmt.Errorf("failed to get data: %w", err)
 	}
 
-  defer rows.Close()
+	defer rows.Close()
 
-  codes, err := GetAllCodes(rows, problemId)
+	codes, err := GetAllCodes(rows, problemId)
 
-  if err != nil {
+	if err != nil {
 		return "", nil, fmt.Errorf("failed to get data: %w", err)
 	}
 
-  baseCode, err := p.GetRawBaseCode(problemId, language)
+	baseCode, err := p.GetRawBaseCode(problemId, language)
 
-  if err != nil {
+	if err != nil {
 		return "", nil, fmt.Errorf("failed to get data: %w", err)
 	}
 
-  return baseCode, codes, nil
+	return baseCode, codes, nil
 }
 
 func (p *Postgres) GetAllCodesFromWorkbook(problemId string, language string, workbookId string) (string, []Element, error) {
-  rows, err := p.client.Query(`SELECT DISTINCT ON (user_id) id, user_id, code, create_time FROM public.submission WHERE problem_id = $1 AND language = $2 AND workbook_id = $3 ORDER BY user_id, update_time DESC`, problemId, language, workbookId)
-  if err != nil {
+	rows, err := p.client.Query(`SELECT DISTINCT ON (user_id) id, user_id, COALESCE(to_jsonb(code), '[]'::jsonb), create_time
+                              FROM public.submission
+                              WHERE problem_id = $1 AND language = $2 AND workbook_id = $3
+                              ORDER BY user_id, update_time DESC`, problemId, language, workbookId)
+	if err != nil {
 		return "", nil, fmt.Errorf("failed to get data: %w", err)
 	}
 
-  defer rows.Close()
+	defer rows.Close()
 
-  codes, err := GetAllCodes(rows, problemId)
+	codes, err := GetAllCodes(rows, problemId)
 
-  if err != nil {
+	if err != nil {
 		return "", nil, fmt.Errorf("failed to get data: %w", err)
 	}
 
-  baseCode, err := p.GetRawBaseCode(problemId, language)
+	baseCode, err := p.GetRawBaseCode(problemId, language)
 
-  if err != nil {
+	if err != nil {
 		return "", nil, fmt.Errorf("failed to get data: %w", err)
 	}
 
-  return baseCode, codes, nil
+	return baseCode, codes, nil
 }
