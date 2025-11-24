168673098b47b697609a615840d97c4f2a464d13
Seungmin Nam <nhjbest22@g.skku.edu>
2025-11-24 13:35:57 +0900

    feat(be): add getSubmissionResult query (#3292)
    ### Description
    
    <!-- Please insert your description here and provide especially info
    about the "what" this PR is solving -->
    
    `submissionId`와 `testcaseId`를 이용해 단일 `Submission`에 대한 결과를 반환하는 GraphQL
    API를 구현했습니다.
    
    ### Additional context
    
    <!-- e.g. is there anything you'd like reviewers to focus on? -->
    
    Closes TAS-2264
    
    ---
    
    ### Before submitting the PR, please make sure you do the following
    
    - [ ] Read the [Contributing
    Guidelines](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md)
    - [ ] Read the [Contributing
    Guidelines](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md#pr-and-branch)
    and follow the [Commit
    Convention](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md#commit-convention)
    - [ ] Provide a description in this PR that addresses **what** the PR is
    solving, or reference the issue that it solves (e.g. `fixes #123`).
    - [ ] Ideally, include relevant tests that fail without this PR but pass
    with it.
    
    ---------
    
    Co-authored-by: Dohyun Ryu <113527604+RyuRaseul@users.noreply.github.com>

--- apps/backend/apps/admin/src/submission/submission.service.ts
+++ apps/backend/apps/admin/src/submission/submission.service.ts
@@ -38,6 +38,7 @@ import type {
 } from './model/get-submissions.input'
 import { RejudgeResult } from './model/rejudge-result.output'
 import { RejudgeInput, RejudgeMode } from './model/rejudge.input'
+import type { SubmissionResultOutput } from './model/submission-result.model'
 import type { SubmissionsWithTotal } from './model/submissions-with-total.output'
 
 @Injectable()
@@ -609,6 +610,77 @@ export class SubmissionService {
       return `Problem_${problemId}`
     }
     return encodeURIComponent(problem.title)
+  }
+
+  async getSubmissionResult(
+    submissionId: number,
+    testcaseId: number,
+    reqUser: AuthenticatedUser
+  ): Promise<SubmissionResultOutput> {
+    const hasPrivilege = reqUser.isAdmin() || reqUser.isSuperAdmin()
+
+    if (!hasPrivilege) {
+      const submission = await this.prisma.submission.findFirst({
+        where: {
+          id: submissionId
+        },
+        select: {
+          assignment: {
+            select: { groupId: true }
+          },
+          contestId: true
+        }
+      })
+
+      if (!submission) throw new EntityNotExistException('Submission')
+
+      if (submission.assignment) {
+        const isGroupLeader = await this.prisma.userGroup.findFirst({
+          where: {
+            groupId: submission.assignment.groupId,
+            userId: reqUser.id,
+            isGroupLeader: true
+          }
+        })
+
+        if (!isGroupLeader)
+          throw new ForbiddenAccessException(
+            `Only allowed to access submissions included in your group`
+          )
+      } else if (submission.contestId) {
+        const isContestManager = await this.prisma.userContest.findFirst({
+          where: {
+            contestId: submission.contestId,
+            userId: reqUser.id,
+            role: {
+              not: ContestRole.Participant
+            }
+          }
+        })
+
+        if (!isContestManager)
+          throw new ForbiddenAccessException(
+            `Only allowed to access your contest`
+          )
+      }
+    }
+
+    const submissionResult = await this.prisma.submissionResult.findFirst({
+      where: {
+        submissionId,
+        problemTestcaseId: testcaseId
+      }
+    })
+
+    if (!submissionResult) throw new EntityNotExistException('SubmissionResult')
+
+    return {
+      ...submissionResult,
+      cpuTime:
+        submissionResult.cpuTime || submissionResult.cpuTime === BigInt(0)
+          ? submissionResult.cpuTime.toString()
+          : null
+    }
   }
 
   async compressSourceCodes(assignmentId: number, problemId: number) {
