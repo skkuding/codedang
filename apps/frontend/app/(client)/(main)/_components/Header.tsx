c2888d26d1c52cd5a7be2342e658576357492462
eggz6z6 <kazy0606@gmail.com>
2025-11-27 15:31:16 +0900

    feat(fe): change position of sidebar, logo, alarm

--- apps/frontend/app/(client)/(main)/_components/Header.tsx
+++ apps/frontend/app/(client)/(main)/_components/Header.tsx
@@ -15,8 +15,9 @@ export function Header() {
   return (
     <header className="backdrop-blur-xs fixed left-0 z-40 grid h-[60px] w-full place-items-center bg-white px-5 lg:bg-white/80 lg:px-[30px]">
       <div className="flex w-full max-w-[1440px] items-center justify-between gap-5 lg:px-[116px]">
-        <div className="flex items-center gap-4 lg:gap-8">
-          <div className="sm:hidden">
+        {/* Mobile */}
+        <div className="flex w-full items-center justify-between sm:hidden">
+          <div>
             {headerTitle ? (
               <span className="max-w-[200px] truncate font-semibold">
                 {headerTitle}
@@ -33,8 +34,35 @@ export function Header() {
               </Link>
             )}
           </div>
+          <div className="flex items-center gap-3">
+            <HeaderAuthPanel session={session} />
+            <MobileMenu session={session} />
+          </div>
+        </div>
 
-          <Link className="hidden sm:block" href="/">
+        {/* FIXME: If you uncomment a group tab, you have to remove a pr-20 tailwind class */}
+        {/* sm 이상: 기존 구조 유지 */}
+        <div className="hidden w-full min-w-fit items-center justify-start gap-8 text-base sm:flex sm:justify-between lg:w-fit lg:justify-start">
+          <MobileMenu session={session} />
+
+          <div className="lg:hidden">
+            {headerTitle ? (
+              <span className="max-w-[200px] truncate font-semibold">
+                {headerTitle}
+              </span>
+            ) : (
+              <Link href="/">
+                <Image
+                  src={codedangLogo}
+                  alt="코드당"
+                  width={135.252}
+                  height={28}
+                  className="cursor-pointer"
+                />
+              </Link>
+            )}
+          </div>
+          <Link className="hidden lg:block" href="/">
             <Image
               src={codedangLogo}
               alt="코드당"
@@ -43,18 +71,21 @@ export function Header() {
               className="cursor-pointer"
             />
           </Link>
-
           <nav className="hidden font-semibold capitalize lg:flex lg:gap-10">
             <NavLink href="/notice" text="NOTICE" />
             <NavLink href="/contest" text="CONTEST" />
             <NavLink href="/problem" text="PROBLEM" />
             <NavLink href="/course" text="COURSE" />
+            {/* TODO: Uncomment a group tab when we start to implement a group feature*/}
+            {/* <NavLink href="/group" text="Group" /> */}
           </nav>
+          <div className="lg:hidden">
+            <HeaderAuthPanel session={session} />
+          </div>
         </div>
 
-        <div className="flex items-center gap-3">
+        <div className="hidden lg:flex">
           <HeaderAuthPanel session={session} />
-          <MobileMenu session={session} />
         </div>
       </div>
     </header>
