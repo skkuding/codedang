37887b63406da6c1d3b5989f87547c913aa15757
eggz6z6 <kazy0606@gmail.com>
2025-12-07 15:49:39 +0900

    feat(fe): modify request

--- apps/frontend/app/(client)/(main)/course/[courseId]/assignment/[assignmentId]/page.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/assignment/[assignmentId]/page.tsx
@@ -94,77 +94,59 @@ export default function AssignmentDetail(props: AssignmentDetailProps) {
   }
 
   return (
-    <div className="flex flex-col gap-[45px] px-4 py-[80px] lg:px-[100px]">
-      <div className="flex flex-col gap-4 lg:flex-row lg:justify-between">
-        <div className="flex flex-col gap-4 lg:gap-7">
-          <p className="text-2xl font-semibold">
-            <span className="text-primary">[Week {assignment.week}] </span>
-            {assignment.title}
-          </p>
-          {record && <TotalScoreLabel record={record} />}
-        </div>
-        <AssignmentStatus
-          startTime={assignment.startTime}
-          endTime={new Date(assignment.dueTime ?? assignment.endTime)}
-          title="Duration"
-        />
-      </div>
-
-      <Separator className="my-0" />
-
-      <div className="flex flex-col gap-[30px]">
-        <p className="text-2xl font-semibold">DESCRIPTION</p>
-        <KatexContent
-          content={assignment.description}
-          classname="text-[#7F7F7F] font-normal text-base"
-        />
-      </div>
-
-      <Separator className="my-0" />
-
-      {problems && (
-        <div>
-          <p className="mb-[16px] text-2xl font-semibold">PROBLEM(S)</p>
-          <div className="flex gap-1 text-base font-semibold lg:mb-[42px]">
-            <span>Total</span>
-            <span className="text-primary">{problems.total}</span>
-            {record && (
-              <>
-                <span>Submit</span>
-                <span className="text-primary">
-                  {submissions?.filter((s) => s.submission !== null).length ??
-                    0}
-                </span>
-              </>
-            )}
+    assignment && (
+      <div className="flex flex-col gap-[45px] px-4 py-[80px] lg:px-[100px]">
+        <div className="flex flex-col gap-4 lg:flex-row lg:justify-between">
+          <div className="flex flex-col gap-4 lg:gap-7">
+            <p className="text-2xl font-semibold">
+              <span className="text-primary">[Week {assignment.week}] </span>
+              {assignment.title}
+            </p>
+            {record && <TotalScoreLabel record={record} />}
           </div>
-        </div>
-      )}
-
-      {!(record && submissions) && problems && (
-        <div className="hidden lg:block">
-          <DataTable
-            data={problems.data}
-            columns={problemColumns()}
-            headerStyle={{
-              order: 'w-[10%]',
-              title: 'text-left w-[40%]',
-              submissions: 'w-[20%]',
-              tc_result: 'w-[20%]',
-              detail: 'w-[10%]'
-            }}
-            linked
-            pathSegment={'problem'}
+          <AssignmentStatus
+            startTime={assignment.startTime}
+            endTime={new Date(assignment.dueTime ?? assignment.endTime)}
+            title="Duration"
           />
         </div>
-      )}
 
-      {record && submissions && (
-        <>
+        <Separator className="my-0" />
+
+        <div className="flex flex-col gap-[30px]">
+          <p className="text-2xl font-semibold">DESCRIPTION</p>
+          <KatexContent
+            content={assignment.description}
+            classname="text-[#7F7F7F] font-normal text-base"
+          />
+        </div>
+
+        <Separator className="my-0" />
+
+        {problems && (
+          <div>
+            <p className="mb-[16px] text-2xl font-semibold">PROBLEM(S)</p>
+            <div className="flex gap-1 text-base font-semibold lg:mb-[42px]">
+              <span>Total</span>
+              <span className="text-primary">{problems.total}</span>
+              {record && (
+                <>
+                  <span>Submit</span>
+                  <span className="text-primary">
+                    {submissions?.filter((s) => s.submission !== null).length ??
+                      0}
+                  </span>
+                </>
+              )}
+            </div>
+          </div>
+        )}
+
+        {!(record && submissions) && problems && (
           <div className="hidden lg:block">
             <DataTable
-              data={record.problems}
-              columns={columns(record, assignment, courseId, submissions)}
+              data={problems.data}
+              columns={problemColumns()}
               headerStyle={{
                 order: 'w-[10%]',
                 title: 'text-left w-[40%]',
@@ -176,24 +158,44 @@ export default function AssignmentDetail(props: AssignmentDetailProps) {
               pathSegment={'problem'}
             />
           </div>
+        )}
 
-          <div className="lg:hidden">
-            <div className="space-y-3">
-              {record.problems.map((problem, index) => (
-                <ProblemCard
-                  key={problem.id}
-                  problem={problem}
-                  parentItem={assignment}
-                  submissions={submissions}
-                  index={index}
-                  courseId={courseId}
-                  type="assignment"
-                />
-              ))}
+        {record && submissions && (
+          <>
+            <div className="hidden lg:block">
+              <DataTable
+                data={record.problems}
+                columns={columns(record, assignment, courseId, submissions)}
+                headerStyle={{
+                  order: 'w-[10%]',
+                  title: 'text-left w-[40%]',
+                  submissions: 'w-[20%]',
+                  tc_result: 'w-[20%]',
+                  detail: 'w-[10%]'
+                }}
+                linked
+                pathSegment={'problem'}
+              />
             </div>
-          </div>
-        </>
-      )}
-    </div>
+
+            <div className="lg:hidden">
+              <div className="space-y-3">
+                {record.problems.map((problem, index) => (
+                  <ProblemCard
+                    key={problem.id}
+                    problem={problem}
+                    parentItem={assignment}
+                    submissions={submissions}
+                    index={index}
+                    courseId={courseId}
+                    type="assignment"
+                  />
+                ))}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    )
   )
 }
--- apps/frontend/app/(client)/(main)/course/[courseId]/exercise/[exerciseId]/page.tsx
+++ apps/frontend/app/(client)/(main)/course/[courseId]/exercise/[exerciseId]/page.tsx
@@ -103,85 +103,67 @@ export default function ExerciseDetail(props: ExerciseDetailProps) {
   }
 
   return (
-    <div className="flex flex-col gap-[45px] px-4 py-[80px] lg:px-[100px]">
-      <div className="flex flex-col gap-4 lg:flex-row lg:justify-between">
-        <p className="text-2xl font-semibold">
-          <span className="text-primary">[Week {exercise.week}] </span>
-          {exercise.title}
-        </p>
-        <AssignmentStatus
-          startTime={exercise.startTime}
-          endTime={new Date(exercise.dueTime ?? exercise.endTime)}
-          title="Duration"
-        />
-      </div>
-
-      <Separator className="my-0" />
-
-      <div className="flex flex-col gap-[30px]">
-        <p className="text-2xl font-semibold">DESCRIPTION</p>
-        <KatexContent
-          content={exercise.description}
-          classname="text-[#7F7F7F] font-normal text-base"
-        />
-      </div>
-
-      <Separator className="my-0" />
-
-      {problems && (
-        <div>
-          <p className="mb-[16px] text-2xl font-semibold">PROBLEMS</p>
-          <div className="flex gap-[30px] lg:mb-[42px]">
-            <div className="flex gap-[6px]">
-              <span className="rounded-full bg-gray-100 px-[25px] py-[2px] text-center text-sm font-normal">
-                Total
-              </span>
-              <span className="text-primary text-base font-semibold">
-                {problems.total}
-              </span>
-            </div>
-            {record && (
-              <div className="flex gap-[6px]">
-                <span className="rounded-full bg-gray-100 px-[25px] py-[2px] text-center text-sm font-normal">
-                  Submit
-                </span>
-                <span className="text-primary text-base font-semibold">
-                  {
-                    (submissions || []).filter(
-                      (submission) => submission.submission !== null
-                    ).length
-                  }
-                </span>
-              </div>
-            )}
-          </div>
-        </div>
-      )}
-
-      {!(record && submissions) && problems && (
-        <div className="hidden lg:block">
-          <DataTable
-            data={problems.data}
-            columns={problemColumns()}
-            headerStyle={{
-              order: 'w-[10%]',
-              title: 'text-left w-[40%]',
-              submissions: 'w-[20%]',
-              tc_result: 'w-[20%]',
-              detail: 'w-[10%]'
-            }}
-            linked
-            pathSegment={'problem'}
+    exercise && (
+      <div className="flex flex-col gap-[45px] px-4 py-[80px] lg:px-[100px]">
+        <div className="flex flex-col gap-4 lg:flex-row lg:justify-between">
+          <p className="text-2xl font-semibold">
+            <span className="text-primary">[Week {exercise.week}] </span>
+            {exercise.title}
+          </p>
+          <AssignmentStatus
+            startTime={exercise.startTime}
+            endTime={new Date(exercise.dueTime ?? exercise.endTime)}
+            title="Duration"
           />
         </div>
-      )}
 
-      {record && submissions && (
-        <>
+        <Separator className="my-0" />
+
+        <div className="flex flex-col gap-[30px]">
+          <p className="text-2xl font-semibold">DESCRIPTION</p>
+          <KatexContent
+            content={exercise.description}
+            classname="text-[#7F7F7F] font-normal text-base"
+          />
+        </div>
+
+        <Separator className="my-0" />
+
+        {problems && (
+          <div>
+            <p className="mb-[16px] text-2xl font-semibold">PROBLEMS</p>
+            <div className="flex gap-[30px] lg:mb-[42px]">
+              <div className="flex gap-[6px]">
+                <span className="rounded-full bg-gray-100 px-[25px] py-[2px] text-center text-sm font-normal">
+                  Total
+                </span>
+                <span className="text-primary text-base font-semibold">
+                  {problems.total}
+                </span>
+              </div>
+              {record && (
+                <div className="flex gap-[6px]">
+                  <span className="rounded-full bg-gray-100 px-[25px] py-[2px] text-center text-sm font-normal">
+                    Submit
+                  </span>
+                  <span className="text-primary text-base font-semibold">
+                    {
+                      (submissions || []).filter(
+                        (submission) => submission.submission !== null
+                      ).length
+                    }
+                  </span>
+                </div>
+              )}
+            </div>
+          </div>
+        )}
+
+        {!(record && submissions) && problems && (
           <div className="hidden lg:block">
             <DataTable
-              data={record.problems}
-              columns={columns(record, exercise, courseId, submissions)}
+              data={problems.data}
+              columns={problemColumns()}
               headerStyle={{
                 order: 'w-[10%]',
                 title: 'text-left w-[40%]',
@@ -193,24 +175,44 @@ export default function ExerciseDetail(props: ExerciseDetailProps) {
               pathSegment={'problem'}
             />
           </div>
+        )}
 
-          <div className="lg:hidden">
-            <div className="space-y-3">
-              {record.problems.map((problem, index) => (
-                <ProblemCard
-                  key={problem.id}
-                  problem={problem}
-                  parentItem={exercise}
-                  submissions={submissions}
-                  index={index}
-                  courseId={courseId}
-                  type="exercise"
-                />
-              ))}
+        {record && submissions && (
+          <>
+            <div className="hidden lg:block">
+              <DataTable
+                data={record.problems}
+                columns={columns(record, exercise, courseId, submissions)}
+                headerStyle={{
+                  order: 'w-[10%]',
+                  title: 'text-left w-[40%]',
+                  submissions: 'w-[20%]',
+                  tc_result: 'w-[20%]',
+                  detail: 'w-[10%]'
+                }}
+                linked
+                pathSegment={'problem'}
+              />
             </div>
-          </div>
-        </>
-      )}
-    </div>
+
+            <div className="lg:hidden">
+              <div className="space-y-3">
+                {record.problems.map((problem, index) => (
+                  <ProblemCard
+                    key={problem.id}
+                    problem={problem}
+                    parentItem={exercise}
+                    submissions={submissions}
+                    index={index}
+                    courseId={courseId}
+                    type="exercise"
+                  />
+                ))}
+              </div>
+            </div>
+          </>
+        )}
+      </div>
+    )
   )
 }
