8263834554f7537e425199c976979cecc56f9aef
Clover229 <jeonsumin2002@gmail.com>
2025-12-21 23:03:38 +0900

    chore(fe): make leaderboard using grid

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/statistics/_components/_leaderboardcomponents/LeaderBoardTable.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/statistics/_components/_leaderboardcomponents/LeaderBoardTable.tsx
@@ -51,82 +51,108 @@ export function LeaderBoardTable({ users, problems }: LeaderBoardTableProps) {
   const filteredUsers = showOnlySelected
     ? users.filter((user) => selectedUserIds.has(user.userId.toString()))
     : users
+  const GRID_COLUMNS = `
+    60px
+    60px
+    220px
+    120px
+    1fr
+  `
 
   return (
-    <Table className="mt-15 !border-separate border-spacing-2">
-      <TableHeader className="w-full border-b-0">
-        <TableRow className="h-10">
-          <TableHead className="bg-color-neutral-99 w-15 rounded-full text-base font-medium">
-            Mark
-          </TableHead>
-          <TableHead className="bg-color-neutral-99 w-15 rounded-full text-base font-medium">
-            Rank
-          </TableHead>
-          <TableHead className="bg-color-neutral-99 w-55 rounded-full text-base font-medium">
-            Participants
-          </TableHead>
-          <TableHead className="bg-color-neutral-99 w-30 rounded-full text-base font-medium">
-            Penalty
-          </TableHead>
-          {problems.contestProblem.map((problem) => (
-            <TableHead
-              key={problem.problemId}
-              className="bg-color-neutral-99 rounded-full text-center text-base font-medium"
-            >
-              {problem.problemOrder}
-            </TableHead>
-          ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody>
+    <div>
+      {/* header */}
+      <div
+        className="mt-15 grid gap-1"
+        style={{
+          gridTemplateColumns: GRID_COLUMNS
+        }}
+      >
+        <div className="bg-color-neutral-99 text-color-neutral-60 flex h-10 items-center justify-center rounded-full text-base font-medium tracking-[-0.03em]">
+          Mark
+        </div>
+        <div className="bg-color-neutral-99 text-color-neutral-60 flex h-10 items-center justify-center rounded-full text-base font-medium tracking-[-0.03em]">
+          Rank
+        </div>
+        <div className="bg-color-neutral-99 text-color-neutral-60 flex h-10 items-center justify-center rounded-full text-base font-medium tracking-[-0.03em]">
+          Participant
+        </div>
+        <div className="bg-color-neutral-99 text-color-neutral-60 flex h-10 items-center justify-center rounded-full text-base font-medium tracking-[-0.03em]">
+          Penalty
+        </div>
+        <div className="bg-color-neutral-99 flex h-10 items-center rounded-full px-4">
+          <div className="flex w-full items-center justify-start gap-2">
+            {problems.contestProblem.map((problem) => (
+              <div
+                key={problem.problemId}
+                className="text-color-neutral-60 flex h-full w-[80px] shrink-0 items-center justify-center text-base font-medium tracking-[-0.03em]"
+              >
+                {problem.problemOrder}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {/* body */}
+      <div className="mt-2 flex flex-col gap-2">
         {filteredUsers.map((user) => (
-          <TableRow key={user.userId}>
-            <TableCell className="border-0">
+          <div
+            key={user.userId}
+            className="h-18 grid items-center gap-1 rounded-full bg-white"
+            style={{ gridTemplateColumns: GRID_COLUMNS }}
+          >
+            <div className="flex justify-center">
               <Checkbox
                 onClick={(e) => e.stopPropagation()}
                 checked={isSelected(user.userId.toString())}
                 onCheckedChange={() => toggleUser(user.userId.toString())}
                 aria-label="Select row"
-                className="flex translate-y-[2px] justify-self-center"
+                className="translate-y-[2px]"
               />
-            </TableCell>
-            <TableCell className="border-0 text-center">
-              {user.userRank}
-            </TableCell>
-            <TableCell className="border-0">{user.userName}</TableCell>
-            <TableCell className="border-0 text-center">
-              - {user.totalPenalty}
-            </TableCell>
-            {problems.contestProblem.map((problem) => {
-              const detail = user.problemDetails[problem.problemId.toString()]
-              const { attempts, penalty, judgeResult } = detail
-              if (judgeResult === 'NoAttempt') {
+            </div>
+
+            <div className="text-center text-base">{user.userRank}</div>
+
+            <div className="truncate px-5 py-5 text-left text-base">
+              {user.userName}
+            </div>
+
+            <div className="text-center text-base">-{user.totalPenalty}</div>
+
+            <div className="flex w-full items-center justify-start gap-2 p-4">
+              {problems.contestProblem.map((problem) => {
+                const detail = user.problemDetails[problem.problemId.toString()]
+                const { attempts, penalty, judgeResult } = detail
+
+                if (judgeResult === 'NoAttempt') {
+                  return (
+                    <div
+                      key={problem.problemId}
+                      className="flex w-[80px] shrink-0 justify-center text-sm text-neutral-400"
+                    >
+                      -
+                    </div>
+                  )
+                }
+
                 return (
-                  <TableCell
+                  <div
                     key={problem.problemId}
-                    className="border-0 text-center"
+                    className="flex w-[80px] shrink-0 flex-col items-center text-sm"
                   >
-                    -
-                  </TableCell>
+                    {judgeResult === 'Accepted' ? (
+                      <div>- {penalty}</div>
+                    ) : (
+                      <div className="text-red-500">Wrong</div>
+                    )}
+                    <div className="text-xs text-neutral-500">{attempts}</div>
+                  </div>
                 )
-              }
-              return (
-                <TableCell
-                  key={problem.problemId}
-                  className="border-0 text-center text-sm"
-                >
-                  {judgeResult === 'Accepted' ? (
-                    <div> - {penalty}</div>
-                  ) : (
-                    <div> Wrong</div>
-                  )}
-                  <div>{attempts}</div>
-                </TableCell>
-              )
-            })}
-          </TableRow>
+              })}
+            </div>
+          </div>
         ))}
-      </TableBody>
-    </Table>
+      </div>
+    </div>
   )
 }
