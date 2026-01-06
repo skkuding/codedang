2bfa700a3e3a709313dc5d35b2a583c1d2258237
junhyunlee123 <tedjoon@icloud.com>
2026-01-06 12:56:45 +0000

    feat(infra): cleanup after onboarding

--- infra/aws/dns/_onboarding.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_route53_record" "example-react" {
-  name    = "example-react.first-task.codedang.com"
-  zone_id = data.aws_route53_zone.codedang.zone_id
-  type    = "A"
-  records = local.stage_cluster_ip
-  ttl     = 300
-}
