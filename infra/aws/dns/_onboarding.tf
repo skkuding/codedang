ae253e2788df977cc5b8c113f6d2efd18c406f16
Tasoo Park <tasoo1118@gmail.com>
2026-01-13 17:00:23 +0900

    infra(feat): change ingress clas to traefik and add route53 DNS entry

--- infra/aws/dns/_onboarding.tf
+++ infra/aws/dns/_onboarding.tf
@@ -1,5 +1,5 @@
-resource "aws_route53_record" "example-react" {
-  name    = "example-react.first-task.codedang.com"
+resource "aws_route53_record" "taesoo-tabby-cat" {
+  name    = "taesoo-tabby-cat.first-task.codedang.com"
   zone_id = data.aws_route53_zone.codedang.zone_id
   type    = "A"
   records = local.stage_cluster_ip
