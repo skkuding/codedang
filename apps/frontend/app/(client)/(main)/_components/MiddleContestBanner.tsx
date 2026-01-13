06a5828d33702ab26bae2800302e39aec9fb8e05
hyeonwooseo <woo943@g.skku.edu>
2026-01-13 14:46:31 +0900

    chore(fe): switch banners for mobile mainpage

--- apps/frontend/app/(client)/(main)/_components/MiddleContestBanner.tsx
+++ apps/frontend/app/(client)/(main)/_components/MiddleContestBanner.tsx
@@ -14,7 +14,7 @@ export function MiddleContestBanner() {
         <ImageBox className={'h-full flex-1'} />
       </div>
       {/* Mobile View */}
-      <div className="w-full lg:hidden">
+      <div className="w-full md:hidden">
         <Link href={`/notice/${contestNoticeId}`} className="block w-full">
           <div className="relative h-[132px] w-full overflow-hidden">
             <Image
