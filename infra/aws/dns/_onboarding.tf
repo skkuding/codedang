0728f47f1609260cec5136d02f7927170df69468
Clover229 <jeonsumin2002@gmail.com>
2026-01-13 19:57:17 +0900

    feat(infra): make tf code for route53

--- infra/aws/dns/_onboarding.tf
+++ infra/aws/dns/_onboarding.tf
@@ -1,5 +1,5 @@
-resource "aws_route53_record" "example-react" {
-  name    = "example-react.first-task.codedang.com"
+resource "aws_route53_record" "sumin-onboarding" {
+  name    = "week2-restaurants.sumin-onboarding.stage.codedang.com"
   zone_id = data.aws_route53_zone.codedang.zone_id
   type    = "A"
   records = local.stage_cluster_ip
