7889c00a26c28cdef2acc0eaf436fe59f4273698
jiwonly <ysjjw2003@naver.com>
2026-01-15 07:31:44 +0000

    feat(infra): add public subnet

--- infra/aws/vpc/outputs.tf
+++ infra/aws/vpc/outputs.tf
@@ -9,3 +9,15 @@ output "public_route_table_id" {
 output "private_route_table_id" {
   value = aws_route_table.private.id
 }
+
+output "mq_subnet_id" {
+  value = aws_subnet.public_mq.id
+}
+
+output "db_subnet_ids" {
+  value = [
+    aws_subnet.public_db1.id,
+    aws_subnet.public_db2.id,
+    aws_subnet.public_db3.id,
+  ]
+}
--- infra/aws/vpc/route_tables.tf
+++ infra/aws/vpc/route_tables.tf
@@ -1,29 +1,3 @@
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "Codedang-InternetFacing"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  route {
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "Codedang-Public-RT"
-  }
-}
-
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
