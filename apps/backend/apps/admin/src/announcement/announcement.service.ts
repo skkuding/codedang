059d11448c2d5b1a1c02fdcbd71c7cde9cf2413b
westsunh <westsunh@gmail.com>
2026-01-20 16:26:01 +0900

    docs(be): add  jsdoc to createAnnouncement

--- apps/backend/apps/admin/src/announcement/announcement.service.ts
+++ apps/backend/apps/admin/src/announcement/announcement.service.ts
@@ -7,6 +7,13 @@ import type { UpdateAnnouncementInput } from './model/update-announcement.input'
 export class AnnouncementService {
   constructor(private readonly prisma: PrismaService) {}
 
+  /**
+   * 특정 대회에 새로운 공지를 등록합니다.
+   *
+   * @param {number} contestId contestId 대회 ID
+   * @param {CreateAnnouncementInput} input 공지 생성 데이터 (problemOrder 문제 순서 - optional, content 내용)
+   * @returns 등록된 공지 정보
+   */
   async createAnnouncement(contestId: number, input: CreateAnnouncementInput) {
     const { problemOrder, content } = input
 
