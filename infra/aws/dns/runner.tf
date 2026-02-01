57ed6734a9354cd509911920f7b6594ff032698f
Tasoo Park <tasoo1118@gmail.com>
2026-02-01 17:10:22 +0900

    Disable trraform DNS entry 'run.stage.codedang.com'

--- infra/aws/dns/runner.tf
+++ infra/aws/dns/runner.tf
@@ -8,10 +8,11 @@
 #   ttl     = 300
 # }
 
-resource "aws_route53_record" "runner_stage" {
-  name    = "run.stage.codedang.com"
-  zone_id = data.aws_route53_zone.codedang.zone_id
-  type    = "A"
-  records = local.stage_cluster_ip
-  ttl     = 300
-}
+# TODO: Add to terraform when stage runner service migration is complete
+# resource "aws_route53_record" "runner_stage" {
+#   name    = "run.stage.codedang.com"
+#   zone_id = data.aws_route53_zone.codedang.zone_id
+#   type    = "A"
+#   records = local.stage_cluster_ip
+#   ttl     = 300
+# }
