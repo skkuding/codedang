f30017b734367b66dfc5f1c8a5ddd75fc29ae6bb
Clover229 <jeonsumin2002@gmail.com>
2026-02-04 15:08:48 +0900

    chore(fe): move skeleton code

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/statistics/_components/StatisticsPage.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/statistics/_components/StatisticsPage.tsx
@@ -2,7 +2,6 @@
 
 import { FetchErrorFallback } from '@/components/FetchErrorFallback'
 import { Separator } from '@/components/shadcn/separator'
-import { Skeleton } from '@/components/shadcn/skeleton'
 import { Switch } from '@/components/shadcn/switch'
 import {
   Tabs,
@@ -17,6 +16,7 @@ import { useState } from 'react'
 import { ProblemStatisticsPage } from './ProblemStatistics'
 import { RealtimeLearBoardPage } from './RealtimeLeaderBoard'
 import { ProblemStatisticsSkeletonWithSidebar } from './StatisticsSkeletons'
+import { RealtimeLearBoardSkeleton } from './StatisticsSkeletons'
 import { UserAnalysisPage } from './UserAnalysis'
 
 export function StatisticsPage() {
@@ -76,34 +76,5 @@ export function StatisticsPage() {
         </TabsContent>
       </div>
     </Tabs>
-  )
-}
-
-//suspense component page가 생기면 옮길 예정
-function RealtimeLearBoardSkeleton() {
-  return (
-    <div className="flex flex-col">
-      <div className="mb-4 flex h-[104px] w-full justify-around gap-2">
-        <Skeleton className="w-70 h-[104px]" />
-        <Skeleton className="w-70 h-[104px]" />
-        <Skeleton className="w-70 h-[104px]" />
-        <Skeleton className="h-[104px] w-[304px]" />
-      </div>
-      <Skeleton className="h-[68px] w-full" />
-      <div className="mt-15 flex w-full flex-col gap-2">
-        <div className="flex h-10 w-full justify-around gap-1">
-          <Skeleton className="w-15 h-10 rounded-full" />
-          <Skeleton className="w-15 h-10 rounded-full" />
-          <Skeleton className="w-55 h-10 rounded-full" />
-          <Skeleton className="w-30 h-10 rounded-full" />
-          <Skeleton className="h-10 w-full rounded-full" />
-        </div>
-        <Skeleton className="h-18 w-full rounded-full" />
-        <Skeleton className="h-18 w-full rounded-full" />
-        <Skeleton className="h-18 w-full rounded-full" />
-        <Skeleton className="h-18 w-full rounded-full" />
-        <Skeleton className="h-18 w-full rounded-full" />
-      </div>
-    </div>
   )
 }
