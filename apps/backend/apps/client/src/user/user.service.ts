6feb018a2edc82f6e5947ec779804dff4cfb93f8
zero1177 <younghur07@gmail.com>
2026-02-04 12:24:35 +0000

    docs(be): add client user jsdoc

--- apps/backend/apps/client/src/user/user.service.ts
+++ apps/backend/apps/client/src/user/user.service.ts
@@ -93,9 +93,12 @@ export class UserService {
   }
 
   /**
+   * 회원가입시 이메일 주소가 중복되지 않고/형식이 올바른지 체크하고 인증을 위한 pin번호를 전송합니다.
    *
    * @param {string} email 이메일 주소
-   * @return
+   * @throws {DuplicateFoundException} 기존에 존재하는 이메일 주소면 예외 발생
+   * @throws {UnprocessableDataException} @skku.edu로 끝나지 않는 메일이면 예외 발생
+   * @return {Promise<string>} pin번호 전송 성공 메시지
    */
   async sendPinForRegisterNewEmail({ email }: UserEmailDto): Promise<string> {
     const duplicatedUser = await this.getUserCredentialByEmail(email)
@@ -169,7 +172,6 @@ export class UserService {
    * @param {number} numberOfDigits 생성하려는 pin 번호의 자릿수
    * @returns 입력한 자릿수의 랜덤 pin 번호를 반환합니다.
    */
-
   createPinRandomly(numberOfDigits: number): string {
     const pin = randomInt(0, Number('1'.padEnd(numberOfDigits + 1, '0')))
       .toString()
@@ -179,11 +181,11 @@ export class UserService {
   }
 
   /**
-   * 사용자 비밀번호를 업데이트합니다.
+   * 사용자 비밀번호가 올바른 형식인지 체크하고 업데이트합니다.
    *
    * @param {string} newPassword 변경하려는 새로운 비밀번호
    * @param {Request} req
-   * @throws {UnprocessableDataException}
+   * @throws {UnprocessableDataException} 새로운 비밀번호가 잘못된 형식일 경우 예외를 발생시킵니다.
    * @returns {Promise<string>} 비밀번호 변경이 성공했다는 메시지를 반환합니다.
    */
   async updatePassword(
@@ -201,6 +203,14 @@ export class UserService {
     return 'Password Reset successfully'
   }
 
+  /**
+   * 클라이언트 요청 헤더에서 JWT 토큰을 추출해 검증하고, 성공시 pin 인증이 완료되었던 email(payload 객체 내용물)을 반환합니다.
+   *
+   * @param {Request} req 브라우저에서 보내는 HTTP 요청 객체
+   * @param {JwtVerifyOptions} jwtVerifyOption 추가로 검증할 조건 (없을 경우 빈 객체)
+   * @throws {InvalidJwtTokenException} jwt 토큰이 없거나, 만료 되었거나, 서명이 일치하지 않을 경우, 예외를 발생시킵니다.
+   * @returns {Promise<EmailAuthJwtObject>} jwt 검증 성공시 payload 객체 내용물 (pin 인증 완료된 이메일 주소)를 반환합니다.
+   */
   async verifyJwtFromRequestHeader(
     req: Request,
     jwtVerifyOptions: JwtVerifyOptions = {}
@@ -257,6 +267,12 @@ export class UserService {
     return user
   }
 
+  /**
+   * 입력한 pin번호를 검증하고 jwt 토큰을 발행합니다.
+   *
+   * @param {EmailAuthenticationPinDto} input pin번호, 이메일 주소 정보가 포함된 객체
+   * @returns {Promise<string>} 발행된 jwt 토큰을 반환합니다.
+   */
   async verifyPinAndIssueJwt({
     pin,
     email
@@ -269,6 +285,14 @@ export class UserService {
     return token
   }
 
+  /**
+   * 사용자가 입력한 pin번호와 캐시에 저장된 pin번호가 일치하는지 검증합니다.
+   *
+   * @param {string} pin 핀번호
+   * @param {string} email 이메일 주소
+   * @throws {UnidentifiedException} 입력한 pin번호와 캐시에 저장된 pin번호가 일치하지 않을 경우 예외를 발생시킵니다.
+   * @returns {boolean} pin번호가 검증되었음을 의미하는 true를 반환합니다.
+   */
   async verifyPin(pin: string, email: string): Promise<boolean> | never {
     const storedResetPin = await this.getPinFromCache(
       emailAuthenticationPinCacheKey(email)
@@ -287,29 +311,46 @@ export class UserService {
     return true
   }
 
+  /**
+   * 캐시 key를 이용해 저장된 pin번호를 조회합니다.
+   *
+   * @param {string} key 캐시에 저장된 pin번호를 찾기 위한 key값
+   * @returns 캐시에 저장된 pin번호를 반환합니다.
+   */
   async getPinFromCache(key: string) {
     const storedPin = await this.cacheManager.get<string>(key)
     this.logger.debug({ key, value: storedPin }, 'getPinFromCache')
     return storedPin
   }
 
+  /**
+   * 캐시 key를 이용해 저장된 pin번호를 삭제합니다.
+   *
+   * @param {string} key 캐시에 저장된 pin번호를 삭제하기 위한 key값
+   */
   async deletePinFromCache(key: string) {
     await this.cacheManager.del(key)
     this.logger.debug({ key }, 'deletePinFromCache')
   }
 
   /**
-   * 사용자 식별을 위해 key값(이메일 주소)과 pin번호를 value값으로 하여 유효 시간(TTL) 동안 서버에 캐시(cache)합니다.
+   * 사용자 식별을 위해 이메일 주소를 key, pin번호를 value로 하여 유효 시간(TTL) 5분 동안 서버에 캐시(cache)합니다.
    *
    * @param {string} key 사용자 식별을 위한 key값 (이메일 주소)
    * @param {string} value 캐시하는 value값 (pin 번호)
-   * @param {number} ttl 인증이 유효한 시간 (TTL)
+   * @param {number} ttl 인증이 유효한 시간 (TTL) : 5분
    */
   async setPinInCache(key: string, value: string, ttl: number) {
     await this.cacheManager.set(key, value, ttl)
     this.logger.debug({ key, value, ttl }, 'setPinInCache')
   }
 
+  /**
+   * 사용자 이메일 주소가 담겨있는 payload 객체를 인코딩 및 암호화하여 서명이 생성된 jwt 토큰을 생성합니다.
+   *
+   * @param {EmailAuthJwtPayload} payload 이메일 주소가 담긴 데이터 객체
+   * @returns payload 객체를 인코딩 및 암호화하여 서명이 생성된 jwt 토큰을 생성합니다.
+   */
   async createJwt(payload: EmailAuthJwtPayload) {
     const jwt = await this.jwtService.signAsync(payload, {
       expiresIn: EMAIL_AUTH_EXPIRE_TIME
@@ -318,6 +359,18 @@ export class UserService {
     return jwt
   }
 
+  /**
+   * 신규 회원가입 시 다양한 조건을 체크하고 통과하면 새로 생성된 user 객체를 반환합니다.
+   * (이메일 인증 여부 / 사용자 이름,비밀번호 형식 / 중복된 사용자 이름 여부 체크)
+   * @param {Request} req 클라이언트에서 보내는 HTTP 요청 객체
+   * @param {SignUpDto} signUpDto 회원가입 정보가 담긴 객체
+   * @throws {UnprocessableDataException}
+   * 1. 인증되지 않은 이메일로 가입을 시도하면 예외를 발생시킵니다.
+   * 2. 올바르지 않은 형식의 사용자 이름으로 가입을 시도하면 예외를 발생시킵니다.
+   * 3. 올바르지 않은 형식의 비밀번호로 가입을 시도하면 예외를 발생시킵니다.
+   * @throws {DuplicateFoundException} 이미 존재하는 사용자 이름으로 가입을 시도할 경우 예외를 발생시킵니다.
+   * @returns
+   */
   async signUp(req: Request, signUpDto: SignUpDto) {
     const { email } = await this.verifyJwtFromRequestHeader(req)
     if (email != signUpDto.email) {
@@ -361,6 +414,14 @@ export class UserService {
     return user
   }
 
+  /**
+   * 소셜 계정으로 회원가입 시 소셜 플랫폼에서 제공받은 계정 정보를 체크하고, user 객체를 생성 및 반환합니다.
+   *
+   * @param {SocialSignUpDto} socialSignUpDto 소셜 플랫폼에서 제공받은 계정 정보
+   * @throws {DuplicateFoundException} 이미 존재하는 사용자 이름일 경우 예외를 발생시킵니다.
+   * @throws {UnprocessableDataException} 사용자 이름이 잘못된 형식일 경우 예외를 발생시킵니다.
+   * @returns 사용자의 소셜 서비스 연동 정보를 DB에 생성하고, user 프로필을 만든 후 user 객체를 반환합니다.
+   */
   async socialSignUp(socialSignUpDto: SocialSignUpDto): Promise<User> {
     const duplicatedUser = await this.prisma.user.findUnique({
       where: {
@@ -397,6 +458,12 @@ export class UserService {
     return user
   }
 
+  /**
+   * 사용자 이름이 영어 소문자(a~z)/숫자 0~9/3자리 이상 10자리 이하를 만족하는지 체크합니다.
+   *
+   * @param {string} username 사용자 이름
+   * @returns 사용자 이름 체크 후 통과하면 true를 반환합니다.
+   */
   isValidUsername(username: string): boolean {
     const validUsername = /^[a-z0-9]{3,10}$/
     if (!validUsername.test(username)) {
@@ -406,10 +473,10 @@ export class UserService {
   }
 
   /**
-   * 입력된 비밀번호가 설정한 형식에 부합하는 올바른 비밀번호인지 검증합니다.
-   *
+   * 입력된 비밀번호가 잘못된 형식인지 검증합니다.
+   * (영어 대문자로만 or 영어 소문자로만 or 특수문자 또는 공백으로만 or 길이가 0~7자리)인 비밀번호는 잘못된 형식
    * @param {string} password 비밀번호
-   * @returns {boolean} 비밀번호가 올바른지 아닌지 반환합니다.
+   * @returns {boolean} 올바른 형식의 비밀번호일 경우 true를 반환합니다.
    */
   isValidPassword(password: string): boolean {
     const invalidPassword = /^(.{0,7}|[a-z]*|[A-Z]*|[0-9]*|[^a-zA-Z0-9]*)$/
@@ -423,7 +490,7 @@ export class UserService {
    * DB상에 새로 회원가입한 사용자(및 정보)를 추가합니다.
    *
    * @param signUpDto 회원가입 정보 DTO 객체
-   * @returns {Promise<User>} 새로 추가된 user 객체
+   * @returns {Promise<User>} 새로 추가된 user 객체를 반환합니다.
    */
   async createUser(signUpDto: SignUpDto): Promise<User> {
     const encryptedPassword = await hash(signUpDto.password, ARGON2_HASH_OPTION) //암호화
@@ -447,7 +514,7 @@ export class UserService {
    *
    * @param socialSignUpDto 소셜 플랫폼에서 제공받은 계정 정보
    * @param {number} userId 사용자 Id
-   * @returns 생성된 소셜 연동 정보 userOAuth 객체
+   * @returns 생성된 소셜 연동 정보 userOAuth 객체를 반환합니다.
    */
   async createUserOAuth(socialSignUpDto: SocialSignUpDto, userId: number) {
     const userOAuth = await this.prisma.userOAuth.create({
@@ -482,6 +549,14 @@ export class UserService {
     return userProfile
   }
 
+  /**
+   * 탈퇴 전 비밀번호를
+   *
+   * @param {string} username 사용자 이름
+   * @param {string} password 비밀번호
+   * @throws {UnidentifiedException} 잘못된 비밀번호를 입력했을 경우 예외를 발생시킵니다.
+   * @throws {ConflictFoundException}
+   */
   async deleteUser(username: string, password: string) {
     const user = await this.getUserCredential(username)
     if (!(user && (await this.jwtAuthService.isValidUser(user, password)))) {
@@ -549,6 +624,13 @@ export class UserService {
     this.logger.debug(deletedUser, 'deleted user')
   }
 
+  /**
+   * 사용자 이름이 DB에 저장되어 있을 경우 조회합니다.
+   *
+   * @param {string} username 사용자 이름
+   * @throws {EntityNotExistException} 사용자 이름이 DB상에 존재하지 않을 경우, 예외를 발생시킵니다.
+   * @returns 사용자 이름을 반환합니다.
+   */
   async getUserCredential(username: string) {
     const user = await this.prisma.user.findUnique({
       where: { username }
@@ -560,6 +642,11 @@ export class UserService {
     return user
   }
 
+  /**
+   *
+   * @param {string} username 사용자 이름
+   * @returns
+   */
   async getUserProfile(username: string) {
     const userWithProfile = await this.prisma.user.findUnique({
       where: { username },
