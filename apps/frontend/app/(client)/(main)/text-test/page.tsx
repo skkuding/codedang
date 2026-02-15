6cb58592dba5f6a0ba67756ab254e4983e6865ea
jwoojin9 <jwoojin9@naver.com>
2026-02-15 11:05:21 +0900

    feat(fe): implement typography token

---
+++ apps/frontend/app/(client)/(main)/text-test/page.tsx
@@ -0,0 +1,26 @@
+export default function TextTest() {
+  return (
+    <div className="mt-16 flex flex-col gap-4">
+      <div className="text-head1_b_40">head1_b_40</div>
+      <div className="text-head2_b_32">head2_b_32</div>
+      <div className="text-head3_sb_28">head3_sb_28</div>
+      <div className="text-head4_m_28">head4_m_28</div>
+      <div className="text-head5_sb_24">head5_sb_24</div>
+      <div className="text-head6_m_24">head6_m_24</div>
+      <div className="text-title1_sb_20">title1_sb_20</div>
+      <div className="text-title2_m_20">title2_m_20</div>
+      <div className="text-sub1_sb_18">sub1_sb_18</div>
+      <div className="text-sub2_m_18">sub2_m_18</div>
+      <div className="text-sub3_sb_16">sub3_sb_16</div>
+      <div className="text-sub4_sb_14">sub4_sb_14</div>
+      <div className="text-body1_m_16">body1_m_16</div>
+      <div className="text-body2_m_14">body2_m_14</div>
+      <div className="text-body3_r_16">body3_r_16</div>
+      <div className="text-body4_r_14">body4_r_14</div>
+      <div className="text-caption1_m_13">caption1_m_13</div>
+      <div className="text-caption2_m_12">caption2_m_12</div>
+      <div className="text-caption3_r_13">caption3_r_13</div>
+      <div className="text-caption4_r_12">caption4_r_12</div>
+    </div>
+  )
+}
