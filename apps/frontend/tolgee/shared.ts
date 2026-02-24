50f0d2ff26f7ba059cbc73555cf24504c5ac3b69
jwoojin9 <jwoojin9@naver.com>
2026-02-25 07:17:58 +0900

    chore(fe): hardcode api url

--- apps/frontend/tolgee/shared.ts
+++ apps/frontend/tolgee/shared.ts
@@ -1,8 +1,11 @@
 import { DevTools, Tolgee, FormatSimple, BackendFetch } from '@tolgee/web'
 
 const apiKey = process.env.NEXT_PUBLIC_TOLGEE_API_KEY
-const apiUrl = process.env.NEXT_PUBLIC_TOLGEE_API_URL
-const cdnUrl = process.env.NEXT_PUBLIC_TOLGEE_CDN_URL
+const apiUrl =
+  process.env.NEXT_PUBLIC_TOLGEE_API_URL || 'https://tolgee.jwoojin.com'
+const cdnUrl =
+  process.env.NEXT_PUBLIC_TOLGEE_CDN_URL ||
+  'https://assets.jwoojin.com/95fee5c1df61c243a07e39498efd9644'
 
 export const ALL_LANGUAGES = ['en', 'ko-KR']
 export const DEFAULT_LANGUAGE = 'ko-KR'
