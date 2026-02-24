c397b577d51fe54270761d11ef842bd42c945cae
Clover229 <jeonsumin2002@gmail.com>
2026-02-24 15:11:30 +0900

    fix(fe): delete throw error & import error page

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/statistics/layout.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/statistics/layout.tsx
@@ -1,6 +1,7 @@
 import { getContest } from '../leaderboard/_libs/apis/getContest'
 import { getContestLeaderboard } from '../leaderboard/_libs/apis/getContestLeaderboard'
 import { StatisticsClientLayout } from './_layout-client'
+import StatisticsError from './error'
 
 interface LayoutProps {
   children: React.ReactNode
@@ -20,16 +21,12 @@ export default async function StatisticsLayout({
     getContest({ contestId })
   ])
 
-  if (new Date(contest.endTime) > new Date()) {
-    throw new Error('Contest not finished')
-  }
-
   const hasFrozenRecords = contestLeaderboard.leaderboard.some((user) =>
     user.problemRecords.some((record) => record.isFrozen)
   )
 
-  if (hasFrozenRecords) {
-    throw new Error('Leaderboard has frozen records')
+  if (new Date(contest.endTime) > new Date() || hasFrozenRecords) {
+    return <StatisticsError />
   }
 
   return <StatisticsClientLayout>{children}</StatisticsClientLayout>
