45171b38ac127719b73cc8960cf120995d0cb5a5
seungmin Nam <nhjbest22@g.skku.edu>
2026-02-25 14:21:58 +0000

    feat(be): add RolesModule import to StudyModule

--- apps/backend/apps/client/src/study/study.module.ts
+++ apps/backend/apps/client/src/study/study.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
+import { RolesModule } from '@libs/auth'
 import { StudyController } from './study.controller'
 import { StudyService } from './study.service'
 
 @Module({
   controllers: [StudyController],
-  providers: [StudyService]
+  providers: [StudyService],
+  imports: [RolesModule]
 })
 export class StudyModule {}
