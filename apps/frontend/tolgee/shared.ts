dff061f918e9051b7b175082b4c5de5fb5e7fd5e
jwoojin9 <jwoojin9@naver.com>
2026-02-25 09:24:59 +0900

    fix(fe): restore omitted bucket

--- apps/frontend/tolgee/shared.ts
+++ apps/frontend/tolgee/shared.ts
@@ -5,7 +5,7 @@ const apiUrl =
   process.env.NEXT_PUBLIC_TOLGEE_API_URL || 'https://tolgee.jwoojin.com'
 const cdnUrl =
   process.env.NEXT_PUBLIC_TOLGEE_CDN_URL ||
-  'https://assets.jwoojin.com/95fee5c1df61c243a07e39498efd9644'
+  'https://assets.jwoojin.com/tolgee-content/95fee5c1df61c243a07e39498efd9644'
 
 export const ALL_LANGUAGES = ['en', 'ko-KR']
 export const DEFAULT_LANGUAGE = 'ko-KR'
