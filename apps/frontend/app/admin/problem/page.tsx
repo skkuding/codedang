d152100d1d6cd194f69799f3cba086ba14012f70
Seo-Eun Jang <169060897+seoeun9@users.noreply.github.com>
2026-02-28 21:41:34 +0900

    fix(fe): modify admin page design (#3460)
    ### Description
    
    이한국 교수님 요청 사항을 반영합니다.
    
    1. 정렬 안 맞음
    아래는 scroll area 컴포넌트 사용으로 해결했는데, 스크롤바 디자인이 살짝 달라지네요
    나중에 바꿔야 할 거 같아요! 일단 급한대로 pr 올려봅니다
    <img width="726" height="656" alt="image"
    src="https://github.com/user-attachments/assets/5b919d3e-6223-4456-a2dd-042aa9bdeb47"
    />
    
    ---
    
    2. 칼럼 순서
    <img width="711" height="299" alt="image"
    src="https://github.com/user-attachments/assets/4e5d2ea7-8656-4cff-a996-90d6a6c5e062"
    />
    
    ### Additional context
    
    <!-- e.g. is there anything you'd like reviewers to focus on? -->
    
    ---
    
    ### Before submitting the PR, please make sure you do the following
    
    - [ ] Read the [Contributing
    Guidelines](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md)
    - [ ] Read the [Contributing
    Guidelines](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md#pr-and-branch)
    and follow the [Commit
    Convention](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md#commit-convention)
    - [ ] Provide a description in this PR that addresses **what** the PR is
    solving, or reference the issue that it solves (e.g. `fixes #123`).
    - [ ] Ideally, include relevant tests that fail without this PR but pass
    with it.

--- apps/frontend/app/admin/problem/page.tsx
+++ apps/frontend/app/admin/problem/page.tsx
@@ -1,5 +1,6 @@
 import { FetchErrorFallback } from '@/components/FetchErrorFallback'
 import { Button } from '@/components/shadcn/button'
+import { ScrollArea } from '@/components/shadcn/scroll-area'
 import { ErrorBoundary } from '@suspensive/react'
 import Link from 'next/link'
 import { Suspense } from 'react'
@@ -10,30 +11,32 @@ import { ProblemsUploadButton } from './_components/ProblemsUploadButton'
 
 export default function Page() {
   return (
-    <div className="container mx-auto py-10">
-      <div className="flex justify-between">
-        <div>
-          <p className="text-4xl font-bold">MY PROBLEM LIST</p>
-          <p className="flex text-lg text-slate-500">
-            Here&apos;s a problem list you made
-          </p>
+    <ScrollArea>
+      <div className="container mx-auto py-10">
+        <div className="flex justify-between">
+          <div>
+            <p className="text-4xl font-bold">MY PROBLEM LIST</p>
+            <p className="flex text-lg text-slate-500">
+              Here&apos;s a problem list you made
+            </p>
+          </div>
+          <div className="flex gap-2">
+            <ProblemsUploadButton />
+            <Button variant="default" className="w-[120px]" asChild>
+              <Link href="/admin/problem/create">
+                <HiMiniPlusCircle className="mr-2 h-5 w-5" />
+                <span className="text-lg">Create</span>
+              </Link>
+            </Button>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <ProblemsUploadButton />
-          <Button variant="default" className="w-[120px]" asChild>
-            <Link href="/admin/problem/create">
-              <HiMiniPlusCircle className="mr-2 h-5 w-5" />
-              <span className="text-lg">Create</span>
-            </Link>
-          </Button>
-        </div>
+        <ProblemTabs />
+        <ErrorBoundary fallback={FetchErrorFallback}>
+          <Suspense fallback={<ProblemTableFallback />}>
+            <ProblemTable />
+          </Suspense>
+        </ErrorBoundary>
       </div>
-      <ProblemTabs />
-      <ErrorBoundary fallback={FetchErrorFallback}>
-        <Suspense fallback={<ProblemTableFallback />}>
-          <ProblemTable />
-        </Suspense>
-      </ErrorBoundary>
-    </div>
+    </ScrollArea>
   )
 }
