4167652b0bd87378d845a08190a96961223b2a6f
sONg20NOW <sgk1004s@naver.com>
2026-03-17 13:22:40 +0900

    chore(fe): rename judge page to solution page

--- apps/frontend/app/(client)/(main)/(create)/problem/create/_components/ProblemCreateContainer.tsx
+++ apps/frontend/app/(client)/(main)/(create)/problem/create/_components/ProblemCreateContainer.tsx
@@ -17,8 +17,8 @@ import { PiMagnifyingGlassFill, PiWrenchFill } from 'react-icons/pi'
 import { CheckerPage } from './CheckerPage'
 import { CollaborationPage } from './CollaborationPage'
 import { GeneratorPage } from './GeneratorPage'
-import { JudgePage } from './JudgePage'
 import { ProblemCreateContentSkeleton } from './ProblemCreateSkeletons'
+import { SolutionPage } from './SolutionPage'
 import { StatementPage } from './StatementPage'
 import { TestsPage } from './TestsPage'
 import { UploadButton } from './UploadButton'
@@ -58,7 +58,7 @@ export function ProblemCreateContainer() {
       label: 'Solution',
       text: '솔루션',
       subText: '솔루션 업로드 및 테스트 검증',
-      Component: JudgePage
+      Component: SolutionPage
     },
     {
       Icon: PiWrenchFill,
