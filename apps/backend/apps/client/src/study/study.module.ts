30bd3a059d7580e89b68f0d94768f2c69d585fcd
westsunh <westsunh@gmail.com>
2026-04-02 07:09:06 +0000

    feat(be): add StudyGateway to StudyModule

--- apps/backend/apps/client/src/study/study.module.ts
+++ apps/backend/apps/client/src/study/study.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 import { RolesModule } from '@libs/auth'
 import { StudyController } from './study.controller'
+import { StudyGateway } from './study.gateway'
 import { StudyService } from './study.service'
 
 @Module({
   controllers: [StudyController],
-  providers: [StudyService],
+  providers: [StudyService, StudyGateway],
   imports: [RolesModule]
 })
 export class StudyModule {}
