edaced88a8a6ded899515adfbb5d6b9d8cbc1d20
zero1177 <younghur07@gmail.com>
2026-04-03 05:19:52 +0000

    feat(be): implement polygon tool upload

--- apps/backend/apps/admin/src/polygon/polygon.service.ts
+++ apps/backend/apps/admin/src/polygon/polygon.service.ts
@@ -1,7 +1,40 @@
 import { Injectable } from '@nestjs/common'
+import { ToolType } from '@prisma/client'
+import type { FileUpload } from 'graphql-upload/processRequest.mjs'
+import { PolygonAMQPService } from '@libs/amqp'
 import { PrismaService } from '@libs/prisma'
+import { FileService } from './file/file.service'
 
 @Injectable()
 export class PolygonService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly fileService: FileService,
+    private readonly polygonAMQPService: PolygonAMQPService
+  ) {}
+
+  async uploadPolygonTool(
+    problemId: number,
+    toolType: ToolType,
+    file: FileUpload
+  ) {
+    //DB에 파일 저장
+    const uploadedTool = await this.fileService.uploadPolygonToolFile(
+      problemId,
+      toolType,
+      file
+    )
+
+    //RabbitMQ 메시지 발행
+    await this.polygonAMQPService.publishPolygonToolUploadMessage(
+      uploadedTool.problemId,
+      uploadedTool.toolType
+    )
+
+    return uploadedTool
+  }
+
+  async deletePolygonTool(problemId: number, toolType: ToolType) {
+    return this.fileService.deletePolygonFile(problemId, toolType)
+  }
 }
