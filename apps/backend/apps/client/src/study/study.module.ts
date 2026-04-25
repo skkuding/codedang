e87f11132b54a4f4724b92f0083774c1ed192da3
westsunh <westsunh@gmail.com>
2026-04-25 05:41:05 +0000

    feat(be): setup BullMQ for study-room

--- apps/backend/apps/client/src/study/study.module.ts
+++ apps/backend/apps/client/src/study/study.module.ts
@@ -1,5 +1,8 @@
+import { BullModule } from '@nestjs/bullmq'
 import { Module } from '@nestjs/common'
 import { RolesModule } from '@libs/auth'
+import { StudyRoomProcessor } from './study-room.processor'
+import { STUDY_ROOM_QUEUE } from './study-room.service'
 import { StudyRoomService } from './study-room.service'
 import { StudyController } from './study.controller'
 import { StudyGateway } from './study.gateway'
@@ -7,7 +10,10 @@ import { StudyService } from './study.service'
 
 @Module({
   controllers: [StudyController],
-  providers: [StudyService, StudyGateway, StudyRoomService],
-  imports: [RolesModule]
+  providers: [StudyService, StudyGateway, StudyRoomService, StudyRoomProcessor],
+  imports: [
+    RolesModule,
+    BullModule.registerQueue({ name: STUDY_ROOM_QUEUE }) // 'study-room
+  ]
 })
 export class StudyModule {}
