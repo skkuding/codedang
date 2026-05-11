c170454306a76e08eb21a188d108a27e44762b82
오종희 <loverduck123@naver.com>
2026-05-11 20:48:39 +0900

    fix(fe): cleanup code and refine design

--- apps/frontend/components/auth/SignUpPage/signup.schema.ts
+++ apps/frontend/components/auth/SignUpPage/signup.schema.ts
@@ -2,48 +2,45 @@ import * as v from 'valibot'
 
 export const signupSchema = v.pipe(
   v.object({
-    name: v.pipe(v.string(), v.minLength(1, '이름을 입력해주세요.')),
+    name: v.pipe(v.string(), v.minLength(1, '이름을 입력해주세요')),
 
     birth: v.pipe(
       v.string(),
-      v.length(6, '생년월일 6자리를 입력해 주세요.'),
-      v.regex(/^\d{6}$/, '생년월일 6자리를 입력해 주세요.')
+      v.length(6, '생년월일 6자리를 입력해 주세요'),
+      v.regex(/^\d{6}$/, '생년월일 6자리를 입력해 주세요')
     ),
 
     userId: v.pipe(
       v.string(),
-      v.minLength(3, '아이디는 3자 이상이어야 합니다.'),
-      v.maxLength(10, '아이디는 10자 이하여야 합니다.'),
-      v.regex(
-        /^[a-z0-9]+$/,
-        '아이디는 영문 소문자와 숫자만 사용할 수 있습니다.'
-      )
+      v.minLength(3, '아이디는 3자 이상이어야 합니다'),
+      v.maxLength(10, '아이디는 10자 이하여야 합니다'),
+      v.regex(/^[a-z0-9]+$/, '아이디는 영문 소문자와 숫자만 사용할 수 있습니다')
     ),
 
     password: v.pipe(
       v.string(),
       v.minLength(
         8,
-        '대문자, 소문자, 숫자 중 2종류 이상 포함 8-20자를 입력해주세요.'
+        '대문자, 소문자, 숫자 중 2종류 이상 포함 8-20자를 입력해주세요'
       ),
       v.maxLength(
         20,
-        '대문자, 소문자, 숫자 중 2종류 이상 포함 8-20자를 입력해주세요.'
+        '대문자, 소문자, 숫자 중 2종류 이상 포함 8-20자를 입력해주세요'
       ),
       v.regex(
         /^(?:(?=.*[a-z])(?=.*[A-Z])|(?=.*[a-z])(?=.*\d)|(?=.*[A-Z])(?=.*\d)).+$/,
-        '대문자, 소문자, 숫자 중 2종류 이상 포함 8-20자를 입력해주세요.'
+        '대문자, 소문자, 숫자 중 2종류 이상 포함 8-20자를 입력해주세요'
       )
     ),
 
     passwordConfirm: v.pipe(
       v.string(),
-      v.minLength(1, '비밀번호 확인을 입력해주세요.')
+      v.minLength(1, '비밀번호 확인을 입력해주세요')
     ),
 
     nickname: v.string(),
 
-    job: v.pipe(v.string(), v.minLength(1, '직업을 선택해주세요.')),
+    job: v.pipe(v.string(), v.minLength(1, '직업을 선택해주세요')),
 
     university: v.string(),
 
@@ -53,14 +50,14 @@ export const signupSchema = v.pipe(
       v.string(),
       v.check(
         (val) => val === '' || /^\d{10}$/.test(val),
-        '학번 10자리를 입력해주세요.'
+        '학번 10자리를 입력해주세요'
       )
     ),
 
     email: v.pipe(
       v.string(),
-      v.minLength(1, '이메일을 입력해주세요.'),
-      v.email('올바른 이메일 형식이 아닙니다.')
+      v.minLength(1, '이메일을 입력해주세요'),
+      v.email('올바른 이메일 형식이 아닙니다')
     ),
 
     terms: v.boolean(),
@@ -71,7 +68,7 @@ export const signupSchema = v.pipe(
   v.forward(
     v.check(
       ({ password, passwordConfirm }) => password === passwordConfirm,
-      '비밀번호가 일치하지 않습니다.'
+      '비밀번호가 일치하지 않습니다'
     ),
     ['passwordConfirm']
   )
