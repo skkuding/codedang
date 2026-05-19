760c94257f932b6b8b3b514f803d530bd1ee9608
sONg20NOW <sgk1004s@naver.com>
2026-05-20 00:02:25 +0900

    chore(fe): fix type 'commmon' to 'common'

--- apps/frontend/app/admin/course/[courseId]/_components/CourseQnaAnsweredTab.tsx
+++ apps/frontend/app/admin/course/[courseId]/_components/CourseQnaAnsweredTab.tsx
@@ -9,7 +9,7 @@ export function CourseQnaAnsweredTab() {
 
   return (
     <div>
-      <div className="bg-color-commmon-100 flex rounded-full border border-[#D8D8D8] p-[5px]">
+      <div className="bg-color-common-100 flex rounded-full border border-[#D8D8D8] p-[5px]">
         {['All Questions', 'Unanswered Question', 'Private Question'].map(
           (tab) => (
             <Button
@@ -19,7 +19,7 @@ export function CourseQnaAnsweredTab() {
                 'w-[190px] flex-1 py-2 text-base font-normal',
                 activeTab === tab
                   ? 'bg-primary border-primary text-white hover:border-blue-600'
-                  : 'bg-color-commmon-100 hover:bg-color-neutral-99 text-[#808080]'
+                  : 'bg-color-common-100 hover:bg-color-neutral-99 text-[#808080]'
               )}
             >
               {tab}
