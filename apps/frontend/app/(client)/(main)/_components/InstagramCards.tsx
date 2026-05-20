05e91901a6e92ba7bb3f4a998e615f33281dbd43
Kang Gyu Song <sgk1004s@naver.com>
2026-05-20 05:45:13 +0000

    feat(fe): redesign instagram cards

--- apps/frontend/app/(client)/(main)/_components/InstagramCards.tsx
+++ apps/frontend/app/(client)/(main)/_components/InstagramCards.tsx
@@ -27,28 +27,35 @@ export function InstagramCards() {
   }, [])
 
   return (
-    <div className="mx-auto w-full max-w-7xl px-4 py-8">
-      <span className="mb-10 block text-3xl font-semibold leading-[120%] tracking-[-0.9px]">
-        TAKE OUR NEWS
-      </span>
-
-      <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {posts.map((post) => (
-          <InstagramCard key={post.id} post={post} />
-        ))}
+    <div className="w-full max-w-[1360px]">
+      <div className="mb-7 flex items-center justify-between">
+        <span className="text-head1_b_40">
+          우리의 최신 소식은 여기에 있어요
+        </span>
+        <div className="flex flex-col items-center justify-center">
+          <Link
+            href="https://www.instagram.com/skkuding/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-color-neutral-30 hover:text-color-neutral-10 flex items-center justify-center gap-1"
+          >
+            <span className="text-sub2_m_18">인스타그램 바로가기</span>
+            <RightArrowIcon className="h-[18px]" />
+          </Link>
+        </div>
       </div>
 
-      <div className="flex flex-col items-center justify-center pt-10">
-        <Link
-          href="https://www.instagram.com/skkuding/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="flex items-center justify-center gap-1 rounded-full bg-black py-[10px] pl-[30px] pr-5 text-xl text-white hover:opacity-75"
-        >
-          <span>Go to Instagram</span>
-          <RightArrowIcon className="text-color-neutral-90 h-[18px] w-[18px]" />
-        </Link>
-      </div>
+      {posts.length > 0 ? (
+        <div className="mb-[78px] grid grid-cols-1 gap-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {posts.map((post) => (
+            <InstagramCard key={post.id} post={post} />
+          ))}
+        </div>
+      ) : (
+        <div className="mb-[78px] grid place-items-center">
+          게시물을 불러오는 데 실패했습니다
+        </div>
+      )}
     </div>
   )
 }
