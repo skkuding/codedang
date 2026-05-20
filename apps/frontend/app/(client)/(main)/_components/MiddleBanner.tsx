e9c769530a1db86ef8cbc3c63acdb9a043e0dc4c
Kang Gyu Song <sgk1004s@naver.com>
2026-05-20 02:22:28 +0000

    chore(fe): adjust gap between subtext and button

--- apps/frontend/app/(client)/(main)/_components/MiddleBanner.tsx
+++ apps/frontend/app/(client)/(main)/_components/MiddleBanner.tsx
@@ -10,7 +10,7 @@ export function MiddleBanner() {
         <h2 className="md:text-head2_b_32 text-sub1_sb_18 text-color-common-100 mb-3">
           코딩 문제, 직접 만들어보세요
         </h2>
-        <p className="text-body3_r_16 text-color-neutral-99 mb-8 hidden max-w-[500px] whitespace-pre-line md:flex">
+        <p className="text-body3_r_16 text-color-neutral-99 mb-7 hidden max-w-[500px] whitespace-pre-line md:flex">
           {
             '학습 목적에 맞게 문제를 설계하고 협업자를 초대하여 피드백을 받아\n효과적인 코딩 문제를 구성해보세요.'
           }
