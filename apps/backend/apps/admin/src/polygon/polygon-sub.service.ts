1b0ae2eb715919b5b4430d25398555caafe0cf72
zero1177 <younghur07@gmail.com>
2026-05-22 04:20:57 +0000

    feat(be): add consuming logic for message result & change name of submissionId

--- apps/backend/apps/admin/src/polygon/polygon-sub.service.ts
+++ apps/backend/apps/admin/src/polygon/polygon-sub.service.ts
@@ -3,6 +3,8 @@ import { plainToInstance } from 'class-transformer'
 import { validateOrReject, ValidationError } from 'class-validator'
 import { Span } from 'nestjs-otel'
 import { PolygonAMQPService } from '@libs/amqp'
+import { UnprocessableDataException } from '@libs/exception'
+import { PrismaService } from '@libs/prisma'
 import {
   GeneratorResultDto,
   ValidatorResultDto
@@ -12,7 +14,10 @@ import {
 export class PolygonSubscriptionService implements OnModuleInit {
   private readonly logger = new Logger(PolygonSubscriptionService.name)
 
-  constructor(private readonly amqpService: PolygonAMQPService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly amqpService: PolygonAMQPService
+  ) {}
 
   onModuleInit() {
     this.amqpService.setMessageHandlers({
@@ -42,6 +47,13 @@ export class PolygonSubscriptionService implements OnModuleInit {
     this.amqpService.startValidatorSubscription()
   }
 
+  /**
+   * Generator를 실행한 결과 메세지를 class-validator를 통해 검증합니다.
+   *
+   * @param msg RabbitMQ에서 전달받은 raw 메세지 객체
+   * validateOrReject(): DTO 인스턴스가 데코레이터 조건을 만족하면 통과하고, 만족하지 않으면 예외를 던지는 메서드
+   * @returns 검증을 거친 GeneratorResultDto 객체
+   */
   @Span()
   async validateGeneratorResultMessage(
     msg: object
@@ -55,6 +67,13 @@ export class PolygonSubscriptionService implements OnModuleInit {
     return res
   }
 
+  /**
+   * Validator를 실행한 결과 메세지를 class-validator를 통해 검증합니다.
+   *
+   * @param msg RabbitMQ에서 전달받은 raw 메세지 객체
+   * validateOrReject(): DTO 인스턴스가 데코레이터 조건을 만족하면 통과하고, 만족하지 않으면 예외를 던지는 메서드
+   * @returns 검증을 거친 ValidatorResultDto 객체
+   */
   @Span()
   async validateValidatorResultMessage(
     msg: object
@@ -70,24 +89,52 @@ export class PolygonSubscriptionService implements OnModuleInit {
 
   @Span()
   async handleGeneratorResult(msg: GeneratorResultDto): Promise<void> {
+    const problemId = Number(msg.messageId)
+    if (!Number.isInteger(problemId)) {
+      throw new UnprocessableDataException(
+        'Invalid generator exercution messageId'
+      )
+    }
+
+    const lastRunPass = msg.resultCode === 0
+
+    await this.prisma.polygonProblem.update({
+      where: { id: problemId },
+      data: { lastRunPass }
+    })
+
     this.logger.log(
       {
-        submissionId: msg.submissionId,
+        messageId: msg.messageId,
+        problemId,
         resultCode: msg.resultCode,
+        lastRunPass,
         generatedTestCases: msg.judgeResult.generatedTestCases,
         totalTestCases: msg.judgeResult.totalTestCases
       },
       'Handled Polygon Generator Result Message'
     )
-
-    // TODO: Generator 실행 결과 수신 후 백엔드 서비스 로직
   }
 
   @Span()
   async handleValidatorResult(msg: ValidatorResultDto): Promise<void> {
+    const problemId = Number(msg.messageId)
+    if (!Number.isInteger(problemId)) {
+      throw new UnprocessableDataException(
+        'Invalid validator execution messageId'
+      )
+    }
+
+    const lastRunPass = msg.resultCode === 0
+
+    await this.prisma.polygonProblem.update({
+      where: { id: problemId },
+      data: { lastRunPass }
+    })
+
     this.logger.log(
       {
-        submissionId: msg.submissionId,
+        messageId: msg.messageId,
         resultCode: msg.resultCode,
         isValid: msg.judgeResult.isValid,
         testcaseCount: msg.judgeResult.testcaseCount
