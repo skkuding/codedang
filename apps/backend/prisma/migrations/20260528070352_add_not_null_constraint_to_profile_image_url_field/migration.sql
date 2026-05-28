30d8624f663a4d5ce8d23d3c54e53dce63aea868
zero1177 <younghur07@gmail.com>
2026-05-28 07:44:25 +0000

    chore(be): set default profileImageUrl seed into username & add not null constraint

---
+++ apps/backend/prisma/migrations/20260528070352_add_not_null_constraint_to_profile_image_url_field/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - Made the column `profile_image_url` on table `user_profile` required. This step will fail if there are existing NULL values in that column.
+
+*/
+
+-- Set default profileImageUrl seed to username for users
+UPDATE "public"."user_profile"
+SET "profile_image_url" = 'https://api.dicebear.com/9.x/notionists/svg?seed=' || (
+  SELECT "username" FROM "public"."user" WHERE "user"."id" = "user_profile"."user_id"
+);
+
+-- Make profileImageUrl not null
+ALTER TABLE "public"."user_profile" ALTER COLUMN "profile_image_url" SET NOT NULL;
+
+
+
