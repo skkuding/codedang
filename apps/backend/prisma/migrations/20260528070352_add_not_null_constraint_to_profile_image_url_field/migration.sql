36a0831b7b848be4a91f7e37ef36a9961299a2fd
zero1177 <younghur07@gmail.com>
2026-06-02 12:16:09 +0000

    chore(be): add where condition

--- apps/backend/prisma/migrations/20260528070352_add_not_null_constraint_to_profile_image_url_field/migration.sql
+++ apps/backend/prisma/migrations/20260528070352_add_not_null_constraint_to_profile_image_url_field/migration.sql
@@ -9,7 +9,8 @@
 UPDATE "public"."user_profile"
 SET "profile_image_url" = 'https://api.dicebear.com/9.x/notionists/svg?seed=' || (
   SELECT "username" FROM "public"."user" WHERE "user"."id" = "user_profile"."user_id"
-);
+)
+WHERE "profile_image_url" IS NULL;
 
 -- Make profileImageUrl not null
 ALTER TABLE "public"."user_profile" ALTER COLUMN "profile_image_url" SET NOT NULL;
