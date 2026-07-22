fc69733658d28e71be3e2ab9e02836d6e2f2bf86
keulma <101859077+keulma@users.noreply.github.com>
2026-07-22 05:58:32 +0000

    feat(infra): deploy infra onboarding practice (#3659)
    ### Description
    
    인프라 온보딩 실습 페이지 배포입니다.
    
    ### Additional context
    
    ---
    
    ### Before submitting the PR, please make sure you do the following
    
    - [ ] Read the [Contributing
    Guidelines](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md)
    - [ ] Read the [Contributing
    Guidelines](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md#pr-and-branch)
    and follow the [Commit
    Convention](https://github.com/skkuding/next/blob/main/CONTRIBUTING.md#commit-convention)
    - [ ] Provide a description in this PR that addresses **what** the PR is
    solving, or reference the issue that it solves (e.g. `fixes #123`).
    - [ ] Ideally, include relevant tests that fail without this PR but pass
    with it.
    
    Closes TAS-2810
    
    ---------
    
    Co-authored-by: jjyoung0403 <jjyoung0403@jeong.localdomain>
    Co-authored-by: 박태수 (PARK TASOO) <tasoo1118@gmail.com>

--- infra/aws/dns/_onboarding.tf
+++ infra/aws/dns/_onboarding.tf
@@ -1,5 +1,4 @@
 # Purpose: Onboarding for fresh team members
-
 # Example
 # resource "aws_route53_record" "example_first_task" {
 #   name    = "example.first-task.codedang.com"
@@ -32,3 +31,11 @@ resource "aws_route53_record" "kjk-test-app" {
   records = local.stage_cluster_ip
   ttl     = 300
  }
+  
+resource "aws_route53_record" "junyoung-terrform-practice" {
+  name    = "junyoung.stage.codedang.com"
+  zone_id = data.aws_route53_zone.codedang.zone_id
+  type = "A"
+  records = local.stage_cluster_ip
+  ttl = 300
+}
