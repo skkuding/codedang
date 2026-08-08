677896285244c5aa0d9308ef5ad50535cb308ca8
mikeyoo0405 <mikeyoo0405@gmail.com>
2026-08-08 09:26:31 +0900

    fix(fe): revert next env changes

--- apps/frontend/next-env.d.ts
+++ apps/frontend/next-env.d.ts
@@ -1,7 +1,5 @@
-/* eslint-disable */
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-/// <reference path="./.next/types/routes.d.ts" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
