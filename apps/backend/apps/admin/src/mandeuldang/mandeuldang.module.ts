5a2bd75427216f7ad86055eadd3999ea938e9316
qkrrudals886-boop <qkrrudals886@g.skku.edu>
2026-08-16 20:23:39 +0900

    chore: move mandeuldang.module.ts to lowercase mandeuldang folder

--- apps/backend/apps/admin/src/mandeuldang/mandeuldang.module.ts
+++ apps/backend/apps/admin/src/mandeuldang/mandeuldang.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AMQPModule } from '@libs/amqp'
 import { RolesModule } from '@libs/auth'
-import { MandeuldangPublicationService } from './Mandeuldang-pub.service'
-import { MandeuldangSubscriptionService } from './Mandeuldang-sub.service'
-import { MandeuldangResolver } from './Mandeuldang.resolver'
-import { MandeuldangService } from './Mandeuldang.service'
+import { MandeuldangPublicationService } from './mandeuldang-pub.service'
+import { MandeuldangSubscriptionService } from './mandeuldang-sub.service'
+import { MandeuldangResolver } from './mandeuldang.resolver'
+import { MandeuldangService } from './mandeuldang.service'
 import { FileService } from './file/file.service'
 
 @Module({
