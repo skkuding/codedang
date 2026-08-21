241374bc6ce03ca2bec13b3b6885507579d62a6d
han25-ya <78807337+han25-ya@users.noreply.github.com>
2026-08-21 10:13:48 +0900

    Simplify condition for no test cases in submission
    Refactor condition to simplify logic for test case handling.

--- apps/backend/apps/client/src/submission/submission.service.ts
+++ apps/backend/apps/client/src/submission/submission.service.ts
@@ -521,9 +521,7 @@ export class SubmissionService {
       )
 
       if (
-        testcasesCount === 0 &&
-        submission.contestId &&
-        judgeOnlyHiddenTestcases
+        testcasesCount === 0
       ) {
         // 채점할 테스트 케이스가 없는 경우 정답 및 후속 처리 후 return
         await this.subscribe.updateSubmissionResult(submission.id, true) // 문제 정답 후속처리
