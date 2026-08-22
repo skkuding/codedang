414ccef5f512e5e31ef4d131ca58571f4b37148e
lukekeum <lukekeum11@gmail.com>
2026-08-22 05:59:58 +0000

    test(be): change parseResonse to parseJudgerResponse

--- apps/backend/apps/client/src/submission/test/submission-sub.service.spec.ts
+++ apps/backend/apps/client/src/submission/test/submission-sub.service.spec.ts
@@ -216,9 +216,9 @@ describe('SubmissionSubscriptionService', () => {
     })
   })
 
-  describe('parseResponse', () => {
+  describe('parseJudgerResponse', () => {
     it('should return JudgerResponse', async () => {
-      const result = await service.parseResponse(msg)
+      const result = await service.parseJudgerResponse(msg)
 
       expect(result).to.be.deep.equal(msg)
     })
@@ -231,7 +231,7 @@ describe('SubmissionSubscriptionService', () => {
         judgeResult
       }
 
-      await expect(service.parseResponse(invalidMsg)).to.be.rejected
+      await expect(service.parseJudgerResponse(invalidMsg)).to.be.rejected
     })
   })
 
