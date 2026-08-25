46f1d866cad91442526b13958f72c07492243084
오종희 <loverduck123@naver.com>
2026-08-25 15:25:41 +0900

    fix(fe): adjust leaderboard skeleton to current padding

--- apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/leaderboard/_components/LeaderboardSkeleton.tsx
+++ apps/frontend/app/(client)/(main)/contest/[contestId]/@tabs/leaderboard/_components/LeaderboardSkeleton.tsx
@@ -2,13 +2,13 @@ import { Skeleton } from '@/components/shadcn/skeleton'
 
 export function LeaderboardSkeleton() {
   return (
-    <div className="relative ml-[116px] w-screen pb-[120px]">
-      <div className="mt-[96px] flex flex-row items-center">
+    <div className="w-[1208px] pb-[120px]">
+      <div className="mt-[80px] flex flex-row items-center">
         <Skeleton className="h-[34px] w-[280px] rounded-md" />
         <Skeleton className="ml-8 h-8 w-8 rounded-full" />
       </div>
       <div className="mb-[62px] mt-[30px]">
-        <Skeleton className="h-[52px] w-[600px] rounded-full" />
+        <Skeleton className="h-10 w-[600px] rounded-full" />
       </div>
       <div className="flex flex-col">
         <div className="flex flex-row space-x-1 pb-[22px]">
