7a6400105b9f791ec6311051a1f24f78e297a1ba
han25-ya <hanjunjo57@gmail.com>
2026-09-02 22:32:07 +0900

    Reapply "feat(be): implement Mandeuldang problem read queries and legacy compatibility (#3732)"
    This reverts commit 1503f5530302034aeccf47ce3f8a1346bd48e1e2.

--- apps/backend/apps/admin/src/mandeuldang/problem/resolvers/problem.resolver.ts
+++ apps/backend/apps/admin/src/mandeuldang/problem/resolvers/problem.resolver.ts
@@ -1,4 +1,54 @@
-import { Resolver } from '@nestjs/graphql'
+import { Args, Context, Int, Query, Resolver } from '@nestjs/graphql'
+import { ProblemStatus } from '@prisma/client'
+import { AuthenticatedRequest, UseDisableAdminGuard } from '@libs/auth'
+import { CursorValidationPipe, RequiredIntPipe } from '@libs/pipe'
+import { MandeuldangProblemOutput } from '../model/problem.output'
+import { MandeuldangProblemService } from '../services/problem.service'
 
-@Resolver()
-export class MandeuldangProblemResolver {}
+@Resolver(() => MandeuldangProblemOutput)
+@UseDisableAdminGuard()
+export class MandeuldangProblemResolver {
+  constructor(private readonly problemService: MandeuldangProblemService) {}
+
+  @Query(() => [MandeuldangProblemOutput])
+  async getMyMandeuldangProblems(
+    @Context('req') req: AuthenticatedRequest,
+    @Args('cursor', { nullable: true, type: () => Int }, CursorValidationPipe)
+    cursor: number | null,
+    @Args('take', { defaultValue: 10, type: () => Int }) take: number,
+    @Args('status', { nullable: true, type: () => ProblemStatus })
+    status?: ProblemStatus
+  ) {
+    return await this.problemService.getMyProblems(
+      req.user.id,
+      cursor,
+      take,
+      status
+    )
+  }
+
+  @Query(() => [MandeuldangProblemOutput])
+  async getInProgressMandeuldangProblems(
+    @Context('req') req: AuthenticatedRequest,
+    @Args('cursor', { nullable: true, type: () => Int }, CursorValidationPipe)
+    cursor: number | null,
+    @Args('take', { defaultValue: 10, type: () => Int }) take: number,
+    @Args('status', { nullable: true, type: () => ProblemStatus })
+    status?: ProblemStatus
+  ) {
+    return await this.problemService.getInProgressProblems(
+      req.user.id,
+      cursor,
+      take,
+      status
+    )
+  }
+
+  @Query(() => MandeuldangProblemOutput)
+  async getMandeuldangProblem(
+    @Context('req') req: AuthenticatedRequest,
+    @Args('id', { type: () => Int }, new RequiredIntPipe('id')) id: number
+  ) {
+    return await this.problemService.getProblem(id, req.user.id, req.user.role)
+  }
+}
