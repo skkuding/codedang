217b24ef0a438ab24df359976cd13e6d7c468db1
Suuuuug <shj20041224@gmail.com>
2026-09-04 12:14:12 +0900

    test(be): implement spec files for update and publish api

--- apps/backend/apps/admin/src/mandeuldang/problem/resolvers/problem.resolver.ts
+++ apps/backend/apps/admin/src/mandeuldang/problem/resolvers/problem.resolver.ts
@@ -95,9 +95,6 @@ export class MandeuldangProblemResolver {
     @Context('req') req: AuthenticatedRequest,
     @Args('id', { type: () => Int }) id: number
   ) {
-    return await this.mandeuldangProblemService.publishProblem(
-      id,
-      req.user.id
-    )
+    return await this.mandeuldangProblemService.publishProblem(id, req.user.id)
   }
 }
