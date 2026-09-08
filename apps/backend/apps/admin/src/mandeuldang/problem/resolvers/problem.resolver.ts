7a36d7f410909a82f48c1fbfd490e22613869e87
yubbbbbbi <yubinjjang01@gmail.com>
2026-09-08 19:23:30 +0900

    feat(be): implement Mandeuldang problem deletion mutation

--- apps/backend/apps/admin/src/mandeuldang/problem/resolvers/problem.resolver.ts
+++ apps/backend/apps/admin/src/mandeuldang/problem/resolvers/problem.resolver.ts
@@ -1,5 +1,6 @@
-import { Args, Context, Mutation, Resolver } from '@nestjs/graphql'
+import { Args, Context, Int, Mutation, Resolver } from '@nestjs/graphql'
 import { UseDisableAdminGuard, type AuthenticatedRequest } from '@libs/auth'
+import { RequiredIntPipe } from '@libs/pipe'
 import { CreateMandeuldangProblemInput } from '../model/problem.input'
 import { MandeuldangProblemOutput } from '../model/problem.output'
 import { MandeuldangProblemService } from '../services/problem.service'
@@ -20,5 +21,13 @@ export class MandeuldangProblemResolver {
       input,
       req.user.id
     )
+  }
+
+  @Mutation(() => MandeuldangProblemOutput)
+  async deleteMandeuldangProblem(
+    @Context('req') req: AuthenticatedRequest,
+    @Args('id', { type: () => Int }, new RequiredIntPipe('id')) id: number
+  ) {
+    return await this.mandeuldangProblemService.deleteProblem(id, req.user.id)
   }
 }
--- apps/backend/apps/admin/src/mandeuldang/problem/services/problem.service.ts
+++ apps/backend/apps/admin/src/mandeuldang/problem/services/problem.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { ForbiddenException, Injectable } from '@nestjs/common'
 import {
   CollaboratorRole,
   CollaboratorStatus,
@@ -8,12 +8,16 @@ import {
 import { MAX_DATE } from '@libs/constants'
 import { UnprocessableDataException } from '@libs/exception'
 import { PrismaService } from '@libs/prisma'
+import { StorageService } from '@libs/storage'
 import type { CreateMandeuldangProblemInput } from '../model/problem.input'
 import type { MandeuldangProblemOutput } from '../model/problem.output'
 
 @Injectable()
 export class MandeuldangProblemService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly storageService: StorageService
+  ) {}
 
   async createProblem(
     input: CreateMandeuldangProblemInput,
@@ -67,5 +71,61 @@ export class MandeuldangProblemService {
       myRole: CollaboratorRole.Owner,
       testFileCount: 0
     }
+  }
+
+  async deleteProblem(id: number, userId: number) {
+    const problem = await this.prisma.problem.findFirstOrThrow({
+      where: {
+        id,
+        creationMode: ProblemCreationMode.Mandeuldang
+      },
+      include: {
+        mandeuldangCollaborators: {
+          where: {
+            userId,
+            role: CollaboratorRole.Owner
+          }
+        }
+      }
+    })
+
+    // 해당 문제의 Owner만 삭제할 수 있다.
+    if (problem.mandeuldangCollaborators.length === 0) {
+      throw new ForbiddenException('Only the owner can delete this problem')
+    }
+
+    // Problem description에 이미지가 포함되어 있다면 삭제
+    const uuidImageFileNames = this.extractUUIDs(problem.description)
+
+    if (uuidImageFileNames.length > 0) {
+      await this.prisma.file.deleteMany({
+        where: {
+          filename: {
+            in: uuidImageFileNames
+          }
+        }
+      })
+
+      const deleteFromS3Results = uuidImageFileNames.map((filename: string) => {
+        return this.storageService.deleteFile(filename)
+      })
+
+      await Promise.all(deleteFromS3Results)
+    }
+
+    return await this.prisma.problem.delete({
+      where: { id }
+    })
+  }
+
+  extractUUIDs(input: string | null) {
+    if (!input) {
+      return []
+    }
+
+    const uuidRegex =
+      /[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/gi
+
+    return input.match(uuidRegex) ?? []
   }
 }
