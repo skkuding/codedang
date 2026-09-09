c48305aed5bd28a8bb1a78f6415659d76e5b8630
yubbbbbbi <yubinjjang01@gmail.com>
2026-09-09 12:24:32 +0900

    fix(be): import StorageModule in MandeuldangProblemModule

--- apps/backend/apps/admin/src/mandeuldang/problem/problem.module.ts
+++ apps/backend/apps/admin/src/mandeuldang/problem/problem.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
+import { StorageModule } from '@libs/storage'
 import { MandeuldangProblemResolver } from './resolvers/problem.resolver'
 import { MandeuldangProblemService } from './services/problem.service'
 
 @Module({
+  imports: [StorageModule],
   providers: [MandeuldangProblemService, MandeuldangProblemResolver]
 })
 export class MandeuldangProblemModule {}
