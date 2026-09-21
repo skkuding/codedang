5d847ee6c66800765f6477218dd0c5eb5dba82fb
howso245-dot <howso245@gmail.com>
2026-09-21 15:14:18 +0900

    feat(fe): add inputfield style

--- apps/frontend/app/admin/course/_components/FormStyles.ts
+++ apps/frontend/app/admin/course/_components/FormStyles.ts
@@ -1,22 +1,19 @@
 import { cva } from 'class-variance-authority'
 
-export const formVariants = cva(
-  'w-full border bg-color-common-100 disabled:bg-color-neutral-95 placeholder:text-color-neutral-90',
-  {
-    variants: {
-      size: {
-        large: 'h-[56px] rounded-[12px] ',
-        middle: 'h-[46px] rounded-[12px] ',
-        small: 'h-[38px] rounded-[10px] '
-      },
-      status: {
-        default: 'border-line',
-        error: 'border-error'
-      }
+export const formVariants = cva('w-full border bg-color-common-100', {
+  variants: {
+    size: {
+      large: 'h-[56px] rounded-[12px] ',
+      middle: 'h-[46px] rounded-[12px] ',
+      small: 'h-[38px] rounded-[10px] '
     },
-    defaultVariants: {
-      size: 'middle',
-      status: 'default'
+    status: {
+      default: 'border-line',
+      error: 'border-error'
     }
+  },
+  defaultVariants: {
+    size: 'middle',
+    status: 'default'
   }
-)
+})
