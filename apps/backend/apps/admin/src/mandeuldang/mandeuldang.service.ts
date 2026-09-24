602547c7330f30afe61eb5c28213e9db34020d18
james <cyunsang@gmail.com>
2026-09-24 14:33:24 +0000

    fix(be): return run request from runGenerator

--- apps/backend/apps/admin/src/mandeuldang/mandeuldang.service.ts
+++ apps/backend/apps/admin/src/mandeuldang/mandeuldang.service.ts
@@ -48,7 +48,7 @@ export class MandeuldangService {
     generatorArgs: string[],
     testcaseCount: number
   ) {
-    await this.publicationService.publishGeneratorMessage(
+    return await this.publicationService.publishGeneratorMessage(
       problemId,
       requesterId,
       generatorArgs,
